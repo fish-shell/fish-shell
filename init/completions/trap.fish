00be44a69aa053ca54fd0ca1c6f0bda4f5630d08
axel <axel@liljencrantz.se>
2005-12-18 07:59:26 +1000

    Add completions for trap and jobs
    darcs-hash:20051217215926-ac50b-3078bbc64adfe19d17579a886a9e2ae7b0c594dd.gz

---
+++ init/completions/trap.fish
@@ -0,0 +1,4 @@
+
+complete -c trap -s l -l list-signals -d 'Display names of all signals'
+complete -c trap -s p -l print -d 'Display all currently defined trap handlers'
+complete -c trap -s h -l help -d 'Display help and exit'

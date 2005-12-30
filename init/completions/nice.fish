8f121dce9c74e67d726a13c114d055ba96c246a9
axel <axel@liljencrantz.se>
2005-12-31 02:28:35 +1000

    Add completions for nice and renice commands
    darcs-hash:20051230162835-ac50b-28220c8ef0c4f22dd1c8825c7a1151fe92ba421c.gz

---
+++ init/completions/nice.fish
@@ -0,0 +1,5 @@
+
+complete -c nice -s n -l adjustment -r -d "Increment priority by specified number first"
+complete -c nice -l help -d "Display help and exit"
+complete -c nice -l version -d "Display version and exit"
+
---
+++ init/completions/renice.fish
@@ -0,0 +1,4 @@
+
+complete -c renice -s p -d "Force following parameters to be process ID’s (The default)"
+complete -c renice -s g -d "Force following parameters to be interpreted as process group ID’s"
+complete -c renice -s u -d "Force following parameters to be interpreted as user names"

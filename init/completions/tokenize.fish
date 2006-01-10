d9663a4ec4e032917127b154ee6130c9cbf93312
axel <axel@liljencrantz.se>
2006-01-11 02:36:03 +1000

    Remove the external tokenize command. One can use tr instead
    darcs-hash:20060110163603-ac50b-204bef8d8d8e3f2ebc593c3de7d5a2eb4b1f0312.gz

--- init/completions/tokenize.fish
+++
@@ -1,3 +0,0 @@
-complete -c tokenize -s e -l with-empty -d (_ "Allow empty tokens")
-complete -c tokenize -s n -l no-empty -d (_ "Ignore empty tokens")
-complete -c tokenize -s d -l delimiter -d (_ "Delimiters for tokenization")

95d68e48f23e497f3b9e5df352101d3dc8759ea0
axel <axel@liljencrantz.se>
2006-01-13 11:02:45 +1000

    Fix the __fish_append shellscript function, as well as some completion bugs in gpg and mount
    darcs-hash:20060113010245-ac50b-9780d63c65c73f2d61247cce484549ad3561fb74.gz

--- init/completions/functions.fish
+++ init/completions/functions.fish
@@ -1,5 +1,5 @@
 complete -c functions -s e -l erase -d (_ "Erase function") -x -a "(functions -n)"
-complete -c functions -xa "(functions -n)" -d (_ "Function")
+complete -c functions -xa "(functions -na)" -d (_ "Function")
 complete -c functions -s a -l all -d (_ "Show hidden functions")
 complete -c functions -s h -l help -d (_ "Display help and exit")
 complete -c functions -s d -l description -d (_ "Set function description") -x

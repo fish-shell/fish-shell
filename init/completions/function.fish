70351b0e4fbff2c5f2e58fd5eb8553f973714ee7
axel <axel@liljencrantz.se>
2006-01-20 01:00:41 +1000

    Add missing completion for the -b switch for function builtin
    darcs-hash:20060119150041-ac50b-a8d27c60747a47c63f3d89e6bb94b70b9f976ee1.gz

--- init/completions/function.fish
+++ init/completions/function.fish
@@ -5,3 +5,4 @@ complete -c function -s j -l on-job-exit -d (_ "Make the function a job exit eve
 complete -c function -s p -l on-process-exit -d (_ "Make the function a process exit event handler") -x
 complete -c function -s s -l on-signal -d (_ "Make the function a signal event handler") -x
 complete -c function -s v -l on-variable -d (_ "Make the function a variable update event handler") -x
+complete -c function -s b -l key-binding -d (_ "Allow dash (-) in function name")

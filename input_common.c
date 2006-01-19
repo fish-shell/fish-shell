e0c2ce24fb0f2c7732ebd0aba1c41173fecc99f7
axel <axel@liljencrantz.se>
2006-01-19 22:23:22 +1000

    Add missing includes in input_common.c
    darcs-hash:20060119122322-ac50b-8d0c2bc21ec137d6cd5bf845156d0fc68b005440.gz

--- input_common.c
+++ input_common.c
@@ -9,6 +9,8 @@ Implementation file for the low level input library
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/time.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <wchar.h>
 

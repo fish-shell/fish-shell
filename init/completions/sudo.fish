631acd529de997d1e9273aa7ff8db1b9731da69d
axel <axel@liljencrantz.se>
2006-01-28 12:20:54 +1000

    Fix typo in sudo completions
    darcs-hash:20060128022054-ac50b-8d859c4481c34c5830e9b5fa9e436e165160547e.gz

--- init/completions/sudo.fish
+++ init/completions/sudo.fish
@@ -4,7 +4,7 @@
 # Generate a list of commands
 #
 
-complete -c sudo -d (_ "Command to run") -x -a "(__fish_complete_commands)"
+complete -c sudo -d (_ "Command to run") -x -a "(__fish_complete_command)"
 
 complete -c sudo -s h -d (_ "Display help and exit")
 complete -c sudo -s v -d (_ "Validate")

fbe66f54aae2eefe04ebcd4844fc85328d0a1bd3
axel <axel@liljencrantz.se>
2006-02-03 01:31:51 +1000

    Make completion of subsommands for time and sudo a bit better at differing between command and subcommand
    darcs-hash:20060202153151-ac50b-d943052af165dc345366b49a5db413f635a2f28a.gz

--- init/completions/sudo.fish
+++ init/completions/sudo.fish
@@ -4,5 +4,5 @@
 
 complete -c sudo -d (_ "Command to run") -x -a "(__fish_complete_subcommand)"
 
-complete -c sudo -s h -d (_ "Display help and exit")
-complete -c sudo -s v -d (_ "Validate")
+complete -c sudo -s h -n "__fish_no_arguments" -d (_ "Display help and exit")
+complete -c sudo -s v -n "__fish_no_arguments" -d (_ "Validate")

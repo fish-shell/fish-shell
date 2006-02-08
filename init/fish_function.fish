47b652c76e240e16875734f7ca251bab97ae6309
Nicholas Pilon <npilon@gmail.com>
2006-02-08 10:42:01 +1000

    Awk Indent Fix
    Indented the awk script for command description.
    
    darcs-hash:20060208004201-494f2-2f08b1b7999c690bd1815b6c1702529c932b364d.gz

--- init/fish_function.fish
+++ init/fish_function.fish
@@ -265,7 +265,14 @@ end
 #
 
 function __fish_describe_command -d "Command used to find descriptions for commands"
-	apropos $argv | awk -v FS=" +- +" '{split($1, names, ", "); for (name in names) if (names[name] ~ /^'"$argv"'.* *\([18]\)/) { sub("\\([18]\\)", "", names[name]); print names[name] "\t" $2; } }'
+	apropos $argv | awk -v FS=" +- +" '{
+		split($1, names, ", ");
+		for (name in names)
+			if (names[name] ~ /^'"$argv"'.* *\([18]\)/) {
+				sub("\\([18]\\)", "", names[name]);
+				print names[name] "\t" $2;
+			}
+	}'
 end
 
 #

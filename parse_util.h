49973b85dac4baf843b95ae22ffaed7f41ae43bc
axel <axel@liljencrantz.se>
2006-02-09 04:47:37 +1000

    Add source documentation for functions in parse_util.h
    darcs-hash:20060208184737-ac50b-e86de902a966621501eea1b706b181bcda91fb61.gz

--- parse_util.h
+++ parse_util.h
@@ -23,23 +23,34 @@ int parse_util_locate_cmdsubst( const wchar_t *in,
 								const wchar_t **end,
 								int allow_incomplete );
 
-
+/**
+   Find the beginning and end of the command substitution under the cursor
+*/
 void parse_util_cmdsubst_extent( const wchar_t *buff,
 								 int cursor_pos,
 								 const wchar_t **a, 
 								 const wchar_t **b );
 
+/**
+   Find the beginning and end of the process definition under the cursor
+*/
 void parse_util_process_extent( const wchar_t *buff,
 								int pos,
 								const wchar_t **a, 
 								const wchar_t **b );
 
 
+/**
+   Find the beginning and end of the job definition under the cursor
+*/
 void parse_util_job_extent( const wchar_t *buff,
 							int pos,
 							const wchar_t **a, 
 							const wchar_t **b );
 
+/**
+   Find the beginning and end of the token under the cursor
+*/
 void parse_util_token_extent( const wchar_t *buff,
 							  int cursor_pos,
 							  const wchar_t **tok_begin,
@@ -47,18 +58,34 @@ void parse_util_token_extent( const wchar_t *buff,
 							  const wchar_t **prev_begin, 
 							  const wchar_t **prev_end );
 
+
+/**
+   Get the linenumber at the specified character offset
+*/
 int parse_util_lineno( const wchar_t *str, int len );
 
+/**
+   Autoload the specified file, if it exists in the specified path. Do
+   not load it multiple times unless it's timestamp changes.
+
+   \param cmd the filename to search for. The suffix '.fish' is always added to this name
+   \param path_var a list of paths to search in. 
+   \param on_load a callback function to run if a suitable file is found, which has not already been run
+   \param reload wheter to recheck file timestamps on already loaded files
+*/
 int parse_util_load( const wchar_t *cmd,
 					 const wchar_t *path_var,
 					 void (*on_load)(const wchar_t *cmd),
 					 int reload );
 
+/**
+   Init the parser utility library
+*/
 void parse_util_init();
 
-
+/**
+   Free resources used by the parser utility library
+*/
 void parse_util_destroy();
-
-
 
 #endif

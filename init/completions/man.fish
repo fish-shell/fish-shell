1c2d3583e70d7f69be5b60bc23a6862b4483253f
axel <axel@liljencrantz.se>
2006-02-15 12:48:22 +1000

    Drop fuction from man completion file, already lives in it's own file
    darcs-hash:20060215024822-ac50b-dbb1e3769f0b3101f634d4bf5f5e530aacc1dfda.gz

--- init/completions/man.fish
+++ init/completions/man.fish
@@ -1,32 +1,4 @@
 
-function __fish_complete_man
-	if test (commandline -ct)
-
-		# Try to guess what section to search in. If we don't know, we
-		# use [^)]*, which should match any section
-
-		set section ""
-		set prev (commandline -poc)
-		set -e prev[1]
-		while count $prev
-			switch $prev[1]
-			case '-**'
-				
-			case '*'
-				set section $prev[1]
-			end
-			set -e prev[1]
-		end
-		
-		set section $section"[^)]*"
-
-		# Do the actual search
-		apropos (commandline -ct) | grep \^(commandline -ct) | sed -n -e 's/\([^ ]*\).*(\('$section'\)) *- */\1\t\2: /p'
-	end
-end
-
-
-
 complete -xc man -a "(__fish_complete_man)"
 
 complete -xc man -a 1 -d (_ "Program section")

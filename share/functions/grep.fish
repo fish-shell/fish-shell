ea0005e16d2e621f35913fb9c4f768f47666953b
axel <axel@liljencrantz.se>
2006-02-20 23:02:03 +1000

    Move code for setting ls and grep colors to function definition files
    darcs-hash:20060220130203-ac50b-feb426f6450a0bbd17cb9dacdca94d4d5b72c113.gz

---
+++ share/functions/grep.fish
@@ -0,0 +1,13 @@
+#
+# Match colors for grep, if supported
+#
+
+if grep --color=auto --help 1>/dev/null 2>/dev/null
+	if not set -q GREP_COLOR 
+		set -gx GREP_COLOR '97;45'
+	end
+	if not set -q GREP_OPTIONS 
+		set -gx GREP_OPTIONS '97;45'
+	end
+end
+

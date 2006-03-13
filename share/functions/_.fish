de50539c026391bb7da789fc0d2c2fd38edb336f
npilon <npilon@gmail.com>
2006-03-13 11:44:58 +1000

    gettext detection fix
    The current method of gettext detection is broken on (at least) OS X and Solaris, where which always returns successfully. This method seems to work on Linux, OS X, and Solaris.
    
    darcs-hash:20060313014458-c90d9-8b169581a8632b6a47c61310ef3c976abd67d8e0.gz

--- share/functions/_.fish
+++ share/functions/_.fish
@@ -3,7 +3,7 @@
 # Alias for gettext (or a fallback if gettext isn't installed)
 #
 
-if which gettext ^/dev/null >/dev/null
+if test -x (which gettext) ^/dev/null >/dev/null
 	function _ -d "Alias for the gettext command"
 		gettext fish $argv
 	end

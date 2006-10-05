8ace1739f59a27dce42fbe8018f59ed903cbb49e
axel <axel@liljencrantz.se>
2006-10-05 16:44:19 +1000

    Add missing newline in error message for popd function. Thanks to Phipip Ganchev.
    darcs-hash:20061005064419-ac50b-4770f393ea02761a5f4b4044b456d1b618d6da23.gz

--- share/functions/popd.fish
+++ share/functions/popd.fish
@@ -3,7 +3,7 @@ function popd -d (N_ "Pop dir from stack")
 	if test $dirstack[1]
 		cd $dirstack[1]
 	else
-		printf (_ "%s: Directory stack is empty...") popd
+		printf (_ "%s: Directory stack is empty...\n") popd
 		return 1
 	end
 

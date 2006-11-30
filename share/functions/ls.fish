b8f2f469454f8114b95f3eecf8c608ea61e370ad
axel <axel@liljencrantz.se>
2006-12-01 01:40:26 +1000

    Use dircolors to set default colors for ls
    darcs-hash:20061130154026-ac50b-d6d5e182a71516aa56ab16851e47510f2561b585.gz

--- share/functions/ls.fish
+++ share/functions/ls.fish
@@ -12,45 +12,11 @@ if command ls --version 1>/dev/null 2>/dev/null
 		command ls $param $argv
 	end
 
-	set -l color_document 35
-	set -l color_image '01;35'
-	set -l color_sound '01;35'
-	set -l color_video '01;35'
-	set -l color_archive '01;31'
-	set -l color_command '01;32'
-	set -l color_backup 37
-
-	set -l default no=00 fi=00 'di=01;34' 'ln=01;36' 'pi=40;33' 'so=01;35' 'bd=40;33;01' 'cd=40;33;01' 'or=01;05;37;41' 'mi=01;05;37;41' ex=$color_command 
-
-	for i in .cmd .exe .com .btm .bat .sh .csh .fish
-		set default $default "*$i=$color_command"
+	if not set -q LS_COLORS
+		if type -f dircolors >/dev/null
+			eval (dircolors -c)
+		end
 	end
-		
-	for i in .tar .tgz .arj .taz .lhz .zip .z .Z .gz .bz2 .bz .tz .rpm .cpio .jar .deb .rar .bin .hqx
-		set default $default "*$i=$color_archive"
-	end
-
-	for i in .jpg .jpeg .gif .bmp .xbm .xpm .png .tif 
-		set default $default "*$i=$color_image"
-	end
-
-	for i in .mp3 .au .wav .aiff .ogg .wma 
-		set default $default "*$i=$color_sound"
-	end
-
-	for i in .avi .mpeg .mpg .divx .mov .qt .wmv .rm
-		set default $default "*$i=$color_video"
-	end
-
-	for i in .htm .html .rtf .wpd .doc .pdf .ps .xls .swf .txt .tex .sxw .dvi INSTALL README ChangeLog
-		set default $default "*$i=$color_document" 
-	end
-	
-	for i in '~' .bak
-		set default $default "*$i=$color_backup" 
-	end
-
-	set -gx LS_COLORS $default
 
 else
 	# BSD, OS X and a few more support colors through the -G switch instead

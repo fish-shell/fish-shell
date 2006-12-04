60769903a5bd78a962160ff31946634e3e2e2f74
axel <axel@liljencrantz.se>
2006-12-04 21:13:46 +1000

    Add subcommand completion support to nice completions
    darcs-hash:20061204111346-ac50b-913f68c7fcbf71117ac6d662423d64d05d38c6e5.gz

--- share/completions/nice.fish
+++ share/completions/nice.fish
@@ -1,5 +1,8 @@
 
-complete -c nice -s n -l adjustment -r -d (N_ "Increment priority by specified number first")
-complete -c nice -l help -d (N_ "Display help and exit")
-complete -c nice -l version -d (N_ "Display version and exit")
+
+complete -c nice -a "(__fish_complete_subcommand -- -n --adjustment)" -d (N_ "Command")
+
+complete -c nice -s n -l adjustment -n "__fish_no_arguments" -d (N_ "Add specified amount to niceness value") -x
+complete -c nice -l help -n "__fish_no_arguments" -d (N_ "Display help and exit")
+complete -c nice -l version -n "__fish_no_arguments" -d (N_ "Display version and exit")
 

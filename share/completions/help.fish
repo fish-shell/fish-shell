df4a0d65bd7ddca5bd9a8737824dd5accbabb481
axel <axel@liljencrantz.se>
2006-12-06 23:10:20 +1000

    Update comlpetions for the help function
    darcs-hash:20061206131020-ac50b-f7f1a3c5aa85d90a6538583681167abca7f50e63.gz

--- share/completions/help.fish
+++ share/completions/help.fish
@@ -2,17 +2,14 @@
 # Completions for the help command
 #
 
-for i in (builtin -n)
-	complete -c help -x -a $i -d (N_ "Help for the specified builtin")
-end
-
 for i in case (sed -n < $__fish_help_dir/commands.html -e "s/.*<h2><a class=\"anchor\" name=\"\([^\"]*\)\">.*/\1/p")
 	complete -c help -x -a $i -d (N_ "Help for the specified command")
 end
 
-for i in syntax todo bugs history;
-	complete -c help -x -a $i -d (N_ "Help section" )
-end
+complete -c help -x -a syntax -d (N_ "Introduction to the fish syntax" )
+complete -c help -x -a todo -d (N_ "Incomplete aspects of fish" )
+complete -c help -x -a bugs -d (N_ "Known fish bugs" )
+complete -c help -x -a history -d (N_ "Help on how to reuse previously entered commands" )
 
 complete -c help -x -a completion -d (N_ "Help on how tab-completion works")
 complete -c help -x -a job-control -d (N_ "Help on how job control works")
@@ -24,7 +21,6 @@ complete -c help -x -a killring -d (N_ "Help on how to copy and paste")
 complete -c help -x -a editor -d (N_ "Help on editor shortcuts")
 complete -c help -x -a variables -d (N_ "Help on environment variables")
 complete -c help -x -a color -d (N_ "Help on setting syntax highlighting colors")
-complete -c help -x -a builtin-overview -d (N_ "A short summary of all builtin commands")
 
 complete -c help -x -a globbing -d (N_ "Help on parameter expansion (Globbing)")
 complete -c help -x -a expand -d (N_ "Help on parameter expansion (Globbing)")

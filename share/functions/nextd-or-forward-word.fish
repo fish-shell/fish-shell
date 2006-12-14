c73d1651658f734250229494a23b8cf0faaa793d
axel <axel@liljencrantz.se>
2006-12-15 04:23:13 +1000

    Make the directory history keyboard shortcuts repaint the screen. Otherwise, the prompt won't reflect the new CWD. This is needed because of the reduced amounts of repainting done since 1.22.2
    darcs-hash:20061214182313-ac50b-a912bcc0fac6e1d78f116e19f31f1c50ffc409cb.gz

--- share/functions/nextd-or-forward-word.fish
+++ share/functions/nextd-or-forward-word.fish
@@ -2,6 +2,7 @@
 function nextd-or-forward-word
 	if test -z (commandline)
 		nextd
+		commandline -f repaint
 	else
 		commandline -f forward-word
 	end
--- share/functions/prevd-or-backward-word.fish
+++ share/functions/prevd-or-backward-word.fish
@@ -1,6 +1,7 @@
 function prevd-or-backward-word
     if test -z (commandline)
         prevd
+		commandline -f repaint
     else
         commandline -f backward-word
     end

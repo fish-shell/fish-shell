e56d9765d7a78b2ec47787bc30c4dc11048f1736
axel <axel@liljencrantz.se>
2007-01-13 01:20:48 +1000

    Add missing completions to ulimit
    darcs-hash:20070112152048-ac50b-b402f5e5fb9bfa0720b7b89de465d06bf66a1ac3.gz

--- share/completions/ulimit.fish
+++ share/completions/ulimit.fish
@@ -1,4 +1,7 @@
 
+complete -c ulimit -s S -l soft -d (N_ "Set or get soft limit")
+complete -c ulimit -s H -l hard -d (N_ "Set or get hard limit")
+
 complete -c ulimit -s a -l all -d (N_ "Set or get all current limits")
 complete -c ulimit -s c -l core-size -d (N_ "Maximum size of core files created")
 complete -c ulimit -s d -l data-size -d (N_ "Maximum size of a process's data segment")
@@ -10,4 +13,7 @@ complete -c ulimit -s s -l stack-size -d (N_ "Maximum stack size")
 complete -c ulimit -s t -l cpu-time -d (N_ "Maximum amount of cpu time in seconds")
 complete -c ulimit -s u -l process-count -d (N_ "Maximum number of processes available to a single user")
 complete -c ulimit -s v -l virtual-memory-size -d (N_ "Maximum amount of virtual memory available to the shell")
+
 complete -c ulimit -s h -l help -d (N_ "Display help and exit")
+
+complete -c ulimit -a "unlimited soft hard" -d (N_ "New resource limit")

542597070637d8b8fb251cfc27e55249ffcd9b1c
axel <axel@liljencrantz.se>
2007-04-24 08:01:48 +1000

    Add a fes minor completion corrections
    darcs-hash:20070423220148-ac50b-460b1d404dca7169054459ab6c7f9845dad51a05.gz

--- share/completions/echo.fish
+++ share/completions/echo.fish
@@ -3,3 +3,4 @@ complete -c echo -s e --description "Use backslash escaped characters"
 complete -c echo -s E --description "Do not use backslash escaped characters"
 complete -c echo -l help --description "Display help and exit"
 complete -c echo -l version --description "Display version and exit"
+complete -c echo -u
--- share/completions/function.fish
+++ share/completions/function.fish
@@ -7,3 +7,4 @@ complete -c function -s s -l on-signal --description "Make the function a signal
 complete -c function -s v -l on-variable --description "Make the function a variable update event handler" -x
 complete -c function -s b -l key-binding --description "Allow dash (-) in function name"
 complete -c function -s a -l argument-names --description "Specify named arguments"
+complete -c function -s S -l no-scope-shadowing --description "Do not shadow variable scope of calling function"

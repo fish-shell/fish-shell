fa9d00a81a82ecc52ed57a03d665bdc470286085
Giorgio Lando <patroclo7@gmail.com>
2007-08-04 21:09:16 +1000

    Completions for ports, a tool for port management in CRUX linux
    darcs-hash:20070804110916-cbd54-5a9d129b9cfc7bf1b8f09248631ddbfbfc0dda22.gz

---
+++ share/completions/ports.fish
@@ -0,0 +1,8 @@
+#completion for ports
+
+complete -f -c ports -s u -l update -a '(__fish_ports_dirs)' -d 'Update ports'
+complete -f -c ports -s l -l list -d 'List ports'
+complete -f -c ports -s d -l diff -d 'List version diffs between local and installed ports'
+complete -f -c ports -s v -l version -d 'Print version'
+complete -f -c ports -s h -l help -d 'Print help'
+
---
+++ share/functions/__fish_ports_dirs.fish
@@ -0,0 +1,5 @@
+# a function to print a list of ports local collections
+
+function __fish_ports_dirs -d 'Obtain a list of ports local collections'
+	ls /usr/ports
+end

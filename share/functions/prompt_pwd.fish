c9fe154c0119359c1f09e20cb8b32909b183cad2
axel <axel@liljencrantz.se>
2007-09-09 05:17:17 +1000

    Avoid using test command in prompt, makes it slightly faster
    darcs-hash:20070908191717-ac50b-c2d936bff0e7aead0aecad6aa9e024e63b72fc97.gz

--- share/functions/prompt_pwd.fish
+++ share/functions/prompt_pwd.fish
@@ -10,11 +10,13 @@ if test (uname) = Darwin
 	end
 else
 	function prompt_pwd --description "Print the current working directory, shortend to fit the prompt"
-		if test "$PWD" != "$HOME"
-			printf "%s" (echo $PWD|sed -e "s|^$HOME|~|" -e 's-/\([^/]\)\([^/]*\)-/\1-g')
-			echo $PWD|sed -n -e 's-.*/[^/]\([^/]*$\)-\1-p'
-		else
+		switch "$PWD" 
+			case "$HOME"
 			echo '~'
+
+			case '*'
+			printf "%s" (echo $PWD|sed -e "s|^$HOME|~|" -e 's-/\([^/]\)\([^/]*\)-/\1-g')
+			echo $PWD|sed -n -e 's-.*/.\([^/]*\)-\1-p'
 		end
 	end
 end

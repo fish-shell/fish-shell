23755783103bc9a59392025b1f0feae8e8931a96
liljencrantz <liljencrantz@gmail.com>
2007-09-22 00:11:21 +1000

    Add a few comments, remove debug message add function descriptions to up-or-search and down-or-search shellscript functions
    darcs-hash:20070921141121-75c98-0c6b3970f766429ba5e5c674ab3f6283f1f8824c.gz

--- share/functions/down-or-search.fish
+++ share/functions/down-or-search.fish
@@ -1,17 +1,15 @@
-function down-or-search
+function down-or-search -d "Depending on cursor position and current mode, either search forward or move down one line"
 	# If we are already in search mode, continue
 	if commandline --search-mode
 		commandline -f history-search-forward
 		return
 	end
 
-	# We are not in search mode.
+	# We are not already in search mode.
 	# If we are on the bottom line, start search mode, 
 	# otherwise move down
 	set lineno (commandline -L)
 	set line_count (commandline|wc -l)
-
-	echo on line $lineno of $line_count >&2
 
 	switch $lineno
 		case $line_count
--- share/functions/up-or-search.fish
+++ share/functions/up-or-search.fish
@@ -1,10 +1,13 @@
-function up-or-search
+function up-or-search -d "Depending on cursor position and current mode, either search backward or move up one line"
 	# If we are already in search mode, continue
 	if commandline --search-mode
 		commandline -f history-search-backward
 		return
 	end
 
+	# We are not already in search mode.
+	# If we are on the top line, start search mode, 
+	# otherwise move up
 	set lineno (commandline -L)
 
 	switch $lineno

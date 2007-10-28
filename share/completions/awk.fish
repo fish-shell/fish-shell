c9f43fb02d4c48c1d284caa7c21b599efaeed4ce
liljencrantz <liljencrantz@gmail.com>
2007-10-29 04:31:40 +1000

    Add command specific comlpetions for awk
    darcs-hash:20071028183140-75c98-0c649a62488fa155d8170ac04a876b9ab426dbc0.gz

---
+++ share/completions/awk.fish
@@ -0,0 +1,10 @@
+#
+# Command specific completions for the awk command.
+# These completions where generated from the commands
+# man page by the make_completions.py script, but may
+# have been hand edited since.
+#
+
+complete -c awk -s F --description 'Define the input field separator to be the extended regular expression ERE, before any input is read; see Regular Expressions' -r
+complete -c awk -s f --description 'Specify the pathname of the file progfile containing an awk program' -r
+complete -c awk -s v --description 'The application shall ensure that the assignment argument is in the same form as an assignment operand' -r

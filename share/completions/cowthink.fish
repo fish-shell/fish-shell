54b1986986d1332e11bfd42074b9a6a73b9db7e5
liljencrantz <liljencrantz@gmail.com>
2007-10-29 09:08:07 +1000

    Add command specific comlpetions for cowthink
    darcs-hash:20071028230807-75c98-1706c8b0f2559c2f5fa9e6055c7aa60e8fc4a2f6.gz

---
+++ share/completions/cowthink.fish
@@ -0,0 +1,19 @@
+
+
+complete -c cowthink -s e -d "Specify eye string" -r
+complete -c cowthink -s f -d "Specify cow file" -x -a '(cowthink -l|tail -n +2|tr \  \n)'
+complete -c cowthink -s h -d "Display help and exit"
+complete -c cowthink -s l -d "List all cowfiles"
+complete -c cowthink -s n -d "No word wrapping"
+complete -c cowthink -s T -d "Specify tounge string"
+complete -c cowthink -s W -d "Column width" -r
+complete -c cowthink -s b -d "Borg cow"
+complete -c cowthink -s d -d "Dead cow"
+complete -c cowthink -s g -d "Greedy cow"
+complete -c cowthink -s p -d "Paranoid cow"
+complete -c cowthink -s s -d "Stoned cow"
+complete -c cowthink -s t -d "Tired cow"
+complete -c cowthink -s w -d "Wired cow"
+complete -c cowthink -s y -d "Young cow"
+
+

6e9489f4f55d24beeecfb63fe86d94b2e504e4c3
liljencrantz <liljencrantz@gmail.com>
2007-10-29 09:06:20 +1000

    Add command specific comlpetions for cowsay
    darcs-hash:20071028230620-75c98-d137040e58b76a570234eb15603192922c4cfe01.gz

---
+++ share/completions/cowsay.fish
@@ -0,0 +1,19 @@
+
+
+complete -c cowsay -s e -d "Specify eye string" -r
+complete -c cowsay -s f -d "Specify cow file" -x -a '(cowsay -l|tail -n +2|tr \  \n)'
+complete -c cowsay -s h -d "Display help and exit"
+complete -c cowsay -s l -d "List all cowfiles"
+complete -c cowsay -s n -d "No word wrapping"
+complete -c cowsay -s T -d "Specify tounge string"
+complete -c cowsay -s W -d "Column width" -r
+complete -c cowsay -s b -d "Borg cow"
+complete -c cowsay -s d -d "Dead cow"
+complete -c cowsay -s g -d "Greedy cow"
+complete -c cowsay -s p -d "Paranoid cow"
+complete -c cowsay -s s -d "Stoned cow"
+complete -c cowsay -s t -d "Tired cow"
+complete -c cowsay -s w -d "Wired cow"
+complete -c cowsay -s y -d "Young cow"
+
+

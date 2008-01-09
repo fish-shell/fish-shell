3a2f7d0edf2ed07c44f8839d632873dc4d9596b1
liljencrantz <liljencrantz@gmail.com>
2008-01-09 13:10:24 +1000

    Add completions for the fish_indent command
    darcs-hash:20080109031024-75c98-23e2782d68822c1005eb9766ae2d14e76ed5499b.gz

---
+++ share/completions/fish_indent.fish
@@ -0,0 +1,3 @@
+complete -c fish_indent -s h -l help --description 'Display help and exit'
+complete -c fish_indent -s v -l version --description 'Display version and exit'
+complete -c fish_indent -s i -l no-indent --description 'Do not indent output, only reformat into one job per line'

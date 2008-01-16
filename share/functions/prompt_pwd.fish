b86856b454ee55cc25eba05cd3a4817eec46ef8d
liljencrantz <liljencrantz@gmail.com>
2008-01-17 08:36:21 +1000

    In prompt_pwd, if a directory name starts with a dot, include first two characters. This patch was written by Denilson F. de Sá
    darcs-hash:20080116223621-75c98-46f96c9f25d5e32cd10148d35713622e6eac50d7.gz

--- share/functions/prompt_pwd.fish
+++ share/functions/prompt_pwd.fish
@@ -2,8 +2,8 @@
 if test (uname) = Darwin
 	function prompt_pwd --description "Print the current working directory, shortend to fit the prompt"
 		if test "$PWD" != "$HOME"
-			printf "%s" (echo $PWD|sed -e 's|/private||' -e "s|^$HOME|~|" -e 's-/\([^/]\)\([^/]*\)-/\1-g')
-			echo $PWD|sed -e 's-.*/[^/]\([^/]*$\)-\1-'
+			printf "%s" (echo $PWD|sed -e 's|/private||' -e "s|^$HOME|~|" -e 's-/\(\.\?[^/]\)\([^/]*\)-/\1-g')
+			echo $PWD|sed -e 's-.*/\.\?[^/]\([^/]*$\)-\1-'
 		else
 			echo '~'
 		end
@@ -15,8 +15,8 @@ else
 			echo '~'
 
 			case '*'
-			printf "%s" (echo $PWD|sed -e "s|^$HOME|~|" -e 's-/\([^/]\)\([^/]*\)-/\1-g')
-			echo $PWD|sed -n -e 's-.*/.\([^/]*\)-\1-p'
+			printf "%s" (echo $PWD|sed -e "s|^$HOME|~|" -e 's-/\(\.\?[^/]\)\([^/]*\)-/\1-g')
+			echo $PWD|sed -n -e 's-.*/\.\?.\([^/]*\)-\1-p'
 		end
 	end
 end

1dc49a40622fe5751d9283b3f4959204c3b89d9e
axel <axel@liljencrantz.se>
2009-02-02 08:24:10 +1000

    Fix color ls detection for BSD and OS X systems. Patch by Sven Axelsson.
    darcs-hash:20090201222410-ac50b-e1e02244ae950c602d3b7e67fe567920c619024b.gz

--- share/functions/ls.fish
+++ share/functions/ls.fish
@@ -20,7 +20,7 @@ if command ls --version 1>/dev/null 2>/dev/null
 
 else
 	# BSD, OS X and a few more support colors through the -G switch instead
-	if command ls / -G 1>/dev/null 2>/dev/null
+	if command ls -G / 1>/dev/null 2>/dev/null
 		function ls --description "List contents of directory"
 			command ls -G $argv
 		end

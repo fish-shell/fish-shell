4f54da3795e90128b1efa0b4a956f2e3878b8d9e
axel <axel@liljencrantz.se>
2009-02-02 08:19:21 +1000

    Fix user completion so it can handle comments in the passwd file. Patch by Sven Axelsson.
    darcs-hash:20090201221921-ac50b-ef0883afe4dc68b43c1c705d49b6b5505dbf1e73.gz

--- share/functions/__fish_complete_users.fish
+++ share/functions/__fish_complete_users.fish
@@ -1,4 +1,4 @@
 
 function __fish_complete_users --description "Print a list of local users, with the real user name as a description"
-	cat /etc/passwd | sed -e "s/^\([^:]*\):[^:]*:[^:]*:[^:]*:\([^:]*\):.*/\1\t\2/"
+	cat /etc/passwd | grep -ve '^#' | sed -e 's/^\([^:]*\):[^:]*:[^:]*:[^:]*:\([^:]*\):.*$/\1'\t'\2/' ^/dev/null
 end

9b56b67c0e1bbd7f043ca6d17715427ab6d40e9f
Siteshwar Vashisht <siteshwar@gmail.com>
2012-01-12 04:21:11 +0530

    Revert "Modified expand.cpp and exec.cpp to use env_get_string()"
    This reverts commit 48655e882ef02efe50e1b5a05e1b0ae653861c3d.

--- exec.cpp
+++ exec.cpp
@@ -1740,7 +1740,7 @@ int exec_subshell( const wchar_t *cmd,
 	
 	CHECK( cmd, -1 );
 
-	ifs = env_get_string(L"IFS").empty()?NULL:env_get_string(L"IFS").c_str();
+	ifs = env_get_string(L"IFS").empty()?NULL:env_get_string(L"IFS");
 
 	if( ifs && ifs[0] )
 	{

78f8317de8507dbd5957922619d657348351f08e
ridiculousfish <corydoras@ridiculousfish.com>
2012-01-23 19:33:57 -0800

    Typo fixes

--- complete.cpp
+++ complete.cpp
@@ -1348,11 +1348,11 @@ static int short_ok( const wchar_t *arg,
 
 /**
    This is an event handler triggered when the definition of a
-   specifiec function is changed. It automatcally removes the
+   specified function is changed. It automatcally removes the
    specified function.
 
    This is to make sure that the function disappears if the file is
-   removed or if ti contains a syntax error.
+   removed or if it contains a syntax error.
 */
 static void complete_load_handler( const wchar_t *cmd )
 {

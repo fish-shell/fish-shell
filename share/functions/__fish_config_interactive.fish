953ab4b3cfea1e3d77b0c768cdcc7cead6bb806b
ridiculousfish <corydoras@ridiculousfish.com>
2012-04-24 11:12:04 -0700

    Clean up __fish_config_interactive a little bit

--- share/functions/__fish_config_interactive.fish
+++ share/functions/__fish_config_interactive.fish
@@ -166,7 +166,7 @@ function __fish_config_interactive -d "Initializations that should be performed 
 	#
 
 	function fish_on_exit --description "Commands to execute when fish exits" --on-process %self
-		printf (_ "Goodbye\n")
+		echo (_ "Goodbye\n")
 	end
 
 	#
@@ -232,7 +232,7 @@ function __fish_config_interactive -d "Initializations that should be performed 
 		end
 	else
 		# Ubuntu Feisty places this command in the regular path instead
-		if which command-not-found >/dev/null
+		if which -s command-not-found
 			function fish_command_not_found_handler --on-event fish_command_not_found
 				command-not-found $argv
 			end

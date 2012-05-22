87c0e0e9715126b7600e2d8edb35987595c5a851
ridiculousfish <corydoras@ridiculousfish.com>
2012-05-22 00:09:39 -0700

    Tweaked make_deb script

--- build_tools/make_deb.sh
+++ build_tools/make_deb.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-rm -f /tmp/fish_fish_deb
+# Terminate on error
+set -e
+
+rm -Rf /tmp/fish_fish_deb
 mkdir /tmp/fish_fish_deb
 git archive --format=tar fish_fish | tar -x -C /tmp/fish_fish_deb
 cd /tmp/fish_fish_deb
+autoconf
 ./configure
 make
 sudo checkinstall make install

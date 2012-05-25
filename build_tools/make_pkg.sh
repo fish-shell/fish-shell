dcd7d6769d9d19eb5e0c9284adefa005a1e1aa84
ridiculousfish <corydoras@ridiculousfish.com>
2012-05-24 23:56:07 -0700

    Updated some build scripts

--- build_tools/make_pkg.sh
+++ build_tools/make_pkg.sh
@@ -5,9 +5,9 @@ mkdir -p /tmp/fish_pkg/
 if make install prefix=/tmp/fish_pkg/usr/local/
 then
 	echo "Root written to /tmp/fish_pkg/"
-	if /Developer/usr/bin/packagemaker --doc ./build_tools/fish_shell.pmdoc --out ~/fishfish.pkg
+	if /Developer/usr/bin/packagemaker --doc ./build_tools/fish_shell.pmdoc --out ~/fish_built/fishfish.pkg
 	then
-		echo "Package written to ~/fishfish.pkg"
+		echo "Package written to ~/fish_built/fishfish.pkg"
 	else
 		echo "Package could not be written"
 	fi
--- build_tools/make_tarball.sh
+++ build_tools/make_tarball.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-rm -f /tmp/fish_fish.tar /tmp/fish_fish.tar.gz
-if git archive --format=tar fish_fish | gzip - > /tmp/fishfish.tar.gz
+rm -f ~/fish_built/fishfish.tar.gz
+if git archive --format=tar fish_fish | gzip - > ~/fish_built/fishfish.tar.gz
 then
-	echo "Tarball written to /tmp/fishfish.tar.gz"
+	echo "Tarball written to ~/fish_built/fishfish.tar.gz"
 else
 	echo "Tarball could not be written"
 fi

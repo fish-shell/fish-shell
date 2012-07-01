bc9bae0f7f93385e6a762e1c9f97f679716b34c6
ridiculousfish <corydoras@ridiculousfish.com>
2012-07-01 14:19:39 -0700

    Update make_tarball.sh to respect new branch name

--- build_tools/make_tarball.sh
+++ build_tools/make_tarball.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -f ~/fish_built/fishfish.tar.gz
-if git archive --format=tar --prefix=fishfish/ fish_fish | gzip - > ~/fish_built/fishfish.tar.gz
+if git archive --format=tar --prefix=fishfish/ master | gzip - > ~/fish_built/fishfish.tar.gz
 then
 	echo "Tarball written to ~/fish_built/fishfish.tar.gz"
 else

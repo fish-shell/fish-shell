fa820abe8309ec7f22eee4d85401b3024787bd1e
Emily Eisenberg <xymostech@gmail.com>
2012-07-06 01:41:07 -0700

    Make prompt_pwd make a lot more sense
    Remove the case statements, which were handled by the sed blocks anyway
    Move around the '/' character in the regex, so only one regex is needed
    Fix a bug where '/' is output as '//'

--- share/functions/prompt_pwd.fish
+++ share/functions/prompt_pwd.fish
@@ -1,22 +1,10 @@
 
 if test (uname) = Darwin
 	function prompt_pwd --description "Print the current working directory, shortend to fit the prompt"
-		if test "$PWD" != "$HOME"
-			printf "%s" (echo $PWD|sed -e 's|^/private\(/.\{1,\}\)|\1|' -e "s|^$HOME|~|" -e 's-/\(\.\{0,1\}[^/]\)\([^/]*\)-/\1-g')
-			echo $PWD|sed -n -e 's-.*/\.\{0,1\}.\([^/]*\)-\1-p'
-		else
-			echo '~'
-		end
+		echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||' -e 's-\([^/]\)[^/]*/-\1/-g'
 	end
 else
 	function prompt_pwd --description "Print the current working directory, shortend to fit the prompt"
-		switch "$PWD"
-			case "$HOME"
-			echo '~'
-
-			case '*'
-			printf "%s" (echo $PWD|sed -e "s|^$HOME|~|" -e 's-/\(\.\{0,1\}[^/]\)\([^/]*\)-/\1-g')
-			echo $PWD|sed -n -e 's-.*/\.\{0,1\}.\([^/]*\)-\1-p'
-		end
+		echo $PWD | sed -e "s|^$HOME|~|" -e 's-\([^/]\)[^/]*/-\1/-g'
 	end
 end

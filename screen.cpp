e3ec361552848754b7d4892031d678874e2de5b8
ridiculousfish <corydoras@ridiculousfish.com>
2012-08-17 17:41:55 -0700

    Fixed indentation in next_tab_stop

--- screen.cpp
+++ screen.cpp
@@ -118,7 +118,7 @@ static size_t next_tab_stop( size_t in )
 	/*
 	  Assume tab stops every 8 characters if undefined
 	*/
-    size_t tab_width = (init_tabs > 0 ? (size_t)init_tabs : 8);
+	size_t tab_width = (init_tabs > 0 ? (size_t)init_tabs : 8);
 	return ( (in/tab_width)+1 )*tab_width;
 }
 

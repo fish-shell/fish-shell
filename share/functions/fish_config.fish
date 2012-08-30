122791646ec7dc421a679cb39cce7a8cbc0a7594
Torsten Grust <torsten.grust@gmail.com>
2012-08-30 13:17:32 +0200

    Fix check for non-empty argument (= initial tab) in fish_config
    The original version (based on 'test') was creating spurious files named "0" in the current working directory

--- share/functions/fish_config.fish
+++ share/functions/fish_config.fish
@@ -1,7 +1,7 @@
 function fish_config --description "Launch fish's web based configuration"
 	# Support passing an initial tab like "colors" or "functions"
 	set -l initial_tab
-	if test (count $argv) > 0
+	if count $argv >/dev/null
 		set initial_tab $argv[1]
 	end
 	eval $__fish_datadir/tools/web_config/webconfig.py $initial_tab

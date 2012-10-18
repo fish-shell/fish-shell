f56f84c6acc336a93a998af311c6e31703c28801
Terje Larsen <terlar@gmail.com>
2012-10-19 01:04:59 +0200

    Use echo -n instead of printf
    - Remove useless comment, because multi-line prompt works good now.

--- share/tools/web_config/sample_prompts/terlar.fish
+++ share/tools/web_config/sample_prompts/terlar.fish
@@ -9,25 +9,24 @@ function fish_prompt --description 'Write out the prompt'
 
   # User
   set_color $fish_color_user
-  printf (whoami)
+  echo -n (whoami)
   set_color normal
 
   echo -n '@'
 
   # Host
   set_color $fish_color_host
-  printf (hostname -s)
+  echo -n (hostname -s)
   set_color normal
 
   echo -n ':'
 
   # PWD
   set_color $fish_color_cwd
-  printf (prompt_pwd)
+  echo -n (prompt_pwd)
   set_color normal
 
   __terlar_git_prompt
-  # I prefer to have a new-line here but messes with the prompt when resizing
   echo
 
   if not test $last_status -eq 0

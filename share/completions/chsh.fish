17e786070034a07478b0f9b5f95d27251f0ab17f
ridiculousfish <corydoras@ridiculousfish.com>
2012-12-01 14:12:11 -0800

    Fix chsh completion https://github.com/fish-shell/fish-shell/issues/415

--- share/completions/chsh.fish
+++ share/completions/chsh.fish
@@ -2,8 +2,8 @@
 # Completions for the chsh command
 #
 
-complete -c chsh -s s -l shell -x -a "(chsh -l)" -d "Specify your login shell"
-complete -c chsh -s l -l list-shells -d "Display the list of shells listed in /etc/shells and exit"
+# This grep tries to match nonempty lines that do not start with hash
+complete -c chsh -s s -l shell -x -a "(sgrep '^[^#]' /etc/shells)" -d "Specify your login shell"
 complete -c chsh -s u -l help -d "Display help and exit"
 complete -c chsh -s v -l version -d "Display version and exit"
 complete -x -c chsh -a "(__fish_complete_users)"

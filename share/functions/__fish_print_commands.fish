fb8f16373f6c20b729b16151436689ce10e9d489
ridiculousfish <corydoras@ridiculousfish.com>
2012-12-27 00:46:54 -0800

    Teach fish_print_commands how to use __fish_datadir

--- share/functions/__fish_print_commands.fish
+++ share/functions/__fish_print_commands.fish
@@ -1,5 +1,5 @@
 function __fish_print_commands --description "Print a list of documented fish commands"
 	if test -d $__fish_datadir/man/man1/
-		find /usr/share/fish/man/man1/ -type f -name \*.1 -execdir basename '{}' .1 ';'
+		find $__fish_datadir/man/man1/ -type f -name \*.1 -execdir basename '{}' .1 ';'
 	end
 end

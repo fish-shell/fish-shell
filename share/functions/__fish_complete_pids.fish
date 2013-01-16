8e7ed21fc54b3bac1f11af64b11885764c360a37
ridiculousfish <corydoras@ridiculousfish.com>
2013-01-16 14:11:43 -0800

    Fix proc and pid completion on OS X, and improve it on Linux. https://github.com/fish-shell/fish-shell/pull/129

--- share/functions/__fish_complete_pids.fish
+++ share/functions/__fish_complete_pids.fish
@@ -1,8 +1,15 @@
 function __fish_complete_pids -d "Print a list of process identifiers along with brief descriptions"
 	# This may be a bit slower, but it's nice - having the tty displayed is really handy
-	ps --no-heading -o pid,comm,tty --ppid %self -N | sed -r 's/ *([0-9]+) +([^ ].*[^ ]|[^ ]) +([^ ]+)$/\1'\t'\2 [\3]/' ^/dev/null
-
-	# If the above is too slow, this is faster but a little less useful
-	#	pgrep -l -v -P %self | sed 's/ /'\t'/'
+	# 'tail -n +2' deletes the first line, which contains the headers
+	# 'grep -v...' removes self from the output
+	set -l SELF %self
+	
+	# Display the tty if available
+	set -l sed_cmds 's/ *([0-9]+) +([^ ].*[^ ]|[^ ]) +([^ ]+) *$/\1'\t'\2 [\3]/'
+	
+	# But not if it's just question marks, meaning no tty
+	set -l sed_cmds $sed_cmds 's/ *\[\?*\] *$//'
+	
+	ps axc -o pid,ucomm,tty | grep -v '^\s*'$SELF'\s' | tail -n +2 | sed -E "-e "$sed_cmds  ^/dev/null
 end
 

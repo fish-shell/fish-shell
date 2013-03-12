2c7e0ef577bcb7e4b4e24ee7bcb77cf9057290fc
aliva <ali.vakilzade@gmail.com>
2013-03-12 19:13:52 +0330

    service name completion for "systemd service manager" added

--- share/completions/service.fish
+++ share/completions/service.fish
@@ -1,6 +1,13 @@
+function print_service_names
+	if type -f systemctl >/dev/null
+        command systemctl list-units  -t service | cut -d ' ' -f 1 | grep '\.service$' | sed -e 's/\.service$//'
+    end
+
+    command ls /etc/init.d
+end
 
 # Fist argument is the names of the service, i.e. a file in /etc/init.d
-complete -c service -n "test (count (commandline -poc)) = 1" -xa "(command ls /etc/init.d)" --description "Service name"
+complete -c service -n "test (count (commandline -poc)) = 1" -xa "(print_service_names)" --description "Service name"
 
 #The second argument is what action to take with the service
 complete -c service -n "test (count (commandline -poc)) -gt 1" -xa '$__fish_service_commands'

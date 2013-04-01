f32a317f20da57dbf0dc66ddf2adb0a327af8b85
Greg Dietsche <Gregory.Dietsche@cuw.edu>
2013-04-01 16:42:40 -0500

    Ignore hashed host names in ssh/known_hosts
    Valid DNS names cannot have a | character and hashed ssh hosts
    always begin with the | character. Therefore we ignore lines that
    begin with |.
    
    Signed-off-by: Greg Dietsche <Gregory.Dietsche@cuw.edu>

--- share/functions/__fish_print_hostnames.fish
+++ share/functions/__fish_print_hostnames.fish
@@ -13,7 +13,7 @@ function __fish_print_hostnames -d "Print a list of known hostnames"
 	end
 
 	# Print hosts with known ssh keys
-	cat ~/.ssh/known_hosts{,2} ^/dev/null|cut -d ' ' -f 1| cut -d , -f 1
+        cat ~/.ssh/known_hosts{,2} ^/dev/null | grep -v '^|' | cut -d ' ' -f 1| cut -d , -f 1
 
 	# Print hosts from ssh configuration file
 	if [ -e ~/.ssh/config ]

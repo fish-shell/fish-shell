22c670c3d344605058f59c4b7e9f677fe5e17694
ridiculousfish <corydoras@ridiculousfish.com>
2013-04-13 01:59:07 -0700

    Replace OS X installer PackageMaker document with a distribution file and script

--- build_tools/make_pkg.sh
+++ build_tools/make_pkg.sh
@@ -1,31 +1,10 @@
 #!/bin/sh -x
 
 make distclean
-rm -rf /tmp/fish_pkg
-mkdir -p /tmp/fish_pkg/
+rm -Rf /tmp/fish_pkg
+mkdir -p /tmp/fish_pkg/root /tmp/fish_pkg/intermediates /tmp/fish_pkg/dst
 
-# Make sure what we build can run on SnowLeopard
-export OSX_SDK="/Developer/SDKs/MacOSX10.6.sdk"
-export MACOSX_DEPLOYMENT_TARGET="10.6"
-export CC="clang -isysroot $OSX_SDK -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-export CCX="clang++ -isysroot $OSX_SDK -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-export CFLAGS="$CFLAGS -isysroot $OSX_SDK -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-export CXXFLAGS="$CXXFLAGS -isysroot $OSX_SDK -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-export LDFLAGS="$LDFLAGS -isysroot $OSX_SDK -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-autoconf
-./configure
+xcodebuild install -scheme install_tree -configuration Release DSTROOT=/tmp/fish_pkg/root/
+pkgbuild --scripts build_tools/osx_package_scripts --root /tmp/fish_pkg/root/ --identifier 'com.ridiculousfish.fish-shell-pkg' /tmp/fish_pkg/intermediates/fish.pkg
 
-# Actually build it now
-if make -j 4 DESTDIR=/tmp/fish_pkg install
-then
-	echo "Root written to /tmp/fish_pkg/"
-	if /Developer/usr/bin/packagemaker --doc ./build_tools/fish_shell.pmdoc --out ~/fish_built/fishfish.pkg
-	then
-		echo "Package written to ~/fish_built/fishfish.pkg"
-	else
-		echo "Package could not be written"
-	fi
-	
-else
-	echo "Root could not be written"
-fi
+productbuild  --package-path /tmp/fish_pkg/intermediates --distribution build_tools/osx_distribution.xml --resources build_tools/osx_package_resources/ ~/fish_built/fish.pkg

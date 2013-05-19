60652c2bd2d1bbce26c90a010913f0f0d8e9365f
Konrad Borowski <glitchmr@myopera.com>
2013-05-19 10:58:40 +0200

    Show first two characters for dotdirs. Fixes #754.

--- share/functions/prompt_pwd.fish
+++ share/functions/prompt_pwd.fish
@@ -1,10 +1,10 @@
 
 if test (uname) = Darwin
 	function prompt_pwd --description "Print the current working directory, shortend to fit the prompt"
-		echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||' -e 's-\([^/]\)[^/]*/-\1/-g'
+		echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||' -e 's-\(\.\?[^/.]\)[^/]*/-\1/-g'
 	end
 else
 	function prompt_pwd --description "Print the current working directory, shortend to fit the prompt"
-		echo $PWD | sed -e "s|^$HOME|~|" -e 's-\([^/]\)[^/]*/-\1/-g'
+		echo $PWD | sed -e "s|^$HOME|~|" -e 's-\(\.\?[^/.]\)[^/]*/-\1/-g'
 	end
 end

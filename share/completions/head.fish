505bbbd1e6600d576fa0d359f76e2aee3c04a439
Johann Weging <johann@weging.net>
2013-05-27 01:04:24 +0200

    Fixed the capitalization and shorten the descriptions.

--- share/completions/head.fish
+++ share/completions/head.fish
@@ -1,6 +1,6 @@
-complete -c head -s c -l bytes -d 'Print the first N bytes of each file; with the leading '-', print all but the last N bytes of each file' -r
-complete -c head -s n -l lines -d 'print the first N lines instead of the first 10; with the leading '-', print all but the last N lines of each file' -r
-complete -c head -s q -l quiet -l silent -d 'Never print headers giving file names'
-complete -c head -s v -l verbose -d 'Always print headers giving file names'
-complete -f -c head -l version -d 'Output version information and exit'
-complete -f -c head -l help -d 'Display help and exit'
+complete -c head -s c -l bytes -d 'Print the first N bytes; Leading '-', truncate the last N bytes' -r
+complete -c head -s n -l lines -d 'Print the first N lines; Leading '-', truncate the last N lines' -r
+complete -c head -s q -l quiet -l silent -d 'Never print file names'
+complete -c head -s v -l verbose -d 'Always print file names'
+complete -f -c head -l version -d 'Display version'
+complete -f -c head -l help -d 'Display help'

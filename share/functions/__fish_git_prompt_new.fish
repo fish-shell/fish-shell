63b886b32e5347002afbf7ca7aba1b62e19c0d1e
Harm Aarts <harmaarts@gmail.com>
2013-07-14 21:12:30 +0200

    indenting

--- share/functions/__fish_git_prompt_new.fish
+++ share/functions/__fish_git_prompt_new.fish
@@ -338,17 +338,17 @@ end
 
 function __fish_git_prompt_validate_chars --description "__fish_git_prompt helper, checks char variables"
 
-	__fish_git_prompt_set_char __fish_git_prompt_char_cleanstate  			'.'
-	__fish_git_prompt_set_char __fish_git_prompt_char_dirtystate  			'*'
-	__fish_git_prompt_set_char __fish_git_prompt_char_stagedstate  			'+'
-	__fish_git_prompt_set_char __fish_git_prompt_char_invalidstate  		'#'
-	__fish_git_prompt_set_char __fish_git_prompt_char_stashstate  			'$'
-	__fish_git_prompt_set_char __fish_git_prompt_char_untrackedfiles  		'%'
-	__fish_git_prompt_set_char __fish_git_prompt_char_upstream_equal  		'='
-	__fish_git_prompt_set_char __fish_git_prompt_char_upstream_behind 		'<'
-	__fish_git_prompt_set_char __fish_git_prompt_char_upstream_ahead  		'>'
-	__fish_git_prompt_set_char __fish_git_prompt_char_upstream_diverged  	'<>'
-	__fish_git_prompt_set_char __fish_git_prompt_char_upstream_prefix  		' '
+	__fish_git_prompt_set_char __fish_git_prompt_char_cleanstate				'.'
+	__fish_git_prompt_set_char __fish_git_prompt_char_dirtystate				'*'
+	__fish_git_prompt_set_char __fish_git_prompt_char_stagedstate				'+'
+	__fish_git_prompt_set_char __fish_git_prompt_char_invalidstate			'#'
+	__fish_git_prompt_set_char __fish_git_prompt_char_stashstate				'$'
+	__fish_git_prompt_set_char __fish_git_prompt_char_untrackedfiles		'%'
+	__fish_git_prompt_set_char __fish_git_prompt_char_upstream_equal		'='
+	__fish_git_prompt_set_char __fish_git_prompt_char_upstream_behind		'<'
+	__fish_git_prompt_set_char __fish_git_prompt_char_upstream_ahead		'>'
+	__fish_git_prompt_set_char __fish_git_prompt_char_upstream_diverged	'<>'
+	__fish_git_prompt_set_char __fish_git_prompt_char_upstream_prefix		' '
 
 end
 

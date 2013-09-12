e031fa72077140c6b0fa46aa4f8b6c350f75fedc
nulltrek <pumabit@gmail.com>
2013-09-12 11:06:45 +0200

    Fix some typos.

--- share/functions/prompt_pwd.fish
+++ share/functions/prompt_pwd.fish
@@ -1,14 +1,14 @@
 switch (uname)
 case Darwin
-	function prompt_pwd --description "Print the current working directory, shortend to fit the prompt"
+	function prompt_pwd --description "Print the current working directory, shortened to fit the prompt"
 		echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||' -e 's-\([^/.]\)[^/]*/-\1/-g'
 	end
 case 'CYGWIN_*'
-	function prompt_pwd --description "Print the current working directory, shortend to fit the prompt"
+	function prompt_pwd --description "Print the current working directory, shortened to fit the prompt"
 		echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/cygdrive/\(.\)|\1/:|' -e 's-\([^/.]\)[^/]*/-\1/-g' -e 's-^\([^/]\)/:/\?-\u\1:/-'
 	end
 case '*'
-	function prompt_pwd --description "Print the current working directory, shortend to fit the prompt"
+	function prompt_pwd --description "Print the current working directory, shortened to fit the prompt"
 		echo $PWD | sed -e "s|^$HOME|~|" -e 's-\([^/.]\)[^/]*/-\1/-g'
 	end
 end

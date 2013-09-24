fish-shell/fish-shell

commit 735af50ce9ee76f9d75dd592fb23a11e5881dc49

default key bindings: add further iTerm2 bindings

share/functions/fish_default_key_bindings.fish
@@ -79,6 +79,8 @@ function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fis
 	bind \ef forward-word
 	bind \e\[1\;5C forward-word
 	bind \e\[1\;5D backward-word
+        bind \e[1\;9B history-token-search-forward # iTerm2
+        bind \e[1\;9A history-token-search-backward # iTerm2
 	bind \e\[1\;9C forward-word #iTerm2
 	bind \e\[1\;9D backward-word #iTerm2
 	bind \ed forward-kill-word

commit 1235e60a296a80af512cbd5e97c9167eb5226aef

default key bindings: fix syntax errors introduced in 735af50

share/functions/fish_default_key_bindings.fish
@@ -79,8 +79,8 @@ function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fis
 	bind \ef forward-word
 	bind \e\[1\;5C forward-word
 	bind \e\[1\;5D backward-word
-        bind \e[1\;9B history-token-search-forward # iTerm2
-        bind \e[1\;9A history-token-search-backward # iTerm2
+        bind \e\[1\;9A history-token-search-backward # iTerm2
+        bind \e\[1\;9B history-token-search-forward # iTerm2
 	bind \e\[1\;9C forward-word #iTerm2
 	bind \e\[1\;9D backward-word #iTerm2
 	bind \ed forward-kill-word
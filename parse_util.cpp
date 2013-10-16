ef18d6f70cf33c44c5dbf03f96fa4ed194957cae
ridiculousfish <corydoras@ridiculousfish.com>
2013-10-16 01:13:57 -0700

    Revert a series of accidentally rebased commits

--- parse_util.cpp
+++ parse_util.cpp
@@ -260,7 +260,8 @@ void parse_util_cmdsubst_extent(const wchar_t *buff, size_t cursor_pos, const wc
             /* No subshell found, all done */
             break;
         }
-        /* Interpret NULL to mean the end */
+        
+        /* Intrepret NULL to mean the end */
         if (end == NULL)
         {
             end = const_cast<wchar_t *>(buff) + bufflen;
@@ -272,9 +273,6 @@ void parse_util_cmdsubst_extent(const wchar_t *buff, size_t cursor_pos, const wc
             begin++;
             ap = begin;
             bp = end;
-            /* pos is where to begin looking for the next one. But if we reached the end there's no next one. */
-            if (begin >= end)
-                break;
             pos = begin + 1;
         }
         else if (begin >= cursor)

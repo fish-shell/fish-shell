73f1b621bec38c7da64eab66716278c6204bc7a8
Jelte Fennema <jelte12345@gmail.com>
2013-11-20 02:27:41 +0100

    Applied patches suggested by zanchey

--- share/completions/eselect.fish
+++ share/completions/eselect.fish
@@ -1,14 +1,16 @@
-set __fish_eselect_cmd eselect --brief --colour=no
+function __fish_eselect_cmd
+    eselect --brief --colour=no $argv
+end
 
 function __fish_complete_eselect_modules
     set -l sedregexp 's/^  ([a-zA-Z0-9_-]*)[ ]*/\1\t/g'
-    eval $__fish_eselect_cmd modules list | grep '^  ' | sed -r $sedregexp
+    __fish_eselect_cmd modules list | sgrep '^  ' | sed -r $sedregexp
 end
 
 function __fish_complete_eselect_actions
     set -l sedregexp 's/^  ([a-zA-Z0-9_-]*)[ ]*/\1\t/g'
     set -l cmdl (commandline -poc)
-    eval $__fish_eselect_cmd $cmdl[2..-1] help | grep '^  [^ -]' | sed -r $sedregexp
+    __fish_eselect_cmd $cmdl[2..-1] help | sgrep '^  [^ -]' | sed -r $sedregexp
 end
 
 function __fish_complete_eselect_action_options
@@ -23,7 +25,7 @@ function __fish_complete_eselect_action_options
     set -l findregexp '/^  '$cmdl[-1]'/,/^  [^ ]/p'
 
     set cmdl[-1] help
-    eval $__fish_eselect_cmd $cmdl[2..-1] | sed -n -re $findregexp | grep '^    --' | sed -re $parseregexp
+    __fish_eselect_cmd $cmdl[2..-1] | sed -n -re $findregexp | sgrep '^    --' | sed -re $parseregexp
 end
 
 function __fish_complete_eselect_targets
@@ -37,7 +39,7 @@ function __fish_complete_eselect_targets
             set cmdl[-1] list
     end
 
-    eval $cmdl[1] --colour=no $cmdl[2..-1] | grep '^  [^ -]' | sed -r $sedregexp
+    eselect --colour=no $cmdl[2..-1] | sgrep '^  [^ -]' | sed -r $sedregexp
 end
 
 complete -c eselect -n "test (__fish_number_of_cmd_args_wo_opts) = 1" \
--- share/functions/__fish_print_service_names.fish
+++ share/functions/__fish_print_service_names.fish
@@ -3,8 +3,8 @@ function __fish_print_service_names -d 'All services known to the system'
         command systemctl list-units  -t service | cut -d ' ' -f 1 | grep '\.service$' | sed -e 's/\.service$//'
     else if type -f rc-service
         command rc-service -l
+    else
+        command ls /etc/init.d
     end
-
-    command ls /etc/init.d
 end
 

9e104c0bff1db4d485fa6f1677fdf0868d40c82f
Yuriy Nemtsov <nemtsov@gmail.com>
2013-11-29 15:52:19 -0500

    add info about branches and stash

--- share/tools/web_config/sample_prompts/robbyrussell.fish
+++ share/tools/web_config/sample_prompts/robbyrussell.fish
@@ -8,7 +8,17 @@ function fish_prompt
     function _git_branch_name
       echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
     end
-	
+
+    function _num_branches
+      set -l num (math (git for-each-ref refs/heads | wc -l) - 1)
+      if [ $num != 0 ]; echo " $num"B; else; echo ''; end
+    end
+
+    function _has_stash
+      git rev-parse --verify refs/stash >/dev/null 2>&1
+      if [ $status = 0 ]; echo ' S'; else; echo ''; end
+    end
+
     function _is_git_dirty
       echo (git status -s --ignore-submodules=dirty ^/dev/null)
     end
@@ -25,7 +35,9 @@ function fish_prompt
 
   if [ (_git_branch_name) ]
     set -l git_branch $red(_git_branch_name)
-    set git_info "$blue git:($git_branch$blue)"
+    set -l num_branches $red(_num_branches)
+    set -l has_stash $red(_has_stash)
+    set git_info "$blue git:($git_branch$num_branches$has_stash$blue)"
 
     if [ (_is_git_dirty) ]
       set -l dirty "$yellow ✗"

b9394b9599c2449a1f542979eba6c0dc51e13b21
Konrad Borowski <glitchmr@myopera.com>
2014-01-14 08:28:15 +0100

    Rename __fish_complete_usb function.

--- share/completions/lsusb.fish
+++ share/completions/lsusb.fish
@@ -1,5 +1,5 @@
 complete -c lsusb -s v -l verbose --description "Increase verbosity (show descriptors)"
-complete -x -c lsusb -s s -a '(__fish_complete_usb)' --description "Show only devices with specified device and/or bus numbers (in decimal)"
+complete -x -c lsusb -s s -a '(__fish_complete_lsusb)' --description "Show only devices with specified device and/or bus numbers (in decimal)"
 complete -c lsusb -s d --description "Show only devices with the specified vendor and product ID numbers (in hexadecimal)"
 complete -c lsusb -s D -l device --description "Selects which device lsusb will examine"
 complete -c lsusb -s t -l tree --description "Dump the physical USB device hierarchy as a tree"

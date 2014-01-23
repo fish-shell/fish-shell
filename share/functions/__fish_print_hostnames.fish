0ab4b1b5b8cd5a36591b80b4f4ff884faeaa17ad
Michael Stillwell <mjs@beebo.org>
2014-01-23 14:59:19 +0000

    Ignore "host", "hostname", and hostname wildcards
    See the PATTERNS section of ssh_config(5) for wildcards supported by ssh.

--- share/functions/__fish_print_hostnames.fish
+++ share/functions/__fish_print_hostnames.fish
@@ -18,13 +18,11 @@ function __fish_print_hostnames -d "Print a list of known hostnames"
 
 	# Print hosts from system wide ssh configuration file
 	if [ -e /etc/ssh/ssh_config ]
-		# Ignore lines containing wildcards
-		sgrep -Eoi '^ *host[^*]*$' /etc/ssh/ssh_config | cut -d '=' -f 2 | tr ' ' '\n'
+		sgrep -Eoi '^ *host[^*]*$' /etc/ssh/ssh_config | cut -d ' ' -f 2 | tr ' ' '\n' | sgrep -v '[\*\?\!]'
 	end
 
 	# Print hosts from ssh configuration file
 	if [ -e ~/.ssh/config ]
-		# Ignore lines containing wildcards
-		sgrep -Eoi '^ *host[^*]*$' ~/.ssh/config | cut -d '=' -f 2 | tr ' ' '\n'
+		sgrep -Eoi '^ *host[^*]*$' ~/.ssh/config | cut -d ' ' -f 2 | tr ' ' '\n' | sgrep -v '[\*\?\!]'
 	end
 end

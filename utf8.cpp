a67dd9fbdd475ce386f9d87f83e7a1c628777eff
Siteshwar Vashisht <siteshwar@gmail.com>
2014-03-23 15:09:43 +0530

    Included missing stdint.h header in utf8.cpp

--- utf8.cpp
+++ utf8.cpp
@@ -13,8 +13,9 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-#include <sys/types.h>
 
+#include <sys/types.h>
+#include <stdint.h>
 #include <wchar.h>
 
 #include "utf8.h"
@@ -22,7 +23,6 @@
 #include <string>
 #include <vector>
 #include <limits>
-
 
 #define _NXT	0x80
 #define _SEQ2	0xc0

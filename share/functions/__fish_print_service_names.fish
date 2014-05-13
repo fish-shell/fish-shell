2024e728108894ac0e82e28f22fdbc4346fc247c
Cameron Norman <CameronNemo@users.noreply.github.com>
2014-05-13 12:46:03 -0700

    Remove unnecessary stdout redirection

--- share/functions/__fish_print_service_names.fish
+++ share/functions/__fish_print_service_names.fish
@@ -1,5 +1,5 @@
 function __fish_print_service_names -d 'All services known to the system'
-    if test -d /run/systemd/system >/dev/null
+    if test -d /run/systemd/system
         command systemctl list-units  -t service | cut -d ' ' -f 1 | grep '\.service$' | sed -e 's/\.service$//'
     else if type -f rc-service
         command rc-service -l

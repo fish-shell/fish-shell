3f526698ab90286a2b7a0c5447a4f4218ef8ce36
ridiculousfish <corydoras@ridiculousfish.com>
2014-08-12 14:42:49 -0700

    Make __fish_print_users work on OS X via dscl

--- share/functions/__fish_print_users.fish
+++ share/functions/__fish_print_users.fish
@@ -2,6 +2,8 @@
 function __fish_print_users --description "Print a list of local users"
 	if test -x /usr/bin/getent
 		getent passwd | cut -d : -f 1
+	else if test -x /usr/bin/dscl # OS X support
+		dscl . -list /Users
 	else
 		sgrep -ve '^#' /etc/passwd | cut -d : -f 1
 	end

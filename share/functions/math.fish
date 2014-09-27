3ac28f2b01f802803521dfadb79bf0b2a05cef08
David Adam <zanchey@ucc.gu.uwa.edu.au>
2014-09-27 23:27:05 +0800

    Revert "math.fish: support floating-point maths"
    This reverts commit 7cad0069e8f4a652d0e22f1b0198f67399e80157.
    
    https://github.com/fish-shell/fish-shell/issues/1723
    http://superuser.com/questions/31445/gnu-bc-modulo-with-scale-other-than-0
    
    This is why we can't have nice things.

--- share/functions/math.fish
+++ share/functions/math.fish
@@ -7,13 +7,8 @@ function math --description "Perform math calculations in bc"
 				return 0
 		end
 
-		# Override the locale so that the output can be used as input
-		set -l LC_NUMERIC C
-		# GNU bc extension
-		set -l BC_LINE_LENGTH 0
-
-		set -l out (printf '%g' (echo $argv| bc -l))
-		test -z "$out"; and return 1
+		set -l out (echo $argv|env BC_LINE_LENGTH=0 bc)
+                test -z "$out"; and return 1
 		echo $out
 		switch $out
 			case 0

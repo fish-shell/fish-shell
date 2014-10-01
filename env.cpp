43e451d4d8dbc96c853d9297065322457ac9c345
Kevin Ballard <kevin@sb.org>
2014-10-01 12:33:30 -0700

    Find fishd file even when $HOME is unset
    env.cpp sets up $HOME based on the current user, if it's not inherited
    from the environment. fishd_get_config should be using the same
    calculated value of $HOME. To that end, move universal variable
    initialization to after $HOME is set up, and read the value from the
    fish environment instead of using getenv().
    
    Fixes #1725.

--- env.cpp
+++ env.cpp
@@ -530,11 +530,6 @@ void env_init(const struct config_paths_t *paths /* or NULL */)
     env_set(L"version", version.c_str(), ENV_GLOBAL);
     env_set(L"FISH_VERSION", version.c_str(), ENV_GLOBAL);
 
-    /* Set up universal variables. The empty string means to use the deafult path. */
-    assert(s_universal_variables == NULL);
-    s_universal_variables = new env_universal_t(L"");
-    s_universal_variables->load();
-
     /*
       Set up SHLVL variable
     */
@@ -569,6 +564,11 @@ void env_init(const struct config_paths_t *paths /* or NULL */)
 
     /* Set PWD */
     env_set_pwd();
+
+    /* Set up universal variables. The empty string means to use the deafult path. */
+    assert(s_universal_variables == NULL);
+    s_universal_variables = new env_universal_t(L"");
+    s_universal_variables->load();
 
     /* Set g_log_forks */
     env_var_t log_forks = env_get_string(L"fish_log_forks");

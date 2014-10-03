fabee7e246f9346924d207a945fc754bcc99e62e
Roman Inflianskas <infroma@gmail.com>
2014-10-04 00:41:10 +0400

    add yast2 completion

---
+++ share/completions/yast2.fish
@@ -0,0 +1,11 @@
+complete -f -c yast2 -a '(yast2 -l)' -d 'Module' 
+
+complete -f -c yast2 -s h -l help           -d 'Show help' 
+complete -f -c yast2 -s l -l list           -d 'List all available modules'
+complete -f -c yast2      -l qt             -d 'Use the QT graphical user interface'
+complete -f -c yast2      -l gtk            -d 'Use the GTK graphical user interface'
+complete -f -c yast2      -l ncurses        -d 'Use the NCURSES text-mode user interface'
+complete -f -c yast2 -s g -l geometry       -d 'Default window size (qt only)'
+complete -f -c yast2      -l noborder       -d 'No window manager border for main window'
+complete -f -c yast2      -l fullscreen     -d 'Use full screen'
+

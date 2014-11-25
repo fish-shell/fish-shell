190cac07a3ff7cd2c65ac24b600808e7c1674412
David Adam <zanchey@ucc.gu.uwa.edu.au>
2014-11-25 08:48:10 +0800

    grep.fish: don't set GREP_COLOR
    grep will fall back to the default colors.
    
    Closes #1316.

--- share/functions/grep.fish
+++ share/functions/grep.fish
@@ -3,9 +3,6 @@
 #
 
 if command grep --color=auto --help 1>/dev/null 2>/dev/null
-	if not set -q GREP_COLOR
-		set -gx GREP_COLOR '97;45'
-	end
 	function grep
 		command grep --color=auto $argv
 	end

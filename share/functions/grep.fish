efc3846fcfa6f0bae8312645c41b132c6156b3d1
David Adam <zanchey@ucc.gu.uwa.edu.au>
2014-11-25 08:47:30 +0800

    grep.fish: don't use GREP_OPTIONS as it is deprecated
    Closes #1825.

--- share/functions/grep.fish
+++ share/functions/grep.fish
@@ -6,8 +6,8 @@ if command grep --color=auto --help 1>/dev/null 2>/dev/null
 	if not set -q GREP_COLOR
 		set -gx GREP_COLOR '97;45'
 	end
-	if not set -q GREP_OPTIONS
-		set -gx GREP_OPTIONS --color=auto
+	function grep
+		command grep --color=auto $argv
 	end
 end
 

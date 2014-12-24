8d50b2509d2b9c1f17aa8bf3b5d4bc76811b4355
ridiculousfish <corydoras@ridiculousfish.com>
2014-12-24 13:07:28 -0800

    Update completions for fish_indent

--- share/completions/fish_indent.fish
+++ share/completions/fish_indent.fish
@@ -1,3 +1,5 @@
 complete -c fish_indent -s h -l help --description 'Display help and exit'
 complete -c fish_indent -s v -l version --description 'Display version and exit'
 complete -c fish_indent -s i -l no-indent --description 'Do not indent output, only reformat into one job per line'
+complete -c fish_indent -l ansi --description 'Colorize the output using ANSI escape sequences'
+complete -c fish_indent -l html --description 'Output in HTML format'

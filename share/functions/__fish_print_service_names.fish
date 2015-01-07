54a92d0df2cea2debabdaf6f8943be479113149a
Jelte Fennema <github-tech@jeltef.nl>
2015-01-07 16:39:19 +0100

    Ignore error message when not on a Gentoo system

--- share/functions/__fish_print_service_names.fish
+++ share/functions/__fish_print_service_names.fish
@@ -1,7 +1,7 @@
 function __fish_print_service_names -d 'All services known to the system'
     if test -d /run/systemd/system
         command systemctl list-units  -t service | cut -d ' ' -f 1 | grep '\.service$' | sed -e 's/\.service$//'
-    else if type -f rc-service
+    else if type -f rc-service 2> /dev/null
         command rc-service -l
     else
         command ls /etc/init.d

f7e2fdbc3061162234538401a5da842d1beed5bf
Geoff Nixon <geoff@geoff.codes>
2015-01-20 14:49:50 -0800

    Fix/workaround some wonkiness with isatty. See #1870.

--- share/functions/isatty.fish
+++ share/functions/isatty.fish
@@ -7,18 +7,29 @@ function isatty -d "Test if a file or file descriptor is a tty."
     case '-h*' '--h*'
       __fish_print_help isatty
 
-    case ''
-      command test -c /dev/stdin
+    case '' '-'
+      tty 0 2>&1 >/dev/null
+      return $status
+
+    case 0 1 2
+      tty $argv 2>&1 >/dev/null
+      return $status
 
     case '*'
-      if test -e "$argv" # The eval here is needed for symlinks. Unsure why.
-        command test -c "$argv"; and eval tty 0>"$argv" >/dev/null
+      if test -e "$argv"
+        command test -c "$argv"
+          and tty 0>"$argv" 2>&1 >/dev/null
+        return $status
 
       else if test -e /dev/"$argv"
-         command test -c /dev/"$argv"; and tty 0>/dev/"$argv" >/dev/null
+        command test -c /dev/"$argv"
+          and tty 0>/dev/"$argv" 2>&1 >/dev/null
+       return $status
 
       else if test -e /dev/fd/"$argv"
-         command test -c /dev/fd/"$argv"; and tty 0>/dev/fd/"$argv" >/dev/null
+        command test -c /dev/fd/"$argv"
+          and tty 0>/dev/fd/"$argv" 2>&1 >/dev/null
+        return $status
 
       else
          return 1

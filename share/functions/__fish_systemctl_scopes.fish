33c0d93b20cac1ab1e9ed41f659c80b4a0d577c1
Fabian Homborg <FHomborg@gmail.com>
2015-02-19 16:02:16 +0100

    systemctl completions: Add more unit types and commands
    It should now support all unit types mentioned in systemd.unit(5)

---
+++ share/functions/__fish_systemctl_scopes.fish
@@ -0,0 +1,11 @@
+function __fish_systemctl_scopes
+	if type -q systemctl
+		if __fish_contains_opt user
+			# Scopes are usually generated at runtime
+			# Therefore show known _units_, not unit-files
+			systemctl --user list-units --no-legend --type=scope ^/dev/null | cut -f 1 -d ' '
+		else
+			systemctl list-units --no-legend --type=scope ^/dev/null | cut -f 1 -d ' '
+		end
+	end
+end

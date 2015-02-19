242b9795733b9cbb34d317493c691679a6154ede
Fabian Homborg <FHomborg@gmail.com>
2015-02-19 14:22:21 +0100

    systemctl completions: Support user-mode

--- share/functions/__fish_systemctl_service_paths.fish
+++ share/functions/__fish_systemctl_service_paths.fish
@@ -1,5 +1,9 @@
 function __fish_systemctl_service_paths
-    if type -q systemctl
-        systemctl list-unit-files --no-legend --type=path ^/dev/null | cut -f 1 -d ' '
-    end
+	if type -q systemctl
+		if __fish_contains_opt user
+			systemctl --user list-unit-files --no-legend --type=path ^/dev/null | cut -f 1 -d ' '
+		else
+			systemctl list-unit-files --no-legend --type=path ^/dev/null | cut -f 1 -d ' '
+		end
+	end
 end

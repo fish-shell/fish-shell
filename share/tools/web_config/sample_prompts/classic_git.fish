879d03d0fe2173e1100786e39dd0bac0e6f72fa0
ridiculousfish <corydoras@ridiculousfish.com>
2015-04-23 17:53:40 -0700

    Reset the color after printing the CWD in classic+git prompt
    Fixes #2034

--- share/tools/web_config/sample_prompts/classic_git.fish
+++ share/tools/web_config/sample_prompts/classic_git.fish
@@ -91,5 +91,5 @@ function fish_prompt --description 'Write out the prompt'
 		)
 	end
 
-	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
+	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
 end

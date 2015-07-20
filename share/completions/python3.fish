f74151aab7753a7455aedf351be215b3663c58d7
Fabian Homborg <FHomborg@gmail.com>
2015-07-20 12:33:34 +0200

    Normalize python* completion

--- share/completions/python3.fish
+++ share/completions/python3.fish
@@ -1,2 +1,4 @@
-__fish_complete_python python3
+complete -c python3 -w python
+# Override this to use python2 instead of python
+complete -c python3 -s m -d 'Run library module as a script (terminates option list)' -xa "( find /usr/lib/(eval python3 -V 2>| sed 's/ //; s/\..\$//; s/P/p/') \$PYTHONPATH -maxdepth 1 -name '*.py' -printf '%f\n' | sed 's/.py//')"
 

6475d763d048618ba84c78d708160c06aa663c9b
Fabian Homborg <FHomborg@gmail.com>
2015-07-20 12:34:42 +0200

    Remove function and descriptions from rc-service completion
    The descriptions are "Start: Start the service"
    				 	 "Stop : Stop the service"
    					 "Restart: Restart the service"

--- share/functions/__fish_complete_service_actions.fish
+++
@@ -1,7 +0,0 @@
-function __fish_complete_service_actions -d "Print a list of all basic service \
-        actions"
-    set -l actions start 'Start the service'
-    set -l actions $actions stop 'Stop the service'
-    set -l actions $actions restart 'Restart the service'
-    printf "%s\t%s\n" $actions
-end

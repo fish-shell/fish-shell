c97a2498c76cebbe4cd91743ad521370a4283ed1
Fabian Homborg <FHomborg@gmail.com>
2015-07-21 11:47:10 +0200

    Fix mountpoints with spaces on linux

--- share/functions/__fish_print_mounted.fish
+++ share/functions/__fish_print_mounted.fish
@@ -2,6 +2,8 @@ function __fish_print_mounted --description 'Print mounted devices'
 	if test (uname) = Darwin
 		mount | cut -d " " -f 1-2|tr " " \n|sed -e "s/[0-9\.]*:\//\//"|sgrep "^/"
 	else
-		cat /etc/mtab | cut -d " " -f 1-2|tr " " \n|sed -e "s/[0-9\.]*:\//\//"|sgrep "^/"
+		# In mtab, spaces are replaced by a literal '\040'
+		# So it's safe to get the second "field" and then replace it
+		sed -e "s/[^ ]\+ \([^ ]\+\) .*/\\1/" -e "s/\\040/ /g" /etc/mtab
 	end
 end

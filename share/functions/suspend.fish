6334047ebd08b6a7f609bdb232f0dddb569e8a59
Mike Meyer <mwm@mired.org>
2015-08-04 08:53:10 -0500

    Add a description to suspend

--- share/functions/suspend.fish
+++ share/functions/suspend.fish
@@ -1,4 +1,4 @@
-function suspend
+function suspend -d "Suspend the current shell so long as it is not a login shell."
     if status --is-login
         echo cannot suspend login shell >&2
     else

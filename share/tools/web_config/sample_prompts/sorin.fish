b615534d8d67e1891437277fa781196111426f80
Fabian Homborg <FHomborg@gmail.com>
2015-08-16 22:49:52 +0200

    Fix sorin prompt, remove fallback
    Closes #2243

--- share/tools/web_config/sample_prompts/sorin.fish
+++ share/tools/web_config/sample_prompts/sorin.fish
@@ -1,14 +1,7 @@
 # name: Sorin
 # author: Ivan Tham <ivanthamjunhoe@gmail.com>
 
-# If login shell, don't do anything
-if status --is-login; and function fish_prompt; echo -n '% '; end; end
-
 function fish_prompt
-	if not set -q -g __prompt_sorin_functions-defined
-		set -g __prompt_sorin_functions-defined
-	end
-
 	test $SSH_TTY; and printf (set_color red)(whoami)(set_color white)'@'(set_color yellow)(hostname)' '
 
     test $USER = 'root'; and echo (set_color red)"#"

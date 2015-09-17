56399b9aa3725881a5dd8ae7529d807de0194959
Fabian Homborg <FHomborg@gmail.com>
2015-09-17 18:16:56 +0200

    systemctl completions: Show instanced services
    This unfortunately requires us to do two systemctl calls, but it's still
    pretty fast.

--- share/functions/__fish_systemctl_services.fish
+++ share/functions/__fish_systemctl_services.fish
@@ -2,8 +2,12 @@ function __fish_systemctl_services
 	if type -q systemctl
 		if __fish_contains_opt user
 			systemctl --user list-unit-files --no-legend --type=service ^/dev/null $argv | cut -f 1 -d ' '
+			systemctl --user list-units --state=loaded --no-legend --type=service ^/dev/null | cut -f 1 -d ' '
 		else
+			# list-unit-files will also show disabled units
 			systemctl list-unit-files --no-legend --type=service ^/dev/null $argv | cut -f 1 -d ' '
+			# list-units will not show disabled units but will show instances (like wpa_supplicant@wlan0.service)
+			systemctl list-units --state=loaded --no-legend --type=service ^/dev/null | cut -f 1 -d ' '
 		end
 	end
 end

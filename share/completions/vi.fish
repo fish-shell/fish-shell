be51fe944dc7d7abfb12b4aa0008d71e8b253d4d
Fabian Homborg <FHomborg@gmail.com>
2015-09-23 19:01:34 +0200

    Fix vi* completions
    I forgot a "-c".

--- share/completions/vi.fish
+++ share/completions/vi.fish
@@ -8,18 +8,17 @@
 if vi --version > /dev/null ^ /dev/null
     complete -c vi -w vim
 else
-    complete vi -s s --description 'Suppress all interactive user feedback'
-    complete vi -s C --description 'Encrypt/decrypt text'
-    complete vi -s l --description 'Set up for editing LISP programs'
-    complete vi -s L --description 'List saved file names after crash'
-    complete vi -s R --description 'Read-only mode'
-    complete vi -s S --description 'Use linear search for tags if tag file not sorted'
-    complete vi -s v --description 'Start in display editing state'
-    complete vi -s V --description 'Verbose mode'
-    complete vi -s x --description 'Encrypt/decrypt text'
-
-    complete vi -r -s r --description 'Recover file after crash'
-    complete vi -r -s t --description 'Edit the file containing a tag'
-    complete vi -r -c t --description 'Begin editing by executing the specified  editor command'
+    complete -c vi -s s --description 'Suppress all interactive user feedback'
+    complete -c vi -s C --description 'Encrypt/decrypt text'
+    complete -c vi -s l --description 'Set up for editing LISP programs'
+    complete -c vi -s L --description 'List saved file names after crash'
+    complete -c vi -s R --description 'Read-only mode'
+    complete -c vi -s S --description 'Use linear search for tags if tag file not sorted'
+    complete -c vi -s v --description 'Start in display editing state'
+    complete -c vi -s V --description 'Verbose mode'
+    complete -c vi -s x --description 'Encrypt/decrypt text'
+    complete -c vi -r -s r --description 'Recover file after crash'
+    complete -c vi -r -s t --description 'Edit the file containing a tag'
+    complete -c vi -r -c t --description 'Begin editing by executing the specified  editor command'
 end
 

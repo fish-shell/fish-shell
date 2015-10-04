176e34cf73c48743a4a0fc87592fa70f83b3aff4
Fabian Homborg <FHomborg@gmail.com>
2015-10-04 15:45:02 +0200

    Remove ls|__fish_sgrep from apt-proxy-import

--- share/completions/apt-proxy-import.fish
+++ share/completions/apt-proxy-import.fish
@@ -4,7 +4,7 @@ complete -f -c apt-proxy-import -s V -l version --description 'Display version a
 complete -f -c apt-proxy-import -s v -l verbose --description 'Verbose mode'
 complete -f -c apt-proxy-import -s q -l quiet --description 'No message to STDOUT'
 complete -f -c apt-proxy-import -s r -l recursive --description 'Recurse into subdir'
-complete -r -c apt-proxy-import -s i -l import-dir -a '(ls -Fp| __fish_sgrep /\$)' --description 'Dir to import'
+complete -r -c apt-proxy-import -s i -l import-dir -a '(for i in */; echo $i; end)' --description 'Dir to import'
 complete -r -c apt-proxy-import -s u -l user -a '(__fish_complete_users)' --description 'Change to user'
 complete -r -c apt-proxy-import -s d -l debug --description 'Debug level[default 0]'
 

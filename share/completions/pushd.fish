2ffb47eba24ad61729676206c6b8cde61c27c219
Fabian Homborg <FHomborg@gmail.com>
2015-10-07 10:26:27 +0200

    Add missing \n to pushd completions

--- share/completions/pushd.fish
+++ share/completions/pushd.fish
@@ -2,7 +2,7 @@ function __fish_complete_pushd_plus
     if count $dirstack > /dev/null
         # print each member of the stack, replace $HOME with ~
         for i in (seq (count $dirstack))
-            printf "+%s\t%s" $i "Rotate to "(string replace -r "^$HOME" "~" -- $dirstack[$i])
+            printf "+%s\t%s\n" $i "Rotate to "(string replace -r "^$HOME" "~" -- $dirstack[$i])
         end
     end
 end
@@ -11,7 +11,7 @@ function __fish_complete_pushd_minus
     if count $dirstack > /dev/null
         # print each member of the stack, replace $HOME with ~
         for i in (seq (count $dirstack) -1 1)
-            printf "-%s\t%s" $i "Rotate to "(string replace -r "^$HOME" "~" -- $dirstack[$i])
+            printf "-%s\t%s\n" $i "Rotate to "(string replace -r "^$HOME" "~" -- $dirstack[$i])
         end
     end
 end
@@ -19,7 +19,7 @@ end
 function __fish_complete_pushd_swap
     if count $dirstack > /dev/null
         # replace $HOME with ~
-        printf "\t%s" "Swap with "(string replace -r "^$HOME" "~" -- $dirstack[1])
+        printf "\t%s\n" "Swap with "(string replace -r "^$HOME" "~" -- $dirstack[1])
     end
 end
 

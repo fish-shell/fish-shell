e72caa85754f7c508753dd51c14fccf05d2210da
Gary Peck <gary+github@realify.com>
2015-10-09 15:37:43 -0700

    Add ability to customize the amount of path shortening in prompt_pwd
    Allows the length of each shortened path component to be customized by setting the `fish_prompt_pwd_abbr` variable to the number of extra characters to include. Maintains the default behavior of shortening path components to just one character. You can also set `fish_prompt_pwd_abbr` to the empty value to disable shortening completely.

--- share/functions/prompt_pwd.fish
+++ share/functions/prompt_pwd.fish
@@ -1,4 +1,6 @@
+set -q fish_prompt_pwd_abbr; or set -U fish_prompt_pwd_abbr 0
+
 function prompt_pwd --description "Print the current working directory, shortened to fit the prompt"
 	set realhome ~
-	string replace -r '^'"$realhome"'($|/)' '~$1' $PWD | string replace -ar '([^/.])[^/]*/' '$1/'
+	string replace -r '^'"$realhome"'($|/)' '~$1' $PWD | string replace -ar '([^/.])([^/]{0,'"$fish_prompt_pwd_abbr"'})[^/]*/' '$1$2/'
 end

54f215294f2387c67ba53e35990f989f6672c46c
Fabian Homborg <FHomborg@gmail.com>
2015-10-12 11:35:45 +0200

    export: Fix replacing
    Also run it through fish_indent

--- share/functions/export.fish
+++ share/functions/export.fish
@@ -1,17 +1,18 @@
 function export --description 'Set global variable. Alias for set -gx, made for bash compatibility'
-        if test -z "$argv"
-           set
-           return 0
+    if test -z "$argv"
+        set
+        return 0
+    end
+    for arg in $argv
+        # Only split on the first =
+        # The literal "\n" is necessary because string doesn't interpret it without -r
+        set -l v (echo $arg | string replace "=" \n)
+        set -l c (count $v)
+        switch $c
+            case 1
+                set -gx $v $$v
+            case 2
+                set -gx $v[1] $v[2]
         end
-        for arg in $argv
-			# Only split on the first =
-            set -l v (echo $arg | string replace "=" "\n")
-            set -l c (count $v)
-            switch $c
-                    case 1
-                            set -gx $v $$v
-                    case 2
-                            set -gx $v[1] $v[2]
-            end
-        end
+    end
 end

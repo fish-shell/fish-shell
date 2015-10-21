3e7dfd7f5e188e1e106cfb78604e84ee54051217
Fabian Homborg <FHomborg@gmail.com>
2015-10-21 12:38:45 +0200

    Switch useradd and chsh to `string`

--- share/completions/chsh.fish
+++ share/completions/chsh.fish
@@ -3,8 +3,8 @@
 #
 
 # This grep tries to match nonempty lines that do not start with hash
-complete -c chsh -s s -l shell -x -a "( __fish_sgrep '^[^#]' /etc/shells)" -d "Specify your login shell"
-complete -c chsh -s u -l help -d "Display help and exit"
+complete -c chsh -s s -l shell -x -a "(string match -r '^[^#].*' < /etc/shells)" -d "Specify your login shell"
+complete -c chsh -s u -l help -d "Display help and exit	"
 complete -c chsh -s v -l version -d "Display version and exit"
 complete -x -c chsh -a "(__fish_complete_users)"
 

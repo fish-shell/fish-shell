e50d1043996e5a232fe5cdea8bae50dc83b32a0c
ridiculousfish <corydoras@ridiculousfish.com>
2015-11-08 23:52:59 -0800

    Remove duplicate \cw binding in default key bindings

--- share/functions/fish_default_key_bindings.fish
+++ share/functions/fish_default_key_bindings.fish
@@ -76,7 +76,6 @@ function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fis
 	bind $argv \ce end-of-line
 	bind $argv \ey yank-pop
 	bind $argv \ch backward-delete-char
-	bind $argv \cw backward-kill-word
 	bind $argv \cp up-or-search
 	bind $argv \cn down-or-search
 	bind $argv \cf forward-char

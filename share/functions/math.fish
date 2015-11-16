5eadb447c942b2e6670c2ca0ae7c3e4d0f94037c
J.W <jakwings@users.noreply.github.com>
2015-11-16 15:43:35 +0800

    Be compatible with older versions of bc

--- share/functions/math.fish
+++ share/functions/math.fish
@@ -8,6 +8,9 @@ function math --description "Perform math calculations in bc"
 		end
 
 		set -lx BC_LINE_LENGTH 0
+		if test (echo 'scale = 70; 1/2' | bc | wc -l) -gt 1
+			set BC_LINE_LENGTH 2147483647  # bc version <= 1.06
+		end
 		set -l out (echo $argv | bc)
                 test -z "$out"; and return 1
 		echo $out

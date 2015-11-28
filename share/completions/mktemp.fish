3868203c76e7570b59feeb9d69d5c98b0cd2c7c5
lordlycastle <lordlycastle@hotmail.com>
2015-11-28 00:35:26 +0000

    Added completions for cat, cp, mktemp, stat, touch for OS X.

--- share/completions/mktemp.fish
+++ share/completions/mktemp.fish
@@ -1,7 +1,14 @@
-complete -c mktemp -s d -l directory     -d 'create a directory, not a file'
-complete -c mktemp -s u -l dry-run       -d 'do not create anything; merely print a name (unsafe)'
-complete -c mktemp -s q -l quiet         -d 'suppress diagnostics about file/dir-creation failure'
-complete -c mktemp -l suffix -r          -d 'append SUFF to TEMPLATE'
-complete -c mktemp -l tmpdir             -d 'interpret TEMPLATE relative to DIR'
-complete -c mktemp -l help               -d 'display this help and exit'
-complete -c mktemp -l version            -d 'output version information and exit'
+if mktemp --version >/dev/null ^/dev/null # GNU
+	complete -c mktemp -s d -l directory    -d 'create a directory, not a file'
+	complete -c mktemp -s u -l dry-run      -d 'do not create anything; merely print a name (unsafe)'
+	complete -c mktemp -s q -l quiet        -d 'suppress diagnostics about file/dir-creation failure'
+	complete -c mktemp -l suffix -r         -d 'append SUFF to TEMPLATE'
+	complete -c mktemp -l tmpdir            -d 'interpret TEMPLATE relative to DIR'
+	complete -c mktemp -l help              -d 'display this help and exit'
+	complete -c mktemp -l version           -d 'output version information and exit'
+else # OS X
+	complete -c mktemp -s d                 -d 'create a directory, not a file'
+	complete -c mktemp -s q                 -d 'suppress diagnostics about file/dir-creation failure'
+	complete -c mktemp -s t -r              -d 'generate a template using PREFIX and TMPDIR (if set)'
+	complete -c mktemp -s u                 -d 'file will be unliked before mktemp exits (unsafe)'
+end

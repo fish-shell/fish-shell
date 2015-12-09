43c7ababf928c1804a5371fdef1fb72d63ab2063
Fabian Homborg <FHomborg@gmail.com>
2015-12-09 20:02:06 +0100

    Make lonetwin prompt use __fish_vcs_prompt

--- share/tools/web_config/sample_prompts/lonetwin.fish
+++ share/tools/web_config/sample_prompts/lonetwin.fish
@@ -15,6 +15,6 @@ function fish_prompt --description 'Write out the prompt'
        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
    end
 
-   echo -n -s "$USER" @ "$__fish_prompt_hostname" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" '> '
+   echo -n -s "$USER" @ "$__fish_prompt_hostname" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_vcs_prompt) "$__fish_prompt_normal" '> '
 
 end

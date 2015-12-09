e29d200ba568d83198dfce88180d8a4402ffa08f
Fabian Homborg <FHomborg@gmail.com>
2015-12-09 20:01:38 +0100

    Switch classic + git prompt to all vcsen -> Rename it to Classic + Vcs
    Keep the variable names for now.

--- share/tools/web_config/sample_prompts/classic_vcs.fish
+++ share/tools/web_config/sample_prompts/classic_vcs.fish
@@ -1,4 +1,4 @@
-# name: Classic + Git
+# name: Classic + Vcs
 # author: Kevin Ballard
 # vim: set noet:
 
@@ -69,5 +69,5 @@ function fish_prompt --description 'Write out the prompt'
 		set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
 	end
 
-	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "> "
+	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_vcs_prompt) $normal $prompt_status "> "
 end

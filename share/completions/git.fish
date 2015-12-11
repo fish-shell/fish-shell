fish-shell/fish-shell

commit 4280df3aa2dab8766642604d02cbb25f810b072b

git: Add missing options to `git clone`
* Add missing options to `git clone` in order to make the suggestions as
  similar to the manual (https://git-scm.com/docs/git-clone) as
  possible.

Signed-off-by: mr.Shu <mr@shu.io>

share/completions/git.fish
@@ -292,7 +292,16 @@ complete -f -c git -n '__fish_git_using_command cherry-pick' -l ff -d 'Fast-forw
 
 ### clone
 complete -f -c git -n '__fish_git_needs_command' -a clone -d 'Clone a repository into a new directory'
-# TODO options
+complete -f -c git -n '__fish_git_using_command clone' -l no-hardlinks -d 'Copy files instead of using hardlinks'
+complete -f -c git -n '__fish_git_using_command clone' -s q -l quiet  -d 'Operate quietly and do not report progress'
+complete -f -c git -n '__fish_git_using_command clone' -s v -l verbose -d 'Provide more information on what is going on'
+complete -f -c git -n '__fish_git_using_command clone' -s n -l no-checkout -d 'No checkout of HEAD is performed after the clone is complete'
+complete -f -c git -n '__fish_git_using_command clone' -l bare 'Make a bare Git repository'
+complete -f -c git -n '__fish_git_using_command clone' -l mirror 'Set up a mirror of the source repository'
+complete -f -c git -n '__fish_git_using_command clone' -s o -l origin 'Use a specific name of the remote instead of the default'
+complete -f -c git -n '__fish_git_using_command clone' -s b -l branch 'Use a specific branch instead of the one used by the cloned repository'
+complete -f -c git -n '__fish_git_using_command clone' -l depth 'Truncate the history to a specified number of revisions'
+complete -f -c git -n '__fish_git_using_command clone' -l recursive 'Initialize all submodules within the cloned repository'
 
 ### commit
 complete -c git -n '__fish_git_needs_command'    -a commit -d 'Record changes to the repository'

commit a0eef2663a6871673d3d9a9535fde704bd92c952

git completions: add missing description flags
Fixes syntax error introduced in 4280df3aa2dab.

share/completions/git.fish
@@ -296,12 +296,12 @@ complete -f -c git -n '__fish_git_using_command clone' -l no-hardlinks -d 'Copy 
 complete -f -c git -n '__fish_git_using_command clone' -s q -l quiet  -d 'Operate quietly and do not report progress'
 complete -f -c git -n '__fish_git_using_command clone' -s v -l verbose -d 'Provide more information on what is going on'
 complete -f -c git -n '__fish_git_using_command clone' -s n -l no-checkout -d 'No checkout of HEAD is performed after the clone is complete'
-complete -f -c git -n '__fish_git_using_command clone' -l bare 'Make a bare Git repository'
-complete -f -c git -n '__fish_git_using_command clone' -l mirror 'Set up a mirror of the source repository'
-complete -f -c git -n '__fish_git_using_command clone' -s o -l origin 'Use a specific name of the remote instead of the default'
-complete -f -c git -n '__fish_git_using_command clone' -s b -l branch 'Use a specific branch instead of the one used by the cloned repository'
-complete -f -c git -n '__fish_git_using_command clone' -l depth 'Truncate the history to a specified number of revisions'
-complete -f -c git -n '__fish_git_using_command clone' -l recursive 'Initialize all submodules within the cloned repository'
+complete -f -c git -n '__fish_git_using_command clone' -l bare -d 'Make a bare Git repository'
+complete -f -c git -n '__fish_git_using_command clone' -l mirror -d 'Set up a mirror of the source repository'
+complete -f -c git -n '__fish_git_using_command clone' -s o -l origin -d 'Use a specific name of the remote instead of the default'
+complete -f -c git -n '__fish_git_using_command clone' -s b -l branch -d 'Use a specific branch instead of the one used by the cloned repository'
+complete -f -c git -n '__fish_git_using_command clone' -l depth -d 'Truncate the history to a specified number of revisions'
+complete -f -c git -n '__fish_git_using_command clone' -l recursive -d 'Initialize all submodules within the cloned repository'
 
 ### commit
 complete -c git -n '__fish_git_needs_command'    -a commit -d 'Record changes to the repository'
3a249bb5495a3366cb98a5cadaf22a141f6eed99
Fabian Homborg <FHomborg@gmail.com>
2015-12-30 19:02:03 +0100

    Include mount helpers in filesystem completion
    Probably not a thing on OSX, but this should print nothing then.
    
    On linux, it helps detect e.g. ntfs-3g.

--- share/functions/__fish_print_filesystems.fish
+++ share/functions/__fish_print_filesystems.fish
@@ -3,6 +3,7 @@ function __fish_print_filesystems -d "Print a list of all known filesystem types
 	set -l fs adfs affs autofs coda coherent cramfs devpts efs ext ext2 ext3
 	set fs $fs hfs hpfs iso9660 jfs minix msdos ncpfs nfs ntfs proc qnx4 ramfs
 	set fs $fs reiserfs romfs smbfs sysv tmpfs udf ufs umsdos vfat xenix xfs xiafs
-	printf "%s\n" $fs
+	# Mount has helper binaries to mount filesystems
+	# These are called mount.* and are placed somewhere in $PATH
+	printf "%s\n" $fs (string replace -ra ".*/mount." "" -- $PATH/mount.*)
 end
-

b908d0b89bd595f5bcbbf8ec9e153b037ba1a2f3
Fabian Homborg <FHomborg@gmail.com>
2016-02-04 13:53:55 +0100

    Another stringification (__fish_complete_command)

--- share/functions/__fish_complete_command.fish
+++ share/functions/__fish_complete_command.fish
@@ -2,11 +2,7 @@ function __fish_complete_command --description 'Complete using all available com
 	set -l ctoken (commandline -ct)
 	switch $ctoken
 	case '*=*'
-        # Some seds (e.g. on Mac OS X), don't support \n in the RHS
-        # Use a literal newline instead
-        # http://sed.sourceforge.net/sedfaq4.html#s4.1
-		set ctoken (echo $ctoken  | sed 's/=/\\
-/')
+		set ctoken (string split "=" -- $ctoken)
 		printf '%s\n' $ctoken[1]=(complete -C$ctoken[2])
 	case '*'
 		complete -C$ctoken

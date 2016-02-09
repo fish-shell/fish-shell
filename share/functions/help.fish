33970a6318a2fafa1106b77c6973d4661bb0db25
Gordon Tyler <Gordon.Tyler@software.dell.com>
2016-02-09 12:47:46 -0500

    Fix indentation.

--- share/functions/help.fish
+++ share/functions/help.fish
@@ -78,10 +78,10 @@ function help --description 'Show help for the fish shell'
 		return 1
 	end
 
-        # In Cygwin, start the user-specified browser using cygstart
-        if begin; type -q cygstart; and test $fish_browser != "cygstart"; end
-                set fish_browser cygstart \"$fish_browser\"
-        end
+	# In Cygwin, start the user-specified browser using cygstart
+	if begin; type -q cygstart; and test $fish_browser != "cygstart"; end
+		set fish_browser cygstart \"$fish_browser\"
+	end
 
 	set -l fish_help_item $argv[1]
 
@@ -115,10 +115,10 @@ function help --description 'Show help for the fish shell'
 		# Help is installed, use it
 		set page_url file://$__fish_help_dir/$fish_help_page
 
-                # In Cygwin, we need to convert the base help dir to a Windows path before converting it to a file URL
-                if type -q cygpath
-                        set page_url file://(cygpath -m $__fish_help_dir)/$fish_help_page
-                end
+		# In Cygwin, we need to convert the base help dir to a Windows path before converting it to a file URL
+		if type -q cygpath
+			set page_url file://(cygpath -m $__fish_help_dir)/$fish_help_page
+		end
 	else
 		# Go to the web. Only include one dot in the version string
 		set -l version_string (echo $FISH_VERSION| cut -d . -f 1,2)

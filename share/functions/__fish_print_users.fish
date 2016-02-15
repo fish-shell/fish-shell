b72837a0f4678d34406bc98df00503d1ab8656cf
Sunguk Lee <d3m3vilurr@gmail.com>
2016-02-15 18:42:48 +0900

    Improve __fish_print_users
    Patch from `__make_users_completions` of killall command completion

--- share/functions/__fish_print_users.fish
+++ share/functions/__fish_print_users.fish
@@ -3,7 +3,7 @@ function __fish_print_users --description "Print a list of local users"
 	if test -x /usr/bin/getent
 		getent passwd | cut -d : -f 1
 	else if test -x /usr/bin/dscl # OS X support
-		dscl . -list /Users
+		dscl . -list /Users | __fish_sgrep -v '^_'
 	else
 		__fish_sgrep -ve '^#' /etc/passwd | cut -d : -f 1
 	end

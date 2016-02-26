55a62655baf4d2dadfb546934a0b440f40c46ae7
Fabian Homborg <FHomborg@gmail.com>
2016-02-27 00:31:21 +0100

    Check ssh's KnownhostsFiles in print_hostnames
    These are an additional source of information
    
    Thanks to @sysbot (#2313) for the inspiration.

--- share/functions/__fish_print_hostnames.fish
+++ share/functions/__fish_print_hostnames.fish
@@ -22,7 +22,6 @@ function __fish_print_hostnames -d "Print a list of known hostnames"
 
 	# Print hosts with known ssh keys
 	# Does not match hostnames with @directives specified
-	__fish_sgrep -Eoh '^[^#@|, ]*' ~/.ssh/known_hosts{,2} ^/dev/null | string replace -r '^\[([^]]+)\]:[0-9]+$' '$1'
 
 	# Print hosts from system wide ssh configuration file
 	if [ -e /etc/ssh/ssh_config ]
@@ -32,5 +31,17 @@ function __fish_print_hostnames -d "Print a list of known hostnames"
 	# Print hosts from ssh configuration file
 	if [ -e ~/.ssh/config ]
 		awk -v FS="[ =]+" -v OFS='\n' 'tolower($0) ~ /^ *host[^*?!]*$/{ $1=""; print }' ~/.ssh/config
+	end
+
+	# Check external ssh KnownHostsFiles
+	set -l known_hosts ~/.ssh/known_hosts{,2} /etc/ssh/known_hosts{,2} # Yes, seriously - the default specifies both with and without "2"
+	for file in /etc/ssh/ssh_config ~/.ssh/config
+		if test -r $file
+			set known_hosts $known_hosts (string match -r '^\s*UserKnownHostsFile|^\s*GlobalKnownHostsFile' <$file \
+			| string replace -r '.*KnownHostsFile\s*' '')
+		end
+	end
+	for file in $known_hosts
+		test -r $file; and string replace -ra '(\S+) .*' '$1' <$file | string replace -ra '^\[([0-9.]+)\]:[0-9]+$' '$1' | string split ","
 	end
 end

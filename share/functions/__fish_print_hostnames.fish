a32e4b270417683c714933e6cdd9d6d9164eeb56
Fabian Homborg <FHomborg@gmail.com>
2016-02-28 16:43:29 +0100

    Read hosts from ssh_config with string
    We're opening those files anyway, might as well not call awk.

--- share/functions/__fish_print_hostnames.fish
+++ share/functions/__fish_print_hostnames.fish
@@ -20,23 +20,13 @@ function __fish_print_hostnames -d "Print a list of known hostnames"
 		string match -r '^\s*[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3]:|^[a-zA-Z\.]*:' </etc/fstab | string replace -r ':.*' ''
 	end
 
-	# Print hosts with known ssh keys
-	# Does not match hostnames with @directives specified
-
-	# Print hosts from system wide ssh configuration file
-	if [ -e /etc/ssh/ssh_config ]
-		awk -v FS="[ =]+" -v OFS='\n' 'tolower($0) ~ /^ *host[^*?!]*$/{ $1=""; print }' /etc/ssh/ssh_config
-	end
-
-	# Print hosts from ssh configuration file
-	if [ -e ~/.ssh/config ]
-		awk -v FS="[ =]+" -v OFS='\n' 'tolower($0) ~ /^ *host[^*?!]*$/{ $1=""; print }' ~/.ssh/config
-	end
-
-	# Check external ssh KnownHostsFiles
+	# Check hosts known to ssh
 	set -l known_hosts ~/.ssh/known_hosts{,2} /etc/ssh/known_hosts{,2} # Yes, seriously - the default specifies both with and without "2"
 	for file in /etc/ssh/ssh_config ~/.ssh/config
 		if test -r $file
+			# Print hosts from system wide ssh configuration file
+			# Note the non-capturing group to avoid printing "name"
+			string match -r '\s*Host(?:name)? \w.*' < $file | string replace -r '^\s*Host(?:name)?\s*(\S+)' '$1'
 			set known_hosts $known_hosts (string match -r '^\s*UserKnownHostsFile|^\s*GlobalKnownHostsFile' <$file \
 			| string replace -r '.*KnownHostsFile\s*' '')
 		end

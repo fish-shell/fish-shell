03a1b34cdbf05d2d6da1bdf7f25e91072bd65211
Fabian Homborg <FHomborg@gmail.com>
2016-03-08 17:25:00 +0100

    Add a skeleton etc/config.fish
    This contains a bit of information on how fish's configuration works for
    the admin, but fish still won't _require_ anything from /etc as the file
    can be safely removed.

---
+++ etc/config.fish
@@ -0,0 +1,14 @@
+# Put system-wide fish configuration entries here
+# or in .fish files in conf.d/
+# Files in conf.d can be overridden by the user
+# by files with the same name in $XDG_CONFIG_HOME/fish/conf.d
+
+# This file is run by all fish instances.
+# To include configuration only for login shells, use
+# if status --is-login
+#    ...
+# end
+# To include configuration only for interactive shells, use
+# if status --is-interactiv
+#   ...
+# end

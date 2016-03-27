bceeba8921194b1833af2b4f611a892fc2549f6e
Josef Gajdusek <atx@atx.name>
2016-03-27 23:00:19 +0200

    Fix typos: ingnore->ignore

--- share/completions/modprobe.fish
+++ share/completions/modprobe.fish
@@ -7,8 +7,8 @@ complete -c modprobe -s v -l verbose --description "Print messages about what th
 complete -c modprobe -s C -l config --description "Configuration file" -r
 complete -c modprobe -s c -l showconfig --description "Dump configuration file"
 complete -c modprobe -s n -l dry-run --description "Do not actually insert/remove module"
-complete -c modprobe -s i -l ingnore-install --description "Ignore install and remove commands in configuration file"
-complete -c modprobe -l ingnore-remove --description "Ignore install and remove commands in configuration file"
+complete -c modprobe -s i -l ignore-install --description "Ignore install and remove commands in configuration file"
+complete -c modprobe -l ignore-remove --description "Ignore install and remove commands in configuration file"
 complete -c modprobe -s q -l quiet --description "Ignore bogus module names"
 complete -c modprobe -s r -l remove --description "Remove modules"
 complete -c modprobe -s V -l version --description "Display version and exit"

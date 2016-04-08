0bb860266e4bbe1716fd094e377625510838194f
Jak Wings <jakwings@gmail.com>
2016-04-08 18:20:21 +0800

    fix handling of line continuation in keywords
    The current behavior is more consistent with line continuation outside
    keywords.
    
    Fixes #2897

--- src/parse_tree.cpp
+++ src/parse_tree.cpp
@@ -1254,6 +1254,19 @@ static parse_keyword_t keyword_with_name(const wchar_t *name)
     return result;
 }
 
+static wcstring strip_line_continuation(const wcstring text)
+{
+    wcstring result = text;
+    const wcstring pattern = L"\\\n";
+    size_t pos = result.find(pattern);
+    while (pos != wcstring::npos)
+    {
+        result.erase(pos, pattern.length());
+        pos = result.find(pattern, pos);
+    }
+    return result;
+}
+
 /* Given a token, returns the keyword it matches, or parse_keyword_none. */
 static parse_keyword_t keyword_for_token(token_type tok, const wcstring &token)
 {
@@ -1266,7 +1279,8 @@ static parse_keyword_t keyword_for_token(token_type tok, const wcstring &token)
     /* If tok_txt is clean (which most are), we can compare it directly. Otherwise we have to expand it. We only expand quotes, and we don't want to do expensive expansions like tilde expansions. So we do our own "cleanliness" check; if we find a character not in our allowed set we know it's not a keyword, and if we never find a quote we don't have to expand! Note that this lowercase set could be shrunk to be just the characters that are in keywords. */
     parse_keyword_t result = parse_keyword_none;
     bool needs_expand = false, all_chars_valid = true;
-    const wchar_t *tok_txt = token.c_str();
+    const wcstring tmp = strip_line_continuation(token);
+    const wchar_t *tok_txt = tmp.c_str();
     const wchar_t *chars_allowed_in_keywords = L"abcdefghijklmnopqrstuvwxyz'\"";
     for (size_t i=0; tok_txt[i] != L'\0'; i++)
     {

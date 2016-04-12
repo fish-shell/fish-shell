8ab980b793e80199f61257b1bc46826a93af8413
Fabian Homborg <FHomborg@gmail.com>
2016-04-12 20:32:29 +0200

    Remove $__fish_vi_mode
    This makes fish_mode_prompt rely on $fish_key_bindings instead.
    
    fish_bind_mode is also set in default mode (only always "default"), so
    it can't be used as the indicator.

--- share/functions/fish_mode_prompt.fish
+++ share/functions/fish_mode_prompt.fish
@@ -1,7 +1,7 @@
 # The fish_mode_prompt function is prepended to the prompt
 function fish_mode_prompt --description "Displays the current mode"
   # Do nothing if not in vi mode
-  if set -q __fish_vi_mode
+  if test "$fish_key_bindings" = "fish_vi_key_bindings"
     switch $fish_bind_mode
       case default
         set_color --bold --background red white

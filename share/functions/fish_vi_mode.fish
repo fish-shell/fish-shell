47a35276fdfbfcfdc16245550da2e2b11b8de1eb
Fabian Homborg <FHomborg@gmail.com>
2016-04-12 20:32:29 +0200

    Remove $__fish_vi_mode
    This makes fish_mode_prompt rely on $fish_key_bindings instead.
    
    fish_bind_mode is also set in default mode (only always "default"), so
    it can't be used as the indicator.

--- share/functions/fish_vi_mode.fish
+++ share/functions/fish_vi_mode.fish
@@ -1,8 +1,4 @@
 function fish_vi_mode
-  # Set the __fish_vi_mode variable
-  # This triggers fish_mode_prompt to output the mode indicator
-  set -g __fish_vi_mode 1
-  
   # Turn on vi keybindings
   set -g fish_key_bindings fish_vi_key_bindings
 end

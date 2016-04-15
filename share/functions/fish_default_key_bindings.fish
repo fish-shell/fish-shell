045f76f7149506b4336ee259d967d2cb3ce5807a
Fabian Homborg <FHomborg@gmail.com>
2016-04-15 10:55:39 +0200

    Remove named arg in fish_default_key_bindings
    This wasn't actually used anywhere.

--- share/functions/fish_default_key_bindings.fish
+++ share/functions/fish_default_key_bindings.fish
@@ -1,6 +1,6 @@
 
-function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fish" -a mode
-	if not set -q mode[1]
+function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fish"
+	if not set -q argv[1]
 		if test "$fish_key_bindings" != "fish_default_key_bindings"
 			set -g fish_key_bindings fish_default_key_bindings # This triggers the handler, which calls us again and ensures the user_key_bindings are executed
 			return

a897ef002521a3991adc4342189f0e30a0d048b0
Kurtis Rader <krader@skepticism.us>
2016-04-29 15:34:10 -0700

    don't use colors when writing the ^C indicator
    There was an extended discussion in https://github.com/fish-shell/fish-shell/issues/2904 about using a bright yellow background to make the cancelled command indicator, ^C, standout. The upshot was that standout (i.e., reversing fg/bg colors) mode should be used until themes are agumented with proper support for background colors and special characters.

--- share/functions/__fish_cancel_commandline.fish
+++ share/functions/__fish_cancel_commandline.fish
@@ -3,8 +3,13 @@ function __fish_cancel_commandline
     set -l cmd (commandline)
     if test -n "$cmd"
         commandline -C 1000000
-        # set a color, output ^C, restore color, clear to EOL (to erase any autosuggestion)
-        echo -n (set_color -b bryellow black)"^C"(set_color normal)\033"[0K"
+        # TODO: Switch from using tput and standout mode to `set_color` when themes have been
+        # augmented to include support for background colors or has support for standout/reverse
+        # mode.
+        #
+        # Set reverse fg/bg color mode, output ^C, restore normal mode, clear to EOL (to erase any
+        # autosuggestion).
+        echo (tput smso)"^C"(tput rmso)(tput el)
         for i in (seq (commandline -L))
             echo ""
         end

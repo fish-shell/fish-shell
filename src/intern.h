8b2cf81f17f1da78c2fdc2940dd489a2321c247a
Kurtis Rader <krader@skepticism.us>
2016-05-01 20:26:32 -0700

    restyle intern module to match project style
    Reduces lint errors from 8 to 6 (-25%). Line count from 112 to 83 (-26%).
    
    Another step in resolving issue #2902.

--- src/intern.h
+++ src/intern.h
@@ -1,26 +1,17 @@
-/** \file intern.h
-
-    Library for pooling common strings
-
-*/
-
+// Library for pooling common strings.
 #ifndef FISH_INTERN_H
 #define FISH_INTERN_H
 
-/**
-   Return an identical copy of the specified string from a pool of unique strings. If the string was not in the pool, add a copy.
-
-   \param in the string to return an interned copy of
-*/
+/// Return an identical copy of the specified string from a pool of unique strings. If the string
+/// was not in the pool, add a copy.
+///
+/// \param in the string to return an interned copy of.
 const wchar_t *intern(const wchar_t *in);
 
-/**
-   Insert the specified string literal into the pool of unique
-   strings. The string will not first be copied, and it will not be
-   free'd on exit.
-
-   \param in the string to add to the interned pool
-*/
+/// Insert the specified string literal into the pool of unique strings. The string will not first
+/// be copied, and it will not be free'd on exit.
+///
+/// \param in the string to add to the interned pool
 const wchar_t *intern_static(const wchar_t *in);
 
 #endif

9d742a4fa18d3bdab0ab2b68f8f57e334ea3c0e1
Kurtis Rader <krader@skepticism.us>
2016-05-02 21:41:17 -0700

    restyle proc module to match project style
    Reduces lint errors from 134 to 101 (-25%). Line count from 1994 to 1466 (-26%).
    
    Another step in resolving issue #2902.

--- src/print_help.h
+++ src/print_help.h
@@ -1,15 +1,8 @@
-
-/** \file print_help.h
-  Print help message for the specified command
-*/
-
+// Print help message for the specified command.
 #ifndef FISH_PRINT_HELP_H
 #define FISH_PRINT_HELP_H
 
-/**
-  Print help message for the specified command
-*/
-
+/// Print help message for the specified command.
 void print_help(const char *cmd, int fd);
 
 #endif

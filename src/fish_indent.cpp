f7c3b02fcdaad5c194d4e076a28759908407aee1
Camille Scholtz <onodera@openmailbox.org>
2016-05-20 00:10:59 +0200

    Remove poll include

--- src/fish_indent.cpp
+++ src/fish_indent.cpp
@@ -27,7 +27,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #include <memory>
 #include <string>
 #include <vector>
-#include <poll.h>
 
 #include "color.h"
 #include "common.h"

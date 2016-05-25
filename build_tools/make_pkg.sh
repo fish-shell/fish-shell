6d6f67ee67d35f19da21c48cfba194db107cebd5
David Adam <zanchey@ucc.gu.uwa.edu.au>
2016-05-25 17:11:02 +0000

    make_pkg: use a temporary directory rather than just /tmp

--- build_tools/make_pkg.sh
+++ build_tools/make_pkg.sh
@@ -15,20 +15,23 @@ echo "Version is $VERSION"
 set -x
 
 make distclean
-rm -Rf /tmp/fish_pkg
 
 #Exit on error
 set -e
 
-mkdir -p /tmp/fish_pkg/root /tmp/fish_pkg/intermediates /tmp/fish_pkg/dst
-xcodebuild install -scheme install_tree -configuration Release DSTROOT=/tmp/fish_pkg/root/
-pkgbuild --scripts build_tools/osx_package_scripts --root /tmp/fish_pkg/root/ --identifier 'com.ridiculousfish.fish-shell-pkg' --version "$VERSION"  /tmp/fish_pkg/intermediates/fish.pkg
+PKGDIR=`mktemp -d`
 
-productbuild  --package-path /tmp/fish_pkg/intermediates --distribution build_tools/osx_distribution.xml --resources build_tools/osx_package_resources/ ~/fish_built/fish-$VERSION.pkg
+mkdir -p $PKGDIR/root $PKGDIR/intermediates $PKGDIR/dst
+xcodebuild install -scheme install_tree -configuration Release DSTROOT=$PKGDIR/root/
+pkgbuild --scripts build_tools/osx_package_scripts --root $PKGDIR/root/ --identifier 'com.ridiculousfish.fish-shell-pkg' --version "$VERSION"  $PKGDIR/intermediates/fish.pkg
+
+productbuild  --package-path $PKGDIR/intermediates --distribution build_tools/osx_distribution.xml --resources build_tools/osx_package_resources/ ~/fish_built/fish-$VERSION.pkg
 
 
 # Make the app
 xcodebuild -scheme fish.app -configuration Release DSTROOT=/tmp/fish_app/ SYMROOT=DerivedData/fish/Build/Products
-rm -f ~/fish_built/fish.app.zip
+
 cd DerivedData/fish/Build/Products/Release/
 zip -r ~/fish_built/fish-$VERSION.app.zip fish.app
+
+rm -r $PKGDIR

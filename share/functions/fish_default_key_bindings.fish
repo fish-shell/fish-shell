31578eb4f2f21c0db3833803c63f3cb256a87784
Fabian Homborg <FHomborg@gmail.com>
2016-05-25 13:09:33 +0200

    Share some bindings between vi- and emacs-mode
    This undoes the inheritance since it shared too much.

--- share/functions/fish_default_key_bindings.fish
+++ share/functions/fish_default_key_bindings.fish
@@ -11,6 +11,9 @@ function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fis
 		bind --erase --all
 	end
 
+    # These are shelly bindings that we share with vi mode
+    __fish_shared_key_bindings
+
 	# This is the default binding, i.e. the one used if no other binding matches
 	bind $argv "" self-insert
 
@@ -18,23 +21,9 @@ function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fis
 	bind $argv \r execute
 
 	bind $argv \ck kill-line
-	bind $argv \cy yank
-	bind $argv \t complete
 
-	bind $argv \e\n "commandline -i \n"
-	bind $argv \e\r "commandline -i \n"
-
-	bind $argv \e\[A up-or-search
-	bind $argv \e\[B down-or-search
-	bind $argv -k down down-or-search
-	bind $argv -k up up-or-search
-
-	# Some linux VTs output these (why?)
-	bind $argv \eOA up-or-search
-	bind $argv \eOB down-or-search
 	bind $argv \eOC forward-char
 	bind $argv \eOD backward-char
-
 	bind $argv \e\[C forward-char
 	bind $argv \e\[D backward-char
 	bind $argv -k right forward-char
@@ -58,31 +47,8 @@ function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fis
 	bind $argv -k end end-of-line 2> /dev/null
 	bind $argv \e\[3\;2~ backward-delete-char # Mavericks Terminal.app shift-delete
 
-	bind $argv \e\eOC nextd-or-forward-word
-	bind $argv \e\eOD prevd-or-backward-word
-	bind $argv \e\e\[C nextd-or-forward-word
-	bind $argv \e\e\[D prevd-or-backward-word
-	bind $argv \eO3C nextd-or-forward-word
-	bind $argv \eO3D prevd-or-backward-word
-	bind $argv \e\[3C nextd-or-forward-word
-	bind $argv \e\[3D prevd-or-backward-word
-	bind $argv \e\[1\;3C nextd-or-forward-word
-	bind $argv \e\[1\;3D prevd-or-backward-word
-
-	bind $argv \e\eOA history-token-search-backward
-	bind $argv \e\eOB history-token-search-forward
-	bind $argv \e\e\[A history-token-search-backward
-	bind $argv \e\e\[B history-token-search-forward
-	bind $argv \eO3A history-token-search-backward
-	bind $argv \eO3B history-token-search-forward
-	bind $argv \e\[3A history-token-search-backward
-	bind $argv \e\[3B history-token-search-forward
-	bind $argv \e\[1\;3A history-token-search-backward
-	bind $argv \e\[1\;3B history-token-search-forward
-
 	bind $argv \ca beginning-of-line
 	bind $argv \ce end-of-line
-	bind $argv \ey yank-pop
 	bind $argv \ch backward-delete-char
 	bind $argv \cp up-or-search
 	bind $argv \cn down-or-search
@@ -100,40 +66,13 @@ function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fis
 	bind $argv \ef forward-word
 	bind $argv \e\[1\;5C forward-word
 	bind $argv \e\[1\;5D backward-word
-	bind $argv \e\[1\;9A history-token-search-backward # iTerm2
-	bind $argv \e\[1\;9B history-token-search-forward # iTerm2
-	bind $argv \e\[1\;9C nextd-or-forward-word #iTerm2
-	bind $argv \e\[1\;9D prevd-or-backward-word #iTerm2
-	# Bash compatibility
-	# https://github.com/fish-shell/fish-shell/issues/89
-	bind $argv \e. history-token-search-backward
 	bind $argv -k ppage beginning-of-history
 	bind $argv -k npage end-of-history
 	bind $argv \e\< beginning-of-buffer
 	bind $argv \e\> end-of-buffer
 
-	bind $argv \el __fish_list_current_token
-	bind $argv \ew 'set tok (commandline -pt); if test $tok[1]; echo; whatis $tok[1]; commandline -f repaint; end'
-	bind $argv \cl 'clear; commandline -f repaint'
-	bind $argv \cc __fish_cancel_commandline
-	bind $argv \cu backward-kill-line
-	bind $argv \cw backward-kill-path-component
-	bind $argv \ed 'set -l cmd (commandline); if test -z "$cmd"; echo; dirh; commandline -f repaint; else; commandline -f kill-word; end'
-	bind $argv \cd delete-or-exit
-
 	bind \ed forward-kill-word
 	bind \ed kill-word
-
-	# Allow reading manpages by pressing F1 (many GUI applications) or Alt+h (like in zsh)
-	bind $argv -k f1 __fish_man_page
-	bind $argv \eh __fish_man_page
-
-	# This will make sure the output of the current command is paged using the default pager when you press Meta-p
-	# If none is set, less will be used
-	bind $argv \ep '__fish_paginate'
-	
-	# shift-tab does a tab complete followed by a search
-	bind $argv --key btab complete-and-search
 
 	# escape cancels stuff	
 	bind \e cancel
@@ -150,8 +89,4 @@ function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fis
 			bind $argv \eOc forward-word
 			bind $argv \eOd backward-word
 	end
-
-	# Make it easy to turn an unexecuted command into a comment in the shell history. Also,
-	# remove the commenting chars so the command can be further edited then executed.
-	bind \e\# __fish_toggle_comment_commandline
 end

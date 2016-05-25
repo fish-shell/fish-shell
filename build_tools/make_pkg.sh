d7a4838a545beebad3786eb9289a04f87de480b6
David Adam <zanchey@ucc.gu.uwa.edu.au>
2016-05-25 17:21:05 +0000

    make_pkg: allow output to an environment-controlled path

--- build_tools/make_pkg.sh
+++ build_tools/make_pkg.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Script to produce an OS X installer .pkg and .app(.zip)
+
 VERSION=`git describe --always --dirty 2>/dev/null`
 if test -z "$VERSION" ; then
   echo "Could not get version from git"
@@ -21,17 +23,19 @@ set -e
 
 PKGDIR=`mktemp -d`
 
+OUTPUT_PATH=${FISH_ARTEFACT_PATH:-~/fish_built}
+
 mkdir -p $PKGDIR/root $PKGDIR/intermediates $PKGDIR/dst
 xcodebuild install -scheme install_tree -configuration Release DSTROOT=$PKGDIR/root/
 pkgbuild --scripts build_tools/osx_package_scripts --root $PKGDIR/root/ --identifier 'com.ridiculousfish.fish-shell-pkg' --version "$VERSION"  $PKGDIR/intermediates/fish.pkg
 
-productbuild  --package-path $PKGDIR/intermediates --distribution build_tools/osx_distribution.xml --resources build_tools/osx_package_resources/ ~/fish_built/fish-$VERSION.pkg
+productbuild  --package-path $PKGDIR/intermediates --distribution build_tools/osx_distribution.xml --resources build_tools/osx_package_resources/ $OUTPUT_PATH/fish-$VERSION.pkg
 
 
 # Make the app
 xcodebuild -scheme fish.app -configuration Release DSTROOT=/tmp/fish_app/ SYMROOT=DerivedData/fish/Build/Products
 
 cd DerivedData/fish/Build/Products/Release/
-zip -r ~/fish_built/fish-$VERSION.app.zip fish.app
+zip -r $OUTPUT_PATH/fish-$VERSION.app.zip fish.app
 
 rm -r $PKGDIR

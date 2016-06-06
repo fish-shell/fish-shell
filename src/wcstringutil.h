2fafb13eaa30981754a8cd0ba8f3c082e3147fa8
Aaron Gyes <me@aaron.gy>
2016-06-05 21:30:24 -0700

    Be a bit more consistent and proper.

--- src/wcstringutil.h
+++ src/wcstringutil.h
@@ -7,18 +7,16 @@
 
 #include "common.h"
 
-/// Typedef that represents a range in a wcstring. The first element is the location, the second is
-/// the count.
+/// @typedef wcstring_range represents a range in a wcstring.
+/// The first element is the location, the second is the count.
 typedef std::pair<wcstring::size_type, wcstring::size_type> wcstring_range;
 
 /// wcstring equivalent of wcstok(). Supports NUL. For convenience and wcstok() compatibility, the
 /// first character of each token separator is replaced with NUL.
-///
-/// Returns a pair of (pos, count).
-/// Returns (npos, npos) when it's done.
-/// Returns (pos, npos) when the token is already known to be the final token.
-///
-/// Note that the final token may not necessarily return (pos, npos).
+/// @return Returns a pair of (pos, count).
+///         This will be (npos, npos) when it's done. In the form of (pos, npos)
+///         when the token is already known to be the final token.
+/// @note The final token may not necessarily return (pos, npos).
 wcstring_range wcstring_tok(wcstring& str, const wcstring& needle,
                             wcstring_range last = wcstring_range(0, 0));
 

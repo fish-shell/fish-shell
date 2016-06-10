222a07e907c239f7dab541512b109730a07828dc
Fabian Homborg <FHomborg@gmail.com>
2016-06-10 14:13:15 +0200

    Allow compressed man pages in `help`
    It seems Fedora compresses our whopping 340k of man pages.
    
    Fixes #3130.
    
    Inspired by @TieDyedDevil's work there.

--- share/functions/__fish_print_commands.fish
+++ share/functions/__fish_print_commands.fish
@@ -1,5 +1,7 @@
 function __fish_print_commands --description "Print a list of documented fish commands"
-	if test -d $__fish_datadir/man/man1/
-		find $__fish_datadir/man/man1/ -type f -name \*.1 -execdir basename '{}' .1 ';'
-	end
+    if test -d $__fish_datadir/man/man1/
+        for file in $__fish_datadir/man/man1/**.1*
+            string replace -r '.*/' '' -- $file | string replace -r '.1(.gz)?$' ''
+        end
+    end
 end

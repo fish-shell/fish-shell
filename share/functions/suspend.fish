772e35562ad3b37383e07577a638c8c24c71e1b8
Aaron Gyes <me@aaron.gy>
2016-07-01 01:53:43 -0700

    suspend output after '\n' not printing before kill

--- share/functions/suspend.fish
+++ share/functions/suspend.fish
@@ -1,15 +1,17 @@
 function suspend -d "Suspend the current shell."
     if begin contains -- $argv --force
-             or not status --is-interactive and not status --is-login
-         end
-         echo "Suspending" %self \n(set_color --bold)" fg"(set_color normal) to resume
-         if contains -- $argv --force
-                echo " (or"(set_color --bold) "kill -CONT" %self (set_color normal)"from another terminal)"
-         end
+            or not status --is-interactive and not status --is-login
+        end
+        printf "Suspending %d\n%s fg%s to resume" %self (set_color --bold) (set_color normal)
+        if contains -- $argv --force
+            printf " (or%s kill -CONT %d%s from another terminal)\n" (set_color --bold) %self (set_color normal)
+        else
+            echo
+        end
 
-         kill -STOP %self
+        kill -STOP %self
     else
-         echo 2>&1 "Refusing to suspend login shell. Use --force to override."
-         echo 2>&1 "This might hang your terminal."
+        echo 2>&1 "Refusing to suspend login shell."
+        echo 2>&1 "Use --force to override. This might hang your terminal."
     end
 end

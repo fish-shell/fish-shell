fish-shell/fish-shell

commit 834ebef53cf8f58ad53344b053f6d3225d78e0fd

Simplify greeting
We have already confirmed we're interactive, and `echo` is a builtin now.

share/functions/__fish_config_interactive.fish
@@ -101,24 +101,15 @@ function __fish_config_interactive -d "Initializations that should be performed 
         eval "$__fish_bin_dir/fish -c 'fish_update_completions > /dev/null ^/dev/null' &"
     end
 
-	if status -i
-		#
-		# Print a greeting
-		#
+	#
+	# Print a greeting
+	# fish_greeting can be a function (preferred) or a variable
+	#
 
-		if functions -q fish_greeting
-			fish_greeting
-		else
-			if set -q fish_greeting
-				switch "$fish_greeting"
-					case ''
-					# If variable is empty, don't print anything, saves us a fork
-
-					case '*'
-					echo $fish_greeting
-				end
-			end
-		end
+	if functions -q fish_greeting
+		fish_greeting
+	else
+		set -q fish_greeting; and echo $fish_greeting
 	end
 
 	#

commit 3a7a6f16eff4591415898b97f54f9521692f2b45

Only show fish greeting for interactive logins
This is a regression introduced by 834ebef53cf8f58ad53344b053f6d3225d78e0fd
Bolster with a check for only login sessions too -- hopefully makes it
less annooying on subshells in general.

Fixes #3261

build_tools/style.fish
@@ -101,7 +101,7 @@ if set -q f_files[1]
             echo $file
             rm $file.new
         else
-            echo (set_color --underline)$file(set_color normal)is being reformatted
+            echo (set_color --underline)$file(set_color normal) is being reformatted
             mv $file.new $file
         end
     end

share/functions/__fish_config_interactive.fish
@@ -110,9 +110,10 @@ function __fish_config_interactive -d "Initializations that should be performed 
     # Print a greeting.
     # fish_greeting can be a function (preferred) or a variable.
     #
-    if status --is-interactive and status --is-login
+    if status --is-interactivel
+        and status --is-login
         if functions -q fish_greeting
-                fish_greeting
+            fish_greeting
         else
             # The greeting used to be skipped when fish_greeting was empty (not just undefined)
             # Keep it that way to not print superfluous newlines on old configuration
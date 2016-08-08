e46978fedb97523270a5c6563f1da7464a417922
Kurtis Rader <krader@skepticism.us>
2016-08-07 19:29:14 -0700

    simplify previous change to the _ script
    There's no need for a local var or echo in a subcommand. Also, fix the
    formatting to match the style guide.

--- share/functions/_.fish
+++ share/functions/_.fish
@@ -1,14 +1,13 @@
 #
-# Alias for gettext (or a fallback if gettext isn't installed)
+# Alias for gettext or a fallback if gettext isn't installed.
 #
-set -l gettext_path (command -v gettext)
-if test -x (echo $gettext_path)
-	function _ --description "Alias for the gettext command"
-		command gettext fish $argv
-	end
+if command -s gettext >/dev/null
+    function _ --description "Alias for the gettext command"
+        command gettext fish $argv
+    end
 else
-	function _ --description "Fallback alias for the gettext command"
-		echo -n $argv
-	end
+    function _ --description "Fallback alias for the gettext command"
+        echo -n $argv
+    end
 end
 

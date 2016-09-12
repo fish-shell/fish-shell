3593b7ec0525933528f8f789d9a63dcaefbb6632
Aaron Gyes <me@aaron.gy>
2016-09-12 05:56:31 -0700

    Use 'grealpath' if installed for realpath fallback
    See discussion in #3370

--- share/functions/realpath.fish
+++ share/functions/realpath.fish
@@ -6,8 +6,10 @@
 # If we see that there is an external command by that name we just return. That will cause fish to
 # run the external command. On the other hand, if an external command isn't found we define a
 # function that will provide fallback behavior.
-if not type -q -P realpath
-    function realpath --description 'fallback realpath implementation'
-        builtin fish_realpath $argv[-1]
-    end
+if not command -v realpath > /dev/null
+	if command -v grealpath > /dev/null
+		alias realpath=grealpath
+	else
+		alias realpath=fish_realpath
+	end
 end

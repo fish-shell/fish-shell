eeea3f3b3c07dc91d6fdc30989022b4c836d9987
ridiculousfish <corydoras@ridiculousfish.com>
2016-10-01 16:48:12 -0700

    Revert "Add completions for `cd -`"
    This reverts commit 9d69f445501bbce01eb50a943e17613f8d59ba1b.

--- share/completions/cd.fish
+++ share/completions/cd.fish
@@ -1,17 +1,3 @@
 
 complete -x -c cd -a "(__fish_complete_cd)"
 complete -c cd -s h -l help --description 'Display help and exit'
-
-function __fish_complete_previous_dir
-    set -l dir
-    if test "$__fish_cd_direction" = "next"
-        set dir $dirnext[-1]
-    else
-        set dir $dirprev[-1]
-    end
-    if set -q dir[1]
-        printf '%s\t%s\n' - "Previous dir: $dir"
-    end
-end
-
-complete -c cd -a '(__fish_complete_previous_dir)'

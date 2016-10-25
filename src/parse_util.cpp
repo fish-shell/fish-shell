fish-shell/fish-shell

commit 42458ff7ab358475488db7c51fbff121f522d78a

lint: "collapsible if statements" warnings

src/parse_util.cpp
@@ -471,7 +471,6 @@ void parse_util_get_parameter_info(const wcstring &cmd, const size_t pos, wchar_
                                    size_t *offset, enum token_type *out_type) {
     size_t prev_pos = 0;
     wchar_t last_quote = '\0';
-    int unfinished;
 
     tokenizer_t tok(cmd.c_str(), TOK_ACCEPT_UNFINISHED | TOK_SQUASH_ERRORS);
     tok_t token;
@@ -488,30 +487,25 @@ void parse_util_get_parameter_info(const wcstring &cmd, const size_t pos, wchar_
     wchar_t *cmd_tmp = wcsdup(cmd.c_str());
     cmd_tmp[pos] = 0;
     size_t cmdlen = wcslen(cmd_tmp);
-    unfinished = (cmdlen == 0);
-    if (!unfinished) {
-        unfinished = (quote != 0);
-
-        if (!unfinished) {
-            if (wcschr(L" \t\n\r", cmd_tmp[cmdlen - 1]) != 0) {
-                if ((cmdlen == 1) || (cmd_tmp[cmdlen - 2] != L'\\')) {
-                    unfinished = 1;
-                }
-            }
+    bool finished = (cmdlen != 0);
+    if (finished) {
+        finished = (quote == NULL);
+        if (finished && wcschr(L" \t\n\r", cmd_tmp[cmdlen - 1]) == 0) {
+            finished = (cmdlen == 1) || (cmd_tmp[cmdlen - 2] != L'\\');
         }
     }
 
     if (quote) *quote = last_quote;
 
     if (offset != 0) {
-        if (!unfinished) {
+        if (finished) {
             while ((cmd_tmp[prev_pos] != 0) && (wcschr(L";|", cmd_tmp[prev_pos]) != 0)) prev_pos++;
-
             *offset = prev_pos;
         } else {
             *offset = pos;
         }
     }
+
     free(cmd_tmp);
 }
 
@@ -927,22 +921,21 @@ static parser_test_error_bits_t detect_dollar_cmdsub_errors(size_t arg_src_offse
                                                             parse_error_list_t *out_errors) {
     parser_test_error_bits_t result_bits = 0;
     wcstring unescaped_arg_src;
-    if (unescape_string(arg_src, &unescaped_arg_src, UNESCAPE_SPECIAL)) {
-        if (!unescaped_arg_src.empty()) {
-            wchar_t last = unescaped_arg_src.at(unescaped_arg_src.size() - 1);
-            if (last == VARIABLE_EXPAND) {
-                result_bits |= PARSER_TEST_ERROR;
-                if (out_errors != NULL) {
-                    wcstring subcommand_first_token = tok_first(cmdsubst_src);
-                    if (subcommand_first_token.empty()) {
-                        // e.g. $(). Report somthing.
-                        subcommand_first_token = L"...";
-                    }
-                    append_syntax_error(
-                        out_errors,
-                        arg_src_offset + arg_src.size() - 1,  // global position of the dollar
-                        ERROR_BAD_VAR_SUBCOMMAND1, truncate_string(subcommand_first_token).c_str());
+    if (unescape_string(arg_src, &unescaped_arg_src, UNESCAPE_SPECIAL) &&
+        !unescaped_arg_src.empty()) {
+        wchar_t last = unescaped_arg_src.at(unescaped_arg_src.size() - 1);
+        if (last == VARIABLE_EXPAND) {
+            result_bits |= PARSER_TEST_ERROR;
+            if (out_errors != NULL) {
+                wcstring subcommand_first_token = tok_first(cmdsubst_src);
+                if (subcommand_first_token.empty()) {
+                    // e.g. $(). Report somthing.
+                    subcommand_first_token = L"...";
                 }
+                append_syntax_error(
+                    out_errors,
+                    arg_src_offset + arg_src.size() - 1,  // global position of the dollar
+                    ERROR_BAD_VAR_SUBCOMMAND1, truncate_string(subcommand_first_token).c_str());
             }
         }
     }

src/builtin.cpp
@@ -3284,11 +3284,10 @@ int builtin_run(parser_t &parser, const wchar_t *const *argv, io_streams_t &stre
     cmd = (int (*)(parser_t & parser, io_streams_t & streams, const wchar_t *const *))(
         data ? data->func : NULL);
 
-    if (argv[1] != NULL && !builtin_handles_help(argv[0])) {
-        if (argv[2] == NULL && (parse_util_argument_is_help(argv[1], 0))) {
-            builtin_print_help(parser, streams, argv[0], streams.out);
-            return STATUS_BUILTIN_OK;
-        }
+    if (argv[1] != NULL && !builtin_handles_help(argv[0]) && argv[2] == NULL &&
+        parse_util_argument_is_help(argv[1], 0)) {
+        builtin_print_help(parser, streams, argv[0], streams.out);
+        return STATUS_BUILTIN_OK;
     }
 
     if (data != NULL) {

src/builtin_complete.cpp
@@ -286,39 +286,35 @@ int builtin_complete(parser_t &parser, io_streams_t &streams, wchar_t **argv) {
         }
     }
 
-    if (!res) {
-        if (condition && wcslen(condition)) {
-            const wcstring condition_string = condition;
-            parse_error_list_t errors;
-            if (parse_util_detect_errors(condition_string, &errors,
-                                         false /* do not accept incomplete */)) {
-                streams.err.append_format(L"%ls: Condition '%ls' contained a syntax error", argv[0],
-                                          condition);
-                for (size_t i = 0; i < errors.size(); i++) {
-                    streams.err.append_format(L"\n%s: ", argv[0]);
-                    streams.err.append(errors.at(i).describe(condition_string));
-                }
-                res = true;
+    if (!res && condition && wcslen(condition)) {
+        const wcstring condition_string = condition;
+        parse_error_list_t errors;
+        if (parse_util_detect_errors(condition_string, &errors,
+                                     false /* do not accept incomplete */)) {
+            streams.err.append_format(L"%ls: Condition '%ls' contained a syntax error", argv[0],
+                                      condition);
+            for (size_t i = 0; i < errors.size(); i++) {
+                streams.err.append_format(L"\n%s: ", argv[0]);
+                streams.err.append(errors.at(i).describe(condition_string));
             }
+            res = true;
         }
     }
 
-    if (!res) {
-        if (comp && wcslen(comp)) {
-            wcstring prefix;
-            if (argv[0]) {
-                prefix.append(argv[0]);
-                prefix.append(L": ");
-            }
+    if (!res && comp && wcslen(comp)) {
+        wcstring prefix;
+        if (argv[0]) {
+            prefix.append(argv[0]);
+            prefix.append(L": ");
+        }
 
-            wcstring err_text;
-            if (parser.detect_errors_in_argument_list(comp, &err_text, prefix.c_str())) {
-                streams.err.append_format(L"%ls: Completion '%ls' contained a syntax error\n",
-                                          argv[0], comp);
-                streams.err.append(err_text);
-                streams.err.push_back(L'\n');
-                res = true;
-            }
+        wcstring err_text;
+        if (parser.detect_errors_in_argument_list(comp, &err_text, prefix.c_str())) {
+            streams.err.append_format(L"%ls: Completion '%ls' contained a syntax error\n", argv[0],
+                                      comp);
+            streams.err.append(err_text);
+            streams.err.push_back(L'\n');
+            res = true;
         }
     }
 

src/builtin_set_color.cpp
@@ -159,19 +159,17 @@ int builtin_set_color(parser_t &parser, io_streams_t &streams, wchar_t **argv) {
     builtin_set_color_output.clear();
     output_set_writer(set_color_builtin_outputter);
 
-    if (bold) {
-        if (enter_bold_mode) writembs(tparm(enter_bold_mode));
+    if (bold && enter_bold_mode) {
+        writembs(tparm(enter_bold_mode));
     }
 
-    if (underline) {
-        if (enter_underline_mode) writembs(enter_underline_mode);
+    if (underline && enter_underline_mode) {
+        writembs(enter_underline_mode);
     }
 
-    if (bgcolor != NULL) {
-        if (bg.is_normal()) {
-            write_color(rgb_color_t::black(), false /* not is_fg */);
-            writembs(tparm(exit_attribute_mode));
-        }
+    if (bgcolor != NULL && bg.is_normal()) {
+        write_color(rgb_color_t::black(), false /* not is_fg */);
+        writembs(tparm(exit_attribute_mode));
     }
 
     if (!fg.is_none()) {
@@ -188,10 +186,8 @@ int builtin_set_color(parser_t &parser, io_streams_t &streams, wchar_t **argv) {
         }
     }
 
-    if (bgcolor != NULL) {
-        if (!bg.is_normal() && !bg.is_reset()) {
-            write_color(bg, false /* not is_fg */);
-        }
+    if (bgcolor != NULL && !bg.is_normal() && !bg.is_reset()) {
+        write_color(bg, false /* not is_fg */);
     }
 
     // Restore saved writer function.

src/common.cpp
@@ -1505,7 +1505,7 @@ bool list_contains_string(const wcstring_list_t &list, const wcstring &str) {
 }
 
 int create_directory(const wcstring &d) {
-    int ok = 0;
+    bool ok = false;
     struct stat buf;
     int stat_res = 0;
 
@@ -1514,18 +1514,10 @@ int create_directory(const wcstring &d) {
     }
 
     if (stat_res == 0) {
-        if (S_ISDIR(buf.st_mode)) {
-            ok = 1;
-        }
-    } else {
-        if (errno == ENOENT) {
-            wcstring dir = wdirname(d);
-            if (!create_directory(dir)) {
-                if (!wmkdir(d, 0700)) {
-                    ok = 1;
-                }
-            }
-        }
+        if (S_ISDIR(buf.st_mode)) ok = true;
+    } else if (errno == ENOENT) {
+        wcstring dir = wdirname(d);
+        if (!create_directory(dir) && !wmkdir(d, 0700)) ok = true;
     }
 
     return ok ? 0 : -1;

src/complete.cpp
@@ -670,22 +670,22 @@ void completer_t::complete_cmd(const wcstring &str_cmd, bool use_function, bool 
     std::vector<completion_t> possible_comp;
 
     if (use_command) {
-        if (expand_string(str_cmd, &this->completions,
-                          EXPAND_SPECIAL_FOR_COMMAND | EXPAND_FOR_COMPLETIONS | EXECUTABLES_ONLY |
-                              this->expand_flags(),
-                          NULL) != EXPAND_ERROR) {
-            if (this->wants_descriptions()) {
-                this->complete_cmd_desc(str_cmd);
+        expand_error_t result = expand_string(str_cmd, &this->completions,
+                                              EXPAND_SPECIAL_FOR_COMMAND | EXPAND_FOR_COMPLETIONS |
+                                                  EXECUTABLES_ONLY | this->expand_flags(),
+                                              NULL);
+        if (result != EXPAND_ERROR && this->wants_descriptions()) {
+            this->complete_cmd_desc(str_cmd);
             }
-        }
     }
-    if (use_implicit_cd) {
-        if (!expand_string(str_cmd, &this->completions,
-                           EXPAND_FOR_COMPLETIONS | DIRECTORIES_ONLY | this->expand_flags(),
-                           NULL)) {
-            // Not valid as implicit cd.
-        }
+
+    // WTF? This seems to be a noop.
+    if (use_implicit_cd &&
+        !expand_string(str_cmd, &this->completions,
+                       EXPAND_FOR_COMPLETIONS | DIRECTORIES_ONLY | this->expand_flags(), NULL)) {
+        // Not valid as implicit cd.
     }
+
     if (str_cmd.find(L'/') == wcstring::npos && str_cmd.at(0) != L'~') {
         if (use_function) {
             wcstring_list_t names = function_get_names(str_cmd.at(0) == L'_');
@@ -877,11 +877,10 @@ bool completer_t::complete_param(const wcstring &scmd_orig, const wcstring &spop
     if (this->type() == COMPLETE_DEFAULT) {
         ASSERT_IS_MAIN_THREAD();
         complete_load(cmd, true);
-    } else if (this->type() == COMPLETE_AUTOSUGGEST) {
-        // Maybe load this command (on the main thread).
-        if (!completion_autoloader.has_tried_loading(cmd)) {
-            iothread_perform_on_main(complete_load_no_reload, &cmd);
-        }
+    } else if (this->type() == COMPLETE_AUTOSUGGEST &&
+               !completion_autoloader.has_tried_loading(cmd)) {
+        // Load this command (on the main thread).
+        iothread_perform_on_main(complete_load_no_reload, &cmd);
     }
 
     // Make a list of lists of all options that we care about.
@@ -927,13 +926,12 @@ bool completer_t::complete_param(const wcstring &scmd_orig, const wcstring &spop
                 for (option_list_t::const_iterator oiter = options.begin(); oiter != options.end();
                      ++oiter) {
                     const complete_entry_opt_t *o = &*oiter;
-                    if (o->type == option_type_single_long) {
-                        if (param_match(o, popt) && this->condition_test(o->condition)) {
-                            old_style_match = true;
-                            if (o->result_mode & NO_COMMON) use_common = false;
-                            if (o->result_mode & NO_FILES) use_files = false;
-                            complete_from_args(str, o->comp, o->localized_desc(), o->flags);
-                        }
+                    if (o->type == option_type_single_long && param_match(o, popt) &&
+                        this->condition_test(o->condition)) {
+                        old_style_match = true;
+                        if (o->result_mode & NO_COMMON) use_common = false;
+                        if (o->result_mode & NO_FILES) use_files = false;
+                        complete_from_args(str, o->comp, o->localized_desc(), o->flags);
                     }
                 }
 

src/env.cpp
@@ -865,9 +865,7 @@ void env_push(bool new_scope) {
     node->next = top;
     node->new_scope = new_scope;
 
-    if (new_scope) {
-        if (local_scope_exports(top)) mark_changed_exported();
-    }
+    if (new_scope && local_scope_exports(top)) mark_changed_exported();
     top = node;
 }
 
@@ -885,7 +883,7 @@ void env_pop() {
             }
         }
 
-        if (killme->new_scope) {
+        if (killme->new_scope) {  //!OCLINT(collapsible if statements)
             if (killme->exportv || local_scope_exports(killme->next)) mark_changed_exported();
         }
 

src/env_universal_common.cpp
@@ -932,17 +932,16 @@ static bool get_mac_address(unsigned char macaddr[MAC_ADDRESS_MAX_LEN],
 
     if (getifaddrs(&ifap) == 0) {
         for (const ifaddrs *p = ifap; p; p = p->ifa_next) {
-            if (p->ifa_addr && p->ifa_addr->sa_family == AF_LINK) {
-                if (p->ifa_name && p->ifa_name[0] &&
-                    !strcmp((const char *)p->ifa_name, interface)) {
-                    const sockaddr_dl &sdl = *reinterpret_cast<sockaddr_dl *>(p->ifa_addr);
+            bool is_af_link = p->ifa_addr && p->ifa_addr->sa_family == AF_LINK;
+            if (is_af_link && p->ifa_name && p->ifa_name[0] &&
+                !strcmp((const char *)p->ifa_name, interface)) {
+                const sockaddr_dl &sdl = *reinterpret_cast<sockaddr_dl *>(p->ifa_addr);
 
-                    size_t alen = sdl.sdl_alen;
-                    if (alen > MAC_ADDRESS_MAX_LEN) alen = MAC_ADDRESS_MAX_LEN;
-                    memcpy(macaddr, sdl.sdl_data + sdl.sdl_nlen, alen);
-                    ok = true;
-                    break;
-                }
+                size_t alen = sdl.sdl_alen;
+                if (alen > MAC_ADDRESS_MAX_LEN) alen = MAC_ADDRESS_MAX_LEN;
+                memcpy(macaddr, sdl.sdl_data + sdl.sdl_nlen, alen);
+                ok = true;
+                break;
             }
         }
         freeifaddrs(ifap);
@@ -1033,12 +1032,11 @@ class universal_notifier_shmem_poller_t : public universal_notifier_t {
         }
 
         // Set the size, if it's too small.
-        if (!errored && size < (off_t)sizeof(universal_notifier_shmem_t)) {
-            if (ftruncate(fd, sizeof(universal_notifier_shmem_t)) < 0) {
-                int err = errno;
-                report_error(err, L"Unable to truncate shared memory object with path '%s'", path);
-                errored = true;
-            }
+        bool set_size = !errored && size < (off_t)sizeof(universal_notifier_shmem_t);
+        if (set_size && ftruncate(fd, sizeof(universal_notifier_shmem_t)) < 0) {
+            int err = errno;
+            report_error(err, L"Unable to truncate shared memory object with path '%s'", path);
+            errored = true;
         }
 
         // Memory map the region.
@@ -1237,10 +1235,12 @@ class universal_notifier_named_pipe_t : public universal_notifier_t {
         int fd = wopen_cloexec(vars_path, O_RDWR | O_NONBLOCK, 0600);
         if (fd < 0 && errno == ENOENT) {
             // File doesn't exist, try creating it.
-            if (mkfifo(narrow_path.c_str(), 0600) >= 0) {
+            int mkfifo_status = mkfifo(narrow_path.c_str(), 0600);
+            if (mkfifo_status != -1) {
                 fd = wopen_cloexec(vars_path, O_RDWR | O_NONBLOCK, 0600);
             }
         }
+
         if (fd < 0) {
             // Maybe open failed, maybe mkfifo failed.
             int err = errno;
@@ -1316,11 +1316,9 @@ class universal_notifier_named_pipe_t : public universal_notifier_t {
             // it back. Nobody is expected to read it except us.
             int pid_nbo = htonl(getpid());
             ssize_t amt_written = write(this->pipe_fd, &pid_nbo, sizeof pid_nbo);
-            if (amt_written < 0) {
-                if (errno == EWOULDBLOCK || errno == EAGAIN) {
-                    // Very unsual: the pipe is full!
-                    drain_excessive_data();
-                }
+            if (amt_written < 0 && errno == EWOULDBLOCK || errno == EAGAIN) {
+                // Very unsual: the pipe is full!
+                drain_excessive_data();
             }
 
             // Now schedule a read for some time in the future.

commit 2bb52c65c2afc449da8b55ded82e271116bd13d8

fix bug introduced by 42458ff7
There was one block of code modified by commit 42458ff7 that had
convoluted, inverted, logic. In the process of collapsing nested
"if" blocks the logic was modified to avoid using "!" everywhere the
bool was tested. Unfortunately I neglected to modify two of the
conditions used to set that var to reflect the changed polarity.

src/parse_util.cpp
@@ -470,7 +470,7 @@ static wchar_t get_quote(const wcstring &cmd_str, size_t len) {
 void parse_util_get_parameter_info(const wcstring &cmd, const size_t pos, wchar_t *quote,
                                    size_t *offset, enum token_type *out_type) {
     size_t prev_pos = 0;
-    wchar_t last_quote = '\0';
+    wchar_t last_quote = L'\0';
 
     tokenizer_t tok(cmd.c_str(), TOK_ACCEPT_UNFINISHED | TOK_SQUASH_ERRORS);
     tok_t token;
@@ -487,11 +487,11 @@ void parse_util_get_parameter_info(const wcstring &cmd, const size_t pos, wchar_
     wchar_t *cmd_tmp = wcsdup(cmd.c_str());
     cmd_tmp[pos] = 0;
     size_t cmdlen = wcslen(cmd_tmp);
-    bool finished = (cmdlen != 0);
+    bool finished = cmdlen != 0;
     if (finished) {
         finished = (quote == NULL);
-        if (finished && wcschr(L" \t\n\r", cmd_tmp[cmdlen - 1]) == 0) {
-            finished = (cmdlen == 1) || (cmd_tmp[cmdlen - 2] != L'\\');
+        if (finished && wcschr(L" \t\n\r", cmd_tmp[cmdlen - 1]) != L'\0') {
+            finished = cmdlen > 1 && cmd_tmp[cmdlen - 2] == L'\\';
         }
     }
 
fish-shell/fish-shell

commit 851e44934780f469927f19770d0fc65d82bed129

eliminate signed/unsigned comparison warnings
This partially addresses #3430.

src/pager.cpp
@@ -50,8 +50,8 @@ static size_t divide_round_up(size_t numer, size_t denom) {
 void pager_t::recalc_min_widths(comp_info_list_t *lst) const {
     for (size_t i = 0; i < lst->size(); i++) {
         comp_t *c = &lst->at(i);
-        c->min_width = mini(c->desc_width, maxi(0, available_term_width / 3 - 2)) +
-                       mini(c->desc_width, maxi(0, available_term_width / 5 - 4)) + 4;
+        c->min_width = mini(c->desc_width, maxi((size_t)0, available_term_width / 3 - 2)) +
+                       mini(c->desc_width, maxi((size_t)0, available_term_width / 5 - 4)) + 4;
     }
 }
 
@@ -84,14 +84,13 @@ static int print_max(const wcstring &str, highlight_spec_t color, int max, bool 
 
 /// Print the specified item using at the specified amount of space.
 line_t pager_t::completion_print_item(const wcstring &prefix, const comp_t *c, size_t row,
-                                      size_t column, int width, bool secondary, bool selected,
+                                      size_t column, size_t width, bool secondary, bool selected,
                                       page_rendering_t *rendering) const {
-    int comp_width = 0, desc_width = 0;
-    int written = 0;
-
+    size_t comp_width = 0, desc_width = 0;
+    size_t written = 0;
     line_t line_data;
 
-    if (c->pref_width <= width) {
+    if (c->pref_width <= (size_t)width) {
         // The entry fits, we give it as much space as it wants.
         comp_width = c->comp_width;
         desc_width = c->desc_width;
@@ -379,12 +378,12 @@ bool pager_t::completion_try_print(size_t cols, const wcstring &prefix, const co
     bool print = false;
 
     // Compute the effective term width and term height, accounting for disclosure.
-    int term_width = this->available_term_width;
-    int term_height =
+    size_t term_width = this->available_term_width;
+    size_t term_height =
         this->available_term_height - 1 -
         (search_field_shown ? 1 : 0);  // we always subtract 1 to make room for a comment row
     if (!this->fully_disclosed) {
-        term_height = mini(term_height, PAGER_UNDISCLOSED_MAX_ROWS);
+        term_height = mini(term_height, (size_t)PAGER_UNDISCLOSED_MAX_ROWS);
     }
 
     size_t row_count = divide_round_up(lst.size(), cols);
@@ -404,15 +403,15 @@ bool pager_t::completion_try_print(size_t cols, const wcstring &prefix, const co
         rendering->remaining_to_disclose = 0;
     }
 
-    int pref_tot_width = 0;
-    int min_tot_width = 0;
+    size_t pref_tot_width = 0;
+    size_t min_tot_width = 0;
 
     // Skip completions on tiny terminals.
     if (term_width < PAGER_MIN_WIDTH) return true;
 
     // Calculate how wide the list would be.
-    for (long col = 0; col < cols; col++) {
-        for (long row = 0; row < row_count; row++) {
+    for (size_t col = 0; col < cols; col++) {
+        for (size_t row = 0; row < row_count; row++) {
             int pref, min;
             const comp_t *c;
             if (lst.size() <= col * row_count + row) continue;
@@ -526,7 +525,7 @@ page_rendering_t pager_t::render() const {
     rendering.search_field_shown = this->search_field_shown;
     rendering.search_field_line = this->search_field_line;
 
-    for (int cols = PAGER_MAX_COLS; cols > 0; cols--) {
+    for (size_t cols = PAGER_MAX_COLS; cols > 0; cols--) {
         // Initially empty rendering.
         rendering.screen_data.resize(0);
 
@@ -641,7 +640,7 @@ bool pager_t::select_next_completion_in_direction(selection_direction_t directio
         // Cardinal directions. We have a completion index; we wish to compute its row and column.
         size_t current_row = this->get_selected_row(rendering);
         size_t current_col = this->get_selected_column(rendering);
-        size_t page_height = maxi(rendering.term_height - 1, 1);
+        size_t page_height = maxi(rendering.term_height - 1, (size_t)1);
 
         switch (direction) {
             case direction_page_north: {

src/pager.h
@@ -17,8 +17,8 @@
 /// Represents rendering from the pager.
 class page_rendering_t {
    public:
-    int term_width;
-    int term_height;
+    size_t term_width;
+    size_t term_height;
     size_t rows;
     size_t cols;
     size_t row_start;
@@ -47,8 +47,8 @@ page_rendering_t render_completions(const completion_list_t &raw_completions,
                                     const wcstring &prefix);
 
 class pager_t {
-    int available_term_width;
-    int available_term_height;
+    size_t available_term_width;
+    size_t available_term_height;
 
     size_t selected_completion_idx;
     size_t suggested_row_start;
@@ -73,13 +73,13 @@ class pager_t {
         /// The representative completion.
         completion_t representative;
         /// On-screen width of the completion string.
-        int comp_width;
+        size_t comp_width;
         /// On-screen width of the description information.
-        int desc_width;
+        size_t desc_width;
         /// Preferred total width.
-        int pref_width;
+        size_t pref_width;
         /// Minimum acceptable width.
-        int min_width;
+        size_t min_width;
 
         comp_t()
             : comp(),
@@ -114,7 +114,7 @@ class pager_t {
                           const wcstring &prefix, const comp_info_list_t &lst,
                           page_rendering_t *rendering) const;
     line_t completion_print_item(const wcstring &prefix, const comp_t *c, size_t row, size_t column,
-                                 int width, bool secondary, bool selected,
+                                 size_t width, bool secondary, bool selected,
                                  page_rendering_t *rendering) const;
 
    public:

src/builtin.cpp
@@ -2001,7 +2001,7 @@ static int builtin_read(parser_t &parser, io_streams_t &streams, wchar_t **argv)
         line = reader_readline(nchars);
         proc_pop_interactive();
         if (line) {
-            if (0 < nchars && nchars < wcslen(line)) {
+            if (0 < nchars && (size_t)nchars < wcslen(line)) {
                 // Line may be longer than nchars if a keybinding used `commandline -i`
                 // note: we're deliberately throwing away the tail of the commandline.
                 // It shouldn't be unread because it was produced with `commandline -i`,

src/common.cpp
@@ -118,7 +118,7 @@ show_stackframe(const wchar_t msg_level, int frame_count, int skip_levels) {
     if (frame_count < 1) frame_count = 999;
     debug_shared(msg_level, L"Backtrace:");
     std::vector<wcstring> bt = demangled_backtrace(frame_count, skip_levels + 2);
-    for (int i = 0; i < bt.size(); i++) {
+    for (int i = 0; (size_t)i < bt.size(); i++) {
         debug_shared(msg_level, bt[i]);
     }
 }

src/common.h
@@ -52,8 +52,8 @@ typedef std::vector<wcstring> wcstring_list_t;
 // Use Unicode "noncharacters" for internal characters as much as we can. This
 // gives us 32 "characters" for internal use that we can guarantee should not
 // appear in our input stream. See http://www.unicode.org/faq/private_use.html.
-#define RESERVED_CHAR_BASE 0xFDD0u
-#define RESERVED_CHAR_END 0xFDF0u
+#define RESERVED_CHAR_BASE (wchar_t)0xFDD0
+#define RESERVED_CHAR_END (wchar_t)0xFDF0
 // Split the available noncharacter values into two ranges to ensure there are
 // no conflicts among the places we use these special characters.
 #define EXPAND_RESERVED_BASE RESERVED_CHAR_BASE
@@ -63,9 +63,9 @@ typedef std::vector<wcstring> wcstring_list_t;
 // Make sure the ranges defined above don't exceed the range for noncharacters.
 // This is to make sure we didn't do something stupid in subdividing the
 // Unicode range for our needs.
-#if WILDCARD_RESERVED_END > RESERVED_CHAR_END
-#error
-#endif
+//#if WILDCARD_RESERVED_END > RESERVED_CHAR_END
+//#error
+//#endif
 
 // These are in the Unicode private-use range. We really shouldn't use this
 // range but have little choice in the matter given how our lexer/parser works.
@@ -79,9 +79,9 @@ typedef std::vector<wcstring> wcstring_list_t;
 // Note: We don't use the highest 8 bit range (0xF800 - 0xF8FF) because we know
 // of at least one use of a codepoint in that range: the Apple symbol (0xF8FF)
 // on Mac OS X. See http://www.unicode.org/faq/private_use.html.
-#define ENCODE_DIRECT_BASE 0xF600u
+#define ENCODE_DIRECT_BASE (wchar_t)0xF600
 #define ENCODE_DIRECT_END (ENCODE_DIRECT_BASE + 256)
-#define INPUT_COMMON_BASE 0xF700u
+#define INPUT_COMMON_BASE (wchar_t)0xF700
 #define INPUT_COMMON_END (INPUT_COMMON_BASE + 64)
 
 // Flags for unescape_string functions.

src/env_universal_common.cpp
@@ -826,7 +826,7 @@ var_table_t env_universal_t::read_message_internal(int fd) {
 
         // Walk over it by lines. The contents of an unterminated line will be left in 'line' for
         // the next iteration.
-        size_t line_start = 0;
+        ssize_t line_start = 0;
         while (line_start < amt) {
             // Run until we hit a newline.
             size_t cursor = line_start;
@@ -1033,7 +1033,7 @@ class universal_notifier_shmem_poller_t : public universal_notifier_t {
         }
 
         // Set the size, if it's too small.
-        if (!errored && size < sizeof(universal_notifier_shmem_t)) {
+        if (!errored && size < (off_t)sizeof(universal_notifier_shmem_t)) {
             if (ftruncate(fd, sizeof(universal_notifier_shmem_t)) < 0) {
                 int err = errno;
                 report_error(err, L"Unable to truncate shared memory object with path '%s'", path);
@@ -1120,7 +1120,7 @@ class universal_notifier_shmem_poller_t : public universal_notifier_t {
         // If it's been less than five seconds since the last change, we poll quickly Otherwise we
         // poll more slowly. Note that a poll is a very cheap shmem read. The bad part about making
         // this high is the process scheduling/wakeups it produces.
-        unsigned long usec_per_sec = 1000000;
+        long long usec_per_sec = 1000000;
         if (get_time() - last_change_time < 5LL * usec_per_sec) {
             return usec_per_sec / 10;  // 10 times a second
         }

src/event.cpp
@@ -301,7 +301,7 @@ int event_get(const event_t &criterion, std::vector<event_t *> *out) {
 bool event_is_signal_observed(int sig) {
     // We are in a signal handler! Don't allocate memory, etc.
     bool result = false;
-    if (sig >= 0 && sig < sizeof s_observed_signals / sizeof *s_observed_signals) {
+    if (sig >= 0 && (unsigned long)sig < sizeof(s_observed_signals) / sizeof(*s_observed_signals)) {
         result = s_observed_signals[sig];
     }
     return result;

src/expand.cpp
@@ -756,13 +756,12 @@ static int expand_variables(const wcstring &instr, std::vector<completion_t> *ou
     for (long i = last_idx - 1; (i >= 0) && is_ok && !empty; i--) {
         const wchar_t c = instr.at(i);
         if ((c == VARIABLE_EXPAND) || (c == VARIABLE_EXPAND_SINGLE)) {
-            long start_pos = i + 1;
-            long stop_pos;
+            size_t start_pos = i + 1;
+            size_t stop_pos;
             long var_len;
             int is_single = (c == VARIABLE_EXPAND_SINGLE);
 
             stop_pos = start_pos;
-
             while (stop_pos < insize) {
                 const wchar_t nc = instr.at(stop_pos);
                 if (nc == VARIABLE_EXPAND_EMPTY) {

commit 075be74cc4252a60df0ef24a4d68d064896b1344

fix regression introduced by commit 851e449
My earlier attempt with commit 851e449 to eliminate all the compiler
warnings about mixing signed and unsigned ints in an expression
introduced a subtle bug. This fixes that mistake.

Fixes #3488

src/pager.cpp
@@ -84,16 +84,15 @@ static int print_max(const wcstring &str, highlight_spec_t color, int max, bool 
 
 /// Print the specified item using at the specified amount of space.
 line_t pager_t::completion_print_item(const wcstring &prefix, const comp_t *c, size_t row,
-                                      size_t column, size_t width, bool secondary, bool selected,
+                                      size_t column, int width, bool secondary, bool selected,
                                       page_rendering_t *rendering) const {
     UNUSED(column);
     UNUSED(row);
     UNUSED(rendering);
-    size_t comp_width = 0, desc_width = 0;
-    size_t written = 0;
+    int comp_width, desc_width;
     line_t line_data;
 
-    if (c->pref_width <= (size_t)width) {
+    if (c->pref_width <= width) {
         // The entry fits, we give it as much space as it wants.
         comp_width = c->comp_width;
         desc_width = c->desc_width;
@@ -102,8 +101,8 @@ line_t pager_t::completion_print_item(const wcstring &prefix, const comp_t *c, s
         // the space to the completion, and whatever is left to the description.
         int desc_all = c->desc_width ? c->desc_width + 4 : 0;
 
-        comp_width = maxi(mini(c->comp_width, 2 * (width - 4) / 3), width - desc_all);
-        if (c->desc_width) desc_width = width - comp_width - 4;
+        comp_width = maxi(mini((int)c->comp_width, 2 * (width - 4) / 3), width - desc_all);
+        desc_width = c->desc_width ? width - 4 - comp_width : 0;
     }
 
     int bg_color = secondary ? highlight_spec_pager_secondary : highlight_spec_normal;
@@ -111,6 +110,7 @@ line_t pager_t::completion_print_item(const wcstring &prefix, const comp_t *c, s
         bg_color = highlight_spec_search_match;
     }
 
+    int written = 0;
     for (size_t i = 0; i < c->comp.size(); i++) {
         const wcstring &comp = c->comp.at(i);
 

src/pager.h
@@ -114,7 +114,7 @@ class pager_t {
                           const wcstring &prefix, const comp_info_list_t &lst,
                           page_rendering_t *rendering) const;
     line_t completion_print_item(const wcstring &prefix, const comp_t *c, size_t row, size_t column,
-                                 size_t width, bool secondary, bool selected,
+                                 int width, bool secondary, bool selected,
                                  page_rendering_t *rendering) const;
 
    public:
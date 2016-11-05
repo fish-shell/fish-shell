c02f5ceb0fed2e0ce7676a4460e2220961cc6661
Aaron Gyes <me@aaron.gy>
2016-11-04 19:05:10 -0700

    Add a fish_key_reader wrapper
    This will be necessary for fkk to work when one is using the .app
    bundle and has not installed fish otherwise.
    
    I just copied fish_indent.fish.

--- share/functions/fish_indent.fish
+++ share/functions/fish_indent.fish
@@ -1,7 +1,7 @@
-# check if command fish_indent works and is the same version that
+# check if command fish_key_reader works and is the same version that
 # came with this fish. This will happen one time.
-command -s fish_indent > /dev/null
-and command fish_indent --version 2>&1 | string match -rq $FISH_VERSION
+command -s fish_key_reader > /dev/null
+and command fish_key_reader --version 2>&1 | string match -rq $FISH_VERSION
 # if alias doesn't define the function here, this is an autoloaded "nothing".
 # the command (if there is one) will be used by default.
-or alias fish_indent=(string escape $__fish_bin_dir/fish_indent)
+or alias fish_key_reader=(string escape $__fish_bin_dir/fish_key_reader)

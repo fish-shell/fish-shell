e75b59893d73577944a1466d94b21dad70440eaf
Kurtis Rader <krader@skepticism.us>
2016-11-15 15:47:19 -0800

    make it easy to get the legacy hybrid key bindings
    The changes related to issue #3068 removed most of the emacs bindings
    from vi mode. However, since fish 2.4.0 was released several people have
    pointed out that the directions for reinstating the legacy hybrid key
    bindings don't work. This change fixes that and makes it easier to use
    the legacy hybrid bindings.
    
    Fixes #3556

--- share/functions/fish_default_key_bindings.fish
+++ share/functions/fish_default_key_bindings.fish
@@ -1,12 +1,13 @@
 function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fish"
     if not set -q argv[1]
-        # Clear earlier bindings, if any
-        bind --erase --all
+        bind --erase --all  # clear earlier bindings, if any
         if test "$fish_key_bindings" != "fish_default_key_bindings"
             # Allow the user to set the variable universally
             set -q fish_key_bindings
             or set -g fish_key_bindings
-            set fish_key_bindings fish_default_key_bindings # This triggers the handler, which calls us again and ensures the user_key_bindings are executed
+            # This triggers the handler, which calls us again and ensures the user_key_bindings
+            # are executed.
+            set fish_key_bindings fish_default_key_bindings
             return
         end
     end

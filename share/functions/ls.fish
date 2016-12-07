b48b2ddfb21dbd04dcfa0a3378167109d403a29e
Kurtis Rader <krader@skepticism.us>
2016-12-07 13:55:31 -0800

    fix extraction of LS_COLORS
    Strip surrounding quotes if present.
    
    Fixes #3630

--- share/functions/ls.fish
+++ share/functions/ls.fish
@@ -23,6 +23,10 @@ if command ls --version >/dev/null ^/dev/null
             # Here we rely on the legacy behavior of `dircolors -c` producing output suitable for
             # csh in order to extract just the data we're interested in.
             set -gx LS_COLORS (dircolors -c $colorfile | string split ' ')[3]
+            # The value should always be quoted but be conservative and check first.
+            if string match -qr '^([\'"]).*\1$' -- $LS_COLORS
+                set LS_COLORS (string match -r '^.(.*).$' $LS_COLORS)[2]
+            end
         end
     end
 else if command ls -G / >/dev/null ^/dev/null

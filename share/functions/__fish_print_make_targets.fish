b8dae97baf6043014f99c45da8a4d3ef4a8e68b2
Terje Larsen <terlar@gmail.com>
2016-12-07 13:30:26 +0100

    Fix print make targets with non-English locale
    Since we filter by text that is localized we need to ensure the correct
    locale is used.

--- share/functions/__fish_print_make_targets.fish
+++ share/functions/__fish_print_make_targets.fish
@@ -1,11 +1,15 @@
 function __fish_print_make_targets --argument directory
+    # Since we filter based on localized text, we need to ensure the
+    # text will be using the correct locale.
+    set -lx LC_ALL C
+
     if test -z "$directory"
         set directory '.'
     end
 
     for file in $directory/{GNUmakefile,Makefile,makefile}
         if test -f $file
-            make -C $directory -prRn | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($1 !~ "^[#.]") {print $1}}' ^/dev/null
+            make -C $directory -prRn | awk -v RS= -F: '/^# Files/,/^# Finished Make data base/ {if ($1 !~ "^[#.]") {print $1}}' ^/dev/null
             break
         end
     end

18e35b08b5321bc3ab3bca8f3fecb0087f90bced
Terje Larsen <terlar@gmail.com>
2016-12-13 12:08:15 +0100

    Support for BSD make
    This detects if the make command have the `-p` switch otherwise it
    assumes it is BSD make and will run a different command to try to figure
    out the available targets.

--- share/functions/__fish_print_make_targets.fish
+++ share/functions/__fish_print_make_targets.fish
@@ -7,9 +7,20 @@ function __fish_print_make_targets --argument directory
         set directory '.'
     end
 
+    set -l bsd_make
+    if make -p -f/dev/null ^/dev/null
+        set bsd_make 0
+    else
+        set bsd_make 1
+    end
+
     for file in $directory/{GNUmakefile,Makefile,makefile}
         if test -f $file
-            make -C $directory -prRn | awk -v RS= -F: '/^# Files/,/^# Finished Make data base/ {if ($1 !~ "^[#.]") {print $1}}' ^/dev/null
+            if test "$bsd_make" = 0
+                make -C $directory -prRn | awk -v RS= -F: '/^# Files/,/^# Finished Make data base/ {if ($1 !~ "^[#.]") {print $1}}' ^/dev/null
+            else
+                make -C $directory -d g1 -rn >/dev/null ^| awk -F, '/^#\*\*\* Input graph:/,/^$/ {if ($1 !~ "^#... ") {gsub(/# /,"",$1); print $1}}' ^/dev/null
+            end
             break
         end
     end

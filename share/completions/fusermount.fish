75edd5ab1891ad411d5d9568acce2b9fe5fdf2df
Pierre Neidhardt <ambrevar@gmail.com>
2016-12-13 10:03:37 +0530

    Fix fusermount completion

--- share/completions/fusermount.fish
+++ share/completions/fusermount.fish
@@ -3,13 +3,7 @@
 #
 # Find mount points, borrowed from umount.fish
 #
-complete -c fusermount --description "Mount point" -x -a '
-(
-	cat /etc/mtab | __fish_sgrep "^sshfs" | cut -d " " -f 1-2|tr " " \n|sed -e "s/[0-9\.]*:\//\//"| __fish_sgrep "^/"
-	cat /etc/mtab | __fish_sgrep "^fuseiso" | cut -d " " -f 1-2|tr " " \n|sed -e "s/[0-9\.]*:\//\//"| __fish_sgrep "^/"
-)
-'
-
+complete -c fusermount --description "Mount point" -x -a '(__fish_print_mounted)'
 complete -c fusermount -s h --description "Display help and exit"
 complete -c fusermount -s v --description "Display version and exit"
 complete -c fusermount -s o -x --description "Mount options"

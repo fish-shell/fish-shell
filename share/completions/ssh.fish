28cde83c3323cf61bbb2ac4a4ae2b99c66d1a7b5
Wilke Schwiedop <drwilly@drwilly.de>
2016-12-21 18:44:34 +0100

    requested changes

--- share/completions/ssh.fish
+++ share/completions/ssh.fish
@@ -23,7 +23,7 @@ complete -c ssh -s a --description "Disables forwarding of the authentication ag
 complete -c ssh -s A --description "Enables forwarding of the authentication agent"
 complete -x -c ssh -s b --description "Interface to transmit from" -a "
 (
-	tail -n +2 /proc/net/arp | cut -d ' ' -f 1 ^/dev/null
+	cut -d ' ' -f 1 /proc/net/arp ^/dev/null | string match -r -v '^IP'
 )
 "
 

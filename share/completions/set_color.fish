41a41b15915895e970c91cc4987e668633a95e26
David Adam <zanchey@ucc.gu.uwa.edu.au>
2017-01-11 22:00:33 +0800

    set_color: add completions for italics, dim and reverse

--- share/completions/set_color.fish
+++ share/completions/set_color.fish
@@ -1,6 +1,9 @@
 complete -c set_color -x --description "Color" -a '(set_color --print-colors)'
 complete -c set_color -s b -l background -x -a '(set_color --print-colors)' --description "Change background color"
 complete -c set_color -s o -l bold --description 'Make font bold'
+complete -c set_color -s i -l italics --description 'Italicise'
+complete -c set_color -s d -l dim --description 'Dim text'
+complete -c set_color -s r -l reverse --description 'Reverse color text'
 complete -c set_color -s u -l underline --description 'Underline text'
 complete -c set_color -s h -l help --description 'Display help and exit'
 complete -c set_color -s c -l print-colors --description 'Print a list of all accepted color names'

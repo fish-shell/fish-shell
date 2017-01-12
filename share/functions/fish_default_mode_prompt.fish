6f9f7632f3b153d64814652390370e4d60677ef3
Cristian Prieto <me@cprieto.com>
2017-01-13 02:07:41 +1100

    Added default mode prompt function (#3727)
    * Added new function for the default prompt mode
    
    Now fish mode prompt will call fish_default_mode_prompt, this will solve #3641
    
    * Added function description
    
    * Change wording for documentation about default mode prompt
    
    * Finish changes requested in code review

---
+++ share/functions/fish_default_mode_prompt.fish
@@ -0,0 +1,23 @@
+function fish_default_mode_prompt --description "Display the default mode for the prompt"
+    # Do nothing if not in vi mode
+    if test "$fish_key_bindings" = "fish_vi_key_bindings"
+        or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
+        switch $fish_bind_mode
+            case default
+                set_color --bold --background red white
+                echo '[N]'
+            case insert
+                set_color --bold --background green white
+                echo '[I]'
+            case replace-one
+                set_color --bold --background green white
+                echo '[R]'
+            case visual
+                set_color --bold --background magenta white
+                echo '[V]'
+        end
+        set_color normal
+        echo -n ' '
+    end
+end
+

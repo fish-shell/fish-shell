9172f7c0ab360e5a69b8aede45b99a2cf756d816
Clément Martinez <clementmartinezdev@gmail.com>
2017-01-17 23:02:47 +0100

    Improve __fish_print_xdg_mimetypes
    It now:
     - Checks if `mimeinfo.cache` file exists before attempting to read it
       (avoiding an error message to be displayed which messes with the
       prompt).
     - Uses `string` instead of `grep` (+ `cat` which was useless here)
       improving performence a bit.

--- share/functions/__fish_print_xdg_mimetypes.fish
+++ share/functions/__fish_print_xdg_mimetypes.fish
@@ -1,5 +1,7 @@
 function __fish_print_xdg_mimetypes --description 'Print XDG mime types'
-    cat ~/.local/share/applications/mimeinfo.cache | grep -v "\[MIME Cache\]" | tr = \t
-    cat /usr/share/applications/mimeinfo.cache | grep -v "\[MIME Cache\]" | tr = \t
-
+    for file in {~/.local,/usr}/share/applications/mimeinfo.cache
+        if test -r $file
+            string match -v "[MIME Cache]" <$file | string replace = \t
+        end
+    end
 end

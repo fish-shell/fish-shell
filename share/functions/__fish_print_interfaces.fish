5dd70bacfa30c567855391c4c344bf3f4bb05da1
Mehul Tikekar <mtikekar@mit.edu>
2017-01-18 12:50:43 -0500

    Remove cd in __fish_print_interfaces

--- share/functions/__fish_print_interfaces.fish
+++ share/functions/__fish_print_interfaces.fish
@@ -1,8 +1,7 @@
 function __fish_print_interfaces --description "Print a list of known network interfaces"
     if test -d /sys/class/net
-        cd /sys/class/net
-        for i in *
-            echo $i
+        for i in /sys/class/net/*
+            basename $i
         end
     else # OSX/BSD
         command ifconfig -l | string split ' '

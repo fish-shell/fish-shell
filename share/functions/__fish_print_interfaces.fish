4b01bbe4e3e5fbc4cebc9abcfa41ba17f5400758
Mehul Tikekar <mtikekar@mit.edu>
2017-01-18 17:54:50 -0500

    Avoid wildcard error in __fish_print_interfaces

--- share/functions/__fish_print_interfaces.fish
+++ share/functions/__fish_print_interfaces.fish
@@ -1,6 +1,7 @@
 function __fish_print_interfaces --description "Print a list of known network interfaces"
     if test -d /sys/class/net
-        string replace /sys/class/net/ '' /sys/class/net/*
+        set -l interfaces /sys/class/net/*
+        string replace /sys/class/net/ '' $interfaces
     else # OSX/BSD
         command ifconfig -l | string split ' '
     end

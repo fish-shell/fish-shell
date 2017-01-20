80c35aa9412c2b2893399806ea64fd84037f8360
Clément Martinez <clementmartinezdev@gmail.com>
2017-01-20 13:09:54 +0100

    mount & udisksctl completions: split mount options

---
+++ share/functions/__fish_print_mount_opts.fish
@@ -0,0 +1,20 @@
+function __fish_print_mount_opts
+    echo async\tUse\ asynchronous\ I/O
+    echo atime\tUpdate\ time\ on\ each\ access
+    echo auto\tMounted\ with\ -a
+    echo defaults\tUse\ default\ options
+    echo dev\tInterpret\ character/block\ special\ devices
+    echo exec\tPermit\ executables _netdev\tFilesystem\ uses\ network
+    echo noatime\tDo\ not\ update\ time\ on\ each\ access
+    echo noauto\tNot\ mounted\ by\ -a
+    echo nodev\tDo\ not\ interpret\ character/block\ special\ devices
+    echo noexec\tDo\ not\ permit\ executables
+    echo nosuid\tIgnore\ suid\ bits nouser\tOnly\ root\ may\ mount
+    echo remount\tRemount\ read-only\ filesystem
+    echo ro\tMount\ read-only rw\tMount\ read-write
+    echo suid\tAllow\ suid\ bits
+    echo sync\tUse\ synchronous\ I/O
+    echo dirsync\tUse\ synchronous\ directory\ operations
+    echo user\tAny\ user\ may\ mount
+    echo users\tAny\ user\ may\ mount\ and\ unmount
+end

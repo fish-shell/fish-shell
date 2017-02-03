708d6b6911be6dcc9ab9af97bba0c15af8c74dff
David Adam <zanchey@ucc.gu.uwa.edu.au>
2017-02-03 09:46:58 +0800

    Bump version for 2.5.0

--- osx/config.h
+++ osx/config.h
@@ -197,7 +197,7 @@
 #define PACKAGE_NAME "fish"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "fish 2.5b1"
+#define PACKAGE_STRING "fish 2.5.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "fish"
@@ -206,7 +206,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.5b1"
+#define PACKAGE_VERSION "2.5.0"
 
 /* The size of `wchar_t', as computed by sizeof. */
 #define SIZEOF_WCHAR_T 4

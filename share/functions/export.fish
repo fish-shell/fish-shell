fish-shell/fish-shell

commit d9b30ab09013ef803314d9656e669bc7577b585e

`setenv` should behave like `export`
Fixes #3897

share/functions/export.fish
@@ -1,6 +1,6 @@
-function export --description 'Set global variable. Alias for set -gx, made for bash compatibility'
-    if test -z "$argv"
-        set
+function export --description 'Set env variable. Alias for `set -gx` for bash compatibility.'
+    if not set -q argv[0]
+        set -x
         return 0
     end
     for arg in $argv

share/functions/setenv.fish
@@ -1,4 +1,20 @@
-
-function setenv --description 'Set global variable. Alias for set -g, made for csh compatibility'
-    set -gx $argv
+function setenv --description 'Set env variable. Alias for `set -gx` for csh compatibility.'
+    if not set -q argv[0]
+        set -x
+        return 0
+    end
+    for arg in $argv
+        set -l v (string split -m 1 "=" -- $arg)
+        switch (count $v)
+            case 1
+                set -gx $v $$v
+            case 2
+                if contains -- $v[1] PATH CDPATH MANPATH
+                    set -l colonized_path (string replace -- "$$v[1]" (string join ":" -- $$v[1]) $v[2])
+                    set -gx $v[1] (string split ":" -- $colonized_path)
+                else
+                    set -gx $v[1] $v[2]
+                end
+        end
+    end
 end

commit 085a9b9267ef217566af6681900a0da6f9a9618f

fix stupid typo introduced by d9b30ab09

share/functions/export.fish
@@ -1,5 +1,5 @@
 function export --description 'Set env variable. Alias for `set -gx` for bash compatibility.'
-    if not set -q argv[0]
+    if not set -q argv[1]
         set -x
         return 0
     end

share/functions/setenv.fish
@@ -1,5 +1,5 @@
 function setenv --description 'Set env variable. Alias for `set -gx` for csh compatibility.'
-    if not set -q argv[0]
+    if not set -q argv[1]
         set -x
         return 0
     end
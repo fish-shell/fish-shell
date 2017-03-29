d181d554d28adb3fb3eabc31c5bd8bcc8fc78e39
SanskritFritz <SanskritFritz+github@gmail.com>
2017-03-29 23:26:38 +0200

    Obsolete completions removed

--- share/completions/burp.fish
+++
@@ -1,7 +0,0 @@
-
-complete -c burp -s u -l user --description 'AUR login username'
-complete -c burp -s p -l password --description 'AUR login password'
-complete -c burp -s c -l category --description 'Specify CATEGORY for the package'
-complete -c burp -s C -l cookies --description 'The path in which to store cookies'
-complete -c burp -s k -l keep-cookies --description 'Cookies will be persistent'
-complete -c burp -s v -l verbose --description 'Be more verbose'

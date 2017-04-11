da09a915f2b348fa5dfee9af174d8231a4487a03
Kurtis Rader <krader@skepticism.us>
2017-04-10 20:40:25 -0700

    improve `__fish_complete_directories` function
    Reviewing a PR for a completion script caused me to look at the
    implementation for the `__fish_complete_directories` function. Which in
    turn lead me to create this change to improve its implementation and add
    unit tests for the function.

--- share/functions/__fish_complete_directories.fish
+++ share/functions/__fish_complete_directories.fish
@@ -2,20 +2,13 @@
 # Find directories that complete $argv[1], output them as completions
 # with description $argv[2] if defined, otherwise use 'Directory'
 #
-
-function __fish_complete_directories -d "Complete using directories" --argument comp
-
-    set desc (_ Directory)
-
-    if test (count $argv) -gt 1
-        set desc $argv[2]
+function __fish_complete_directories -d "Complete directory prefixes" --argument comp desc
+    if not set -q desc[1]
+        set desc (_ Directory)
     end
 
-    eval "set dirs "$comp"*/"
-
-    if test $dirs[1]
+    set -l dirs $comp*/
+    if set -q dirs[1]
         printf "%s\t$desc\n" $dirs
     end
-
 end
-

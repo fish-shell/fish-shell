bea0b17c9ef2d1b9c32e75598ca9baab5381b2f3
Christian Rondeau <christian.rondeau@gmail.com>
2017-04-23 08:13:02 -0400

    Aucompletion guards for /etc/passwd #3980

--- share/functions/__fish_complete_users.fish
+++ share/functions/__fish_complete_users.fish
@@ -4,7 +4,7 @@ function __fish_complete_users --description "Print a list of local users, with 
         getent passwd | cut -d : -f 1,5 | string replace -r ':' \t
     else if test -x /usr/bin/dscl
         dscl . -list /Users RealName | string match -r -v '^_' | string replace -r ' {2,}' \t
-    else
+    else if test -e /etc/passwd
         string match -v -r '^\s*#' </etc/passwd | cut -d : -f 1,5 | string replace ':' \t
     end
 end

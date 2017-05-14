846de82bb4f43940a86982db4d268eeffba52a50
David Adam <zanchey@ucc.gu.uwa.edu.au>
2017-05-14 20:13:55 +0800

    build_tools: drop completions generators
    The generators for various version control systems have not been run for
    some time and will overwrite the updated completions already in the the
    tree.

--- build_tools/make_vcs_completions.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-#
-# This file produces command specific completions for hg, darcs and a
-# few other vcs systems.
-
-build_tools/make_darcs_completions.fish
-build_tools/make_hg_completions.fish
-build_tools/make_svn_completions.fish
-build_tools/make_csv_completions.fish

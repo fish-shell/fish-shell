fish-shell/fish-shell

commit 21521b295397f44d073e94276b8b761f009410ea

lint: too few branches in switch statement
Someone was way too enamored of the `switch` statement. Using it in
places where a simple `if...else if...else` was clearer and shorter.

src/builtin.cpp
@@ -345,23 +345,16 @@ static int get_terminfo_sequence(const wchar_t *seq, wcstring *out_seq, io_strea
     if (input_terminfo_get_sequence(seq, out_seq)) {
         return 1;
     }
+
     wcstring eseq = escape_string(seq, 0);
-    switch (errno) {
-        case ENOENT: {
-            streams.err.append_format(_(L"%ls: No key with name '%ls' found\n"), L"bind",
-                                      eseq.c_str());
-            break;
-        }
-        case EILSEQ: {
-            streams.err.append_format(_(L"%ls: Key with name '%ls' does not have any mapping\n"),
-                                      L"bind", eseq.c_str());
-            break;
-        }
-        default: {
-            streams.err.append_format(_(L"%ls: Unknown error trying to bind to key named '%ls'\n"),
-                                      L"bind", eseq.c_str());
-            break;
-        }
+    if (errno == ENOENT) {
+        streams.err.append_format(_(L"%ls: No key with name '%ls' found\n"), L"bind", eseq.c_str());
+    } else if (errno == EILSEQ) {
+        streams.err.append_format(_(L"%ls: Key with name '%ls' does not have any mapping\n"),
+                                  L"bind", eseq.c_str());
+    } else {
+        streams.err.append_format(_(L"%ls: Unknown error trying to bind to key named '%ls'\n"),
+                                  L"bind", eseq.c_str());
     }
     return 0;
 }
@@ -525,43 +518,37 @@ static int builtin_bind(parser_t &parser, io_streams_t &streams, wchar_t **argv)
             break;
         }
         case BIND_INSERT: {
-            switch (argc - w.woptind) {
-                case 0: {
-                    builtin_bind_list(bind_mode_given ? bind_mode : NULL, streams);
-                    break;
+            int arg_count = argc - w.woptind;
+            if (arg_count == 0) {
+                builtin_bind_list(bind_mode_given ? bind_mode : NULL, streams);
+            } else if (arg_count == 1) {
+                wcstring seq;
+                if (use_terminfo) {
+                    if (!get_terminfo_sequence(argv[w.woptind], &seq, streams)) {
+                        res = STATUS_BUILTIN_ERROR;
+                        // get_terminfo_sequence already printed the error.
+                        break;
+                    }
+                } else {
+                    seq = argv[w.woptind];
                 }
-                case 1: {
-                    wcstring seq;
+                if (!builtin_bind_list_one(seq, bind_mode, streams)) {
+                    res = STATUS_BUILTIN_ERROR;
+                    wcstring eseq = escape_string(argv[w.woptind], 0);
                     if (use_terminfo) {
-                        if (!get_terminfo_sequence(argv[w.woptind], &seq, streams)) {
-                            res = STATUS_BUILTIN_ERROR;
-                            // get_terminfo_sequence already printed the error.
-                            break;
-                        }
+                        streams.err.append_format(_(L"%ls: No binding found for key '%ls'\n"),
+                                                  argv[0], eseq.c_str());
                     } else {
-                        seq = argv[w.woptind];
+                        streams.err.append_format(_(L"%ls: No binding found for sequence '%ls'\n"),
+                                                  argv[0], eseq.c_str());
                     }
-                    if (!builtin_bind_list_one(seq, bind_mode, streams)) {
-                        res = STATUS_BUILTIN_ERROR;
-                        wcstring eseq = escape_string(argv[w.woptind], 0);
-                        if (use_terminfo) {
-                            streams.err.append_format(_(L"%ls: No binding found for key '%ls'\n"),
-                                                      argv[0], eseq.c_str());
-                        } else {
-                            streams.err.append_format(
-                                _(L"%ls: No binding found for sequence '%ls'\n"), argv[0],
-                                eseq.c_str());
-                        }
-                    }
-                    break;
                 }
-                default: {
-                    if (builtin_bind_add(argv[w.woptind], argv + (w.woptind + 1),
-                                         argc - (w.woptind + 1), bind_mode, sets_bind_mode,
-                                         use_terminfo, streams)) {
-                        res = STATUS_BUILTIN_ERROR;
-                    }
-                    break;
+                break;
+            } else {
+                if (builtin_bind_add(argv[w.woptind], argv + (w.woptind + 1),
+                                     argc - (w.woptind + 1), bind_mode, sets_bind_mode,
+                                     use_terminfo, streams)) {
+                    res = STATUS_BUILTIN_ERROR;
                 }
             }
             break;
@@ -1776,39 +1763,33 @@ static int builtin_random(parser_t &parser, io_streams_t &streams, wchar_t **arg
         }
     }
 
-    switch (argc - w.woptind) {
-        case 0: {
-            long res;
-            if (!seeded) {
-                seeded = 1;
-                srand48_r(time(0), &seed_buffer);
-            }
-            lrand48_r(&seed_buffer, &res);
-            streams.out.append_format(L"%ld\n", res % 32768);
-            break;
-        }
-        case 1: {
-            long foo;
-            wchar_t *end = 0;
-
-            errno = 0;
-            foo = wcstol(argv[w.woptind], &end, 10);
-            if (errno || *end) {
-                streams.err.append_format(_(L"%ls: Seed value '%ls' is not a valid number\n"),
-                                          argv[0], argv[w.woptind]);
-
-                return STATUS_BUILTIN_ERROR;
-            }
+    int arg_count = argc - w.woptind;
+    if (arg_count == 0) {
+        long res;
+        if (!seeded) {
             seeded = 1;
-            srand48_r(foo, &seed_buffer);
-            break;
+            srand48_r(time(0), &seed_buffer);
         }
-        default: {
-            streams.err.append_format(_(L"%ls: Expected zero or one argument, got %d\n"), argv[0],
-                                      argc - w.woptind);
-            builtin_print_help(parser, streams, argv[0], streams.err);
+        lrand48_r(&seed_buffer, &res);
+        streams.out.append_format(L"%ld\n", res % 32768);
+    } else if (arg_count == 1) {
+        long foo;
+        wchar_t *end = 0;
+
+        errno = 0;
+        foo = wcstol(argv[w.woptind], &end, 10);
+        if (errno || *end) {
+            streams.err.append_format(_(L"%ls: Seed value '%ls' is not a valid number\n"), argv[0],
+                                      argv[w.woptind]);
             return STATUS_BUILTIN_ERROR;
         }
+        seeded = 1;
+        srand48_r(foo, &seed_buffer);
+    } else {
+        streams.err.append_format(_(L"%ls: Expected zero or one argument, got %d\n"), argv[0],
+                                  argc - w.woptind);
+        builtin_print_help(parser, streams, argv[0], streams.err);
+        return STATUS_BUILTIN_ERROR;
     }
     return STATUS_BUILTIN_OK;
 }
@@ -1900,19 +1881,16 @@ static int builtin_read(parser_t &parser, io_streams_t &streams, wchar_t **argv)
                 errno = 0;
                 nchars = fish_wcstoi(w.woptarg, &end, 10);
                 if (errno || *end != 0) {
-                    switch (errno) {
-                        case ERANGE: {
-                            streams.err.append_format(_(L"%ls: Argument '%ls' is out of range\n"),
-                                                      argv[0], w.woptarg);
-                            builtin_print_help(parser, streams, argv[0], streams.err);
-                            return STATUS_BUILTIN_ERROR;
-                        }
-                        default: {
-                            streams.err.append_format(
-                                _(L"%ls: Argument '%ls' must be an integer\n"), argv[0], w.woptarg);
-                            builtin_print_help(parser, streams, argv[0], streams.err);
-                            return STATUS_BUILTIN_ERROR;
-                        }
+                    if (errno == ERANGE) {
+                        streams.err.append_format(_(L"%ls: Argument '%ls' is out of range\n"),
+                                                  argv[0], w.woptarg);
+                        builtin_print_help(parser, streams, argv[0], streams.err);
+                        return STATUS_BUILTIN_ERROR;
+                    } else {
+                        streams.err.append_format(_(L"%ls: Argument '%ls' must be an integer\n"),
+                                                  argv[0], w.woptarg);
+                        builtin_print_help(parser, streams, argv[0], streams.err);
+                        return STATUS_BUILTIN_ERROR;
                     }
                 }
                 break;
@@ -2045,18 +2023,10 @@ static int builtin_read(parser_t &parser, io_streams_t &streams, wchar_t **argv)
                     finished = 1;
                 } else {
                     size_t sz = mbrtowc(&res, &b, 1, &state);
-                    switch (sz) {
-                        case (size_t)-1: {
-                            memset(&state, 0, sizeof(state));
-                            break;
-                        }
-                        case (size_t)-2: {
-                            break;
-                        }
-                        default: {
-                            finished = 1;
-                            break;
-                        }
+                    if (sz == (size_t)-1) {
+                        memset(&state, 0, sizeof(state));
+                    } else if (sz != (size_t)-2) {
+                        finished = 1;
                     }
                 }
             }
@@ -2310,27 +2280,22 @@ static int builtin_status(parser_t &parser, io_streams_t &streams, wchar_t **arg
 static int builtin_exit(parser_t &parser, io_streams_t &streams, wchar_t **argv) {
     int argc = builtin_count_args(argv);
 
-    long ec = 0;
-    switch (argc) {
-        case 1: {
-            ec = proc_get_last_status();
-            break;
-        }
-        case 2: {
-            wchar_t *end;
-            errno = 0;
-            ec = wcstol(argv[1], &end, 10);
-            if (errno || *end != 0) {
-                streams.err.append_format(_(L"%ls: Argument '%ls' must be an integer\n"), argv[0],
-                                          argv[1]);
-                builtin_print_help(parser, streams, argv[0], streams.err);
-                return STATUS_BUILTIN_ERROR;
-            }
-            break;
-        }
-        default: {
-            streams.err.append_format(BUILTIN_ERR_TOO_MANY_ARGUMENTS, argv[0]);
+    if (argc > 2) {
+        streams.err.append_format(BUILTIN_ERR_TOO_MANY_ARGUMENTS, argv[0]);
+        builtin_print_help(parser, streams, argv[0], streams.err);
+        return STATUS_BUILTIN_ERROR;
+    }
 
+    long ec;
+    if (argc == 1) {
+        ec = proc_get_last_status();
+    } else {
+        wchar_t *end;
+        errno = 0;
+        ec = wcstol(argv[1], &end, 10);
+        if (errno || *end != 0) {
+            streams.err.append_format(_(L"%ls: Argument '%ls' must be an integer\n"), argv[0],
+                                      argv[1]);
             builtin_print_help(parser, streams, argv[0], streams.err);
             return STATUS_BUILTIN_ERROR;
         }
@@ -2752,29 +2717,26 @@ static int builtin_breakpoint(parser_t &parser, io_streams_t &streams, wchar_t *
 /// Function for handling the \c return builtin.
 static int builtin_return(parser_t &parser, io_streams_t &streams, wchar_t **argv) {
     int argc = builtin_count_args(argv);
-    int status = proc_get_last_status();
 
-    switch (argc) {
-        case 1: {
-            break;
-        }
-        case 2: {
-            wchar_t *end;
-            errno = 0;
-            status = fish_wcstoi(argv[1], &end, 10);
-            if (errno || *end != 0) {
-                streams.err.append_format(_(L"%ls: Argument '%ls' must be an integer\n"), argv[0],
-                                          argv[1]);
-                builtin_print_help(parser, streams, argv[0], streams.err);
-                return STATUS_BUILTIN_ERROR;
-            }
-            break;
-        }
-        default: {
-            streams.err.append_format(_(L"%ls: Too many arguments\n"), argv[0]);
+    if (argc > 2) {
+        streams.err.append_format(_(L"%ls: Too many arguments\n"), argv[0]);
+        builtin_print_help(parser, streams, argv[0], streams.err);
+        return STATUS_BUILTIN_ERROR;
+    }
+
+    int status;
+    if (argc == 2) {
+        wchar_t *end;
+        errno = 0;
+        status = fish_wcstoi(argv[1], &end, 10);
+        if (errno || *end != 0) {
+            streams.err.append_format(_(L"%ls: Argument '%ls' must be an integer\n"), argv[0],
+                                      argv[1]);
             builtin_print_help(parser, streams, argv[0], streams.err);
             return STATUS_BUILTIN_ERROR;
         }
+    } else {
+        status = proc_get_last_status();
     }
 
     // Find the function block.

src/reader.cpp
@@ -1379,27 +1379,23 @@ static bool handle_completions(const std::vector<completion_t> &comp,
     const wcstring tok(begin, end - begin);
 
     // Check trivial cases.
-    switch (comp.size()) {
-        case 0: {
-            // No suitable completions found, flash screen and return.
-            reader_flash();
-            done = true;
-            success = false;
-            break;
-        }
-        case 1: {
-            // Exactly one suitable completion found - insert it.
-            const completion_t &c = comp.at(0);
+    int size = comp.size();
+    if (size == 0) {
+        // No suitable completions found, flash screen and return.
+        reader_flash();
+        done = true;
+        success = false;
+    } else if (size == 1) {
+        // Exactly one suitable completion found - insert it.
+        const completion_t &c = comp.at(0);
 
-            // If this is a replacement completion, check that we know how to replace it, e.g. that
-            // the token doesn't contain evil operators like {}.
-            if (!(c.flags & COMPLETE_REPLACES_TOKEN) || reader_can_replace(tok, c.flags)) {
-                completion_insert(c.completion.c_str(), c.flags);
-            }
-            done = true;
-            success = true;
-            break;
+        // If this is a replacement completion, check that we know how to replace it, e.g. that
+        // the token doesn't contain evil operators like {}.
+        if (!(c.flags & COMPLETE_REPLACES_TOKEN) || reader_can_replace(tok, c.flags)) {
+            completion_insert(c.completion.c_str(), c.flags);
         }
+        done = true;
+        success = true;
     }
 
     if (!done) {
@@ -1790,24 +1786,22 @@ static void handle_token_history(int forward, int reset) {
             tokenizer_t tok(data->token_history_buff.c_str(), TOK_ACCEPT_UNFINISHED);
             tok_t token;
             while (tok.next(&token)) {
-                switch (token.type) {
-                    case TOK_STRING: {
-                        if (token.text.find(data->search_buff) != wcstring::npos) {
-                            // debug( 3, L"Found token at pos %d\n", tok_get_pos( &tok ) );
-                            if (token.offset >= current_pos) {
-                                break;
-                            }
-                            // debug( 3, L"ok pos" );
-
-                            if (find(data->search_prev.begin(), data->search_prev.end(),
-                                     token.text) == data->search_prev.end()) {
-                                data->token_history_pos = token.offset;
-                                str = token.text;
-                            }
+                if (token.type == TOK_STRING) {
+                    if (token.text.find(data->search_buff) != wcstring::npos) {
+                        // debug( 3, L"Found token at pos %d\n", tok_get_pos( &tok ) );
+                        if (token.offset >= current_pos) {
+                            break;
                         }
-                        break;
+                        // debug( 3, L"ok pos" );
+
+                        if (find(data->search_prev.begin(), data->search_prev.end(), token.text) ==
+                            data->search_prev.end()) {
+                            data->token_history_pos = token.offset;
+                            str = token.text;
+                        }
                     }
-                    default: { break; }
+                } else {
+                    break;
                 }
             }
         }
@@ -2873,36 +2867,30 @@ const wchar_t *reader_readline(int nchars) {
                     data->history_search.skip_matches(skip_list);
                 }
 
-                switch (data->search_mode) {
-                    case LINE_SEARCH: {
-                        if ((c == R_HISTORY_SEARCH_BACKWARD) ||
-                            (c == R_HISTORY_TOKEN_SEARCH_BACKWARD)) {
-                            data->history_search.go_backwards();
-                        } else {
-                            if (!data->history_search.go_forwards()) {
-                                // If you try to go forwards past the end, we just go to the end.
-                                data->history_search.go_to_end();
-                            }
+                if (data->search_mode == LINE_SEARCH) {
+                    if ((c == R_HISTORY_SEARCH_BACKWARD) ||
+                        (c == R_HISTORY_TOKEN_SEARCH_BACKWARD)) {
+                        data->history_search.go_backwards();
+                    } else {
+                        if (!data->history_search.go_forwards()) {
+                            // If you try to go forwards past the end, we just go to the end.
+                            data->history_search.go_to_end();
                         }
-
-                        wcstring new_text;
-                        if (data->history_search.is_at_end()) {
-                            new_text = data->search_buff;
-                        } else {
-                            new_text = data->history_search.current_string();
-                        }
-                        set_command_line_and_position(&data->command_line, new_text,
-                                                      new_text.size());
-                        break;
                     }
-                    case TOKEN_SEARCH: {
-                        if ((c == R_HISTORY_SEARCH_BACKWARD) ||
-                            (c == R_HISTORY_TOKEN_SEARCH_BACKWARD)) {
-                            handle_token_history(SEARCH_BACKWARD, reset);
-                        } else {
-                            handle_token_history(SEARCH_FORWARD, reset);
-                        }
-                        break;
+
+                    wcstring new_text;
+                    if (data->history_search.is_at_end()) {
+                        new_text = data->search_buff;
+                    } else {
+                        new_text = data->history_search.current_string();
+                    }
+                    set_command_line_and_position(&data->command_line, new_text, new_text.size());
+                } else if (data->search_mode == TOKEN_SEARCH) {
+                    if ((c == R_HISTORY_SEARCH_BACKWARD) ||
+                        (c == R_HISTORY_TOKEN_SEARCH_BACKWARD)) {
+                        handle_token_history(SEARCH_BACKWARD, reset);
+                    } else {
+                        handle_token_history(SEARCH_FORWARD, reset);
                     }
                 }
                 break;

src/builtin_commandline.cpp
@@ -152,15 +152,11 @@ static void write_part(const wchar_t *begin, const wchar_t *end, int cut_at_curs
         while (tok.next(&token)) {
             if ((cut_at_cursor) && (token.offset + token.text.size() >= pos)) break;
 
-            switch (token.type) {
-                case TOK_STRING: {
-                    wcstring tmp = token.text;
-                    unescape_string_in_place(&tmp, UNESCAPE_INCOMPLETE);
-                    out.append(tmp);
-                    out.push_back(L'\n');
-                    break;
-                }
-                default: { break; }
+            if (token.type == TOK_STRING) {
+                wcstring tmp = token.text;
+                unescape_string_in_place(&tmp, UNESCAPE_INCOMPLETE);
+                out.append(tmp);
+                out.push_back(L'\n');
             }
         }
 
@@ -476,24 +472,18 @@ int builtin_commandline(parser_t &parser, io_streams_t &streams, wchar_t **argv)
         }
     }
 
-    switch (argc - w.woptind) {
-        case 0: {
-            write_part(begin, end, cut_at_cursor, tokenize, streams);
-            break;
+    int arg_count = argc - w.woptind;
+    if (arg_count == 0) {
+        write_part(begin, end, cut_at_cursor, tokenize, streams);
+    } else if (arg_count == 1) {
+        replace_part(begin, end, argv[w.woptind], append_mode);
+    } else {
+        wcstring sb = argv[w.woptind];
+        for (int i = w.woptind + 1; i < argc; i++) {
+            sb.push_back(L'\n');
+            sb.append(argv[i]);
         }
-        case 1: {
-            replace_part(begin, end, argv[w.woptind], append_mode);
-            break;
-        }
-        default: {
-            wcstring sb = argv[w.woptind];
-            for (int i = w.woptind + 1; i < argc; i++) {
-                sb.push_back(L'\n');
-                sb.append(argv[i]);
-            }
-            replace_part(begin, end, sb.c_str(), append_mode);
-            break;
-        }
+        replace_part(begin, end, sb.c_str(), append_mode);
     }
 
     return 0;

commit 8f78e71b6d71bd26fabbe4ab88aa44fba1f68351

fix regression introduced by 21521b2
The problem was overlooking a `break` statement when refactoring a
`switch` block into a simpler `if...else...` block. This fixes the
behavior of the `history-token-search-backward` function and its forward
searching analog.

Fixes #4065

src/builtin.cpp
@@ -560,7 +560,6 @@ static int builtin_bind(parser_t &parser, io_streams_t &streams, wchar_t **argv)
                                                   argv[0], eseq.c_str());
                     }
                 }
-                break;
             } else {
                 if (builtin_bind_add(argv[w.woptind], argv + (w.woptind + 1),
                                      argc - (w.woptind + 1), bind_mode, sets_bind_mode,

src/reader.cpp
@@ -1781,22 +1781,14 @@ static void handle_token_history(int forward, int reset) {
             tokenizer_t tok(data->token_history_buff.c_str(), TOK_ACCEPT_UNFINISHED);
             tok_t token;
             while (tok.next(&token)) {
-                if (token.type == TOK_STRING) {
-                    if (token.text.find(data->search_buff) != wcstring::npos) {
-                        // debug( 3, L"Found token at pos %d\n", tok_get_pos( &tok ) );
-                        if (token.offset >= current_pos) {
-                            break;
-                        }
-                        // debug( 3, L"ok pos" );
+                if (token.type != TOK_STRING) continue;
+                if (token.text.find(data->search_buff) == wcstring::npos) continue;
+                if (token.offset >= current_pos) continue;
 
-                        if (find(data->search_prev.begin(), data->search_prev.end(), token.text) ==
-                            data->search_prev.end()) {
-                            data->token_history_pos = token.offset;
-                            str = token.text;
-                        }
-                    }
-                } else {
-                    break;
+                auto found = find(data->search_prev.begin(), data->search_prev.end(), token.text);
+                if (found == data->search_prev.end()) {
+                    data->token_history_pos = token.offset;
+                    str = token.text;
                 }
             }
         }
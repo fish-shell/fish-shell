7e7bc873952b4c1f7755fc39bad1e8dbb522d546
David Marchal <penegal@Aethelthryth>
2017-07-10 14:10:02 +0200

    Fixed __fish_complete_users to better format GECOS

--- share/functions/__fish_complete_users.fish
+++ share/functions/__fish_complete_users.fish
@@ -2,11 +2,11 @@
 # completion. If you just want a list of user names use __fish_print_users.
 function __fish_complete_users --description "Print a list of local users, with the real user name as a description"
     if command -sq getent
-        command getent passwd | cut -d : -f 1,5 | string replace -r ':' \t
+        command getent passwd | cut -d : -f 1,5 | string replace -r ':' \t | cut -d',' -f1
     else if command -sq dscl
         # This is the "Directory Service command line utility" used on macOS in place of getent.
         command dscl . -list /Users RealName | string match -r -v '^_' | string replace -r ' {2,}' \t
     else if test -r /etc/passwd
-        string match -v -r '^\s*#' </etc/passwd | cut -d : -f 1,5 | string replace ':' \t
+        string match -v -r '^\s*#' </etc/passwd | cut -d : -f 1,5 | string replace ':' \t | cut -d',' -f1
     end
 end

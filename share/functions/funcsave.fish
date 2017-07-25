fish-shell/fish-shell

commit 86af63cd398f7b8ac3b98ffd110f1e90366ce90f

update `funcsave` to use `argparse`

share/functions/funcsave.fish
@@ -1,19 +1,12 @@
-
 function funcsave --description "Save the current definition of all specified functions to file"
+    set -l options 'h/help'
+    argparse -n funcsave --min-args=1 $options -- $argv
+    or return
 
-    if count $argv >/dev/null
-        switch $argv[1]
-            case -h --h --he --hel --help
-                __fish_print_help funcsave
-                return 0
-        end
-    else
-        printf (_ "%s: Expected function name\n") funcsave
+    if set -q _flag_help
         __fish_print_help funcsave
-        return 1
+        return 0
     end
-
-    set -l res 0
 
     set -l configdir ~/.config
     if set -q XDG_CONFIG_HOME
@@ -29,11 +22,12 @@ function funcsave --description "Save the current definition of all specified fu
         end
     end
 
-    for i in $argv
-        if functions -q -- $i
-            functions -- $i >$configdir/fish/functions/$i.fish
+    set -l res 0
+    for funcname in $argv
+        if functions -q -- $funcname
+            functions -- $i >$configdir/fish/functions/$funcname
         else
-            printf (_ "%s: Unknown function '%s'\n") funcsave $i
+            printf (_ "%s: Unknown function '%s'\n") funcsave $funcname
             set res 1
         end
     end

commit 35a7db50a29742205f671f276bfb6c9b5fc40945

fix bug introduced by commit 86af63cd3

share/functions/funcsave.fish
@@ -22,16 +22,16 @@ function funcsave --description "Save the current definition of all specified fu
         end
     end
 
-    set -l res 0
+    set -l retval 0
     for funcname in $argv
         if functions -q -- $funcname
-            functions -- $i >$configdir/fish/functions/$funcname
+            functions -- $funcname >$configdir/fish/functions/$funcname.fish
         else
             printf (_ "%s: Unknown function '%s'\n") funcsave $funcname
-            set res 1
+            set retval 1
         end
     end
 
-    return $res
+    return $retval
 end
 
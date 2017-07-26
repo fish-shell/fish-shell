f37edfc272491258c127b772fdb3a78228489743
Thales Mello <thalesmello@gmail.com>
2017-07-25 21:59:59 -0300

    Refactor npm.fish for code reutilization
    Created function to handle both cases of npm run completion parse, with or without `jq` completion.

--- share/completions/npm.fish
+++ share/completions/npm.fish
@@ -71,22 +71,22 @@ end
 complete -f -c npm -n 'not __fish_npm_needs_option; and not __fish_npm_using_command run; and not __fish_npm_using_command run-script' -a "(__fish_complete_npm)"
 
 # list available npm scripts and their parial content
+function __fish_parse_npm_run_completions
+    while read -l name
+        set -l trim 20
+        read -l value
+        set value (string sub -l $trim -- $value)
+        printf "%s\t%s\n" $name $value
+    end
+end
+
+
 function __fish_npm_run
     # Like above, only try to call npm if there's a command by that name to facilitate aliases that call nvm.
     if command -sq jq; and test -e package.json
-        jq -r '.scripts | to_entries[] | .key,.value' <package.json | while read -l name
-            set -l trim 20
-            read -l value
-            echo "$value" | cut -c1-$trim | read -l value
-            printf "%s\t%s\n" $name $value
-        end
+        jq -r '.scripts | to_entries[] | .key,.value' <package.json | __fish_parse_npm_run_completions
     else if command -sq npm
-        command npm run | string match -r -v '^[^ ]|^$' | string trim | while read -l name
-            set -l trim 20
-            read -l value
-            echo "$value" | cut -c1-$trim | read -l value
-            printf "%s\t%s\n" $name $value
-        end
+        command npm run | string match -r -v '^[^ ]|^$' | string trim | __fish_parse_npm_run_completions
     end
 end
 

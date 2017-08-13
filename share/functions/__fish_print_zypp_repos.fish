ada9993c9fea51b0b2e98a22deb48a3406f93bda
Sam Yu <ytz1995@gmail.com>
2017-08-13 23:17:26 +0800

    Replace sed with string in __fish_print_zypp_repos

--- share/functions/__fish_print_zypp_repos.fish
+++ share/functions/__fish_print_zypp_repos.fish
@@ -1,3 +1,5 @@
 function __fish_print_zypp_repos -d "Print repositories of zypper package manager"
-    sed -n '/\[.*\]/s/\[\(.*\)\]/\1/gp' /etc/zypp/repos.d/*.repo
+    for repofile in /etc/zypp/repos.d/*.repo
+        string replace -f -r '\[(.+)\]' '$1' < $repofile
+    end
 end

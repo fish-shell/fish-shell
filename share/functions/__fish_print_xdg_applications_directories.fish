2761cf1bf573667b1980d006241e8c90a3de49ce
MoritzKn <moritz.kneilmann@gmx.de>
2017-08-15 21:21:49 +0200

    Update xdg-mime helper
    This will respect the `/usr/local/share/applications/` directory when
    fetching mime infos.
    
    Update xdg-mime helper to comply with the xdg spec.
    
    This also makes sure __fish_print_xdg_applications_directories only prints
    directories that exist.
    
    Relevant specs:
    https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
    https://specifications.freedesktop.org/desktop-entry-spec/latest/ape.html

---
+++ share/functions/__fish_print_xdg_applications_directories.fish
@@ -0,0 +1,20 @@
+function __fish_print_xdg_applications_directories --description 'Print directories where desktop files are stored'
+    set -l data_home $XDG_DATA_HOME
+    if test -z "$data_home"
+        set data_home $HOME/.local/share/
+    end
+
+    set -l data_dirs $XDG_DATA_DIRS
+    if test -z "$data_dirs"
+        set data_dirs /usr/local/share/:/usr/share/
+    end
+
+    set data_dirs $data_home:$data_dirs
+
+    for path in (string split : $data_dirs)
+        set path $path"applications"
+        if test -d $path
+            echo $path
+        end
+    end
+end

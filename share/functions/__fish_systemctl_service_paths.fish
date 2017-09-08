281d468285f3d6f0a9c22afb315bd80bacdc4afd
Fabian Homborg <FHomborg@gmail.com>
2017-09-08 18:11:43 +0200

    Rewrite systemctl completion with argparse
    This
    
    - Offers more candidates
    
    - Is more reactive (it'll always incorporate "--state=" and "--type="
    
    - Is faster (about 800ms to about 120ms)
    
    - Needs fewer function files
    
    All __fish_systemctl_* functions except __fish_systemctl_services have
    been removed.

--- share/functions/__fish_systemctl_service_paths.fish
+++
@@ -1,9 +0,0 @@
-function __fish_systemctl_service_paths
-    if type -q systemctl
-        if __fish_contains_opt user
-            systemctl --user list-unit-files --no-legend --type=path ^/dev/null $argv | cut -f 1 -d ' '
-        else
-            systemctl list-unit-files --no-legend --type=path ^/dev/null $argv | cut -f 1 -d ' '
-        end
-    end
-end

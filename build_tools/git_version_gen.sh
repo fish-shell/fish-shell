861b55d7d8939d4c4560c1a2d6bcdccad2ac1509
ridiculousfish <corydoras@ridiculousfish.com>
2017-10-04 21:28:55 -0700

    Build FISH-BUILD-VERSION-FILE in CMake build
    This adds support for creating the FISH-BUILD-VERSION-FILE in the CMake
    build. A FISH-BUILD-VERSION-FILE is created in the CMake directory
    and only updated when necessary.

--- build_tools/git_version_gen.sh
+++ build_tools/git_version_gen.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # Originally from the git sources (GIT-VERSION-GEN)
 # Presumably (C) Junio C Hamano <junkio@cox.net>
 # Reused under GPL v2.0
 # Modified for fish by David Adam <zanchey@ucc.gu.uwa.edu.au>
+
+# Find the fish git directory as two levels up from this directory.
+GIT_DIR=$(dirname $(dirname "${BASH_SOURCE[0]}"))
 
 FBVF=FISH-BUILD-VERSION-FILE
 DEF_VER=unknown
@@ -12,17 +15,17 @@ DEF_VER=unknown
 if test -f version
 then
 	VN=$(cat version) || VN="$DEF_VER"
-elif ! VN=$(git describe --always --dirty 2>/dev/null); then
+elif ! VN=$(git -C "$GIT_DIR" describe --always --dirty 2>/dev/null); then
 	VN="$DEF_VER"
 fi
 
 if test -r $FBVF
 then
-	VC=$(sed -e 's/^FISH_BUILD_VERSION = //' <$FBVF)
+	VC=$(sed -e 's/^FISH_BUILD_VERSION=//' <$FBVF)
 else
 	VC=unset
 fi
 test "$VN" = "$VC" || {
-	echo >&2 "FISH_BUILD_VERSION = $VN"
-	echo "FISH_BUILD_VERSION = $VN" >$FBVF
+	echo >&2 "FISH_BUILD_VERSION=$VN"
+	echo "FISH_BUILD_VERSION=$VN" >$FBVF
 }

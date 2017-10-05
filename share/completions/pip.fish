86830acf210d7ad2e8b449562d7c63bcd607c536
Cristian Prieto <me@cprieto.com>
2017-10-05 14:44:54 +1100

    Add completions for pip2/3
     * In some systems pip is not aliased and we have pip2 and pip3
     * In those cases, we just load the completions for those commands

--- share/completions/pip.fish
+++ share/completions/pip.fish
@@ -1,3 +1,13 @@
 if command -sq pip
     eval (pip completion --fish)
 end
+
+# Some distributions prefer to have separate pip commands
+# depending on the version of Python running
+if command -sq pip2
+    eval (pip2 completion --fish)
+end
+
+if command -sq pip3
+    eval (pip3 completion --fish)
+end

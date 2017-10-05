0fb80537df0328a1829d16f28e6341e74ef583b5
Cristian Prieto <me@cprieto.com>
2017-10-05 15:04:02 +1100

    Separate pip2/3 completions in their own file
     * As requested by @floam

--- share/completions/pip.fish
+++ share/completions/pip.fish
@@ -2,12 +2,3 @@ if command -sq pip
     eval (pip completion --fish)
 end
 
-# Some distributions prefer to have separate pip commands
-# depending on the version of Python running
-if command -sq pip2
-    eval (pip2 completion --fish)
-end
-
-if command -sq pip3
-    eval (pip3 completion --fish)
-end

de45232a373f29403684d88be65465d4870d1587
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-10-05 13:26:56 +0200

    Fix copy-and-paste pip3 -> pip2 typo
    Thanks @floam. See #4448
    
    (cherry picked from commit 78fe53406eb89422ea1080ea2cbe005e1280d092)

--- share/completions/pip2.fish
+++ share/completions/pip2.fish
@@ -1,3 +1,3 @@
 if command -sq pip2
-    pip3 completion --fish | string replace -r "\b-c\s+pip\b" -- "-c pip2" | source
+    pip2 completion --fish | string replace -r "\b-c\s+pip\b" -- "-c pip2" | source
 end

020f8097f036697f7c9b32b2700ef3ac95f1e364
Aaron Gyes <me@aaron.gy>
2017-10-11 10:07:45 -0700

    uniq completions: fix spelling
    it's -> its

--- share/completions/uniq.fish
+++ share/completions/uniq.fish
@@ -13,8 +13,8 @@ if uniq --version > /dev/null ^ /dev/null
 	complete -c uniq -s w -l check-chars --description "Compare only specified number of characters" -r
 	complete -c uniq -l help --description "Display help and exit"
 	complete -c uniq -l version --description "Display version and exit"
-else # OS X
-	complete -c uniq -s c -d 'Precede each output line with count of it\'s occurrence'
+else # BSD
+	complete -c uniq -s c -d 'Precede each output line with count of its occurrence'
 	complete -c uniq -s d -d 'Only print duplicates'
 	complete -c uniq -s f -d 'Avoid comparing first N fields' -x
 	complete -c uniq -s s -d 'Avoid comparing fist N characters' -x

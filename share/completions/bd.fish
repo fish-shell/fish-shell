fa030d67d719ecc8881db5a904f3f64d47a5a630
Ron Gebauer <Mazorius@users.noreply.github.com>
2017-10-12 10:37:59 +0200

    Update bd.fish
    Remove space in-front of colon.

--- share/completions/bd.fish
+++ share/completions/bd.fish
@@ -5,8 +5,8 @@
 # https://github.com/0rax/fish-bd
 #
 
-complete -c bd -s c -d "Classic mode : goes back to the first directory named as the string"
-complete -c bd -s s -d "Seems mode : goes back to the first directory containing string"
+complete -c bd -s c -d "Classic mode: goes back to the first directory named as the string"
+complete -c bd -s s -d "Seems mode: goes back to the first directory containing string"
 complete -c bd -s i -d "Case insensitive move (implies seems mode)"
 complete -c bd -s h -x -d "Display help and exit"
 complete -c bd -A -f

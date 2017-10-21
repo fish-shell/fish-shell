a1263afc452d771b10f92f85219e203a33480d5b
MhdAli <ma.alfarra@gmail.com>
2017-10-22 06:56:17 +0800

    Updated version details

--- share/completions/exercism.fish
+++ share/completions/exercism.fish
@@ -10,7 +10,7 @@ end
 complete -c exercism -s c -l config -d "path to config file [$EXERCISM_CONFIG_FILE, $XDG_CONFIG_HOME]"
 complete -c exercism -s v -l verbose -d "turn on verbose logging"
 complete -c exercism -s h -l help -d "show help"
-complete -c exercism -s v -l version -d "turn on verbose logging"
+complete -c exercism -s v -l version -d "print the version"
 complete -f -n '__fish_exercism_no_subcommand' -c exercism -a 'configure' -d "Writes config values to a JSON file."
 complete -f -n '__fish_exercism_no_subcommand' -c exercism -a 'debug' -d "Outputs useful debug information."
 complete -f -n '__fish_exercism_no_subcommand' -c exercism -a 'download' -d "Downloads a solution given the ID of the latest iteration."

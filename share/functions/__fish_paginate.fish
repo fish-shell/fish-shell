af8266678ddf70bdb02a8330ffc413ee484521b8
Judson <nyarly@gmail.com>
2017-10-25 16:13:42 -0700

    Paginating last command
    Often, I want to paginate the command I just ran.
    
    Easy enough to <up><alt-p>, but this patch saves a keystroke.

--- share/functions/__fish_paginate.fish
+++ share/functions/__fish_paginate.fish
@@ -1,13 +1,16 @@
-function __fish_paginate -d "Paginate the current command using the users default pager"
+function __fish_paginate --description 'Paginate the current command using the users default pager'
+	set -l cmd less
+  if set -q PAGER
+    set cmd $PAGER
+  end
 
-    set -l cmd less
-    if set -q PAGER
-        set cmd $PAGER
-    end
+  if test -z (commandline -j)
+    commandline -a $history[1]
+  end
 
-    if commandline -j | string match -q -r -v "$cmd *\$"
-
-        commandline -aj " ^&1 |$cmd;"
-    end
-
+  if commandline -j | string match -q -r -v "$cmd *\$"
+    commandline -aj " ^&1 | $cmd;"
+  else
+    commandline -aj "# ^&1 | $cmd;"
+  end
 end

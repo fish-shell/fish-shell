06065f60efdfd571016c55fb59f236d6cd71ba02
Judson <nyarly@gmail.com>
2017-10-29 16:33:14 -0700

    Reducing edit noise

--- share/functions/__fish_paginate.fish
+++ share/functions/__fish_paginate.fish
@@ -1,16 +1,17 @@
-function __fish_paginate --description 'Paginate the current command using the users default pager'
-	set -l cmd less
-  if set -q PAGER
-    set cmd $PAGER
-  end
+function __fish_paginate -d "Paginate the current command using the users default pager"
 
-  if test -z (commandline -j)
-    commandline -a $history[1]
-  end
+    set -l cmd less
+    if set -q PAGER
+        set cmd $PAGER
+    end
 
-  if commandline -j | string match -q -r -v "$cmd *\$"
-    commandline -aj " ^&1 | $cmd;"
-  else
-    commandline -aj "# ^&1 | $cmd;"
-  end
+    if test -z (commandline -j)
+        commandline -a $history[1]
+    end
+
+    if commandline -j | string match -q -r -v "$cmd *\$"
+
+        commandline -aj " ^&1 | $cmd;"
+    end
+
 end

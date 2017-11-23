dcf9ce6fc5ba802876b3bdda44d3563f2ac51e9f
David Guyot <david.guyot@europecamions-interactive.com>
2017-11-23 13:55:59 +0100

    Added completions/translations for zfs and zpool

---
+++ share/functions/__fish_complete_zfs_pools.fish
@@ -0,0 +1,3 @@
+function __fish_complete_zfs_pools -d "Completes with available ZFS pools"
+	zpool list -o name,comment -H | string replace -a \t'-' ''
+end
---
+++ share/functions/__fish_print_zfs_snapshots.fish
@@ -0,0 +1,3 @@
+function __fish_print_zfs_snapshots -d "Lists ZFS snapshots"
+	zfs list -t snapshot -o name -H
+end

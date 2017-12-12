d09d45bece7de3f7a1076a896f129393562a0232
Alan Somers <asomers@gmail.com>
2017-12-12 16:21:26 -0700

    Fix the build on FreeBSD ARM and ARM64
    Downstream bug:
    https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=224254
    
    Fixes #4589

--- src/wgetopt.h
+++ src/wgetopt.h
@@ -177,7 +177,7 @@ struct woption {
     int *flag;
     /// If \c flag is non-null, this is the value that flag will be set to. Otherwise, this is the
     /// return-value of the function call.
-    int val;
+    wchar_t val;
 };
 
 // Names for the values of the `has_arg' field of `struct option'.

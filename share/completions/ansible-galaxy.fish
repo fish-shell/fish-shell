54c9d57e421950aa58b9826611553a9baccbbaa2
Benoit Hamon <benoit.hamon@epitech.eu>
2018-02-06 17:53:23 +0100

    Ansible completions (#4697)
    * :rocket:
    
    * prepare to merge into fish-shell
    
    * split into different files
    
    * remove deprecated option
    
    * captitalize descriptions
    
    * make shorter description for ansible
    
    * update ansible-playbook (and ansible for consistency)
    
    * update version on vault and galaxy

---
+++ share/completions/ansible-galaxy.fish
@@ -0,0 +1,4 @@
+complete -c ansible-galaxy -a "delete import info init install list login remove search setup"
+complete -c ansible-galaxy -s h -l help -d "Show this help message and exit"
+complete -c ansible-galaxy -s v -l verbose -d "Verbose mode (-vvv for more, -vvvv to enable connection debugging)"
+complete -c ansible-galaxy -l version -d "Display version and exit"

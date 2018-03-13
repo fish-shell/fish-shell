3ec1fab7aac7700af60630d61e529a060a504c52
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-03-13 06:12:35 -0700

    Update to handle latest Atom options (#4801)
    * Update to handle latest Atom options
    
    * Fix typo and wording

--- share/completions/atom.fish
+++ share/completions/atom.fish
@@ -1,4 +1,4 @@
-# Atom Editor v1.0.2
+# Atom Editor v1.24.1
 
 # Usage: atom [options] [path ...]
 
@@ -8,30 +8,39 @@
 # window.
 
 # Options:
-#   -1, --one                  This option is no longer supported.                           [boolean]
-#   --include-deprecated-apis  This option is not currently supported.                       [boolean]
-#   -d, --dev                  Run in development mode.                                      [boolean]
-complete -c atom -s d -l dev -d "Run in development mode."
-#   -f, --foreground           Keep the browser process in the foreground.                   [boolean]
-complete -c atom -s f -l foreground -d "Keep the browser process in the foreground."
-#   -h, --help                 Print this usage message.                                     [boolean]
-complete -c atom -s h -l help -d "Print usage message."
-#   -l, --log-file             Log all output to file.                                        [string]
-complete -c atom -s l -l log-file -r -d "Log all output to file."
-#   -n, --new-window           Open a new window.                                            [boolean]
-complete -c atom -s n -l new-window -d "Open a new window."
-#   --profile-startup          Create a profile of the startup execution time.               [boolean]
-complete -c atom -l profile-startup -d "Create a profile of the startup execution time."
-#   -r, --resource-path        Set the path to the Atom source directory and enable dev-mode. [string]
-complete -c atom -s r -l resource-path -d "Set the path to the Atom source directory and enable dev-mode."
-#   -s, --spec-directory       Set the directory from which to run package specs (default: Atom's spec
-#                              directory).                                                    [string]
-complete -c atom -s s -l spec-directory -d "Set the directory from which to run package specs (default: Atom's spec directory)."
-#   --safe                     Do not load packages from ~/.atom/packages or ~/.atom/dev/packages. [boolean]
-complete -c atom -l safe -d "Do not load packages from ~/.atom/packages or ~/.atom/dev/packages."
-#   -t, --test                 Run the specified specs and exit with error code on failures. [boolean]
-complete -c atom -s t -l test -d "Run the specified specs and exit with error code on failures."
-#   -v, --version              Print the version.                                            [boolean]
-complete -c atom -s v -l version -f -d "Print the version."
-#   -w, --wait                 Wait for window to be closed before returning.                [boolean]
-complete -c atom -s w -l wait -d "Wait for window to be closed before returning."
+#   -d, --dev                  Run in development mode.  [boolean]
+complete -c atom -s d -l dev -d 'Run in development mode'
+#   -f, --foreground           Keep the main process in the foreground.  [boolean]
+complete -c atom -s f -l foreground -d 'Keep the main process in the foreground'
+#   -h, --help                 Print this usage message.  [boolean]
+complete -c atom -s h -l help -d 'Print usage message'
+#   -l, --log-file             Log all output to file.  [string]
+complete -c atom -s l -l log-file -r -d 'Log all output to file'
+#   -n, --new-window           Open a new window.  [boolean]
+complete -c atom -s n -l new-window -d 'Open a new window'
+#   --profile-startup          Create a profile of the startup execution time.  [boolean]
+complete -c atom -l profile-startup -d 'Create a profile of the startup execution time'
+#   -r, --resource-path        Set the path to the Atom source directory and enable dev-mode.  [string]
+complete -c atom -s r -l resource-path -r -d 'Set the path to the Atom source directory and enable dev mode'
+#   --safe                     Do not load packages from ~/.atom/packages or ~/.atom/dev/packages.  [boolean]
+complete -c atom -l safe -d 'Run in safe mode'
+#   --benchmark                Open a new window that runs the specified benchmarks.  [boolean]
+complete -c atom -l benchmark -d 'Run the specified benchmarks in a new window'
+#   --benchmark--test          Run a faster version of the benchmarks in headless mode.
+complete -c atom -l benchmark-test -d 'Run a faster version of the benchmarks in headless mode'
+#   -t, --test                 Run the specified specs and exit with error code on failures.  [boolean]
+complete -c atom -s t -l test -d 'Run the specified specs and exit with error code on failures'
+#   -m, --main-process         Run the specified specs in the main process.  [boolean]
+complete -c atom -s m -l main-process -d 'Run the specified specs in the main process'
+#   --timeout                  When in test mode, waits until the specified time (in minutes) and kills the process (exit code: 130).  [string]
+complete -c atom -l timeout -r -d 'When in test mode, waits until the specified time (in minutes) and kills the process'
+#   -v, --version              Print the version information.  [boolean]
+complete -c atom -s v -l version -d 'Print the version information'
+#   -w, --wait                 Wait for window to be closed before returning.  [boolean]
+complete -c atom -s w -l wait -d 'Wait for editor to be closed before returning'
+#   --clear-window-state       Delete all Atom environment state.  [boolean]
+complete -c atom -l clear-window-state -d 'Delete all Atom environment state'
+#   --enable-electron-logging  Enable low-level logging messages from Electron.  [boolean]
+complete -c atom -l enable-electron-logging -d 'Enable low-level logging messages from Electron'
+#   -a, --add                  Open path as a new project in last used window.  [boolean]
+complete -c atom -s a -l add -d 'Open path as a new project in last used window'

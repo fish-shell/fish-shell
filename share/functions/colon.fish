007ae0b15eb0977c4c56792f4107f7191ebe2b22
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-03-14 17:41:09 -0500

    Add colon.fish for : compatibility with sh and bash
    no-op function for compatibility with sh, bash, and others.
    Often used to insert a comment into a chain of commands without having
    it eat up the remainder of the line, handy in Makefiles.

---
+++ share/functions/colon.fish
@@ -0,0 +1,5 @@
+function :
+    # no-op function for compatibility with sh, bash, and others.
+    # Often used to insert a comment into a chain of commands without having
+	# it eat up the remainder of the line, handy in Makefiles.
+end

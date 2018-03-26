3ec0f0dedb3825db2a7b6ab0bd206edf6ea59ad4
Wilke Schwiedop <drwilly@drwilly.de>
2018-03-26 19:49:02 +0200

    fix 'grep ... | sed'

--- share/functions/__fish_print_xwindows.fish
+++ share/functions/__fish_print_xwindows.fish
@@ -1,4 +1,3 @@
 function __fish_print_xwindows --description 'Print X windows'
-    xwininfo -root -children | grep '^\s\+0x' | sed '/(has no name)/d; s/^\s*\(\S\+\)\s\+"\(.\+\)":\s\+(\(.*\)).*$/\1\t\2 (\3)/'
-
+	xwininfo -root -children | sed '/^\s\+0x/!d; /(has no name)/d; s/^\s*\(\S\+\)\s\+"\(.\+\)":\s\+(\(.*\)).*$/\1\t\2 (\3)/'
 end

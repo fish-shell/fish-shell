45067042ec7e91afad9a343f1c2b73d2ba906242
Wilke Schwiedop <drwilly@drwilly.de>
2018-04-15 18:59:35 +0200

    remove sed from __fish_complete_groups.fish

--- share/functions/__fish_complete_groups.fish
+++ share/functions/__fish_complete_groups.fish
@@ -1,8 +1,8 @@
 
 function __fish_complete_groups --description "Print a list of local groups, with group members as the description"
     if command -sq getent
-        getent group | cut -d ':' -f 1,4 | sed 's/:/\t/'
+        getent group | cut -d ':' -f 1,4 | string replace : \t
     else
-        cut -d ':' -f 1,4 /etc/group | sed 's/:/\t/'
+        cut -d ':' -f 1,4 /etc/group | string replace : \t
     end
 end

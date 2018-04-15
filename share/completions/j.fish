a2cb9cceaf021f482ea87908d8477df91844c064
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-04-14 23:23:33 -0500

    Erase default autojump completions in j.fish completions
    The default completions that autojump ships with for fish are broken
    (emitting output like "1\___\#...") as they use hackes to work around
    the previous lack of `complete -k`. The history-based autojump
    completions fully replace it.

--- share/completions/j.fish
+++ share/completions/j.fish
@@ -7,4 +7,6 @@ function __history_completions --argument limit
 	history --prefix (commandline) | string replace -r \^$tokens[1]\\s\* "" | head -n$limit
 end
 
+# erase the stock autojump completions, which are no longer needed with this
+complete -c j -e
 complete -k -c j -a '(__history_completions 25)' -f

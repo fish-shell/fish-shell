376a4eca58fd0c16545548c4c4b87569521eeccc
Fabian Homborg <FHomborg@gmail.com>
2018-04-28 18:15:52 +0200

    Remove ^ redirection from sample prompts
    A bunch of these did ^/dev/null. Replace it with 2>/dev/null.
    
    [ci skip]

--- share/tools/web_config/sample_prompts/classic_vcs.fish
+++ share/tools/web_config/sample_prompts/classic_vcs.fish
@@ -12,25 +12,25 @@ function fish_prompt --description 'Write out the prompt'
 
         function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
             if status --is-interactive
-                commandline -f repaint ^/dev/null
+                commandline -f repaint 2>/dev/null
             end
         end
 
         function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
             if status --is-interactive
-                commandline -f repaint ^/dev/null
+                commandline -f repaint 2>/dev/null
             end
         end
 
         function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
             if status --is-interactive
-                commandline -f repaint ^/dev/null
+                commandline -f repaint 2>/dev/null
             end
         end
 
         function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
             if status --is-interactive
-                commandline -f repaint ^/dev/null
+                commandline -f repaint 2>/dev/null
             end
         end
 

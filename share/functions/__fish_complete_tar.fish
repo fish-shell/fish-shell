6c788edf833d3ad10e17e14aab824f7312813063
Andrew Lobos <andrew@lobos.me>
2018-04-28 11:59:53 -0400

    Remove caret redirection from tar completion

--- share/functions/__fish_complete_tar.fish
+++ share/functions/__fish_complete_tar.fish
@@ -7,7 +7,7 @@ function __fish_complete_tar -d "Peek inside of archives and list all files"
             case '-*f' '--file'
                 set -e args[1]
                 if test -f $args[1]
-                    set -l file_list (tar -atf $args[1] ^ /dev/null)
+                    set -l file_list (tar -atf $args[1] 2> /dev/null)
                     if test -n "$file_list"
                         printf (_ "%s\tArchived file\n") $file_list
                     end

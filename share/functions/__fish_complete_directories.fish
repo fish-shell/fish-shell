5f1168002edde000ece0c24cd98200dcb141a176
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-05-12 14:04:49 -0500

    Complete current commandline token with __fish_complete_directories if
    Akin to __fish_complete_suffix, __fish_complete_directories now attempts
    to complete the current commandline token if no token is explicitly
    passed in as an argument.

--- share/functions/__fish_complete_directories.fish
+++ share/functions/__fish_complete_directories.fish
@@ -1,10 +1,15 @@
 #
 # Find directories that complete $argv[1], output them as completions
-# with description $argv[2] if defined, otherwise use 'Directory'
+# with description $argv[2] if defined, otherwise use 'Directory'.
+# If no arguments are provided, attempts to complete current commandline token.
 #
 function __fish_complete_directories -d "Complete directory prefixes" --argument comp desc
     if not set -q desc[1]
         set desc (_ "Directory")
+    end
+
+    if not set -q comp[1]
+        set comp (commandline -ct)
     end
 
     set -l dirs $comp*/

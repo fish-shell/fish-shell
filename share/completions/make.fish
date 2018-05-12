84fe46e763cbad602783d7b4226a00d6c590ea19
Fabian Homborg <FHomborg@gmail.com>
2018-05-12 18:52:04 +0200

    [make completions] Reenable files if token contains "="
    Fixes #4967.

--- share/completions/make.fish
+++ share/completions/make.fish
@@ -14,10 +14,9 @@ end
 # This completion reenables file completion on
 # assignments, so e.g. 'make foo FILES=<tab>' will receive standard
 # filename completion.
-complete -c make -n 'commandline -ct | string match "*=*"'
-
-complete -x -c make -a "(__fish_complete_make_targets (commandline -c))" -d "Target"
-complete -r -c make -s f -d "Use file as makefile" -r
+complete -c make -n 'commandline -ct | string match -q "*=*"' -a "(__fish_complete_make_targets (commandline -c))" -d "Target"
+complete -f -c make -n 'commandline -ct | not string match -q "*=*"' -a "(__fish_complete_make_targets (commandline -c))" -d "Target"
+complete -c make -s f -d "Use file as makefile" -r
 complete -x -c make -s C -l directory -x -a "(__fish_complete_directories (commandline -ct))" -d "Change directory"
 complete -c make -s d -d "Debug mode"
 complete -c make -s e -d "Environment before makefile"

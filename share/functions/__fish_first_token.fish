ec145077231c99721643f2eb5cbb1099f84bad7a
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-05-20 01:21:03 -0500

    Add function to retrieve first token
    This should probably be updated to use argparse at some point?

---
+++ share/functions/__fish_first_token.fish
@@ -0,0 +1,9 @@
+# Retrieves the first non-switch argument from the command line buffer
+function __fish_first_token
+	set -l tokens (commandline -co)
+	set -e tokens[1]
+	set -l tokens (string replace -r --filter '^([^-].*)' '$1' -- $tokens)
+	if set -q tokens[1]
+		echo $tokens[1]
+	end
+end

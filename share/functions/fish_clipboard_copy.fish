240e6570f828e9239fc03be45051148facfe1003
kewl fft <kewl@alto.eu.org>
2018-06-02 21:53:02 +0100

    add xclip support for clipboard copy and paste functions

--- share/functions/fish_clipboard_copy.fish
+++ share/functions/fish_clipboard_copy.fish
@@ -5,5 +5,7 @@ function fish_clipboard_copy
         # Silence error so no error message shows up
         # if e.g. X isn't running.
         commandline | xsel --clipboard 2>/dev/null
+    else if type -q xclip
+        commandline | xclip -selection clipboard 2>/dev/null
     end
 end

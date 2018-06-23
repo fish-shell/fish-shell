9470f8a05b8184a53fceb20a3d7b6d9dfe0e47e0
Sam Yu <ytz1995@gmail.com>
2018-06-24 06:30:31 +0800

    Fix cut completion (#5069)

--- share/completions/cut.fish
+++ share/completions/cut.fish
@@ -1,11 +1,12 @@
-complete -c cut -x
 complete -c cut -s b -l bytes -x -d "Output byte range"
 complete -c cut -s c -l characters -x -d "Output character range"
 complete -c cut -s d -l delimiter -x -d "Select field delimiter"
-complete -c cut -s d -l fields -x -d "Select fields"
+complete -c cut -s f -l fields -x -d "Select fields"
 complete -c cut -s n -d "Don't split multi byte characters"
+complete -c cut -l complement -d "complement the set of selected bytes, characters or fields"
 complete -c cut -s s -l only-delimited -d "Do not print lines without delimiter"
-complete -c cut -l output-delimiter -d "Select output delimiter"
+complete -c cut -l output-delimiter -x -d "Select output delimiter"
+complete -c cut -s z -l zero-terminated -d "line delimiter is NUL, not newline"
 complete -c cut -l help -d "Display help and exit"
 complete -c cut -l version -d "Display version and exit"
 

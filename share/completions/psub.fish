375d7bcb6b71913f8baf8ee9000a799dc74b8a78
raichoo <raichoo@googlemail.com>
2018-07-22 21:26:11 +0200

    add more options to `psub` completion

--- share/completions/psub.fish
+++ share/completions/psub.fish
@@ -1,2 +1,4 @@
 complete -c psub -s h -l help -d "Display help and exit"
 complete -c psub -s f -l file -d "Communicate using a regular file, not a named pipe"
+complete -c psub -s F -l fifo -d "Communicate using a named pipe"
+complete -c psub -s s -l suffix -d "Append suffix to the filename"

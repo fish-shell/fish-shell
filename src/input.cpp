fish-shell/fish-shell

commit 444f9f87154fd3970f11a1bcb8bafee48d2afa76

Add separation of "preset" bindings
This allows for marking certain bindings as part of a preset, which allows us to

- only erase those when switching presets
- go back to the preset binding when erasing a user binding
- only show user customization if requested
- make bare bind statements in config.fish work (!!!11elf!!!)

Fixes #5191.
Fixes #3699.

src/input.cpp
@@ -142,6 +142,7 @@ wcstring describe_char(wint_t c) {
 
 /// Mappings for the current input mode.
 static std::vector<input_mapping_t> mapping_list;
+static std::vector<input_mapping_t> preset_mapping_list;
 
 /// Terminfo map list.
 static std::vector<terminfo_mapping_t> terminfo_mappings;
@@ -205,15 +206,17 @@ static bool specification_order_is_less_than(const input_mapping_t &m1, const in
 
 /// Inserts an input mapping at the correct position. We sort them in descending order by length, so
 /// that we test longer sequences first.
-static void input_mapping_insert_sorted(const input_mapping_t &new_mapping) {
+static void input_mapping_insert_sorted(const input_mapping_t &new_mapping, bool user = true) {
+    auto& ml = user ? mapping_list : preset_mapping_list;
+    
     std::vector<input_mapping_t>::iterator loc = std::lower_bound(
-        mapping_list.begin(), mapping_list.end(), new_mapping, length_is_greater_than);
-    mapping_list.insert(loc, new_mapping);
+        ml.begin(), ml.end(), new_mapping, length_is_greater_than);
+    ml.insert(loc, new_mapping);
 }
 
 /// Adds an input mapping.
 void input_mapping_add(const wchar_t *sequence, const wchar_t *const *commands, size_t commands_len,
-                       const wchar_t *mode, const wchar_t *sets_mode) {
+                       const wchar_t *mode, const wchar_t *sets_mode, bool user) {
     CHECK(sequence, );
     CHECK(commands, );
     CHECK(mode, );
@@ -226,8 +229,10 @@ void input_mapping_add(const wchar_t *sequence, const wchar_t *const *commands, 
     // Remove existing mappings with this sequence.
     const wcstring_list_t commands_vector(commands, commands + commands_len);
 
-    for (size_t i = 0; i < mapping_list.size(); i++) {
-        input_mapping_t &m = mapping_list.at(i);
+    auto& ml = user ? mapping_list : preset_mapping_list;
+    
+    for (size_t i = 0; i < ml.size(); i++) {
+        input_mapping_t &m = ml.at(i);
         if (m.seq == sequence && m.mode == mode) {
             m.commands = commands_vector;
             m.sets_mode = sets_mode;
@@ -237,12 +242,12 @@ void input_mapping_add(const wchar_t *sequence, const wchar_t *const *commands, 
 
     // Add a new mapping, using the next order.
     const input_mapping_t new_mapping = input_mapping_t(sequence, commands_vector, mode, sets_mode);
-    input_mapping_insert_sorted(new_mapping);
+    input_mapping_insert_sorted(new_mapping, user);
 }
 
 void input_mapping_add(const wchar_t *sequence, const wchar_t *command, const wchar_t *mode,
-                       const wchar_t *sets_mode) {
-    input_mapping_add(sequence, &command, 1, mode, sets_mode);
+                       const wchar_t *sets_mode, bool user) {
+    input_mapping_add(sequence, &command, 1, mode, sets_mode, user);
 }
 
 /// Handle interruptions to key reading by reaping finshed jobs and propagating the interrupt to the
@@ -267,20 +272,20 @@ void init_input() {
     init_input_terminfo();
 
     // If we have no keybindings, add a few simple defaults.
-    if (mapping_list.empty()) {
-        input_mapping_add(L"", L"self-insert");
-        input_mapping_add(L"\n", L"execute");
-        input_mapping_add(L"\r", L"execute");
-        input_mapping_add(L"\t", L"complete");
-        input_mapping_add(L"\x3", L"commandline ''");
-        input_mapping_add(L"\x4", L"exit");
-        input_mapping_add(L"\x5", L"bind");
-        input_mapping_add(L"\x7f", L"backward-delete-char");
+    if (preset_mapping_list.empty()) {
+        input_mapping_add(L"", L"self-insert", DEFAULT_BIND_MODE, DEFAULT_BIND_MODE, false);
+        input_mapping_add(L"\n", L"execute", DEFAULT_BIND_MODE, DEFAULT_BIND_MODE, false);
+        input_mapping_add(L"\r", L"execute", DEFAULT_BIND_MODE, DEFAULT_BIND_MODE, false);
+        input_mapping_add(L"\t", L"complete", DEFAULT_BIND_MODE, DEFAULT_BIND_MODE, false);
+        input_mapping_add(L"\x3", L"commandline ''", DEFAULT_BIND_MODE, DEFAULT_BIND_MODE, false);
+        input_mapping_add(L"\x4", L"exit", DEFAULT_BIND_MODE, DEFAULT_BIND_MODE, false);
+        input_mapping_add(L"\x5", L"bind", DEFAULT_BIND_MODE, DEFAULT_BIND_MODE, false);
+        input_mapping_add(L"\x7f", L"backward-delete-char", DEFAULT_BIND_MODE, DEFAULT_BIND_MODE, false);
         // Arrows - can't have functions, so *-or-search isn't available.
-        input_mapping_add(L"\x1B[A", L"up-line");
-        input_mapping_add(L"\x1B[B", L"down-line");
-        input_mapping_add(L"\x1B[C", L"forward-char");
-        input_mapping_add(L"\x1B[D", L"backward-char");
+        input_mapping_add(L"\x1B[A", L"up-line", DEFAULT_BIND_MODE, DEFAULT_BIND_MODE, false);
+        input_mapping_add(L"\x1B[B", L"down-line", DEFAULT_BIND_MODE, DEFAULT_BIND_MODE, false);
+        input_mapping_add(L"\x1B[C", L"forward-char", DEFAULT_BIND_MODE, DEFAULT_BIND_MODE, false);
+        input_mapping_add(L"\x1B[D", L"backward-char", DEFAULT_BIND_MODE, DEFAULT_BIND_MODE, false);
     }
 
     input_initialized = true;
@@ -415,21 +420,28 @@ static void input_mapping_execute_matching_or_generic(bool allow_commands) {
 
     const wcstring bind_mode = input_get_bind_mode();
 
-    for (size_t i = 0; i < mapping_list.size(); i++) {
-        const input_mapping_t &m = mapping_list.at(i);
-
-        // debug(0, L"trying mapping (%ls,%ls,%ls)\n", escape_string(m.seq.c_str(),
-        // ESCAPE_ALL).c_str(),
-        //           m.mode.c_str(), m.sets_mode.c_str());
-
+    for (auto& m : mapping_list) {
         if (m.mode != bind_mode) {
-            // debug(0, L"skipping mapping because mode %ls != %ls\n", m.mode.c_str(),
-            // input_get_bind_mode().c_str());
             continue;
         }
 
         if (m.seq.length() == 0) {
             generic = &m;
+        } else if (input_mapping_is_match(m)) {
+            input_mapping_execute(m, allow_commands);
+            return;
+        }
+    }
+
+    // HACK: This is ugly duplication.
+    for (auto& m : preset_mapping_list) {
+        if (m.mode != bind_mode) {
+            continue;
+        }
+
+        if (m.seq.length() == 0) {
+            // Only use this generic if the user list didn't have one.
+            if (!generic) generic = &m;
         } else if (input_mapping_is_match(m)) {
             input_mapping_execute(m, allow_commands);
             return;
@@ -515,10 +527,10 @@ wint_t input_readch(bool allow_commands) {
     }
 }
 
-std::vector<input_mapping_name_t> input_mapping_get_names() {
+std::vector<input_mapping_name_t> input_mapping_get_names(bool user) {
     // Sort the mappings by the user specification order, so we can return them in the same order
     // that the user specified them in.
-    std::vector<input_mapping_t> local_list = mapping_list;
+    std::vector<input_mapping_t> local_list = user ? mapping_list : preset_mapping_list;
     std::sort(local_list.begin(), local_list.end(), specification_order_is_less_than);
     std::vector<input_mapping_name_t> result;
     result.reserve(local_list.size());
@@ -530,14 +542,25 @@ std::vector<input_mapping_name_t> input_mapping_get_names() {
     return result;
 }
 
-bool input_mapping_erase(const wcstring &sequence, const wcstring &mode) {
+void input_mapping_clear(const wchar_t *mode, bool user) {
+    auto& ml = user ? mapping_list : preset_mapping_list;
+    for (std::vector<input_mapping_t>::iterator it = ml.begin(), end = ml.end();
+         it != end; ++it) {
+        if (mode == NULL || mode == it->mode) {
+            ml.erase(it);
+        }
+    }
+}
+
+bool input_mapping_erase(const wcstring &sequence, const wcstring &mode, bool user) {
     ASSERT_IS_MAIN_THREAD();
     bool result = false;
 
-    for (std::vector<input_mapping_t>::iterator it = mapping_list.begin(), end = mapping_list.end();
+    auto& ml = user ? mapping_list : preset_mapping_list;
+    for (std::vector<input_mapping_t>::iterator it = ml.begin(), end = ml.end();
          it != end; ++it) {
         if (sequence == it->seq && mode == it->mode) {
-            mapping_list.erase(it);
+            ml.erase(it);
             result = true;
             break;
         }
@@ -545,11 +568,12 @@ bool input_mapping_erase(const wcstring &sequence, const wcstring &mode) {
     return result;
 }
 
-bool input_mapping_get(const wcstring &sequence, const wcstring &mode, wcstring_list_t *out_cmds,
+bool input_mapping_get(const wcstring &sequence, const wcstring &mode, wcstring_list_t *out_cmds, bool user,
                        wcstring *out_sets_mode) {
     bool result = false;
-    for (std::vector<input_mapping_t>::const_iterator it = mapping_list.begin(),
-                                                      end = mapping_list.end();
+    auto& ml = user ? mapping_list : preset_mapping_list;
+    for (std::vector<input_mapping_t>::const_iterator it = ml.begin(),
+                                                      end = ml.end();
          it != end; ++it) {
         if (sequence == it->seq && mode == it->mode) {
             *out_cmds = it->commands;

share/functions/__fish_shared_key_bindings.fish
@@ -9,103 +9,103 @@ function __fish_shared_key_bindings -d "Bindings shared between emacs and vi mod
         return 1
     end
 
-    bind $argv \cy yank
+    bind --preset $argv \cy yank
     or return # protect against invalid $argv
-    bind $argv \ey yank-pop
+    bind --preset $argv \ey yank-pop
 
     # Left/Right arrow
-    bind $argv -k right forward-char
-    bind $argv -k left backward-char
-    bind $argv \e\[C forward-char
-    bind $argv \e\[D backward-char
+    bind --preset $argv -k right forward-char
+    bind --preset $argv -k left backward-char
+    bind --preset $argv \e\[C forward-char
+    bind --preset $argv \e\[D backward-char
     # Some terminals output these when they're in in keypad mode.
-    bind $argv \eOC forward-char
-    bind $argv \eOD backward-char
+    bind --preset $argv \eOC forward-char
+    bind --preset $argv \eOD backward-char
 
-    bind $argv -k ppage beginning-of-history
-    bind $argv -k npage end-of-history
+    bind --preset $argv -k ppage beginning-of-history
+    bind --preset $argv -k npage end-of-history
 
     # Interaction with the system clipboard.
-    bind $argv \cx fish_clipboard_copy
-    bind $argv \cv fish_clipboard_paste
+    bind --preset $argv \cx fish_clipboard_copy
+    bind --preset $argv \cv fish_clipboard_paste
 
-    bind $argv \e cancel
-    bind $argv \t complete
-    bind $argv \cs pager-toggle-search
+    bind --preset $argv \e cancel
+    bind --preset $argv \t complete
+    bind --preset $argv \cs pager-toggle-search
     # shift-tab does a tab complete followed by a search.
-    bind $argv --key btab complete-and-search
+    bind --preset $argv --key btab complete-and-search
 
-    bind $argv \e\n "commandline -i \n"
-    bind $argv \e\r "commandline -i \n"
+    bind --preset $argv \e\n "commandline -i \n"
+    bind --preset $argv \e\r "commandline -i \n"
 
-    bind $argv -k down down-or-search
-    bind $argv -k up up-or-search
-    bind $argv \e\[A up-or-search
-    bind $argv \e\[B down-or-search
-    bind $argv \eOA up-or-search
-    bind $argv \eOB down-or-search
+    bind --preset $argv -k down down-or-search
+    bind --preset $argv -k up up-or-search
+    bind --preset $argv \e\[A up-or-search
+    bind --preset $argv \e\[B down-or-search
+    bind --preset $argv \eOA up-or-search
+    bind --preset $argv \eOB down-or-search
 
     # Alt-left/Alt-right
-    bind $argv \e\eOC nextd-or-forward-word
-    bind $argv \e\eOD prevd-or-backward-word
-    bind $argv \e\e\[C nextd-or-forward-word
-    bind $argv \e\e\[D prevd-or-backward-word
-    bind $argv \eO3C nextd-or-forward-word
-    bind $argv \eO3D prevd-or-backward-word
-    bind $argv \e\[3C nextd-or-forward-word
-    bind $argv \e\[3D prevd-or-backward-word
-    bind $argv \e\[1\;3C nextd-or-forward-word
-    bind $argv \e\[1\;3D prevd-or-backward-word
-    bind $argv \e\[1\;9C nextd-or-forward-word #iTerm2
-    bind $argv \e\[1\;9D prevd-or-backward-word #iTerm2
+    bind --preset $argv \e\eOC nextd-or-forward-word
+    bind --preset $argv \e\eOD prevd-or-backward-word
+    bind --preset $argv \e\e\[C nextd-or-forward-word
+    bind --preset $argv \e\e\[D prevd-or-backward-word
+    bind --preset $argv \eO3C nextd-or-forward-word
+    bind --preset $argv \eO3D prevd-or-backward-word
+    bind --preset $argv \e\[3C nextd-or-forward-word
+    bind --preset $argv \e\[3D prevd-or-backward-word
+    bind --preset $argv \e\[1\;3C nextd-or-forward-word
+    bind --preset $argv \e\[1\;3D prevd-or-backward-word
+    bind --preset $argv \e\[1\;9C nextd-or-forward-word #iTerm2
+    bind --preset $argv \e\[1\;9D prevd-or-backward-word #iTerm2
 
     # Alt-up/Alt-down
-    bind $argv \e\eOA history-token-search-backward
-    bind $argv \e\eOB history-token-search-forward
-    bind $argv \e\e\[A history-token-search-backward
-    bind $argv \e\e\[B history-token-search-forward
-    bind $argv \eO3A history-token-search-backward
-    bind $argv \eO3B history-token-search-forward
-    bind $argv \e\[3A history-token-search-backward
-    bind $argv \e\[3B history-token-search-forward
-    bind $argv \e\[1\;3A history-token-search-backward
-    bind $argv \e\[1\;3B history-token-search-forward
-    bind $argv \e\[1\;9A history-token-search-backward # iTerm2
-    bind $argv \e\[1\;9B history-token-search-forward # iTerm2
+    bind --preset $argv \e\eOA history-token-search-backward
+    bind --preset $argv \e\eOB history-token-search-forward
+    bind --preset $argv \e\e\[A history-token-search-backward
+    bind --preset $argv \e\e\[B history-token-search-forward
+    bind --preset $argv \eO3A history-token-search-backward
+    bind --preset $argv \eO3B history-token-search-forward
+    bind --preset $argv \e\[3A history-token-search-backward
+    bind --preset $argv \e\[3B history-token-search-forward
+    bind --preset $argv \e\[1\;3A history-token-search-backward
+    bind --preset $argv \e\[1\;3B history-token-search-forward
+    bind --preset $argv \e\[1\;9A history-token-search-backward # iTerm2
+    bind --preset $argv \e\[1\;9B history-token-search-forward # iTerm2
     # Bash compatibility
     # https://github.com/fish-shell/fish-shell/issues/89
-    bind $argv \e. history-token-search-backward
+    bind --preset $argv \e. history-token-search-backward
 
-    bind $argv \el __fish_list_current_token
-    bind $argv \ew 'set tok (commandline -pt); if test $tok[1]; echo; whatis $tok[1]; commandline -f repaint; end'
+    bind --preset $argv \el __fish_list_current_token
+    bind --preset $argv \ew 'set tok (commandline -pt); if test $tok[1]; echo; whatis $tok[1]; commandline -f repaint; end'
     # ncurses > 6.0 sends a "delete scrollback" sequence along with clear.
     # This string replace removes it.
-    bind $argv \cl 'echo -n (clear | string replace \e\[3J ""); commandline -f repaint'
-    bind $argv \cc __fish_cancel_commandline
-    bind $argv \cu backward-kill-line
-    bind $argv \cw backward-kill-path-component
-    bind $argv \e\[F end-of-line
-    bind $argv \e\[H beginning-of-line
+    bind --preset $argv \cl 'echo -n (clear | string replace \e\[3J ""); commandline -f repaint'
+    bind --preset $argv \cc __fish_cancel_commandline
+    bind --preset $argv \cu backward-kill-line
+    bind --preset $argv \cw backward-kill-path-component
+    bind --preset $argv \e\[F end-of-line
+    bind --preset $argv \e\[H beginning-of-line
 
-    bind $argv \ed 'set -l cmd (commandline); if test -z "$cmd"; echo; dirh; commandline -f repaint; else; commandline -f kill-word; end'
-    bind $argv \cd delete-or-exit
+    bind --preset $argv \ed 'set -l cmd (commandline); if test -z "$cmd"; echo; dirh; commandline -f repaint; else; commandline -f kill-word; end'
+    bind --preset $argv \cd delete-or-exit
 
     # Allow reading manpages by pressing F1 (many GUI applications) or Alt+h (like in zsh).
-    bind $argv -k f1 __fish_man_page
-    bind $argv \eh __fish_man_page
+    bind --preset $argv -k f1 __fish_man_page
+    bind --preset $argv \eh __fish_man_page
 
     # This will make sure the output of the current command is paged using the default pager when
     # you press Meta-p.
     # If none is set, less will be used.
-    bind $argv \ep '__fish_paginate'
+    bind --preset $argv \ep '__fish_paginate'
 
     # Make it easy to turn an unexecuted command into a comment in the shell history. Also,
     # remove the commenting chars so the command can be further edited then executed.
-    bind $argv \e\# __fish_toggle_comment_commandline
+    bind --preset $argv \e\# __fish_toggle_comment_commandline
 
     # The [meta-e] and [meta-v] keystrokes invoke an external editor on the command buffer.
-    bind \ee edit_command_buffer
-    bind \ev edit_command_buffer
+    bind --preset $argv \ee edit_command_buffer
+    bind --preset $argv \ev edit_command_buffer
 
     # Support for "bracketed paste"
     # The way it works is that we acknowledge our support by printing
@@ -135,17 +135,17 @@ function __fish_shared_key_bindings -d "Bindings shared between emacs and vi mod
 
     # Exclude paste mode or there'll be an additional binding after switching between emacs and vi
     for mode in (bind --list-modes | string match -v paste)
-        bind -M $mode -m paste \e\[200~ '__fish_start_bracketed_paste'
+        bind --preset -M $mode -m paste \e\[200~ '__fish_start_bracketed_paste'
     end
     # This sequence ends paste-mode and returns to the previous mode we have saved before.
-    bind -M paste \e\[201~ '__fish_stop_bracketed_paste'
+    bind --preset -M paste \e\[201~ '__fish_stop_bracketed_paste'
     # In paste-mode, everything self-inserts except for the sequence to get out of it
-    bind -M paste "" self-insert
+    bind --preset -M paste "" self-insert
     # Without this, a \r will overwrite the other text, rendering it invisible - which makes the exercise kinda pointless.
     # TODO: Test this in windows (\r\n line endings)
-    bind -M paste \r "commandline -i \n"
-    bind -M paste "'" "__fish_commandline_insert_escaped \' \$__fish_paste_quoted"
-    bind -M paste \\ "__fish_commandline_insert_escaped \\\ \$__fish_paste_quoted"
+    bind --preset -M paste \r "commandline -i \n"
+    bind --preset -M paste "'" "__fish_commandline_insert_escaped \' \$__fish_paste_quoted"
+    bind --preset -M paste \\ "__fish_commandline_insert_escaped \\\ \$__fish_paste_quoted"
 end
 
 function __fish_commandline_insert_escaped --description 'Insert the first arg escaped if a second arg is given'

share/functions/fish_default_key_bindings.fish
@@ -6,7 +6,7 @@ function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fis
     end
 
     if not set -q argv[1]
-        bind --erase --all # clear earlier bindings, if any
+        bind --erase --all --preset # clear earlier bindings, if any
         if test "$fish_key_bindings" != "fish_default_key_bindings"
             # Allow the user to set the variable universally
             set -q fish_key_bindings
@@ -28,76 +28,76 @@ function fish_default_key_bindings -d "Default (Emacs-like) key bindings for fis
     or return # protect against invalid $argv
 
     # This is the default binding, i.e. the one used if no other binding matches
-    bind $argv "" self-insert
+    bind --preset $argv "" self-insert
     or exit # protect against invalid $argv
 
-    bind $argv \n execute
-    bind $argv \r execute
+    bind --preset $argv \n execute
+    bind --preset $argv \r execute
 
-    bind $argv \ck kill-line
+    bind --preset $argv \ck kill-line
 
-    bind $argv \eOC forward-char
-    bind $argv \eOD backward-char
-    bind $argv \e\[C forward-char
-    bind $argv \e\[D backward-char
-    bind $argv -k right forward-char
-    bind $argv -k left backward-char
+    bind --preset $argv \eOC forward-char
+    bind --preset $argv \eOD backward-char
+    bind --preset $argv \e\[C forward-char
+    bind --preset $argv \e\[D backward-char
+    bind --preset $argv -k right forward-char
+    bind --preset $argv -k left backward-char
 
-    bind $argv -k dc delete-char
-    bind $argv -k backspace backward-delete-char
-    bind $argv \x7f backward-delete-char
+    bind --preset $argv -k dc delete-char
+    bind --preset $argv -k backspace backward-delete-char
+    bind --preset $argv \x7f backward-delete-char
 
     # for PuTTY
     # https://github.com/fish-shell/fish-shell/issues/180
-    bind $argv \e\[1~ beginning-of-line
-    bind $argv \e\[3~ delete-char
-    bind $argv \e\[4~ end-of-line
+    bind --preset $argv \e\[1~ beginning-of-line
+    bind --preset $argv \e\[3~ delete-char
+    bind --preset $argv \e\[4~ end-of-line
 
     # OS X SnowLeopard doesn't have these keys. Don't show an annoying error message.
-    bind $argv -k home beginning-of-line 2>/dev/null
-    bind $argv -k end end-of-line 2>/dev/null
-    bind $argv \e\[3\;2~ backward-delete-char # Mavericks Terminal.app shift-ctrl-delete
+    bind --preset $argv -k home beginning-of-line 2>/dev/null
+    bind --preset $argv -k end end-of-line 2>/dev/null
+    bind --preset $argv \e\[3\;2~ backward-delete-char # Mavericks Terminal.app shift-ctrl-delete
 
-    bind $argv \ca beginning-of-line
-    bind $argv \ce end-of-line
-    bind $argv \ch backward-delete-char
-    bind $argv \cp up-or-search
-    bind $argv \cn down-or-search
-    bind $argv \cf forward-char
-    bind $argv \cb backward-char
-    bind $argv \ct transpose-chars
-    bind $argv \et transpose-words
-    bind $argv \eu upcase-word
+    bind --preset $argv \ca beginning-of-line
+    bind --preset $argv \ce end-of-line
+    bind --preset $argv \ch backward-delete-char
+    bind --preset $argv \cp up-or-search
+    bind --preset $argv \cn down-or-search
+    bind --preset $argv \cf forward-char
+    bind --preset $argv \cb backward-char
+    bind --preset $argv \ct transpose-chars
+    bind --preset $argv \et transpose-words
+    bind --preset $argv \eu upcase-word
 
     # This clashes with __fish_list_current_token
-    # bind $argv \el downcase-word
-    bind $argv \ec capitalize-word
+    # bind --preset $argv \el downcase-word
+    bind --preset $argv \ec capitalize-word
     # One of these is alt+backspace.
-    bind $argv \e\x7f backward-kill-word
-    bind $argv \e\b backward-kill-word
-    bind $argv \eb backward-word
-    bind $argv \ef forward-word
-    bind $argv \e\[1\;5C forward-word
-    bind $argv \e\[1\;5D backward-word
-    bind $argv \e\< beginning-of-buffer
-    bind $argv \e\> end-of-buffer
+    bind --preset $argv \e\x7f backward-kill-word
+    bind --preset $argv \e\b backward-kill-word
+    bind --preset $argv \eb backward-word
+    bind --preset $argv \ef forward-word
+    bind --preset $argv \e\[1\;5C forward-word
+    bind --preset $argv \e\[1\;5D backward-word
+    bind --preset $argv \e\< beginning-of-buffer
+    bind --preset $argv \e\> end-of-buffer
 
-    bind $argv \ed kill-word
+    bind --preset $argv \ed kill-word
 
     # Ignore some known-bad control sequences
     # https://github.com/fish-shell/fish-shell/issues/1917
-    bind $argv \e\[I 'begin;end'
-    bind $argv \e\[O 'begin;end'
+    bind --preset $argv \e\[I 'begin;end'
+    bind --preset $argv \e\[O 'begin;end'
 
     # term-specific special bindings
     switch "$TERM"
         case 'rxvt*'
-            bind $argv \e\[8~ end-of-line
-            bind $argv \eOc forward-word
-            bind $argv \eOd backward-word
+            bind --preset $argv \e\[8~ end-of-line
+            bind --preset $argv \eOc forward-word
+            bind --preset $argv \eOd backward-word
         case 'xterm-256color'
             # Microsoft's conemu uses xterm-256color plus
             # the following to tell a console to paste:
-            bind $argv \e\x20ep fish_clipboard_paste
+            bind --preset $argv \e\x20ep fish_clipboard_paste
     end
 end

share/functions/fish_hybrid_key_bindings.fish
@@ -1,5 +1,5 @@
 function fish_hybrid_key_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
-    bind --erase --all # clear earlier bindings, if any
+    bind --erase --all --preset # clear earlier bindings, if any
 
     if test "$fish_key_bindings" != "fish_hybrid_key_bindings"
         # Allow the user to set the variable universally

commit 05d14f24d6c18b1179b76a6f58a1530fe2f8a96f

Fix a crash in bind
444f9f87154fd3970f11a1bcb8bafee48d2afa76 introduced a bug where we would
use an iterator that had been invalidated by erase(). Fix that.

src/input.cpp
@@ -544,10 +544,11 @@ std::vector<input_mapping_name_t> input_mapping_get_names(bool user) {
 
 void input_mapping_clear(const wchar_t *mode, bool user) {
     auto& ml = user ? mapping_list : preset_mapping_list;
-    for (std::vector<input_mapping_t>::iterator it = ml.begin(), end = ml.end();
-         it != end; ++it) {
+    for (std::vector<input_mapping_t>::iterator it = ml.begin(); it != ml.end();) {
         if (mode == NULL || mode == it->mode) {
-            ml.erase(it);
+            it = ml.erase(it);
+        } else {
+            ++it;
         }
     }
 }
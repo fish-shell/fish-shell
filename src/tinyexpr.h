9a88b3e42af152587fe7483cbf56b85d96d74d2e
Fabian Homborg <FHomborg@gmail.com>
2018-10-18 17:00:29 +0200

    [tinyexpr] Port to C++
    This removes the need to run c-compilation on one file, and allows us
    to in future c++-ify this a bit.
    
    There's a lot of bit-fiddling here that is quite unnecessary, better
    error-handling would be nice...
    
    So far this removes a few more unused things (because I would have had
    to port them), including:
    
    - Functions with ARITY > 3 (even 3 isn't used, but just so we don't
    get complacent)
    
    - Variables
    
    - Most functions moved out of the header, because only te_interp is used.
    
    - The te_print function

--- src/tinyexpr.h
+++ src/tinyexpr.h
@@ -22,80 +22,29 @@
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
-// This version was altered for inclusion in fish.
+// This version was altered and ported to C++ for inclusion in fish.
 
 #ifndef __TINYEXPR_H__
 #define __TINYEXPR_H__
 
+typedef enum {
+    TE_ERROR_NONE = 0,
+    TE_ERROR_UNKNOWN_VARIABLE = 1,
+    TE_ERROR_MISSING_CLOSING_PAREN = 2,
+    TE_ERROR_MISSING_OPENING_PAREN = 3,
+    TE_ERROR_TOO_FEW_ARGS = 4,
+    TE_ERROR_TOO_MANY_ARGS = 5,
+    TE_ERROR_MISSING_OPERATOR = 6,
+    TE_ERROR_UNKNOWN = 7
+} te_error_type_t;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-
-    typedef enum {
-        TE_ERROR_NONE = 0,
-        TE_ERROR_UNKNOWN_VARIABLE = 1,
-        TE_ERROR_MISSING_CLOSING_PAREN = 2,
-        TE_ERROR_MISSING_OPENING_PAREN = 3,
-        TE_ERROR_TOO_FEW_ARGS = 4,
-        TE_ERROR_TOO_MANY_ARGS = 5,
-        TE_ERROR_MISSING_OPERATOR = 6,
-        TE_ERROR_UNKNOWN = 7
-    } te_error_type_t;
-
-
-    typedef struct te_error_t {
-        te_error_type_t type;
-        int position;
-    } te_error_t;
-
-typedef struct te_expr {
-    int type;
-    union {double value; const double *bound; const void *function;};
-    void *parameters[1];
-} te_expr;
-
-
-enum {
-    TE_VARIABLE = 0,
-
-    TE_FUNCTION0 = 8, TE_FUNCTION1, TE_FUNCTION2, TE_FUNCTION3,
-    TE_FUNCTION4, TE_FUNCTION5, TE_FUNCTION6, TE_FUNCTION7,
-
-    TE_FLAG_PURE = 32
-};
-
-typedef struct te_variable {
-    const char *name;
-    const void *address;
-    int type;
-    void *context;
-} te_variable;
-
-
+typedef struct te_error_t {
+    te_error_type_t type;
+    int position;
+} te_error_t;
 
 /* Parses the input expression, evaluates it, and frees it. */
 /* Returns NaN on error. */
 double te_interp(const char *expression, te_error_t *error);
-
-/* Parses the input expression and binds variables. */
-/* Returns NULL on error. */
-te_expr *te_compile(const char *expression, const te_variable *variables, int var_count, te_error_t *error);
-
-/* Evaluates the expression. */
-double te_eval(const te_expr *n);
-
-/* Prints debugging information on the syntax tree. */
-void te_print(const te_expr *n);
-
-/* Frees the expression. */
-/* This is safe to call on NULL pointers. */
-void te_free(te_expr *n);
-
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /*__TINYEXPR_H__*/

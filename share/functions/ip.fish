0e50fd2ecdc8f366b4e78b6c69eb3937cb294762
exploide <me@exploide.net>
2018-11-19 19:47:33 +0100

    color ip output only when writing to stdout

--- share/functions/ip.fish
+++ share/functions/ip.fish
@@ -1,9 +1,14 @@
 #
 # Enable ip colored output if this is available
+# But only when writing to stdout
 #
 
 if command ip -c l >/dev/null 2>/dev/null
     function ip
-        command ip -c $argv
+        if isatty stdout
+            command ip -c $argv
+        else
+            command ip $argv
+        end
     end
 end

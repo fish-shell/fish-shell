281c684301f9c386497f806726504ef2fa14a8c3
exploide <me@exploide.net>
2018-11-19 20:38:07 +0100

    ip function: cleanup conditionals

--- share/functions/ip.fish
+++ share/functions/ip.fish
@@ -3,14 +3,12 @@
 # But only when writing to stdout
 #
 
-if command -s ip >/dev/null
-    if command ip -c l >/dev/null 2>/dev/null
-        function ip
-            if isatty stdout
-                command ip -c $argv
-            else
-                command ip $argv
-            end
+if command -sq ip; and command ip -c l >/dev/null 2>/dev/null
+    function ip
+        if isatty stdout
+            command ip -c $argv
+        else
+            command ip $argv
         end
     end
 end

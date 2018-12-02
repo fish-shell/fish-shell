df4a41ff8190235c389af768a5bb0375c5840c6c
ridiculousfish <corydoras@ridiculousfish.com>
2018-12-02 14:53:26 -0800

    Fix a crash with -d5 and block processes

--- src/proc.h
+++ src/proc.h
@@ -203,7 +203,12 @@ class job_t {
     /// untruncated job string when we don't care what the job is, only which of the currently
     /// running jobs it is.
     wcstring preview() const {
-        return processes.empty() ? L"" : processes[0]->argv0() + wcstring(L" ...");
+        if (processes.empty())
+            return L"";
+        // Note argv0 may be empty in e.g. a block process.
+        const wchar_t *argv0 = processes.front()->argv0();
+        wcstring result = argv0 ? argv0 : L"null";
+        return result + L" ...";
     }
 
     /// All the processes in this job.

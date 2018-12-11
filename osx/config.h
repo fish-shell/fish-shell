83fbd881e76a041f816557286ae0779653852fe4
David Adam <zanchey@ucc.gu.uwa.edu.au>
2018-12-11 21:46:38 +0800

    osx/config.h: mirror changes in f2a829aa230113b6f080
    [ci skip]

--- osx/config.h
+++ osx/config.h
@@ -194,7 +194,7 @@
 #define NOMACROS 1
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "fish-users@lists.sourceforge.net"
+#define PACKAGE_BUGREPORT "https://github.com/fish-shell/fish-shell/issues"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "fish"

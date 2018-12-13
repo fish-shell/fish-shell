5f175f510193e9be8df9cfd78ebb32cc5d23f824
Leonard Hecker <leonard@hecker.io>
2018-12-13 18:51:34 +0100

    Removed superfluous whitespace

--- share/tools/web_config/sample_prompts/sorin.fish
+++ share/tools/web_config/sample_prompts/sorin.fish
@@ -23,7 +23,7 @@ end
 function fish_right_prompt
     set -l cmd_status $status
     if test $cmd_status -ne 0
-        echo -n (set_color red)"✘ $cmd_status "
+        echo -n (set_color red)"✘ $cmd_status"
     end
 
     if not command -sq git

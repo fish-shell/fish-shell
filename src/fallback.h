bc0a0b4bc84fcc4f9feeac06bc2c76993691ac34
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2019-01-02 17:25:33 -0600

    Fix `wcstod_l` infinite recursion under FreeBSD
    This was the actual issue leading to memory corruption under FreeBSD in
    issue #5453, worked around by correcting the detection of `wcstod_l` so
    that our version of the function is not called at all.
    
    If we are 100% certain that `wcstod_l` does not exist, then then the
    existing code is fine. But given that our checks have failed seperately
    on two different platforms already (FreeBSD and Cygwin/newlib), it's a
    good precaution to take.

--- src/fallback.h
+++ src/fallback.h
@@ -200,5 +200,15 @@ int flock(int fd, int op);
 #endif
 
 #ifndef HAVE_WCSTOD_L
-double wcstod_l(const wchar_t *enptr, wchar_t **endptr, locale_t loc);
+// On some platforms if this is incorrectly detected and a system-defined
+// defined version of `wcstod_l` exists, calling `wcstod` from our own
+// `wcstod_l` can call back into `wcstod_l` causing infinite recursion.
+// e.g. FreeBSD defines `wcstod(x, y)` as `wcstod_l(x, y, __get_locale())`.
+// Solution: namespace our implementation to make sure there is no symbol
+// duplication.
+#undef wcstod_l
+namespace fish_compat {
+    double wcstod_l(const wchar_t *enptr, wchar_t **endptr, locale_t loc);
+}
+#define wcstod_l(x, y, z) fish_compat::wcstod_l(x, y, z)
 #endif

a1df72dbb60f26b1ca6e24b6c3fd39e07a6ebf14
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2019-01-02 17:25:33 -0600

    Fix `wcstod_l` infinite recursion under FreeBSD
    This was the actual issue leading to memory corruption under FreeBSD in
    issue #5453, worked around by correcting the detection of `wcstod_l` so
    that our version of the function is not called at all.
    
    If we are 100% certain that `wcstod_l` does not exist, then then the
    existing code is fine. But given that our checks have failed seperately
    on two different platforms already (FreeBSD and Cygwin/newlib), it's a
    good precaution to take.

--- src/fallback.cpp
+++ src/fallback.cpp
@@ -390,9 +390,10 @@ int flock(int fd, int op) {
 #endif  // HAVE_FLOCK
 
 #ifndef HAVE_WCSTOD_L
-// musl doesn't feature wcstod_l,
-// so we just wrap wcstod.
-double wcstod_l(const wchar_t *enptr, wchar_t **endptr, locale_t loc) {
+#undef wcstod_l
+// For platforms without wcstod_l C extension, wrap wcstod after changing the
+// thread-specific locale.
+double fish_compat::wcstod_l(const wchar_t *enptr, wchar_t **endptr, locale_t loc) {
     char *saved_locale = strdup(setlocale(LC_NUMERIC, NULL));
     // Yes, this is hardcoded to use the "C" locale.
     // That's the only thing we need, and uselocale(loc) broke in my testing.

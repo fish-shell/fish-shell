fish-shell/fish-shell

commit a8eb02f9f59350f222295e16b8f68dc3b45d9ed5

Fix wcstod_l detection under FreeBSD

cmake/ConfigureChecks.cmake
@@ -72,7 +72,7 @@ CHECK_CXX_SYMBOL_EXISTS(wcsdup wchar.h HAVE_WCSDUP)
 CHECK_CXX_SYMBOL_EXISTS(wcslcpy wchar.h HAVE_WCSLCPY)
 CHECK_CXX_SYMBOL_EXISTS(wcsncasecmp wchar.h HAVE_WCSNCASECMP)
 CHECK_CXX_SYMBOL_EXISTS(wcsndup wchar.h HAVE_WCSNDUP)
-CHECK_CXX_SYMBOL_EXISTS(wcstod_l wchar.h HAVE_WCSTOD_L)
+CHECK_CXX_SYMBOL_EXISTS(wcstod_l "wchar.h;xlocale.h" HAVE_WCSTOD_L)
 
 CHECK_CXX_SYMBOL_EXISTS(_sys_errs stdlib.h HAVE__SYS__ERRS)
 

commit 380bae80bf3e3b317b4b58bb73137831daace7e2

Fix `wcstod_l` detection under Linux
This was broken in a8eb02f9f59350f222295e16b8f68dc3b45d9ed5 when the
detection was corrected for FreeBSD. This patch makes the detection work
for both Linux and FreeBSD instead of one or the other (tested).

cmake/ConfigureChecks.cmake
@@ -90,9 +90,17 @@ CHECK_CXX_SYMBOL_EXISTS(wcsncasecmp wchar.h HAVE_WCSNCASECMP)
 CHECK_CXX_SYMBOL_EXISTS(wcsndup wchar.h HAVE_WCSNDUP)
 
 CMAKE_PUSH_CHECK_STATE(RESET)
-# wcstod_l is a GNU-extension, sometimes hidden behind the following define
+# `wcstod_l` is a GNU-extension, sometimes hidden behind the following define
 LIST(APPEND CMAKE_REQUIRED_DEFINITIONS -D_GNU_SOURCE=1)
-CHECK_CXX_SYMBOL_EXISTS(wcstod_l "wchar.h;xlocale.h" HAVE_WCSTOD_L)
+# `xlocale.h` is required to find `wcstod_l` in `wchar.h` under FreeBSD, but
+# it's not present under Linux.
+SET(WCSTOD_L_INCLUDES "")
+CHECK_INCLUDE_FILES("xlocale.h" HAVE_XLOCALE_H)
+IF(HAVE_XLOCALE_H)
+    LIST(APPEND WCSTOD_L_INCLUDES "xlocale.h")
+ENDIF()
+LIST(APPEND WCSTOD_L_INCLUDES "wchar.h")
+CHECK_CXX_SYMBOL_EXISTS(wcstod_l "${WCSTOD_L_INCLUDES}" HAVE_WCSTOD_L)
 CMAKE_POP_CHECK_STATE()
 
 CHECK_CXX_SYMBOL_EXISTS(_sys_errs stdlib.h HAVE__SYS__ERRS)
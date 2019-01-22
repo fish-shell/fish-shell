fish-shell/fish-shell

commit 364c83927968c7fd8936bfb3d9f895d60886c071

Unconditionally set the tty mode in reader_readline
There was a bogus check for is_interactive_session. But if we are in
reader_readline we are necessarily interactive (even if we are not in
an interactive session, i.e. a fish script invoked some interactive
functionality).

Remove this check.

Fixes #5519

src/reader.cpp
@@ -2474,11 +2474,9 @@ const wchar_t *reader_readline(int nchars) {
     // Get the current terminal modes. These will be restored when the function returns.
     if (tcgetattr(STDIN_FILENO, &old_modes) == -1 && errno == EIO) redirect_tty_output();
     // Set the new modes.
-    if (is_interactive_session) {
-        if (tcsetattr(0, TCSANOW, &shell_modes) == -1) {
-            if (errno == EIO) redirect_tty_output();
-            wperror(L"tcsetattr");
-        }
+    if (tcsetattr(0, TCSANOW, &shell_modes) == -1) {
+        if (errno == EIO) redirect_tty_output();
+        wperror(L"tcsetattr");
     }
 
     while (!finished && !data->end_loop) {

commit 0edaf42d100053ed35051c0281f039572007d16a

Fix regression for #4178 and others introduced by 364c839
...while still keeping intact the fix for #5519.

src/reader.cpp
@@ -2492,8 +2492,16 @@ const wchar_t *reader_readline(int nchars) {
     if (tcgetattr(STDIN_FILENO, &old_modes) == -1 && errno == EIO) redirect_tty_output();
     // Set the new modes.
     if (tcsetattr(0, TCSANOW, &shell_modes) == -1) {
-        if (errno == EIO) redirect_tty_output();
-        wperror(L"tcsetattr");
+        int err = errno;
+        if (err == EIO) {
+            redirect_tty_output();
+        }
+        // This check is required to work around certain issues with fish's approach to
+        // terminal control when launching interactive processes while in non-interactive
+        // mode. See #4178 for one such example.
+        if (err != ENOTTY || is_interactive_session) {
+            wperror(L"tcsetattr");
+        }
     }
 
     while (!finished && !data->end_loop) {
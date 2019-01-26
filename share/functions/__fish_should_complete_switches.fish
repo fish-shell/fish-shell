7ad779ac00c975b3b6e00c1d1f7bc6f35d83d243
Fabian Homborg <FHomborg@gmail.com>
2019-01-26 19:11:27 +0100

    Remove __fish_can_complete_switches
    This was only used in __fish_should_complete_switches, and is a tiny
    helper function that is better integrated directly.
    
    Part of #5279.
    
    [ci skip]

--- share/functions/__fish_should_complete_switches.fish
+++ share/functions/__fish_should_complete_switches.fish
@@ -1,8 +1,10 @@
 # Returns whether we *should* complete a -s or --long argument.
 # The preference is NOT to do so, i.e. prefer subcommands over switches.
 function __fish_should_complete_switches
-	if not __fish_can_complete_switches
-		return 1
+	for arg in (commandline -ct)[-1..1]
+		if not string match -qr -- "^-\S*\$" "$arg"
+			return 1
+		end
 	end
 	if string match -qr -- "^-" (commandline -ct)[-1]
 		return 0

e0bf825bde4fbe4cd48e94c76f28a2cf8d3e15c1
ridiculousfish <corydoras@ridiculousfish.com>
2019-01-25 16:45:06 -0800

    make_pkg.sh to build with CMake instead of Xcode

--- build_tools/make_pkg.sh
+++ build_tools/make_pkg.sh
@@ -23,10 +23,11 @@ set -e
 
 PKGDIR=`mktemp -d`
 
+SRC_DIR=$PWD
 OUTPUT_PATH=${FISH_ARTEFACT_PATH:-~/fish_built}
 
-mkdir -p $PKGDIR/root $PKGDIR/intermediates $PKGDIR/dst
-xcodebuild install -scheme install_tree -configuration Release DSTROOT=$PKGDIR/root/
+mkdir -p $PKGDIR/build $PKGDIR/root $PKGDIR/intermediates $PKGDIR/dst
+( cd "$PKGDIR/build" && cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo "$SRC_DIR" && make -j 4 && env DESTDIR=$PKGDIR/root/ make install )
 pkgbuild --scripts build_tools/osx_package_scripts --root $PKGDIR/root/ --identifier 'com.ridiculousfish.fish-shell-pkg' --version "$VERSION"  $PKGDIR/intermediates/fish.pkg
 
 productbuild  --package-path $PKGDIR/intermediates --distribution build_tools/osx_distribution.xml --resources build_tools/osx_package_resources/ $OUTPUT_PATH/fish-$VERSION.pkg

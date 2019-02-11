3debfe753425f4ecf2fb697c3766a2cc21c35798
ridiculousfish <corydoras@ridiculousfish.com>
2019-02-10 18:01:37 -0800

    Break out commands into its own file

--- sphinx_doc_src/conf.py
+++ sphinx_doc_src/conf.py
@@ -104,7 +104,7 @@ html_static_path = ['_static']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-# html_sidebars = {}
+html_sidebars = { '**': ['globaltoc.html', 'localtoc.html', 'searchbox.html'] }
 
 
 # -- Options for HTMLHelp output ---------------------------------------------

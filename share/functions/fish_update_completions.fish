ddc0e68f29e6e330a3003ccabb6380454a8036c6
Fabian Homborg <FHomborg@gmail.com>
2019-02-26 09:22:23 +0100

    functions/fish_update_completions: Stop cleaning up in ~/.config
    That seems suspect.
    
    It removes files starting with "# Autogenerated", but those files
    usually do not show up in ~/.config - they're in ~/.local/share.
    
    So let's be careful and not mess with the user's config.
    
    (I'm pretty sure that the previous commit re-enabled cleanup as the
    `~` was quoted before then)
    
    [ci skip]

--- share/functions/fish_update_completions.fish
+++ share/functions/fish_update_completions.fish
@@ -1,7 +1,7 @@
 function fish_update_completions --description "Update man-page based completions"
     # Don't write .pyc files, use the manpath, clean up old completions
     # display progress.
-    set -l update_args -B $__fish_data_dir/tools/create_manpage_completions.py --manpath --cleanup-in ~/.config/fish/completions --cleanup-in ~/.config/fish/generated_completions --progress $argv
+    set -l update_args -B $__fish_data_dir/tools/create_manpage_completions.py --manpath --cleanup-in ~/.config/fish/generated_completions --progress $argv
     if command -qs python3
         python3 $update_args
     else if command -qs python2

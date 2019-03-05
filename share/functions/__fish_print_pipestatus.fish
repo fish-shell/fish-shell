a634356bcca3845e5445d7a13e963d06b28bdc74
zabereer <albertpretorius@yahoo.co.uk>
2019-03-05 18:25:16 +0000

    remove redundant comment from __fish_print_pipestatus.fish

--- share/functions/__fish_print_pipestatus.fish
+++ share/functions/__fish_print_pipestatus.fish
@@ -1,6 +1,4 @@
 function __fish_print_pipestatus --description "Print pipestatus for prompt"
-    # maybe these could be in global variables similar to __fish_color_status to allow
-    # users to set the variables to modify the braces/separator/color used
     set -l left_brace $argv[1]
     set -l right_brace $argv[2]
     set -l separator $argv[3]

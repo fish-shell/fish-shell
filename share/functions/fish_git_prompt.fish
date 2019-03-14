18d7123ff4f2c5cdc37728b446d37c83e57492bf
Fabian Homborg <FHomborg@gmail.com>
2019-03-14 20:26:40 +0100

    fish_git_prompt: Use `count` from stdin
    Removes any uses of `wc` in our codebase.

--- share/functions/fish_git_prompt.fish
+++ share/functions/fish_git_prompt.fish
@@ -533,11 +533,11 @@ function __fish_git_prompt_informative_status
     set -l x (count $stagedFiles)
     set -l invalidstate (count (string match -r "U" -- $stagedFiles))
     set -l stagedstate (math $x - $invalidstate)
-    set -l untrackedfiles (command git ls-files --others --exclude-standard | wc -l | string trim)
+    set -l untrackedfiles (command git ls-files --others --exclude-standard | count)
     set -l stashstate 0
     set -l stashfile "$argv[1]/logs/refs/stash"
     if set -q __fish_git_prompt_showstashstate; and test -e "$stashfile"
-        set stashstate (wc -l $stashfile | string match -r '\d+')
+        set stashstate (count < $stashfile)
     end
 
     set -l info

5f7f4c638f737861e4ee7894513b9e6673a3fb58
Aaron Gyes <me@aaron.gy>
2019-04-09 01:13:10 -0700

    informative.fish: stop caching_set_color, use br* directly
    No longer uses global vars to cache set_color output, this was
    from before set_color was a builtin, it is pointless now.
    
    This is also a prompt from before we had bright named colors,
    and it appears it was relying on -o red to get bright red.
    so use brred, etc.

--- share/tools/web_config/sample_prompts/informative.fish
+++ share/tools/web_config/sample_prompts/informative.fish
@@ -1,47 +1,30 @@
 # name: Informative
 # http://michal.karzynski.pl/blog/2009/11/19/my-informative-shell-prompt/
 
-
-function fish_prompt --description 'Write out the prompt'
+function fish_prompt --description 'Informative prompt'
     #Save the return status of the previous command
     set -l last_pipestatus $pipestatus
-    set stat $status
-
-    if not set -q __fish_prompt_normal
-        set -g __fish_prompt_normal (set_color normal)
-    end
-
-    if not set -q __fish_color_blue
-        set -g __fish_color_blue (set_color -o blue)
-    end
+    set -l last_status $status
 
     #Set the color for the status depending on the value
-    set __fish_color_status (set_color -o green)
     if test $stat -gt 0
-        set __fish_color_status (set_color -o red)
+        set -l status_color (set_color brred)
+    else
+        set -l status_color (set_color brgreen)
     end
 
     switch "$USER"
-
         case root toor
-
-            if not set -q __fish_prompt_cwd
-                if set -q fish_color_cwd_root
-                    set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-                else
-                    set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-                end
-            end
-
-            printf '%s@%s %s%s%s# ' $USER (prompt_hostname) "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal"
-
+            printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root && \
+                                                             set_color $fish_color_cwd_root || \
+                                                             set_color $fish_color_cwd) \
+                   (prompt_pwd) (set_color normal)
         case '*'
+            set -l pipestatus_string (__fish_print_pipestatus "[" "] " "|" (set_color yellow) \
+                                      (set_color bryellow) $last_pipestatus)
 
-            if not set -q __fish_prompt_cwd
-                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-            end
-            set -l pipestatus_string (__fish_print_pipestatus "[" "] " "|" (set_color yellow) (set_color --bold yellow) $last_pipestatus)
-            printf '[%s] %s%s@%s %s%s %s%s(%s)%s \f\r> ' (date "+%H:%M:%S") "$__fish_color_blue" $USER (prompt_hostname) "$__fish_prompt_cwd" "$PWD" "$pipestatus_string" "$__fish_color_status" "$stat" "$__fish_prompt_normal"
-
+            printf '[%s] %s%s@%s %s%s %s%s(%s)%s \f\r> ' (date "+%H:%M:%S") (set_color brblue) \
+                   $USER (prompt_hostname) (set_color $fish_color_cwd) $PWD "$pipestatus_string" \
+                   $status_color $last_status (set_color normal)
     end
 end

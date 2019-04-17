a173c079d5a9de8b956c6871e2fd9ccda04838ad
ridiculousfish <corydoras@ridiculousfish.com>
2019-04-16 22:36:11 -0700

    expand_abbreviation to always accept an environment_t
    Now that snapshotting is fixed, we don't need to get the principal
    environment stack any more.

--- src/expand.cpp
+++ src/expand.cpp
@@ -1191,12 +1191,6 @@ maybe_t<wcstring> expand_abbreviation(const wcstring &src, const environment_t &
     return none();
 }
 
-maybe_t<wcstring> expand_abbreviation(const wcstring &src) {
-    // FIXME: We use the principal environment stack because we don't know which variables need to
-    // be captured in a snapshot.
-    return expand_abbreviation(src, env_stack_t::principal());
-}
-
 std::map<wcstring, wcstring> get_abbreviations(const environment_t &vars) {
     // TODO: try to make this cheaper
     const size_t fish_abbr_len = std::wcslen(L"_fish_abbr_");

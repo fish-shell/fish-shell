1f37834e9bf99f1b2424beb8b1b0d70636c96ae6
Lior Stern <liorst4@gmail.com>
2019-04-22 22:42:02 +0300

    Don't parse ls output in share/completions/use_sample_prompt.fish

--- share/completions/use_sample_prompt.fish
+++ share/completions/use_sample_prompt.fish
@@ -1,1 +1,7 @@
-complete -f -c use_sample_prompt -a "(ls $__fish_sample_prompt_directory | cut -d '.' -f 1)"
+function __fish_list_sample_prompts --description 'list all available sample prompts'
+    for i in $__fish_sample_prompt_directory/*.fish
+        echo (basename $i | cut -d '.' -f 1)
+    end
+end
+
+complete -f -c use_sample_prompt -a "(__fish_list_sample_prompts)"

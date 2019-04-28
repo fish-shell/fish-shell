c22af0d8c7368ff0853970ccc2f430f824bd86bc
Fabian Homborg <FHomborg@gmail.com>
2019-04-28 12:07:38 +0200

    functions/fish_vi_cursor: Simplify detection
    This was quite famously rather complicated.
    
    We drop a bunch of cases - we can't handle tmux-starting-terminals
    100% accurately, so we just don't try. It should be quite rare that
    somebody starts a different terminal from tmux.
    
    We drop the `tput` since it is useless (like terminfo in general for
    feature-detection, because everyone claims to be xterm).
    
    So we just check if we are in konsole, iTerm, vte or genuine-xterm.
    
    Fixes #3696.
    
    See #3481.

--- share/functions/fish_vi_cursor.fish
+++ share/functions/fish_vi_cursor.fish
@@ -1,17 +1,5 @@
 function fish_vi_cursor -d 'Set cursor shape for different vi modes'
     # Check hard if we are in a supporting terminal.
-    #
-    # Challenges here are term-in-a-terms (emacs ansi-term does not support this, tmux does),
-    # that we can only figure out if we are in konsole/iterm/vte via exported variables,
-    # and ancient xterm versions.
-    #
-    # tmux defaults to $TERM = screen, but can do this if it is in a supporting terminal.
-    # Unfortunately, we can only detect this via the exported variables, so we miss some cases.
-    #
-    # We will also miss some cases of terminal-stacking,
-    # e.g. tmux started in suckless' st (no support) started in konsole.
-    # But since tmux in konsole seems rather common and that case so uncommon,
-    # we will just fail there (though it seems that tmux or st swallow it anyway).
 
     # If we're not interactive, there is effectively no bind mode.
     if not status is-interactive
@@ -22,56 +10,32 @@ function fish_vi_cursor -d 'Set cursor shape for different vi modes'
         return
     end
 
-    # vte-based terms set $TERM = xterm*, but only gained support relatively recently.
+    # vte-based terms set $TERM = xterm*, but only gained support in 2015.
     # From https://bugzilla.gnome.org/show_bug.cgi?id=720821, it appears it was version 0.40.0
     if set -q VTE_VERSION
         and test "$VTE_VERSION" -lt 4000 2>/dev/null
         return
     end
 
-    # Similarly, XTerm can do it since v280.
-    # Other terminals that set TERM=xterm don't set XTERM_VERSION.
+    # Similarly, genuine XTerm can do it since v280.
     if set -q XTERM_VERSION
         and not test (string replace -r "XTerm\((\d+)\)" '$1' -- "$XTERM_VERSION") -ge 280 2>/dev/null
         return
     end
 
-    # We use the `tput` here just to see if terminfo thinks we can change the cursor.
-    # We cannot use that sequence directly as it's not the correct one for konsole and iTerm,
-    # and because we may want to change the cursor even though terminfo says we can't (tmux).
-    if begin
-            not command -sq tput; or not tput Ss >/dev/null 2>/dev/null
-        end
-        # Whitelist tmux...
-        and not begin
-            set -q TMUX
-            # ...in a supporting term...
-            and begin
-                set -q KONSOLE_PROFILE_NAME
-                or set -q ITERM_PROFILE
-                or set -q VTE_VERSION # which version is already checked above
-                or set -q XTERM_VERSION
-            end
-            # .. unless an unsupporting terminal has been started in tmux inside a supporting one
-            and begin string match -q "screen*" -- $TERM
-                or string match -q "tmux*" -- $TERM
-            end
-        end
-        and not string match -q "konsole*" -- $TERM
-        or begin
-            # TERM = xterm is special because plenty of things claim to be it, but aren't fully compatible
-            # This includes old vte-terms (without $VTE_VERSION), old xterms (without $XTERM_VERSION or < 280)
-            # and maybe other stuff.
-            # This needs to be kept _at least_ as long as Ubuntu 14.04 is still a thing
-            # because that ships a gnome-terminal without support and without $VTE_VERSION.
-            string match -q 'xterm*' -- $TERM
-            and not begin set -q KONSOLE_PROFILE_NAME
-                or set -q ITERM_PROFILE
-                or set -q VTE_VERSION # which version is already checked above
-                or set -q XTERM_VERSION
-            end
-        end
-
+    # We need one of these terms.
+    # It would be lovely if we could rely on terminfo, but:
+    # - The "Ss" entry isn't a thing in macOS' old and crusty terminfo
+    # - It is set for xterm, and everyone and their dog claims to be xterm
+    #
+    # So we just don't care about $TERM _at all_ - it is useless for our purposes.
+    #
+    # Note: Previous versions also checked $TMUX, and made sure that then $TERM was screen* or tmux*.
+    # We don't care, since we *cannot* handle term-in-a-terms 100% correctly.
+    if not set -q KONSOLE_PROFILE_NAME
+        and not set -q ITERM_PROFILE
+        and not set -q VTE_VERSION # which version is already checked above
+        and not set -q XTERM_VERSION
         return
     end
 

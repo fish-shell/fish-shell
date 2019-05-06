cc205a134b7eb9858503cbffd8f110c67a83e2f3
Fabian Homborg <FHomborg@gmail.com>
2019-05-06 17:06:40 +0200

    Disable fish_title in emacs

--- share/functions/fish_title.fish
+++ share/functions/fish_title.fish
@@ -1,3 +1,6 @@
 function fish_title
-    echo (status current-command) (__fish_pwd)
+    # emacs is basically the only term that can't handle it.
+    if not set -q INSIDE_EMACS
+        echo (status current-command) (__fish_pwd)
+    end
 end

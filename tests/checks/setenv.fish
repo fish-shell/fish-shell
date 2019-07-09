cdcea0f00dfc286f8397eac8018ba526bd0f01a5
ridiculousfish <corydoras@ridiculousfish.com>
2019-07-09 10:02:50 -0700

    Port setenv tests to littlecheck

--- tests/checks/setenv.fish
+++ tests/checks/setenv.fish
@@ -1,3 +1,5 @@
+# RUN: %fish %s
+
 # Verify the correct behavior of the `setenv` compatibility shim.
 
 # No args to `setenv` should emit the current set of env vars. The first two
@@ -6,15 +8,21 @@ set -g setenv1 abc
 setenv | grep '^setenv1=$'
 set -gx setenv1 xyz
 setenv | grep '^setenv1=xyz$'
+# CHECK: setenv1=xyz
 
 # A single arg should set and export the named var to nothing.
 setenv setenv2
 env | grep '^setenv2=$'
+# CHECK: setenv2=
 
 # Three or more args should be an error.
 echo too many arguments test >&2
 setenv var hello you
+# CHECKERR: too many arguments test
+# CHECKERR: setenv: Too many arguments
+
 
 # Two args should set the named var to the second arg
 setenv setenv3 'hello you'
 setenv | grep '^setenv3=hello you'
+# CHECK: setenv3=hello you

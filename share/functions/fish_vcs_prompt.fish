47e9e8ab8ab7bbe8855cc3b7dda926bd84922beb
Fabian Homborg <FHomborg@gmail.com>
2019-07-15 17:31:35 +0200

    fish_vcs_prompt: Stop at first success
    Closes #5980.

--- share/functions/fish_vcs_prompt.fish
+++ share/functions/fish_vcs_prompt.fish
@@ -1,5 +1,7 @@
 function fish_vcs_prompt --description "Print the prompts for all available vcsen"
+    # If a prompt succeeded, we assume that it's printed the correct info.
+    # This is so we don't try svn if git already worked.
     fish_git_prompt
-    fish_hg_prompt
-    fish_svn_prompt
+    or fish_hg_prompt
+    or fish_svn_prompt
 end

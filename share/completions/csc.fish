67cf50eda430ee53a97ebbad119aecd4934edd4f
Shun Sakai <sorairolake@protonmail.ch>
2019-07-31 10:09:20 +0900

    Added completions of languages developed by Microsoft
    * csc, csi, vbc - .NET Compiler Platform (Roslyn)
    * fsharpc, fsharpi - F# compiler and REPL environment
    * tsc - TypeScript compiler

---
+++ share/completions/csc.fish
@@ -0,0 +1,130 @@
+# Completions for the Visual C# Compiler(Roslyn)
+# See: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-options/listed-by-category
+
+# Optimization
+for bytes in 512 1024 2048 4096 8192
+    complete -c csc -o "filealign:$bytes" -d "Specifies the size of sections in the output file"
+end
+
+complete -c csc -s o -o "o+" -o optimize -o "optimize+" -d "Enable optimizations"
+complete -c csc -o "o-" -o "optimize-" -d "Disable optimizations"
+
+# Output Files
+complete -c csc -o deterministic -d "Causes the compiler to output an assembly whose binary content is identical across compilations if inputs are identical"
+complete -c csc -o "doc:" -d "Specifies an XML file where processed documentation comments are to be written"
+complete -c csc -o "out:" -d "Specifies the output file"
+complete -c csc -o "pathmap:" -d "Specify a mapping for source path names output by the compiler"
+complete -c csc -o "pdb:" -d "Specifies the file name and location of the .pdb file"
+complete -c csc -o "platform:anycpu" -d "Specify any platform as the output platform (default)"
+complete -c csc -o "platform:anycpu32bitpreferred" -d "Specify any platform as the output platform"
+complete -c csc -o "platform:ARM" -d "Specify ARM as the output platform"
+complete -c csc -o "platform:ARM64" -d "Specify ARM64 as the output platform"
+complete -c csc -o "platform:x64" -d "Specify AMD64 or EM64T as the output platform"
+complete -c csc -o "platform:x86" -d "Specify x86 as the output platform"
+complete -c csc -o "platform:Itanium" -d "Specify Itanium as the output platform"
+complete -c csc -o "preferreduilang:" -d "Specify a language for compiler output"
+complete -c csc -o "refout:" -d "Generate a reference assembly in addition to the primary assembly"
+complete -c csc -o refonly -d "Generate a reference assembly instead of a primary assembly"
+complete -c csc -o "t:appcontainerexe" -o "target:appcontainerexe" -d "Specify .exe file for Windows 8.x Store apps as the format of the output file"
+complete -c csc -o "t:exe" -o "target:exe" -d "Specify .exe file as the format of the output file (default)"
+complete -c csc -o "t:library" -o "target:library" -d "Specify code library as the format of the output file"
+complete -c csc -o "t:module" -o "target:module" -d "Specify module as the format of the output file"
+complete -c csc -o "t:winexe" -o "target:winexe" -d "Specify Windows program as the format of the output file"
+complete -c csc -o "t:winmdobj" -o "target:winmdobj" -d "Specify intermediate .winmdobj file as the format of the output file"
+complete -c csc -o "modulename:" -d "Specify the name of the source module"
+
+# .NET Framework Assemblies
+complete -c csc -o "addmodule:" -d "Specifies one or more modules to be part of this assembly"
+complete -c csc -o delaysign -o "delaysign+" -d "Instructs the compiler to add the public key but to leave the assembly unsigned"
+complete -c csc -o "delaysign-" -d "Disable -delaysign"
+complete -c csc -o "keycontainer:" -d "Specifies the name of the cryptographic key container"
+complete -c csc -o "keyfile:" -d "Specifies the filename containing the cryptographic key"
+complete -c csc -o "lib:" -d "Specifies the location of assemblies referenced by means of -reference"
+complete -c csc -o nostdlib -o "nostdlib+" -d "Instructs the compiler not to import the standard library (mscorlib.dll)"
+complete -c csc -o "nostdlib-" -d "Disable -nostdlib"
+complete -c csc -o publicsign -d "Apply a public key without signing the assembly, but set the bit in the assembly indicating the assembly is signed"
+complete -c csc -o "r:" -o "reference:" -d "Imports metadata from a file that contains an assembly"
+complete -c csc -o "a:" -o "analyzer:" -d "Run the analyzers from this assembly"
+complete -c csc -o "additionalfile:" -d "Names additional files that don't directly affect code generation but may be used by analyzers for producing errors or warnings"
+complete -c csc -o embed -d "Embed all source files in the PDB"
+complete -c csc -o "embed:" -d "Embed specific files in the PDB"
+
+# Debugging/Error Checking
+complete -c csc -o "bugreport:" -d "Creates a file that contains information that makes it easy to report a bug"
+complete -c csc -o checked -o "checked+" -d "Specifies whether integer arithmetic that overflows the bounds of the data type will cause an exception at run time"
+complete -c csc -o "checked-" -d "Disable -checked"
+
+complete -c csc -o debug -o "debug+" -d "Instruct the compiler to emit debugging information"
+for arguments in full pdbonly
+    complete -c csc -o "debug:$arguments" -d "Instruct the compiler to emit debugging information"
+end
+complete -c csc -o "debug-" -d "Disable -debug"
+
+for arguments in none prompt queue send
+    complete -c csc -o "errorreport:$arguments" -d "Sets error reporting behavior"
+end
+
+complete -c csc -o fullpaths -d "Specifies the absolute path to the file in compiler output"
+complete -c csc -o "nowarn:" -d "Suppresses the compiler's generation of specified warnings"
+
+for warning_level in (seq 0 4)
+    if test $warning_level -ne 4
+        complete -c csc -o "w:$warning_level" -o "warn:$warning_level" -d "Sets the warning level to $warning_level"
+    else
+        complete -c csc -o "w:$warning_level" -o "warn:$warning_level" -d "Sets the warning level to $warning_level (default)"
+    end
+end
+
+complete -c csc -o warnaserror -o "warnaserror+" -d "Promotes warnings to errors"
+complete -c csc -o "warnaserror-" -d "Disable -warnaserror"
+complete -c csc -o "ruleset:" -d "Specify a ruleset file that disables specific diagnostics"
+
+# Preprocessor
+complete -c csc -o "d:" -o "define:" -d "Defines preprocessor symbols"
+
+# Resources
+complete -c csc -o "l:" -o "link:" -d "Makes COM type information in specified assemblies available to the project"
+complete -c csc -o "linkres:" -o "linkresource:" -d "Creates a link to a managed resource"
+complete -c csc -o "res:" -o "resource:" -d "Embeds a .NET Framework resource into the output file"
+complete -c csc -o "win32icon:" -d "Specifies an .ico file to insert into the output file"
+complete -c csc -o "win32res:" -d "Specifies a Win32 resource to insert into the output file"
+
+# Miscellaneous
+complete -c csc -s "?" -o help -d "Lists compiler options to stdout"
+complete -c csc -o "baseaddress:" -d "Specifies the preferred base address at which to load a DLL"
+complete -c csc -o "codepage:" -d "Specifies the code page to use for all source code files in the compilation"
+complete -c csc -o highentropyva -o "highentropyva+" -d "Specifies that the executable file supports address space layout randomization (ASLR)"
+complete -c csc -o "highentropyva-" -d "Disable -highentropyva"
+
+complete -c csc -o "langversion:?" -d "Display the allowed values for language version"
+complete -c csc -o "langversion:default" -d "Specify latest major version as language version"
+complete -c csc -o "langversion:latest" -d "Specify latest version (including minor releases) as language version"
+complete -c csc -o "langversion:latestMajor" -d "Specify latest major version as language version"
+complete -c csc -o "langversion:preview" -d "Specify latest preview version as language version"
+complete -c csc -o "langversion:ISO-1" -d "Specify ISO/IEC 23270:2003 C# (1.0/1.2) as language version"
+complete -c csc -o "langversion:ISO-2" -d "Specify ISO/IEC 23270:2006 C# (2.0) as language version"
+for version_number in (seq 3 8)
+    switch $version_number
+        case 7
+            complete -c csc -o "langversion:$version_number" -d "Specify C# $version_number.0 as language version"
+            complete -c csc -o "langversion:$version_number.1" -d "Specify C# $version_number.1 as language version"
+            complete -c csc -o "langversion:$version_number.2" -d "Specify C# $version_number.2 as language version"
+            complete -c csc -o "langversion:$version_number.3" -d "Specify C# $version_number.3 as language version"
+        case 8
+            complete -c csc -o "langversion:$version_number.0" -d "Specify C# $version_number.0 as language version"
+        case "*"
+            complete -c csc -o "langversion:$version_number" -d "Specify C# $version_number.0 as language version"
+    end
+end
+
+complete -c csc -o "m:" -o "main:" -d "Specifies the location of the Main method"
+complete -c csc -o noconfig -d "Instructs the compiler not to compile with csc.rsp"
+complete -c csc -o nologo -d "Suppresses compiler banner information"
+complete -c csc -o "recurse:" -d "Searches subdirectories for source files to compile"
+complete -c csc -o "subsystemversion:" -d "Specifies the minimum version of the subsystem that the executable file can use"
+complete -c csc -o unsafe -d "Enables compilation of code that uses the unsafe keyword"
+complete -c csc -o utf8output -d "Displays compiler output using UTF-8 encoding"
+complete -c csc -o parallel -o "parallel+" -d "Specifies whether to use concurrent build"
+complete -c csc -o "parallel-" -d "Disable -parallel"
+complete -c csc -o "checksumalgorithm:SHA1" -d "Specify SHA1 as the algorithm for calculating the source file checksum stored in PDB (default)"
+complete -c csc -o "checksumalgorithm:SHA256" -d "Specify SHA256 as the algorithm for calculating the source file checksum stored in PDB"
---
+++ share/completions/tsc.fish
@@ -0,0 +1,120 @@
+# Completions for the TypeScript compiler
+# See: https://www.typescriptlang.org/docs/handbook/compiler-options.html
+
+complete -c tsc -l allowJs -d "Allow JavaScript files to be compiled"
+complete -c tsc -l allowSyntheticDefaultImports -d "Allow default imports from modules with no default export"
+complete -c tsc -l allowUmdGlobalAccess -d "Allow accessing UMD globals from modules"
+complete -c tsc -l allowUnreachableCode -d "Do not report errors on unreachable code"
+complete -c tsc -l allowUnusedLabels -d "Do not report errors on unused labels"
+complete -c tsc -l alwaysStrict -d "Parse in strict mode and emit \"use strict\" for each source file"
+complete -c tsc -l baseUrl -d "Base directory to resolve non-relative module names"
+complete -c tsc -s b -l build -d "Builds this project and all of its dependencies specified by Project References"
+complete -c tsc -l charset -d "The character set of the input files"
+complete -c tsc -l checkJs -d "Report errors in .js files"
+complete -c tsc -l composite -d "Ensure TypeScript can determine where to find the outputs of the referenced project to compile project"
+complete -c tsc -s d -l declaration -d "Generates corresponding .d.ts file"
+complete -c tsc -l declarationDir -d "Output directory for generated declaration files"
+complete -c tsc -l declarationMap -d "Generates a sourcemap for each corresponding ‘.d.ts’ file"
+complete -c tsc -l diagnostics -d "Show diagnostic information"
+complete -c tsc -l disableSizeLimit -d "Disable size limitation on JavaScript project"
+complete -c tsc -l downlevelIteration -d "Provide full support for iterables in for..of, spread and destructuring when targeting ES5 or ES3"
+complete -c tsc -l emitBOM -d "Emit a UTF-8 Byte Order Mark (BOM) in the beginning of output files"
+complete -c tsc -l emitDeclarationOnly -d "Only emit ‘.d.ts’ declaration files"
+complete -c tsc -l emitDecoratorMetadata -d "Emit design-type metadata for decorated declarations in source"
+complete -c tsc -l esModuleInterop -d "Emit __importStar and __importDefault helpers for runtime babel ecosystem compatibility and enable --allowSyntheticDefaultImports for typesystem compatibility"
+complete -c tsc -l experimentalDecorators -d "Enables experimental support for ES decorators"
+complete -c tsc -l extendedDiagnostics -d "Show verbose diagnostic information"
+complete -c tsc -l forceConsistentCasingInFileNames -d "Disallow inconsistently-cased references to the same file"
+complete -c tsc -s h -l help -d "Print help message"
+complete -c tsc -l importHelpers -d "Import emit helpers (e.g. __extends, __rest, etc..) from tslib"
+complete -c tsc -l incremental -d "Enable incremental compilation by reading/writing information from prior compilations to a file on disk"
+complete -c tsc -l inlineSourceMap -d "Emit a single file with source maps instead of having a separate file"
+complete -c tsc -l inlineSources -d "Emit the source alongside the sourcemaps within a single file"
+complete -c tsc -l init -d "Initializes a TypeScript project and creates a tsconfig.json file"
+complete -c tsc -l isolatedModules -d "Transpile each file as a separate module (similar to “ts.transpileModule”)"
+complete -c tsc -l jsx -d "Support JSX in .tsx files: \"react\", \"preserve\", \"react-native\""
+complete -c tsc -l jsxFactory -d "Specify the JSX factory function to use when targeting react JSX emit, e.g. React.createElement or h"
+complete -c tsc -l keyofStringsOnly -d "Resolve keyof to string valued property names only (no numbers or symbols)"
+complete -c tsc -l lib -x -a "ES5 ES6 ES2015 ES7 ES2016 ES2017 ES2018 ESNext DOM DOM.Iterable WebWorker ScriptHost ES2015.Core ES2015.Collection ES2015.Generator ES2015.Iterable ES2015.Promise ES2015.Proxy ES2015.Reflect ES2015.Symbol ES2015.Symbol.WellKnown ES2016.Array.Include ES2017.object ES2017.Intl ES2017.SharedMemory ES2017.String ES2017.TypedArrays ES2018.Intl ES2018.Promise ES2018.RegExp ESNext.AsyncIterable ESNext.Array ESNext.Intl ESNext.Symbol" -d "List of library files to be included in the compilation"
+complete -c tsc -l listEmittedFiles -d "Print names of generated files part of the compilation"
+complete -c tsc -l listFiles -d "Print names of files part of the compilation"
+
+complete -c tsc -l locale -x -a "(
+    printf '%s\t%s\n' en 'English (US)'
+    printf '%s\t%s\n' cs Czech
+    printf '%s\t%s\n' de German
+    printf '%s\t%s\n' es Spanish
+    printf '%s\t%s\n' fr French
+    printf '%s\t%s\n' it Italian
+    printf '%s\t%s\n' ja Japanese
+    printf '%s\t%s\n' ko Korean
+    printf '%s\t%s\n' pl Polish
+    printf '%s\t%s\n' pt-BR 'Portuguese(Brazil)'
+    printf '%s\t%s\n' ru Russian
+    printf '%s\t%s\n' tr Turkish
+    printf '%s\t%s\n' zh-CN 'Simplified Chinese'
+    printf '%s\t%s\n' zh-TW 'Traditional Chinese'
+)" -d "The locale to use to show error messages, e.g. en-us"
+
+complete -c tsc -l mapRoot -d "Specifies the location where debugger should locate map files instead of generated locations"
+complete -c tsc -l maxNodeModuleJsDepth -d "The maximum dependency depth to search under node_modules and load JavaScript files"
+complete -c tsc -s m -l module -x -a "None CommonJS AMD System UMD ES6 ES2015 ESNext" -d "Specify module code generation"
+complete -c tsc -l moduleResolution -x -a "Node Classic" -d "Determine how modules get resolved"
+
+complete -c tsc -l newLine -x -a "(
+    printf '%s\t%s\n' crlf windows
+    printf '%s\t%s\n' lf unix
+)" -d "Use the specified end of line sequence to be used when emitting files"
+
+complete -c tsc -l noEmit -d "Do not emit outputs"
+complete -c tsc -l noEmitHelpers -d "Do not generate custom helper functions like __extends in compiled output"
+complete -c tsc -l noEmitOnError -d "Do not emit outputs if any errors were reported"
+complete -c tsc -l noErrorTruncation -d "Do not truncate error messages"
+complete -c tsc -l noFallthroughCasesInSwitch -d "Report errors for fallthrough cases in switch statement"
+complete -c tsc -l noImplicitAny -d "Raise error on expressions and declarations with an implied any type"
+complete -c tsc -l noImplicitReturns -d "Report an error when not all code paths in function return a value"
+complete -c tsc -l noImplicitThis -d "Raise error on this expressions with an implied any type"
+complete -c tsc -l noImplicitUseStrict -d "Do not emit \"use strict\" directives in module output"
+complete -c tsc -l noLib -d "Do not include the default library file (lib.d.ts)"
+complete -c tsc -l noResolve -d "Do not add triple-slash references or module import targets to the list of compiled files"
+complete -c tsc -l noStrictGenericChecks -d "Disable strict checking of generic signatures in function types"
+complete -c tsc -l noUnusedLocals -d "Report errors on unused locals"
+complete -c tsc -l noUnusedParameters -d "Report errors on unused parameters"
+complete -c tsc -l outDir -d "Redirect output structure to the directory"
+complete -c tsc -l outFile -d "Concatenate and emit output to single file"
+complete -c tsc -l preserveConstEnums -d "Do not erase const enum declarations in generated code"
+complete -c tsc -l preserveSymlinks -d "Do not resolve symlinks to their real path; treat a symlinked file like a real one"
+complete -c tsc -l preserveWatchOutput -d "Keep outdated console output in watch mode instead of clearing the screen"
+complete -c tsc -l pretty -d "Stylize errors and messages using color and context"
+complete -c tsc -s p -l project -d "Compile a project given a valid configuration file"
+complete -c tsc -l removeComments -d "Remove all comments except copy-right header comments beginning with /*!"
+complete -c tsc -l resolveJsonModule -d "Include modules imported with .json extension"
+complete -c tsc -l rootDir -d "Specifies the root directory of input files"
+complete -c tsc -l showConfig -d "Rather than actually execute a build with the other input options and config files, show the final implied config file in the output"
+complete -c tsc -l skipLibCheck -d "Skip type checking of all declaration files (*.d.ts)"
+complete -c tsc -l sourceMap -d "Generates corresponding .map file"
+complete -c tsc -l sourceRoot -d "Specifies the location where debugger should locate TypeScript files instead of source locations"
+complete -c tsc -l strict -d "Enable all strict type checking options"
+complete -c tsc -l strictBindCallApply -d "Enable stricter checking of the bind, call, and apply methods on functions"
+complete -c tsc -l strictFunctionTypes -d "Disable bivariant parameter checking for function types"
+complete -c tsc -l strictPropertyInitialization -d "Ensure non-undefined class properties are initialized in the constructor"
+complete -c tsc -l strictNullChecks -d "In strict null checking mode, the null and undefined values are not in the domain of every type and are only assignable to themselves and any (the one exception being that undefined is also assignable to void)"
+complete -c tsc -l suppressExcessPropertyErrors -d "Suppress excess property checks for object literals"
+complete -c tsc -l suppressImplicitAnyIndexErrors -d "Suppress --noImplicitAny errors for indexing objects lacking index signatures"
+
+complete -c tsc -s t -l target -x -a "(
+    printf '%s\t%s\n' ES3 'ECMAScript 3 (default)'
+    printf '%s\t%s\n' ES5 'ECMAScript 5'
+    printf '%s\t%s\n' ES6 'ECMAScript 6'
+    printf '%s\t%s\n' ES2015 'ECMAScript 2015'
+    printf '%s\t%s\n' ES2016 'ECMAScript 2016'
+    printf '%s\t%s\n' ES2017 'ECMAScript 2017'
+    printf '%s\t%s\n' ESNext 'Latest supported ECMAScript'
+)" -d "Specify ECMAScript target version"
+
+complete -c tsc -l traceResolution -d "Report module resolution log messages"
+complete -c tsc -l tsBuildInfoFile -d "Specify what file to store incremental build information in"
+complete -c tsc -l types -d "List of names of type definitions to include"
+complete -c tsc -l typeRoots -d "List of folders to include type definitions from"
+complete -c tsc -s v -l version -d "Print the compiler’s version"
+complete -c tsc -s w -l watch -d "Run the compiler in watch mode"

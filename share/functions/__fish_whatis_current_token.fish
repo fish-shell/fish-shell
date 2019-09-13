25afba768bdd936850d1937e12eea8a52f190ec1
Greg Anders <greg@gpanders.com>
2019-09-13 10:59:10 -0600

    Make variable local and remove unneeded if statement

--- share/functions/__fish_whatis_current_token.fish
+++ share/functions/__fish_whatis_current_token.fish
@@ -1,17 +1,15 @@
 # This function is typically bound to Alt-W, it is used to list man page entries
 # for the command under the cursor.
 function __fish_whatis_current_token -d "Show man page entries related to the token under the cursor"
-    set tok (commandline -pt)
+    set -l tok (commandline -pt)
 
     if test $tok[1]
         printf "\n"
         whatis $tok[1]
 
         set -l line_count (count (fish_prompt))
-        if test $line_count -gt 1
-            for x in (seq 2 $line_count)
-                printf "\n"
-            end
+        for x in (seq 2 $line_count)
+            printf "\n"
         end
 
         commandline -f repaint

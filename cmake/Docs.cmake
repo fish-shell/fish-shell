b52d3d641eb3c9845065236a60c03ad65d470292
Johannes Altmanninger <aclopte@gmail.com>
2019-10-20 08:15:58 +0200

    Fix clean build of sphinx-docs

--- cmake/Docs.cmake
+++ cmake/Docs.cmake
@@ -16,7 +16,8 @@ SET(SPHINX_MANPAGE_DIR "${SPHINX_ROOT_DIR}/man")
 # sphinx-docs uses fish_indent for highlighting.
 # Prepend the output dir of fish_indent to PATH.
 ADD_CUSTOM_TARGET(sphinx-docs
-    ${CMAKE_COMMAND} -E copy_if_different ${SPHINX_SRC_DIR}/_static/pygments.css ${SPHINX_HTML_DIR}/_static/
+    mkdir -p ${SPHINX_HTML_DIR}/_static/
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${SPHINX_SRC_DIR}/_static/pygments.css ${SPHINX_HTML_DIR}/_static/
     COMMAND ${CMAKE_COMMAND} -E copy_if_different ${SPHINX_SRC_DIR}/_static/custom.css ${SPHINX_HTML_DIR}/_static/
     COMMAND env PATH="$<TARGET_FILE_DIR:fish_indent>:$$PATH"
         ${SPHINX_EXECUTABLE}

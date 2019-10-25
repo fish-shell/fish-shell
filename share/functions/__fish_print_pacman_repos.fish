3658dc9adebb5241eaa15b7d9a84b8eba3341de2
Akatsuki <3736910+akiirui@users.noreply.github.com>
2019-10-25 17:05:39 +0800

    functions/__fish_print_pacman_repos.fish: fix This function return wrong data. Fix this.

--- share/functions/__fish_print_pacman_repos.fish
+++ share/functions/__fish_print_pacman_repos.fish
@@ -1,3 +1,3 @@
 function __fish_print_pacman_repos --description "Print the repositories configured for arch's pacman package manager"
-    string replace -r -a "\[(.+)\]" "\1" </etc/pacman.conf | string match -r -v "^#|options"
+    string match -er "\[.*\]" </etc/pacman.conf | string match -r -v "^#|options" | string replace -ra "\[|\]" ""
 end

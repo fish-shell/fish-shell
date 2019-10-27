56b4763c67c4aea415bcd5cc5721483f0a71055e
Johannes Altmanninger <aclopte@gmail.com>
2019-10-27 08:07:54 +0100

    Fix file completion for builtins "set" and "test -f" and others

--- share/functions/__fish_config_interactive.fish
+++ share/functions/__fish_config_interactive.fish
@@ -167,7 +167,7 @@ function __fish_config_interactive -d "Initializations that should be performed 
     #
     # Only a few builtins take filenames; initialize the rest with no file completions
     #
-    complete -c(builtin -n | string match -rv 'source|cd|exec|realpath') --no-files
+    complete -c(builtin -n | string match -rv 'source|cd|exec|realpath|set|\[|test') --no-files
 
     # Reload key bindings when binding variable change
     function __fish_reload_key_bindings -d "Reload key bindings when binding variable change" --on-variable fish_key_bindings

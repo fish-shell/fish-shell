9a3fb3e265a738eb8804e9d95c83a350df77b1da
Fabian Homborg <FHomborg@gmail.com>
2019-11-27 16:21:11 +0100

    Use POSIX sh in the benchmark driver
    There are reasons to use bash, `hash` is not one of them, as `command
    -v` is perfectly capable and in POSIX.
    
    [ci skip]

--- benchmarks/driver.sh
+++ benchmarks/driver.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: driver.sh /path/to/fish"
@@ -10,7 +10,7 @@ BENCHMARKS_DIR=$(dirname "$0")/benchmarks
 for benchmark in "$BENCHMARKS_DIR"/*; do
     echo $(basename "$benchmark")
     ${FISH_PATH} --print-rusage-self $benchmark > /dev/null
-    if hash hyperfine 2>/dev/null; then
+    if command -v hyperfine >/dev/null 2>&1; then
         hyperfine "${FISH_PATH} $benchmark > /dev/null"
     fi
 done

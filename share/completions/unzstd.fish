8d56609734b6bd1505d7ce1e37bdf76bf8caa731
Shun Sakai <sorairolake@protonmail.ch>
2019-11-29 07:18:33 +0900

    Add zstd completions
    Squashed commit of the following:
    
    commit 55c8e73faff2418161404f012440acced09580e4
    Author: Shun Sakai <sorairolake@protonmail.ch>
    Date:   Fri Nov 29 05:25:02 2019 +0900
    
        Implementation of `zstdless` completions
    
    commit e81ae6f83fb9f23bdd6038fbf29ea594da098c2c
    Author: Shun Sakai <sorairolake@protonmail.ch>
    Date:   Fri Nov 29 05:20:33 2019 +0900
    
        Implementation of `zstdgrep` completions
    
    commit 2ade3a9c01bdc5b024f785c369fcb6c3e007cb19
    Author: Shun Sakai <sorairolake@protonmail.ch>
    Date:   Fri Nov 29 04:50:42 2019 +0900
    
        Implementation of `pzstd` completions
    
    commit 0b9cb86a6936f3de18db7b2c012da6efec89c8e8
    Author: Shun Sakai <sorairolake@protonmail.ch>
    Date:   Fri Nov 29 04:39:52 2019 +0900
    
        Implementation of `zstdcat` completions
    
    commit e4a4101d18a82fb06ee451560d079b9b81af5f3e
    Author: Shun Sakai <sorairolake@protonmail.ch>
    Date:   Fri Nov 29 04:25:05 2019 +0900
    
        Implementation of `unzstd` completions
    
    commit 88ad0af46d486a92ab3989c55abb3ff477e5a372
    Author: Shun Sakai <sorairolake@protonmail.ch>
    Date:   Fri Nov 29 04:18:38 2019 +0900
    
        Implementation of `zstdmt` completions
    
    commit 13f2bf7951501031c61a5c0f143b8a29eaa9437e
    Author: Shun Sakai <sorairolake@protonmail.ch>
    Date:   Fri Nov 29 01:13:52 2019 +0900
    
        Implementation of `zstd` completions

---
+++ share/completions/unzstd.fish
@@ -0,0 +1,35 @@
+# Completions for unzstd
+
+complete -c unzstd -x -a "
+(
+    __fish_complete_suffix .zst
+)
+"
+
+complete -c unzstd -s t -l test -d "Test the integrity"
+complete -r -c unzstd -l train -d "Create a dictionary from specified file(s)"
+complete -c unzstd -s l -l list -d "List information about .zst file(s)"
+complete -c unzstd -l long -d "Enable long distance matching with specified windowLog"
+complete -c unzstd -l single-thread -d "Single-thread mode"
+complete -r -c unzstd -s D -d "Use specified file as dictionary"
+complete -r -c unzstd -s o -d "Specify file to save"
+complete -c unzstd -s f -l force -d "Overwrite without prompting"
+complete -c unzstd -s c -l stdout -d "Force write to stdout"
+complete -c unzstd -l sparse -d "Enable sparse mode"
+complete -c unzstd -l no-sparse -d "Disable sparse mode"
+complete -c unzstd -l rm -d "Remove input file(s) after decompression"
+complete -c unzstd -s k -l keep -d "Keep input file(s) (default)"
+complete -c unzstd -s r -d "Recurse directories"
+complete -c unzstd -l filelist -d "Read a list of files"
+complete -c unzstd -l output-dir-flat -d "Specify a directory to output all files"
+
+for format in zstd gzip xz lzma lz4
+    complete -c unzstd -l format="$format" -d "Specify the format to use for decompression"
+end
+
+complete -c unzstd -s h -l help -d "Show help"
+complete -c unzstd -s H -d "Show long help"
+complete -c unzstd -s V -l version -d "Show version"
+complete -c unzstd -s v -l verbose -d "Be verbose"
+complete -c unzstd -s q -l quiet -d "Suppress warnings"
+complete -c unzstd -l no-progress -d "Do not show the progress bar"
---
+++ share/completions/zstdcat.fish
@@ -0,0 +1,28 @@
+# Completions for zstdcat
+
+complete -c zstdcat -x -a "
+(
+    __fish_complete_suffix .zst
+)
+"
+
+complete -c zstdcat -s t -l test -d "Test the integrity"
+complete -c zstdcat -s l -l list -d "List information about .zst file(s)"
+complete -c zstdcat -l long -d "Enable long distance matching with specified windowLog"
+complete -c zstdcat -l single-thread -d "Single-thread mode"
+complete -r -c zstdcat -s D -d "Use specified file as dictionary"
+complete -c zstdcat -l sparse -d "Enable sparse mode"
+complete -c zstdcat -l no-sparse -d "Disable sparse mode"
+complete -c zstdcat -s r -d "Recurse directories"
+complete -c zstdcat -l filelist -d "Read a list of files"
+
+for format in zstd gzip xz lzma lz4
+    complete -c zstdcat -l format="$format" -d "Specify the format to use for decompression"
+end
+
+complete -c zstdcat -s h -l help -d "Show help"
+complete -c zstdcat -s H -d "Show long help"
+complete -c zstdcat -s V -l version -d "Show version"
+complete -c zstdcat -s v -l verbose -d "Be verbose"
+complete -c zstdcat -s q -l quiet -d "Suppress warnings"
+complete -c zstdcat -l no-progress -d "Do not show the progress bar"

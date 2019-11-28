bc2634eaafb5b2ee2684d3f0a50eda482bb3a55d
Shun Sakai <sorairolake@protonmail.ch>
2019-11-29 07:14:30 +0900

    Add lz4 completions
    Squashed commit of the following:
    
    commit 08ea083bf87a613675789937d7f6740daa26fc61
    Author: Shun Sakai <sorairolake@protonmail.ch>
    Date:   Thu Nov 28 23:51:35 2019 +0900
    
        Implementation of `lz4cat` completions
    
    commit 027adedfeb7bb65ffd46e44b4266df3d98368326
    Author: Shun Sakai <sorairolake@protonmail.ch>
    Date:   Thu Nov 28 23:36:12 2019 +0900
    
        Implementation of `unlz4` completions
    
    commit 522925450a8076d4a0d3377cd9233abc643bbbf7
    Author: Shun Sakai <sorairolake@protonmail.ch>
    Date:   Thu Nov 28 23:19:45 2019 +0900
    
        Implementation of `lz4c` completions
    
    commit 298ce5e05e3be2cac774063ed2ee8289ba53cf24
    Author: Shun Sakai <sorairolake@protonmail.ch>
    Date:   Thu Nov 28 23:02:52 2019 +0900
    
        Implementation of `lz4` completions

---
+++ share/completions/unlz4.fish
@@ -0,0 +1,23 @@
+# Completions for unlz4
+
+complete -c unlz4 -x -a "
+(
+    __fish_complete_suffix .lz4
+)
+"
+
+complete -c unlz4 -s t -l test -d "Test the integrity"
+complete -c unlz4 -l list -d "List information about .lz4 file(s)"
+complete -r -c unlz4 -s D -d "Use specified file as dictionary"
+complete -c unlz4 -s f -l force -d "Overwrite without prompting"
+complete -c unlz4 -s c -l stdout -l to-stdout -d "Force write to stdout"
+complete -c unlz4 -s m -l multiple -d "Multiple input files"
+complete -c unlz4 -l sparse -d "Enable sparse mode"
+complete -c unlz4 -l no-sparse -d "Disable sparse mode"
+complete -c unlz4 -s v -l verbose -d "Be verbose"
+complete -c unlz4 -s q -l quiet -d "Suppress warnings"
+complete -c unlz4 -s h -l help -d "Show help"
+complete -c unlz4 -s H -d "Show long help"
+complete -c unlz4 -s V -l version -d "Show version"
+complete -c unlz4 -s k -l keep -d "Keep input file(s) (default)"
+complete -c unlz4 -l rm -d "Remove input file(s) after decompression"

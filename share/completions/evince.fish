c36d802dda7c9797fbf8bbd3dce1e413ccb20bb5
mk2 <mk2@mk2.red>
2019-11-29 21:57:11 +0900

    Move __fish_complete_file_url to evince.fish

--- share/completions/evince.fish
+++ share/completions/evince.fish
@@ -1,4 +1,24 @@
 
+function __fish_complete_file_url
+    set -l comp
+
+    if set -q argv[1]
+        set comp $argv[1]
+    else
+        set comp (commandline -ct)
+    end
+
+    set -l prefix (echo $comp|cut -c 1-7)
+
+    if test file:// = $prefix
+        set -l stripped (echo $comp|cut -c 8-)
+        printf "%s\n" file://(complete -C"echo $stripped")
+    else
+        echo file://
+    end
+
+end
+
 complete -c evince -a '(__fish_complete_file_url)'
 complete -c evince -s p -l page-label -d "The page of the document to display" -x
 complete -c evince -s f -l fullscreen -d "Run evince in fullscreen mode"

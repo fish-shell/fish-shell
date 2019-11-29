eb67f461ec124809cd8b35ee6789efd43b532e97
Fabian Homborg <FHomborg@gmail.com>
2019-11-29 20:09:02 +0100

    alias: Use `read --tokenize`
    This did some weird unescaping to try to extract the first word.
    
    So we're now more likely to be *correct*, and the alias benchmark is
    about 20% *faster*.
    
    Call it a win-win.

--- tests/checks/alias.fish
+++ tests/checks/alias.fish
@@ -10,9 +10,11 @@ my_alias
 
 alias a-2='echo "hello there"'
 
+alias foo '"a b" c d e'
 # Bare `alias` should list the aliases we have created and nothing else
 # We have to exclude two aliases because they're an artifact of the unit test
 # framework and we can't predict the definition.
 alias | grep -Ev '^alias (fish_indent|fish_key_reader) '
 # CHECK: alias a-2 'echo "hello there"'
+# CHECK: alias foo '"a b" c d e'
 # CHECK: alias my_alias 'foo; and echo foo ran'

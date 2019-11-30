c397242f48d09cb15974bdc939788215687372c2
Jason <jason@kayoway.com>
2019-11-30 21:00:40 +0900

    Move __fish_complete_tar into the completion script

--- share/completions/tar.fish
+++ share/completions/tar.fish
@@ -1,5 +1,23 @@
+function __fish_complete_tar -d "Peek inside of archives and list all files"
+    set -l args (commandline -poc)
+    while count $args >/dev/null
+        switch $args[1]
+            case '-*f' '--file'
+                set -e args[1]
+                if test -f $args[1]
+                    set -l file_list (tar -atf $args[1] 2> /dev/null)
+                    if test -n "$file_list"
+                        printf (_ "%s\tArchived file\n") $file_list
+                    end
+                    return
+                end
+            case '*'
+                set -e args[1]
+                continue
+        end
+    end
+end
 
-# Peek inside of archives and list all files
 complete -c tar -a "(__fish_complete_tar)"
 
 complete -c tar -s A -l catenate -d "Append archive to archive"

b212d376388c71a9f888d6cd183ef85e6220071c
Jason <jason@kayoway.com>
2019-11-30 20:49:24 +0900

    Remove deprecated function

--- share/functions/fish_vi_mode.fish
+++
@@ -1,6 +0,0 @@
-function fish_vi_mode
-    echo 'The `fish_vi_mode` function is deprecated.' >&2
-    echo 'Please switch to calling `fish_vi_key_bindings`.' >&2
-    # Turn on vi keybindings
-    set -g fish_key_bindings fish_vi_key_bindings
-end

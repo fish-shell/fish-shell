1fdb23637a56ff4687b49301e5061ffdd8f3b22f
Fabian Homborg <FHomborg@gmail.com>
2019-12-03 21:10:09 +0100

    Clean up Classic + VCS prompt
    - Don't use a guard uvar - we're only setting variables now, and
    - that's basically free.
    - Allow non-universal color variables
    - Simplify the root color setting a bit.
    - Some comments
    
    [ci skip]

--- share/tools/web_config/sample_prompts/classic_vcs.fish
+++ share/tools/web_config/sample_prompts/classic_vcs.fish
@@ -1,39 +1,31 @@
 # name: Classic + Vcs
 # author: Lily Ballard
-# vim: set noet:
 
 function fish_prompt --description 'Write out the prompt'
     set -l last_pipestatus $pipestatus
     set -l last_status $status
     set -l normal (set_color normal)
 
-    # initialize our new variables
-    if not set -q __fish_classic_git_prompt_initialized
-        set -qU fish_color_user
-        or set -U fish_color_user -o green
-        set -qU fish_color_host
-        or set -U fish_color_host -o cyan
-        set -qU fish_color_status
-        or set -U fish_color_status red
-        set -U __fish_classic_git_prompt_initialized
-    end
+    # Initialize our variables.
+    set -q fish_color_user
+    or set -U fish_color_user -o green
+    set -q fish_color_host
+    or set -U fish_color_host -o cyan
+    set -q fish_color_status
+    or set -U fish_color_status red
 
-    set -l color_cwd
+    # Color the prompt differently when we're root
+    set -l color_cwd $fish_color_cwd
     set -l prefix
-    set -l suffix
-    switch "$USER"
-        case root toor
+    set -l suffix '>'
+    if contains -- $USER root toor
             if set -q fish_color_cwd_root
                 set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
             end
             set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix '>'
     end
 
+    # Write pipestatus
     set -l prompt_status (__fish_print_pipestatus "[" "] " "|" (set_color yellow) (set_color --bold yellow) $last_pipestatus)
     if test $last_status -ne 0
         set prompt_status " $prompt_status" (set_color $fish_color_status) "[$last_status]" "$normal"

b6cf848c4ab628b7eaca4993e32ed8ad9b71bf8c
Jason <jason@kayoway.com>
2019-12-03 18:46:29 +0900

    Move __fish_portage_print_repository_names into emaint completion script

--- share/completions/emaint.fish
+++ share/completions/emaint.fish
@@ -1,3 +1,8 @@
+function __fish_portage_print_repository_names --description 'Print the names of all configured repositories'
+    # repos.conf may be a file or a directory
+    find /etc/portage/repos.conf -type f -exec cat '{}' + | string replace -r --filter '^\s*\[([[:alnum:]_][[:alnum:]_-]*)\]' '$1' | string match -v -e DEFAULT
+end
+
 ## Global Opts
 complete -c emaint -s h -l help -d "Show this help message and exit"
 complete -c emaint -s c -l check -d "Check for problems"

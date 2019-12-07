b2332e9f3e2f27d9313d3c2c29afe4a8503d44dd
Fabian Homborg <FHomborg@gmail.com>
2019-12-07 19:14:02 +0100

    Switch default prompt to Classic+VCS
    This has more info, in particular vcs and status.
    
    The old default is still available as the "classic" prompt.
    
    Fixes #6375.

--- share/functions/fish_prompt.fish
+++ share/functions/fish_prompt.fish
@@ -1,22 +1,35 @@
-# vim: set noet:
-#
-# Set the default prompt command.
+# name: Classic + Vcs
+# author: Lily Ballard
 
-function fish_prompt --description "Write out the prompt"
-    set -l color_cwd
-    set -l suffix
-    switch "$USER"
-        case root toor
+function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -l last_status $status
+    set -l normal (set_color normal)
+
+    # Initialize our variables.
+    set -q fish_color_user
+    or set -U fish_color_user -o green
+    set -q fish_color_host
+    or set -U fish_color_host -o cyan
+    set -q fish_color_status
+    or set -U fish_color_status red
+
+    # Color the prompt differently when we're root
+    set -l color_cwd $fish_color_cwd
+    set -l prefix
+    set -l suffix '>'
+    if contains -- $USER root toor
             if set -q fish_color_cwd_root
                 set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
             end
             set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix '>'
     end
 
-    echo -n -s "$USER" @ (prompt_hostname) ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
+    # Write pipestatus
+    set -l prompt_status (__fish_print_pipestatus "[" "] " "|" (set_color yellow) (set_color --bold yellow) $last_pipestatus)
+    if test $last_status -ne 0
+        set prompt_status " $prompt_status" (set_color $fish_color_status) "[$last_status]" "$normal"
+    end
+
+    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal $prompt_status $suffix " "
 end

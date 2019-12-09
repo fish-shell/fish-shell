efb72f1f9182174f79849c9d28fcd0bc229abf51
Akatsuki <3736910+akiirui@users.noreply.github.com>
2019-12-10 04:01:08 +0800

    Revert "Move __fish_systemd_machines into machinectl completion script"
    This reverts commit 9c15b5b7a4008488f1a0e4e786db42a6cea397ca.

---
+++ share/functions/__fish_systemd_machines.fish
@@ -0,0 +1,6 @@
+# It seems machinectl will eliminate spaces from machine names so we don't need to handle that
+function __fish_systemd_machines
+    machinectl --no-legend --no-pager list --all | while read -l a b
+        echo $a
+    end
+end

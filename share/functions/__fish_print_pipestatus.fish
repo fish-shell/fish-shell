cc7ae030708307c1e74bc4a637525d3d43b40f66
Johannes Altmanninger <aclopte@gmail.com>
2019-12-11 09:46:40 +0100

    prompt: don't consider SIGPIPE a failure
    [ci skip]

--- share/functions/__fish_print_pipestatus.fish
+++ share/functions/__fish_print_pipestatus.fish
@@ -7,7 +7,8 @@ function __fish_print_pipestatus --description "Print pipestatus for prompt"
     set -e argv[1 2 3 4 5]
 
     # only output status codes if some process in the pipe failed
-    if string match -qvr '^0$' $argv
+    # SIGPIPE (141 = 128 + 13) is usually not a failure, see #6375.
+    if string match -qvr '^(0|141)$' $argv
         set -l sep (set_color normal){$brace_sep_color}{$separator}(set_color normal){$status_color}
         set -l last_pipestatus_string (string join "$sep" (__fish_pipestatus_with_signal $argv))
         printf "%s%s%s%s%s%s%s%s%s%s" (set_color normal )$brace_sep_color $left_brace \

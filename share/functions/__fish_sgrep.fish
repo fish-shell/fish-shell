48bf3a4907cb86cd83c58f02e3334a8ea565daa9
Jason <ammgws@users.noreply.github.com>
2019-12-16 22:23:45 +0900

    Delete __fish_sgrep.fish

--- share/functions/__fish_sgrep.fish
+++
@@ -1,5 +0,0 @@
-# This function is deprecated. Do not introduce new uses. Use the `string` command instead.
-function __fish_sgrep -d "Call grep without honoring GREP_OPTIONS settings"
-    set -l GREP_OPTIONS
-    command grep $argv
-end

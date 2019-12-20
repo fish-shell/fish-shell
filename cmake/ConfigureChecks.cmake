780bac671f45ab2c5ef15c9e7d6dff0031673e28
David Adam <zanchey@ucc.gu.uwa.edu.au>
2019-12-20 23:00:06 +0800

    cmake: add -latomic on platforms that need it for 64-bit atomic operations
    Closes #5865.

--- cmake/ConfigureChecks.cmake
+++ cmake/ConfigureChecks.cmake
@@ -187,3 +187,16 @@ int main () {
 )
 
 FIND_PROGRAM(SED sed)
+
+# https://github.com/fish-shell/fish-shell/issues/5865
+CHECK_CXX_SOURCE_COMPILES("
+#include <atomic>
+struct big { int foo[64]; };
+std::atomic<big> x;
+int main() {
+   return x.load().foo[13];
+}"
+LIBATOMIC_NOT_NEEDED)
+IF (NOT LIBATOMIC_NOT_NEEDED)
+    SET(ATOMIC_LIBRARY "atomic")
+ENDIF()

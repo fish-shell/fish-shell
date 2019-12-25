aba5beaeb45b53dbdcbc7b6fb52e5651e5befb93
Fabian Homborg <FHomborg@gmail.com>
2019-12-25 16:16:08 +0100

    completions/pipenv: Force it to print fish completions
    This checks $SHELL to determine which completions to print, and $SHELL
    is typically set by your login program.
    
    So if the login shell isn't fish, this will print the wrong
    completions.
    
    Fixes #6454
    
    [ci skip]

--- share/completions/pipenv.fish
+++ share/completions/pipenv.fish
@@ -1,3 +1,4 @@
 if command -sq pipenv
-    pipenv --completion 2>/dev/null | source
+    # pipenv determines which completions to print via $SHELL, so override it for this.
+    SHELL=(status fish-path) pipenv --completion 2>/dev/null | source
 end

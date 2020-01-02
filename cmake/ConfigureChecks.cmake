fish-shell/fish-shell

commit 8ca936aea644c91b5ed913fb5eb036627748bd1f

cmake: use C++ extension options when checking features
This matches the CMake default of extensions turned on.

cmake/ConfigureChecks.cmake
@@ -183,7 +183,7 @@ CMAKE_POP_CHECK_STATE()
 # standards flag.
 # Also see https://github.com/fish-shell/fish-shell/issues/5865
 IF(NOT POLICY CMP0067)
-  LIST(APPEND CMAKE_REQUIRED_FLAGS "${CMAKE_CXX${CMAKE_CXX_STANDARD}_STANDARD_COMPILE_OPTION}")
+  LIST(APPEND CMAKE_REQUIRED_FLAGS "${CMAKE_CXX${CMAKE_CXX_EXTENSION}_STANDARD_COMPILE_OPTION}")
 ENDIF()
 
 CHECK_CXX_SOURCE_COMPILES("

commit 5df84c1998a59bc9f8a678ee54b6757e5400f566

cmake: use correct variable for C++ compiler flags
Broken in 8ca936aea644c91b5ed913fb5eb036627748bd1f

cmake/ConfigureChecks.cmake
@@ -183,7 +183,7 @@ CMAKE_POP_CHECK_STATE()
 # standards flag.
 # Also see https://github.com/fish-shell/fish-shell/issues/5865
 IF(NOT POLICY CMP0067)
-  LIST(APPEND CMAKE_REQUIRED_FLAGS "${CMAKE_CXX${CMAKE_CXX_EXTENSION}_STANDARD_COMPILE_OPTION}")
+  LIST(APPEND CMAKE_REQUIRED_FLAGS "${CMAKE_CXX${CMAKE_CXX_STANDARD}_EXTENSION_COMPILE_OPTION}")
 ENDIF()
 
 CHECK_CXX_SOURCE_COMPILES("
f637b81fb60c22076d7a26eae77b004eda9c96fe
Fabian Homborg <FHomborg@gmail.com>
2020-01-06 18:16:34 +0100

    sample_prompts/acidhub: Use prompt_pwd
    This removes a call to `sed` and allows the user to specify shortening
    via the variable.
    
    We still default to disabling shortening because this prompt never
    did.
    
    [ci skip]

--- share/tools/web_config/sample_prompts/acidhub.fish
+++ share/tools/web_config/sample_prompts/acidhub.fish
@@ -38,8 +38,12 @@ function fish_prompt -d "Write out the prompt"
         set git_info "(git$git_status$git_branch"(set_color white)")"
     end
 
+    # Disable PWD shortening by default.
+    set -q fish_prompt_pwd_dir_length
+    or set -lx fish_prompt_pwd_dir_length 0
+
     set_color -b black
-    printf '%s%s%s%s%s%s%s%s%s%s%s%s%s' (set_color -o white) '❰' (set_color green) $USER (set_color white) '❙' (set_color yellow) (echo $PWD | sed -e "s|^$HOME|~|") (set_color white) $git_info (set_color white) '❱' (set_color white)
+    printf '%s%s%s%s%s%s%s%s%s%s%s%s%s' (set_color -o white) '❰' (set_color green) $USER (set_color white) '❙' (set_color yellow) (prompt_pwd) (set_color white) $git_info (set_color white) '❱' (set_color white)
     if test $laststatus -eq 0
         printf "%s✔%s≻%s " (set_color -o green) (set_color white) (set_color normal)
     else

db3bb48b01fb9d5e9fb50ce68a124af6aa7ca8da
239 <239@users.noreply.github.com>
2020-01-09 22:26:04 +0100

    Apply suggested change for filtering options

--- share/completions/keybase.fish
+++ share/completions/keybase.fish
@@ -1,11 +1,7 @@
 #Keybase 5.1.1
 
 function __fish_keybase_line_ends_with
-    set -l line
-    for s in (commandline -poc)
-        set -l c (string sub -l 1 -- $s)
-        if test "$c" != "-"; set -a line $s; end
-    end
+    set -l line (commandline -poc | string match -v -r '^-')
     for i in (seq -1 -1 -(count $argv))
         if test "$line[$i]" != "$argv[$i]"; return 1; end
     end

103a26ffb17819ca561b20adba9de9c2ae48dc6c
Jason Nader <jason.nader@protonmail.com>
2020-01-22 00:00:28 +0900

    Fix function output on FreeBSD/Cygwin systems

--- share/functions/__fish_print_encodings.fish
+++ share/functions/__fish_print_encodings.fish
@@ -1,3 +1,8 @@
 function __fish_print_encodings -d "Complete using available character encodings"
-    iconv --list | sed -e 's|//||'
+    if iconv --usage &>/dev/null  # only GNU has this flag
+        # Note: GNU iconv changes its output to contain the forward slashes below when stdout is not a tty.
+        iconv --list | string replace -ra '//' ''
+    else # non-GNU
+        iconv --list | string replace -ra '\s+' '\n'
+    end
 end

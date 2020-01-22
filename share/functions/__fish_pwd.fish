c92376a39fa8b6a3adb1865ddd55436cbd35f98b
Jason Nader <jason.nader@protonmail.com>
2020-01-22 18:33:24 +0900

    Stringify __fish_pwd.fish

--- share/functions/__fish_pwd.fish
+++ share/functions/__fish_pwd.fish
@@ -1,7 +1,12 @@
 switch (uname)
     case 'CYGWIN_*'
         function __fish_pwd --description "Show current path"
-            pwd | sed -e 's-^/cygdrive/\(.\)/\?-\u\1:/-'
+            if status test-feature regex-easyesc
+                pwd | string replace -r '^/cygdrive/(.)?' '\U$1:'
+            else
+                # TODO: Remove this once regex-easyesc becomes the default
+                pwd | string replace -r '^/cygdrive/(.)?' '\\\U$1:'
+            end
         end
     case '*'
         function __fish_pwd --description "Show current path"

8a9c39d433fa32538f3a5c99dbf13df5f96d0843
ridiculousfish <corydoras@ridiculousfish.com>
2020-01-25 14:10:45 -0800

    Add a test for some 'status' subcommands

---
+++ tests/checks/status-command.fish
@@ -0,0 +1,33 @@
+#RUN: env FISH_PATH=%fish FILE_PATH=%s %fish %s
+
+status line-number
+# CHECK: 3
+
+# Check status fish-path
+# No output expected on success
+set status_fish_path (realpath (status fish-path))
+set env_fish_path (realpath $FISH_PATH)
+test "$status_fish_path" = "$env_fish_path"
+or echo "Fish path disagreement: $status_fish_path vs $env_fish_path"
+
+# Check is-block
+status is-block ; echo $status
+begin ; status is-block ; echo $status ; end
+# CHECK: 1
+# CHECK: 0
+
+# Check filename
+set status_filename (status filename)
+test (status filename) = "$FILE_PATH"
+or echo "File path disagreement: $status_filename vs $FILE_PATH"
+
+function print_my_name
+    status function
+end
+print_my_name
+# CHECK: print_my_name
+
+status is-command-substitution; echo $status
+echo (status is-command-substitution; echo $status)
+# CHECK: 1
+# CHECK: 0

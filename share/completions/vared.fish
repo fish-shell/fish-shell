c96dc3f545a1f653e7b685f5978ad02d827e28c4
Jason Nader <jason.nader@protonmail.com>
2020-01-26 21:17:22 +0900

    Stringify vared completions

--- share/completions/vared.fish
+++ share/completions/vared.fish
@@ -1,3 +1,2 @@
-complete -c vared -x -a "(set|sed -e 's/ /'\t'Variable: /')"
+complete -c vared -x -a "(set | string replace -r ' ' '\tVariable: ')"
 complete -c vared -s h -l help -d "Display help and exit"
-

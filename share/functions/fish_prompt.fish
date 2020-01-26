bcf2e2cdbd7f699c8744f5abbe452becb135c6a8
Jason <ammgws@users.noreply.github.com>
2020-01-26 20:22:02 +0900

    Remove unused variable

--- share/functions/fish_prompt.fish
+++ share/functions/fish_prompt.fish
@@ -7,7 +7,6 @@ function fish_prompt --description 'Write out the prompt'
 
     # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd
-    set -l prefix
     set -l suffix '>'
     if contains -- $USER root toor
         if set -q fish_color_cwd_root

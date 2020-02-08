bf7629462a2c412cf42480b41f40de1a45e53f11
Fabian Homborg <FHomborg@gmail.com>
2020-02-08 10:38:11 +0100

    Port some small tests to littlecheck

---
+++ tests/checks/exec.fish
@@ -0,0 +1,13 @@
+#RUN: %fish %s
+exec cat < nosuchfile
+#CHECKERR: warning: An error occurred while redirecting file 'nosuchfile'
+#CHECKERR: open: No such file or directory
+echo "failed: $status"
+#CHECK: failed: 1
+not exec cat < nosuchfile
+#CHECKERR: warning: An error occurred while redirecting file 'nosuchfile'
+#CHECKERR: open: No such file or directory
+echo "neg failed: $status"
+#CHECK: neg failed: 0
+exec cat < /dev/null
+echo "not reached"
--- tests/checks/expansion.fish
+++ tests/checks/expansion.fish
@@ -11,3 +11,9 @@ printf '<%s>\n' ($fish -c 'if $f[a]; end' 2>&1)
 # CHECK: <fish: Invalid index value>
 # CHECK: <if $f[a]; end>
 # CHECK: <      ^>
+
+set a A
+set aa AA
+set aaa AAA
+echo {$aa}a{1,2,3}(for a in 1 2 3; echo $a; end)
+#CHECK: AAa11 AAa21 AAa31 AAa12 AAa22 AAa32 AAa13 AAa23 AAa33

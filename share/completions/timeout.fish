3371dec2c95de3eb365ea0f59fc5c7cf4d17ee52
Jason Nader <jason.nader@protonmail.com>
2020-02-28 22:41:43 +0900

    completions: update timeout

--- share/completions/timeout.fish
+++ share/completions/timeout.fish
@@ -1,7 +1,13 @@
+__fish_make_completion_signals
 
 complete -c timeout -l foreground -d 'Run COMMAND in the foreground'
-complete -c timeout -s k -l kill-after -d 'Send a KILL signal after DURATION'
-complete -c timeout -s s -l signal -d 'Specify the signal to be sent'
-complete -c timeout -l help -d 'Display this help and exit'
-complete -c timeout -l version -d 'Output version information and exit'
+complete -c timeout -s k -l kill-after -d 'Send a KILL signal after DURATION' -x
+complete -c timeout -s s -l signal -d 'Specify the signal to be sent' -xa "$__kill_signals"
+complete -c timeout -l preserve-status -d 'Exit with same status as COMMAND'
 
+# GNU coreutils ver
+if timeout --version 2>/dev/null
+    complete -c timeout -l help -d 'Display this help and exit'
+    complete -c timeout -l version -d 'Output version and exit'
+    complete -c timeout -s v -l verbose -d 'Send diagnostic info to stderr'
+end

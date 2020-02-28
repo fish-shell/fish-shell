f897948442e682f450b7fa86c8b0f1b8d7347ce8
Jason Nader <jason.nader@protonmail.com>
2020-02-28 22:24:12 +0900

    completions: update rfkill

--- share/completions/rfkill.fish
+++ share/completions/rfkill.fish
@@ -1,4 +1,9 @@
 complete -c rfkill -xa 'block unblock list event help' -n 'not __fish_seen_subcommand_from block unblock list event help'
 complete -c rfkill -n '__fish_seen_subcommand_from block unblock list' -d 'device group' -xa "all wifi wlan bluetooth uwb ultrawideband wimax wwan gps fm (rfkill list | tr : \t)"
-complete -c rfkill -l version -d 'Print version'
-
+complete -c rfkill -s V -l version -d 'Print version'
+complete -c rfkill -s h -l help -d 'Print help'
+complete -c rfkill -s J -l json -d 'JSON output'
+complete -c rfkill -s r -l raw -d 'Raw output'
+complete -c rfkill -s n -l noheadings -d "Don't print headings"
+complete -c rfkill -s o -l output -d "Columns to output" -xa "DEVICE ID TYPE TYPE-DESC SOFT HARD"
+complete -c rfkill -l output-all -d "Output all columns"

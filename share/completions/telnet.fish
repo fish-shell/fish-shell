2d700136a4e11ebde3920dca2b1877248da01def
Jason Nader <jason.nader@protonmail.com>
2020-02-28 21:23:49 +0900

    Run fish_indent on completions

--- share/completions/telnet.fish
+++ share/completions/telnet.fish
@@ -17,35 +17,35 @@ complete -c telnet -s n -x -d "Log to tracefile"
 complete -c telnet -s r -d "User interface similar to rlogin"
 
 switch (uname -s)
-  case FreeBSD NetBSD Linux
-    complete -c telnet -s d -d "Sets debug mode"
+    case FreeBSD NetBSD Linux
+        complete -c telnet -s d -d "Sets debug mode"
 end
 
 switch (uname -s)
-  case FreeBSD NetBSD
-    complete -c telnet -s F -s f -d "Forward local credentials if using Kerberos V5 authentication"
-    complete -c telnet -s S -x -d "Sets IP TOS"
-    complete -c telnet -s x -d "Turn on encryption"
-    complete -c telnet -s k -x -d "Use Kerberos realm for authentication"
-    complete -c telnet -s N -d "Prevent IP address to name lookup"
-    complete -c telnet -s X -x -d "Disables specified type of authentication"
+    case FreeBSD NetBSD
+        complete -c telnet -s F -s f -d "Forward local credentials if using Kerberos V5 authentication"
+        complete -c telnet -s S -x -d "Sets IP TOS"
+        complete -c telnet -s x -d "Turn on encryption"
+        complete -c telnet -s k -x -d "Use Kerberos realm for authentication"
+        complete -c telnet -s N -d "Prevent IP address to name lookup"
+        complete -c telnet -s X -x -d "Disables specified type of authentication"
 end
 
 switch (uname -s)
-  case FreeBSD
-    complete -c telnet -s y -d "Turn off encryption"
-    complete -c telnet -s B -x -d "Set baudrate"
-    complete -c telnet -s s -x -d "Set source IP address"
-    complete -c telnet -s u -d "Force AF_UNIX addresses only"
-  case NetBSD
-    complete -c telnet -s P -x -d "Set IPsec policy specfication"
-  case OpenBSD
-    complete -c telnet -s 7 -d "Do not try to negotiate TELNET BINARY option"
-    complete -c telnet -s V -x -d "Set routing table"
-    complete -c telnet -s D -d "Disable rewriting of the DISPLAY var"
-    complete -c telnet -s b -x -a "(__fish_print_addresses)" -d "Local address to bind to"
-  case Linux
-    complete -c telnet -s '?' -l help -d "Print help and exit"
-    complete -c telnet -l usage -d "Print short usage and exit"
-    complete -c telnet -s V -l version -d "Print program version"
+    case FreeBSD
+        complete -c telnet -s y -d "Turn off encryption"
+        complete -c telnet -s B -x -d "Set baudrate"
+        complete -c telnet -s s -x -d "Set source IP address"
+        complete -c telnet -s u -d "Force AF_UNIX addresses only"
+    case NetBSD
+        complete -c telnet -s P -x -d "Set IPsec policy specfication"
+    case OpenBSD
+        complete -c telnet -s 7 -d "Do not try to negotiate TELNET BINARY option"
+        complete -c telnet -s V -x -d "Set routing table"
+        complete -c telnet -s D -d "Disable rewriting of the DISPLAY var"
+        complete -c telnet -s b -x -a "(__fish_print_addresses)" -d "Local address to bind to"
+    case Linux
+        complete -c telnet -s '?' -l help -d "Print help and exit"
+        complete -c telnet -l usage -d "Print short usage and exit"
+        complete -c telnet -s V -l version -d "Print program version"
 end

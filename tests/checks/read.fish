0c74ff4209b9685de6fda593d391cae1687a6788
Johannes Altmanninger <aclopte@gmail.com>
2020-02-29 09:53:53 +0100

    Revert "read: discard IFS delimiters before the last token"
    See #6650.
    
    This reverts commit 1410f938aad04216d824aec747bc7919849d7005.
    
    (cherry-picked from commmit 91fcb8c42c7e995517cd2b476366b5c4f6c74b6f)

--- tests/checks/read.fish
+++ tests/checks/read.fish
@@ -12,13 +12,3 @@ set -l
 # CHECK: a 'afoo barb'
 # CHECK: b
 # CHECK: c
-
-echo "a  b b" | read a b
-string escape $a $b
-# CHECK: a
-# CHECK: 'b b'
-
-echo 'a<><>b<>b' | read -d '<>' a b
-printf %s\n $a $b
-# CHECK: a
-# CHECK: <>b<>b

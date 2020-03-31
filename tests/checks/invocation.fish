a29bc127cec6a911fcd5919e0c19dcc14b0ef360
Fabian Homborg <FHomborg@gmail.com>
2020-03-31 19:52:56 +0200

    Move the invocation checks to invocation.fish
    These where separate files so we could use the %s substitution to run
    that fish, but since discovering setting $fish that
    workaround isn't necessary.

--- tests/checks/invocation.fish
+++ tests/checks/invocation.fish
@@ -10,3 +10,31 @@ echo $status
 PATH= $fish -c "echo (command a)" 2>/dev/null
 echo $status
 # CHECK: 127
+
+if not set -q GITHUB_WORKFLOW
+    $fish -c 'if status --is-login ; echo login shell ; else ; echo not login shell ; end; if status --is-interactive ; echo interactive ; else ; echo not interactive ; end' -i
+    # CHECK: not login shell
+    # CHECK: interactive
+    $fish -c 'if status --is-login ; echo login shell ; else ; echo not login shell ; end; if status --is-interactive ; echo interactive ; else ; echo not interactive ; end' -l -i
+    # CHECK: login shell
+    # CHECK: interactive
+else
+    # Github Action doesn't start this in a terminal, so fish would complain.
+    # Instead, we just fake the result, since we have no way to indicate a skipped test.
+    echo not login shell
+    echo interactive
+    echo login shell
+    echo interactive
+end
+
+$fish -c 'if status --is-login ; echo login shell ; else ; echo not login shell ; end; if status --is-interactive ; echo interactive ; else ; echo not interactive ; end' -l
+# CHECK: login shell
+# CHECK: not interactive
+
+$fish -c 'if status --is-login ; echo login shell ; else ; echo not login shell ; end; if status --is-interactive ; echo interactive ; else ; echo not interactive ; end'
+# CHECK: not login shell
+# CHECK: not interactive
+
+$fish -c 'if status --is-login ; echo login shell ; else ; echo not login shell ; end; if status --is-interactive ; echo interactive ; else ; echo not interactive ; end' -l
+# CHECK: login shell
+# CHECK: not interactive
--- tests/checks/login-interactive.fish
+++
@@ -1,9 +0,0 @@
-# RUN: %fish -C 'set -l fish %fish' %s
-if not set -q GITHUB_WORKFLOW
-    $fish -c 'if status --is-login ; echo login shell ; else ; echo not login shell ; end; if status --is-interactive ; echo interactive ; else ; echo not interactive ; end' -l -i
-else
-    echo login shell
-    echo interactive
-end
-# CHECK: login shell
-# CHECK: interactive

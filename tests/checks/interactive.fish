d54609bd6360fea42dad86f148bff8e5ca4f0fee
Fabian Homborg <FHomborg@gmail.com>
2020-03-31 19:34:05 +0200

    Skip interactive tests on github actions for now
    Github actions doesn't run this in a terminal, so the `fish -i` tests won't really work.
    
    But still, it might be nicer than Travis.

--- tests/checks/interactive.fish
+++ tests/checks/interactive.fish
@@ -1,3 +1,10 @@
-#RUN: %fish -c 'if status --is-login ; echo login shell ; else ; echo not login shell ; end; if status --is-interactive ; echo interactive ; else ; echo not interactive ; end' -i
+# RUN: %fish -C 'set -l fish %fish' %s
+if not set -q GITHUB_WORKFLOW
+    $fish -c 'if status --is-login ; echo login shell ; else ; echo not login shell ; end; if status --is-interactive ; echo interactive ; else ; echo not interactive ; end' -i
+else
+    # FAKE
+    echo not login shell
+    echo interactive
+end
 # CHECK: not login shell
 # CHECK: interactive

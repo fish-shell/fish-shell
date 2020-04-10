9e553bec901c1a1b11c2d5c21d9bb09105220c84
Benjamin Kellermann <Benjamin.Kellermann@gmx.de>
2020-04-10 17:35:26 +0200

    add completion for busybox netcat

--- share/completions/nc.fish
+++ share/completions/nc.fish
@@ -2,4 +2,18 @@
 # Try to figure out which is the current used one
 # and load the right set of completions.
 
-complete -c nc -w (basename (realpath (which nc)))
+set -l netcat (basename (realpath (which nc)))
+switch $netcat
+case busybox
+    complete -c nc -s l -d "Listen mode, for inbound connects"
+    complete -c nc -s p -x -d "Local port"
+    complete -c nc -s w -x -d "Connect timeout"
+    complete -c nc -s i -x -d "Delay interval for lines sent"
+    complete -c nc -s f -r -d "Use file (ala /dev/ttyS0) instead of network"
+    complete -c nc -s e -r -d "Run PROG after connect"
+case nc.traditional nc.openbsd ncat
+    complete -c nc -w $netcat
+case '*'
+    # fallback to the most restricted one
+    complete -c nc -w nc.traditional
+end

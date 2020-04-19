2421eb618074442d02800e7ff1e811fddef87d30
Charles Gould <cgould@protonmail.com>
2020-04-18 22:43:30 -0500

    docs: Add completions for fish_key_reader

---
+++ share/completions/fish_key_reader.fish
@@ -0,0 +1,3 @@
+complete -c fish_key_reader -s h -l help -d 'Display help and exit'
+complete -c fish_key_reader -s v -l version -d 'Display version and exit'
+complete -c fish_key_reader -s c -l continuous -d 'Start a continuous session'

18b2a65fdcf73893a51d6e76bf0dd34fc0828910
Fabian Homborg <FHomborg@gmail.com>
2020-04-21 19:22:13 +0200

    Remove unused --inherit-variable

--- share/functions/__fish_complete_proc.fish
+++ share/functions/__fish_complete_proc.fish
@@ -1,4 +1,4 @@
-function __fish_complete_proc --inherit-variable ps
+function __fish_complete_proc
     # "comm=" means "print comm field with an empty name", which causes the header to be removed.
     # On many systems, comm is truncated (e.g. on Linux it's 15 chars),
     # but that's okay since commands that accept them as argument also only use those (e.g. pgrep).

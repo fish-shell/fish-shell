90c89c877f8f7c4d1beab2a2c492945fcdf61e81
Jason Nader <jason.nader@protonmail.com>
2020-05-07 22:32:52 +0900

    Use short flag for --description for consistency in code base

--- share/completions/pkg_delete.fish
+++ share/completions/pkg_delete.fish
@@ -1,8 +1,3 @@
-#completion for pkg_delete
-
-
-complete -c pkg_delete -a '(__fish_print_packages)' --description Package
-
+complete -c pkg_delete -a '(__fish_print_packages)' -d Package
 complete -c pkg_delete -o a -d 'Delete unused deps'
 complete -c pkg_delete -o V -d 'Turn on stats'
-

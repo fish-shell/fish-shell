fdf42d16bd32cc1084d36e624329569d187ce9ce
Enrico Borba <enricozb@gmail.com>
2020-05-16 15:55:26 -0700

    __fish_prepend_sudo: Toggle "sudo" on multiple presses
    At the moment calling __fish_prepend_sudo multiple times does not toggle
    sudo, and also unnecessarily uses the `-c` flag to `commandline` to see if
    the first token on the commandline is "sudo".
    
    This change removes the `-c` switch and also toggles "sudo" on multiple
    calls to __fish_prepend_sudo, while maintaining the cursor position and
    while maintaining any spaces between "sudo" and the next token on the
    commandline.

--- share/functions/__fish_prepend_sudo.fish
+++ share/functions/__fish_prepend_sudo.fish
@@ -1,9 +1,17 @@
 function __fish_prepend_sudo -d "Prepend 'sudo ' to the beginning of the current commandline"
-    set -l cmd (commandline -poc)
+    set -l cmd (commandline -po)
+    set -l cursor (commandline -C)
     if test "$cmd[1]" != sudo
-        set -l cursor (commandline -C)
         commandline -C 0
         commandline -i "sudo "
         commandline -C (math $cursor + 5)
+    else
+        commandline -r (string sub --start=6 (commandline -p))
+        set -l new_cursor (math $cursor - 5)
+        if test "$new_cursor" -lt 0
+          commandline -C 0
+        else
+          commandline -C $new_cursor
+        end
     end
 end

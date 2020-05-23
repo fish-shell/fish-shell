928e80ad6afd6757a483980c8d9f5b7796f28ea5
Fabian Homborg <FHomborg@gmail.com>
2020-05-23 09:12:52 +0200

    Extract setting $LS_COLORS to its own function
    Makes it easier to override ls while keeping $LS_COLORS.

--- share/functions/ls.fish
+++ share/functions/ls.fish
@@ -9,28 +9,11 @@
 
 for opt in --color=auto -G --color -F
     if command ls $opt / >/dev/null 2>/dev/null
-
         function ls --description "List contents of directory" -V opt
+            __fish_set_lscolors
             isatty stdout
             and set -a opt -F
             command ls $opt $argv
-        end
-
-        if [ $opt = --color=auto ] && ! set -qx LS_COLORS && set -l cmd (command -s {g,}dircolors)[1]
-            set -l colorfile
-            for file in ~/.dir_colors ~/.dircolors /etc/DIR_COLORS
-                if test -f $file
-                    set colorfile $file
-                    break
-                end
-            end
-            # Here we rely on the legacy behavior of `dircolors -c` producing output
-            # suitable for csh in order to extract just the data we're interested in.
-            set -gx LS_COLORS ($cmd -c $colorfile | string split ' ')[3]
-            # The value should always be quoted but be conservative and check first.
-            if string match -qr '^([\'"]).*\1$' -- $LS_COLORS
-                set LS_COLORS (string match -r '^.(.*).$' $LS_COLORS)[2]
-            end
         end
 
         break

1f434cfde8ffff8825dd66fee80c1e5aba9095a4
Fabian Homborg <FHomborg@gmail.com>
2020-05-30 12:09:16 +0200

    fish_config: Don't mention python 2
    See #6537.
    
    [ci skip]

--- share/functions/fish_config.fish
+++ share/functions/fish_config.fish
@@ -5,7 +5,7 @@ function fish_config --description "Launch fish's web based configuration"
     else
         echo (set_color $fish_color_error)Cannot launch the web configuration tool:(set_color normal)
         echo (set_color -o)fish_config(set_color normal) requires Python.
-        echo Installing python2 or python3 will fix this, and also enable completions to be
+        echo Installing python will fix this, and also enable completions to be
         echo automatically generated from man pages.\n
         echo To change your prompt, create a (set_color -o)fish_prompt(set_color normal) function.
         echo There are examples in (set_color $fish_color_valid_path)$__fish_data_dir/tools/web_config/sample_prompts(set_color normal).\n

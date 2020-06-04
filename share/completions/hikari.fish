f432ed173c66b68e03b8bd19a7d51507345e1ed9
Alexander Sieg <alex@xanderio.de>
2020-06-04 18:19:46 +0200

    requested changes by code reviewer

--- share/completions/hikari.fish
+++ share/completions/hikari.fish
@@ -1,8 +1,7 @@
 set PROG 'hikari'
 
-complete -c $PROG -e 
 complete -c $PROG -f
-complete -c $PROG -s 'a' -a 'executable' -d 'Specify autostart executable'
-complete -c $PROG -s 'c' -a 'config' -d 'Specify a configuration file'
+complete -c $PROG -s 'a' -F -d 'Specify autostart executable'
+complete -c $PROG -s 'c' -F -d 'Specify a configuration file'
 complete -c $PROG -f -s 'h' -d 'Show help message and quit'
 complete -c $PROG -f -s 'v' -d 'Show version and quit'

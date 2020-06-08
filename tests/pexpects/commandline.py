7076880da911d358ed3d025f28a6241c34d840f8
Fabian Homborg <FHomborg@gmail.com>
2020-06-08 17:08:48 +0200

    Move commandline test to pexpect

---
+++ tests/pexpects/commandline.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from pexpect_helper import SpawnedProc
+
+sp = SpawnedProc()
+send, sendline, sleep, expect_prompt = sp.send, sp.sendline, sp.sleep, sp.expect_prompt
+expect_prompt()
+
+sendline("bind '~' 'handle_tilde'")
+expect_prompt()
+
+# printing the current buffer should not remove quoting
+sendline("function handle_tilde; echo; echo '@GUARD:1@'; commandline -b; echo '@/GUARD:1@'; commandline -b ''; end")
+expect_prompt()
+sendline ("echo \en one \"two three\" four'five six'{7} 'eight~")
+expect_prompt("\r\n@GUARD:1@\r\n(.*)\r\n@/GUARD:1@\r\n")
+
+# printing the buffer with -o should remove quoting
+sendline("function handle_tilde; echo; echo '@GUARD:2@'; commandline -bo; echo '@/GUARD:2@'; commandline -b ''; end")
+expect_prompt()
+sendline("echo one \"two three\" four'five six'{7} 'eight~")
+expect_prompt("\r\n@GUARD:2@\r\n(.*)\r\n@/GUARD:2@\r\n")

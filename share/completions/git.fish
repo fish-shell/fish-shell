2fe585e011d0b679d3ac3b143faa58674e2629ee
Tin Lai <oscar@tinyiu.com>
2020-06-09 16:16:34 +1000

    remove autogenerated path from sourcing git-* completions

--- share/completions/git.fish
+++ share/completions/git.fish
@@ -1870,9 +1870,20 @@ complete -c git -n '__fish_git_using_command bisect; and __fish_seen_argument --
 
 ## Custom commands (git-* commands installed in the PATH)
 complete -c git -n __fish_git_needs_command -a '(__fish_git_custom_commands)' -d 'Custom command'
+
+# Get the path to the generated completions
+# If $XDG_DATA_HOME is set, that's it, if not, it will be removed and ~/.local/share will remain.
+set -l generated_path $XDG_DATA_HOME/fish/generated_completions ~/.local/share/fish/generated_completions
+
+# We don't want to modify $fish_complete_path here, so we make a copy.
+set -l complete_dirs $fish_complete_path
+
+# Remove the path to the generated completions if it is in the list
+set -l ind (contains -i -- $generated_path[1] $complete_dirs); and set -e complete_dirs[$ind]
+
 # source git-* commands' autocompletion file if exists
 set -l __fish_git_custom_commands_completion
-for git_ext in $fish_complete_path/git-*.fish
+for git_ext in $complete_dirs/git-*.fish
     # ignore this completion as executable does not exists
     set -l cmd (string replace -r '.*/([^/]*)\.fish' '$1' $git_ext)
     not command -q $cmd

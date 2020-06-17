1b37eb0476ccaaaac402f92dd79ca21970c59988
Daniel Bengtsson <dbengt@redhat.com>
2020-06-18 00:35:34 +0200

    Fix long description in completion.
    Simplify the description, I think it's sufficient like this.
    
    Fixes: #6981

--- share/completions/awk.fish
+++ share/completions/awk.fish
@@ -5,6 +5,6 @@
 # have been hand edited since.
 #
 
-complete -c awk -s F -d 'Define the input field separator to be the extended regular expression ERE, before any input is read; see Regular Expressions' -r
+complete -c awk -s F -d 'Define the input field separator' -r
 complete -c awk -s f -d 'Specify the pathname of the file progfile containing an awk program' -r
-complete -c awk -s v -d 'The application shall ensure that the assignment argument is in the same form as an assignment operand' -r
+complete -c awk -s v -d 'Ensure that the assignment argument is in the same form as an assignment operand' -r

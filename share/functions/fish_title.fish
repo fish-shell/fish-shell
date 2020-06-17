4660be372ad26f01bde62a3ba1a20316a241e172
Fabian Homborg <FHomborg@gmail.com>
2020-06-17 16:42:35 +0200

    Only disable title in emacs "term"
    There's more than one emacs terminal (for some reason), and term is
    the most broken one and can't even handle a title.
    
    Fixes #7122.

--- share/functions/fish_title.fish
+++ share/functions/fish_title.fish
@@ -1,6 +1,6 @@
 function fish_title
-    # emacs is basically the only term that can't handle it.
-    if not set -q INSIDE_EMACS
+    # emacs' "term" is basically the only term that can't handle it.
+    if not set -q INSIDE_EMACS; or string match -vq '*,term:*' -- $INSIDE_EMACS
         echo (status current-command) (__fish_pwd)
     end
 end

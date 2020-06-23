a658b27a10a99eda18d8aa3300ab75f68f1c606a
Gokul <gokulps15@gmail.com>
2020-06-24 00:20:10 +0530

    completions/git: Handle multiline config values
    When completing `git config` only display the first
    line of config value as description if it is
    multiline, appended with an ellipsis.

--- share/completions/git.fish
+++ share/completions/git.fish
@@ -550,7 +550,12 @@ end
 
 function __fish_git_config_keys
     # Print already defined config values first
-    git config -l | string replace '=' \t
+    # Config keys may span multiple lines, so parse using null char
+    # With -z, key and value are separated by space, not "="
+    git config -lz | while read -lz key value
+        # Print only first line of value(with an ellipsis) if multiline
+        printf '%s\t%s\n' $key (string replace \n …\n $value)[1]
+    end
     # Print all recognized config keys; duplicates are not shown twice by fish
     printf '%s\n' (__fish_git help --config)[1..-2] # Last line is a footer; ignore it
 end

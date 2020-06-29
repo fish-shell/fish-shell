da21ab5892406bad79ec91fd60b7df7b0b7384a9
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2020-06-28 23:02:13 -0500

    [xinput completions] Support devices with : in name
    [ci skip]

--- share/completions/xinput.fish
+++ share/completions/xinput.fish
@@ -4,7 +4,7 @@ set -l cmds version list get-feedbacks set-pointer set-mode set-ptr-feedback set
 function __fish_xinput_devices
     # Yeah, this includes a "↳" char
     # There's either this or printing only name or id, not both
-    xinput list --short | string replace -r '^[\W↳]*([\w/ ]+)\s+id=([0-9]+).*$' '$2\t$1'
+    xinput list --short | string replace -r '^[\W↳]*(.*?)\s+id=([0-9]+).*$' '$2\t$1'
 end
 
 complete -c xinput -f -n "not __fish_seen_subcommand_from $cmds" -a "$cmds"

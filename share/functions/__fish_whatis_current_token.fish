851722e50bce97cfc9e4cc6dcd44367708e088e4
Gokul Soumya <gokulps15@gmail.com>
2020-07-12 00:43:47 +0530

    Show builtin description with whatis_current_token

--- share/functions/__fish_whatis_current_token.fish
+++ share/functions/__fish_whatis_current_token.fish
@@ -16,6 +16,9 @@ function __fish_whatis_current_token -d "Show man page entries or function descr
                 test $funcinfo[5] != "n/a"
                 and set desc "$token - $funcinfo[5]"
 
+            case "builtin"
+                set desc (__fish_print_help $token | awk "/^$token /{print; exit}")
+
             case "file"
                 set -l tmpdesc (whatis $token 2>/dev/null)
                 and set desc $tmpdesc

a30630a1dfb490ef6a6458595a977b0c586cac70
Fabian Homborg <FHomborg@gmail.com>
2020-07-21 20:31:32 +0200

    Add some rudimentary `fg` tests
    See #5451

---
+++ tests/pexpects/fg.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from pexpect_helper import SpawnedProc
+import subprocess
+import sys
+import time
+
+sp = SpawnedProc()
+send, sendline, sleep, expect_prompt, expect_re, expect_str = (
+    sp.send,
+    sp.sendline,
+    sp.sleep,
+    sp.expect_prompt,
+    sp.expect_re,
+    sp.expect_str,
+)
+expect_prompt()
+
+sendline("cat | cat")
+sendline("set -l foo bar; echo $foo")
+expect_str("set -l foo bar; echo $foo")
+
+send("\x1A")
+expect_prompt()
+sendline("set -l foo bar; echo $foo")
+expect_str("bar")
+
+expect_prompt()
+sendline("fg")
+expect_str("Send job 1, 'cat | cat' to foreground")
+sendline("set -l foo bar; echo $foo")
+expect_str("set -l foo bar; echo $foo")
+send("\x04")
+
+expect_prompt()
+sendline("set -l foo bar; echo $foo")
+expect_str("bar")

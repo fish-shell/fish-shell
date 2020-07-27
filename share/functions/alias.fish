fish-shell/fish-shell

commit 115892ccd270cce4f6b1a90810d7bf2b5bb5b3a2

alias: Use `read --tokenize`
This did some weird unescaping to try to extract the first word.

So we're now more likely to be *correct*, and the alias benchmark is
about 20% *faster*.

Call it a win-win.

share/functions/alias.fish
@@ -44,22 +44,12 @@ function alias --description 'Creates a function wrapping a command'
         return 1
     end
 
-    # Extract the first command from the body. This is supposed to replace all non-escaped (i.e.
-    # preceded by an odd number of `\`) spaces with a newline so it splits on them. See issue #2220
-    # for why the following borderline incomprehensible code exists.
-    set -l tmp (string replace -ra -- "([^\\\ ])((\\\\\\\)*) " '$1\n' $body)
-    set first_word (string trim -- $tmp[1])
-    # If the user does something like `alias x 'foo; bar'` we need to strip the semicolon.
-    set base_command (string trim -c ';' -- $first_word)
-    if set -q tmp[2]
-        set body $tmp[2..-1]
-    else
-        set body
-    end
+    # Extract the first command from the body.
+    printf '%s\n' $body | read -lt first_word body
 
     # Prevent the alias from immediately running into an infinite recursion if
     # $body starts with the same command as $name.
-    if test $base_command = $name
+    if test $first_word = $name
         if contains $name (builtin --names)
             set prefix builtin
         else

tests/checks/alias.fish
@@ -10,9 +10,11 @@ my_alias
 
 alias a-2='echo "hello there"'
 
+alias foo '"a b" c d e'
 # Bare `alias` should list the aliases we have created and nothing else
 # We have to exclude two aliases because they're an artifact of the unit test
 # framework and we can't predict the definition.
 alias | grep -Ev '^alias (fish_indent|fish_key_reader) '
 # CHECK: alias a-2 'echo "hello there"'
+# CHECK: alias foo '"a b" c d e'
 # CHECK: alias my_alias 'foo; and echo foo ran'

benchmarks/benchmarks/aliases.fish
@@ -44,22 +44,12 @@ function alias --description 'Creates a function wrapping a command'
         return 1
     end
 
-    # Extract the first command from the body. This is supposed to replace all non-escaped (i.e.
-    # preceded by an odd number of `\`) spaces with a newline so it splits on them. See issue #2220
-    # for why the following borderline incomprehensible code exists.
-    set -l tmp (string replace -ra -- "([^\\\ ])((\\\\\\\)*) " '$1\n' $body)
-    set first_word (string trim -- $tmp[1])
-    # If the user does something like `alias x 'foo; bar'` we need to strip the semicolon.
-    set base_command (string trim -c ';' -- $first_word)
-    if set -q tmp[2]
-        set body $tmp[2..-1]
-    else
-        set body
-    end
+    # Extract the first command from the body.
+    printf '%s\n' $body | read -lt first_word body
 
     # Prevent the alias from immediately running into an infinite recursion if
     # $body starts with the same command as $name.
-    if test $base_command = $name
+    if test $first_word = $name
         if contains $name (builtin --names)
             set prefix builtin
         else

commit 84b25855b0859cc66ef74325571eec7b915d1223

alias: Escape the first word again
See https://stackoverflow.com/questions/63115744/alias-with-spaces-and-arguments-in-fish-3

This was broken in 115892ccd270cce4f6b1a90810d7bf2b5bb5b3a2

share/functions/alias.fish
@@ -57,6 +57,12 @@ function alias --description 'Creates a function wrapping a command'
         end
     end
     set -l cmd_string (string escape -- "alias $argv")
+
+    # We've tokenized the first word, but we need to reescape them so they won't be split apart by `source`.
+    # E.g. if the first_word is "some command with spaces", that needs to be understood as one argument.
+    # As $body is the rest of the tokenized read, it doesn't need to be escaped.
+    set first_word (string escape -- $first_word)
+
     set wrapped_cmd (string join ' ' -- $first_word $body | string escape)
     echo "function $name --wraps $wrapped_cmd --description $cmd_string; $prefix $first_word $body \$argv; end" | source
     if set -q _flag_save

tests/checks/alias.fish
@@ -24,3 +24,10 @@ alias | grep -Ev '^alias (fish_indent|fish_key_reader) '
 # #4756 - missing "--" argument to string causing issues with "--" options
 alias l. "ls -d .*"
 # No output
+
+alias d "'/mnt/c/Program Files (x86)/devenv.exe' /Edit"
+functions d
+# CHECK: # Defined in - @ line 1
+# CHECK: function d --wraps=\'/mnt/c/Program\ Files\ \(x86\)/devenv.exe\'\ /Edit --description alias\ d\ \'/mnt/c/Program\ Files\ \(x86\)/devenv.exe\'\ /Edit
+# CHECK: '/mnt/c/Program Files (x86)/devenv.exe' /Edit $argv;
+# CHECK: end
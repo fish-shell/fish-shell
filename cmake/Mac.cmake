d823eee339a04c316f54f92e3dbd00eba101aebc
ridiculousfish <rf@fishshell.com>
2020-07-31 12:11:20 -0700

    Disable code signing by default on the Mac
    This has caused too much pain for other packagers.

--- cmake/Mac.cmake
+++ cmake/Mac.cmake
@@ -1,8 +1,9 @@
 set(CMAKE_OSX_DEPLOYMENT_TARGET "10.9" CACHE STRING "Minimum OS X deployment version")
 
-# Code signing ID on Mac. A default '-' is ad-hoc codesign.
+# Code signing ID on Mac.
 # If this is falsey, codesigning is disabled.
-set(MAC_CODESIGN_ID "-" CACHE STRING "Mac code-signing identity")
+# '-' is ad-hoc codesign.
+set(MAC_CODESIGN_ID "" CACHE STRING "Mac code-signing identity")
 
 # Whether to inject the "get-task-allow" entitlement, which permits debugging
 # on the Mac.

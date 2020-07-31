afa3a4694b3dbd4a63960353c4e9f7f7a9723436
Daniel Kravetz Malabud <daniel.kravetz@gmail.com>
2020-07-31 10:03:26 +0200

    Implement suggestions by krobelus

--- share/completions/k3d.fish
+++ share/completions/k3d.fish
@@ -8,15 +8,11 @@ function __fish_k3d_no_subcommand --description 'Test if k3d has yet to be given
 end
 
 function __fish_k3d_print_clusters --description 'Print a list of k3d clusters'
-    for key in (k3d cluster list --no-headers | awk '{print $1}')
-        echo $key
-    end
+    k3d cluster list --no-headers | awk '{print $1}'
 end
 
 function __fish_k3d_print_nodes --description 'Print a list of k3d nodes'
-    for key in (k3d node list --no-headers | awk '{print $1}')
-        echo $key
-    end
+    k3d node list --no-headers | awk '{print $1}'
 end
 
 complete -f -c k3d -n '__fish_k3d_no_subcommand' -s h -l help -d "More information about a command"
@@ -32,9 +28,7 @@ complete -f -c k3d -n '__fish_seen_subcommand_from cluster' -a list -d "List clu
 complete -f -c k3d -n '__fish_seen_subcommand_from cluster' -a delete -d "Delete cluster(s)"
 complete -f -c k3d -n '__fish_seen_subcommand_from cluster' -a start -d "Start existing k3d cluster(s)"
 complete -f -c k3d -n '__fish_seen_subcommand_from cluster' -a stop -d "Stop existing k3d cluster(s)"
-complete -f -c k3d -n '__fish_seen_subcommand_from cluster; and __fish_seen_subcommand_from delete' -a '(__fish_k3d_print_clusters)' -d "Cluster"
-complete -f -c k3d -n '__fish_seen_subcommand_from cluster; and __fish_seen_subcommand_from start' -a '(__fish_k3d_print_clusters)' -d "Cluster"
-complete -f -c k3d -n '__fish_seen_subcommand_from cluster; and __fish_seen_subcommand_from stop' -a '(__fish_k3d_print_clusters)' -d "Cluster"
+complete -f -c k3d -n '__fish_seen_subcommand_from cluster; and __fish_seen_subcommand_from delete start stop' -a '(__fish_k3d_print_clusters)' -d "Cluster"
 complete -f -c k3d -n '__fish_seen_subcommand_from cluster' -s h -l help -d "Print usage"
 
 complete -f -c k3d -n '__fish_seen_subcommand_from image' -a import -d "Import image(s) from docker into k3d cluster(s)"
@@ -43,8 +37,8 @@ complete -f -c k3d -n '__fish_seen_subcommand_from image' -s h -l help -d "Print
 
 complete -f -c k3d -n '__fish_seen_subcommand_from kubeconfig' -a get -d "Get kubeconfig from cluster(s)"
 complete -f -c k3d -n '__fish_seen_subcommand_from kubeconfig' -a merge -d "Merge/write kubeconfig(s) from cluster(s) into existing kubeconfig/file"
-complete -f -c k3d -n '__fish_seen_subcommand_from kubeconfig get' -a '(__fish_k3d_print_clusters)' -d "Cluster"
-complete -f -c k3d -n '__fish_seen_subcommand_from kubeconfig get' -s a -l all -d "Get kubeconfig from all existing clusters"
+complete -f -c k3d -n '__fish_seen_subcommand_from kubeconfig get merge' -a '(__fish_k3d_print_clusters)' -d "Cluster"
+complete -f -c k3d -n '__fish_seen_subcommand_from kubeconfig get merge' -s a -l all -d "Get kubeconfig from all existing clusters"
 complete -f -c k3d -n '__fish_seen_subcommand_from kubeconfig' -s h -l help -d "Print usage"
 
 complete -f -c k3d -n '__fish_seen_subcommand_from node' -a create -d "Create a new k3s node in docker"
@@ -52,7 +46,5 @@ complete -f -c k3d -n '__fish_seen_subcommand_from node' -a delete -d "Delete no
 complete -f -c k3d -n '__fish_seen_subcommand_from node' -a list -d "List node(s)"
 complete -f -c k3d -n '__fish_seen_subcommand_from node' -a start -d "Start an existing k3d node"
 complete -f -c k3d -n '__fish_seen_subcommand_from node' -a stop -d "Stop an existing k3d node"
-complete -f -c k3d -n '__fish_seen_subcommand_from node; and __fish_seen_subcommand_from delete' -a '(__fish_k3d_print_nodes)' -d "Node"
-complete -f -c k3d -n '__fish_seen_subcommand_from node; and __fish_seen_subcommand_from start' -a '(__fish_k3d_print_nodes)' -d "Node"
-complete -f -c k3d -n '__fish_seen_subcommand_from node; and __fish_seen_subcommand_from stop' -a '(__fish_k3d_print_nodes)' -d "Node"
+complete -f -c k3d -n '__fish_seen_subcommand_from node; and __fish_seen_subcommand_from delete start stop' -a '(__fish_k3d_print_nodes)' -d "Node"
 complete -f -c k3d -n '__fish_seen_subcommand_from node' -s h -l help -d "Print usage"

677e699a7a0b1774c60644a3190b2d27c29e7e82
Fabian Homborg <FHomborg@gmail.com>
2020-08-07 08:17:20 +0200

    Stop repainting after paste
    "repaint" here is a bit of a misnomer. It *doesn't* re-highlight, that
    just happens on its own.
    
    It re-runs the prompt, which can take quite a while (depending on the
    configuration), and which is also useless in this context as this
    isn't something the prompt will be reacting to (theoretically it
    could, but I doubt the utility of displaying "PASTE" for a few milliseconds).

--- share/functions/__fish_shared_key_bindings.fish
+++ share/functions/__fish_shared_key_bindings.fish
@@ -190,5 +190,4 @@ function __fish_stop_bracketed_paste
     # Restore the last bind mode.
     set fish_bind_mode $__fish_last_bind_mode
     set -e __fish_paste_quoted
-    commandline -f force-repaint
 end

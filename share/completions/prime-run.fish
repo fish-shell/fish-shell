46a69d6b902d9d3f0b4484a36149846731b823c9
Johannes Altmanninger <aclopte@gmail.com>
2020-08-10 20:46:07 +0200

    Simplify prime-run completions

--- share/completions/prime-run.fish
+++ share/completions/prime-run.fish
@@ -1,6 +1,6 @@
 function __fish_complete_prime_run_subcommand
     set -lx -a PATH /usr/local/sbin /sbin /usr/sbin
-    __fish_complete_subcommand --commandline $args
+    __fish_complete_subcommand
 end
 
 complete -c prime-run -x -a "(__fish_complete_prime_run_subcommand)"

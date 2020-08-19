35c8de7cd87c337cab566f24caab15483a6fb343
Daniel Bengtsson <dbengt@redhat.com>
2020-08-19 16:19:09 +0200

    Add completion for cd.
    Add completion for the new cd options.

--- share/completions/cd.fish
+++ share/completions/cd.fish
@@ -1,2 +1,4 @@
 complete -c cd -a "(__fish_complete_cd)"
 complete -c cd -s h -l help -d 'Display help and exit'
+complete -c cd -s L -l logical -d 'force symoblic links to be followed'
+complete -c cd -s P -l physical -d 'Use the physical directory structure'

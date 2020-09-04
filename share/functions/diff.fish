9776674a975dcab0b2de4d7a666763a7793f0b9c
Johannes Altmanninger <aclopte@gmail.com>
2020-09-04 18:42:49 +0200

    Update share/functions/diff.fish

--- share/functions/diff.fish
+++ share/functions/diff.fish
@@ -1,5 +1,5 @@
 # Use colours in diff output, if supported
-if command diff --color=auto /dev/null /dev/null 2>&1
+if command -vq diff; and command diff --color=auto /dev/null{,} >/dev/null 2>&1
     function diff
         command diff --color=auto $argv
     end

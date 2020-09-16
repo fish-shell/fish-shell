fish-shell/fish-shell

commit d415350aaf050104d1a8c1d4f4b1179f202aa105

reader: Add delete-or-exit bind function
This is an important binding and should therefore be in C++.

src/reader.cpp
@@ -1010,6 +1010,7 @@ static bool command_ends_paging(readline_cmd_t c, bool focused_on_search_field) 
         case rl::history_token_search_backward:
         case rl::history_token_search_forward:
         case rl::accept_autosuggestion:
+        case rl::delete_or_exit:
         case rl::cancel: {
             // These commands always end paging.
             return true;
@@ -2454,6 +2455,17 @@ void reader_data_t::handle_readline_command(readline_cmd_t c, readline_loop_stat
         case rl::end_of_buffer: {
             update_buff_pos(&command_line, command_line.size());
             reader_repaint_needed();
+            break;
+        }
+        case rl::delete_or_exit: {
+            if (!command_line.text.empty()) {
+                command_line.position = 0;
+                command_line.text = L"";
+                update_buff_pos(&command_line, 0);
+                reader_repaint_needed();
+            } else {
+                reader_set_end_loop(true);
+            }
             break;
         }
         case rl::cancel: {

src/input.cpp
@@ -132,6 +132,7 @@ static const input_function_metadata_t input_function_metadata[] = {
     {readline_cmd_t::reverse_repeat_jump, L"repeat-jump-reverse"},
     {readline_cmd_t::func_and, L"and"},
     {readline_cmd_t::expand_abbr, L"expand-abbr"},
+    {readline_cmd_t::delete_or_exit, L"delete-or-exit"},
     {readline_cmd_t::cancel, L"cancel"}};
 
 static_assert(sizeof(input_function_metadata) / sizeof(input_function_metadata[0]) ==

src/input_common.h
@@ -66,6 +66,7 @@ enum class readline_cmd_t {
     backward_jump_till,
     func_and,
     expand_abbr,
+    delete_or_exit,
     cancel,
     repeat_jump,
     // NOTE: This one has to be last.

commit 1215717d20c9ef5654a0953a9ebe67a6e5f5459f

Set exit status with delete-or-exit
(regression from d415350aaf050104d1a8c1d4f4b1179f202aa105)

This is important especially in e.g. the new Windows Terminal, because
for some reason that lets the tab stick around if the process exited
with a non-zero status.

Will add tests as soon as I figure out how.

src/reader.cpp
@@ -2993,6 +2993,8 @@ void reader_data_t::handle_readline_command(readline_cmd_t c, readline_loop_stat
             if (el->position() < el->size()) {
                 delete_char(false /* backward */);
             } else if (c == rl::delete_or_exit && el->empty()) {
+                // This is by definition a successful exit, override the status
+                parser().set_last_statuses(statuses_t::just(STATUS_CMD_OK));
                 exit_loop_requested = true;
                 check_exit_loop_maybe_warning(this);
             }
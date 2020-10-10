3f0e2ef1dc0186538cc9a8465b265e2abefedb18
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2020-10-10 11:53:57 -0500

    [openssl] Support openssl 1.1.0 completions

--- share/completions/openssl.fish
+++ share/completions/openssl.fish
@@ -3,5 +3,9 @@ function __fish_openssl_subcommand_options --description "Print options for open
     openssl list -options $cmd[2] | string replace -r -- '^(\S*)\s*.*' '-$1'
 end
 
-complete -c openssl -n __fish_use_subcommand -x -a "(openssl list -1 -commands -cipher-commands -digest-commands)"
+if command openssl list -1 2>&1 >/dev/null
+    complete -c openssl -n __fish_use_subcommand -x -a "(openssl list -1 -commands -cipher-commands -digest-commands)"
+else
+    complete -c openssl -n __fish_use_subcommand -x -a "(openssl help 2>&1 | string match -rv '^[A-Z]|^\$' | string split -n ' ')"
+end
 complete -c openssl -n 'not __fish_use_subcommand && string match -qr -- "^-" (commandline -ct)' -a "(__fish_openssl_subcommand_options)"

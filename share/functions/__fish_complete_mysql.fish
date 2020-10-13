8c748aec8b4df2805cf51abd004cf234d0cba6af
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2020-10-13 14:39:14 +0200

    Make mysql completions work with log-in

---
+++ share/functions/__fish_complete_mysql.fish
@@ -0,0 +1,24 @@
+function __fish_complete_mysql
+    set -l command $argv[1]
+    function __fish_get_mysql_cmd
+        argparse -i 'u/user=' 'P/port=' 'h/host=' 'p/password=?' 'S/socket=' -- (commandline -po)
+        set -l mysql_cmd mysql
+        for flag in u P h S
+            if set -q _flag_$flag
+                set -l flagvar _flag_$flag
+                set -a mysql_cmd -$flag $$flagvar
+            end
+        end
+        if [ -n "$_flag_p" ]
+            set -a mysql_cmd -p$_flag_p
+        end
+        echo $mysql_cmd
+    end
+
+    function __fish_complete_mysql_databases
+        echo 'show databases' | eval (__fish_get_mysql_cmd) 2>/dev/null
+    end
+
+    complete -c $command -s D -l database -x -d 'The database to use' -a '(__fish_complete_mysql_databases)'
+    complete -c $command -a '(__fish_complete_mysql_databases)'
+end

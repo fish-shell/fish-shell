a29b5b6e8f6f3f753e2b4eca2fa26e028937dbed
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2020-10-25 23:00:59 -0500

    Run __fish_complete_entries test in its own sandbox
    It was being unduly influenced by any files created for or by other
    tests in the fish config/home directories.

--- tests/checks/complete_directories.fish
+++ tests/checks/complete_directories.fish
@@ -1,5 +1,8 @@
 #RUN: %fish %s
-cd ..
+mkdir -p __fish_complete_directories/
+cd __fish_complete_directories
+mkdir -p test/buildroot
+mkdir -p test/fish_expand_test
 mkdir -p test/data/abc
 mkdir -p test/data/abcd
 touch test/data/af
@@ -19,7 +22,6 @@ __fish_complete_directories test/data
 __fish_complete_directories test/data/
 #CHECK: test/data/abc/	Directory
 #CHECK: test/data/abcd/	Directory
-#CHECK: test/data/fish/	Directory
 #CHECK: test/data/xy/	Directory
 #CHECK: test/data/xyz/	Directory
 __fish_complete_directories test/data/abc 'abc dirs'

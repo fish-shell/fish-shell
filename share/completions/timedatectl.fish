bf275dfa3a97ce0c2c6c76f101f71a56f54855e8
Akatsuki Rui <3736910+akiirui@users.noreply.github.com>
2020-10-27 20:46:08 +0800

    timedatectl.fish: add missing 2 commands
    timesync-status
    show-timesync
    
    and some missing options

--- share/completions/timedatectl.fish
+++ share/completions/timedatectl.fish
@@ -1,19 +1,27 @@
-set -l commands status set-time{,zone} list-timezones set-local-rtc set-ntp
+set -l commands status show set-time set-timezone list-timezones set-local-rtc set-ntp timesync-status show-timesync
 
 complete -c timedatectl -f
-complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a status
-complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a set-time
-complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a set-timezone
-complete -c timedatectl -n "__fish_seen_subcommand_from set-timezone" -a "(timedatectl list-timezones)"
-complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a list-timezones
-complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a set-local-rtc -d "Maintain RTC in local time"
-complete -c timedatectl -n "__fish_seen_subcommand_from set-local-rtc" -a "true false"
-complete -c timedatectl -n "__fish_seen_subcommand_from set-local-rtc" -l adjust-system-clock -d 'Synchronize system clock from the RTC'
-complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a set-ntp -d "Use NTP"
-complete -c timedatectl -n "__fish_seen_subcommand_from set-ntp" -a "true false"
-complete -c timedatectl -l no-ask-password -d "Don't ask for password"
-complete -c timedatectl -s H -l host -d 'Execute the operation on a remote host'
-complete -c timedatectl -s M -l machine -d 'Execute operation on a local container'
-complete -c timedatectl -s h -l help -d 'Print a short help text and exit'
-complete -c timedatectl -l version -d 'Print a short version string and exit'
+complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a status -d 'Show current time settings'
+complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a show -d 'Show properties of systemd-timedated'
+complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a set-time -d 'Set system time'
+complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a set-timezone -d 'Set system time zone'
+complete -c timedatectl -n "__fish_seen_subcommand_from set-timezone" -a (timedatectl list-timezones)
+complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a list-timezones -d 'Show known time zones'
+complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a set-local-rtc -d 'Control whether RTC is in local time'
+complete -c timedatectl -n "__fish_seen_subcommand_from set-local-rtc" -a 'true false'
+complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a set-ntp -d 'Control network time sync'
+complete -c timedatectl -n "__fish_seen_subcommand_from set-ntp" -a 'true false'
+complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a timesync-status -d 'Show status of systemd-timesyncd'
+complete -c timedatectl -n "not __fish_seen_subcommand_from $commands" -a show-timesync -d 'Show properties of systemd-timesyncd'
+
+complete -c timedatectl -s h -l help -d 'Show this help message'
+complete -c timedatectl -l version -d 'Show package version'
 complete -c timedatectl -l no-pager -d 'Do not pipe output into a pager'
+complete -c timedatectl -l no-ask-password -d 'Do not prompt for password'
+complete -c timedatectl -s H -l host -r -d 'Operate on remote HOST'
+complete -c timedatectl -s M -l machine -r -d 'Operate on local CONTAINER'
+complete -c timedatectl -l adjust-system-clock -d 'Adjust system clock when changing local RTC mode'
+complete -c timedatectl -l monitor -d 'Monitor status of systemd-timesyncd'
+complete -c timedatectl -s p -l property -r -d 'Show only properties by this NAME'
+complete -c timedatectl -s a -l all -d 'Show all properties'
+complete -c timedatectl -l value -d 'Only show properties with values'

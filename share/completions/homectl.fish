aaafa76114755a57efe124d08510d51b56dab7bc
Akatsuki Rui <3736910+akiirui@users.noreply.github.com>
2020-10-30 09:06:34 +0800

    completions/homectl: refine and fix typo
    __homectl_subcommand_is: simplify
    condition: simplify
    typo: remove extra space in description

--- share/completions/homectl.fish
+++ share/completions/homectl.fish
@@ -7,10 +7,7 @@ end
 
 function __homectl_subcommand_is
     set -l cmd (commandline -poc)
-    if contains -- $cmd[-1] $argv
-        return 0
-    end
-    return 1
+    contains -- $cmd[-1] $argv
 end
 
 # Commands
@@ -47,83 +44,83 @@ complete -c homectl -s E -d 'Equals -j --export-format (twice to minimal)'
 complete -c homectl -o EE -d 'Equals -j --export-format=minimal'
 
 # User Record Properties
-for condition in '__fish_seen_subcommand_from create update'
-    # General User Record Properties
-    complete -c homectl -n $condition -s c -l real-name -d 'Real name for user'
-    complete -c homectl -n $condition -l realm -d 'Realm to create user in'
-    complete -c homectl -n $condition -l email-address -d 'Email address for user'
-    complete -c homectl -n $condition -l location -d 'Set location of user on earth'
-    complete -c homectl -n $condition -l icon-name -d 'Icon name for user'
-    complete -c homectl -n $condition -s d -l home-dir -d 'Home directory' -r
-    complete -c homectl -n $condition -s u -l uid -d 'Numeric UID for user'
-    complete -c homectl -n $condition -s G -l member-of -d 'Add user to group'
-    complete -c homectl -n $condition -l skel -d 'Skeleton directory to use' -r
-    complete -c homectl -n $condition -l shell -d 'Shell for account' -xa "(string match -r '^[^#].*' < /etc/shells)"
-    complete -c homectl -n $condition -l setenv -d 'Set an environment variable at log-in'
-    complete -c homectl -n $condition -l timezone -d 'Set a time-zone'
-    complete -c homectl -n $condition -l language -d 'Set preferred language'
-    complete -c homectl -n $condition -l ssh-authorized-keys -d 'Specify SSH public keys'
-    complete -c homectl -n $condition -l pkcs11-token-uri -d 'URI to PKCS#11 security token'
-    complete -c homectl -n $condition -l fido2-device -d 'Path to FIDO2 hidraw device' -r
+set -l condition '__fish_seen_subcommand_from create update'
 
-    # Account Management User Record Properties
-    complete -c homectl -n $condition -l locked -d 'Set locked account state' -xa 'yes no'
-    complete -c homectl -n $condition -l not-before -d 'Do not allow logins before'
-    complete -c homectl -n $condition -l not-after -d 'Do not allow logins after'
-    complete -c homectl -n $condition -l rate-limit-interval -d 'Login rate-limit interval in seconds'
-    complete -c homectl -n $condition -l rate-limit-burst -d 'Login rate-limit attempts per interval'
+# General User Record Properties
+complete -c homectl -n $condition -s c -l real-name -d 'Real name for user'
+complete -c homectl -n $condition -l realm -d 'Realm to create user in'
+complete -c homectl -n $condition -l email-address -d 'Email address for user'
+complete -c homectl -n $condition -l location -d 'Set location of user on earth'
+complete -c homectl -n $condition -l icon-name -d 'Icon name for user'
+complete -c homectl -n $condition -s d -l home-dir -d 'Home directory' -r
+complete -c homectl -n $condition -s u -l uid -d 'Numeric UID for user'
+complete -c homectl -n $condition -s G -l member-of -d 'Add user to group'
+complete -c homectl -n $condition -l skel -d 'Skeleton directory to use' -r
+complete -c homectl -n $condition -l shell -d 'Shell for account' -xa "(string match -r '^[^#].*' < /etc/shells)"
+complete -c homectl -n $condition -l setenv -d 'Set an environment variable at log-in'
+complete -c homectl -n $condition -l timezone -d 'Set a time-zone'
+complete -c homectl -n $condition -l language -d 'Set preferred language'
+complete -c homectl -n $condition -l ssh-authorized-keys -d 'Specify SSH public keys'
+complete -c homectl -n $condition -l pkcs11-token-uri -d 'URI to PKCS#11 security token'
+complete -c homectl -n $condition -l fido2-device -d 'Path to FIDO2 hidraw device' -r
 
-    # Password Policy User Record Properties:
-    complete -c homectl -n $condition -l password-hint -d 'Set Password hint'
-    complete -c homectl -n $condition -l enforce-password-policy -d "Control enforce password policy" -xa 'yes no'
-    complete -c homectl -n $condition -s P -d' Equivalent to --enforce-password-password=no'
-    complete -c homectl -n $condition -l password-change-now -d 'Require the password to be changed on next login' -xa 'yes no'
-    complete -c homectl -n $condition -l password-change-min -d 'Require minimum time between password changes'
-    complete -c homectl -n $condition -l password-change-max -d 'Require maximum time between password changes'
-    complete -c homectl -n $condition -l password-change-warn -d 'How much time to warn before password expiry'
-    complete -c homectl -n $condition -l password-change-inactive -d 'How much time to block password after expiry'
+# Account Management User Record Properties
+complete -c homectl -n $condition -l locked -d 'Set locked account state' -xa 'yes no'
+complete -c homectl -n $condition -l not-before -d 'Do not allow logins before'
+complete -c homectl -n $condition -l not-after -d 'Do not allow logins after'
+complete -c homectl -n $condition -l rate-limit-interval -d 'Login rate-limit interval in seconds'
+complete -c homectl -n $condition -l rate-limit-burst -d 'Login rate-limit attempts per interval'
 
-    # Resource Management User Record Properties:
-    complete -c homectl -n $condition -l disk-size -d 'Size to assign the user on disk'
-    complete -c homectl -n $condition -l access-mode -d 'User home directory access mode'
-    complete -c homectl -n $condition -l umask -d 'Umask for user when logging in'
-    complete -c homectl -n $condition -l nice -d 'Nice level for user'
-    complete -c homectl -n $condition -l rlimit -d 'Set resource limits'
-    complete -c homectl -n $condition -l tasks-max -d 'Set maximum number of per-user tasks'
-    complete -c homectl -n $condition -l memory-high -d 'Set high memory threshold in bytes'
-    complete -c homectl -n $condition -l memory-max -d 'Set maximum memory limit'
-    complete -c homectl -n $condition -l cpu-weight -d 'Set CPU weight'
-    complete -c homectl -n $condition -l io-weight -d 'Set IO weight'
+# Password Policy User Record Properties:
+complete -c homectl -n $condition -l password-hint -d 'Set Password hint'
+complete -c homectl -n $condition -l enforce-password-policy -d "Control enforce password policy" -xa 'yes no'
+complete -c homectl -n $condition -s P -d' Equivalent to --enforce-password-password=no'
+complete -c homectl -n $condition -l password-change-now -d 'Require the password to be changed on next login' -xa 'yes no'
+complete -c homectl -n $condition -l password-change-min -d 'Require minimum time between password changes'
+complete -c homectl -n $condition -l password-change-max -d 'Require maximum time between password changes'
+complete -c homectl -n $condition -l password-change-warn -d 'How much time to warn before password expiry'
+complete -c homectl -n $condition -l password-change-inactive -d 'How much time to block password after expiry'
 
-    # Storage User Record Properties:
-    complete -c homectl -n $condition -l storage -d 'Storage type to use' -xa 'luks fscrypt directory subvolume cifs'
-    complete -c homectl -n $condition -l image-path -d 'Path to image file/directory' -r
+# Resource Management User Record Properties:
+complete -c homectl -n $condition -l disk-size -d 'Size to assign the user on disk'
+complete -c homectl -n $condition -l access-mode -d 'User home directory access mode'
+complete -c homectl -n $condition -l umask -d 'Umask for user when logging in'
+complete -c homectl -n $condition -l nice -d 'Nice level for user'
+complete -c homectl -n $condition -l rlimit -d 'Set resource limits'
+complete -c homectl -n $condition -l tasks-max -d 'Set maximum number of per-user tasks'
+complete -c homectl -n $condition -l memory-high -d 'Set high memory threshold in bytes'
+complete -c homectl -n $condition -l memory-max -d 'Set maximum memory limit'
+complete -c homectl -n $condition -l cpu-weight -d 'Set CPU weight'
+complete -c homectl -n $condition -l io-weight -d 'Set IO weight'
 
-    # LUKS Storage User Record Properties:
-    complete -c homectl -n $condition -l fs-type -d 'File system type to use in case of luks storage' -xa 'ext4 xfs btrfs'
-    complete -c homectl -n $condition -l luks-discard -d 'Whether to use discard feature' -xa 'yes no'
-    complete -c homectl -n $condition -l luks-offline-discard -d 'Whether to trim file on logout' -xa 'yes no'
-    complete -c homectl -n $condition -l luks-cipher -d 'Cipher to use for LUKS encryption'
-    complete -c homectl -n $condition -l luks-cipher-mode -d 'Cipher mode to use for LUKS encryption'
-    complete -c homectl -n $condition -l luks-volume-key-size -d 'Volume key size to use for LUKS encryption'
-    complete -c homectl -n $condition -l luks-pbkdf-type -d 'Password-based Key Derivation Function to use'
-    complete -c homectl -n $condition -l luks-pbkdf-hash-algorithm -d 'PBKDF hash algorithm to use'
-    complete -c homectl -n $condition -l luks-pbkdf-time-cost -d 'Time cost for PBKDF in seconds'
-    complete -c homectl -n $condition -l luks-pbkdf-memory-cost -d ' Memory cost for PBKDF in bytes'
-    complete -c homectl -n $condition -l luks-pbkdf-parallel-threads -d 'Number of parallel threads for PKBDF'
+# Storage User Record Properties:
+complete -c homectl -n $condition -l storage -d 'Storage type to use' -xa 'luks fscrypt directory subvolume cifs'
+complete -c homectl -n $condition -l image-path -d 'Path to image file/directory' -r
 
-    # Mounting User Record Properties:
-    complete -c homectl -n $condition -l nosuid -d 'Control the nosuid flag of the home mount' -xa 'yes no'
-    complete -c homectl -n $condition -l nodev -d 'Control the nodev flag of the home mount' -xa 'yes no'
-    complete -c homectl -n $condition -l noexec -d ' Control the noexec flag of the home mount' -xa 'yes no'
+# LUKS Storage User Record Properties:
+complete -c homectl -n $condition -l fs-type -d 'File system type to use in case of luks storage' -xa 'ext4 xfs btrfs'
+complete -c homectl -n $condition -l luks-discard -d 'Whether to use discard feature' -xa 'yes no'
+complete -c homectl -n $condition -l luks-offline-discard -d 'Whether to trim file on logout' -xa 'yes no'
+complete -c homectl -n $condition -l luks-cipher -d 'Cipher to use for LUKS encryption'
+complete -c homectl -n $condition -l luks-cipher-mode -d 'Cipher mode to use for LUKS encryption'
+complete -c homectl -n $condition -l luks-volume-key-size -d 'Volume key size to use for LUKS encryption'
+complete -c homectl -n $condition -l luks-pbkdf-type -d 'Password-based Key Derivation Function to use'
+complete -c homectl -n $condition -l luks-pbkdf-hash-algorithm -d 'PBKDF hash algorithm to use'
+complete -c homectl -n $condition -l luks-pbkdf-time-cost -d 'Time cost for PBKDF in seconds'
+complete -c homectl -n $condition -l luks-pbkdf-memory-cost -d 'Memory cost for PBKDF in bytes'
+complete -c homectl -n $condition -l luks-pbkdf-parallel-threads -d 'Number of parallel threads for PKBDF'
 
-    # CIFS User Record Properties:
-    complete -c homectl -n $condition -l cifs-domain -d ' CIFS (Windows) domain'
-    complete -c homectl -n $condition -l cifs-user-name -d 'CIFS (Windows) user name'
-    complete -c homectl -n $condition -l cifs-service -d 'CIFS (Windows) service to mount as home area'
+# Mounting User Record Properties:
+complete -c homectl -n $condition -l nosuid -d 'Control the nosuid flag of the home mount' -xa 'yes no'
+complete -c homectl -n $condition -l nodev -d 'Control the nodev flag of the home mount' -xa 'yes no'
+complete -c homectl -n $condition -l noexec -d 'Control the noexec flag of the home mount' -xa 'yes no'
 
-    # Login Behaviour User Record Properties:
-    complete -c homectl -n $condition -l stop-delay -d 'How long to leave user services running after logout'
-    complete -c homectl -n $condition -l kill-processes -d 'Whether to kill user processes when sessions terminate' -xa 'yes no'
-    complete -c homectl -n $condition -l auto-login -d 'Try to log this user in automatically' -xa 'yes no'
-end
+# CIFS User Record Properties:
+complete -c homectl -n $condition -l cifs-domain -d 'CIFS (Windows) domain'
+complete -c homectl -n $condition -l cifs-user-name -d 'CIFS (Windows) user name'
+complete -c homectl -n $condition -l cifs-service -d 'CIFS (Windows) service to mount as home area'
+
+# Login Behaviour User Record Properties:
+complete -c homectl -n $condition -l stop-delay -d 'How long to leave user services running after logout'
+complete -c homectl -n $condition -l kill-processes -d 'Whether to kill user processes when sessions terminate' -xa 'yes no'
+complete -c homectl -n $condition -l auto-login -d 'Try to log this user in automatically' -xa 'yes no'

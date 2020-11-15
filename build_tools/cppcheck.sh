236587774e67f235e9daf1ddaf6ab5fa682aeafe
Fabian Homborg <FHomborg@gmail.com>
2020-11-15 12:41:55 +0100

    cppcheck: Make less annoying
    Force checking all #ifdef paths, disable StlAlgorithm
    suggestion
    
    Removes a lot of borderline useless spew.
    
    [ci skip]

--- build_tools/cppcheck.sh
+++ build_tools/cppcheck.sh
@@ -3,4 +3,5 @@
 cppcheck --enable=all --std=posix --quiet \
          --suppressions-list=build_tools/cppcheck.suppressions \
          --rule-file=build_tools/cppcheck.rules \
+         --force \
          ./src/

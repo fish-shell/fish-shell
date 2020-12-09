349e4b7ab1e313b0ad3d1bca163dbcd563d74c49
Fabian Homborg <FHomborg@gmail.com>
2020-12-09 20:51:07 +0100

    Change fish_trace prefix to "->" instead of plusses
    This matches what we do in --profile's output:
    
    ```
    > source /home/alfa/.config/fish/config.fish
    --> set -gx XDG_CACHE_HOME /home/alfa/.cache
    --> set -gx XDG_CONFIG_HOME /home/alfa/.config
    --> set -gx XDG_DATA_HOME /home/alfa/.local/share
    ```
    
    instead of
    
    ```
    + source /home/alfa/.config/fish/config.fish
    +++ set -gx XDG_CACHE_HOME /home/alfa/.cache
    +++ set -gx XDG_CONFIG_HOME /home/alfa/.config
    +++ set -gx XDG_DATA_HOME /home/alfa/.local/share
    ```

--- tests/checks/trace.fish
+++ tests/checks/trace.fish
@@ -13,11 +13,11 @@ end
 # CHECK: 2
 # CHECK: 3
 
-# CHECKERR: + for 1 2 3
-# CHECKERR: ++ echo 1
-# CHECKERR: ++ echo 2
-# CHECKERR: ++ echo 3
-# CHECKERR: + end for
+# CHECKERR: > for 1 2 3
+# CHECKERR: -> echo 1
+# CHECKERR: -> echo 2
+# CHECKERR: -> echo 3
+# CHECKERR: > end for
 
 while true
     and true
@@ -27,12 +27,12 @@ end
 
 # CHECK: inside
 
-# CHECKERR: + while
-# CHECKERR: + true
-# CHECKERR: + true
-# CHECKERR: ++ echo inside
-# CHECKERR: ++ break
-# CHECKERR: + end while
+# CHECKERR: > while
+# CHECKERR: > true
+# CHECKERR: > true
+# CHECKERR: -> echo inside
+# CHECKERR: -> break
+# CHECKERR: > end while
 
 while true && true
     echo inside2
@@ -41,12 +41,12 @@ end
 
 # CHECK: inside2
 
-# CHECKERR: + while
-# CHECKERR: + true
-# CHECKERR: + true
-# CHECKERR: ++ echo inside2
-# CHECKERR: ++ break
-# CHECKERR: + end while
+# CHECKERR: > while
+# CHECKERR: > true
+# CHECKERR: > true
+# CHECKERR: -> echo inside2
+# CHECKERR: -> break
+# CHECKERR: > end while
 
 if true && false
 else if false || true
@@ -56,17 +56,17 @@ end
 
 # CHECK: inside3
 
-# CHECKERR: + if
-# CHECKERR: + true
-# CHECKERR: + false
-# CHECKERR: + else if
-# CHECKERR: + false
-# CHECKERR: + true
-# CHECKERR: ++ echo inside3
-# CHECKERR: + end if
+# CHECKERR: > if
+# CHECKERR: > true
+# CHECKERR: > false
+# CHECKERR: > else if
+# CHECKERR: > false
+# CHECKERR: > true
+# CHECKERR: -> echo inside3
+# CHECKERR: > end if
 
 set -e fish_trace
-# CHECKERR: + set -e fish_trace
+# CHECKERR: > set -e fish_trace
 
 echo untraced
 # CHECK: untraced

0fdef958b60397dbf4ace658ba9b5e7129dd1832
Fabian Homborg <FHomborg@gmail.com>
2020-12-11 22:42:59 +0100

    __fish_complete_blockdevice: Default to /dev
    This gives all the blockdevices for `mount `
    
    Fixes #7543.

--- share/functions/__fish_complete_blockdevice.fish
+++ share/functions/__fish_complete_blockdevice.fish
@@ -2,7 +2,7 @@
 function __fish_complete_blockdevice
     set -l cmd (commandline -ct)
     test "" = "$cmd"
-    and return
+    and set cmd /dev/
     for f in $cmd*
         test -b $f
         and printf "%s\t%s\n" $f "Block device"

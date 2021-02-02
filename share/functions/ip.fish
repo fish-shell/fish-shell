cf4f8ae929f05f9a794bd329de3599918dc95aab
Fabian Homborg <FHomborg@gmail.com>
2021-02-02 18:44:44 +0100

    Revert "Color "ip" output if available"
    This reverts commit abb59a6ec925ca3430433d0df619f9b7c06269c2.
    
    This is still buffered if piped to another function, like with the default `grep`.
    
    See #5340, #5356.

--- share/functions/ip.fish
+++
@@ -1,9 +0,0 @@
-#
-# Enable ip colored output if available
-#
-
-if command -sq ip; and command ip -color=auto link >/dev/null 2>/dev/null
-    function ip
-        command ip -color=auto $argv
-    end
-end

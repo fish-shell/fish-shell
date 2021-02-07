254729f86de092f591c93aba3f231e05e4d267c3
Ilya Grigoriev <ilyagr@gmail.com>
2021-02-06 21:56:41 -0800

    Fix Alt+L when using the "Informative" prompt
    I ran into problems described in https://github.com/fish-shell/fish-shell/issues/718 when using this prompt. This seems to be a bug in the prompt -- this change fixes it, at least on my system. 
    
    I tried this in tmux (TERM=screen) and gnome-terminal (TERM=xterm-256) with fish 3.1.2, on Linux.

--- share/tools/web_config/sample_prompts/informative.fish
+++ share/tools/web_config/sample_prompts/informative.fish
@@ -15,7 +15,7 @@ function fish_prompt --description 'Informative prompt'
         set -l pipestatus_string (__fish_print_pipestatus "[" "] " "|" (set_color $fish_color_status) \
                                   (set_color --bold $fish_color_status) $last_pipestatus)
 
-        printf '[%s] %s%s@%s %s%s %s%s%s \f\r> ' (date "+%H:%M:%S") (set_color brblue) \
+        printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color brblue) \
             $USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
             (set_color normal)
     end

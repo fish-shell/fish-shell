fish-shell/fish-shell

commit e27d97b02e6c0f9fad079e1bf7ec937661077f86

Do not add spaces after completions ending in "-"
Some programs use this to separate things in a word, see
https://github.com/spf13/cobra/pull/1249#discussion_r563605048

Require the token to be at least length 2 for the no-space behavior,
for completions of "-" like for python.

src/complete.cpp
@@ -196,7 +196,7 @@ static complete_flags_t resolve_auto_space(const wcstring &comp, complete_flags_
     if (flags & COMPLETE_AUTO_SPACE) {
         new_flags &= ~COMPLETE_AUTO_SPACE;
         size_t len = comp.size();
-        if (len > 0 && (std::wcschr(L"/=@:.,", comp.at(len - 1)) != nullptr))
+        if (len > 1 && (std::wcschr(L"/=@:.,-", comp.at(len - 1)) != nullptr))
             new_flags |= COMPLETE_NO_SPACE;
     }
     return new_flags;

src/reader.cpp
@@ -1868,7 +1868,7 @@ static uint32_t get_best_rank(const completion_list_t &comp) {
 ///
 /// - If the list is empty, flash the terminal.
 /// - If the list contains one element, write the whole element, and if the element does not end on
-/// a '/', '@', ':', '.', ',' or a '=', also write a trailing space.
+/// a '/', '@', ':', '.', ',', '-' or a '=', also write a trailing space.
 /// - If the list contains multiple elements, insert their common prefix, if any and display
 /// the list in the pager.  Depending on terminal size and the length of the list, the pager
 /// may either show less than a screenfull and exit or use an interactive pager to allow the

commit 552c7bc40bc1792a9283bfe2b01a7f8c28fae97f

Don't add a space if completion only added a single "/"
This added a space if only one character was added, e.g.

```fish
cd dev<TAB>
```

would complete to

```fish
cd dev/<SPACE>
```

which makes picking deeper directories awkward.

So just go back to the old behavior of doing it for any length.

This is a regression from e27d97b02e6c0f9fad079e1bf7ec937661077f86.

cc @krobelus

src/complete.cpp
@@ -196,7 +196,7 @@ static complete_flags_t resolve_auto_space(const wcstring &comp, complete_flags_
     if (flags & COMPLETE_AUTO_SPACE) {
         new_flags &= ~COMPLETE_AUTO_SPACE;
         size_t len = comp.size();
-        if (len > 1 && (std::wcschr(L"/=@:.,-", comp.at(len - 1)) != nullptr))
+        if (len > 0 && (std::wcschr(L"/=@:.,-", comp.at(len - 1)) != nullptr))
             new_flags |= COMPLETE_NO_SPACE;
     }
     return new_flags;
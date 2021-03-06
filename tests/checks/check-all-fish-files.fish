8a07db8e8fd861ad0c69a7b9afad40aa323c5427
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2021-03-06 17:07:24 -0600

    Revert "Revert "Speed up check-all-fish-files when executed locally""
    This reverts commit e240d81ff841a86294e9e0e6b26bf42a1b4c18ed and
    introduces a more compatible method of finding newly added fish scripts
    to syntax check.
    
    `find -newer` is the original and is supported by everything under the
    sun (including FreeBSD, NetBSD, Solaris, OpenIndiana, macOS 10.10, WSL,
    and more), and if not, the tests will succeed anyway. `find -mnewer` was
    added later around the time `find -cnewer` and co (which checks the
    creation date rather than the modification date) was introduced, but
    apparently the GNU version of coreutils never introduced the `-mnewer`
    alias for `-newer`.
    
    Yes, this is hacky and yes it would be ideal if the build system is the
    one that picked which tests to run rather than the test itself picking.
    But let's not pretend that our tests are idealogically ideal or pure
    right now and until we fix the mess that is our CMake test integration
    (e.g. use ctest and configure each test to be run separately with
    configurable payloads, etc) eight seconds is still eight seconds, and
    again, the CI isn't affected.

--- tests/checks/check-all-fish-files.fish
+++ tests/checks/check-all-fish-files.fish
@@ -2,7 +2,19 @@
 # Test ALL THE FISH FILES
 # in share/, that is - the tests are exempt because they contain syntax errors, on purpose
 
-for file in $__fish_data_dir/**.fish
-    $fish -n $file
+set timestamp_file ./last_check_all_files
+set -l find_args
+if test -f $timestamp_file
+    set find_args -newer $timestamp_file
 end
+set -l fail_count 0
+for file in (find $__fish_data_dir/ -name "*.fish" $find_args 2>/dev/null; or find $__fish_data_dir/ -name "*.fish")
+    $fish -n $file; or set fail_count (math $fail_count + 1)
+end
+
+# Prevent setting timestamp if any errors were encountered
+if test "$fail_count" -eq 0
+    touch $timestamp_file
+end
+
 # No output is good output

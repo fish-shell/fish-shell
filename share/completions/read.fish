9fe2b27bbabcb80608a7551b82a21e39698d0699
Caroleq <karolina.gontarek20@gmail.com>
2021-03-09 20:28:25 +0100

    Shorten description in completions  (#7788)
    * Fix long descriptions in completions
    
    * PR review modifications

--- share/completions/read.fish
+++ share/completions/read.fish
@@ -5,12 +5,12 @@ complete -c read -s P -l prompt-str -d "Set prompt using provided string" -x
 complete -c read -s x -l export -d "Export variable to subprocess"
 complete -c read -s g -l global -d "Make variable scope global"
 complete -c read -s l -l local -d "Make variable scope local"
-complete -c read -s U -l universal -d "Make variable scope universal, i.e. share variable with all the users fish processes on this computer"
+complete -c read -s U -l universal -d "Share variable with all the users fish processes on the computer"
 complete -c read -s u -l unexport -d "Do not export variable to subprocess"
 complete -c read -s m -l mode-name -d "Name to load/save history under" -r -a "read fish"
 complete -c read -s c -l command -d "Initial contents of read buffer when reading interactively" -r
-complete -c read -s S -l shell -d "Use syntax highlighting, tab completions and command termination suitable for entering shellscript code"
-complete -c read -s s -l silent -d "Secure mode: mask characters at the command line (suitable for passwords)"
+complete -c read -s S -l shell -d "Read like the shell would"
+complete -c read -s s -l silent -d "Mask input with ●"
 complete -c read -s n -l nchars -d "Read the specified number of characters" -x
 complete -c read -s a -l list -l array -d "Store the results as an array"
 complete -c read -s R -l right-prompt -d "Set right-hand prompt command" -x

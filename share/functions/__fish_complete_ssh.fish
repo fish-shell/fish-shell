5eadee61c595344c26a867368105897d302e6073
exploide <me@exploide.net>
2021-03-16 19:48:10 +0100

    improved SSH completions
    - use __fish_complete_ssh also for sftp
    - removed old options -1 and -2
    - added several newer options
    - cleaned up and sorted completion code

--- share/functions/__fish_complete_ssh.fish
+++ share/functions/__fish_complete_ssh.fish
@@ -1,58 +1,114 @@
 function __fish_complete_ssh -d "common completions for ssh commands" --argument-names command
-    complete -c $command -s 1 -d "Protocol version 1 only"
-    complete -c $command -s 2 -d "Protocol version 2 only"
-    complete -c $command -s 4 -d "IPv4 addresses only"
-    complete -c $command -s 6 -d "IPv6 addresses only"
+    complete -c $command -s 4 -d "IPv4 only"
+    complete -c $command -s 6 -d "IPv6 only"
+    complete -c $command -s A -d "Enables forwarding of the authentication agent"
     complete -c $command -s C -d "Compress all data"
-    complete -xc $command -s c -d "Encryption algorithm" -a "blowfish 3des des"
-    complete -r -c $command -s F -d "Configuration file"
-    complete -r -c $command -s i -d "Identity file"
-    complete -x -c $command -s o -d Options -a "
-		AddressFamily
-		BatchMode
-		BindAddress
-		ChallengeResponseAuthentication
-		CheckHostIP
-		Cipher
-		Ciphers
-		Compression
-		CompressionLevel
-		ConnectionAttempts
-		ConnectTimeout
-		ControlMaster
-		ControlPath
-		GlobalKnownHostsFile
-		GSSAPIAuthentication
-		GSSAPIDelegateCredentials
-		Host
-		HostbasedAuthentication
-		HostKeyAlgorithms
-		HostKeyAlias
-		HostName
-		IdentityFile
-		IdentitiesOnly
-		LogLevel
-		MACs
-		NoHostAuthenticationForLocalhost
-		NumberOfPasswordPrompts
-		PasswordAuthentication
-		Port
-		PreferredAuthentications
-		Protocol
-		ProxyCommand
-		PubkeyAuthentication
-		RhostsRSAAuthentication
-		RSAAuthentication
-		SendEnv
-		ServerAliveInterval
-		ServerAliveCountMax
-		SmartcardDevice
-		StrictHostKeyChecking
-		TCPKeepAlive
-		UsePrivilegedPort
-		User
-		UserKnownHostsFile
-		VerifyHostKeyDNS
-	"
+    complete -c $command -s c -d "Encryption algorithm" -xa "(__fish_complete_list , __fish_ssh_ciphers)"
+    complete -c $command -s F -d "Configuration file" -rF
+    complete -c $command -s i -d "Identity key file" -rF
+    complete -c $command -s J -d 'ProxyJump host' -xa "(__fish_complete_user_at_hosts)"
+    complete -c $command -s o -d Options -xa "
+        AddKeysToAgent
+        AddressFamily
+        BatchMode
+        BindAddress
+        BindInterface
+        CanonicalDomains
+        CanonicalizeFallbackLocal
+        CanonicalizeHostname
+        CanonicalizeMaxDots
+        CanonicalizePermittedCNAMEs
+        CASignatureAlgorithms
+        CertificateFile
+        ChallengeResponseAuthentication
+        CheckHostIP
+        Ciphers
+        ClearAllForwardings
+        Compression
+        ConnectionAttempts
+        ConnectTimeout
+        ControlMaster
+        ControlPath
+        ControlPersist
+        DynamicForward
+        EscapeChar
+        ExitOnForwardFailure
+        FingerprintHash
+        ForwardAgent
+        ForwardX11
+        ForwardX11Timeout
+        ForwardX11Trusted
+        GatewayPorts
+        GlobalKnownHostsFile
+        GSSAPIAuthentication
+        GSSAPIClientIdentity
+        GSSAPIDelegateCredentials
+        GSSAPIKexAlgorithms
+        GSSAPIKeyExchange
+        GSSAPIRenewalForcesRekey
+        GSSAPIServerIdentity
+        GSSAPITrustDns
+        HashKnownHosts
+        Host
+        HostbasedAuthentication
+        HostbasedKeyTypes
+        HostKeyAlgorithms
+        HostKeyAlias
+        Hostname
+        IdentitiesOnly
+        IdentityAgent
+        IdentityFile
+        IPQoS
+        KbdInteractiveAuthentication
+        KbdInteractiveDevices
+        KexAlgorithms
+        LocalCommand
+        LocalForward
+        LogLevel
+        MACs
+        Match
+        NoHostAuthenticationForLocalhost
+        NumberOfPasswordPrompts
+        PasswordAuthentication
+        PermitLocalCommand
+        PKCS11Provider
+        Port
+        PreferredAuthentications
+        ProxyCommand
+        ProxyJump
+        ProxyUseFdpass
+        PubkeyAcceptedKeyTypes
+        PubkeyAuthentication
+        RekeyLimit
+        RemoteCommand
+        RemoteForward
+        RequestTTY
+        SendEnv
+        ServerAliveCountMax
+        ServerAliveInterval
+        SetEnv
+        StreamLocalBindMask
+        StreamLocalBindUnlink
+        StrictHostKeyChecking
+        TCPKeepAlive
+        Tunnel
+        TunnelDevice
+        UpdateHostKeys
+        User
+        UserKnownHostsFile
+        VerifyHostKeyDNS
+        VisualHostKey
+        XAuthLocation
+    "
+    complete -c $command -s q -d "Quiet mode"
     complete -c $command -s v -d "Verbose mode"
+end
+
+
+function __fish_ssh_ciphers -d "List of possible SSH cipher algorithms"
+    ssh -Q cipher
+end
+
+function __fish_ssh_macs -d "List of possible SSH MAC algorithms"
+    ssh -Q mac
 end

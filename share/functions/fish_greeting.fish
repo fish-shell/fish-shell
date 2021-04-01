5f3eed0a34b4c77d2c0667a4bb4f585b49fb511d
Jonathan Allard <jonathan@allard.io>
2021-04-01 15:29:52 -0400

    fish_greeting: Change default to a random fish emoji

--- share/functions/fish_greeting.fish
+++ share/functions/fish_greeting.fish
@@ -1,8 +1,6 @@
 function fish_greeting
     if not set -q fish_greeting
-        set -l line1 (_ 'Welcome to fish, the friendly interactive shell')
-        set -l line2 \n(printf (_ 'Type %shelp%s for instructions on how to use fish') (set_color green) (set_color normal))
-        set -g fish_greeting "$line1$line2"
+        set -g fish_greeting (random choice 🐟 🐠 🐡)
     end
 
     if set -q fish_private_mode

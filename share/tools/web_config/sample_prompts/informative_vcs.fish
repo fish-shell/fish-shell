34d4afac99287fbbcdfdbbebae17327158ab3a0a
Fabian Homborg <FHomborg@gmail.com>
2021-04-03 08:55:27 +0200

    Sample prompts: Remove stray space after pipestatus
    This would, with the default color, have an ugly red background.
    
    So just remove the space.

--- share/tools/web_config/sample_prompts/informative_vcs.fish
+++ share/tools/web_config/sample_prompts/informative_vcs.fish
@@ -54,7 +54,7 @@ function fish_prompt --description 'Write out the prompt'
 
     printf '%s ' (fish_vcs_prompt)
 
-    set -l pipestatus_string (__fish_print_pipestatus "[" "] " "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
+    set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
     echo -n $pipestatus_string
     set_color normal
 

4239ba133df09cb3aa37c0fb995ba1e2166a5667
Fabian Homborg <FHomborg@gmail.com>
2021-04-04 15:26:20 +0200

    Sample prompts: Add a "simple" prompt
    This should be a simple prompt that doesn't place a huge strain on the
    system but communicates the most important information simply and
    effectively.
    
    It should be a good jumping off point for making your own prompt.

---
+++ share/tools/web_config/sample_prompts/simple.fish
@@ -0,0 +1,23 @@
+# name: Simple
+
+function fish_prompt
+    # This is a simple prompt. It looks like
+    # alfa@nobby /path/to/dir $
+    # with the path shortened and colored
+    # and a "#" instead of a "$" when run as root.
+    set -l symbol ' $ '
+    set -l color $fish_color_cwd
+    if fish_is_root_user
+        set symbol ' # '
+        set -q fish_color_cwd_root
+        and set color $fish_color_cwd_root
+    end
+
+    echo -n $USER@$hostname
+
+    set_color $color
+    echo -n (prompt_pwd)
+    set_color normal
+
+    echo -n $symbol
+end

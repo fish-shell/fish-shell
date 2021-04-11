af2952dd2fa995a23c6a85741bf2155a5e0bcfe6
Fabian Homborg <FHomborg@gmail.com>
2021-04-11 21:44:44 +0200

    Allow passing directories to prompt_pwd
    This allows us to test it without cd-ing about the place.

--- share/functions/prompt_pwd.fish
+++ share/functions/prompt_pwd.fish
@@ -1,12 +1,15 @@
 function prompt_pwd --description 'Print the current working directory, shortened to fit the prompt'
     set -l options h/help d/dir-length= D/full-length-dirs=
-    argparse -n prompt_pwd --max-args=0 $options -- $argv
+    argparse -n prompt_pwd $options -- $argv
     or return
 
     if set -q _flag_help
         __fish_print_help prompt_pwd
         return 0
     end
+
+    set -q argv[1]
+    or set argv $PWD
 
     set -ql _flag_d
     and set -l fish_prompt_pwd_dir_length $_flag_d
@@ -19,24 +22,26 @@ function prompt_pwd --description 'Print the current working directory, shortene
     and set fish_prompt_pwd_full_dirs $_flag_D
 
     set -q fish_prompt_pwd_full_dirs
-    or set -l fish_prompt_pwd_full_dirs 0
+    or set -l fish_prompt_pwd_full_dirs 1
 
-    # Replace $HOME with "~"
-    set -l realhome ~
-    set -l tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
+    for path in $argv
+        # Replace $HOME with "~"
+        set -l realhome ~
+        set -l tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $path)
 
-    if test "$fish_prompt_pwd_dir_length" -eq 0
-        echo $tmp
-    else
-        # Shorten to at most $fish_prompt_pwd_dir_length characters per directory
-        # with full-length-dirs components left at full length.
-        set -l full
-        if test $fish_prompt_pwd_full_dirs -gt 0
-            set -l all (string split -m (math $fish_prompt_pwd_full_dirs - 1) -r / $tmp)
-            set tmp $all[1]
-            set full $all[2..]
+        if test "$fish_prompt_pwd_dir_length" -eq 0
+            echo $tmp
+        else
+            # Shorten to at most $fish_prompt_pwd_dir_length characters per directory
+            # with full-length-dirs components left at full length.
+            set -l full
+            if test $fish_prompt_pwd_full_dirs -gt 0
+                set -l all (string split -m (math $fish_prompt_pwd_full_dirs - 1) -r / $tmp)
+                set tmp $all[1]
+                set full $all[2..]
+            end
+
+            string join / (string replace -ar '(\.?[^/]{'"$fish_prompt_pwd_dir_length"'})[^/]*/' '$1/' $tmp) $full
         end
-
-        string join / (string replace -ar '(\.?[^/]{'"$fish_prompt_pwd_dir_length"'})[^/]*/' '$1/' $tmp) $full
     end
 end

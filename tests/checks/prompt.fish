e8f061efde1fecb84054091fb3a82f1f8bd7337a
Fabian Homborg <FHomborg@gmail.com>
2021-04-11 21:44:44 +0200

    Allow passing directories to prompt_pwd
    This allows us to test it without cd-ing about the place.

---
+++ tests/checks/prompt.fish
@@ -0,0 +1,13 @@
+#RUN: %fish %s
+
+prompt_pwd -d 1 /foo/bar/baz
+# CHECK: /f/b/baz
+
+prompt_pwd /usr/share/fish/tools/web_config/sample_prompts
+# CHECK: /u/s/f/t/w/sample_prompts
+
+prompt_pwd -D 2 /usr/share/fish/tools/web_config/sample_prompts
+# CHECK: /u/s/f/t/web_config/sample_prompts
+
+prompt_pwd -D 0 /usr/share/fish/tools/web_config/sample_prompts
+# CHECK: /u/s/f/t/w/sample_prompts

5ceee0b6c36bbac297382fa3b225b50da9634e35
Kid <44045911+kidonng@users.noreply.github.com>
2021-05-16 12:02:03 +0800

    Add missing options for `complete`

--- share/completions/complete.fish
+++ share/completions/complete.fish
@@ -6,6 +6,7 @@ complete -c complete -s s -l short-option -d "POSIX-style short option to comple
 complete -c complete -s l -l long-option -d "GNU-style long option to complete" -x
 complete -c complete -s o -l old-option -d "Old style long option to complete" -x
 complete -c complete -s f -l no-files -d "Don't use file completion"
+complete -c complete -s F -l force-files -d "Use file completion even if another applicable complete doesn't"
 complete -c complete -s r -l require-parameter -d "Require parameter"
 complete -c complete -s x -l exclusive -d "Require parameter and don't use file completion"
 complete -c complete -s a -l arguments -d "Space-separated list of possible option arguments" -x
@@ -15,6 +16,7 @@ complete -c complete -s h -l help -d "Display help and exit"
 complete -c complete -s C -l do-complete -d "Print completions for a commandline specified as a parameter"
 complete -c complete -s n -l condition -d "Completion only used if command has zero exit status" -x
 complete -c complete -s w -l wraps -d "Inherit completions from specified command" -xa '(__fish_complete_command)'
+complete -c complete -s k -l keep-order -d "Keep order of option arguments instead of sorting alphabetically"
 
 # Deprecated options
 

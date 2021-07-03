a672f0c5377c8ff0463fcc92b103e90196faa9e4
Kid <44045911+kidonng@users.noreply.github.com>
2021-07-04 00:03:28 +0800

    Add missing options for `bind`

--- share/completions/bind.fish
+++ share/completions/bind.fish
@@ -53,6 +53,9 @@ complete -c bind -s K -l key-names -d 'Print names of available keys'
 complete -c bind -s M -l mode -d 'Specify the bind mode that the bind is used in' -xa '(bind -L)'
 complete -c bind -s m -l sets-mode -d 'Change current mode after bind is executed' -xa '(bind -L)'
 complete -c bind -s L -l list-modes -d 'Display a list of defined bind modes'
+complete -c bind -s s -l silent -d 'Operate silently'
+complete -c bind -l preset -d 'Operate on preset bindings'
+complete -c bind -l user -d 'Operate on user bindings'
 
 complete -c bind -n __fish_bind_test1 -a '(bind --key-names)' -d 'Key name' -x
 complete -c bind -n __fish_bind_test2 -a '(bind --function-names)' -d 'Function name' -x

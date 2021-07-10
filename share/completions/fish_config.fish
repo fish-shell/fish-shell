09df07ff50e33a09d8e78b78b1db783a648d919f
Fabian Homborg <FHomborg@gmail.com>
2021-07-10 10:26:58 +0200

    fish_config: Add CLI-based theme selector
    `fish_config theme`:
    
    - `list` to list all available themes (files in the two theme
    directories - either the web_config/themes one or
    ~/.config/fish/themes!)
    - `show` to show select (or all) themes right in the terminal - this
    starts another fish that reads the theme file and prints the sample
    text, manually colored
    - `choose` to load a theme *now*, setting the variables globally
    - `save` to load a theme and save the variables universally
    - `dump` to write the current theme in .theme format (to stdout)
    - `demo` to display the current theme

--- share/completions/fish_config.fish
+++ share/completions/fish_config.fish
@@ -1,5 +1,6 @@
 complete fish_config -f
 set -l prompt_commands choose save show list
+set -l theme_commands choose demo dump save show list
 complete fish_config -n '__fish_use_subcommand' -a prompt -d 'View and pick from the sample prompts'
 complete fish_config -n "__fish_seen_subcommand_from prompt; and not __fish_seen_subcommand_from $prompt_commands" \
     -a choose -d 'View and pick from the sample prompts'
@@ -12,3 +13,18 @@ complete fish_config -n "__fish_seen_subcommand_from prompt; and not __fish_seen
 complete fish_config -n '__fish_seen_subcommand_from prompt; and __fish_seen_subcommand_from choose save show' -a '(fish_config prompt list)'
 
 complete fish_config -n '__fish_use_subcommand' -a browse -d 'Open the web-based UI'
+
+complete fish_config -n '__fish_use_subcommand' -a theme -d 'View and pick from the sample themes'
+complete fish_config -n '__fish_seen_subcommand_from theme; and __fish_seen_subcommand_from choose save show' -a '(fish_config theme list)'
+complete fish_config -n "__fish_seen_subcommand_from theme; and not __fish_seen_subcommand_from $theme_commands" \
+    -a choose -d 'View and pick from the sample themes'
+complete fish_config -n "__fish_seen_subcommand_from theme; and not __fish_seen_subcommand_from $theme_commands" \
+    -a show -d 'Show what theme would look like'
+complete fish_config -n "__fish_seen_subcommand_from theme; and not __fish_seen_subcommand_from $theme_commands" \
+    -a list -d 'List all available themes'
+complete fish_config -n "__fish_seen_subcommand_from theme; and not __fish_seen_subcommand_from $theme_commands" \
+    -a save -d 'Save the given theme as universal variables'
+complete fish_config -n "__fish_seen_subcommand_from theme; and not __fish_seen_subcommand_from $theme_commands" \
+    -a demo -d 'Show example in the current theme'
+complete fish_config -n "__fish_seen_subcommand_from theme; and not __fish_seen_subcommand_from $theme_commands" \
+    -a dump -d 'Print the current theme in .theme format'

eb3388d949a6c88bcd9908e87fcadd10d2540f10
Johannes Altmanninger <aclopte@gmail.com>
2021-07-10 20:08:34 +0200

    Teach fish_indent about our feature flags
    So it can handle syntax changes that call for different formatting.

--- tests/checks/features-ampersand-nobg-in-token1.fish
+++ tests/checks/features-ampersand-nobg-in-token1.fish
@@ -1,4 +1,4 @@
-#RUN: %fish --features=ampersand-nobg-in-token %s
+#RUN: %fish --features=ampersand-nobg-in-token -C 'set -g fish_indent %fish_indent' %s
 
 echo no&background
 # CHECK: no&background
@@ -10,3 +10,6 @@ echo background &
 # CHECK: background
 
 wait
+
+echo no&bg | fish_features=ampersand-nobg-in-token $fish_indent --check
+echo $status #CHECK: 0

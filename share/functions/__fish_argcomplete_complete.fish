d158c82a9888eef3e5e8b924344146115bd03f03
Kevin Konrad <kevin.konrad@unicepta.com>
2021-07-28 17:59:42 +0200

    extract argcomplete completion mechanism into its own function

---
+++ share/functions/__fish_argcomplete_complete.fish
@@ -0,0 +1,10 @@
+function __fish_argcomplete_complete
+  set -lx _ARGCOMPLETE 1
+  set -lx _ARGCOMPLETE_IFS \n
+  set -lx _ARGCOMPLETE_SUPPRESS_SPACE 1
+  set -lx _ARGCOMPLETE_SHELL fish
+  set -lx COMP_LINE (commandline -pc)
+  set -lx COMP_POINT (string length (commandline -cp))
+  set -lx COMP_TYPE
+  $argv 8>&1 9>&2 2>/dev/null
+end

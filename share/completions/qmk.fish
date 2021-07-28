413fd2fc03c13915ed0b567366a53127554f7d5c
Kevin Konrad <kevin.konrad@unicepta.com>
2021-07-28 17:59:42 +0200

    extract argcomplete completion mechanism into its own function

--- share/completions/qmk.fish
+++ share/completions/qmk.fish
@@ -1,12 +1,1 @@
-function __fish_qmk_complete
-  set -lx _ARGCOMPLETE 1
-  set -lx _ARGCOMPLETE_IFS \n
-  set -lx _ARGCOMPLETE_SUPPRESS_SPACE 1
-  set -lx _ARGCOMPLETE_SHELL fish
-  set -lx COMP_LINE (commandline -pc)
-  set -lx COMP_POINT (string length (commandline -cp))
-  set -lx COMP_TYPE
-  qmk 8>&1 9>&2 2>/dev/null
-end
-
-complete -c qmk -f -a '(__fish_qmk_complete)'
+complete -c qmk -f -a '(__fish_argcomplete_complete qmk)'

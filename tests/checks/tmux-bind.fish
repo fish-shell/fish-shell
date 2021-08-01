fish-shell/fish-shell

commit 35ca42413d1cc38fe2ac591de85a5181fcbc7393

Simplify some parse_util functions
Don't just reflexively drop down to wchar_t.

src/parse_util.cpp
@@ -42,51 +42,31 @@
 /// Maximum length of a variable name to show in error reports before truncation
 static constexpr int var_err_len = 16;
 
-int parse_util_lineno(const wchar_t *str, size_t offset) {
-    if (!str) return 0;
+int parse_util_lineno(const wcstring &str, size_t offset) {
+    // Return the line number of position offset, starting with 1.
+    if (str.empty()) return 0;
 
-    int res = 1;
-    for (size_t i = 0; i < offset && str[i] != L'\0'; i++) {
-        if (str[i] == L'\n') {
-            res++;
-        }
-    }
-    return res;
+    auto end = offset > str.length() ? str.end() : str.begin() + offset;
+    return std::count(str.begin(), end, L'\n') + 1;
 }
 
 int parse_util_get_line_from_offset(const wcstring &str, size_t pos) {
-    const wchar_t *buff = str.c_str();
-    int count = 0;
-    for (size_t i = 0; i < pos; i++) {
-        if (!buff[i]) {
-            return -1;
-        }
-
-        if (buff[i] == L'\n') {
-            count++;
-        }
-    }
-    return count;
+    // Return the line pos is on, or -1 if it's after the end.
+    if (str.length() > pos) return -1;
+    return std::count(str.begin() + pos, str.end(), L'\n');
 }
 
 size_t parse_util_get_offset_from_line(const wcstring &str, int line) {
-    const wchar_t *buff = str.c_str();
-    size_t i;
-    int count = 0;
-
+    // Return the first position on line X, counting from 0.
     if (line < 0) return static_cast<size_t>(-1);
     if (line == 0) return 0;
 
-    for (i = 0;; i++) {
-        if (!buff[i]) return static_cast<size_t>(-1);
-
-        if (buff[i] == L'\n') {
-            count++;
-            if (count == line) {
-                return i + 1;
-            }
-        }
+    ssize_t i = 0;
+    while (auto pos = str.find(L'\n')) {
+        i++;
+        if (i == line) return pos + 1;
     }
+    return static_cast<size_t>(-1);
 }
 
 size_t parse_util_get_offset(const wcstring &str, int line, long line_offset) {

src/builtin_commandline.cpp
@@ -348,7 +348,7 @@ maybe_t<int> builtin_commandline(parser_t &parser, io_streams_t &streams, const 
 
     if (line_mode) {
         streams.out.append_format(L"%d\n",
-                                  parse_util_lineno(rstate.text.c_str(), rstate.cursor_pos));
+                                  parse_util_lineno(rstate.text, rstate.cursor_pos));
         return STATUS_CMD_OK;
     }
 

src/parse_util.h
@@ -79,7 +79,7 @@ void parse_util_token_extent(const wchar_t *buff, size_t cursor_pos, const wchar
                              const wchar_t **prev_end);
 
 /// Get the linenumber at the specified character offset.
-int parse_util_lineno(const wchar_t *str, size_t offset);
+int parse_util_lineno(const wcstring &str, size_t offset);
 
 /// Calculate the line number of the specified cursor position.
 int parse_util_get_line_from_offset(const wcstring &str, size_t pos);

src/reader.cpp
@@ -3573,7 +3573,7 @@ void reader_data_t::handle_readline_command(readline_cmd_t c, readline_loop_stat
                 else
                     line_new = line_old + 1;
 
-                int line_count = parse_util_lineno(el->text().c_str(), el->size()) - 1;
+                int line_count = parse_util_lineno(el->text(), el->size()) - 1;
 
                 if (line_new >= 0 && line_new <= line_count) {
                     auto indents = parse_util_compute_indents(el->text());

commit 3a375c239955744d70041d30d40e9307dc05b3d0

reader: fix regressions when moving between lines
Fixes some regressions from 35ca42413 ("Simplify some parse_util functions").
The tmux tests are not beautiful but I find them easy to write.
Probably a pexpect test would also be enough here?

src/parse_util.cpp
@@ -54,8 +54,8 @@ int parse_util_lineno(const wcstring &str, size_t offset) {
 
 int parse_util_get_line_from_offset(const wcstring &str, size_t pos) {
     // Return the line pos is on, or -1 if it's after the end.
-    if (str.length() > pos) return -1;
-    return std::count(str.begin() + pos, str.end(), L'\n');
+    if (pos > str.length()) return -1;
+    return std::count(str.begin(), str.begin() + pos, L'\n');
 }
 
 size_t parse_util_get_offset_from_line(const wcstring &str, int line) {
@@ -63,10 +63,11 @@ size_t parse_util_get_offset_from_line(const wcstring &str, int line) {
     if (line < 0) return static_cast<size_t>(-1);
     if (line == 0) return 0;
 
-    ssize_t i = 0;
-    while (auto pos = str.find(L'\n')) {
-        i++;
-        if (i == line) return pos + 1;
+    size_t pos = -1;
+    int count = 0;
+    while ((pos = str.find(L'\n', pos + 1)) != wcstring::npos) {
+        count++;
+        if (count == line) return pos + 1;
     }
     return static_cast<size_t>(-1);
 }
@@ -221,7 +222,6 @@ long parse_util_slice_length(const wchar_t *in) {
     assert(bracket_count > 0 && "Should have unclosed brackets");
     return -1;
 }
-
 
 int parse_util_locate_cmdsubst_range(const wcstring &str, size_t *inout_cursor_offset,
                                      wcstring *out_contents, size_t *out_start, size_t *out_end,
@@ -1112,8 +1112,9 @@ static bool detect_errors_in_decorated_statement(const wcstring &buff_src,
     const wcstring &com = dst.command.source(buff_src, storage);
     if (com == L"$status") {
         parse_error_offset_source_start(parse_errors, source_start);
-        errored = append_syntax_error(parse_errors, source_start,
-                                      _(L"$status is not valid as a command. See `help conditions`"));
+        errored =
+            append_syntax_error(parse_errors, source_start,
+                                _(L"$status is not valid as a command. See `help conditions`"));
     }
 
     const wcstring &unexp_command = dst.command.source(buff_src, storage);

tests/checks/tmux-bind.fish
@@ -1,0 +1,36 @@
+#RUN: %fish -C 'set -g fish %fish' %s
+#REQUIRES: command -v tmux
+
+set fish (builtin realpath $fish)
+
+# Isolated tmux.
+set -g tmpdir (mktemp -d)
+
+# Don't CD elsewhere, because tmux socket file is relative to CWD. Using
+# absolute path to socket file is prone to 'socket file name too long' error.
+cd $tmpdir
+
+set -g tmux tmux -S .tmux-socket -f /dev/null
+
+set -g sleep sleep .1
+set -q CI && set sleep sleep 1
+
+
+$tmux new-session -x 80 -y 10 -d $fish -C '
+    # This is similar to "tests/interactive.config".
+    function fish_greeting; end
+    function fish_prompt; printf "prompt $status_generation> "; end
+    # No autosuggestion from older history.
+    set fish_history ""
+'
+$sleep # Let fish draw a prompt.
+
+# Test moving around with up-or-search on a multi-line commandline.
+$tmux send-keys 'echo 12' M-Enter 'echo ab' C-p 345 C-n cde
+$sleep
+$tmux capture-pane -p
+# CHECK: prompt 0> echo 12345
+# CHECK: echo abcde
+
+$tmux kill-server
+rm -r $tmpdir
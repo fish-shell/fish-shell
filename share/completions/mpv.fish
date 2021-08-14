703a71766024c0efa479d579c371c284de8fd743
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-08-13 20:07:59 -0400

    completion: support `--no` prefixes for mpv flag options

--- share/completions/mpv.fish
+++ share/completions/mpv.fish
@@ -3,8 +3,14 @@ for opt in $options
     complete -c mpv -l "$opt"
 end
 
+set -l flag_options (string replace -fr '^\s*--([\w-]+).*Flag.*' '$1' -- (command mpv --list-options 2>/dev/null))
+for flag_opt in $flag_options
+    complete -c mpv -l "no-$flag_opt"
+end
+
 complete -c mpv -l start -x -d "Seek to given position (%, s, hh:mm:ss)"
 complete -c mpv -l no-audio -d "Disable audio"
+complete -c mpv -l no-audio-display -d "Hide attached picture for audio"
 complete -c mpv -l no-video -d "Disable video"
 complete -c mpv -l fs -d "Fullscreen playback"
 complete -c mpv -l sub-file -r -d "Specify subtitle file"

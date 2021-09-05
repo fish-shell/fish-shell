d74cd1de83362ef69312fe11af6aa6d5d0da87e4
Kid <44045911+kidonng@users.noreply.github.com>
2021-09-05 19:46:06 +0800

    Fix `fish_key_reader` wrapper check

--- share/functions/fish_key_reader.fish
+++ share/functions/fish_key_reader.fish
@@ -1,7 +1,7 @@
 # check if command fish_key_reader works and is the same version that
 # came with this fish. This will happen one time.
 command -sq fish_key_reader
-and command fish_key_reader --version 2>&1 | string match -q -- $version
+and command fish_key_reader --version 2>&1 | string match -rq -- $version
 # if alias doesn't define the function here, this is an autoloaded "nothing".
 # the command (if there is one) will be used by default.
 or alias fish_key_reader=(string escape $__fish_bin_dir/fish_key_reader)

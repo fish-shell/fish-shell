6e491ad45749a4e4058a4eedaa3650611c9805f2
Fabian Homborg <FHomborg@gmail.com>
2021-10-19 17:45:18 +0200

    abbr: add missing options
    Fixes #8367.

--- share/completions/abbr.fish
+++ share/completions/abbr.fish
@@ -1,7 +1,10 @@
+complete -c abbr -f
 complete -c abbr -f -s a -l add -d 'Add abbreviation'
-# Abbr keys can't contain spaces, so we can safely replace the first space with a tab
-# `abbr -s` won't work here because that already escapes
-complete -c abbr -s e -l erase -d 'Erase abbreviation' -xa '(abbr --list)'
+complete -c abbr -f -s q -l query -d 'Check if an abbreviation exists'
+complete -c abbr -f -s r -l rename -d 'Rename an abbreviation' -xa '(abbr --list)'
+complete -c abbr -f -s e -l erase -d 'Erase abbreviation' -xa '(abbr --list)'
 complete -c abbr -f -s s -l show -d 'Print all abbreviations'
 complete -c abbr -f -s l -l list -d 'Print all abbreviation names'
+complete -c abbr -f -s g -l global -d 'Store abbreviation in a global variable'
+complete -c abbr -f -s U -l universal -d 'Store abbreviation in a universal variable'
 complete -c abbr -f -s h -l help -d Help

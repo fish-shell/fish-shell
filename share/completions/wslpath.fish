be9b8829d06d6079dd151a0eba14bfcd2cfc96ee
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2021-10-23 06:22:02 +0900

    Add completions for `wslpath` (#8364)
    * add completions for `wslpath`
    
    * add changelog entry for `wslpath` completion

---
+++ share/completions/wslpath.fish
@@ -0,0 +1,4 @@
+complete -c wslpath -s a -d 'force result to absolute path format'
+complete -c wslpath -s u -d 'translate from a Windows path to a WSL path (default)'
+complete -c wslpath -s w -d 'translate from a WSL path to a Windows path'
+complete -c wslpath -s m -d 'translate from a WSL path to a Windows path, with \'/\' instead of \'\\\''

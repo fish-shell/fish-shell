c444ee2e6aaa2096ac5af33f7eabbd8127c3a3f1
Fabian Homborg <FHomborg@gmail.com>
2021-10-25 18:42:18 +0200

    benchmarks: Add globbing

---
+++ benchmarks/benchmarks/glob.fish
@@ -0,0 +1,8 @@
+# Glob fish's source directory.
+# This timing is bound to change if the repo does,
+# so it's best to build two fishes, check out one version of the repo,
+# and then run this script with both.
+set -l dir (dirname (status current-filename))
+for i in (seq 1 100)
+    echo $dir/../../**
+end

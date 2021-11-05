f910133c8256d401af7870d406f73e6325cd52d1
Aaron Gyes <var.log@icloud.com>
2021-11-04 19:54:44 -0700

    Add kmutil ArgumentParser completions
    This is another one like Shortcuts.

---
+++ share/completions/kmutil.fish
@@ -0,0 +1,11 @@
+# SYNOPSIS
+#        kmutil <subcommand>
+#        kmutil <load|unload|showloaded>
+#        kmutil <find|libraries|print-diagnostics>
+#        kmutil <create|inspect|check|log|dumpstate>
+#        kmutil <clear-staging|trigger-panic-medic>
+#        kmutil -h
+
+if [ (command -v kmutil) = /usr/bin/kmutil ]
+	command kmutil --generate-completion-script=fish | source
+end

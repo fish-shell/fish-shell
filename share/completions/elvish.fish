54a751bcaea572a79631e4fd50572cdc7cbb371a
Emily Seville <emilyseville7cf@gmail.com>
2021-11-08 02:13:16 +1000

    elvish completions

---
+++ share/completions/elvish.fish
@@ -0,0 +1,11 @@
+complete -c elvish -o help -d 'Show help and exit'
+complete -c elvish -o version -d 'Show version and exit'
+
+complete -c elvish -o buildinfo -d 'Show build info and exit'
+complete -c elvish -s c -o compileonly -d 'Compile'
+complete -c elvish -o cpuprofile -d 'Write cpu profile'
+complete -c elvish -o deprecation-level -f -d 'Warn about deprecated features'
+complete -c elvish -s i -o json -d 'Show output in JSON'
+complete -c elvish -o log -d 'Use log file'
+complete -c elvish -o norc -d 'Don\'t use startup file'
+complete -c elvish -o rc -d 'Use startup file'

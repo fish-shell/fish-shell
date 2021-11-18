e98f4a7ec6d6dcfe03a2785e6de2b9e171d01d68
Emily Grace Seville <emilyseville7cf@gmail.com>
2021-11-19 09:59:40 +1000

    Create ikdasm.fish

---
+++ share/completions/ikdasm.fish
@@ -0,0 +1,8 @@
+complete --command ikdasm --old-option help --description 'Show help and exit'
+
+complete --command ikdasm --old-option out --description 'Direct output to file rather than stdout'
+complete --command ikdasm --old-option assembly --description 'Dump the contents of the Assembly table'
+complete --command ikdasm --old-option assemblyref --description 'Dump the contents of the AssemblyRef table'
+complete --command ikdasm --old-option moduleref --description 'Dump the contents of the ModuleRef table'
+complete --command ikdasm --old-option exported --description 'Dump the contents of the ExportedType table'
+complete --command ikdasm --old-option customattr --description 'Dump the contents of the CustomAttribute table'

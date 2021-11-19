42cea408f29299a4648cd9cc3d7b241a08e4016c
Emily Grace Seville <emilyseville7cf@gmail.com>
2021-11-19 10:19:06 +1000

    Create sqlsharp.fish

---
+++ share/completions/sqlsharp.fish
@@ -0,0 +1,3 @@
+complete --command sqlsharp --short-option f --require-parameter --description 'Output file to load SQL commands from'
+complete --command sqlsharp --short-option o --require-parameter --description 'Output file to send results'
+complete --command sqlsharp --short-option s --description 'Silent mode'

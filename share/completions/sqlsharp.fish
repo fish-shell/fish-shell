65d4353bf489f920e6c7b38025702dc65f008b05
Emily Grace Seville <emilyseville7cf@gmail.com>
2021-11-20 15:17:16 +1000

    Standardize according #8463 issue

--- share/completions/sqlsharp.fish
+++ share/completions/sqlsharp.fish
@@ -1,3 +1,3 @@
-complete --command sqlsharp --short-option f --require-parameter --description 'Output file to load SQL commands from'
-complete --command sqlsharp --short-option o --require-parameter --description 'Output file to send results'
-complete --command sqlsharp --short-option s --description 'Silent mode'
+complete --command sqlsharp --short-option f --require-parameter --description 'Read SQL commands from file'
+complete --command sqlsharp --short-option o --require-parameter --description 'Redirect stdout to file'
+complete --command sqlsharp --short-option s --description 'Use silent mode'

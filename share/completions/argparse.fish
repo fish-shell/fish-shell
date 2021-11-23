df7dc556d26d672edc1a12576145a547ccb55724
Emily Grace Seville <emilyseville7cf@gmail.com>
2021-11-23 10:39:27 +1000

    Standardize completion

--- share/completions/argparse.fish
+++ share/completions/argparse.fish
@@ -2,11 +2,11 @@ set --local CONDITION '! __fish_seen_argument --short r --long required-val --sh
 
 complete --command argparse -f
 
-complete --command argparse --short-option h --long-option help --description 'Show help and exit'
+complete --command argparse --short-option h --long-option help --description 'Show help'
 
 complete --command argparse --short-option n --long-option name --description 'Use function name'
 complete --command argparse --short-option x --long-option exclusive --description 'Specify mutually exclusive options'
-complete --command argparse --short-option N --long-option min-args --description 'Specify minimum non-option arg count'
-complete --command argparse --short-option X --long-option max-args --description 'Specify maximum non-option arg count'
+complete --command argparse --short-option N --long-option min-args --description 'Specify minimum non-option argument count'
+complete --command argparse --short-option X --long-option max-args --description 'Specify maximum non-option argument count'
 complete --command argparse --short-option i --long-option ignore-unknown --description 'Ignore unknown options'
 complete --command argparse --short-option s --long-option stop-nonopt --description 'Exit on subcommand'

a625d93051d7d655d2519e5480d1ef640ba1d931
Emily Grace Seville <emilyseville7cf@gmail.com>
2021-11-23 10:37:07 +1000

    Add  --require-parameter

--- share/completions/fish_opt.fish
+++ share/completions/fish_opt.fish
@@ -4,8 +4,8 @@ complete --command fish_opt --no-files
 
 complete --command fish_opt --short-option h --long-option help --description 'Show help'
 
-complete --command fish_opt --short-option s --long-option short --description 'Specify short option'
-complete --command fish_opt --short-option l --long-option long --description 'Specify long option'
+complete --command fish_opt --short-option s --long-option short --require-parameter --description 'Specify short option'
+complete --command fish_opt --short-option l --long-option long --require-parameter --description 'Specify long option'
 complete --command fish_opt --long-option longonly --description 'Use only long option'
 complete --command fish_opt --short-option o --long-option optional-val -n $CONDITION --description 'Don\'t require value'
 complete --command fish_opt --short-option r --long-option required-val -n $CONDITION  --description 'Require value'

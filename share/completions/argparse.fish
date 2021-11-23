5755635e46e2f62d971de380700cb6f690090ff8
Emily Grace Seville <emilyseville7cf@gmail.com>
2021-11-23 13:30:14 +1000

    Completions for --exclusive

--- share/completions/argparse.fish
+++ share/completions/argparse.fish
@@ -1,3 +1,44 @@
+function __argparse_find_option_specs --description 'Internal function to find all option specs'
+  set --local delimiter ','
+  set --local cmd (commandline --tokenize)
+  set --erase cmd[1]
+
+  if set index (contains --index -- '--' $cmd)
+    set index (math $index - 1)
+  else
+    set index (count $cmd)
+  end
+
+  set --local remove_last false
+  set --local specs
+  while test $index -gt 0; and not string match --quiet -- '-*' $cmd[$index]
+    if test $index -gt 1; and string match --regex --quiet -- '^(-x|--exclusive)$' $cmd[(math $index - 1)]
+      set remove_last true
+    end
+
+    if string match --quiet '*=*' $cmd[$index]
+      set cmd[$index] (string replace --regex '=.*' '' $cmd[$index])
+    else if string match --quiet '*=\?*' $cmd[$index]
+      set cmd[$index] (string replace --regex '=\?.*' '' $cmd[$index])
+    else if string match --quiet '*=+*' $cmd[$index]
+      set cmd[$index] (string replace --regex '=\+*' '' $cmd[$index])
+    end
+
+    if string match --quiet '*/*' $cmd[$index]
+      set --append specs (string split '/' $cmd[$index])
+    else
+      set --append specs $cmd[$index]
+    end
+    set index (math $index - 1)
+  end
+
+  if test $remove_last = true
+    set --erase specs[(count $specs)]
+  end
+
+  echo -n $specs | string replace --regex --all ' ' '\n'
+end
+
 set --local CONDITION '! __fish_seen_argument --short r --long required-val --short o --long optional-val'
 
 complete --command argparse --no-files
@@ -5,7 +46,7 @@ complete --command argparse --no-files
 complete --command argparse --short-option h --long-option help --description 'Show help'
 
 complete --command argparse --short-option n --long-option name --require-parameter --arguments '(functions --all | string replace ", " "\n")' --description 'Use function name'
-complete --command argparse --short-option x --long-option exclusive --description 'Specify mutually exclusive options'
+complete --command argparse --short-option x --long-option exclusive --no-files --require-parameter --arguments '(__fish_append "," (__argparse_find_option_specs))' --description 'Specify mutually exclusive options'
 complete --command argparse --short-option N --long-option min-args --description 'Specify minimum non-option argument count'
 complete --command argparse --short-option X --long-option max-args --description 'Specify maximum non-option argument count'
 complete --command argparse --short-option i --long-option ignore-unknown --description 'Ignore unknown options'

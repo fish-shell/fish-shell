a3d0f4f4a3eb4073251537143ae29f9868b3fe7e
Emily Grace Seville <emilyseville7cf@gmail.com>
2021-11-23 10:51:27 +1000

    Show function names for --name

--- share/completions/argparse.fish
+++ share/completions/argparse.fish
@@ -4,7 +4,7 @@ complete --command argparse --no-files
 
 complete --command argparse --short-option h --long-option help --description 'Show help'
 
-complete --command argparse --short-option n --long-option name --description 'Use function name'
+complete --command argparse --short-option n --long-option name --require-parameter --arguments '(functions --all | string replace ", " "\n")' --description 'Use function name'
 complete --command argparse --short-option x --long-option exclusive --description 'Specify mutually exclusive options'
 complete --command argparse --short-option N --long-option min-args --description 'Specify minimum non-option argument count'
 complete --command argparse --short-option X --long-option max-args --description 'Specify maximum non-option argument count'

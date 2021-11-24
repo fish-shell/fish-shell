fc9e9e77b46d96346e986a7e568e95a45ba0e056
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-11-24 18:47:50 +1000

    Debug values support

--- share/completions/pabcnetcclear.fish
+++ share/completions/pabcnetcclear.fish
@@ -1,6 +1,17 @@
-complete --command pabcnetcclear --arguments "/Help\t'Display help' \
-  /H\t'Display help' \
-  /\?\t'Display help' \
-  /Debug\t'Generate code with optimizations' \
-  /output\t'Executable name' \
-  /SearchDir\t'Add path to unit search directories'"
+function __pabcnetcclear_generate_args --description 'Function to generate args'
+  set --local current_token (commandline --current-token --cut-at-cursor)
+  switch $current_token
+    case '/Debug:*'
+      echo -e $current_token"0\tEnable generation
+$current_token"1"\tDisable generation"
+    case '*'
+      echo -e "/Help\tDisplay help
+/H\tDisplay help
+/?\tDisplay help
+/Debug\tGenerate code with optimizations
+/output\tExecutable name
+/SearchDir\tAdd path to unit search directories"
+  end
+end
+
+complete --command pabcnetcclear --no-files --arguments "(__pabcnetcclear_generate_args)"

f3ebc23c6df74c8ca35ee937bd09e83ee1bcdf3e
DevAtDawn <86164137+DevAtDawn@users.noreply.github.com>
2021-11-25 13:56:49 -0700

    Update vi_increment_decrement.fish

--- share/functions/vi_increment_decrement.fish
+++ share/functions/vi_increment_decrement.fish
@@ -1,34 +1,30 @@
-alias vi_dec 'vi_increment_decrement subtract'
-alias vi_inc 'vi_increment_decrement add'
+function change_number
+    set -l cursor_position (commandline -C)
+    set -l current_buffer (commandline -b)
+    set -l num 0
+    set -l split_str (string split '' $current_buffer)
+    set -l the_num (math $cursor_position + 1)
+    set -l line_char $split_str[$the_num]
+    set -l new_string ""
+    
+    string match -qr '^[0-9]+$' $line_char
+    or return
 
-function vi_increment_decrement --description 'increment or decrement numbers with ctrl+x ctrl+a'
-    set lnum (commandline --cursor)
-    set lstr (commandline -b)
-    set num 0
-    set split_str (string split '' $lstr)
-    set the_num (math $lnum + 1)
-    set lchar $split_str[$the_num]
-    set new_string ""
-
-    if string match -qr '^[0-9]+$' $lchar
-        if [ $argv = 'add' ]
-            set new_num (math $lchar + 1)
-        else if [ $argv = 'subtract' ]
-            set new_num (math $lchar - 1)
-        end
-
-        for x in (string split '' $lstr)
-            if [ $num = $lnum ]
-                set new_string (echo $new_string$new_num | string collect)
-            else
-                set new_string (echo $new_string$x | string collect)
-            end
-            set num (math $num + 1)
-        end
-        commandline -r $new_string
-        commandline -C $lnum
-        commandline -f repaint
-    else
-        return
+    if [ $argv = 'increase' ]
+        set new_num (math $line_char + 1)
+    else if [ $argv = 'decrease' ]
+        set new_num (math $line_char - 1)
     end
+
+    for x in (string split '' $current_buffer)
+        if [ $num = $cursor_position ]
+            set new_string (echo $new_string$new_num | string collect)
+        else
+            set new_string (echo $new_string$x | string collect)
+        end
+        set num (math $num + 1)
+    end
+    commandline -r $new_string
+    commandline -C $cursor_position
+    commandline -f repaint
 end

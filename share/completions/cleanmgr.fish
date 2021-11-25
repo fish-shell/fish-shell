555166425b5b1163aba3c36a711c297382ba700e
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-11-25 14:28:04 +1000

    Completion for cleanmgr command

---
+++ share/completions/cleanmgr.fish
@@ -0,0 +1,7 @@
+complete --command cleanmgr --no-files --arguments '/d\t"Use drive to clean-up"
+/sageset\t"Show the Disk Cleanup Settings dialog box and also create a registry key to store the settings that you select"
+/sagerun\t"Run the specified tasks that are assigned to the n value if you use the /sageset option"
+/tuneup\t"Run /sageset and /sagerun for the same n"
+/lowdisk\t"Use default settings"
+/verylowdisk\t"Use default settings and hide all prompts"
+/?\t"Show help"'

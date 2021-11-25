94625dddc9dada8917993a4279b81c3070d3587e
DevAtDawn <86164137+DevAtDawn@users.noreply.github.com>
2021-11-25 14:47:02 -0700

    Update and rename vi_increment_decrement.fish to change_number.fish

--- share/functions/change_number.fish
+++ share/functions/change_number.fish
@@ -1,28 +1,28 @@
 function change_number
     set -l cursor_position (commandline -C)
     set -l current_buffer (commandline -b)
-    set -l num 0
+    set -l count 0
     set -l split_str (string split '' $current_buffer)
-    set -l the_num (math $cursor_position + 1)
-    set -l line_char $split_str[$the_num]
-    set -l new_string ""
+    set -l char_position (math $cursor_position + 1)
+    set -l buffer_char $split_str[$char_position]
+    set new_string ""
     
-    string match -qr '^[0-9]+$' $line_char
+    string match -qr '^[0-9]+$' $buffer_char
     or return
 
     if test "$argv[1]" = increase
-        set new_num (math $line_char + 1)
+        set new_num (math $buffer_char + 1)
     else if test "$argv[1]" = decrease
-        set new_num (math $line_char - 1)
+        set new_num (math $buffer_char - 1)
     end
 
     for x in (string split '' $current_buffer)
-        if [ $num = $cursor_position ]
-            set -l new_string "$new_string$new_num"
+        if [ $count = $cursor_position ]
+            set new_string $new_string$new_num
         else
-            set -l new_string "$new_string$x"
+            set new_string $new_string$x
         end
-        set num (math $num + 1)
+        set count (math $count + 1)
     end
     
     commandline -r $new_string

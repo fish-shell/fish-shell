2bd7daec20d0054ff70dd3d287f6a41990002947
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-11-25 17:36:03 +1000

    Completion for copy command

---
+++ share/completions/copy.fish
@@ -0,0 +1,16 @@
+function __copy_generate_args --description 'Function to generate args'
+  if not __fish_seen_argument --windows 'a' --windows 'b'
+    echo -e '/a\tIndicate an ASCII text file
+/b\tIndicate a binary file'
+  end
+
+  echo -e '/d\tAllow the encrypted files being copied to be saved as decrypted files at the destination
+/v\tVerify that new files are written correctly
+/n\tUses a short file name, if available
+/y=\tHide prompts to confirm that you want to overwrite an existing destination file
+/-y\tShow prompts to confirm that you want to overwrite an existing destination file
+/z\tCopy networked files in restartable mode
+/?\tShow help'
+end
+
+complete --command copy --no-files --arguments '(__copy_generate_args)'

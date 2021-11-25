63165085e18e59155e2cee5d8cffad17b51033df
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-11-25 14:14:17 +1000

    Completion for choice command

---
+++ share/completions/choice.fish
@@ -0,0 +1,7 @@
+complete --command choice --no-files --arguments '/c\t"Specify the list of choices to be created"
+/n\t"Hide the list of choices"
+/cs\t"Respect case"
+/t\t"Specify the number of seconds to pause before using the default choice"
+/d\t"Use as the default choice"
+/m\t"Specify a message to display before the list of choices"
+/?\t"Show help"'

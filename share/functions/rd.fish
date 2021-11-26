c5a84c30ada3f5faa4f3be2a6b25933f78fa1b85
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-11-26 19:29:54 +1000

    Clean-up md/rd completions: - use __fish_list_windows_drives - hide dependent options

--- share/functions/rd.fish
+++ share/functions/rd.fish
@@ -2,11 +2,14 @@ function __rd_generate_args --description 'Function to generate args'
   set --local current_token (commandline --current-token --cut-at-cursor)
   switch $current_token
     case '/*'
+      if __fish_seen_argument --windows 's'
+        echo -e '/q\tSpecify quiet mode'
+      end
+
       echo -e '/s\tDelete a directory tree
-/q\tSpecify quiet mode
 /?\tShow help'
     case '*'
-      wmic logicaldisk get name | tail --lines +2
+      __fish_list_windows_drives
   end
 end
 

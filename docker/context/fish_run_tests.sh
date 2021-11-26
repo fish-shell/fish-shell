15ac74d233c42f43ea593b1a31059edd68f0e7a8
ridiculousfish <rf@fishshell.com>
2021-11-26 13:13:08 -0800

    Use new fish_run_tests target in Dockerfiles
    This fixes the Xenial Dockerfile and perhaps others.

--- docker/context/fish_run_tests.sh
+++ docker/context/fish_run_tests.sh
@@ -12,7 +12,7 @@ then
     bash -i || exit
 fi
 
-ninja && ninja test
+ninja && ninja fish_run_tests
 
 # Drop the user into a shell if FISH_RUN_SHELL_AFTER_TESTS is set.
 test -n "$FISH_RUN_SHELL_AFTER_TESTS" && exec bash -i

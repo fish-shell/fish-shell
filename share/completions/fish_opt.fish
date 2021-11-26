f87395f5e96403e6978b614902c57a4ec6460fdb
Emily Grace Seville <emilyseville7cf@gmail.com>
2021-11-27 04:26:35 +1000

    fish_opt completion (#8433)
    * Basic fish_opt completion
    
    * Condition for -o and -r fish_opt options
    
    * Change -g to --local
    
    * Enhance readability
    
    * Add  --require-parameter
    
    * Add --no-files for -s|-l options

---
+++ share/completions/fish_opt.fish
@@ -0,0 +1,12 @@
+set --local CONDITION '! __fish_seen_argument --short r --long required-val --short o --long optional-val'
+
+complete --command fish_opt --no-files
+
+complete --command fish_opt --short-option h --long-option help --description 'Show help'
+
+complete --command fish_opt --short-option s --long-option short --no-files --require-parameter --description 'Specify short option'
+complete --command fish_opt --short-option l --long-option long --no-files --require-parameter --description 'Specify long option'
+complete --command fish_opt --long-option longonly --description 'Use only long option'
+complete --command fish_opt --short-option o --long-option optional-val -n $CONDITION --description 'Don\'t require value'
+complete --command fish_opt --short-option r --long-option required-val -n $CONDITION  --description 'Require value'
+complete --command fish_opt --long-option multiple-vals --description 'Store all values'

2683dc4af85b1805eda76d687d5c380260f86a97
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-11-26 18:50:41 +1000

    Clean-up vol completion: - use __fish_list_windows_drives - reformat

--- share/completions/vol.fish
+++ share/completions/vol.fish
@@ -1,10 +1,11 @@
 function __vol_generate_args --description 'Function to generate args'
   set --local current_token (commandline --current-token --cut-at-cursor)
+  
   switch $current_token
     case '/*'
       echo -e '/?\tShow help'
     case '*'
-      wmic logicaldisk get name | tail --lines +2
+      __fish_list_windows_drives
   end
 end
 

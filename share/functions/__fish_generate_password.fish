02c12de5c6cacafa566c7b3243dabef66e61470d
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-11-26 10:00:31 +1000

    Generate shorter passwords

--- share/functions/__fish_generate_password.fish
+++ share/functions/__fish_generate_password.fish
@@ -1,4 +1,4 @@
 function __fish_generate_password --description 'Generate password'
-  date +%s | sha256sum | base64 | head --bytes 32
+  date +%s | sha256sum | base64 | head --bytes 10
   echo
 end

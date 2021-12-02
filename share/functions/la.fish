75d16c13ea4aae0dcfba77e7f6f7465e29fbaa56
Kid <44045911+kidonng@users.noreply.github.com>
2021-12-02 17:32:53 +0800

    Use `--almost-all` in `la` function

--- share/functions/la.fish
+++ share/functions/la.fish
@@ -2,5 +2,5 @@
 # These are very common and useful
 #
 function la --wraps ls --description "List contents of directory, including hidden files in directory using long format"
-    ls -lah $argv
+    ls -lAh $argv
 end

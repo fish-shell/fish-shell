7dbf1abc097c4819fa5eb8311e9fb8cf1e6a1e21
Ilan Cosman <ilancosman@gmail.com>
2021-12-04 18:52:10 -0800

    Speed up fish_status_to_signal

--- share/functions/fish_status_to_signal.fish
+++ share/functions/fish_status_to_signal.fish
@@ -1,13 +1,13 @@
 function fish_status_to_signal --description "Print signal name from argument (\$status), or just argument"
     for arg in $argv
         if test $arg -gt 128
-            set -l signals SIGHUP SIGINT SIGQUIT SIGILL SIGTRAP SIGABRT SIGBUS \
-                SIGFPE SIGKILL SIGUSR1 SIGSEGV SIGUSR2 SIGPIPE SIGALRM \
-                SIGTERM SIGSTKFLT SIGCHLD SIGCONT SIGSTOP SIGTSTP \
-                SIGTTIN SIGTTOU SIGURG SIGXCPU SIGXFSZ SIGVTALRM \
-                SIGPROF SIGWINCH SIGIO SIGPWR SIGSYS
             set -l sigix (math $arg - 128)
-            if test $sigix -le (count $signals)
+            if test $sigix -le 31 # 31 = number of named signals below
+                set -l signals SIGHUP SIGINT SIGQUIT SIGILL SIGTRAP SIGABRT SIGBUS \
+                    SIGFPE SIGKILL SIGUSR1 SIGSEGV SIGUSR2 SIGPIPE SIGALRM \
+                    SIGTERM SIGSTKFLT SIGCHLD SIGCONT SIGSTOP SIGTSTP \
+                    SIGTTIN SIGTTOU SIGURG SIGXCPU SIGXFSZ SIGVTALRM \
+                    SIGPROF SIGWINCH SIGIO SIGPWR SIGSYS
                 echo $signals[$sigix]
             else
                 echo $arg

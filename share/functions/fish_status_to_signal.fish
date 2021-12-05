f957b297d68e79448eeb609fc9560ec7d5454d8d
Ilan Cosman <ilancosman@gmail.com>
2021-12-05 08:11:17 -0800

    Move SIG from every member of list to the echo

--- share/functions/fish_status_to_signal.fish
+++ share/functions/fish_status_to_signal.fish
@@ -1,14 +1,12 @@
 function fish_status_to_signal --description "Print signal name from argument (\$status), or just argument"
     for arg in $argv
         if test $arg -gt 128
-            set -l sigix (math $arg - 128)
+            set -l sigix (math $arg-128)
             if test $sigix -le 31 # 31 = number of named signals below
-                set -l signals SIGHUP SIGINT SIGQUIT SIGILL SIGTRAP SIGABRT SIGBUS \
-                    SIGFPE SIGKILL SIGUSR1 SIGSEGV SIGUSR2 SIGPIPE SIGALRM \
-                    SIGTERM SIGSTKFLT SIGCHLD SIGCONT SIGSTOP SIGTSTP \
-                    SIGTTIN SIGTTOU SIGURG SIGXCPU SIGXFSZ SIGVTALRM \
-                    SIGPROF SIGWINCH SIGIO SIGPWR SIGSYS
-                echo $signals[$sigix]
+                set -l signals HUP INT QUIT ILL TRAP ABRT BUS FPE KILL USR1 \
+                    SEGV USR2 PIPE ALRM TERM STKFLT CHLD CONT STOP TSTP \
+                    TTIN TTOU URG XCPU XFSZ VTALRM PROF WINCH IO PWR SYS
+                echo SIG$signals[$sigix]
             else
                 echo $arg
             end

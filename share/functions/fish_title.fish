93a95e4d0ced9b68d1ce7ef97e40d40a7dae146f
Emily Grace Seville <emilyseville7cf@gmail.com>
2021-12-08 18:54:10 +1000

    Join several lines in fish_title

--- share/functions/fish_title.fish
+++ share/functions/fish_title.fish
@@ -3,8 +3,6 @@ function fish_title --argument-names command_override --desription 'Sets fish ti
     if not set --query INSIDE_EMACS; or string match --quiet --invert '*,term:*' -- $INSIDE_EMACS
         # An override for the current command is passed as the first parameter.
         # This is used by `fg` to show the true process name, among others.
-        echo (set --query $command_override
-            and echo $command_override
-            or status current-command) (__fish_pwd)
+        echo (set --query $command_override; and echo $command_override; or status current-command) (__fish_pwd)
     end
 end

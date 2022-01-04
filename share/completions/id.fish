271a883210ef4fd79fc34dd84fbc139e7f02a760
exploide <me@exploide.net>
2022-01-04 17:29:06 +0100

    completions: complete users for id command and added two further options

--- share/completions/id.fish
+++ share/completions/id.fish
@@ -1,3 +1,10 @@
+complete -c id -xa "(__fish_complete_users)"
+
+if string match -eq 'GNU coreutils' (id --version 2>&1)
+    complete -c id -s Z -l context -d "Print security context"
+    complete -c id -s z -l zero -d "Delimit entries with NUL"
+end
+
 complete -c id -s g -l group -d "Print effective group id"
 complete -c id -s G -l groups -d "Print all group ids"
 complete -c id -s n -l name -d "Print name, not number"

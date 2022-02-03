0afb9b570933c7fea7b12059aa29abb7c7290339
Fabian Homborg <FHomborg@gmail.com>
2022-02-03 13:52:59 +0100

    fish_title: Shorten command to 20 characters
    Otherwise this makes massive terminal tabs

--- share/functions/fish_title.fish
+++ share/functions/fish_title.fish
@@ -4,14 +4,14 @@ function fish_title
         # An override for the current command is passed as the first parameter.
         # This is used by `fg` to show the true process name, among others.
         if set -q argv[1]
-            echo -- $argv[1] (prompt_pwd -d 1 -D 0)
+            echo -- (string sub -l 20 -- $argv[1]) (prompt_pwd -d 1 -D 0)
         else
             # Don't print "fish" because it's redundant
             set -l command (status current-command)
             if test "$command" = fish
                 set command
             end
-            echo -- $command (prompt_pwd -d 1 -D 1)
+            echo -- (string sub -l 20 -- $command) (prompt_pwd -d 1 -D 1)
         end
     end
 end

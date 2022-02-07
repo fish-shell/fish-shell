99e0aa3c6494ec14278cc35bce1c4cecb686cdaa
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2022-02-07 12:56:27 -0600

    Clarify fish_is_nth_token description
    Yes, this does make it longer but it's a very important point.

--- share/functions/fish_is_nth_token.fish
+++ share/functions/fish_is_nth_token.fish
@@ -1,4 +1,4 @@
-function fish_is_nth_token --description 'Test if current token is on Nth place' --argument-names n
+function fish_is_nth_token --description 'Test if current token is the Nth (ignoring command and switches/flags)' --argument-names n
     set -l tokens (commandline -poc)
     set -l tokens (string replace -r --filter '^([^-].*)' '$1' -- $tokens)
     test (count $tokens) -eq "$n"

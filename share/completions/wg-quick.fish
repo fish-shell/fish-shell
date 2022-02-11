794d347ab97801874268b2b5d0a925dc89e5356e
Jason Nader <jason.nader@protonmail.com>
2022-02-11 20:46:15 +0900

    fixup

--- share/completions/wg-quick.fish
+++ share/completions/wg-quick.fish
@@ -1,18 +1,12 @@
 set -l valid_subcmds up down strip save
 
-function __wg_complete_interfaces
-    wg show 2>| string replace -rf 'Unable to access interface (.*): Operation not permitted' '$1'
-end
-
-function __wg_subcmd --no-scope-shadowing
-    set -l subcmd $argv[1]
-    set -e argv[1]
-    complete -f -c wg-quick -n "not __fish_seen_subcommand_from $valid_subcmds" -a $subcmd $argv
+function __fish_wg_complete_interfaces
+    LC_ALL=C wg show 2>| string replace -rf 'Unable to access interface (.*): Operation not permitted' '$1'
 end
 
 complete -c wg-quick -f
-complete -k -c wg-quick -n "__fish_seen_subcommand_from $valid_subcmds" -a '(__wg_complete_interfaces | sort)'
-__wg_subcmd up -d 'Add and set up an interface'
-__wg_subcmd down -d 'Tear down and remove an interface' 
-__wg_subcmd strip -d 'Output config for use with wg'
-__wg_subcmd save -d 'Saves the configuration of an existing interface'
+complete -c wg-quick -n "__fish_seen_subcommand_from $valid_subcmds" -a '(__fish_wg_complete_interfaces)'
+complete -c wg-quick -n "not __fish_seen_subcommand_from $valid_subcmds" -a up -d 'Add and set up an interface'
+complete -c wg-quick -n "not __fish_seen_subcommand_from $valid_subcmds" -a down -d 'Tear down and remove an interface'
+complete -c wg-quick -n "not __fish_seen_subcommand_from $valid_subcmds" -a strip -d 'Output config for use with wg'
+complete -c wg-quick -n "not __fish_seen_subcommand_from $valid_subcmds" -a save -d 'Saves the configuration of an existing interface'

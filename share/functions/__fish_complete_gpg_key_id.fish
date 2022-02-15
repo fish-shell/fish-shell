5cf0f9862585bb54fa5049f91bbfa87d9a599877
Spenser Black <spenserblack01@gmail.com>
2022-02-15 14:59:48 -0500

    fixup! List only secret keys for `gpg --try-secret-key`

--- share/functions/__fish_complete_gpg_key_id.fish
+++ share/functions/__fish_complete_gpg_key_id.fish
@@ -11,7 +11,8 @@ function __fish_complete_gpg_key_id -d 'Complete using gpg key ids' -a __fish_co
                 set -l __uid (string split ":" -- $garbage)
                 set -l uid (string replace -a '\x3a' ':' -- $__uid[10])
                 printf "%s\t%s\n" $keyid $uid
-            case "pub*"
+            # NOTE key is preceded by "sec" instead of "pub" when listing secret keys
+            case "pub*" "sec*"
                 # Extract key fingerprints (no subkeys)
                 set -l __pub (string split ":" -- $garbage)
                 set keyid $__pub[5]

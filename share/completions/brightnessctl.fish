17a43503207b4a4fe10c30638452b6339aca426c
Missu <mizzunet@protonmail.com>
2022-03-06 03:40:57 +0000

    Update brightnessctl.fish

--- share/completions/brightnessctl.fish
+++ share/completions/brightnessctl.fish
@@ -1,3 +1,3 @@
 complete -f brightnessctl
-complete -k -c brightnessctl -n "test (__fish_number_of_cmd_args_wo_opts) = 1" \
+complete -c brightnessctl -n "test (__fish_number_of_cmd_args_wo_opts) = 1" \
     -xa "info get set max"

06b131b8541723bf6b77aeae6c79a90194c0ff02
Radu Potop <radu@wooptoo.com>
2022-03-10 21:34:57 +0000

    Added initial commit for the Starfish prompt theme

---
+++ share/tools/web_config/sample_prompts/starfish.fish
@@ -0,0 +1,34 @@
+# vim: filetype=sh
+# name: Starfish
+# author: Radu Potop
+# description: A prompt theme simiar to Starship.rs but without the emojis.
+
+function fish_prompt --description 'Write out the prompt'
+    set -l last_status $status
+    set -l normal (set_color normal)
+    set -l status_color (set_color brgreen)
+    set -l cwd_color (set_color brcyan)
+    set -l vcs_color (set_color brpurple)
+    set -l prompt_status ""
+
+    # Since we display the prompt on a new line allow the directory names to be longer.
+    set -g fish_prompt_pwd_dir_length 10
+
+    # Color the prompt differently when we're root
+    set -l suffix '❯'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        end
+        set suffix '#'
+    end
+
+    # Color the prompt in red on error
+    if test $last_status -ne 0
+        set status_color (set_color $fish_color_error)
+        set prompt_status $status_color "[" $last_status "]" $normal
+    end
+
+    echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+    echo -n -s $status_color $suffix ' ' $normal
+end

d2225e5ce79f4387249fc2a6c5c28b48aa155a6c
aj <aj@codetaco.xyz>
2022-03-15 14:01:14 -0400

    remove heroku-two-factor as it is deprecated per: https://github.com/heroku/heroku-two-factor
    update complete $heroku_looking -xa git:clone to make it as short as possible without losing meaning.

--- share/completions/heroku.fish
+++ share/completions/heroku.fish
@@ -78,11 +78,10 @@ complete $heroku_looking -xa plugins -d 'manage plugins to the heroku gem'
 complete $heroku_looking -xa regions -d 'list available regions'
 complete $heroku_looking -xa stack -d 'manage the stack for an app'
 complete $heroku_looking -xa status -d 'check status of heroku platform'
-complete $heroku_looking -xa twofactor
 complete $heroku_looking -xa update -d 'update the heroku client'
 complete $heroku_looking -xa version -d 'display version'
 
-complete $heroku_looking -xa git:clone -d "APP DIRECTORY clones a heroku app to your local machine at DIRECTORY (defaults to app name)"
+complete $heroku_looking -xa git:clone -d "clones heroku application to machine at DIRECTORY. defaults to app name."
 complete $heroku_looking -xa git:remote -d "adds a git remote to an app repo (-a APP)"
 
 # Addons subcommands

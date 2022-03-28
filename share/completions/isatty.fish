e4c68f2239842cf92b7ef781b5baed472685b2a9
Kid <44045911+kidonng@users.noreply.github.com>
2022-03-29 02:25:50 +0800

    Complete `/dev/fd` in `isatty`

--- share/completions/isatty.fish
+++ share/completions/isatty.fish
@@ -1,1 +1,5 @@
-complete -c isatty -x -k -a "stdin stdout stderr"
+complete -c isatty -x 
+complete -c isatty -k -a "(string replace /dev/fd/ '' /dev/fd/*)"
+complete -c isatty -k -a "stderr" -d "2"
+complete -c isatty -k -a "stdout" -d "1"
+complete -c isatty -k -a "stdin" -d "0"

90d52ee6693007c23f5fa8bded39fe20ff5ffd8c
Kid <44045911+kidonng@users.noreply.github.com>
2022-03-31 00:29:59 +0800

    Complete `/dev/fd` in `isatty` (#8840)
    * Complete `/dev/fd` in `isatty`
    
    * Check `/dev/fd` existence first

--- share/completions/isatty.fish
+++ share/completions/isatty.fish
@@ -1,1 +1,9 @@
-complete -c isatty -x -k -a "stdin stdout stderr"
+complete -c isatty -x
+
+if test -d /dev/fd
+    complete -c isatty -k -a "(string replace /dev/fd/ '' /dev/fd/*)"
+end
+
+complete -c isatty -k -a "stderr" -d "2"
+complete -c isatty -k -a "stdout" -d "1"
+complete -c isatty -k -a "stdin" -d "0"

fish-shell/fish-shell

commit fa2450db30e5a92d3667793ff06235226b0c8f6e

vared: avoid using local variables
The tmp and prompt variables collide with variables used as arguments.
Just avoid them entirely, at the cost of making the internals of the
functions somewhat more complicated.

Closes #8836.

share/functions/vared.fish
@@ -17,25 +17,18 @@ function vared --description "Edit variable value"
 
             case '*'
                 if test (count $$argv ) -lt 2
-                    set -l init ''
-                    if test -n "$$argv"
-                        set init $$argv
-                    end
-                    set -l prompt 'set_color green; echo '$argv'; set_color normal; echo "> "'
-                    if read -p $prompt -c $init tmp
+                    # Avoid using any local variables in this function, otherwise they can't be edited
+                    # https://github.com/fish-shell/fish-shell/issues/8836
 
-                        # If variable already exists, do not add any
-                        # switches, so we don't change export rules. But
-                        # if it does not exist, we make the variable
-                        # global, so that it will not die when this
-                        # function dies
+                    # The command substitution in this line controls the scope.
+                    # If variable already exists, do not add any switches, so we don't change
+                    # scoping or export rules. But if it does not exist, we make the variable
+                    # global, so that it will not die when this function dies.
 
-                        if test -n "$$argv"
-                            set $argv $tmp
-                        else
-                            set -g $argv $tmp
-                        end
-                    end
+                    read -p 'set_color green; echo '$argv'; set_color normal; echo "> "' \
+                        (if not set -q $argv; echo -g; end) \
+                        -c "$$argv" \
+                        $argv
                 else
                     printf (_ '%s: %s is an array variable. Use %svared%s %s[n]%s to edit the n:th element of %s\n') vared $argv (set_color $fish_color_command; echo) (set_color $fish_color_normal; echo) $argv (set_color normal; echo) $argv
                 end

commit 1bba97984b80c79710c2cf039facafe639076c0a

Fix vared of indexed value
You can use an index with vared, like `vared PATH[4]`. However this was
inadverently broken in fa2450db30e5, because you cannot use `read` to
modify an element of a variable, only the whole variable. Fix this.

Unfortunately this means using another local variable, so we name it
__fish_vared_temp_value instead of just temp so that collisions are
unlikely.

share/functions/vared.fish
@@ -17,8 +17,10 @@ function vared --description "Edit variable value"
 
             case '*'
                 if test (count $$argv ) -lt 2
-                    # Avoid using any local variables in this function, otherwise they can't be edited
+                    # Try to avoid using local variables in this function, otherwise they can't be edited.
                     # https://github.com/fish-shell/fish-shell/issues/8836
+                    # However we need to use one local, as you can't read directly into an index (like vared PATH[4]).
+                    set -l __fish_vared_temp_value
 
                     # The command substitution in this line controls the scope.
                     # If variable already exists, do not add any switches, so we don't change
@@ -28,7 +30,12 @@ function vared --description "Edit variable value"
                     read -p 'set_color green; echo '$argv'; set_color normal; echo "> "' \
                         (if not set -q $argv; echo -g; end) \
                         -c "$$argv" \
-                        $argv
+                        __fish_vared_temp_value
+                    if test -n "$$argv"
+                        set $argv $__fish_vared_temp_value
+                    else
+                        set -g $argv $__fish_vared_temp_value
+                    end
                 else
                     printf (_ '%s: %s is an array variable. Use %svared%s %s[n]%s to edit the n:th element of %s\n') vared $argv (set_color $fish_color_command; echo) (set_color $fish_color_normal; echo) $argv (set_color normal; echo) $argv >&2
                 end
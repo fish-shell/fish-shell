dc6d33191de9c82b1f0c258b9ea66b29eb48193a
NextAlone <12210746+NextAlone@users.noreply.github.com>
2022-04-24 08:43:35 +0800

    feat: add btrfs to known filesystems

--- share/functions/__fish_print_filesystems.fish
+++ share/functions/__fish_print_filesystems.fish
@@ -1,5 +1,5 @@
 function __fish_print_filesystems -d "Print a list of all known filesystem types"
-    set -l fs adfs affs autofs coda coherent cramfs devpts efs ext ext2 ext3 ffs
+    set -l fs adfs affs autofs btrfs coda coherent cramfs devpts efs ext ext2 ext3 ffs
     set -a fs hfs hpfs iso9660 jfs minix msdos ncpfs nfs ntfs proc qnx4 ramfs
     set -a fs reiserfs romfs smbfs sysv tmpfs udf ufs umsdos vfat xenix xfs xiafs
     # Mount has helper binaries to mount filesystems

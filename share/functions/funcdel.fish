0b4104c859f1bf2f6b2506fedd2f8722c8be77c7
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-23 20:54:32 -0500

    use fish_function_path

--- share/functions/funcdel.fish
+++ share/functions/funcdel.fish
@@ -1,4 +1,4 @@
-function funcdel
-    functions -e $argv
-    rm $__fish_config_dir/functions/$argv.fish
+function funcdel -a funcname
+    functions -e $funcname
+    rm $fish_function_path/$funcname.fish
 end

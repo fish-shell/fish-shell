63a130bffe762ef21b3548b3098736b4a253f1db
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-23 21:11:29 -0500

    check if function and file exists

--- share/functions/funcdel.fish
+++ share/functions/funcdel.fish
@@ -1,4 +1,18 @@
 function funcdel -a funcname
+
+    set funcpath $fish_function_path/$funcname.fish
+
+    if not type -q $funcname
+        echo "$funcname is not defined as a function"
+        return 1
+    end
+
+    if not test -e $funcpath
+        echo "$funcname does not exist as a user function"
+        return 2
+    end
+
     functions -e $funcname
-    rm $fish_function_path/$funcname.fish
+    rm $funcpath
+
 end

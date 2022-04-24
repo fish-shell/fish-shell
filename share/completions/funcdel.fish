5a879947c1441f3a135fd23980db9f71fb3e85a2
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-23 22:56:31 -0500

    funcdel

---
+++ share/completions/funcdel.fish
@@ -0,0 +1,2 @@
+complete -c funcdel -s d -l directory -d "dir to remove function(s) from" -a '$fish_function_path' -r
+complete -c funcdel -s q -d "suppress output" -r
--- share/functions/funcdel.fish
+++ share/functions/funcdel.fish
@@ -1,18 +1,46 @@
-function funcdel -a funcname
+function funcdel --description "Remove the current definition of all specified functions from session and file"
+    set -l options q/quiet h/help d/directory=
+    argparse -n funcdel $options -- $argv
+    or return
 
-    set funcpath $fish_function_path/$funcname.fish
+    if set -q _flag_help
+        __fish_print_help funcdel
+        return 0
+    end
 
-    if not type -q $funcname
-        echo "$funcname is not defined as a function"
+    if set -q _flag_directory
+        set funcdir $_flag_directory
+    else
+        set funcdir $fish_function_path
+    end
+
+    if not set -q argv[1]
+        printf (_ "%ls: Expected at least %d args, got only %d\n") funcdel 1 0 >&2
         return 1
     end
 
-    if not test -e $funcpath
-        echo "$funcname does not exist as a user function"
-        return 2
+    if not test -d $funcdir
+        printf (_ "%s: Configuration directory does not exist '%s'\n") funcdel $funcdir >&2
+        return 1
     end
 
-    functions -e $funcname
-    rm $funcpath
+    set -l retval 0
+    for funcname in $argv
+        set -l funcpath "$funcdir/$funcname.fish"
 
+        if not functions -q -- $funcname
+            printf (_ "%s: not defined as a function '%s'\n") funcdel $funcname >&2
+            set retval 1
+        else if not test -e $funcpath
+            echo "$funcname does not exist as a user function"
+            printf (_ "%s: not a saved user function '%s'\n") funcdel $funcname >&2
+            set retval 2
+        else
+            functions --erase -- $funcname
+            rm $funcpath
+            and set -q _flag_quiet || printf (_ "%s: removed %s\n") funcdel $funcpath
+        end
+    end
+
+    return $retval
 end

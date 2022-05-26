5d96f5d00b42f29693d5973b230169a9f93c2107
Fabian Homborg <FHomborg@gmail.com>
2022-05-26 13:15:36 +0200

    Update completions

--- share/completions/path.fish
+++ share/completions/path.fish
@@ -9,20 +9,23 @@ complete -f -c path -n "test (count (commandline -opc)) -lt 2" -a normalize -d '
 complete -f -c path -n "test (count (commandline -opc)) -lt 2" -a resolve -d 'Normalize given paths and resolve symlinks'
 complete -f -c path -n "test (count (commandline -opc)) -lt 2" -a filter -d 'Print paths that match a filter'
 complete -f -c path -n "test (count (commandline -opc)) -lt 2" -a is -d 'Return true if any path matched a filter'
-complete -f -c path -n "test (count (commandline -opc)) -lt 2" -a match -d 'Match paths against a glob'
-complete -f -c path -n "test (count (commandline -opc)) -lt 2" -a expand -d 'Expand globs'
+complete -f -c path -n "test (count (commandline -opc)) -lt 2" -a sort -d 'Sort paths'
 complete -f -c path -n "test (count (commandline -opc)) -ge 2" -s q -l quiet -d "Only return status, no output"
 complete -f -c path -n "test (count (commandline -opc)) -ge 2" -s z -l null-in -d "Handle NULL-delimited input"
 complete -f -c path -n "test (count (commandline -opc)) -ge 2" -s Z -l null-out -d "Print NULL-delimited output"
-complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is match" -s v -l invert -d "Invert meaning of filters"
-complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is match expand" -s t -l type -d "Filter by type" -x -a '(__fish_append , file link dir block char fifo socket)'
-complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is match expand" -s f -d "Filter files" -x -a '(__fish_append , read write exec suid sgid sticky user group)'
-complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is match expand" -s d -d "Filter directories" -x -a '(__fish_append , read write exec suid sgid sticky user group)'
-complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is match expand" -s l -d "Filter symlinks" -x -a '(__fish_append , read write exec suid sgid sticky user group)'
-complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is match expand" -s p -l perm -d "Filter by permission" -x -a '(__fish_append , read write exec suid sgid sticky user group)'
-complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is match expand" -s r -d "Filter readable paths" -x -a '(__fish_append , read write exec suid sgid sticky user group)'
-complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is match expand" -s w -d "Filter writable paths" -x -a '(__fish_append , read write exec suid sgid sticky user group)'
-complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is match expand" -s x -d "Filter executale paths" -x -a '(__fish_append , read write exec suid sgid sticky user group)'
+complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is" -s v -l invert -d "Invert meaning of filters"
+complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is" -s t -l type -d "Filter by type" -x -a '(__fish_append , file link dir block char fifo socket)'
+complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is" -s f -d "Filter files"
+complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is" -s d -d "Filter directories"
+complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is" -s l -d "Filter symlinks"
+complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is" -s p -l perm -d "Filter by permission" -x -a '(__fish_append , read write exec suid sgid user group)'
+complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is" -s r -d "Filter readable paths"
+complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is" -s w -d "Filter writable paths"
+complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] filter is" -s x -d "Filter executable paths"
+complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] sort" \
+    -l key -x -a 'basename\t"Sort only by basename" dirname\t"Sort only by dirname" path\t"Sort by full path"'
+complete -f -c path -n "test (count (commandline -opc)) -ge 2; and contains -- (commandline -opc)[2] sort" -s u -l unique -d 'Only leave the first of each run with the same key'
+
 # Turn on file completions again.
 # match takes a glob as first arg, expand takes only globs.
 # We still want files completed then!

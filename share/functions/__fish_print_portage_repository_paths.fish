f7c615a47856ebc1dc1f921d511f7abcbe0a81c4
pl3w5y <pl3w5y@tuta.io>
2022-06-12 09:18:45 -0400

    Revised version of share/functions/__fish_print_portage_repository_paths.fish

--- share/functions/__fish_print_portage_repository_paths.fish
+++ share/functions/__fish_print_portage_repository_paths.fish
@@ -1,18 +1,14 @@
 function __fish_print_portage_repository_paths --description 'Print the paths of all configured repositories'
-    set a /etc/portage/repos.conf
-    set default /usr/share/portage/config/repos.conf
-    set confs
-    if test -f $a
-        set confs $a
-    else if test -d $a
-        set -p confs (find $a -type f -name '*.conf')
-        if not contains "$a/gentoo.conf" $confs
-            set -p confs $default
-        end
-    else
-        set confs $default
-    end
-    for b in $confs
-        cat $b | string match -g -r 'location = (.*$)'
-    end
+    set -l a /etc/portage/repos.conf
+    set -l b
+    set -l c /usr/share/portage/config/repos.conf
+    test -d $a
+    and set b (find $a -type f )
+    test -f $a
+    and set b $a
+    test -n (echo $b)
+    and string match -q "[gentoo]" (cat $b)
+    and set c $b
+    or set -a c $b
+    cat $c | string match -g -r 'location = (.*$)'
 end

c5240033df3e43d731758007449699fc72800179
Alexander M <iammorjj@gmail.com>
2022-07-07 00:00:36 +0300

    Fix long descriptions in gdb.fish
    Work on #6981
    
    (cherry picked from commit 8d57bc6a9aae6b86edb5a0e675dd77c9067ab46b)

--- share/completions/gdb.fish
+++ share/completions/gdb.fish
@@ -6,19 +6,19 @@
 #
 
 complete -c gdb -o help -s h -d 'List all options, with brief explanations'
-complete -c gdb -o symbols -s s -d 'Read symbol table from file file' -r
+complete -c gdb -o symbols -s s -d 'Read symbol table from <file>' -r
 complete -c gdb -o write -d 'Enable writing into executable and core files'
-complete -c gdb -o exec -s e -d 'Use file file as the executable file to execute when appropri ate, and for examining pure data in conjunction with a core dump' -r
-complete -c gdb -o se -d 'Read symbol table from file file and use it as the executable file' -r
-complete -c gdb -o core -s c -d 'Use file file as a core dump to examine' -r
-complete -c gdb -o command -s x -d 'Execute GDB commands from file file' -r
-complete -c gdb -o directory -s d -d 'Add directory to the path to search for source files' -x -a '(__fish_complete_directories (commandline -ct))'
+complete -c gdb -o exec -s e -d 'Set executable' -r
+complete -c gdb -o se -d 'Read symbol table from <file> and execute it' -r
+complete -c gdb -o core -s c -d 'Use <file> as a core dump to examine' -r
+complete -c gdb -o command -s x -d 'Execute GDB commands from <file>' -r
+complete -c gdb -o directory -s d -d 'Add directory with source files' -x -a '(__fish_complete_directories (commandline -ct))'
 complete -c gdb -o nx -s n -d 'Do not execute commands from any .gdbinit files'
 complete -c gdb -o quiet -s q -d Quiet
 complete -c gdb -o batch -d 'Run in batch mode'
-complete -c gdb -o cd -d 'Run GDB using directory as its working directory, instead of the current directory' -x -a '(__fish_complete_directories (commandline -ct))'
-complete -c gdb -o fullname -s f -d 'Emacs sets this option when it runs GDB as a subprocess'
-complete -c gdb -s b -d 'Bps Set the line speed (baud rate or bits per second) of any serial interface used by GDB for remote debugging'
-complete -c gdb -o tty -d 'Run using device for your programs standard input and output' -r
-complete -c gdb -l args -d 'Pass arguments after the program name to the program when it is run'
+complete -c gdb -o cd -d 'Set GDB\'s working directory' -x -a '(__fish_complete_directories (commandline -ct))'
+complete -c gdb -o fullname -s f -d 'Used by Emacs'
+complete -c gdb -s b -d 'Set throughput in bps for remote debugging'
+complete -c gdb -o tty -d 'Set device for the program\'s stdin/stdout' -r
+complete -c gdb -l args -d 'Pass arguments to the program after its name'
 complete -c gdb -o tui -d 'Run GDB using a text (console) user interface'

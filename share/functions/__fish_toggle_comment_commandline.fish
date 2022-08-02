b9590d04e7bdca46348c53a4162d44313da03c8a
Aaron Gyes <var.log@icloud.com>
2022-08-02 00:30:06 -0700

    fish_toggle_commandline: minor test cleanup

--- share/functions/__fish_toggle_comment_commandline.fish
+++ share/functions/__fish_toggle_comment_commandline.fish
@@ -9,7 +9,7 @@
 
 function __fish_toggle_comment_commandline --description 'Comment/uncomment the current command'
     set -l cmdlines (commandline -b)
-    if test "$cmdlines" = ""
+    if test -z "$cmdlines"
         set cmdlines (history search -p "#" --max=1)
     end
     set -l cmdlines (printf '%s\n' '#'$cmdlines | string replace -r '^##' '')

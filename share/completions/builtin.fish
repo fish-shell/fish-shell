d8e0cbb759b0e9ba11da05228c9bbdccfbdb3f6f
Kid <44045911+kidonng@users.noreply.github.com>
2022-08-19 17:30:51 +0000

    Missing `-q/--query` flag for `builtin`

--- share/completions/builtin.fish
+++ share/completions/builtin.fish
@@ -1,4 +1,5 @@
 complete -c builtin -n 'test (count (commandline -opc)) -eq 1' -s h -l help -d 'Display help and exit'
 complete -c builtin -n 'test (count (commandline -opc)) -eq 1' -s n -l names -d 'Print names of all existing builtins'
+complete -c builtin -n 'test (count (commandline -opc)) -eq 1' -s q -l query -d 'Tests if builtin exists'
 complete -c builtin -n 'test (count (commandline -opc)) -eq 1' -xa '(builtin -n)'
 complete -c builtin -n 'test (count (commandline -opc)) -ge 2' -xa '(__fish_complete_subcommand)'

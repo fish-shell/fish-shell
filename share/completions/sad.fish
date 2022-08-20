1262a69cd1bf5790e245c88de71adae682c26fbd
Bagohart <bagohart@gmx.de>
2022-08-20 12:06:14 +0200

    ran fish_indent on completion file

--- share/completions/sad.fish
+++ share/completions/sad.fish
@@ -1,9 +1,9 @@
-complete sad    -s 0 -l read0                    -d 'Use \0 as stdin delimiter'
-complete sad    -s e -l exact                    -d 'String literal mode'
-complete sad -x -s f -l flags                    -d "Regex flags: use `--help` instead of `-h` to see details"
-complete sad -x      -l fzf     -a "never"       -d "Additional Fzf options, disable = never"
-complete sad    -s h -l help                     -d 'Print help information'
-complete sad    -s k -l commit                   -d 'No preview, write changes to file'
-complete sad -x -s p -l pager   -a "never"       -d 'Colourizing program, disable = never, default = $GIT_PAGER'
+complete sad -s 0 -l read0 -d 'Use \0 as stdin delimiter'
+complete sad -s e -l exact -d 'String literal mode'
+complete sad -x -s f -l flags -d "Regex flags: use `--help` instead of `-h` to see details"
+complete sad -x -l fzf -a never -d "Additional Fzf options, disable = never"
+complete sad -s h -l help -d 'Print help information'
+complete sad -s k -l commit -d 'No preview, write changes to file'
+complete sad -x -s p -l pager -a never -d 'Colourizing program, disable = never, default = $GIT_PAGER'
 complete sad -x -s u -l unified -a "0 1 2 3 4 5" -d 'Same as in GNU diff --unified={size}, affects aggregate size'
-complete sad -x -s V -l version                  -d 'Print version information'
+complete sad -x -s V -l version -d 'Print version information'

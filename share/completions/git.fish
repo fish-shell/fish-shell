6df57a67126e15feaaa00ec63787641bce4bbdff
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2022-09-30 15:09:14 -0500

    git completions: Change some default ASC/DESC for sort
    Dates and file sizes are kept DESC while names, emails, and hashes are now
    defaulted to ASC.

--- share/completions/git.fish
+++ share/completions/git.fish
@@ -2314,17 +2314,17 @@ function __fish_git_sort_keys
     echo -authordate\tWhen the latest commit was actually made
     echo -committerdate\tWhen the branch was last committed or rebased
     echo -creatordate\tWhen the latest commit or tag was created
-    echo -creator\tThe name of the commit author
-    echo -objectname\tThe complete SHA1
-    echo -objectname:short\tThe shortest non-ambiguous SHA1
-    echo -refname\tThe complete, unambiguous git ref name
-    echo -refname:short\tThe shortest non-ambiguous ref name
-    echo -author\tThe name of the author of the latest commit
-    echo -committer\tThe name of the person who committed latest
-    echo -tagger\tThe name of the person who created the tag
-    echo -authoremail\tThe email of the author of the latest commit
-    echo -committeremail\tThe email of the person who committed last
-    echo -taggeremail\tThe email of the person who created the tag
+    echo creator\tThe name of the commit author
+    echo objectname\tThe complete SHA1
+    echo objectname:short\tThe shortest non-ambiguous SHA1
+    echo refname\tThe complete, unambiguous git ref name
+    echo refname:short\tThe shortest non-ambiguous ref name
+    echo author\tThe name of the author of the latest commit
+    echo committer\tThe name of the person who committed latest
+    echo tagger\tThe name of the person who created the tag
+    echo authoremail\tThe email of the author of the latest commit
+    echo committeremail\tThe email of the person who committed last
+    echo taggeremail\tThe email of the person who created the tag
 end
 complete -f -c git -n "__fish_seen_subcommand_from $sortcommands" -l sort -d 'Sort results by' -a "(__fish_git_sort_keys)"
 

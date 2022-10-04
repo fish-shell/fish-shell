e4f07fe010e4c294d35fd96a65ac6f3e17c6a1a4
Fabian Boehm <FHomborg@gmail.com>
2022-10-04 17:01:19 +0200

    Stop using alias for fish_indent/fish_key_reader

--- share/functions/fish_indent.fish
+++ share/functions/fish_indent.fish
@@ -2,6 +2,8 @@
 # came with this fish. This will happen one time.
 command -sq fish_indent
 and command fish_indent --version 2>&1 | string match -rq $version
-# if alias doesn't define the function here, this is an autoloaded "nothing".
+# if we don't define the function here, this is an autoloaded "nothing".
 # the command (if there is one) will be used by default.
-or alias fish_indent=(string escape $__fish_bin_dir/fish_indent)
+or function fish_indent
+    $__fish_bin_dir/fish_indent $argv
+end
--- share/functions/fish_key_reader.fish
+++ share/functions/fish_key_reader.fish
@@ -2,6 +2,8 @@
 # came with this fish. This will happen one time.
 command -sq fish_key_reader
 and command fish_key_reader --version 2>&1 | string match -rq -- $version
-# if alias doesn't define the function here, this is an autoloaded "nothing".
+# if we don't define the function here, this is an autoloaded "nothing".
 # the command (if there is one) will be used by default.
-or alias fish_key_reader=(string escape $__fish_bin_dir/fish_key_reader)
+or function fish_key_reader
+    $__fish_bin_dir/fish_key_reader $argv
+end

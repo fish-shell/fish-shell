5447c130fc96060598e62a1a881af30681221356
Aaron Gyes <var.log@icloud.com>
2022-10-30 22:17:48 -0700

    remove fish_key_reader finder/wrapper thing.
    This was just added since "it works for fish_indent, might
    as well". It's of limited utility, remove it.

--- share/functions/fish_key_reader.fish
+++
@@ -1,9 +0,0 @@
-# check if command fish_key_reader works and is the same version that
-# came with this fish. This will happen one time.
-command -sq fish_key_reader
-and command fish_key_reader --version 2>&1 | string match -rq -- $version
-# if we don't define the function here, this is an autoloaded "nothing".
-# the command (if there is one) will be used by default.
-or function fish_key_reader
-    $__fish_bin_dir/fish_key_reader $argv
-end

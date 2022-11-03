6311a9d2761b898cbf34e2b56bbc4c058194ea4d
Phillip Wood <phillip.wood@dunelm.org.uk>
2022-11-03 16:37:15 +0000

    add pexpect tests for commandline cursor movement and text insertion
    Add some tests in preparation for changing the commandline builtin to
    use readline functions when moving the cursor and inserting text.

--- tests/pexpects/commandline.py
+++ tests/pexpects/commandline.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from pexpect_helper import SpawnedProc, control
+from re import escape
 
 sp = SpawnedProc()
 send, sendline, sleep, expect_prompt, expect_re, expect_str = (
@@ -11,6 +12,72 @@ send, sendline, sleep, expect_prompt, expect_re, expect_str = (
     sp.expect_str,
 )
 expect_prompt()
+
+# Setup commandline test bindings
+sendline(r"""function query_commandline;\
+                 printf '\n%s ' (commandline --cursor); commandline;\
+                 commandline -f cancel-commandline;\
+             end;\
+             bind \eQ query_commandline;\
+             bind \eT test_commandline;""")
+expect_prompt()
+
+# Run a commandline test
+#   commandline is the command line text
+#   binding is the function body for test_commandline
+#   expect is the expected value of $result (see query_commandline above)
+def t(commandline, binding, expect):
+    sendline(f"function test_commandline; {binding}; end")
+    expect_prompt()
+    sendline(f"{commandline}\x1bT\x1bQ")
+    expect_prompt(escape(f"\r\n{expect}\r\n"))
+
+# commandline --cursor
+t("abcdef", "commandline --cursor 3", "3 abcdef")
+t("abc defghi jkl",
+  "commandline --cursor 8; commandline --current-token --cursor 1",
+  "5 abc defghi jkl")
+t("abc def", "commandline --current-token --cursor -- -3", "1 abc def")
+t("abc def", "commandline --current-token --cursor -- -100", "0 abc def")
+t("abc def",
+  "commandline --current-token --cursor 0; commandline --insert x",
+  "5 abc xdef")
+t("echo ===; echo hello | echo there",
+  "commandline --current-job --cursor 5",
+  "14 echo ===; echo hello | echo there" )
+t("echo hello | echo there",
+  "commandline --current-process --cursor 4",
+  "16 echo hello | echo there")
+
+# commandline --insert
+t("abc", "commandline --cursor 2; commandline --insert x", "3 abxc")
+t("abc defghi jkl",
+  "commandline --cursor 8; commandline --current-token --insert x",
+  "9 abc defgxhi jkl")
+
+# commandline --append
+t("abc def; ghi", "commandline --append x", "12 abc def; ghix")
+t("abc def ghi",
+  "commandline --cursor 5; commandline --current-token --append x",
+  "5 abc defx ghi")
+t("abc def | efg hij",
+  "commandline --cursor 1; commandline --current-process --append x",
+  "1 abc def x| efg hij")
+t("echo ===; echo hello | echo there; echo bye",
+  "commandline --cursor 12; commandline --current-job --append x",
+  "12 echo ===; echo hello | echo therex; echo bye")
+
+# commandline --replace
+t("abc def; ghi", "commandline --replace 'hello'", "5 hello")
+t("abc def ghi",
+  "commandline --cursor 5; commandline --current-token --replace xxx",
+  "7 abc xxx ghi")
+t("abc def | efg hij",
+  "commandline --cursor 2; commandline --current-process --replace xxx",
+  "3 xxx| efg hij")
+t("echo ===; echo hello | echo there; echo bye",
+  "commandline --cursor 12; commandline --current-job --replace xxx",
+  "12 echo ===;xxx; echo bye")
 
 sendline("bind '~' 'handle_tilde'")
 expect_prompt()

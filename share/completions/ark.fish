721a62061d41ab56294ee53789f905f08590917b
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2022-11-20 15:47:01 +1000

    Add `ark` completion

---
+++ share/completions/ark.fish
@@ -0,0 +1,13 @@
+complete -c ark -s d -l dialog -d 'Show a dialog with options'
+complete -c ark -s m -l mimetypes -d 'List all supported MIME types'
+complete -c ark -s o -l destination -d 'Specify an extraction directory'
+
+complete -c ark -s c -l add -d 'Add files and directories to an archive interactively'
+complete -c ark -s t -l add-to -d 'Add files and directories to an archive'
+complete -c ark -s p -l changetofirstpath -d 'Use a first argument as input path'
+complete -c ark -s f -l autofilename -d 'Specify suffix for automatically choosen filename'
+
+complete -c ark -s b -l batch -d 'Use a batch interface'
+complete -c ark -s e -l autodestination -d 'Use a first argument as output path'
+complete -c ark -s a -l autosubfolder -d 'Create subfolder automatically for multiple archives'
+complete -c ark -s O -l opendestination -d 'Open upon extraction completion'

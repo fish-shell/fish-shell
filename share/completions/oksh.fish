6608ddc95b34615d3f4912cfd60cc0e3f1783bf0
Emily Grace Seville <EmilySeville7cfg@gmail.com>
2022-12-04 06:37:50 +1000

    Use just options described in man page

--- share/completions/oksh.fish
+++ share/completions/oksh.fish
@@ -1,7 +1,6 @@
-set -l options allexport notify noclobber errexit noglob trackall keyword monitor noexec privileged stdin nounset verbose markdirs xtrace
-complete -c oksh -a "+{a,b,C,e,f,h,i,k,l,m,n,p,r,u,v,X,x,o}"
-complete -c oksh -a "-{a,b,C,e,f,h,i,k,l,m,n,p,r,u,v,X,x,o}"
-complete -c oksh -s c
-complete -c oksh -s s
-
-complete -c oksh -a "$options"
+complete -c oksh -s c -d 'Specify commands to run'
+complete -c oksh -s i -d 'Use interactive mode'
+complete -c oksh -s l -d 'Use login shell'
+complete -c oksh -s p -d 'Use privileged shell'
+complete -c oksh -s r -d 'Use restricted shell'
+complete -c oksh -s c -d 'Run commands from stdin'

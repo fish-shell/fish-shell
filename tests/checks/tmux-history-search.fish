fish-shell/fish-shell

commit 3b30d92b6224eabe78115fd667efa8f35d5d6a4d

Commit transient edit when closing pager
When selecting items in the pager, only the latest of those items is kept
in the edit history, as so-called transient edit.  Each new transient edit
evicts any old transient edit (via undo).

If the pager is closed by a command that performs another transient edit
(like history-token-search-backward) we thus inadvertently undo (= remove)
the token inserted by the pager.  Fix this by closing a transient edit
session when closing the pager.  Token search will start its own session.

Fixes #9160

src/reader.cpp
@@ -2045,6 +2045,7 @@ void reader_data_t::accept_autosuggestion(bool full, bool single, move_word_styl
 void reader_data_t::clear_pager() {
     pager.clear();
     history_pager_active = false;
+    command_line_has_transient_edit = false;
 }
 
 void reader_data_t::select_completion_in_direction(selection_motion_t dir,

tests/checks/tmux-complete.fish
@@ -68,3 +68,9 @@ isolated-tmux capture-pane -p | head -1
 # The second one is the autosuggestion. Maybe we should turn them off for this test.
 # TODO there should be a prefix ("prompt 4> foo4") but we fail to draw that in this case.
 # CHECK: {{.*}} b-short-arg a-long-arg{{.*}}
+
+# Check that completion pager followed by token search search inserts two separate tokens.
+isolated-tmux send-keys C-u echo Space old-arg Enter C-l foo2 Space Tab Tab M-.
+tmux-sleep
+isolated-tmux capture-pane -p
+# CHECK: prompt 5> foo2 aabc old-arg

commit e84f588d11a86d38ff708d4c16aab1316ac09b6c

reader: make Escape during history search restore commandline again
Commit 3b30d92b6 (Commit transient edit when closing pager, 2022-08-31)
inadvertently introduced two regressions to history search:

1. It made Escape keeps the selected history entry,
   instead of restoring the commandline before history search.
2. It made history search commands add undo entries.

Fix both of this issues.

src/reader.cpp
@@ -4482,7 +4482,8 @@ maybe_t<wcstring> reader_data_t::readline(int nchars_or_0) {
 
             // Clear the pager if necessary.
             bool focused_on_search_field = (active_edit_line() == &pager.search_field_line);
-            if (command_ends_paging(readline_cmd, focused_on_search_field)) {
+            if (!history_search.active() &&
+                command_ends_paging(readline_cmd, focused_on_search_field)) {
                 clear_pager();
             }
 

tests/checks/tmux-history-search.fish
@@ -3,6 +3,9 @@
 # disable on github actions because it's flakey
 #REQUIRES: test -z "$CI"
 
+set -g isolated_tmux_fish_extra_args -C '
+    set -g fish_autosuggestion_enabled 0
+'
 isolated-tmux-start
 
 isolated-tmux send-keys 'true needle' Enter
@@ -15,3 +18,12 @@ isolated-tmux send-keys C-p C-a M-f M-f M-f M-.
 # CHECK: prompt 2> true hay needle hay
 tmux-sleep
 isolated-tmux capture-pane -p
+
+isolated-tmux send-keys C-e C-u true Up Up Escape
+tmux-sleep
+isolated-tmux capture-pane -p | grep 'prompt 2'
+# CHECK: prompt 2> true
+isolated-tmux send-keys C-z _
+tmux-sleep
+isolated-tmux capture-pane -p | grep 'prompt 2'
+# CHECK: prompt 2> _
fish-shell/fish-shell

commit f81e8c7debf53bba7cc042c2fd36343a646d935b

completions/git: complete refs for "git grep"

share/completions/git.fish
@@ -1388,6 +1388,7 @@ complete -f -c git -n '__fish_git_using_command grep' -l or -d 'Combine patterns
 complete -f -c git -n '__fish_git_using_command grep' -l not -d 'Combine patterns using not'
 complete -f -c git -n '__fish_git_using_command grep' -l all-match -d 'Only match files that can match all the pattern expressions when giving multiple'
 complete -f -c git -n '__fish_git_using_command grep' -s q -l quiet -d 'Just exit with status 0 when there is a match and with non-zero status when there isn\'t'
+complete -f -c git -n '__fish_git_using_command grep' -n 'not contains -- -- (commandline -opc)' -ka '(__fish_git_refs)'
 # TODO options, including max-depth, h, open-files-in-pager, contexts, threads, file
 
 ### init

commit 243ade838b14cc30a2e1a7b80543e5f745383f7b

completions/git: also complete filepaths as second argument to git grep
Fixes a regression in f81e8c7de (completions/git: complete refs for "git
grep", 2022-12-08).

Fixes #9513

share/completions/git.fish
@@ -1468,7 +1468,7 @@ complete -f -c git -n '__fish_git_using_command grep' -l or -d 'Combine patterns
 complete -f -c git -n '__fish_git_using_command grep' -l not -d 'Combine patterns using not'
 complete -f -c git -n '__fish_git_using_command grep' -l all-match -d 'Only match files that can match all the pattern expressions when giving multiple'
 complete -f -c git -n '__fish_git_using_command grep' -s q -l quiet -d 'Just exit with status 0 when there is a match and with non-zero status when there isn\'t'
-complete -f -c git -n '__fish_git_using_command grep' -n 'not contains -- -- (commandline -opc)' -ka '(__fish_git_refs)'
+complete -c git -n '__fish_git_using_command grep' -n 'not contains -- -- (commandline -opc)' -ka '(__fish_git_refs)'
 # TODO options, including max-depth, h, open-files-in-pager, contexts, threads, file
 
 ### init
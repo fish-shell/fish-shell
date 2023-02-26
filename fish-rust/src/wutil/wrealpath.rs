52877b0aa81546947a2871484b80e491ff486cc3
Victor Song <vms2@rice.edu>
2023-02-26 14:17:08 -0500

    Addressed PR comments regarding allocation

--- fish-rust/src/wutil/wrealpath.rs
+++ fish-rust/src/wutil/wrealpath.rs
@@ -1,4 +1,8 @@
-use std::{ffi::OsStr, fs::canonicalize, os::unix::prelude::OsStrExt};
+use std::{
+    ffi::OsStr,
+    fs::canonicalize,
+    os::unix::prelude::{OsStrExt, OsStringExt},
+};
 
 use cxx::let_cxx_string;
 
@@ -15,7 +19,6 @@ pub fn wrealpath(pathname: &wstr) -> Option<WString> {
         return None;
     }
 
-    let mut real_path = Vec::new();
     let mut narrow_path: Vec<u8> = wcs2string(&pathname.to_ffi()).from_ffi();
 
     // Strip trailing slashes. This is treats "/a//" as equivalent to "/a" if /a is a non-directory.
@@ -28,8 +31,8 @@ pub fn wrealpath(pathname: &wstr) -> Option<WString> {
     // outside of WSL + Cygwin (which should be fairly Unix-like anyways)
     let narrow_res = canonicalize(OsStr::from_bytes(&narrow_path));
 
-    if let Ok(result) = narrow_res {
-        real_path.extend_from_slice(result.as_os_str().as_bytes());
+    let real_path = if let Ok(result) = narrow_res {
+        result.into_os_string().into_vec()
     } else {
         // Check if everything up to the last path component is valid.
         let pathsep_idx = narrow_path.iter().rposition(|&c| c == b'/');
@@ -37,8 +40,7 @@ pub fn wrealpath(pathname: &wstr) -> Option<WString> {
         if let Some(0) = pathsep_idx {
             // If the only pathsep is the first character then it's an absolute path with a
             // single path component and thus doesn't need conversion.
-            real_path.clear();
-            real_path.extend(narrow_path);
+            narrow_path
         } else {
             // Only call realpath() on the portion up to the last component.
             let narrow_res = if let Some(pathsep_idx) = pathsep_idx {
@@ -53,7 +55,7 @@ pub fn wrealpath(pathname: &wstr) -> Option<WString> {
 
             let pathsep_idx = pathsep_idx.map_or(0, |idx| idx + 1);
 
-            real_path.extend_from_slice(narrow_result.as_os_str().as_bytes());
+            let mut real_path = narrow_result.into_os_string().into_vec();
 
             // This test is to deal with cases such as /../../x => //x.
             if real_path.len() > 1 {
@@ -61,8 +63,10 @@ pub fn wrealpath(pathname: &wstr) -> Option<WString> {
             }
 
             real_path.extend_from_slice(&narrow_path[pathsep_idx..]);
+
+            real_path
         }
-    }
+    };
 
     let_cxx_string!(s = real_path);
 

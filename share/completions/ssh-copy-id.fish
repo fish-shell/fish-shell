dc2b7bb2e99864012f1e235cf0acf698b8729ff1
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-03-23 00:55:12 +0800

    refactor: extract __ssh_history_completions
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- share/completions/ssh-copy-id.fish
+++ share/completions/ssh-copy-id.fish
@@ -1,8 +1,4 @@
-# Retrieve `user@host` entries from history
-function __ssh_history_completions
-    history --prefix ssh --max=100 | string replace -rf '.* ([A-Za-z0-9._:-]+@[A-Za-z0-9._:-]+).*' '$1'
-end
-
+# Commands
 complete -c ssh-copy-id -d Remote -xa "(__fish_complete_user_at_hosts)"
 complete -c ssh-copy-id -d Remote -k -fa '(__ssh_history_completions)'
 

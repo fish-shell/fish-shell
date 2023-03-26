76145145fd1434e116d02eb1eea715b9aca75c2a
Johannes Altmanninger <aclopte@gmail.com>
2023-03-26 17:23:05 +0200

    global_safety: port RelaxedAtomicBool

---
+++ fish-rust/src/global_safety.rs
@@ -0,0 +1,18 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+
+pub struct RelaxedAtomicBool(AtomicBool);
+
+impl RelaxedAtomicBool {
+    pub const fn new(value: bool) -> Self {
+        Self(AtomicBool::new(value))
+    }
+    pub fn load(&self) -> bool {
+        self.0.load(Ordering::Relaxed)
+    }
+    pub fn store(&self, value: bool) {
+        self.0.store(value, Ordering::Relaxed)
+    }
+    pub fn swap(&self, value: bool) -> bool {
+        self.0.swap(value, Ordering::Relaxed)
+    }
+}

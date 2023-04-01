de5bd624d8da48bdba1e2ef4b1e2517571a16f4c
Robert Szulist <szuro@users.noreply.github.com>
2023-04-01 05:13:40 +0200

    Add Zabbix completions (#9647)
    Add Zabbix completions
    
    (cherry picked from commit 9bd1dc14e520b5da00539141c6b7c3f029c1ce90)

---
+++ share/completions/zabbix_web_service.fish
@@ -0,0 +1,3 @@
+complete -c zabbix_web_service -s c -l config -d "Use an alternate config-file."
+complete -c zabbix_web_service -s h -l help -d "Display this help and exit."
+complete -c zabbix_web_service -s V -l version -d "Output version information and exit."

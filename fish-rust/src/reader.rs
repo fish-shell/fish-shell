9be96b42cb6876850affd5cbcd1dc342d58a30ca
Johannes Altmanninger <aclopte@gmail.com>
2023-04-09 14:33:20 +0200

    [proc] WIP Rust fringe

---
+++ fish-rust/src/reader.rs
@@ -0,0 +1,8 @@
+pub fn reader_schedule_prompt_repaint() {
+    todo!()
+}
+
+/// \return whether fish is currently unwinding the stack in preparation to exit.
+pub fn fish_is_unwinding_for_exit() -> bool {
+    todo!()
+}

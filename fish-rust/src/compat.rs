0c4addaa1cdfc556dd661cf3d201b9b69508e474
Johannes Altmanninger <aclopte@gmail.com>
2023-04-09 13:54:41 +0200

    compat.c: wrapper to access ncurses cur_term

--- fish-rust/src/compat.rs
+++ fish-rust/src/compat.rs
@@ -3,6 +3,11 @@ pub fn MB_CUR_MAX() -> usize {
     unsafe { C_MB_CUR_MAX() }
 }
 
+pub fn cur_term() -> bool {
+    unsafe { has_cur_term() }
+}
+
 extern "C" {
     fn C_MB_CUR_MAX() -> usize;
+    fn has_cur_term() -> bool;
 }

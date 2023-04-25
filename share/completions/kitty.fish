638a29badc99cb0071e0a98d4839fa12648e03d3
Kid <44045911+kidonng@users.noreply.github.com>
2023-04-25 19:01:58 +0800

    Remove kitty completion in favor of official integration
    (cherry picked from commit 93dc8485dd05efa873f9822321a4b9aa977c7176)

--- share/completions/kitty.fish
+++
@@ -1,7 +0,0 @@
-function __ksi_completions
-    set --local ct (commandline --current-token)
-    set --local tokens (commandline --tokenize --cut-at-cursor --current-process)
-    printf "%s\n" $tokens $ct | command kitty +complete fish2
-end
-
-complete -f -c kitty -a "(__ksi_completions)"

5699e7857a446d041bf4ae58a6adb9f4b7ab6465
AsukaMinato <i@asukaminato.eu.org>
2023-06-19 04:04:43 +0900

    Add i o for unzip (#9850)
    * add -I -O for unzip
    
    * for different distroes.
    
    * avoid grep
    
    (cherry picked from commit bab8fb9517b9359beb50b6a7f8f4734cb17a1931)

--- share/completions/unzip.fish
+++ share/completions/unzip.fish
@@ -23,6 +23,13 @@ complete -c unzip -s X -d "restore UID/GID info"
 complete -c unzip -s V -d "retain VMS version numbers"
 complete -c unzip -s K -d "keep setuid/setgid/tacky permissions"
 complete -c unzip -s M -d "pipe through `more` pager"
+# Some distro has -O and -I, some hasn't.
+if unzip --help | string match -rq -- -O
+    complete -c unzip -s O -d "specify a character encoding for DOS, Windows and OS/2 archives" -x -a "(__fish_print_encodings)"
+end
+if unzip --help | string match -rq -- -I
+    complete -c unzip -s I -d "specify a character encoding for UNIX and other archives" -x -a "(__fish_print_encodings)"
+end
 
 # Debian version of unzip
 if unzip -v 2>/dev/null | string match -eq Debian

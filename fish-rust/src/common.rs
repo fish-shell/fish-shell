677d40cfc74d892d0ca550141557ac2ad00a0b1a
Henrik Hørlück Berg <stage.roomers_0i@icloud.com>
2023-07-04 17:27:03 +0200

    Speed up tests by fuzzing less

--- fish-rust/src/common.rs
+++ fish-rust/src/common.rs
@@ -2074,7 +2074,7 @@ mod tests {
     use rand::random;
 
     /// The number of tests to run.
-    const ESCAPE_TEST_COUNT: usize = 100_000;
+    const ESCAPE_TEST_COUNT: usize = 10_000;
     /// The average length of strings to unescape.
     const ESCAPE_TEST_LENGTH: usize = 100;
     /// The highest character number of character to try and escape.

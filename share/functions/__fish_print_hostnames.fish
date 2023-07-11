fish-shell/fish-shell

commit 94c12d84e267bf6603022907e6a791fa596d8c31

Stringify reading nfs in print_hostnames

share/functions/__fish_print_hostnames.fish
@@ -15,8 +15,8 @@ function __fish_print_hostnames -d "Print a list of known hostnames"
 		| string replace -r '[0-9.]*\s*' '' | string trim | string replace -ra '\s+' '\n'
 	end
 	# Print nfs servers from /etc/fstab
-		if test -r /etc/fstab
-		__fish_sgrep </etc/fstab "^\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\|[a-zA-Z.]*\):"|cut -d : -f 1
+	if test -r /etc/fstab
+		string match -r '^\s*[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3]:|[a-zA-Z\.]*:' </etc/fstab | string replace -r ':.*' ''
 	end
 
 	# Print hosts with known ssh keys

commit 2b0e3ba3b883e06f43da92d4bf65db1ac996f534

__fish_print_hostnames: Fix regex
This used `]` when it should have been `}`, which made the regex nonsensical

Broken since 94c12d84e267bf6603022907e6a791fa596d8c31 in 2016

share/functions/__fish_print_hostnames.fish
@@ -19,7 +19,7 @@ function __fish_print_hostnames -d "Print a list of known hostnames"
 
     # Print nfs servers from /etc/fstab
     if test -r /etc/fstab
-        string match -r '^\s*[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3]:|^[a-zA-Z\.]*:' </etc/fstab |
+        string match -r '^\s*[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:|^[a-zA-Z\.]*:' </etc/fstab |
             string replace -r ':.*' ''
     end
 
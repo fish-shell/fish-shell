6865d03927aa9483f1e384898e17811d2ea6cee9
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2023-07-23 11:15:54 +1000

    fix(completion): helper functions

--- share/completions/blender.fish
+++ share/completions/blender.fish
@@ -1,11 +1,11 @@
 function __blender_list_scenes -a file
-    blender -b $file --python-expr 'import bpy
+    stdbuf --output=0 blender --background test.blend --python-expr 'import bpy
 
-    for name in [scene.name for scene in list(bpy.data.scenes)]:
-        print(name)'
+for name in [scene.name for scene in list(bpy.data.scenes)]:
+    print(name)' | head --lines=-2 | tail --lines=+6
 end
 
 function __blender_list_addons
     ls --format single-column /usr/share/blender/scripts/addons --color=never |
-        string replace --regex '.py$' ''
+        string replace -r '.py$' ''
 end

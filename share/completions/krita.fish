86eaf56624bbd7bf00a257f52b3a8e232447c5f0
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2023-07-23 21:29:55 +1000

    feat(completion): dynamically generate workspace list

--- share/completions/krita.fish
+++ share/completions/krita.fish
@@ -28,6 +28,11 @@ YCbCrA' $current_token
     end
 end
 
+function __krita_list_workspaces
+    ls --format single-column --color=never $HOME/.local/share/krita/workspaces/ |
+        string replace -r '.kws$' ''
+end
+
 complete -c krita -s h -l help -d 'show help'
 complete -c krita -l help-all -d 'show help with Qt options'
 complete -c krita -s v -l version -d 'show version'
@@ -43,7 +48,7 @@ complete -c krita -l template -d 'open template' -r
 complete -c krita -l nosplash -d 'hide splash screen'
 complete -c krita -l canvasonly -d 'open with canvasonly mode'
 complete -c krita -l fullscreen -d 'open with fullscreen mode'
-complete -c krita -l workspace -d 'open with workspace' -a 'Animation Big_Paint_2 Big_Paint Big_Vector Default Minimal Small_Vector Storyboarding VFX_Paint' -x
+complete -c krita -l workspace -d 'open with workspace' -a '(__krita_list_workspaces)' -x
 complete -c krita -l file-layer -d 'open with file-layer' -r
 complete -c krita -l resource-location -d 'open with resource' -r
 

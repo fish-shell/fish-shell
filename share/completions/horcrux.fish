00771ae15d5e54315c9b2138f2811cfc529af0b7
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2023-08-01 18:32:49 +1000

    fix(completion): condition for -n

--- share/completions/horcrux.fish
+++ share/completions/horcrux.fish
@@ -1,6 +1,6 @@
 set -l subcommands 'bind split'
 set -l subcommand_show_condition "not __fish_seen_subcommand_from $subcommands"
-set -l split_option_show_condition "contains -- split (commandline -po)"
+set -l split_option_show_condition "__fish_seen_subcommand_from split"
 
 complete -c horcrux -a bind -n "$subcommand_show_condition" -f -d 'Bind directory'
 complete -c horcrux -a split -n "$subcommand_show_condition" -f -d 'Split file'

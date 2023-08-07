c1def90682ba20bdf7dc143698f192e598780c12
Matteo De Wint <matteo@gorilla.co>
2023-08-07 12:45:38 +0200

    Add COMP_POINT in aws completions

--- share/completions/aws.fish
+++ share/completions/aws.fish
@@ -1,7 +1,13 @@
 # AWS CLI comes bundled with a completion command.
 # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-completion.html
 if type -q aws_completer
-    complete -c aws -f -a "(COMP_LINE=(commandline -pc) aws_completer)"
+    function __aws_completer
+        set -lx COMP_LINE (commandline -pc)
+        set -lx COMP_POINT (string length $COMP_LINE)
+        aws_completer
+    end
+
+    complete -c aws -f -a "(__aws_completer)"
     exit 0
 end
 

1c72d1c77bd7567d41430e2418e0c0c9967112fc
Gabriel Górski <glaeqen@gmail.com>
2023-08-09 09:03:30 +0200

    Changed the `&& \` into `|| return`

--- share/functions/__fish_is_zfs_feature_enabled.fish
+++ share/functions/__fish_is_zfs_feature_enabled.fish
@@ -2,6 +2,6 @@ function __fish_is_zfs_feature_enabled \
     -a feature pool \
     -d "Returns 0 if the given ZFS pool feature is active or enabled for the given pool or for any pool if none specified"
 
-    type -q zpool && \
+    type -q zpool || return
     zpool get -H -o value $feature $pool 2> /dev/null | string match -rq '^(enabled|active)$'
 end

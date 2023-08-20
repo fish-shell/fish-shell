45be1348ce0d0286e4f73ace07f02499ce4ed063
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2023-08-20 15:02:54 +0200

    Add test that confirms behavior when PATH is unset

---
+++ tests/checks/default-setup-path.fish
@@ -0,0 +1,11 @@
+#RUN: %fish -C 'set -g fish %fish' %s
+
+# FIXME: Rationalize behavior when PATH is explicitly unset, should this not behave like PATH=""?
+# "" is threated like ".", see https://github.com/fish-shell/fish-shell/issues/3914
+if command -q getconf
+    env -u PATH $fish -c 'test "$PATH" = "$('(command -s getconf)' PATH)"; and echo Success'
+else
+    # this is DEFAULT_PATH
+    env -u PATH $fish -c 'test "$PATH" = "/usr/bin:/bin"; and echo Success'
+end
+# CHECK: Success

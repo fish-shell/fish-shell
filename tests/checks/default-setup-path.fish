676c3c9bc2c8e81088a7197906263c8e31a649a1
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2023-08-20 16:08:54 +0200

    Re-use DEFAULT_PATH in setup_path
    - No need to hard-code a different default

--- tests/checks/default-setup-path.fish
+++ tests/checks/default-setup-path.fish
@@ -6,6 +6,7 @@ if command -q getconf
     env -u PATH $fish -c 'test "$PATH" = "$('(command -s getconf)' PATH)"; and echo Success'
 else
     # this is DEFAULT_PATH
-    env -u PATH $fish -c 'test "$PATH" = "/usr/bin:/bin"; and echo Success'
+    # the first element (usually `/usr/local/bin`) depends on PREFIX set in CMake, so we ignore it
+    env -u PATH $fish -c 'test "$PATH[2..]" = "/usr/bin:/bin"; and echo Success'
 end
 # CHECK: Success

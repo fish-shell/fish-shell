d122f7e59b636f8b0e75b740acd9a3faf401c25b
Jason Nader <24377231+6A61736F6E206E61646572@users.noreply.github.com>
2023-09-06 16:49:33 +0900

    Update ibmcloud.fish

--- share/completions/ibmcloud.fish
+++ share/completions/ibmcloud.fish
@@ -1,11 +1,1 @@
-set -l commands (ibmcloud --generate-bash-completion)
-
-complete -c ibmcloud --no-files \
-    --condition "not __fish_seen_subcommand_from $commands" \
-    --arguments "$commands"
-
-for cmd in $commands
-    complete -c ibmcloud --no-files \
-        --condition "__fish_seen_subcommand_from $cmd" \
-        --arguments "(ibmcloud $cmd --generate-bash-completion)"
-end
+complete -c ibmcloud -f -a '(__fish_argcomplete_complete (commandline -opc) --generate-bash-completion)'

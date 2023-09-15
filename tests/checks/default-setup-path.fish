6194899c6b0a12d3abd62d2608ef5e464496c509
Fabian Boehm <FHomborg@gmail.com>
2023-09-15 20:04:13 +0200

    tests: Remove a misunderstanding

--- tests/checks/default-setup-path.fish
+++ tests/checks/default-setup-path.fish
@@ -1,7 +1,5 @@
 #RUN: %fish -C 'set -g fish %fish' %s
 
-# FIXME: Rationalize behavior when PATH is explicitly unset, should this not behave like PATH=""?
-# "" is threated like ".", see https://github.com/fish-shell/fish-shell/issues/3914
 if command -q getconf
     env -u PATH $fish -c 'test "$PATH" = "$('(command -s getconf)' PATH)"; and echo Success'
 else

84e6344c9140b99b280a854532b5285248528982
Kid <44045911+kidonng@users.noreply.github.com>
2023-10-20 01:38:03 +0800

    Add `--verbose` completion to `fish_key_reader`
    This was missing from #8467.

--- share/completions/fish_key_reader.fish
+++ share/completions/fish_key_reader.fish
@@ -1,3 +1,4 @@
 complete -c fish_key_reader -s h -l help -d 'Display help and exit'
 complete -c fish_key_reader -s v -l version -d 'Display version and exit'
 complete -c fish_key_reader -s c -l continuous -d 'Start a continuous session'
+complete -c fish_key_reader -s V -l verbose -d 'Output timing and explain sequence'

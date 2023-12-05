e2ab7397acc5502736cd8ed681e33dbd15f74030
Amy Grace <zgracem@users.noreply.github.com>
2023-12-05 12:05:50 -0700

    completions: add BSD calendar
    (cherry picked from commit d1a906026a23cdf94990b228e0de243a20fd3c02)

---
+++ share/completions/calendar.fish
@@ -0,0 +1,10 @@
+# calendar
+
+complete -c calendar -s A -s l -d "Today + N days forward"
+complete -c calendar -s a -d "Process all users' calendars and mail the results"
+complete -c calendar -s B -d "Today - N days backward"
+complete -c calendar -s b -d "Cyrillic date calculation mode"
+complete -c calendar -s e -d  "Today + N days forward, if today is Friday"
+complete -c calendar -s f -rF -d "Path to default calendar file"
+complete -c calendar -s t -x -d "Today is [[[cc]yy]mm]dd]"
+complete -c calendar -s w -d "Print day of the week name"

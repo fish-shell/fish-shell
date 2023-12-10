3373cf6b624f6b61d7bb59f748613d4903ef3eb0
Amy Grace <zgracem@users.noreply.github.com>
2023-12-10 10:17:39 -0700

    add missing completion helper to `xxd`

--- share/completions/xxd.fish
+++ share/completions/xxd.fish
@@ -1,3 +1,16 @@
+function __fish_complete_exclusives
+    # Returns true if none of the switch names appear in the command line.
+    # E.g.: __fish_complete_exclusives i ps r verbose
+    set -l exclusive_args $argv
+    for arg in $exclusive_args
+        switch (string length $arg)
+            case 2; __fish_seen_argument -o $arg; and return 1
+            case 1; __fish_seen_argument -s $arg; and return 1
+            case '*'; __fish_seen_argument -l $arg; and return 1
+        end
+    end
+end
+
 set -l xxd_exclusive_args b e i ps r
 
 complete -c xxd -s a -d 'Toggle autoskip'

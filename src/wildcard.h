3cdca4738a6e24be60ff5a7856a35d9b3e04f2be
David Adam <zanchey@ucc.gu.uwa.edu.au>
2024-01-02 00:48:25 +0800

    drop unused wildcard module
    Some of the definitions in wildcard.h are still used in C++.

--- src/wildcard.h
+++ src/wildcard.h
@@ -6,36 +6,9 @@
 #include <stddef.h>
 
 #include "common.h"
-#include "complete.h"
-#include "expand.h"
 #if INCLUDE_RUST_HEADERS
 #include "wildcard.rs.h"
 #endif
-
-/// Description for generic executable.
-#define COMPLETE_EXEC_DESC _(L"command")
-/// Description for link to executable.
-#define COMPLETE_EXEC_LINK_DESC _(L"command link")
-/// Description for character device.
-#define COMPLETE_CHAR_DESC _(L"char device")
-/// Description for block device.
-#define COMPLETE_BLOCK_DESC _(L"block device")
-/// Description for fifo buffer.
-#define COMPLETE_FIFO_DESC _(L"fifo")
-/// Description for fifo buffer.
-#define COMPLETE_FILE_DESC _(L"file")
-/// Description for symlink.
-#define COMPLETE_SYMLINK_DESC _(L"symlink")
-/// Description for symlink.
-#define COMPLETE_DIRECTORY_SYMLINK_DESC _(L"dir symlink")
-/// Description for Rotten symlink.
-#define COMPLETE_BROKEN_SYMLINK_DESC _(L"broken symlink")
-/// Description for symlink loop.
-#define COMPLETE_LOOP_SYMLINK_DESC _(L"symlink loop")
-/// Description for socket files.
-#define COMPLETE_SOCKET_DESC _(L"socket")
-/// Description for directories.
-#define COMPLETE_DIRECTORY_DESC _(L"directory")
 
 // Enumeration of all wildcard types.
 enum {
@@ -48,30 +21,6 @@ enum {
     /// This is a special pseudo-char that is not used other than to mark the
     /// end of the the special characters so we can sanity check the enum range.
     ANY_SENTINEL
-};
-
-/// Expand the wildcard by matching against the filesystem.
-///
-/// wildcard_expand works by dividing the wildcard into segments at each directory boundary. Each
-/// segment is processed separately. All except the last segment are handled by matching the
-/// wildcard segment against all subdirectories of matching directories, and recursively calling
-/// wildcard_expand for matches. On the last segment, matching is made to any file, and all matches
-/// are inserted to the list.
-///
-/// If wildcard_expand encounters any errors (such as insufficient privileges) during matching, no
-/// error messages will be printed and wildcard_expand will continue the matching process.
-///
-/// \param wc The wildcard string
-/// \param working_directory The working directory
-/// \param flags flags for the search. Can be any combination of for_completions and
-/// executables_only
-/// \param output The list in which to put the output
-///
-enum class wildcard_result_t {
-    no_match,  /// The wildcard did not match.
-    match,     /// The wildcard did match.
-    cancel,    /// Expansion was cancelled (e.g. control-C).
-    overflow,  /// Expansion produced too many results.
 };
 
 #endif

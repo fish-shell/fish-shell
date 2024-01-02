ee837f254b2334cdc3216f88669a37c6df8ff126
Josef Litoš <54900518+JosefLitos@users.noreply.github.com>
2024-01-02 18:20:24 +0100

    feat: adb completion cleared of awk

--- share/completions/adb.fish
+++ share/completions/adb.fish
@@ -74,7 +74,7 @@ function __fish_adb_list_files
     end
 
     # Return list of directories suffixed with '/'
-    __fish_adb_run_command find -H "$token*" -maxdepth 0 -type d 2\>/dev/null | awk '{print $0"/"}'
+    __fish_adb_run_command find -H "$token*" -maxdepth 0 -type d 2\>/dev/null | string -r '$' /
     # Return list of files
     __fish_adb_run_command find -H "$token*" -maxdepth 0 -type f 2\>/dev/null
 end

ae24d983562c82c9be36128f0f3be783e4e5705d
Fabian Boehm <FHomborg@gmail.com>
2024-01-07 19:33:15 +0100

    benchmarks: Run glob only once
    We assume that you use something like hyperfine to run warmups, like
    our driver script does.
    
    This allows the script to be run e.g. in valgrind without being too
    much of a pain in the gluteus.

--- benchmarks/benchmarks/glob.fish
+++ benchmarks/benchmarks/glob.fish
@@ -3,6 +3,5 @@
 # so it's best to build two fishes, check out one version of the repo,
 # and then run this script with both.
 set -l dir (dirname (status current-filename))
-for i in (seq 1 10)
-    echo $dir/../../**
-end
+# No repetitions, this is plenty slow enough.
+echo $dir/../../**

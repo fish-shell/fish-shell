033f64fde644ffa4303889b82973a836f14e14ca
Johannes Altmanninger <aclopte@gmail.com>
2024-01-27 16:39:29 +0100

    Fix indentation in bitflags macro

--- src/complete.rs
+++ src/complete.rs
@@ -93,24 +93,24 @@ pub const PROG_COMPLETE_SEP: char = '\t';
 bitflags! {
     #[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
     pub struct CompleteFlags: u8 {
-    /// Do not insert space afterwards if this is the only completion. (The default is to try insert
-    /// a space).
-    const NO_SPACE = 1 << 0;
-    /// This is not the suffix of a token, but replaces it entirely.
-    const REPLACES_TOKEN = 1 << 1;
-    /// This completion may or may not want a space at the end - guess by checking the last
-    /// character of the completion.
-    const AUTO_SPACE = 1 << 2;
-    /// This completion should be inserted as-is, without escaping.
-    const DONT_ESCAPE = 1 << 3;
-    /// If you do escape, don't escape tildes.
-    const DONT_ESCAPE_TILDES = 1 << 4;
-    /// Do not sort supplied completions
-    const DONT_SORT = 1 << 5;
-    /// This completion looks to have the same string as an existing argument.
-    const DUPLICATES_ARGUMENT = 1 << 6;
-    /// This completes not just a token but replaces the entire commandline.
-    const REPLACES_COMMANDLINE = 1 << 7;
+        /// Do not insert space afterwards if this is the only completion. (The default is to try insert
+        /// a space).
+        const NO_SPACE = 1 << 0;
+        /// This is not the suffix of a token, but replaces it entirely.
+        const REPLACES_TOKEN = 1 << 1;
+        /// This completion may or may not want a space at the end - guess by checking the last
+        /// character of the completion.
+        const AUTO_SPACE = 1 << 2;
+        /// This completion should be inserted as-is, without escaping.
+        const DONT_ESCAPE = 1 << 3;
+        /// If you do escape, don't escape tildes.
+        const DONT_ESCAPE_TILDES = 1 << 4;
+        /// Do not sort supplied completions
+        const DONT_SORT = 1 << 5;
+        /// This completion looks to have the same string as an existing argument.
+        const DUPLICATES_ARGUMENT = 1 << 6;
+        /// This completes not just a token but replaces the entire commandline.
+        const REPLACES_COMMANDLINE = 1 << 7;
     }
 }
 

677e4ff698405f40442558a1334f8ad0e736f8e3
Fabian Boehm <FHomborg@gmail.com>
2024-01-27 18:36:36 +0100

    anypython: Add more versions

--- share/functions/__fish_anypython.fish
+++ share/functions/__fish_anypython.fish
@@ -2,7 +2,7 @@ function __fish_anypython
     # Try python3 first, because that's usually faster and generally nicer.
     # Do not consider the stub /usr/bin/python3 that comes installed on Darwin to be Python
     # unless Xcode reports a real directory path.
-    for py in python3 python3.{9,8,7,6,5,4,3} python2 python2.7 python
+    for py in python3 python3.{12,11,10,9,8,7,6,5,4,3} python2 python2.7 python
         if set -l py_path (command -s $py)
             if string match -q /usr/bin/python3 -- $py_path
                 and string match -q Darwin -- "$(uname)"

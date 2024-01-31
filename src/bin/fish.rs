54f6da7a5a4e00edfacc107c2781b12c8405aad7
Fabian Boehm <FHomborg@gmail.com>
2024-01-31 19:11:35 +0100

    Make fish installable
    When built with the default "installable" feature:
    
    Run `fish --install` or `fish --install=noconfirm` (for
    non-interactive use)
    
    CMake disables the default features so nothing changes for that, but this allows installing via `cargo install`,
    and even making a static binary that you can then just upload and have extract itself.

--- src/bin/fish.rs
+++ src/bin/fish.rs
@@ -74,6 +74,73 @@ const DATA_DIR: &str = env!("DATADIR");
 const SYSCONF_DIR: &str = env!("SYSCONFDIR");
 const BIN_DIR: &str = env!("BINDIR");
 
+#[cfg(feature = "installable")]
+fn install(noconfirm: bool) {
+    use rust_embed::RustEmbed;
+
+    #[derive(RustEmbed)]
+    #[folder = "share/"]
+    struct Asset;
+
+    use std::fs;
+    use std::io::Write;
+    use std::io::{stderr, stdin};
+    let dir = PathBuf::from(DATA_DIR).join(DATA_DIR_SUBDIR);
+
+    // TODO: Translation,
+    // FLOG?
+    // - Install: Translations
+    // - Install: Manpages (build via build.rs)
+    // - Don't install: __fish_build_paths.fish.in
+    if !noconfirm {
+        eprintln!(
+            "This will write fish's data files to '{}'.\n\
+             Please enter 'yes' to continue.",
+            dir.display()
+        );
+        eprint!("> ");
+        let _ = stderr().flush();
+
+        let mut input = String::new();
+        stdin()
+            .read_line(&mut input)
+            .expect("Failed to read from stdin");
+
+        if input != "yes\n" {
+            eprintln!("Exiting without writing any files");
+            std::process::exit(1);
+        }
+    } else {
+        eprintln!("Installing fish's data files to '{}'.", dir.display());
+    }
+
+    for file in Asset::iter() {
+        let path = dir.join(file.as_ref());
+        let Ok(_) = fs::create_dir_all(path.parent().unwrap()) else {
+            eprintln!(
+                "Creating directory '{}' failed",
+                path.parent().unwrap().display()
+            );
+            std::process::exit(1);
+        };
+        let res = File::create(&path);
+        let Ok(mut f) = res else {
+            eprintln!("Creating file '{}' failed", path.display());
+            continue;
+        };
+        // This should be impossible.
+        let d = Asset::get(&file).expect("File was somehow not included???");
+        f.write_all(&d.data).expect("FAILED TO WRITE");
+    }
+    std::process::exit(0);
+}
+
+#[cfg(not(feature = "installable"))]
+fn install(_noconfirm: bool) {
+    eprintln!("Fish was built without support for self-installation");
+    std::process::exit(1);
+}
+
 /// container to hold the options specified within the command line
 #[derive(Default, Debug)]
 struct FishCmdOpts {
@@ -267,8 +334,14 @@ fn read_init(parser: &Parser, paths: &ConfigPaths) {
         let escaped_pathname = escape(&datapath);
         FLOGF!(
             error,
-            "Fish cannot find its asset files in '%ls'. Refusing to read configuration.",
-            escaped_pathname
+            "Fish cannot find its asset files in '%ls'.\n\
+             Refusing to read configuration because of this.",
+            escaped_pathname,
+        );
+        #[cfg(feature = "installable")]
+        FLOG!(
+            error,
+            "If you installed via `cargo install`, please run `fish --install` and restart fish."
         );
         return;
     }
@@ -329,6 +402,7 @@ fn fish_parse_opt(args: &mut [WString], opts: &mut FishCmdOpts) -> ControlFlow<i
         wopt(L!("no-config"), NoArgument, 'N'),
         wopt(L!("no-execute"), NoArgument, 'n'),
         wopt(L!("print-rusage-self"), NoArgument, RUSAGE_ARG),
+        wopt(L!("install"), OptionalArgument, 'I'),
         wopt(
             L!("print-debug-categories"),
             NoArgument,
@@ -363,6 +437,21 @@ fn fish_parse_opt(args: &mut [WString], opts: &mut FishCmdOpts) -> ControlFlow<i
             'f' => opts.features = w.woptarg.unwrap().to_owned(),
             'h' => opts.batch_cmds.push("__fish_print_help fish".into()),
             'i' => opts.is_interactive_session = true,
+            'I' => {
+                let noconfirm = match w.woptarg {
+                    None => false,
+                    Some(n) if n == L!("noconfirm") => true,
+                    _ => {
+                        FLOGF!(
+                            error,
+                            "Unknown argument to --install: '%ls'",
+                            w.woptarg.unwrap()
+                        );
+                        std::process::exit(1);
+                    }
+                };
+                install(noconfirm);
+            }
             'l' => opts.is_login = true,
             'N' => {
                 opts.no_config = true;

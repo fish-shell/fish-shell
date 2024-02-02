ed3de2f194dc73b060263176fa1ed9c583a5c1e3
Samuel Collins <samuel.collins@live.co.uk>
2024-02-02 11:52:35 +0000

    test builtin help redirects

---
+++ tests/checks/print-help.fish
@@ -0,0 +1,15 @@
+# RUN: %fish %s
+# Test redirecting builtin help with a pipe
+
+set -lx __fish_data_dir (mktemp -d)
+mkdir -p $__fish_data_dir/man/man1
+# Create $__fish_data_dir/man/man1/and.1
+echo '.\" Test manpage for and (not real).
+.TH "AND" "1" "Feb 02, 2024" "3.7" "fish-shell"
+.SH NAME
+and \- conditionally execute a command' >$__fish_data_dir/man/man1/and.1
+
+# help should be redirected to grep instead of appearing on STDOUT
+builtin and --help | grep -q "and - conditionally execute a command"
+echo $status
+#CHECK: 0

e207f8464eed41bdf781e3d7fe5ceb7d16d46b07
Jadi <jadijadi@gmail.com>
2024-02-17 12:11:29 +0330

    Make descriptions shorter: iex, jhipster, lpadmin, mocp, rmmod

--- share/completions/lpadmin.fish
+++ share/completions/lpadmin.fish
@@ -1,6 +1,6 @@
 complete -c lpadmin -s c -d 'Adds the named printer to class' -x
-complete -c lpadmin -s i -d 'Sets a  System V style interface script for the printer' -x
-complete -c lpadmin -s m -d 'Sets a standard System V interface script or PPD file ' -x
+complete -c lpadmin -s i -d 'Sets a  SysV style interface script for printer' -x
+complete -c lpadmin -s m -d 'Sets a standard SysV interface script or PPD file ' -x
 complete -c lpadmin -s R -d 'Deletes the named option from printer' -xa '(__fish_print_lpr_options)'
 complete -c lpadmin -s r -d 'Removes the named printer from class' -x
 
@@ -19,11 +19,11 @@ complete -c lpadmin -s o -xa job-quota-period= -d 'Sets the accounting period fo
 complete -c lpadmin -s o -xa job-sheets-default= -d 'Sets the default banner page(s) to use for print jobs'
 complete -c lpadmin -s o -d 'Sets a  PPD option for the printer' -xa '(__fish_complete_lpr_option)'
 #complete -c lpadmin -s o -d 'Sets a default server-side option for the destination' -xa '(__fish_complete_lpr_option | sed "s/=/-default=/")'
-complete -c lpadmin -s o -d 'Sets the binary communications program to use when printing' -xa 'port-monitor=none port-monitor=bcp port-monitor=tbcp'
+complete -c lpadmin -s o -d 'Set the binary communications program to use' -xa 'port-monitor=none port-monitor=bcp port-monitor=tbcp'
 complete -c lpadmin -s o -d "Set error policy if printer backend can't send job" -xa 'printer-error-policy=abort-job printer-error-policy=retry-job printer-error-policy=retry-current-job printer-error-policy=stop-printer'
-complete -c lpadmin -s o -xa printer-is-shared=true -d 'Sets destination to shared/published or unshared/unpublished'
-complete -c lpadmin -s o -xa printer-is-shared=false -d 'Sets destination to shared/published or unshared/unpublished'
-complete -c lpadmin -s o -d 'Set IPP operation policy associated with destination' -xa "printer-policy=(test -r /etc/cups/cupsd.conf; and string replace -r --filter '<Policy (.*)>' '$1' < /etc/cups/cupsd.conf)"
+complete -c lpadmin -s o -xa printer-is-shared=true -d 'Sets dest to shared/published or unshared/unpublished'
+complete -c lpadmin -s o -xa printer-is-shared=false -d 'Sets dest to shared/published or unshared/unpublished'
+complete -c lpadmin -s o -d 'Set IPP operation policy associated with dest' -xa "printer-policy=(test -r /etc/cups/cupsd.conf; and string replace -r --filter '<Policy (.*)>' '$1' < /etc/cups/cupsd.conf)"
 
 complete -c lpadmin -s u -xa 'allow:all allow:none (__fish_complete_list , __fish_complete_users allow:)' -d 'Sets user-level access control on a destination'
 complete -c lpadmin -s u -xa '(__fish_complete_list , __fish_complete_groups allow: @)' -d 'Sets user-level access control on a destination'

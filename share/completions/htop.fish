62d172060527804f600c02eafc19b246caa6834e
Johannes Altmanninger <aclopte@gmail.com>
2024-03-16 10:30:46 +0100

    completions/htop: fix --sort-key completions

--- share/completions/htop.fish
+++ share/completions/htop.fish
@@ -7,7 +7,7 @@ complete -c htop -l no-colour -d 'Start htop in monochrome mode'
 complete -c htop -l filter -s F -d 'Filter processes by terms matching the commands' -x
 complete -c htop -l help -s h -d 'Show help and exit'
 complete -c htop -l pid -s p -d 'Show only given PIDs' -xa '(__fish_append , (__fish_complete_pids))'
-complete -c htop -l sort-key -s s -d 'Sort column' -xa '(htop --sort-key help)'
+complete -c htop -l sort-key -s s -d 'Sort column' -xa '(htop --sort-key help | sed -E "s/^\s*([^[:space:]]*)\s*(.*)/\1\t\2/")'
 complete -c htop -l user -s u -d 'Monitor given user' -xa '(__fish_complete_users)'
 complete -c htop -l no-unicode -s U -d 'Do not use unicode but ASCII characters for graph meters'
 complete -c htop -l no-mouse -s M -d 'Disable support of mouse control'

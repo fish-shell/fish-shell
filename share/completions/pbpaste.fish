d006c76aceb4ec4151b7feeb9afc8d1d09097260
Felix Luciano Salomon <felixluciano.a@gmail.com>
2024-03-22 15:12:03 -0700

    Added pbpaste command completions

---
+++ share/completions/pbpaste.fish
@@ -0,0 +1,3 @@
+complete -c pbpaste -s help -d "Display command help"
+complete -c pbpaste -s pboard -d "Specifies which paste specifies which pasteboard to copy to or paste from. If no pasteboard is given, the general pasteboard will be used by default. {general | ruler | find | font}"
+complete -c pbpaste -s Prefer -d "Tells pbpaste what type of data to look for in the pasteboard first. {txt | rtf | ps}"

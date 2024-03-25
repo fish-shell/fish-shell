33e2c44e9e686ccf24741ec1ecfc90e434e5536e
Felix Luciano Salomon <felixluciano.a@gmail.com>
2024-03-24 23:05:53 -0700

    Modified format completion to include formats as closed list arguments

--- share/completions/bru.fish
+++ share/completions/bru.fish
@@ -9,7 +9,7 @@ complete -c bru -l cacert --condition '__fish_seen_subcommand_from run' -d "CA c
 complete -c bru -l env --condition '__fish_seen_subcommand_from run' -d "Environment variables"
 complete -c bru -l env-var --condition '__fish_seen_subcommand_from run' -d "Overwrite a single environment variable, multiple usages possible"
 complete -c bru -s o -l output --condition '__fish_seen_subcommand_from run' -d "Path to write file results to"
-complete -c bru -s f -l format --condition '__fish_seen_subcommand_from run' -d "Format of the file results; available formats are \"json\" (default), \"junit\" or \"html\""
+complete -c bru -s f -l format --condition '__fish_seen_subcommand_from run' -d "Format of the file results" -xa "json junit html"
 complete -c bru -l insecure --condition '__fish_seen_subcommand_from run' -d "Allow insecure connections"
 complete -c bru -l tests-only --condition '__fish_seen_subcommand_from run' -d "Only run requests that have a test"
 complete -c bru -l bail --condition '__fish_seen_subcommand_from run' -d "Stop execution after a failure of a request, test or assertion"

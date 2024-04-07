d30fab372f8c530ee22b32d59da6b65125478b7f
Johannes Altmanninger <aclopte@gmail.com>
2024-04-07 12:59:28 +0200

    Pop CSI u mode on SIGTERM
    As implied by the changelog.
    
    Unfortunately it's not obvious how to access the RefCell value in spite
    of a potential (albeit unlikely) present mutable borrow. We need to use a
    different type to make it work in such cases, hopefully doing that in future.
    
    In future we could even use panic=abort and use this style of cleanup for
    panics (instead of RAII).

--- src/signal.rs
+++ src/signal.rs
@@ -2,6 +2,7 @@ use std::num::NonZeroI32;
 
 use crate::common::{exit_without_destructors, restore_term_foreground_process_group_for_exit};
 use crate::event::{enqueue_signal, is_signal_observed};
+use crate::input_common::TERMINAL_PROTOCOLS;
 use crate::nix::getpid;
 use crate::reader::{reader_handle_sigint, reader_sighup};
 use crate::termsize::TermsizeContainer;
@@ -88,6 +89,9 @@ extern "C" fn fish_signal_handler(
             // Handle sigterm. The only thing we do is restore the front process ID, then die.
             if !observed {
                 restore_term_foreground_process_group_for_exit();
+                if let Ok(mut term_protocols) = TERMINAL_PROTOCOLS.get().try_borrow_mut() {
+                    *term_protocols = None;
+                }
                 // Safety: signal() and raise() are async-signal-safe.
                 unsafe {
                     libc::signal(libc::SIGTERM, libc::SIG_DFL);

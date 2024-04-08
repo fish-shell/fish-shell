d025b245f6cd2ae11183cc863471ae544d10d5f3
Johannes Altmanninger <aclopte@gmail.com>
2024-04-08 17:33:26 +0200

    Fix parsing of single-digit function keys

--- src/tests/key.rs
+++ src/tests/key.rs
@@ -1,4 +1,4 @@
-use crate::key::{self, ctrl, parse_keys, Key};
+use crate::key::{self, ctrl, function_key, parse_keys, Key};
 use crate::wchar::prelude::*;
 
 #[test]
@@ -10,4 +10,9 @@ fn test_parse_key() {
     assert_eq!(parse_keys(L!("\x1b")), Ok(vec![Key::from_raw(key::Escape)]));
     assert_eq!(parse_keys(L!("ctrl-a")), Ok(vec![ctrl('a')]));
     assert_eq!(parse_keys(L!("\x01")), Ok(vec![ctrl('a')]));
+    assert!(parse_keys(L!("F0")).is_err());
+    assert_eq!(
+        parse_keys(L!("F1")),
+        Ok(vec![Key::from_raw(function_key(1))])
+    );
 }

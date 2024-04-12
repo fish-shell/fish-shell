fish-shell/fish-shell

commit 2c2ab0c1fac1c982a682594d920e8fef735106fd

Always `source` file after `funced` (#10318)
... even if the file hasn't changed. This addresses an oddity in the following
case:

* Shell is started,
* function `foo` is sourced from foo.fish
* foo.fish is *externally* edited and saved
* <Loaded definition of `foo` is now stale, but fish is unaware>
* `funced foo` loads `type -p foo` showing changed definition, user exits
  $EDITOR saving no changes (or with $status 0, more generally).
* Stale definition of `foo` remains

share/functions/funced.fish
@@ -115,7 +115,9 @@ function funced --description 'Edit function definition'
                 if test "$new_checksum" = "$checksum"
                     echo (_ "Editor exited but the function was not modified")
                     echo (_ "If the editor is still running, check if it waits for completion, maybe a '--wait' option?")
-                    # Don't source or save an unmodified file.
+                    # Source but don't save an unmodified file.
+                    # (Source in case the file changed externally since we first loaded it.)
+                    source "$writepath"
                     break
                 end
             end

commit 5c3a0251b77c6cc656a2510887eebb8adaf6aa3a

funced: don't try to source interactive-only function
Regressed in 2c2ab0c1f (Always `source` file after `funced` (#10318),
2024-02-22) which was only intended for functions that are backed by a file.

share/functions/funced.fish
@@ -104,7 +104,9 @@ function funced --description 'Edit function definition'
                     echo (_ "If the editor is still running, check if it waits for completion, maybe a '--wait' option?")
                     # Source but don't save an unmodified file.
                     # (Source in case the file changed externally since we first loaded it.)
-                    source "$writepath"
+                    if set -q writepath[1]
+                        source "$writepath"
+                    end
                     break
                 end
             end
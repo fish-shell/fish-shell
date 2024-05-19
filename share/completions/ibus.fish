255c8da22d85aa44b6f53c03be4d28065afbda7f
tesuji <15225902+tesuji@users.noreply.github.com>
2024-05-19 21:05:40 +0700

    add completions for ibus (#10500)

---
+++ share/completions/ibus.fish
@@ -0,0 +1,14 @@
+# Completions for `ibus` (https://github.com/ibus/ibus/)
+
+complete -c ibus -n __fish_use_subcommand -f -a engine       -d 'Set or get engine'
+complete -c ibus -n __fish_use_subcommand -f -a exit         -d 'Exit ibus-daemon'
+complete -c ibus -n __fish_use_subcommand -f -a list-engine  -d 'Show available engines'
+complete -c ibus -n __fish_use_subcommand -f -a restart      -d 'Restart ibus-daemon'
+complete -c ibus -n __fish_use_subcommand -f -a version      -d 'Show version'
+complete -c ibus -n __fish_use_subcommand -f -a read-cache   -d 'Show the content of registry cache'
+complete -c ibus -n __fish_use_subcommand -f -a write-cache  -d 'Create registry cache'
+complete -c ibus -n __fish_use_subcommand -f -a address      -d 'Print the D-Bus address of ibus-daemon'
+complete -c ibus -n __fish_use_subcommand -f -a read-config  -d 'Show the configuration values'
+complete -c ibus -n __fish_use_subcommand -f -a reset-config -d 'Reset the configuration values'
+complete -c ibus -n __fish_use_subcommand -f -a emoji        -d 'Save emoji on dialog to clipboard'
+complete -c ibus -n __fish_use_subcommand -f -a help

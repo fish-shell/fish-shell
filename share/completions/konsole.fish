7c89de0ee26fc0a85f942c9df0d67685621b2610
Yuntao Zhao <yuntaz@proton.me>
2024-05-26 16:34:45 -0700

    Improve konsole profile completion to be dynamic
    Directly complete --profile as a long argument

--- share/completions/konsole.fish
+++ share/completions/konsole.fish
@@ -8,19 +8,13 @@ complete -c konsole -l version -xs v -d 'Show version and exit'
 complete -c konsole -l author -d 'Show author info and exit'
 complete -c konsole -l license -d 'Show license info and exit'
 complete -c konsole -l desktopfile -d 'Base file name of desktop entry'
-
-set -l profiles (konsole --list-profiles)
-complete -c konsole -l profile -d 'Name of profile to use'
-for p in $profiles
-    complete -c konsole -n "__fish_seen_subcommand_from --profile; and not __fish_seen_subcommand_from $profiles" -a $p
-end
-
-complete -c konsole -l layout -rF -d 'json layoutfile to be loaded'
+complete -c konsole -l profile -d 'Name of profile to use' -xa "(konsole --list-profiles)"
+complete -c konsole -l layout -r -d 'json layoutfile to be loaded'
 complete -c konsole -l builtin-profile -d 'Use the built-in profile'
 complete -c konsole -l workdir -d 'Set initial working dir' -xa "(__fish_complete_directories)"
 complete -c konsole -l hold -l noclose -d 'Do not close when command exists'
 complete -c konsole -l new-tab -d 'Create a new tab'
-complete -c konsole -l tabs-from-file -rF -d 'Create tabs from tabs config'
+complete -c konsole -l tabs-from-file -r -d 'Create tabs from tabs config'
 complete -c konsole -l background-mode -d 'Start Konsole in the background'
 complete -c konsole -l separate -l nofork -d 'Run in a separate process'
 complete -c konsole -l show-menubar -d 'Show the menubar'
@@ -29,8 +23,8 @@ complete -c konsole -l show-tabbar -d 'Show the tabbar'
 complete -c konsole -l hide-tabbar -d 'Hide the tabbar'
 complete -c konsole -l fullscreen -d 'Start Konsole in fullscreen mode'
 complete -c konsole -l notransparency -d 'Disable transparent backgrounds'
-complete -c konsole -l list-profiles -x -d 'List the available profiles'
-complete -c konsole -l list-profile-properties -x -d 'List all the profile properties'
-complete -c konsole -s p -x -d '<P=V> Change profile property'
-complete -c konsole -s e -x -d '<CMD> Command to execute'
+complete -c konsole -l list-profiles -d 'List the available profiles'
+complete -c konsole -l list-profile-properties -d 'List all the profile properties'
+complete -c konsole -xs p -d '<P=V> Change profile property'
+complete -c konsole -xs e -d '<CMD> Command to execute'
 complete -c konsole -l force-reuse -d 'Force re-using existing instance'

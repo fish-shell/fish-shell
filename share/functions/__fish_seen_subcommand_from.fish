4e0cb5d3e9719ab0ae583be3363123ea99e07d17
Johannes Altmanninger <aclopte@gmail.com>
2024-07-20 13:09:59 +0200

    __fish_seen_subcommand_from: don't clobber global variable

--- share/functions/__fish_seen_subcommand_from.fish
+++ share/functions/__fish_seen_subcommand_from.fish
@@ -5,6 +5,6 @@
 #
 
 function __fish_seen_subcommand_from
-    set regex (string escape --style=regex -- (commandline -pxc)[2..] | string join '|')
+    set -l regex (string escape --style=regex -- (commandline -pxc)[2..] | string join '|')
     string match -rq -- '^'$regex'$' $argv
 end

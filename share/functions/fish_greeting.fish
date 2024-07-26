264677e22ff325fed24858a0d577843ceba25e8a
Marcelo Lacerda <marceloslacerda@users.noreply.github.com>
2024-07-26 08:12:55 -0300

    Upgrade fish_greeting.fish with terseness
    "Go fish" could also become the official slogan of the project

--- share/functions/fish_greeting.fish
+++ share/functions/fish_greeting.fish
@@ -1,7 +1,7 @@
 function fish_greeting
     if not set -q fish_greeting
         set -l line1 (_ 'Welcome to fish, the friendly interactive shell')
-        set -l line2 \n(printf (_ 'Type %shelp%s for instructions on how to use fish') (set_color green) (set_color normal))
+        set -l line2 \n(printf (_ 'Type %shelp%s for instructions on how to fish') (set_color green) (set_color normal))
         set -g fish_greeting "$line1$line2"
     end
 

fish-shell/fish-shell

commit 2bfa7db7bce2b5a09249c4bbdc95a7e7a5e4ba48

Restructure __fish_seen_subcommand_from
Both are plenty fast enough, but this way the output of fish_trace isn't
completely taken over by the loops (seems fair since fish_trace probably gets
used rather heavily for completions).

share/functions/__fish_seen_subcommand_from.fish
@@ -1,16 +1,10 @@
 #
 # Test to see if we've seen a subcommand from a list.
 # This logic may seem backwards, but the commandline will often be much shorter
-#  than the list
+# than the list
 #
 
 function __fish_seen_subcommand_from
-    set -l cmd (commandline -pxc)
-    set -e cmd[1]
-    for i in $cmd
-        if contains -- $i $argv
-            return 0
-        end
-    end
-    return 1
+    set regex (string escape --style=regex -- (commandline -pxc)[2..] | string join '|')
+    string match -rq -- '^'$regex'$' $argv
 end

commit bd5f9babd7cc57ea5467772073bcab39ee40db59

__fish_seen_subcommand_from: Fix error when there's no second token
Regression from 2bfa7db7bce2b5a09249c4bbdc95a7e7a5e4ba48

Can be triggered e.g. with `complete -C"history "`.

share/functions/__fish_seen_subcommand_from.fish
@@ -6,5 +6,5 @@
 
 function __fish_seen_subcommand_from
     set -l regex (string escape --style=regex -- (commandline -pxc)[2..] | string join '|')
-    string match -rq -- '^'$regex'$' $argv
+    string match -rq -- "^$regex"'$' $argv
 end

tests/checks/complete.fish
@@ -614,3 +614,10 @@ function __fish_describe_command
 end
 test (count (complete -C"wh" | string match -rv "\tcommand|^while")) -gt 0 && echo "found" || echo "fail"
 # CHECK: found
+
+set -l commands check search show
+complete -c testcommand -n "not __fish_seen_subcommand_from $commands" -a 'check\t"Check the frobnicator" search\t"Search for frobs" show\t"Show all frobs"'
+complete -C'testcommand '
+# CHECK: check{{\t}}Check the frobnicator
+# CHECK: search{{\t}}Search for frobs
+# CHECK: show{{\t}}Show all frobs
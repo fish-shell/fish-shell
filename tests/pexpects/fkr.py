045cf967a1ebb86165168de9bd53ca30a1acb22d
Fabian Boehm <FHomborg@gmail.com>
2024-08-11 14:49:08 +0200

    Convert ASCII DEL to \x7f
    Annoying when you press backspace in fish_key_reader

--- tests/pexpects/fkr.py
+++ tests/pexpects/fkr.py
@@ -29,11 +29,13 @@ expect_str("Press a key:")
 
 # Is a single control char echoed correctly?
 send("\x07")
+expect_str("# decoded from: \\x07\r\n")
 expect_str("bind ctrl-g 'do something'\r\n")
 
 # Is a non-ASCII UTF-8 sequence prefaced by an escape char handled correctly?
 sleep(0.020)
 send("\x1B")
+expect_str("# decoded from: \\e\r\n")
 expect_str("bind escape 'do something'\r\n")
 send("\u1234")
 expect_str("bind ሴ 'do something'\r\n")
@@ -44,6 +46,7 @@ send("\x00")
 expect_str("bind ctrl-space 'do something'\r\n")
 
 send("\x1b\x7f")
+expect_str("# decoded from: \\e\\x7f\r\n")
 expect_str("bind alt-backspace 'do something'\r\n")
 
 send("\x1c")

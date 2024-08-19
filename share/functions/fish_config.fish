fish-shell/fish-shell

commit f3b950157de40b07afb6fd14946e20b98c9b5e26

Remove special case for fish_right_prompt in config.fish

share/functions/fish_config.fish
@@ -96,7 +96,7 @@ function fish_config --description "Launch fish's web based configuration"
                     end
 
                     # Erase the right prompt if it didn't have any.
-                    if functions -q fish_right_prompt; and test (functions --details fish_right_prompt) !=  $have[1]
+                    if functions -q fish_right_prompt; and test (functions --details fish_right_prompt) != $have[1]
                         functions --erase fish_right_prompt
                     end
                 case save
@@ -123,16 +123,7 @@ function fish_config --description "Launch fish's web based configuration"
                         funcsave fish_prompt
                         or return
 
-                        if functions -q fish_right_prompt; and test (functions --details fish_right_prompt) =  $have[1]
-                            # We just read a right prompt, save it.
-                            funcsave fish_right_prompt
-                        else if test -e "$__fish_config_dir/functions/fish_right_prompt.fish"; and test (functions --details fish_right_prompt) != "$have[1]"
-                            # We used to have a right prompt, overwrite it with an empty one.
-                            function fish_right_prompt
-                            end
-                            funcsave fish_right_prompt
-                        end
-
+                        funcsave fish_right_prompt
                         return
                     else
                         echo Not overwriting

share/functions/funcsave.fish
@@ -1,5 +1,5 @@
 function funcsave --description "Save the current definition of all specified functions to file"
-    set -l q options h/help d/directory=
+    set -l options q h/help d/directory=
     argparse -n funcsave $options -- $argv
     or return
 

commit c9bc04f2745a3df18937844d9f13ada8aa59ca45

fish_config: Write an empty prompt if necessary
This clears the right prompt if the new preset doesn't have any.

This was supposed to be fixed but was broken again in
f3b950157de40b07afb6fd14946e20b98c9b5e26.

Fixes #10675.

share/functions/fish_config.fish
@@ -118,12 +118,18 @@ function fish_config --description "Launch fish's web based configuration"
                         # or we'd throw an error on a stock fish.
                         path is $__fish_config_dir/functions/fish_prompt.fish
                         and cp $__fish_config_dir/functions/fish_prompt.fish{,.bak}
+                        path is $__fish_config_dir/functions/fish_right_prompt.fish
+                        and cp $__fish_config_dir/functions/fish_right_prompt.fish{,.bak}
 
                         set -l have
                         if set -q argv[1]
                             for f in $prompt_dir/$argv[1].fish
                                 if test -f $f
                                     set have $f
+                                    # Set the functions to empty so we empty the file
+                                    # if necessary.
+                                    function fish_prompt; end
+                                    function fish_right_prompt; end
                                     source $f
                                     or return 2
                                 end
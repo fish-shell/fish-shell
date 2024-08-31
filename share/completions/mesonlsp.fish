d05b375a2ddfbbcc99bb60bab1916cab3c59ffc9
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-08-31 22:28:51 +1000

    feat: add mesonlsp completion

---
+++ share/completions/mesonlsp.fish
@@ -0,0 +1,9 @@
+complete -c mesonlsp -l help -d 'Show help'
+complete -c mesonlsp -l version -d 'Show version'
+
+complete -c mesonlsp -l path -d 'Specify path to parse 100x times'
+complete -c mesonlsp -l lsp -d 'Start language server using stdio'
+complete -c mesonlsp -l wrap -d 'Extract and parse this wrap file'
+complete -c mesonlsp -l wrap-output -d 'Set the directory into that the given wraps should be extracted'
+complete -c mesonlsp -l wrap-package-files -d 'Set the location of the package files containing auxiliary files'
+complete -c mesonlsp -l full -d 'Fully setup and check a project'

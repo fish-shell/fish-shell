e14263a218e61cf5bc7981566fff87d3e70e68d6
DevAtDawn <86164137+DevAtDawn@users.noreply.github.com>
2024-09-05 04:33:17 -0600

    Create fish_commandline_stdin.fish

---
+++ share/functions/fish_commandline_stdin.fish
@@ -0,0 +1,10 @@
+# Pipe data to commandline
+function fish_commandline_stdin
+    read -l line; commandline -a $line
+end
+# function clia
+    # read -l line; commandline -a $line
+# end
+# function to_buffer
+   # read -l line; commandline -a $line
+# end

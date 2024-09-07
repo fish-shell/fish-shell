17ba349476be84d21fc59d9de9c112903b964007
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-09-08 05:19:42 +1000

    feat(completion): add test-spice support

---
+++ share/completions/test-spice.fish
@@ -0,0 +1,4 @@
+complete -c test-spice -l help -d 'Show help'
+
+complete -c test-spice -s r -l remove -d 'Remove all test spices'
+complete -c test-spice -s s -l skip -d 'Skip validation with `validate-spice`'

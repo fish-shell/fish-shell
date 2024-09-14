ca613ee7b034239f66b085fbd539d4771ae7e023
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-09-15 07:50:43 +1000

    feat(completion): support termux-call-log command

---
+++ share/completions/termux-call-log.fish
@@ -0,0 +1,17 @@
+set command termux-call-log
+
+complete -c $command \
+    -s h \
+    -d 'Show [h]elp'
+
+complete -c $command \
+    -a '10\tdefault' \
+    -s l \
+    -d '[l]imit the amount of listed calls' \
+    -x
+
+complete -c $command \
+    -a '0\tdefault' \
+    -s o \
+    -d 'Start listing calls with a specific one' \
+    -x

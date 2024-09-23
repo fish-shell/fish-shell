308ed62d83b4787df4e9f5003a2f2d4e99c7f000
Johannes Altmanninger <aclopte@gmail.com>
2024-09-23 20:07:27 +0200

    fish_key_reader: stop emitting \n for ctrl-j
    I guess it's nice to know that these two are the same but that info is not
    needed here, it just adds confusion. The user must have pressed ctrl-j if
    we get here, so echo that back.
    
    See the parent commit.

--- src/key.rs
+++ src/key.rs
@@ -421,7 +421,6 @@ fn ctrl_to_symbol(buf: &mut WString, c: char) {
     let c = u8::try_from(c).unwrap();
     let symbolic_name = match c {
         9 => L!("\\t"),
-        10 => L!("\\n"),
         13 => L!("\\r"),
         27 => L!("\\e"),
         _ => return sprintf!(=> buf, "\\x%02x", c),

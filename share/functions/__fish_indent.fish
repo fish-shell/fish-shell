8447c32d65b4bfaecc3cfaf7a61bb7d419c7bc9c
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2024-10-14 19:01:41 -0500

    __fish_indent: Cache `fish_indent` location

--- share/functions/__fish_indent.fish
+++ share/functions/__fish_indent.fish
@@ -1,8 +1,11 @@
-function __fish_indent
-    set -l dir "$(path dirname -- (status fish-path 2>/dev/null))"
-    if command -v $dir/fish_indent >/dev/null
+set -l dir "$(path dirname -- (status fish-path 2>/dev/null))"
+
+if command -v $dir/fish_indent >/dev/null
+    function __fish_indent --wraps fish_indent --inherit-variable dir
         $dir/fish_indent $argv
-    else
+    end
+else
+    function __fish_indent --wraps fish_indent
         fish_indent $argv
     end
 end

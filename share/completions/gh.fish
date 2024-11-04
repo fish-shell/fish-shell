d7f4ad7ab8193b76a1f1af48f7e875cdc70ba09b
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2024-11-04 16:33:30 -0600

    Silence gh warnings re unable to read plugins dir
    Unlike the errors, these *do* get printed to stderr.

--- share/completions/gh.fish
+++ share/completions/gh.fish
@@ -1,6 +1,6 @@
 # gh, at least as of version 1.17.5, does not write errors to stderr, causing
 # `checks/completions.fish` to fail if the `gh-completion` module is missing.
 # It also does not exit with a non-zero error code, making this harder than it needs to be :(
-set completion "$(gh completion --shell fish)"
+set completion "$(gh completion --shell fish 2>/dev/null)"
 string match -rq '^Error' -- $completion && return -1
 echo $completion | source

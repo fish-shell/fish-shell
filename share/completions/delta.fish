bb57f2391fde0edd9ef6853ed844e8673d7edd3d
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2024-11-12 09:59:10 -0600

    Fix a bad comment and clarify `delta` behavior
    s/fish/delta/ and provide a better explanation.

--- share/completions/delta.fish
+++ share/completions/delta.fish
@@ -1,2 +1,3 @@
-# Old versions of fish do not have a --generate-completion option!
+# Older versions of delta do not have a --generate-completion option and will complain
+# to stderr but emit nothing to stdout, making it safe (but a no-op) to source.
 delta --generate-completion fish 2>/dev/null | source

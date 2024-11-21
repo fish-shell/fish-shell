36c5ee045c9bab98d13008b51a63baa65ac9bd79
Fabian Boehm <FHomborg@gmail.com>
2024-11-21 21:20:35 +0100

    fixup! filter control sequences

--- tests/filter-control-sequences.fish
+++ tests/filter-control-sequences.fish
@@ -1,14 +1,11 @@
-#!/usr/bin/fish
 # Remove the sorts of escape sequences interactive fish prints.
 
-# First the enable sequences
+# First the enable sequences, then a "|" combiner and then the disable ones
 set -l escapes "\e\[\?2004h"\
 "\e\[>4;1m"\
 "\e\[>5u"\
 "\e="\
-# or
 "|"\
-# the disable sequences
 "\e\[\?2004l"\
 "\e\[>4;0m"\
 "\e\[<1u"\

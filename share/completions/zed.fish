67fa71a94d0f09e87b0cdbcec3ce82211fd214b3
Klaus Hipp <khipp@users.noreply.github.com>
2024-12-17 02:42:57 +0100

    Update `zed` completions (#10907)
    Add uninstall option.

--- share/completions/zed.fish
+++ share/completions/zed.fish
@@ -5,4 +5,5 @@ complete -c zed -s v -l version -d "Print Zed's version and the app path"
 complete -c zed -l foreground -d "Run zed in the foreground (useful for debugging)"
 complete -c zed -l zed -d "Custom path to Zed.app or the zed binary" -r
 complete -c zed -l dev-server-token -d "Run zed in dev-server mode" -r
+complete -c zed -l uninstall -d "Uninstall Zed from user system"
 complete -c zed -s h -l help -d "Print help (see a summary with '-h')"

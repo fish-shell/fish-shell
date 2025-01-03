670541eec8b99897cd462b3fd2bc53b238b8b1fe
David Adam <zanchey@ucc.gu.uwa.edu.au>
2025-01-03 12:38:14 +0800

    fish_jj_prompt: don't error if jj not installed

--- share/functions/fish_jj_prompt.fish
+++ share/functions/fish_jj_prompt.fish
@@ -1,4 +1,9 @@
 function fish_jj_prompt
+    # If jj isn't installed, there's nothing we can do
+    # Return 1 so the calling prompt can deal with it
+    if not command -sq jj
+        return 1
+    end
     jj log 2>/dev/null --no-graph --ignore-working-copy --color=always --revisions @ \
         --template '
             concat(

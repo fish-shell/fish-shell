fish-shell/fish-shell

commit 610338cc708f25434a2b2c4e2537489f7d9e078a

On undo after execute, restore the cursor position
Ever since 149594f974 (Initial revision, 2005-09-20), we move the
cursor to the end of the commandline just before executing it.

This is so we can move the cursor to the line below the command line,
so moving the cursor is relevant if one presses enter on say, the
first line of a multi-line commandline.

As mentioned in #10838 and others, it can be useful to restore the
cursor position when recalling commandline from history. Make undo
restore the position where enter was pressed, instead of implicitly
moving the cursor to the end. This allows to quickly correct small
mistakes in large commandlines that failed recently.

This requires a new way of moving the cursor below the command line.
Test changes include unrelated cleanup of history.py.

src/reader.rs
@@ -1972,11 +1972,8 @@ impl<'a> Reader<'a> {
             zelf.finish_highlighting_before_exec();
         }
 
-        // Emit a newline so that the output is on the line after the command.
-        // But do not emit a newline if the cursor has wrapped onto a new line all its own - see #6826.
-        if !zelf.screen.cursor_is_wrapped_to_own_line() {
-            let _ = write_to_fd(b"\n", STDOUT_FILENO);
-        }
+        // Move the cursor so that output is on the line after the command.
+        zelf.screen.move_to_end();
 
         // HACK: If stdin isn't the same terminal as stdout, we just moved the cursor.
         // For now, just reset it to the beginning of the line.
@@ -3582,7 +3579,6 @@ impl<'a> Reader<'a> {
 
         self.add_to_history();
         self.rls_mut().finished = true;
-        self.update_buff_pos(elt, Some(self.command_line_len()));
         true
     }
 

src/screen.rs
@@ -473,6 +473,10 @@ impl Screen {
         self.save_status();
     }
 
+    pub fn move_to_end(&mut self) {
+        self.r#move(0, self.actual.line_count());
+    }
+
     /// Resets the screen buffer's internal knowledge about the contents of the screen,
     /// abandoning the current line and going to the next line.
     /// If clear_to_eos is set,

build_tools/pexpect_helper.py
@@ -129,6 +129,10 @@ class Message(object):
         """Return a output message with the given text."""
         return Message(Message.DIR_OUTPUT, text, when)
 
+# Sequences for moving the cursor below the commandline. This happens before executing.
+MOVE_TO_END: str = r"(?:\r\n|\x1b\[2 q)"
+TO_END: str  = MOVE_TO_END + r"[^\n]*"
+TO_END_SUFFIX: str  = r"[^\n]*" + MOVE_TO_END
 
 class SpawnedProc(object):
     """A process, talking to our ptty. This wraps pexpect.spawn.

tests/pexpects/bind.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from pexpect_helper import SpawnedProc
+from pexpect_helper import SpawnedProc, TO_END
 import os
 import platform
 import sys
@@ -42,7 +42,7 @@ expect_prompt("")
 # Start by testing with no delay. This should transpose the words.
 send("echo abc def")
 send("\033t\r")
-expect_prompt("\r\n.*def abc\r\n")  # emacs transpose words, default timeout: no delay
+expect_prompt(TO_END + "def abc\r\n")  # emacs transpose words, default timeout: no delay
 
 # Now test with a delay > 0 and < the escape timeout. This should transpose
 # the words.
@@ -51,7 +51,7 @@ send("\033")
 sleep(0.010)
 send("t\r")
 # emacs transpose words, default timeout: short delay
-expect_prompt("\r\n.*jkl ghi\r\n")
+expect_prompt(TO_END + "jkl ghi\r\n")
 
 # Now test with a delay > the escape timeout. The transposition should not
 # occur and the "t" should become part of the text that is echoed.
@@ -60,11 +60,11 @@ send("\033")
 sleep(0.250)
 send("t\r")
 # emacs transpose words, default timeout: long delay
-expect_prompt("\r\n.*mno pqrt\r\n")
+expect_prompt(TO_END + "mno pqrt\r\n")
 
 # Now test that exactly the expected bind modes are defined
 sendline("bind --list-modes")
-expect_prompt("\r\n.*default", unmatched="Unexpected bind modes")
+expect_prompt(TO_END + "default", unmatched="Unexpected bind modes")
 
 # Test vi key bindings.
 # This should leave vi mode in the insert state.
@@ -74,7 +74,7 @@ expect_prompt()
 # Go through a prompt cycle to let fish catch up, it may be slow due to ASAN
 sendline("echo success: default escape timeout")
 expect_prompt(
-    "\r\n.*success: default escape timeout", unmatched="prime vi mode, default timeout"
+    TO_END + "success: default escape timeout", unmatched="prime vi mode, default timeout"
 )
 
 send("echo fail: default escape timeout")
@@ -88,7 +88,7 @@ sleep(0.250)
 send("ddi")
 sendline("echo success: default escape timeout")
 expect_prompt(
-    "\r\n.*success: default escape timeout\r\n",
+    TO_END + "success: default escape timeout\r\n",
     unmatched="vi replace line, default timeout: long delay",
 )
 
@@ -103,7 +103,7 @@ send("\033")
 sleep(0.400)
 send("hhrAi\r")
 expect_prompt(
-    "\r\n.*TAXT\r\n", unmatched="vi mode replace char, default timeout: long delay"
+    TO_END + "TAXT\r\n", unmatched="vi mode replace char, default timeout: long delay"
 )
 
 # Test deleting characters with 'x'.
@@ -115,7 +115,7 @@ send("xxxxx\r")
 
 # vi mode delete char, default timeout: long delay
 expect_prompt(
-    "\r\n.*MORE\r\n", unmatched="vi mode delete char, default timeout: long delay"
+    TO_END + "MORE\r\n", unmatched="vi mode delete char, default timeout: long delay"
 )
 
 # Test jumping forward til before a character with t
@@ -127,7 +127,7 @@ send("0tTD\r")
 
 # vi mode forward-jump-till character, default timeout: long delay
 expect_prompt(
-    "\r\n.*MORE\r\n",
+    TO_END + "MORE\r\n",
     unmatched="vi mode forward-jump-till character, default timeout: long delay",
 )
 
@@ -140,7 +140,7 @@ expect_prompt(
 # send("TSD\r")
 # # vi mode backward-jump-till character, default timeout: long delay
 # expect_prompt(
-#     "\r\n.*MORE-TEXT-IS\r\n",
+#     TO_END + "MORE-TEXT-IS\r\n",
 #     unmatched="vi mode backward-jump-till character, default timeout: long delay",
 # )
 
@@ -152,7 +152,7 @@ sleep(0.250)
 send("F-;D\r")
 # vi mode backward-jump-to character and repeat, default timeout: long delay
 expect_prompt(
-    "\r\n.*MORE-TEXT\r\n",
+    TO_END + "MORE-TEXT\r\n",
     unmatched="vi mode backward-jump-to character and repeat, default timeout: long delay",
 )
 
@@ -164,7 +164,7 @@ sleep(0.250)
 send("F-F-,D\r")
 # vi mode backward-jump-to character, and reverse, default timeout: long delay
 expect_prompt(
-    "\r\n.*MORE-TEXT-IS\r\n",
+    TO_END + "MORE-TEXT-IS\r\n",
     unmatched="vi mode backward-jump-to character, and reverse, default timeout: long delay",
 )
 
@@ -179,7 +179,7 @@ send("ddi")
 sleep(0.25)
 send("echo success: lengthened escape timeout\r")
 expect_prompt(
-    "\r\n.*success: lengthened escape timeout\r\n",
+    TO_END + "success: lengthened escape timeout\r\n",
     unmatched="vi replace line, 100ms timeout: long delay",
 )
 
@@ -191,7 +191,7 @@ sleep(0.010)
 send("ddi")
 send("inserted\r")
 expect_prompt(
-    "\r\n.*fail: no normal modediinserted\r\n",
+    TO_END + "fail: no normal modediinserted\r\n",
     unmatched="vi replace line, 100ms timeout: short delay",
 )
 
@@ -208,7 +208,7 @@ expect_str("echo TEXT")
 send("\033")
 sleep(0.200)
 send("hhtTrN\r")
-expect_prompt("\r\n.*TENT\r\n", unmatched="Couldn't find expected output 'TENT'")
+expect_prompt(TO_END + "TENT\r\n", unmatched="Couldn't find expected output 'TENT'")
 
 # Test sequence key delay
 send("set -g fish_sequence_key_delay_ms 200\r")
@@ -239,7 +239,7 @@ expect_prompt("foo")
 # send("echo some TExT\033")
 # sleep(0.300)
 # send("hh~~bbve~\r")
-# expect_prompt("\r\n.*SOME TeXT\r\n", unmatched="Couldn't find expected output 'SOME TeXT")
+# expect_prompt(TO_END + "SOME TeXT\r\n", unmatched="Couldn't find expected output 'SOME TeXT")
 
 # Now test that exactly the expected bind modes are defined
 sendline("bind --list-modes")
@@ -255,7 +255,7 @@ expect_prompt()
 # Verify the custom escape timeout set earlier is still in effect.
 sendline("echo fish_escape_delay_ms=$fish_escape_delay_ms")
 expect_prompt(
-    "\r\n.*fish_escape_delay_ms=50\r\n",
+    TO_END + "fish_escape_delay_ms=50\r\n",
     unmatched="default-mode custom timeout not set correctly",
 )
 
@@ -270,7 +270,7 @@ send("echo abc def")
 send("\033")
 send("t\r")
 expect_prompt(
-    "\r\n.*def abc\r\n", unmatched="emacs transpose words fail, 200ms timeout: no delay"
+    TO_END + "def abc\r\n", unmatched="emacs transpose words fail, 200ms timeout: no delay"
 )
 
 # Verify special characters, such as \cV, are not intercepted by the kernel
@@ -301,7 +301,7 @@ expect_prompt()
 send("foo ")
 expect_str("echo foonanana")
 send(" banana\r")
-expect_str(" banana\r")
+expect_str(" banana")
 expect_prompt("foonanana banana")
 
 # Ensure that nul can be bound properly (#3189).
@@ -329,7 +329,7 @@ expect_prompt()
 send("a b c d\x01")  # ctrl-a, move back to the beginning of the line
 send("\x07")  # ctrl-g, kill bigword
 sendline("echo")
-expect_prompt("\n.*b c d")
+expect_prompt(TO_END + "b c d")
 
 # Test that overriding the escape binding works
 # and does not inhibit other escape sequences (up-arrow in this case).
@@ -345,7 +345,7 @@ expect_prompt()
 send("    a b c d\x01")  # ctrl-a, move back to the beginning of the line
 send("\x07")  # ctrl-g, kill bigword
 sendline("echo")
-expect_prompt("\n.*b c d")
+expect_prompt(TO_END + "b c d")
 
 # Check that ctrl-z can be bound
 sendline('bind ctrl-z "echo bound ctrl-z"')

tests/pexpects/bind_mode_events.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from pexpect_helper import SpawnedProc
+from pexpect_helper import SpawnedProc, TO_END
 import os
 import sys
 import signal
@@ -16,7 +16,7 @@ send("set -g fish_key_bindings fish_vi_key_bindings\r")
 expect_prompt()
 
 send("echo ready to go\r")
-expect_prompt(f"\r\n.*ready to go\r\n")
+expect_prompt(TO_END + f"ready to go\r\n")
 send(
     "function add_change --on-variable fish_bind_mode ; set -g MODE_CHANGES $MODE_CHANGES $fish_bind_mode ; end\r"
 )
@@ -42,7 +42,7 @@ send("i")
 sleep(10 if "CI" in os.environ else 1)
 
 send("echo mode changes: $MODE_CHANGES\r")
-expect_prompt("\r\n.*mode changes: default insert default insert\r\n")
+expect_prompt(TO_END + "mode changes: default insert default insert\r\n")
 
 # Regression test for #8125.
 # Control-C should return us to insert mode.
@@ -70,4 +70,4 @@ sleep(timeout)
 
 # We should be back in insert mode now.
 send("echo mode changes: $MODE_CHANGES\r")
-expect_prompt("\r\n.*mode changes: default insert\r\n")
+expect_prompt(TO_END + "mode changes: default insert\r\n")

tests/pexpects/complete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from pexpect_helper import SpawnedProc
+from pexpect_helper import SpawnedProc, TO_END
 
 sp = SpawnedProc()
 send, sendline, sleep, expect_prompt, expect_re, expect_str = (
@@ -75,6 +75,6 @@ send("echo fo\t")
 expect_re("foooo")
 send("\x07")
 sendline("echo bar")
-expect_re("\n.*bar")
+expect_re(TO_END + "bar")
 sendline("echo fo\t")
 expect_re("foooo")

tests/pexpects/history.py
@@ -11,7 +11,7 @@
 
 # The history function might pipe output through the user's pager. We don't
 # want something like `less` to complicate matters so force the use of `cat`.
-from pexpect_helper import SpawnedProc
+from pexpect_helper import SpawnedProc, TO_END, TO_END_SUFFIX
 import os
 
 os.environ["PAGER"] = "cat"
@@ -98,7 +98,7 @@ expect_prompt("echo start1; builtin history; echo end1\r\n")
 # ==========
 # Delete a single command we recently ran.
 sendline("history delete -e -C 'echo hello'")
-expect_prompt("history delete -e -C 'echo hello'\r\n")
+expect_prompt("history delete -e -C 'echo hello'" + TO_END_SUFFIX)
 sendline("echo count hello (history search -e -C 'echo hello' | wc -l | string trim)")
 expect_prompt("count hello 0\r\n")
 
@@ -107,12 +107,13 @@ expect_prompt("count hello 0\r\n")
 # delete the first entry matched by the prefix search (the most recent command
 # sent above that matches).
 sendline("history delete -p 'echo hello'")
-expect_re("history delete -p 'echo hello'\r\n")
-expect_re("\[1\] echo hello AGAIN\r\n")
-expect_re("\[2\] echo hello again\r\n\r\n")
-expect_re(
-    "Enter nothing to cancel the delete, or\r\nEnter one or more of the entry IDs or ranges like '5..12', separated by a space.\r\nFor example '7 10..15 35 788..812'.\r\nEnter 'all' to delete all the matching entries.\r\n"
-)
+expect_re("history delete -p 'echo hello'" + TO_END_SUFFIX)
+expect_re("\[1\] echo hello AGAIN" + TO_END_SUFFIX)
+expect_re("\[2\] echo hello again" + TO_END_SUFFIX)
+expect_re("Enter nothing to cancel the delete, or\r\n")
+expect_re("Enter one or more of the entry IDs or ranges like '5..12', separated by a space.\r\n")
+expect_re("For example '7 10..15 35 788..812'.\r\n")
+expect_re("Enter 'all' to delete all the matching entries.\r\n")
 expect_re("Delete which entries\? ")
 sendline("1")
 expect_prompt('Deleting history entry 1: "echo hello AGAIN"\r\n')
@@ -177,7 +178,7 @@ expect_prompt()
 sendline("history clear-session")
 expect_prompt()
 sendline("history search --exact 'echo after' | cat")
-expect_prompt("\r\n")
+expect_prompt()
 
 # Check history filtering
 # We store anything that starts with "echo ephemeral".

tests/pexpects/read.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from pexpect_helper import SpawnedProc
+from pexpect_helper import SpawnedProc, TO_END
 
 sp = SpawnedProc()
 send, sendline, sleep, expect_prompt, expect_re, expect_str = (
@@ -17,7 +17,7 @@ def expect_read_prompt():
 
 
 def expect_marker(text):
-    expect_prompt("\r\n.*@MARKER:" + str(text) + "@\\r\\n")
+    expect_prompt(TO_END + "@MARKER:" + str(text) + "@\\r\\n")
 
 
 def print_var_contents(varname, expected):

commit 0e512f8033639a708f5e680a0a81c55e7018aeff

Fix spurious blank lines when executing scrolled commandline
The result of

	commandline -i ": '$(seq $LINES)"\n"first scrolled line'"

is a commandline that is scrolled by one line.

Before executing that commandline, we move the cursor down by one
too many line. This is a regression from 610338cc70 (On undo after
execute, restore the cursor position, 2024-12-21). Fix that.

The test also demonstrates an unrelated problem, probably specific
to tmux.

src/reader.rs
@@ -2134,7 +2134,9 @@ impl<'a> Reader<'a> {
 
         // Redraw the command line. This is what ensures the autosuggestion is hidden, etc. after the
         // user presses enter.
-        if zelf.is_repaint_needed(None) || zelf.screen.scrolled || zelf.conf.inputfd != STDIN_FILENO
+        if zelf.is_repaint_needed(None)
+            || zelf.screen.scrolled()
+            || zelf.conf.inputfd != STDIN_FILENO
         {
             zelf.layout_and_repaint_before_execution();
         }

src/screen.rs
@@ -189,8 +189,9 @@ impl ScreenData {
 pub struct Screen {
     /// Whether the last-drawn autosuggestion (if any) is truncated, or hidden entirely.
     pub autosuggestion_is_truncated: bool,
-    /// True if the last rendering was so large we could only display part of the command line.
-    pub scrolled: bool,
+    /// If the last rendering was so large we could only display part of the command line,
+    /// this is the number of lines that were pushed to scrollback.
+    pub scroll_amount: usize,
 
     /// Receiver for our output.
     outp: &'static RefCell<Outputter>,
@@ -227,7 +228,7 @@ impl Screen {
         Self {
             outp: Outputter::stdoutput(),
             autosuggestion_is_truncated: Default::default(),
-            scrolled: Default::default(),
+            scroll_amount: Default::default(),
             desired: Default::default(),
             actual: Default::default(),
             actual_left_prompt: Default::default(),
@@ -239,6 +240,10 @@ impl Screen {
             mtime_stdout: Default::default(),
             mtime_stderr: Default::default(),
         }
+    }
+
+    pub fn scrolled(&self) -> bool {
+        self.scroll_amount != 0
     }
 
     /// This is the main function for the screen output library. It is used to define the desired
@@ -459,7 +464,7 @@ impl Screen {
         // Append pager_data (none if empty).
         self.desired.append_lines(&page_rendering.screen_data);
 
-        self.scrolled = scrolled_cursor.scroll_amount != 0;
+        self.scroll_amount = scrolled_cursor.scroll_amount;
 
         self.update(
             vars,
@@ -500,7 +505,7 @@ impl Screen {
     }
 
     pub fn move_to_end(&mut self) {
-        self.r#move(0, self.actual.line_count());
+        self.r#move(0, self.actual.line_count() - self.scroll_amount);
     }
 
     pub fn push_to_scrollback(&mut self, cursor_y: usize) {
@@ -1035,14 +1040,15 @@ impl Screen {
         let term = term.as_ref();
 
         // Output the left prompt if it has changed.
-        if zelf.scrolled && !is_final_rendering {
+        if zelf.scrolled() && !is_final_rendering {
             zelf.r#move(0, 0);
             zelf.outp
                 .borrow_mut()
                 .tputs_if_some(&term.and_then(|term| term.clr_eol.as_ref()));
             zelf.actual_left_prompt.clear();
             zelf.actual.cursor.x = 0;
-        } else if left_prompt != zelf.actual_left_prompt || (zelf.scrolled && is_final_rendering) {
+        } else if left_prompt != zelf.actual_left_prompt || (zelf.scrolled() && is_final_rendering)
+        {
             zelf.r#move(0, 0);
             let mut start = 0;
             let osc_133_prompt_start =
@@ -1092,7 +1098,7 @@ impl Screen {
             // Note that skip_remaining is a width, not a character count.
             let mut skip_remaining = start_pos;
 
-            let shared_prefix = if zelf.scrolled {
+            let shared_prefix = if zelf.scrolled() {
                 0
             } else {
                 line_shared_prefix(o_line(&zelf, i), s_line(&zelf, i))

tests/checks/tmux-commandline.fish
@@ -32,6 +32,23 @@ isolated-tmux capture-pane -p
 # CHECK: 10
 # CHECK: scroll_here
 
+# The output is broken here (seems tmux specific).
+isolated-tmux send-keys C-c
+tmux-sleep
+isolated-tmux send-keys C-l 'commandline -i ": \'$(seq $LINES)" A B "C\'"' Enter Enter
+tmux-sleep
+isolated-tmux capture-pane -p
+# CHECK: 4
+# CHECK: 5
+# CHECK: 6
+# CHECK: 7
+# CHECK: 8
+# CHECK: 9
+# CHECK: 10
+# CHECK: prompt 5>
+# CHECK: B
+# CHECK: C'
+
 # Soft-wrapped commandline with omitted right prompt.
 isolated-tmux send-keys C-c
 tmux-sleep
@@ -43,7 +60,7 @@ isolated-tmux send-keys C-l '
 ' Enter
 tmux-sleep
 isolated-tmux capture-pane -p | sed 1,5d
-# CHECK: prompt 5> echo 00000000000000000000000000000000000000000000000000000000000000000
+# CHECK: prompt {{\d+}}> echo 00000000000000000000000000000000000000000000000000000000000000000
 # CHECK: 000000000000000
 # CHECK: 00000000000000000000000000000000000000000000000000000000000000000000000000000000
-# CHECK: prompt 6>                                                           right-prompt
+# CHECK: prompt {{\d+}}>                                                           right-prompt
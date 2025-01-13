fish-shell/fish-shell

commit 2e55e34544f57f88e0be5b331f434a082204bf33

Reformat

share/functions/__fish_complete_mount_opts.fish
@@ -135,7 +135,6 @@ function __fish_complete_mount_opts
     barrier={0,1}\
     user_xattr\
     acl\
-
     set -l token (commandline -tc | string replace -r '^-o' -- '')
     set -l args (string split , -- $token)
 

build_tools/littlecheck.py
@@ -13,6 +13,7 @@ import re
 import shlex
 import subprocess
 import sys
+
 try:
     from itertools import zip_longest
 except ImportError:
@@ -20,7 +21,7 @@ except ImportError:
 from difflib import SequenceMatcher
 
 # Directives can occur at the beginning of a line, or anywhere in a line that does not start with #.
-COMMENT_RE = r'^(?:[^#].*)?#\s*'
+COMMENT_RE = r"^(?:[^#].*)?#\s*"
 
 # A regex showing how to run the file.
 RUN_RE = re.compile(COMMENT_RE + r"RUN:\s+(.*)\n")
@@ -139,9 +140,10 @@ class Line(object):
             ret = ret.replace("{", "{{").replace("}", "}}")
         return ret
 
+
 class RunCmd(object):
-    """ A command to run on a given Checker.
-    
+    """A command to run on a given Checker.
+
     Attributes:
         args: Unexpanded shell command as a string.
     """
@@ -215,10 +217,16 @@ class TestFailure(object):
                 "",
             ]
         if self.error_annotation_lines:
-            fields["error_annotation"] = "    ".join([x.text for x in self.error_annotation_lines])
-            fields["error_annotation_lineno"] = str(self.error_annotation_lines[0].number)
+            fields["error_annotation"] = "    ".join(
+                [x.text for x in self.error_annotation_lines]
+            )
+            fields["error_annotation_lineno"] = str(
+                self.error_annotation_lines[0].number
+            )
             if len(self.error_annotation_lines) > 1:
-                fields["error_annotation_lineno"] += ":" + str(self.error_annotation_lines[-1].number)
+                fields["error_annotation_lineno"] += ":" + str(
+                    self.error_annotation_lines[-1].number
+                )
             fmtstrs += [
                 "  additional output on stderr:{error_annotation_lineno}:",
                 "    {BOLD}{error_annotation}{RESET}",
@@ -229,14 +237,19 @@ class TestFailure(object):
             lastcheckline = None
             for d in self.diff.get_grouped_opcodes():
                 for op, alo, ahi, blo, bhi in d:
-                    color="{BOLD}"
-                    if op == 'replace' or op == 'delete':
-                        color="{RED}"
+                    color = "{BOLD}"
+                    if op == "replace" or op == "delete":
+                        color = "{RED}"
                     # We got a new chunk, so we print a marker.
                     if alo > lasthi:
                         fmtstrs += [
-                            "    [...] from line " + str(self.checks[blo].line.number)
-                            + " (" + self.lines[alo].file + ":" + str(self.lines[alo].number) + "):"
+                            "    [...] from line "
+                            + str(self.checks[blo].line.number)
+                            + " ("
+                            + self.lines[alo].file
+                            + ":"
+                            + str(self.lines[alo].number)
+                            + "):"
                         ]
                     lasthi = ahi
 
@@ -244,17 +257,41 @@ class TestFailure(object):
                     lastcheck = False
                     for a, b in zip_longest(self.lines[alo:ahi], self.checks[blo:bhi]):
                         # Clean up strings for use in a format string - double up the curlies.
-                        astr = color + a.escaped_text(for_formatting=True) + "{RESET}" if a else ""
+                        astr = (
+                            color + a.escaped_text(for_formatting=True) + "{RESET}"
+                            if a
+                            else ""
+                        )
                         if b:
-                            bstr = "'{BLUE}" + b.line.escaped_text(for_formatting=True) + "{RESET}'" + " on line " + str(b.line.number)
+                            bstr = (
+                                "'{BLUE}"
+                                + b.line.escaped_text(for_formatting=True)
+                                + "{RESET}'"
+                                + " on line "
+                                + str(b.line.number)
+                            )
                             lastcheckline = b.line.number
 
-                        if op == 'equal':
+                        if op == "equal":
                             fmtstrs += ["    " + astr]
                         elif b and a:
-                            fmtstrs += ["    " + astr + " <= does not match " + b.type + " " + bstr]
+                            fmtstrs += [
+                                "    "
+                                + astr
+                                + " <= does not match "
+                                + b.type
+                                + " "
+                                + bstr
+                            ]
                         elif b:
-                            fmtstrs += ["    " + astr + " <= nothing to match " + b.type + " " + bstr]
+                            fmtstrs += [
+                                "    "
+                                + astr
+                                + " <= nothing to match "
+                                + b.type
+                                + " "
+                                + bstr
+                            ]
                         elif not b:
                             string = "    " + astr
                             if bhi == len(self.checks):
@@ -262,7 +299,10 @@ class TestFailure(object):
                                     string += " <= no more checks"
                                     lastcheck = True
                             elif lastcheckline is not None:
-                                string += " <= no check matches this, previous check on line " + str(lastcheckline)
+                                string += (
+                                    " <= no check matches this, previous check on line "
+                                    + str(lastcheckline)
+                                )
                             else:
                                 string += " <= no check matches"
                             fmtstrs.append(string)
@@ -276,8 +316,8 @@ class TestFailure(object):
 
 
 def perform_substitution(input_str, subs):
-    """ Perform the substitutions described by subs to str
-        Return the substituted string.
+    """Perform the substitutions described by subs to str
+    Return the substituted string.
     """
     # Sort our substitutions into a list of tuples (key, value), descending by length.
     # It needs to be descending because we need to try longer substitutions first.
@@ -368,11 +408,22 @@ class TestRun(object):
         # If there's a mismatch or still lines or checkers, we have a failure.
         # Otherwise it's success.
         if mismatches:
-            return TestFailure(mismatches[0][0], mismatches[0][1], self, diff=diff, lines=usedlines, checks=usedchecks)
+            return TestFailure(
+                mismatches[0][0],
+                mismatches[0][1],
+                self,
+                diff=diff,
+                lines=usedlines,
+                checks=usedchecks,
+            )
         elif lineq:
-            return TestFailure(lineq[-1], None, self, diff=diff, lines=usedlines, checks=usedchecks)
+            return TestFailure(
+                lineq[-1], None, self, diff=diff, lines=usedlines, checks=usedchecks
+            )
         elif checkq:
-            return TestFailure(None, checkq[-1], self, diff=diff, lines=usedlines, checks=usedchecks)
+            return TestFailure(
+                None, checkq[-1], self, diff=diff, lines=usedlines, checks=usedchecks
+            )
         else:
             # Success!
             return None
@@ -381,8 +432,8 @@ class TestRun(object):
         """ Run the command. Return a TestFailure, or None. """
 
         def split_by_newlines(s):
-            """ Decode a string and split it by newlines only,
-                retaining the newlines.
+            """Decode a string and split it by newlines only,
+            retaining the newlines.
             """
             return [s + "\n" for s in s.decode("utf-8").split("\n")]
 
@@ -420,7 +471,7 @@ class TestRun(object):
         # non-matching or unmatched stderr text, then annotate the outfail
         # with it.
         if outfail and errfail and errfail.line:
-            outfail.error_annotation_lines = errlines[errfail.line.number - 1:]
+            outfail.error_annotation_lines = errlines[errfail.line.number - 1 :]
             # Trim a trailing newline
             if outfail.error_annotation_lines[-1].text == "\n":
                 del outfail.error_annotation_lines[-1]
@@ -528,8 +579,8 @@ def check_path(path, subs, config, failure_handler):
 
 
 def parse_subs(subs):
-    """ Given a list of input substitutions like 'foo=bar',
-       return a dictionary like {foo:bar}, or exit if invalid.
+    """Given a list of input substitutions like 'foo=bar',
+    return a dictionary like {foo:bar}, or exit if invalid.
     """
     result = {}
     for sub in subs:

build_tools/pexpect_helper.py
@@ -79,7 +79,7 @@ def pexpect_error_type(err):
 
 
 class Message(object):
-    """ Some text either sent-to or received-from the spawned proc.
+    """Some text either sent-to or received-from the spawned proc.
 
     Attributes:
         dir: the message direction, either DIR_INPUT or DIR_OUTPUT
@@ -115,7 +115,7 @@ class Message(object):
 
 
 class SpawnedProc(object):
-    """ A process, talking to our ptty. This wraps pexpect.spawn.
+    """A process, talking to our ptty. This wraps pexpect.spawn.
 
     Attributes:
         colorize: whether error messages should have ANSI color escapes
@@ -127,15 +127,15 @@ class SpawnedProc(object):
     """
 
     def __init__(self, name="fish", timeout=TIMEOUT_SECS, env=os.environ.copy()):
-        """ Construct from a name, timeout, and environment.
+        """Construct from a name, timeout, and environment.
 
-            Args:
-                name: the name of the executable to launch, as a key into the
-                      environment dictionary. By default this is 'fish' but may be
-                      other executables.
-                timeout: A timeout to pass to pexpect. This indicates how long to wait
-                         before giving up on some expected output.
-                env: a string->string dictionary, describing the environment variables.
+        Args:
+            name: the name of the executable to launch, as a key into the
+                  environment dictionary. By default this is 'fish' but may be
+                  other executables.
+            timeout: A timeout to pass to pexpect. This indicates how long to wait
+                     before giving up on some expected output.
+            env: a string->string dictionary, describing the environment variables.
         """
         if name not in env:
             raise ValueError("'name' variable not found in environment" % name)
@@ -155,8 +155,8 @@ class SpawnedProc(object):
         return now - self.start_time
 
     def send(self, s):
-        """ Cover over pexpect.spawn.send().
-            Send the given string to the tty, returning the number of bytes written.
+        """Cover over pexpect.spawn.send().
+        Send the given string to the tty, returning the number of bytes written.
         """
         res = self.spawn.send(s)
         when = self.time_since_first_message()
@@ -164,22 +164,22 @@ class SpawnedProc(object):
         return res
 
     def sendline(self, s):
-        """ Cover over pexpect.spawn.sendline().
-            Send the given string + linesep to the tty, returning the number of bytes written.
+        """Cover over pexpect.spawn.sendline().
+        Send the given string + linesep to the tty, returning the number of bytes written.
         """
         return self.send(s + os.linesep)
 
     def expect_re(self, pat, pat_desc=None, unmatched=None, **kwargs):
-        """ Cover over pexpect.spawn.expect().
-            Consume all "new" output of self.spawn until the given pattern is matched, or
-            the timeout is reached.
-            Note that output between the current position and the location of the match is
-            consumed as well.
-            The pattern is typically a regular expression in string form, but may also be
-            any of the types accepted by pexpect.spawn.expect().
-            If the 'unmatched' parameter is given, it is printed as part of the error message
-            of any failure.
-            On failure, this prints an error and exits.
+        """Cover over pexpect.spawn.expect().
+        Consume all "new" output of self.spawn until the given pattern is matched, or
+        the timeout is reached.
+        Note that output between the current position and the location of the match is
+        consumed as well.
+        The pattern is typically a regular expression in string form, but may also be
+        any of the types accepted by pexpect.spawn.expect().
+        If the 'unmatched' parameter is given, it is printed as part of the error message
+        of any failure.
+        On failure, this prints an error and exits.
         """
         try:
             self.spawn.expect(pat, **kwargs)
@@ -201,15 +201,15 @@ class SpawnedProc(object):
         return self.expect_re(re.escape(s), **kwargs)
 
     def expect_prompt(self, *args, increment=True, **kwargs):
-        """ Convenience function which matches some text and then a prompt.
-            Match the given positional arguments as expect_re, and then look
-            for a prompt.
-            If increment is set, then this should be a new prompt and the prompt counter
-            should be bumped; otherwise this is not a new prompt.
-            Returns None on success, and exits on failure.
-            Example:
-               sp.sendline("echo hello world")
-               sp.expect_prompt("hello world")
+        """Convenience function which matches some text and then a prompt.
+        Match the given positional arguments as expect_re, and then look
+        for a prompt.
+        If increment is set, then this should be a new prompt and the prompt counter
+        should be bumped; otherwise this is not a new prompt.
+        Returns None on success, and exits on failure.
+        Example:
+           sp.sendline("echo hello world")
+           sp.expect_prompt("hello world")
         """
         if args:
             self.expect_re(*args, **kwargs)
@@ -221,10 +221,10 @@ class SpawnedProc(object):
         )
 
     def report_exception_and_exit(self, pat, unmatched, err):
-        """ Things have gone badly.
-            We have an exception 'err', some pexpect.ExceptionPexpect.
-            Report it to stdout, along with the offending call site.
-            If 'unmatched' is set, print it to stdout.
+        """Things have gone badly.
+        We have an exception 'err', some pexpect.ExceptionPexpect.
+        Report it to stdout, along with the offending call site.
+        If 'unmatched' is set, print it to stdout.
         """
         colors = self.colors()
         failtype = pexpect_error_type(err)

doc_src/conf.py
@@ -114,8 +114,8 @@ pygments_style = None
 # of _static/pygments.css
 html_theme_path = ["."]
 html_theme = "python_docs_theme"
-#html_theme_path = ["./cloud_sptheme/themes"]
-#html_theme = "cloud"
+# html_theme_path = ["./cloud_sptheme/themes"]
+# html_theme = "cloud"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -196,7 +196,13 @@ man_pages = [
     ("tutorial", "fish-tutorial", "fish-shell tutorial", [author], 1),
     ("CHANGELOG", "fish-changelog", "fish-shell changelog", [author], 1),
     ("completions", "fish-completions", "Writing fish completions", [author], 1),
-    ("fish_for_bash_users", "fish-for-bash-users", "A quick fish primer for those coming from bash", [author], 1),
+    (
+        "fish_for_bash_users",
+        "fish-for-bash-users",
+        "A quick fish primer for those coming from bash",
+        [author],
+        1,
+    ),
     ("faq", "fish-faq", "fish-shell faq", [author], 1),
 ]
 for path in sorted(glob.glob("cmds/*")):

doc_src/fish_indent_lexer.py
@@ -62,9 +62,9 @@ ROLE_TO_TOKEN = {
 
 
 def token_for_text_and_role(text, role):
-    """ Return the pygments token for some input text and a fish role
-    
-        This applies any special cases of ROLE_TO_TOKEN.
+    """Return the pygments token for some input text and a fish role
+
+    This applies any special cases of ROLE_TO_TOKEN.
     """
     if text.isspace():
         # Here fish will return 'normal' or 'statement_terminator' for newline.
@@ -77,13 +77,13 @@ def token_for_text_and_role(text, role):
 
 
 def tokenize_fish_command(code, offset):
-    """ Tokenize some fish code, offset in a parent string, by shelling
-        out to fish_indent.
-        
-        fish_indent will output a list of csv lines: start,end,type.
+    """Tokenize some fish code, offset in a parent string, by shelling
+    out to fish_indent.
 
-        This function returns a list of (start, tok, value) tuples, as
-        Pygments expects.
+    fish_indent will output a list of csv lines: start,end,type.
+
+    This function returns a list of (start, tok, value) tuples, as
+    Pygments expects.
     """
     proc = subprocess.Popen(
         ["fish_indent", "--pygments"],
@@ -108,11 +108,11 @@ class FishIndentLexer(Lexer):
     filenames = ["*.fish"]
 
     def get_tokens_unprocessed(self, input_text):
-        """ Return a list of (start, tok, value) tuples.
+        """Return a list of (start, tok, value) tuples.
 
-            start is the index into the string
-            tok is the token type (as above)
-            value is the string contents of the token
+        start is the index into the string
+        tok is the token type (as above)
+        value is the string contents of the token
         """
         result = []
         if not any(s.startswith(">") for s in input_text.splitlines()):

doc_src/python_docs_theme/__init__.py
@@ -3,10 +3,9 @@ import os
 
 def setup(app):
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    app.add_html_theme(
-        'python_docs_theme', current_dir)
+    app.add_html_theme("python_docs_theme", current_dir)
 
     return {
-        'parallel_read_safe': True,
-        'parallel_write_safe': True,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
     }

share/completions/bluetoothctl.fish
@@ -6,14 +6,14 @@ function __fish_list_bluetoothctl_devices
     # Output of `bluetoothctl devices`:
     # Device 01:23:34:56:89:AB Name1
     # Device 01:23:34:56:89:AC Name2
-    bluetoothctl devices 2> /dev/null | string replace -r "^Device " "" | string replace " " \t
+    bluetoothctl devices 2>/dev/null | string replace -r "^Device " "" | string replace " " \t
 end
 
 function __fish_list_bluetoothctl_controllers
     # Output of `bluetoothctl list`:
     # Controller 01:23:34:56:89:AB Name1 [default]
     # Controller 01:23:34:56:89:AC Name2
-    bluetoothctl list 2> /dev/null | string replace -r "^Controller " "" | string replace " " \t
+    bluetoothctl list 2>/dev/null | string replace -r "^Controller " "" | string replace " " \t
 end
 
 complete -f -c bluetoothctl -n "not __fish_seen_subcommand_from $cmds" -a list -d "List available controllers"

share/completions/ffmpeg.fish
@@ -83,7 +83,7 @@ function __fish_ffmpeg_filters
     end
 
     ffmpeg -hide_banner -loglevel quiet -filters |
-        string match -e -- '->' |  # skip past the header
+        string match -e -- '->' | # skip past the header
         string match -er $filter |
         string replace -rf '^ [TSC.]{3} +(\S+) +\S+->\S+ +(.*)' '$1\t$2'
 end
@@ -97,7 +97,7 @@ end
 
 function __fish_ffmpeg_tunes
     set -l cmdline (commandline)
-    if string match -req '264'
+    if string match -req 264
         printf "%s\n" film animation grain stillimage fastdecode zerolatency psnr ssim
     end
     if string match -req '265|hevc'
@@ -110,7 +110,7 @@ function __fish_ffmpeg_crfs
 end
 
 function __fish_ffmpeg_profile
-    if string match -req '264'
+    if string match -req 264
         printf "%s\n" baseline main high
     end
     if string match -req '265|hevc'
@@ -204,7 +204,7 @@ complete -c ffmpeg -s b -o "b:v" -d "Video bitrate"
 complete -c ffmpeg -o dn -d "Disable data"
 # Advanced video options
 complete -c ffmpeg -o pix_fmt
-__fish_ffmpeg_complete_regex "-pix_fmt" "(__fish_ffmpeg_pix_fmts)"
+__fish_ffmpeg_complete_regex -pix_fmt "(__fish_ffmpeg_pix_fmts)"
 
 # Audio options
 complete -c ffmpeg -o aframes -d "Set the number of audio frames to output"
@@ -231,11 +231,11 @@ complete -c ffmpeg -o spre -d "Set the subtitle options to the indicated preset"
 complete -c ffmpeg -o pre -o preset -d "Preset name"
 __fish_ffmpeg_complete_regex 'pre(set)?' "(__fish_ffmpeg_presets)"
 complete -c ffmpeg -o tune
-__fish_ffmpeg_complete_regex 'tune' "(__fish_ffmpeg_tunes)"
+__fish_ffmpeg_complete_regex tune "(__fish_ffmpeg_tunes)"
 complete -c ffmpeg -o crf -o q
 __fish_ffmpeg_complete_regex 'crf|q' "(__fish_ffmpeg_crfs)"
 complete -c ffmpeg -o profile
-__fish_ffmpeg_complete_regex 'profile' "(__fish_ffmpeg_profiles)"
+__fish_ffmpeg_complete_regex profile "(__fish_ffmpeg_profiles)"
 
 # Filters
 #
@@ -281,11 +281,11 @@ function __fish_ffmpeg_concat_filter_args
 end
 
 function __fish_ffmpeg_complete_filter
-    set -l filter_type "all"
+    set -l filter_type all
     if string match -rq -- '^-(vf(ilter)?|f(ilter)?:v)' (__fish_ffmpeg_last_arg)
-        set filter_type "video"
+        set filter_type video
     else if string match -rq -- '^-(af(ilter)?|f(ilter)?:a' (__fish_ffmpeg_last_arg)
-        set filter_type "audio"
+        set filter_type audio
     end
 
     # echo -e "\n **** $filter_type **** \n" > /dev/tty

commit b46417c77bf21a9f9badfea7f854e376f018177f

Fix broken completions for "mount -ouid="
Regressed in 2e55e34544 (Reformat, 2020-11-22).

share/functions/__fish_complete_mount_opts.fish
@@ -134,8 +134,9 @@ function __fish_complete_mount_opts
         data_err={ignore,abort} \
         barrier={0,1} \
         user_xattr \
-        acl \
-        set -l token (commandline -tc | string replace -r '^-o' -- '')
+        acl
+
+    set -l token (commandline -tc | string replace -r '^-o' -- '')
     set -l args (string split , -- $token)
 
     if test (count $args) -ne 0
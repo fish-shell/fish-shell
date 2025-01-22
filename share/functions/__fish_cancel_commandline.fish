fd3ed7cfa55fd07ed17fe065c26e45debe8ed0fc
Fabian Boehm <FHomborg@gmail.com>
2025-01-22 17:43:59 +0100

    functions/__fish_cancel_commandline: Follow rename of bind function
    See #10935

--- share/functions/__fish_cancel_commandline.fish
+++ share/functions/__fish_cancel_commandline.fish
@@ -1,4 +1,4 @@
 # This is meant to be bound to something like ctrl-c
 function __fish_cancel_commandline
-    commandline -f cancel-commandline-traditional
+    commandline -f cancel-commandline
 end

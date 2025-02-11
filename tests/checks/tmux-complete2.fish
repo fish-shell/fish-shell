7197bc776024338ebcae36b1ac07b18df88e9645
Johannes Altmanninger <aclopte@gmail.com>
2025-02-11 22:17:09 +0100

    Add a test for replacing completions of directory-variable name

--- tests/checks/tmux-complete2.fish
+++ tests/checks/tmux-complete2.fish
@@ -11,3 +11,14 @@ isolated-tmux send-keys 'touch ~/"path with spaces"' Enter C-l \
 tmux-sleep
 isolated-tmux capture-pane -p
 # CHECK: prompt 1> cat ~/path\ with\ spaces
+
+isolated-tmux send-keys C-c '
+    set -g fish_autosuggestion_enabled 0
+    set -l FISH_TEST_VAR_1 /
+    set -l FISH_TEST_VAR_2 /
+' Enter C-l 'echo $FISH_TEST_v' Tab
+tmux-sleep
+isolated-tmux capture-pane -p
+# CHECK: prompt 2> echo $FISH_TEST_VAR_
+# CHECK: $FISH_TEST_VAR_1  (Variable: /)  $FISH_TEST_VAR_2  (Variable: /)
+

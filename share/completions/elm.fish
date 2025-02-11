1b6e107131cd40fa851e70b172a50902f473d6eb
David Adam <zanchey@ucc.gu.uwa.edu.au>
2025-02-11 22:27:47 +0800

    completions/elm: remove = in long options
    elm's argument parser copes just fine without them
    
    Review comment from
    https://github.com/fish-shell/fish-shell/pull/10759#discussion_r1786918645
    
    (cherry picked from commit 2849cd11aedcc4ba9d470ebab064f06d61212677)

--- share/completions/elm.fish
+++ share/completions/elm.fish
@@ -4,16 +4,16 @@ complete -c elm -f
 # repl completions
 complete -c elm -n "not __fish_seen_subcommand_from $commands" -a repl -d 'Open up an interactive programming session'
 complete -c elm -n "__fish_seen_subcommand_from repl" -l no-colors -d 'Turn off the colors in REPL'
-complete -c elm -n "__fish_seen_subcommand_from repl" -l interpreter= -d 'Path to an alternative JS interpreter'
+complete -c elm -n "__fish_seen_subcommand_from repl" -l interpreter -d 'Path to an alternative JS interpreter'
 # reactor completions
 complete -c elm -n "not __fish_seen_subcommand_from $commands" -a reactor -d 'Compile code with a click'
-complete -c elm -n "__fish_seen_subcommand_from reactor" -l port= -d 'Compile code with a click'
+complete -c elm -n "__fish_seen_subcommand_from reactor" -l port -d 'Compile code with a click'
 # make completions
 complete -c elm -n "not __fish_seen_subcommand_from $commands" -a make -d 'Compiles Elm code in JS or HTML'
-complete -c elm -n "__fish_seen_subcommand_from make" -l output= -F -r -d 'Specify the name of resulting JS file'
+complete -c elm -n "__fish_seen_subcommand_from make" -l output -F -r -d 'Specify the name of resulting JS file'
 complete -c elm -n "__fish_seen_subcommand_from make" -l debug -d 'Turn on the time-travelling debugger'
 complete -c elm -n "__fish_seen_subcommand_from make" -l optimize -d 'Turn on optimizations to make code smaller and faster'
-complete -c elm -n "__fish_seen_subcommand_from make" -l docs= -d 'Generate a JSON file of documentation for a package'
+complete -c elm -n "__fish_seen_subcommand_from make" -l docs -d 'Generate a JSON file of documentation for a package'
 #other commands completions
 complete -c elm -n "not __fish_seen_subcommand_from $commands" -a init -d 'Start an Elm project'
 complete -c elm -n "not __fish_seen_subcommand_from $commands" -a install -d 'Fetches packages from Elm repository'

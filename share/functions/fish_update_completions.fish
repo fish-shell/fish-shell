5462bac4f244a3152c7592b5e164958d51af6fcc
Fabian Boehm <FHomborg@gmail.com>
2025-02-19 16:52:55 +0100

    functions/*: Extract data when needed
    Technically the fish_update_completions files could also be piped to
    python, but they'd have to be one file.
    
    So for now, if you start a single-file fish, you'll have to run
    fish_update_completions manually.
    
    That fits the idea of having a single file that you move somewhere
    better, given that it otherwise would run a script in the background
    that creates a bunch of files

--- share/functions/fish_update_completions.fish
+++ share/functions/fish_update_completions.fish
@@ -1,11 +1,26 @@
 function fish_update_completions --description "Update man-page based completions"
-    # Don't write .pyc files, use the manpath, clean up old completions
-    # display progress.
-    set -l update_args -B $__fish_data_dir/tools/create_manpage_completions.py --manpath --cleanup-in $__fish_user_data_dir/generated_completions --cleanup-in $__fish_cache_dir/generated_completions --progress $argv
-    if set -l python (__fish_anypython)
-        $python $update_args
-    else
+    set -l script $__fish_data_dir/tools/create_manpage_completions.py
+    set -l python (__fish_anypython)
+    or begin
         printf "%s\n" (_ "python executable not found") >&2
         return 1
     end
+
+    if not test -e "$script"
+        if not status list-files tools/create_manpage_completions.py &>/dev/null
+            echo "Cannot find man page completion generator. Please check your fish installation."
+            return 1
+        end
+        set -l temp (mktemp -d)
+        for file in create_manpage_completions.py deroff.py
+            status get-file tools/$file >$temp/$file
+            or return
+        end
+        set script $temp/create_manpage_completions.py
+    end
+
+    # Don't write .pyc files, use the manpath, clean up old completions
+    # display progress.
+    set -l update_args -B $script --manpath --cleanup-in $__fish_user_data_dir/generated_completions --cleanup-in $__fish_cache_dir/generated_completions --progress $argv
+    $python $update_args
 end

fish-shell/fish-shell

commit 9af6a64fd2bd132ac1866af846fdc8368e086206

Fix bad contrast in search match highlighting
This is another problem that has been bothering me for years: as mentioned
in 1dd901e52 (Maintain cursor in history prefix search, 2024-04-12), up-arrow
search highlights search matches but the contrast is really bad, especially in
command position, because the search matches --background=brblack is combined
with whatever foreground syntax highlighting the command has.  The history
pager had a similar problem (for the selected history item) but circumented
it by disabling syntax highlighting altogether for the selected item.

fish_color_search_match's foreground component is ignored.
Let's use it instead of syntax highlighting.

This fixes the contrast on some default colorschemes but the bryellow
foreground looks weirdly like an error/warning on some terminals.  Change it
to white. This needs a hack because we don't have a canonical way to tell
if a uvar has been set by the user. Fortunately the foreground component
hasn't been used at all so far, so we're not so much changing it as much as
initializing it.

src/reader.rs
@@ -1433,6 +1433,7 @@ impl ReaderData {
             }
 
             for color in &mut colors[data.history_search_range.unwrap().start()..end] {
+                color.foreground = HighlightRole::search_match;
                 color.background = HighlightRole::search_match;
             }
         }
@@ -3316,7 +3317,8 @@ impl ReaderData {
         // Save off the colors and set the background.
         let saved_colors = data.colors.clone();
         for i in 0..self.command_line.position() {
-            data.colors[i] = HighlightSpec::with_bg(HighlightRole::search_match);
+            data.colors[i].foreground = HighlightRole::search_match;
+            data.colors[i].background = HighlightRole::search_match;
         }
         self.rendered_layout = data.clone(); // need to copy the data since we will use it again.
         self.paint_layout(L!("flash"));

share/functions/__fish_config_interactive.fish
@@ -55,9 +55,6 @@ end" >$__fish_config_dir/config.fish
         __init_uvar fish_color_cwd green
         __init_uvar fish_color_cwd_root red
 
-        # Background color for search matches
-        __init_uvar fish_color_search_match bryellow --background=brblack
-
         # Background color for selections
         __init_uvar fish_color_selection white --bold --background=brblack
 
@@ -74,6 +71,13 @@ end" >$__fish_config_dir/config.fish
         # Directory history colors
         #
         __init_uvar fish_color_history_current --bold
+    end
+    if test $__fish_initialized -lt 3800
+        # Background color for search matches
+        __init_uvar fish_color_search_match white --background=brblack
+        if test "$fish_color_search_match[1]" = bryellow
+            set --universal fish_color_search_match[1] white
+        end
     end
 
     #
@@ -227,7 +231,7 @@ end" >$__fish_config_dir/config.fish
 
     # Bump this whenever some code below needs to run once when upgrading to a new version.
     # The universal variable __fish_initialized is initialized in share/config.fish.
-    set __fish_initialized 3400
+    set __fish_initialized 3800
 
     functions -e __fish_config_interactive
 end

commit b6269438e9e9424013d59cac39eea7d65de8885e

Apply fish_color_search_match foreground only if explicit
Historically, up-arrow search matches have been highlighted by

1. using the usual foreground (from syntax highlighting) 
2. using the background from $fish_color_search_match

Commit 9af6a64fd2 (Fix bad contrast in search match highlighting, 2024-04-15)
broke this by also applying the foreground from $fish_color_search_match.

As reported on gitter, there is a meaningful scenario where the foreground
from syntax highlighting should not be overwritten:

	set fish_color_search_match --reverse

this copies the foreground from syntax highlighting to the background.

Since commit 9af6a64fd2 overwrites the foreground highlight, the resulting
background will be monocolored (black in my case) instead of whatever is
the syntax-colored foreground.

FWIW the reversed foreground will always be monocolored, because we have
always done 2.

Let's unbreak this scenario by using the foreground from
fish_color_search_match only if it's explicitly set (like we do since
9af6a64fd2).

This is hacky because an empty color is normally the same as "normal", but
it gets us closer to historical behavior. In future we should try to come
up with a better approach to color blending/transparency.

src/output.rs
@@ -1,5 +1,5 @@
 // Generic output functions.
-use crate::color::RgbColor;
+use crate::color::{self, RgbColor};
 use crate::common::{self, wcs2string_appending};
 use crate::env::EnvVar;
 use crate::terminal::{self, tparm1, Term};
@@ -493,6 +493,14 @@ pub fn best_color(candidates: &[RgbColor], support: ColorSupport) -> RgbColor {
 ///       In particular, the argument parsing still isn't fully capable.
 #[allow(clippy::collapsible_else_if)]
 pub fn parse_color(var: &EnvVar, is_background: bool) -> RgbColor {
+    let mut result = parse_color_maybe_none(var, is_background);
+    if result.is_none() {
+        result.typ = color::Type::Normal;
+    }
+    result
+}
+
+pub fn parse_color_maybe_none(var: &EnvVar, is_background: bool) -> RgbColor {
     let mut is_bold = false;
     let mut is_underline = false;
     let mut is_italics = false;
@@ -551,9 +559,6 @@ pub fn parse_color(var: &EnvVar, is_background: bool) -> RgbColor {
     }
 
     let mut result = best_color(&candidates, get_color_support());
-    if result.is_none() {
-        result = RgbColor::NORMAL;
-    }
     result.set_bold(is_bold);
     result.set_underline(is_underline);
     result.set_italics(is_italics);

src/reader.rs
@@ -104,6 +104,7 @@ use crate::libc::MB_CUR_MAX;
 use crate::nix::{getpgrp, getpid, isatty};
 use crate::operation_context::{get_bg_context, OperationContext};
 use crate::output::parse_color;
+use crate::output::parse_color_maybe_none;
 use crate::output::Outputter;
 use crate::pager::{PageRendering, Pager, SelectionMotion};
 use crate::panic::AT_EXIT;
@@ -1639,8 +1640,15 @@ impl<'a> Reader<'a> {
                 range.end = colors.len();
             }
 
+            let explicit_foreground = self
+                .vars()
+                .get_unless_empty(L!("fish_color_search_match"))
+                .is_some_and(|var| !parse_color_maybe_none(&var, false).is_none());
+
             for color in &mut colors[range] {
-                color.foreground = HighlightRole::search_match;
+                if explicit_foreground {
+                    color.foreground = HighlightRole::search_match;
+                }
                 color.background = HighlightRole::search_match;
             }
         }
5cd2ef903a98a6d0d7fe5935f9b4fb0bc0d016ce
Fabian Boehm <FHomborg@gmail.com>
2025-03-04 16:57:59 +0100

    key: Add super modifier
    Fixes #11217
    
    (cherry picked from commit 9f5e1736a860ac5e42b00bd6ebee37f6559495be)

--- src/key.rs
+++ src/key.rs
@@ -54,6 +54,7 @@ pub struct Modifiers {
     pub ctrl: bool,
     pub alt: bool,
     pub shift: bool,
+    pub sup: bool,
 }
 
 impl Modifiers {
@@ -62,6 +63,7 @@ impl Modifiers {
             ctrl: false,
             alt: false,
             shift: false,
+            sup: false,
         }
     }
     pub(crate) const ALT: Self = {
@@ -271,6 +273,7 @@ pub(crate) fn parse_keys(value: &wstr) -> Result<Vec<Key>, WString> {
                     _ if modifier == "ctrl" => modifiers.ctrl = true,
                     _ if modifier == "alt" => modifiers.alt = true,
                     _ if modifier == "shift" => modifiers.shift = true,
+                    _ if modifier == "super" => modifiers.sup = true,
                     _ => {
                         return Err(wgettext_fmt!(
                             "unknown modifier '%s' in '%s'",
@@ -406,6 +409,9 @@ impl From<Key> for WString {
         }
         if key.modifiers.ctrl {
             res.insert_utfstr(0, L!("ctrl-"));
+        }
+        if key.modifiers.sup {
+            res.insert_utfstr(0, L!("super-"));
         }
 
         res

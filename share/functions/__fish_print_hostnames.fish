59412c493b7ce5cc6878405c920c4b0a681d600a
Niklas Liechti <niklas.liechti@threema.ch>
2025-03-05 14:04:30 +0100

    Split content of *KnownHostsFile as this is a supported config option by ssh.

--- share/functions/__fish_print_hostnames.fish
+++ share/functions/__fish_print_hostnames.fish
@@ -111,7 +111,7 @@ function __fish_print_hostnames -d "Print a list of known hostnames"
             # Multiple names for a single host can be given separated by spaces, so just split it explicitly (#6698).
             string replace -rfi '^\s*Host\s+(\S.*?)\s*$' '$1' -- $contents | string split " " | string match -rv '[\*\?]'
             # Also extract known_host paths.
-            set known_hosts $known_hosts (string replace -rfi '.*KnownHostsFile\s*' '' -- $contents)
+            set known_hosts $known_hosts (string replace -rfi '.*KnownHostsFile\s*' '' -- $contents | string split " ")
         end
     end
 

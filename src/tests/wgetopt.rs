fa8bc29fecd0661d654fa7271e835a57a4b56a4d
m1kc (Max Musatov) <m1kc@yandex.ru>
2025-03-05 14:41:37 +0400

    wgetopt: remove support for abbreviated long options. Closes #7341.

--- src/tests/wgetopt.rs
+++ src/tests/wgetopt.rs
@@ -41,3 +41,40 @@ fn test_wgetopt() {
     assert_eq!(arguments.len(), 3);
     assert_eq!(join_strings(&arguments, ' '), "emacsnw emacs -nw");
 }
+
+#[test]
+fn test_wgetopt_proper_longopts() {
+    // Test for issue #7341 (disable abbreviated long options, e.g. --forc shouldn't work as an alias to --force).
+    const short_options: &wstr = L!("-f");
+    const long_options: &[WOption] = &[wopt(L!("force"), ArgType::NoArgument, 'f')];
+    let mut argv = [L!("rm"), L!("--forc"), L!("filename1"), L!("filename2")];
+    let mut w = WGetopter::new(short_options, long_options, &mut argv);
+    let mut f_count = 0;
+    let mut unknown_count = 0;
+    let mut arguments = vec![];
+    while let Some(opt) = w.next_opt() {
+        match opt {
+            'f' => {
+                f_count += 1;
+            }
+            '\x01' => {
+                // non-option argument
+                arguments.push(w.woptarg.as_ref().unwrap().to_owned());
+            }
+            '?' => {
+                // unrecognized option
+                unknown_count += 1;
+                if let Some(arg) = w.argv.get(w.wopt_index - 1) {
+                    arguments.push(arg.to_owned());
+                }
+            }
+            _ => {
+                panic!("unexpected option: {:?}", opt);
+            }
+        }
+    }
+    assert_eq!(f_count, 0);
+    assert_eq!(unknown_count, 1);
+    assert_eq!(arguments.len(), 3);
+    assert_eq!(join_strings(&arguments, ' '), "--forc filename1 filename2");
+}

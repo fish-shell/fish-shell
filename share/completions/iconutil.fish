3d7d57d61217ca59ef1d3bcab1e838fd010bc981
Lucas Garron <code@garron.net>
2025-04-16 00:03:54 -0700

    Add completions for `iconutil` (macOS).
    Closes #11392

---
+++ share/completions/iconutil.fish
@@ -0,0 +1,2 @@
+complete -c iconutil -s c -l convert -r -f --arguments "icns iconset" -d "Convert to the specified type."
+complete -c iconutil -s o -l output -r --description "Overrides the default output file name."

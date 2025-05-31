7f2f5bb2f44ee362fcee390327d3e2ca7c0bc3cb
Johannes Altmanninger <aclopte@gmail.com>
2025-05-31 20:30:23 +0200

    completions/git: remove code clone

--- share/completions/git.fish
+++ share/completions/git.fish
@@ -52,8 +52,8 @@ end
 function __fish_git_branches
     # This is much faster than using `git branch` and avoids dealing with localized "detached HEAD" messages.
     # We intentionally only sort local branches by recency. See discussion in #9248.
-    __fish_git for-each-ref --format='%(refname:strip=2)%09Local Branch' --sort=-committerdate refs/heads/ 2>/dev/null
-    __fish_git for-each-ref --format='%(refname:strip=2)%09Remote Branch' refs/remotes/ 2>/dev/null
+    __fish_git_local_branches
+    __fish_git_remote_branches
 end
 
 function __fish_git_submodules

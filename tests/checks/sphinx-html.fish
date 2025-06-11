b2e5685511b5f8dbf88eedb9f2e0c98e67be1209
Daniel Rainer <daniel.rainer@localhost>
2025-06-11 19:00:32 +0200

    Split up sphinx HTML and MAN file generation
    This test is the one with the longest runtime. Splitting the two targets into
    separate tests allows them to run in parallel, which can speed up the tests.

---
+++ tests/checks/sphinx-html.fish
@@ -0,0 +1,7 @@
+#RUN: fish_indent=%fish_indent fish=%fish %fish %s
+#REQUIRES: command -v sphinx-build
+
+# sphinx-build needs fish_indent in $PATH
+set -l build_script (status dirname)/sphinx-shared.sh
+set -lxp PATH (path dirname $fish_indent)
+$build_script html

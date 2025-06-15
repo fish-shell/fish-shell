df1478bbe5f34a1da564292b4384a4d3649b244b
Daniel Rainer <daniel.rainer@localhost>
2025-06-15 23:35:38 +0200

    Add gettext localization test
    This test requires certain locales, so it might not run in most circumstances.
    In CI, we can generate the locales we need, so at least there the test should
    run without issues.

---
+++ tests/checks/gettext.fish
@@ -0,0 +1,27 @@
+# RUN: fish=%fish %fish %s
+#REQUIRES: command -v msgfmt
+
+set -l custom_gettext_var asdf
+
+set -lx LANG de_DE.utf8
+
+# Check if translations work.
+
+$fish --not-a-real-flag
+# CHECKERR: fish: --not-a-real-flag: unbekannte Option
+
+set --show custom_gettext_var
+# CHECK: $custom_gettext_var: Gesetzt in local Gültigkeit als nicht exportiert, mit 1 Elementen
+# CHECK: $custom_gettext_var[1]: |asdf|
+
+set -lx LANG en_US.utf8
+
+
+# Changing the locale should result in different localizations.
+
+$fish --not-a-real-flag
+# CHECKERR: fish: --not-a-real-flag: unknown option
+
+set --show custom_gettext_var
+# CHECK: $custom_gettext_var: set in local scope, unexported, with 1 elements
+# CHECK: $custom_gettext_var[1]: |asdf|

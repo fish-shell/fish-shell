1d6fa258f670f2e9664f123fddc9d42e17b3c921
Johannes Altmanninger <aclopte@gmail.com>
2025-06-21 13:14:23 +0200

    Restore terminal state on SIGTERM again
    Commit 941701da3d8 (Restore some async-signal discipline to SIGTERM,
    2025-06-15) made two changes
    1. removed a mutex lock in signal handler (regression from 55fd43d86ca
       (Port reader, 2023-12-22))
    2. removed some SIGTERM cleanup
    
    I'm not sure what's the reason for 2, so let's revert it I guess.  This code
    path already uses FLOG_SAFE for async-signal safety.
    
    There is an avoidable panic when `Outputter::stdoutput()` is already
    borrowed. Fix that.
    
    Closes #11597

--- src/signal.rs
+++ src/signal.rs
@@ -4,7 +4,8 @@ use std::num::NonZeroI32;
 use crate::common::exit_without_destructors;
 use crate::event::{enqueue_signal, is_signal_observed};
 use crate::nix::getpid;
-use crate::reader::{reader_handle_sigint, reader_sighup, safe_restore_term_mode};
+use crate::panic::AT_EXIT;
+use crate::reader::{reader_handle_sigint, reader_sighup};
 use crate::termsize::TermsizeContainer;
 use crate::topic_monitor::{topic_monitor_principal, Generation, GenerationsList, Topic};
 use crate::wchar::prelude::*;
@@ -89,7 +90,9 @@ extern "C" fn fish_signal_handler(
         libc::SIGTERM => {
             // Handle sigterm. The only thing we do is restore the front process ID, then die.
             if !observed {
-                safe_restore_term_mode();
+                if let Some(at_exit) = AT_EXIT.get() {
+                    (at_exit)();
+                }
                 // Safety: signal() and raise() are async-signal-safe.
                 unsafe {
                     libc::signal(libc::SIGTERM, libc::SIG_DFL);

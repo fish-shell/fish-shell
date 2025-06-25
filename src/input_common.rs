fish-shell/fish-shell

commit 97581ed20ffa30a8006e65b7180cd017679b2374

Do send bracketed paste inside midnight commander
It can handle it fine (well, it simply strips the control sequences..).

src/input_common.rs
@@ -474,14 +474,11 @@ fn test_parse_version() {
 }
 
 pub fn terminal_protocols_enable_ifn() {
-    if IN_MIDNIGHT_COMMANDER.load() {
-        return;
-    }
     if TERMINAL_PROTOCOLS.load(Ordering::Relaxed) {
         return;
     }
     TERMINAL_PROTOCOLS.store(true, Ordering::Release);
-    let sequences = if IN_WEZTERM.load() {
+    let sequences = if IN_WEZTERM.load() || IN_MIDNIGHT_COMMANDER.load() {
         "\x1b[?2004h"
     } else if IN_ITERM_PRE_CSI_U.load() {
         concat!("\x1b[?2004h", "\x1b[>4;1m", "\x1b[>5u", "\x1b=",)

commit 1ceebdf580496724f75c7aa3c8318af997b3782d

Fix some CSI commands being sent to old midnight commander
Commit 97581ed20ff (Do send bracketed paste inside midnight commander,
2024-10-12) accidentally started sending CSI commands such as "CSI >5;0m",
which we intentionally didn't do for some old versions of Midnight Commander,
which fail to parse them. Fix that.

Fixes #11617

src/input_common.rs
@@ -710,7 +710,9 @@ pub(crate) fn terminal_protocols_disable_ifn() {
     if !TERMINAL_PROTOCOLS.load(Ordering::Acquire) {
         return;
     }
-    let sequences = if !feature_test(FeatureFlag::keyboard_protocols) {
+    let sequences = if !feature_test(FeatureFlag::keyboard_protocols)
+        || IN_MIDNIGHT_COMMANDER_PRE_CSI_U.load()
+    {
         "\x1b[?2004l"
     } else if IN_JETBRAINS.load() || IN_ITERM_PRE_CSI_U.load() {
         concat!("\x1b[?2004l", "\x1b[>4;0m", "\x1b>",)
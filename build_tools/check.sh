e7fb830ca8b0107e0faa6b05c3f5f8bc06858fae
Daniel Rainer <daniel.rainer@localhost>
2025-06-26 00:04:38 +0200

    Add clarifying comments for `build_dir`

--- build_tools/check.sh
+++ build_tools/check.sh
@@ -30,6 +30,10 @@ cleanup () {
 trap cleanup EXIT INT TERM HUP
 
 repo_root="$(dirname "$0")/.."
+# The directory containing the binaries produced by cargo/rustc.
+# Currently, all builds are debug builds.
+# Note that `$target_triple` might be empty, resulting in `//debug` which is interpreted as
+# `/debug`.
 build_dir="${CARGO_TARGET_DIR:-$repo_root/target}/${target_triple}/debug"
 
 if $lint && command -v rustup >/dev/null && rustup show active-toolchain | grep -q ^stable-; then

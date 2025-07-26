e200abe39c27d7b0fa74c439d4b61f0236f65b4c
Johannes Altmanninger <aclopte@gmail.com>
2025-07-26 13:04:51 +0200

    __fish_seen_subcommand_from: fix regression causing false negatives given multiple arguments
    Fixes 2bfa7db7bce (Restructure __fish_seen_subcommand_from, 2024-07-07)
    Fixes #11685
    
    (cherry picked from commit 4412164fd4e80376f246a6c2eacec8c61fea4633)

--- share/functions/__fish_seen_subcommand_from.fish
+++ share/functions/__fish_seen_subcommand_from.fish
@@ -6,5 +6,5 @@
 
 function __fish_seen_subcommand_from
     set -l regex (string escape --style=regex -- (commandline -pxc)[2..] | string join '|')
-    string match -rq -- "^$regex"'$' $argv
+    string match -rq -- "^($regex)\$" $argv
 end

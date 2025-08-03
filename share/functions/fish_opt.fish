c569c7243a12a371bbb5bede34f2eb129bc609dc
Isaac Oscar Gariano <IsaacOscar@live.com.au>
2025-08-03 10:07:08 +1000

    Added a -v/--validate option to fish_opt
    This new flag causes fish_opt to generrate an option spec with !
    (e.g. "fish_opt -s s -rv some code" will output "s=!some code").
    
    Such validation scripts are not particular useful (they are highly limited as they cannot access the values for other options,
    and must be quoted appropriately so they can be passed to argparse).
    I merely added the option to fish_opt so that it can now generate any valid option spec.

--- share/functions/fish_opt.fish
+++ share/functions/fish_opt.fish
@@ -1,6 +1,15 @@
 # This is a helper function for `fish_opt`. It does some basic validation of the arguments.
 function __fish_opt_validate_args --no-scope-shadowing
-    if set -q _flag_short && test 1 -ne (string length -- $_flag_short)
+    if not set -q _flag_validate && test (count $argv) -ne 0
+        printf (_ "%s: Extra non-option arguments were provided\n") fish_opt >&2
+        return 1
+    else if set -q _flag_validate && test (count $argv) -eq 0
+        printf (_ "%s: The --validate flag requires subsequent arguments\n") fish_opt >&2
+        return 1
+    else if set -q _flag_validate && not set -q _flag_multiple_vals && not set -q _flag_optional_val && not set -q _flag_required_val
+        printf (_ "%s: The --validate flag requires the --required-val, --optional-value, or --multiple-vals flag\n") fish_opt >&2
+        return 1
+    else if set -q _flag_short && test 1 -ne (string length -- $_flag_short)
         printf (_ "%s: The --short flag must be a single character\n") fish_opt >&2
         return 1
     else if not set -q _flag_short && not set -q _flag_long
@@ -17,8 +26,8 @@ end
 
 # The `fish_opt` command.
 function fish_opt -d 'Produce an option specification suitable for use with `argparse`.'
-    set -l options h/help 's/short=' 'l/long=' d/delete o/optional-val r/required-val m/multiple-vals long-only
-    argparse -n fish_opt --max-args=0 --exclusive=r,o $options -- $argv
+    set -l options h/help 's/short=' 'l/long=' d/delete o/optional-val r/required-val m/multiple-vals long-only v/validate
+    argparse -n fish_opt --stop-nonopt --exclusive=r,o $options -- $argv
     or return
 
     if set -q _flag_help
@@ -26,7 +35,7 @@ function fish_opt -d 'Produce an option specification suitable for use with `arg
         return 0
     end
 
-    __fish_opt_validate_args
+    __fish_opt_validate_args $argv
     or return
 
     if not set -q _flag_short
@@ -51,6 +60,10 @@ function fish_opt -d 'Produce an option specification suitable for use with `arg
 
     if set -q _flag_delete
         set opt_spec "$opt_spec&"
+    end
+
+    if set -q _flag_validate
+        set opt_spec "$opt_spec!$argv"
     end
 
     echo $opt_spec

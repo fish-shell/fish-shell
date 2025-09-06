7693a0c7c7bf95654711e85f56f1dc6946b386e6
Johannes Altmanninger <aclopte@gmail.com>
2025-09-06 12:50:18 +0200

    config_paths: separate ifdef'd path logic into functions
    No functional change, only reduce the number of times we check for
    presence of the embed-data feature.
    
    While at it, move ConfigPaths?

--- src/env/config_paths.rs
+++ src/env/config_paths.rs
@@ -1,14 +1,18 @@
-use super::ConfigPaths;
 use crate::{common::get_executable_path, FLOG, FLOGF};
 use once_cell::sync::Lazy;
+use std::path::Path;
 use std::path::PathBuf;
 
-const DOC_DIR: &str = env!("DOCDIR");
-const DATA_DIR: &str = env!("DATADIR");
-const DATA_DIR_SUBDIR: &str = env!("DATADIR_SUBDIR");
-const SYSCONF_DIR: &str = env!("SYSCONFDIR");
-const LOCALE_DIR: &str = env!("LOCALEDIR");
-const BIN_DIR: &str = env!("BINDIR");
+/// A struct of configuration directories, determined in main() that fish will optionally pass to
+/// env_init.
+#[derive(Default)]
+pub struct ConfigPaths {
+    pub data: Option<PathBuf>,   // e.g., /usr/local/share
+    pub sysconf: PathBuf,        // e.g., /usr/local/etc
+    pub doc: PathBuf,            // e.g., /usr/local/share/doc/fish
+    pub bin: Option<PathBuf>,    // e.g., /usr/local/bin
+    pub locale: Option<PathBuf>, // e.g., /usr/local/share/locale
+}
 
 pub static CONFIG_PATHS: Lazy<ConfigPaths> = Lazy::new(|| {
     // Read the current executable and follow all symlinks to it.
@@ -21,37 +25,78 @@ pub static CONFIG_PATHS: Lazy<ConfigPaths> = Lazy::new(|| {
         std::env::current_exe().unwrap_or(argv0)
     };
     let argv0 = argv0.canonicalize().unwrap_or(argv0);
-    let mut paths = ConfigPaths::default();
-    #[allow(unused_mut)]
-    let mut done = false;
     let exec_path = get_executable_path(&argv0);
-    #[cfg(not(feature = "embed-data"))]
-    if let Ok(exec_path) = exec_path.canonicalize() {
-        FLOG!(
-            config,
-            format!("exec_path: {:?}, argv[0]: {:?}", exec_path, &argv0)
-        );
-        // TODO: we should determine program_name from argv0 somewhere in this file
 
-        // If we're in Cargo's target directory or CMake's build directory, use the source files.
-        if exec_path.starts_with(env!("FISH_BUILD_DIR")) {
-            let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let paths = ConfigPaths::new(&argv0, exec_path);
+
+    FLOGF!(
+        config,
+        "determine_config_directory_paths() results:\npaths.data: %ls\npaths.sysconf: \
+        %ls\npaths.doc: %ls\npaths.bin: %ls\npaths.locale: %ls",
+        paths
+            .data
+            .clone()
+            .map(|x| x.display().to_string())
+            .unwrap_or("|not found|".to_string()),
+        paths.sysconf.display().to_string(),
+        paths.doc.display().to_string(),
+        paths
+            .bin
+            .clone()
+            .map(|x| x.display().to_string())
+            .unwrap_or("|not found|".to_string()),
+        paths
+            .locale
+            .clone()
+            .map(|x| x.display().to_string())
+            .unwrap_or("|not found|".to_string()),
+    );
+
+    paths
+});
+
+const SYSCONF_DIR: &str = env!("SYSCONFDIR");
+const DOC_DIR: &str = env!("DOCDIR");
+
+impl ConfigPaths {
+    #[cfg(feature = "embed-data")]
+    fn new(_argv0: &Path, exec_path: PathBuf) -> Self {
+        FLOG!(config, "embed-data feature is active, ignoring data paths");
+        ConfigPaths {
+            data: None,
+            sysconf: PathBuf::from(SYSCONF_DIR).join("fish"),
+            doc: DOC_DIR.into(),
+            bin: exec_path.parent().map(|x| x.to_path_buf()),
+            locale: None,
+        }
+    }
+
+    #[cfg(not(feature = "embed-data"))]
+    fn new(argv0: &Path, exec_path: PathBuf) -> Self {
+        if let Ok(exec_path) = exec_path.canonicalize() {
             FLOG!(
+                config,
+                format!("exec_path: {:?}, argv[0]: {:?}", exec_path, &argv0)
+            );
+            // TODO: we should determine program_name from argv0 somewhere in this file
+
+            // If we're in Cargo's target directory or CMake's build directory, use the source files.
+            if exec_path.starts_with(env!("FISH_BUILD_DIR")) {
+                let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+                FLOG!(
                 config,
                 "Running out of target directory, using paths relative to CARGO_MANIFEST_DIR:\n",
                 manifest_dir.display()
             );
-            done = true;
-            paths = ConfigPaths {
-                data: Some(manifest_dir.join("share")),
-                sysconf: manifest_dir.join("etc"),
-                doc: manifest_dir.join("user_doc/html"),
-                bin: Some(exec_path.parent().unwrap().to_owned()),
-                locale: Some(manifest_dir.join("share/locale")),
+                return ConfigPaths {
+                    data: Some(manifest_dir.join("share")),
+                    sysconf: manifest_dir.join("etc"),
+                    doc: manifest_dir.join("user_doc/html"),
+                    bin: Some(exec_path.parent().unwrap().to_owned()),
+                    locale: Some(manifest_dir.join("share/locale")),
+                };
             }
-        }
 
-        if !done {
             // The next check is that we are in a relocatable directory tree
             if exec_path.ends_with("bin/fish") {
                 let base_path = exec_path.parent().unwrap().parent().unwrap();
@@ -83,61 +128,18 @@ pub static CONFIG_PATHS: Lazy<ConfigPaths> = Lazy::new(|| {
                 if !paths.doc.exists() {
                     paths.doc = PathBuf::from(DOC_DIR);
                 }
-                done = true;
+                return paths;
             }
         }
-    }
 
-    if !done {
         // Fall back to what got compiled in.
-        let data = if cfg!(feature = "embed-data") {
-            None
-        } else {
-            Some(PathBuf::from(DATA_DIR).join(DATA_DIR_SUBDIR))
-        };
-        let bin = if cfg!(feature = "embed-data") {
-            exec_path.parent().map(|x| x.to_path_buf())
-        } else {
-            Some(PathBuf::from(BIN_DIR))
-        };
-        let locale = if cfg!(feature = "embed-data") {
-            None
-        } else {
-            Some(PathBuf::from(LOCALE_DIR))
-        };
-
         FLOG!(config, "Using compiled in paths:");
-        paths = ConfigPaths {
-            data,
+        ConfigPaths {
+            data: Some(PathBuf::from(env!("DATADIR")).join(env!("DATADIR_SUBDIR"))),
             sysconf: PathBuf::from(SYSCONF_DIR).join("fish"),
             doc: DOC_DIR.into(),
-            bin,
-            locale,
+            bin: Some(PathBuf::from(env!("BINDIR"))),
+            locale: Some(PathBuf::from(env!("LOCALEDIR"))),
         }
     }
-
-    FLOGF!(
-        config,
-        "determine_config_directory_paths() results:\npaths.data: %ls\npaths.sysconf: \
-        %ls\npaths.doc: %ls\npaths.bin: %ls\npaths.locale: %ls",
-        paths
-            .data
-            .clone()
-            .map(|x| x.display().to_string())
-            .unwrap_or("|not found|".to_string()),
-        paths.sysconf.display().to_string(),
-        paths.doc.display().to_string(),
-        paths
-            .bin
-            .clone()
-            .map(|x| x.display().to_string())
-            .unwrap_or("|not found|".to_string()),
-        paths
-            .locale
-            .clone()
-            .map(|x| x.display().to_string())
-            .unwrap_or("|not found|".to_string()),
-    );
-
-    paths
-});
+}

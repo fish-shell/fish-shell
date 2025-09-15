a1b4b391b2378c279e02cd6b443f951a498365e2
Piotr Kubaj <pkubaj@FreeBSD.org>
2025-09-15 19:19:39 +0000

    path.rs: fix build on ARM / POWER
    error[E0308]: mismatched types
       --> src/path.rs:749:13
        |
    748 |         let remoteness = remoteness_via_statfs(
        |                          --------------------- arguments to this function are incorrect
    749 |             libc::statfs,
        |             ^^^^^^^^^^^^ expected fn pointer, found fn item
        |
        = note: expected fn pointer `unsafe extern "C" fn(*const i8, _) -> _`
                      found fn item `unsafe extern "C" fn(*const u8, _) -> _ {libc::statfs}`
    note: function defined here
       --> src/path.rs:712:12
        |
    712 |         fn remoteness_via_statfs<StatFS, Flags>(
        |            ^^^^^^^^^^^^^^^^^^^^^
    713 |             statfn: unsafe extern "C" fn(*const i8, *mut StatFS) -> libc::c_int,
        |             -------------------------------------------------------------------
    
    error[E0308]: mismatched types
       --> src/path.rs:725:34
        |
    725 |             if unsafe { (statfn)(path.as_ptr(), buf.as_mut_ptr()) } < 0 {
        |                         -------- ^^^^^^^^^^^^^ expected `*const i8`, found `*const u8`
        |                         |
        |                         arguments to this function are incorrect
        |
        = note: expected raw pointer `*const i8`
                   found raw pointer `*const u8`
    
    For more information about this error, try `rustc --explain E0308`.
    error: could not compile `fish` (lib) due to 2 previous errors
    
    (cherry picked from commit 91ee45b0e172d2cc75b600505b6fefe9ecb01bd5)

--- src/path.rs
+++ src/path.rs
@@ -710,7 +710,7 @@ fn path_remoteness(path: &wstr) -> DirRemoteness {
     #[cfg(not(target_os = "linux"))]
     {
         fn remoteness_via_statfs<StatFS, Flags>(
-            statfn: unsafe extern "C" fn(*const i8, *mut StatFS) -> libc::c_int,
+            statfn: unsafe extern "C" fn(*const libc::c_char, *mut StatFS) -> libc::c_int,
             flagsfn: fn(&StatFS) -> Flags,
             is_local_flag: u64,
             path: &std::ffi::CStr,

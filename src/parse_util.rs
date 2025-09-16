dc0b5f250a8d5cad654935104cb4c28101110537
The0x539 <the0x539@gmail.com>
2025-09-16 17:08:01 -0500

    parse_util: remove unnecessary is_same_node use

--- src/parse_util.rs
+++ src/parse_util.rs
@@ -1,7 +1,5 @@
 //! Various mostly unrelated utility functions related to parsing, loading and evaluating fish code.
-use crate::ast::{
-    self, is_same_node, Ast, Keyword, Kind, Leaf, Node, NodeVisitor, Token, Traversal,
-};
+use crate::ast::{self, Ast, Keyword, Kind, Leaf, Node, NodeVisitor, Token, Traversal};
 use crate::builtins::shared::builtin_exists;
 use crate::common::{
     escape_string, unescape_string, valid_var_name, valid_var_name_char, EscapeFlags,
@@ -1584,7 +1582,7 @@ fn detect_errors_in_backgrounded_job(
         // Find the index of ourselves in the job list.
         let index = jlist
             .iter()
-            .position(|job| is_same_node(job, job_conj))
+            .position(|job| std::ptr::eq::<ast::JobConjunction>(job, job_conj))
             .expect("Should have found the job in the list");
 
         // Try getting the next job and check its decorator.
@@ -1649,7 +1647,7 @@ fn detect_errors_in_decorated_statement(
     // Check our pipeline position.
     let pipe_pos = if job.continuation.is_empty() {
         PipelinePosition::none
-    } else if is_same_node(&job.statement, st) {
+    } else if std::ptr::eq::<ast::Statement>(&job.statement, st) {
         PipelinePosition::first
     } else {
         PipelinePosition::subsequent

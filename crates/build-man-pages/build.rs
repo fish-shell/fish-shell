4f8aa0a78c4e0999019006e9a24c6b2da6b76e1f
Johannes Altmanninger <aclopte@gmail.com>
2025-09-24 11:35:19 +0200

    Show warnings from sphinx-build when building with cargo

--- crates/build-man-pages/build.rs
+++ crates/build-man-pages/build.rs
@@ -14,7 +14,10 @@ fn main() {
 
 #[cfg(not(clippy))]
 fn build_man(man_dir: &Path) {
-    use std::{env, process::Command};
+    use std::{
+        env,
+        process::{Command, Stdio},
+    };
 
     use fish_build_helper::workspace_root;
 
@@ -54,30 +57,42 @@ fn build_man(man_dir: &Path) {
     // - if we skipped the docs with sphinx not installed, installing it would not then build the docs.
     // That means you need to explicitly set $FISH_BUILD_DOCS=0 (`FISH_BUILD_DOCS=0 cargo install --path .`),
     // which is unfortunate - but the docs are pretty important because they're also used for --help.
-    match Command::new("sphinx-build").args(args).spawn() {
-        Err(x) if x.kind() == std::io::ErrorKind::NotFound => {
+    let sphinx_build = match Command::new("sphinx-build")
+        .args(args)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+    {
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
             if env::var("FISH_BUILD_DOCS") == Ok("1".to_string()) {
                 panic!("Could not find sphinx-build to build man pages.\nInstall sphinx or disable building the docs by setting $FISH_BUILD_DOCS=0.");
             }
             rsconf::warn!("Cannot find sphinx-build to build man pages.");
             rsconf::warn!("If you install it now you need to run `cargo clean` and rebuild, or set $FISH_BUILD_DOCS=1 explicitly.");
+            return;
         }
-        Err(x) => {
+        Err(e) => {
             // Another error - permissions wrong etc
-            panic!("Error starting sphinx-build to build man pages: {:?}", x);
+            panic!("Error starting sphinx-build to build man pages: {:?}", e);
         }
-        Ok(mut x) => match x.wait() {
-            Err(err) => {
-                panic!(
-                    "Error waiting for sphinx-build to build man pages: {:?}",
-                    err
-                );
+        Ok(sphinx_build) => sphinx_build,
+    };
+
+    match sphinx_build.wait_with_output() {
+        Err(err) => {
+            panic!(
+                "Error waiting for sphinx-build to build man pages: {:?}",
+                err
+            );
+        }
+        Ok(out) => {
+            if !out.stderr.is_empty() {
+                rsconf::warn!("sphinx-build: {}", String::from_utf8_lossy(&out.stderr));
             }
-            Ok(out) => {
-                if !out.success() {
-                    panic!("sphinx-build failed to build the man pages.");
-                }
+            assert_eq!(&String::from_utf8_lossy(&out.stdout), "");
+            if !out.status.success() {
+                panic!("sphinx-build failed to build the man pages.");
             }
-        },
+        }
     }
 }

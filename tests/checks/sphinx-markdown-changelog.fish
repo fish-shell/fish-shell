6e90d9bd6f8adf920c9b97b4692116e488761d1e
Johannes Altmanninger <aclopte@gmail.com>
2025-09-25 11:47:04 +0200

    Fix markdown changelog generation test
    System tests typically run outside the workspace directory, but they
    still have read-only access to the workspace; fix it accordingly.
    This test only works on git checkouts, not in tarballs, so skip it
    if .git doesn't exist.

--- tests/checks/sphinx-markdown-changelog.fish
+++ tests/checks/sphinx-markdown-changelog.fish
@@ -2,8 +2,12 @@
 #REQUIRES: command -v sphinx-build
 #REQUIRES: python -c 'import sphinx_markdown_builder'
 
-set -l build_script (status dirname)/../../build_tools/release-notes.sh
-$build_script -q >relnotes.md
+set -l workspace_root (status dirname)/../..
+if not test -e $workspace_root/.git
+    return
+end
+
+$workspace_root/build_tools/release-notes.sh -q >relnotes.md
 or echo "Failed to build Markdown release notes."
-sed -n 1p relnotes.md
-# CHECK: ## fish {{.*}} (released {{.*}})
+sed -n 1p relnotes.md | grep -q '^## fish \S* (released .*)'
+or echo "Unexpected changelog title"

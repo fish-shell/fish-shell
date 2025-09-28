6f0532460a5be9c06fb15caa1f00e716ecef6420
Johannes Altmanninger <aclopte@gmail.com>
2025-09-28 12:21:43 +0200

    fish_config: fix for non-embedded builds
    I only tested with embedded-builds; CMake tests were failing because
    they use different code paths here.
    
    fish_config could use some love.  Start by extracting common
    functionality between "{theme,prompt} show", fixing the behavior.
    
    Fixes 29a35a79518 (fish_config: fix "prompt/theme show" in embed-data
    builds, 2025-09-28).

--- tests/checks/fish_config.fish
+++ tests/checks/fish_config.fish
@@ -1,22 +1,28 @@
  #RUN: %fish %s
 
-fish_config theme show non-existant-theme >/dev/null
+# This still demos the current theme.
+fish_config theme show non-existant-theme
+# CHECK: {{\x1b\[m}}{{\x1b\[4m}}Current{{\x1b\[m}}
+# CHECK: /bright/vixens{{\x1b\[m}} jump{{\x1b\[m}} |{{\x1b\[m}} "fowl"{{\x1b\[m}} > quack{{\x1b\[m}} &{{\x1b\[m}} # This is a comment
+# CHECK: {{\x1b\[m}}echo{{\x1b\[m}} 'Errors are the portal to discovery
+# CHECK: {{\x1b\[m}}Th{{\x1b\[m}}is an autosuggestion
 
-fish_config prompt show non-existant-prompt >/dev/null
+fish_config prompt show non-existant-prompt
 
-# TODO This shouldn't print terminal escape sequences when stdout is not a TTY..
 fish_config prompt show default
 # CHECK: {{\x1b\[4m}}default{{\x1b\[m}}
 # CHECK: {{.*}}@{{.*}}>{{.*}}
 
 fish_config theme show "fish default"
-# CHECK: {{\x1b\[m}}{{\x1b}}[4mCurrent{{\x1b\[m}}
+# CHECK: {{\x1b\[m}}{{\x1b\[4m}}Current{{\x1b\[m}}
 # CHECK: /bright/vixens{{\x1b\[m}} jump{{\x1b\[m}} |{{\x1b\[m}} "fowl"{{\x1b\[m}} > quack{{\x1b\[m}} &{{\x1b\[m}} # This is a comment
-# CHECK: {{\x1b}}[mecho{{\x1b\[m}} 'Errors are the portal to discovery
-# CHECK: {{\x1b}}[mTh{{\x1b}}[mis an autosuggestion
+# CHECK: {{\x1b\[m}}echo{{\x1b\[m}} 'Errors are the portal to discovery
+# CHECK: {{\x1b\[m}}Th{{\x1b\[m}}is an autosuggestion
+# CHECK: {{\x1b\[m}}{{\x1b\[4m}}fish default{{\x1b\[m}}
+# CHECK: {{\x1b\[m}}/bright/vixens{{\x1b\[m}} {{\x1b\[36m}}jump{{\x1b\[m}} {{\x1b\[36m}}{{\x1b\[1m}}|{{\x1b\[m}} {{\x1b\[33m}}"fowl"{{\x1b\[m}} {{\x1b\[36m}}{{\x1b\[1m}}> quack{{\x1b\[m}} {{\x1b\[32m}}&{{\x1b\[m}}{{\x1b\[31m}} # This is a comment
+# CHECK: {{\x1b\[m}}{{\x1b\[m}}echo{{\x1b\[m}} {{\x1b\[91m}}'{{\x1b\[33m}}Errors are the portal to discovery
+# CHECK: {{\x1b\[m}}{{\x1b\[m}}Th{{\x1b\[m}}{{\x1b\[90m}}is an autosuggestion
 
-fish_config theme show
-# CHECK: {{\x1b\[m}}{{\x1b}}[4mCurrent{{\x1b\[m}}
-# CHECK: /bright/vixens{{\x1b\[m}} jump{{\x1b\[m}} |{{\x1b\[m}} "fowl"{{\x1b\[m}} > quack{{\x1b\[m}} &{{\x1b\[m}} # This is a comment
-# CHECK: {{\x1b}}[mecho{{\x1b\[m}} 'Errors are the portal to discovery
-# CHECK: {{\x1b}}[mTh{{\x1b}}[mis an autosuggestion
+fish_config theme show | grep -E 'fish default|Default Dark'
+# CHECK: {{\x1b\[m}}{{\x1b\[4m}}Base16 Default Dark{{\x1b\[m}}
+# CHECK: {{\x1b\[m}}{{\x1b\[4m}}fish default{{\x1b\[m}}

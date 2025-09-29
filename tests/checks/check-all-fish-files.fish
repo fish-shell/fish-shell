1dcc290e297f7d3fd3912f9d501a16d3f085dad9
Johannes Altmanninger <aclopte@gmail.com>
2025-09-29 09:30:35 +0200

    tests/checks/check-all-fish-files.fish: follow naming convention

--- tests/checks/check-all-fish-files.fish
+++ tests/checks/check-all-fish-files.fish
@@ -2,14 +2,14 @@
 # disable on CI ASAN because it's suuuper slow
 #REQUIRES: test -z "$FISH_CI_SAN"
 
-set -l root (path resolve -- (status dirname)/../../)
-set timestamp_file $root/tests/.last-check-all-files
+set -l workspace_root (path resolve -- (status dirname)/../../)
+set timestamp_file $workspace_root/tests/.last-check-all-files
 set -l find_args
 if test -f $timestamp_file
     set find_args -newer $timestamp_file
 end
 set -l fail_count 0
-for file in (find $root/{benchmarks,build_tools,etc,share,tests} -name "*.fish" $find_args)
+for file in (find $workspace_root/{benchmarks,build_tools,etc,share,tests} -name "*.fish" $find_args)
     $fish -n $file; or set fail_count (math $fail_count + 1)
 end
 

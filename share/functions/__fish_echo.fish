d1c85966d90071a2d5f1be9db0c05d958a9eedbd
Johannes Altmanninger <aclopte@gmail.com>
2025-10-06 10:21:24 +0200

    __fish_echo: fully overwrite lines, take 2
    For the same reason as before (upcoming multi-line autosuggestions),
    reapply 1fdf37cc4a2 (__fish_echo: fully overwrite lines, 2025-09-17)
    after it was reverted in b7fabb11ac8 (Make command run by __fish_echo
    output to TTY for color detection, 2025-10-05)
     (Make command run by __fish_echo output
    to TTY for color detection, 2025-10-05).  Use clear-to-end-of-screen
    to allow making "ls" output directly to the terminal.
    
    Alternatively, we could create a pseudo TTY (e.g. call something like
    "unbuffer ls --color=auto").

--- share/functions/__fish_echo.fish
+++ share/functions/__fish_echo.fish
@@ -1,16 +1,16 @@
 # localization: skip(private)
-set -l erase_line "$(
+set -l erase_to_end_of_screen "$(
     if status test-feature ignore-terminfo
-        echo \e\[K
+        echo \e\[J
     else if type -q tput
-        tput el
+        tput ed
     end
 )"
 
-function __fish_echo --inherit-variable erase_line --description 'run the given command after the current commandline and redraw the prompt'
+function __fish_echo --inherit-variable erase_to_end_of_screen --description 'run the given command after the current commandline and redraw the prompt'
     set -l line (commandline --line)
     string >&2 repeat -N \n --count=(math (commandline | count) - $line + 1)
-    printf %s $erase_line >&2
+    printf %s $erase_to_end_of_screen >&2
     $argv >&2
     string >&2 repeat -N \n --count=(math (count (fish_prompt)) - 1)
     string >&2 repeat -N \n --count=(math $line - 1)

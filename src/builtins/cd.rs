b6d08af37039add326c20ba8d902e4e588d20c1e
Asuka Minato <i@asukaminato.eu.org>
2025-10-08 04:26:16 +0900

    try impl cd -P -L

--- src/builtins/cd.rs
+++ src/builtins/cd.rs
@@ -5,11 +5,18 @@ use crate::{
     env::{EnvMode, Environment},
     fds::{wopen_dir, BEST_O_SEARCH},
     path::path_apply_cdpath,
-    wutil::{normalize_path, wperror, wreadlink},
+    wutil::{normalize_path, wperror, wreadlink, wrealpath},
 };
 use errno::Errno;
 use libc::{fchdir, EACCES, ELOOP, ENOENT, ENOTDIR, EPERM};
 use std::{os::fd::AsRawFd, sync::Arc};
+
+const SHORT_OPTIONS: &wstr = L!("hLP");
+const LONG_OPTIONS: &[WOption] = &[
+    wopt(L!("help"), ArgType::NoArgument, 'h'),
+    wopt(L!("logical"), ArgType::NoArgument, 'L'),
+    wopt(L!("physical"), ArgType::NoArgument, 'P'),
+];
 
 // The cd builtin. Changes the current directory to the one specified or to $HOME if none is
 // specified. The directory can be relative to any directory in the CDPATH variable.
@@ -18,18 +25,58 @@ pub fn cd(parser: &Parser, streams: &mut IoStreams, args: &mut [&wstr]) -> Built
         return Err(STATUS_INVALID_ARGS);
     };
 
-    let opts = HelpOnlyCmdOpts::parse(args, parser, streams)?;
+    let argc = args.len();
+    let mut use_physical = false;
+    let mut w = WGetopter::new(SHORT_OPTIONS, LONG_OPTIONS, args);
+    while let Some(opt) = w.next_opt() {
+        match opt {
+            'L' => use_physical = false,
+            'P' => use_physical = true,
+            'h' => {
+                builtin_print_help(parser, streams, cmd);
+                return Ok(SUCCESS);
+            }
+            ';' => {
+                builtin_unexpected_argument(
+                    parser,
+                    streams,
+                    cmd,
+                    args[w.wopt_index - 1],
+                    false,
+                );
+                return Err(STATUS_INVALID_ARGS);
+            }
+            '?' => {
+                builtin_unknown_option(
+                    parser,
+                    streams,
+                    cmd,
+                    args[w.wopt_index - 1],
+                    false,
+                );
+                return Err(STATUS_INVALID_ARGS);
+            }
+            _ => panic!("unexpected option {}", opt),
+        }
+    }
 
-    if opts.print_help {
-        builtin_print_help(parser, streams, cmd);
-        return Ok(SUCCESS);
+    let optind = w.wopt_index;
+    let non_option_argc = argc - optind;
+    if non_option_argc > 1 {
+        streams.err.append(wgettext_fmt!(
+            BUILTIN_ERR_ARG_COUNT1,
+            cmd,
+            1,
+            non_option_argc
+        ));
+        return Err(STATUS_INVALID_ARGS);
     }
 
     let vars = parser.vars();
     let tmpstr;
 
-    let dir_in: &wstr = if args.len() > opts.optind {
-        args[opts.optind]
+    let dir_in: &wstr = if non_option_argc == 1 {
+        args[optind]
     } else {
         match vars.get_unless_empty(L!("HOME")) {
             Some(v) => {
@@ -58,7 +105,15 @@ pub fn cd(parser: &Parser, streams: &mut IoStreams, args: &mut [&wstr]) -> Built
         return Err(STATUS_CMD_ERROR);
     }
 
-    let pwd = vars.get_pwd_slash();
+    let mut pwd = vars.get_pwd_slash();
+    if use_physical {
+        if let Some(mut real_pwd) = wrealpath(&pwd) {
+            if !real_pwd.ends_with('/') {
+                real_pwd.push('/');
+            }
+            pwd = real_pwd;
+        }
+    }
 
     let dirs = path_apply_cdpath(dir_in, &pwd, vars);
     if dirs.is_empty() {
@@ -127,7 +182,13 @@ pub fn cd(parser: &Parser, streams: &mut IoStreams, args: &mut [&wstr]) -> Built
         // Stash the fd for the cwd in the parser.
         parser.libdata_mut().cwd_fd = Some(dir_fd);
 
-        parser.set_var_and_fire(L!("PWD"), EnvMode::EXPORT | EnvMode::GLOBAL, vec![norm_dir]);
+        let mut new_pwd = norm_dir;
+        if use_physical {
+            if let Some(real_dir) = wrealpath(&new_pwd) {
+                new_pwd = real_dir;
+            }
+        }
+        parser.set_var_and_fire(L!("PWD"), EnvMode::EXPORT | EnvMode::GLOBAL, vec![new_pwd]);
         return Ok(SUCCESS);
     }
 

15065255e9726a145e50f97ae5440ec8e7c73209
Johannes Altmanninger <aclopte@gmail.com>
2025-10-07 15:55:46 +0200

    fish_config: fix regression "theme show" not showing custom themes
    This regressed in 6f0532460a5~2..6f0532460a5 (fish_config: fix for
    non-embedded builds, 2025-09-28).
    
    Fixes #11903

--- tests/checks/fish_config.fish
+++ tests/checks/fish_config.fish
@@ -23,6 +23,19 @@ fish_config theme show "fish default"
 # CHECK: {{\x1b\[m}}{{\x1b\[m}}echo{{\x1b\[m}} {{\x1b\[91m}}'{{\x1b\[33m}}Errors are the portal to discovery
 # CHECK: {{\x1b\[m}}{{\x1b\[m}}Th{{\x1b\[m}}{{\x1b\[90m}}is an autosuggestion
 
-fish_config theme show | grep -E 'fish default|Default Dark'
+mkdir $__fish_config_dir/themes
+touch $__fish_config_dir/themes/custom-from-userconf.theme
+fish_config theme show | grep -E 'fish default|Default Dark|custom-from-userconf'
+# CHECK: {{\x1b\[m}}{{\x1b\[4m}}custom-from-userconf{{\x1b\[m}}
 # CHECK: {{\x1b\[m}}{{\x1b\[4m}}Base16 Default Dark{{\x1b\[m}}
 # CHECK: {{\x1b\[m}}{{\x1b\[4m}}fish default{{\x1b\[m}}
+
+# Override a default theme with different colors.
+status get-file tools/web_config/themes/None.theme \
+    >$__fish_config_dir/themes/"fish default.theme"
+fish_config theme show | grep -E 'fish default|Base16 Default Dark' -A1
+# CHECK: {{\x1b\[m}}{{\x1b\[4m}}fish default{{\x1b\[m}}
+# CHECK: {{\x1b\[m}}/bright/vixens{{\x1b\[m}} {{\x1b\[m}}jump{{\x1b\[m}}{{.*}}
+# CHECK: --
+# CHECK: {{\x1b\[m}}{{\x1b\[4m}}Base16 Default Dark{{\x1b\[m}}
+# CHECK: {{.*}}/bright/vixens{{.*}}

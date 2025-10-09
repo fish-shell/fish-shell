eddb26d490e3e8e2f8024a933445d8369fe8c144
Fabian Boehm <FHomborg@gmail.com>
2025-10-09 17:14:09 +0200

    completions/ssh: Don't read ":" from historical hosts
    Fixes #11917

--- share/functions/__ssh_history_completions.fish
+++ share/functions/__ssh_history_completions.fish
@@ -1,4 +1,5 @@
 # localization: tier3
 function __ssh_history_completions -d "Retrieve `user@host` entries from history"
-    history --prefix ssh --max=100 | string replace -rf '.* ([A-Za-z0-9._:-]+@[A-Za-z0-9._:-]+).*' '$1'
+    # Accept the typical hostname/ip chars, but no ":" at the end
+    history --prefix ssh --max=100 | string replace -rf '.* ([A-Za-z0-9._:-]+@[A-Za-z0-9._:-]*[A-Za-z0-9._-]).*' '$1'
 end

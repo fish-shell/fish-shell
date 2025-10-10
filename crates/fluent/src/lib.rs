cf746cbf258be70fa019c22c66c63a96be45c7b9
Daniel Rainer <daniel.rainer@localhost>
2025-10-10 18:06:49 +0200

    fluent: add initial implementation
    Adds an implementation allowing to use Fluent for localizations in
    Rust. This implementation is not used yet.
    
    The goal of this code is to eventually replace gettext, at least for
    localization in Rust, but possibly also for the ones in fish scripts.
    
    To make use of the added code, the `localize!` macro it introduces
    should be used. It takes a `FluentID` as its first argument. This ID is
    used in FTL files to identify messages. In Rust, it can be constructed
    using the new `fluent_id!` macro, which takes a string literal, or by
    directly passing a string literal to `localize!`.
    
    If arguments should be passed to Fluent, this can be done via additional
    arguments to `localize!`. Either, by passing `&FluentArgs` (a type from
    the `fluent` crate), or by specifying the arguments as comma-separated
    key-value pairs, where the key needs to be a string literal, and the
    value needs to be a valid argument to `FluentArgs.set` (also from the
    `fluent` crate). The following example demonstrates the syntax:
    `localize!("some-id", string_arg = "a string", number = 42)`
    The result will be a `String`, formatted according to the rules in the
    relevant FTL file. On errors, this macro panics.
    To integrate with fish, we should add a macro which wraps `localize!`
    and does PUA-decoding and printing in addition to
    localization+formatting.
    
    For specifying a precedence of languages to use for localization, the
    `set_language_precedence` function can be used. Its argument should be a
    (possibly empty) list of language identifiers, such as `de` or `zh_CN`,
    with the preferred language coming first.
    By default, `en` is used as the only language.
    Treating `en` as a fallback language means that every message must have
    a translation in this language, which we enforce via the
    `fish-fluent-check` crate. Therefore, we ignore any language specified
    after `en` in the precedence list. `en` is always the last entry in the
    precedence list, even if it is not specified explicitly, which allows us
    to assume that there is always a valid fallback option.
    
    Translations are specified in the `fluent` directory. For each language,
    there is a file in Fluent's FTL format. The filename indicates the
    language, and has the suffix `.ftl`, e.g. `en.ftl`.
    At the moment, we only have the `en.ftl`, since no translations have
    been added yet.
    `rust-embed` is used to make these files available to the binary at
    runtime. Files will only be parsed if they are specified in the
    precedence list. (`en.ftl` is always parsed.)
    
    The `fish-fluent-check` crate contains a binary which can be used to
    check our FTL files. It uses the `fish-fluent-extract` crate, to extract
    Fluent IDs from the Rust sources.
    It panics if any of these properties are violated:
    - `en.ftl` does not contain every ID contained in the sources.
    - Any FTL file contains an ID not contained in the sources.
    - Any FTL file is not sorted by ID.
    These checks still need to be integrated into `check.sh` and CI.

---
+++ crates/fluent/src/lib.rs
@@ -0,0 +1,262 @@
+use std::{
+    borrow::Cow,
+    collections::{HashMap, HashSet},
+    sync::Mutex,
+};
+
+use cfg_if::cfg_if;
+use fluent::{FluentArgs, FluentResource, concurrent::FluentBundle};
+use rust_embed::{EmbeddedFile, RustEmbed};
+
+#[cfg(feature = "fluent-extract")]
+pub extern crate fish_fluent_extraction;
+
+use once_cell::sync::Lazy;
+use unic_langid::LanguageIdentifier;
+
+pub static FALLBACK_LANGUAGE: &str = "en";
+
+static LANGUAGE_BUNDLES: Lazy<Mutex<HashMap<String, &'static FluentBundle<FluentResource>>>> =
+    Lazy::new(|| Mutex::new(HashMap::new()));
+
+static LANGUAGE_BUNDLE_PRECEDENCE: Lazy<Mutex<Vec<&'static FluentBundle<FluentResource>>>> =
+    Lazy::new(|| Mutex::new(vec![get_fallback_bundle()]));
+
+cfg_if!(
+    if #[cfg(feature = "localize-messages")] {
+        #[derive(RustEmbed)]
+        #[folder = "../../localization/fluent/"]
+        #[include = "*.ftl"]
+        struct FtlFiles;
+    } else {
+        #[derive(RustEmbed)]
+        #[folder = "../../localization/fluent/"]
+        #[include = "en.ftl"]
+        struct FtlFiles;
+    }
+);
+
+fn add_file(language: &str, file: EmbeddedFile) -> &'static FluentBundle<FluentResource> {
+    let mut bundles = LANGUAGE_BUNDLES.lock().unwrap();
+    // Reuse existing bundles.
+    if let Some(bundle) = bundles.get(language) {
+        return bundle;
+    }
+    let langid: LanguageIdentifier = language
+        .parse()
+        .map_err(|e| format!("Failed to parse language identifier {language}: {e}"))
+        .unwrap();
+    let mut bundle = FluentBundle::new_concurrent(vec![langid]);
+    match FluentResource::try_new(
+        String::from_utf8(Vec::from(file.data))
+            .unwrap_or_else(|e| panic!("FTL file for {language} is not valid UTF-8: {e}")),
+    ) {
+        Ok(res) => {
+            bundle
+                .add_resource(res)
+                .map_err(|e| {
+                    format!(
+                        "Failed to add FTL resources to the bundle for language {language}: {e:?}"
+                    )
+                })
+                .unwrap();
+            // Isolation marks can result in undesirable behavior if the terminal does not support
+            // them properly.
+            // Turn this off for now.
+            // If we add detection for terminal support, we could enable it conditionally.
+            // Without these marks, text order can be incorrect when right-to-left characters are
+            // involved.
+            // https://www.w3.org/International/questions/qa-bidi-unicode-controls
+            // https://github.com/fish-shell/fish-shell/pull/11928#discussion_r2488850606
+            bundle.set_use_isolating(false);
+            // Leak to create static reference.
+            let bundle = Box::leak(Box::new(bundle));
+            bundles.insert(language.into(), bundle);
+            bundle
+        }
+        Err((_resource, errors)) => {
+            let mut error_string = format!("Errors parsing FTL file for {language}:\n");
+            for error in errors {
+                error_string.push_str(&format!("{error}\n"));
+            }
+            panic!("{error_string}");
+        }
+    }
+}
+
+/// Tries to add a bundle for the given language and adds it to the `LANGUAGE_BUNDLES` map.
+fn add_language(language: &str) -> Vec<&'static FluentBundle<FluentResource>> {
+    if let Some(file) = FtlFiles::get(&format!("{language}.ftl")) {
+        // exact match
+        return vec![add_file(language, file)];
+    }
+    let language_without_country_code = language.split_once('_').map_or(language, |(ll, _cc)| ll);
+    if language == language_without_country_code {
+        // `language` is in `ll` format.
+        // In this case, we already know that there is no exact match,
+        // so we add all languages starting with `ll_` in arbitrary order.
+        // Note that it is important to include the underscore in the pattern, otherwise `ll` might
+        // fall back to `llx_CC`, where `llx` is a 3-letter language identifier.
+        let ll_prefix = format!("{language}_");
+        let mut langs = vec![];
+        for lang_name in FtlFiles::iter() {
+            if lang_name.starts_with(&ll_prefix) {
+                langs.push(add_file(
+                    lang_name.strip_suffix(".ftl").unwrap(),
+                    FtlFiles::get(&lang_name).unwrap(),
+                ));
+            }
+        }
+        return langs;
+    } else {
+        // If `language` contained a country code, we only try to fall back to a catalog
+        // without a country code.
+        if let Some(file) = FtlFiles::get(&format!("{language_without_country_code}.ftl")) {
+            vec![add_file(language_without_country_code, file)]
+        } else {
+            vec![]
+        }
+    }
+}
+
+/// Gets the bundle for the fallback language.
+/// Panics if the fallback language does not match exactly one available bundle.
+fn get_fallback_bundle() -> &'static FluentBundle<FluentResource> {
+    let fallback_bundles = add_language(FALLBACK_LANGUAGE);
+    assert_eq!(
+        fallback_bundles.len(),
+        1,
+        "add_language must return exactly one bundle for the fallback language"
+    );
+    fallback_bundles[0]
+}
+
+/// Set the order in which languages should be tried for localization.
+/// The fallback language (en) will be added to the end of the list.
+/// The provided list contains `en`, anything after it will be ignored, since we assume that every
+/// message can be localized in English, so there is no point in specifying further fallback
+/// options.
+/// This function also takes care of lazily loading and parsing data from the embedded FTL files,
+/// so no additional initialization is needed.
+pub fn set_language_precedence(languages: &[&str]) {
+    let fallback_bundle = get_fallback_bundle();
+    // Only take the languages preceding the fallback language, since it is assumed that everything
+    // is translatable in the fallback language.
+    // Filter bundles such that only first occurrence is used, to speed up lookups.
+    let mut seen_languages = HashSet::new();
+    let mut new_precedence: Vec<_> = languages
+        .iter()
+        .take_while(|&&lang| lang != FALLBACK_LANGUAGE)
+        .flat_map(|&lang| add_language(lang))
+        .filter(|bundle| seen_languages.insert(bundle.locales[0].language))
+        .collect();
+    // Add the fallback language at the end of the precedence list.
+    new_precedence.push(fallback_bundle);
+    let mut bundle_precedence = LANGUAGE_BUNDLE_PRECEDENCE.lock().unwrap();
+    *bundle_precedence = new_precedence;
+}
+
+/// Use the [`localize!`] macro instead of calling this directly.
+/// Panics on errors.
+pub fn format_localized(id: &FluentID, args: &FluentArgs) -> Cow<'static, str> {
+    let id = id.0;
+    let mut errors = vec![];
+    let bundle_precedence = LANGUAGE_BUNDLE_PRECEDENCE.lock().unwrap();
+    for bundle in bundle_precedence.iter() {
+        let Some(message) = bundle.get_message(id) else {
+            continue;
+        };
+        let pattern = message.value().expect("Message has no value.");
+        let value = bundle.format_pattern(pattern, Some(args), &mut errors);
+        // NOTE: Unused arguments are not considered errors.
+        if !errors.is_empty() {
+            let mut error_message = format!(
+                "Unexpected formatting errors occurred for message ID '{id}' in language '{}':\n",
+                bundle.locales[0].language
+            );
+            for error in errors {
+                error_message.push_str(&format!("{error}\n"));
+            }
+            panic!("{error_message}");
+        }
+        return value;
+    }
+    panic!("Message '{id}' not available in any catalog.")
+}
+
+cfg_if!(
+    if #[cfg(feature = "fluent-extract")] {
+        /// Use this to construct `FluentID`s.
+        #[macro_export]
+        macro_rules! fluent_id {
+            ($id:literal) => {
+                $crate::FluentID::new(fish_fluent_extraction::fluent_extract!($id))
+            };
+        }
+    } else {
+        #[macro_export]
+        macro_rules! fluent_id {
+            ($id:literal) => {
+                $crate::FluentID::new($id)
+            };
+        }
+    }
+);
+
+/// Takes a string literal or a [`FluentID`].
+/// If the argument is a string literal, it is converted to a [`FluentID`],
+/// which marks it for extraction.
+#[macro_export]
+macro_rules! ensure_fluent_id {
+    ($id:literal) => {
+        $crate::fluent_id!($id)
+    };
+    ($id:expr) => {
+        $id
+    };
+}
+
+/// Takes a [`FluentID`] and an [`Option<&FluentArgs>`].
+/// Returns a localized string according to the current language precedence,
+/// or panics if an error occurs.
+#[macro_export]
+macro_rules! localize_impl {
+    ($id:expr, $args:expr) => {
+        $crate::format_localized(&$crate::ensure_fluent_id!($id), $args)
+    };
+}
+
+/// Call this to localize a message with Fluent.
+/// The first argument can be a `&FluentID` or a string literal, which will then be converted to a
+/// `&FluentID` implicitly.
+/// Note that Fluent attributes are not supported.
+/// If arguments need to be passed, this can be done by specifying a `&FluentArgs` value as the
+/// second argument, or by adding comma-separated `key = value` pairs,
+/// where the key is syntactically a Rust identifier.
+#[macro_export]
+macro_rules! localize {
+    // For passing pre-built `&FluentArgs`
+    ($id:expr, $args:expr) => {
+        $crate::localize_impl!($id, $args)
+    };
+    // For passing args in `key = value` format (or no arguments)
+    ($id:expr $(, $key:ident = $value:expr)* $(,)?) => {
+        {
+            let mut args = fluent::FluentArgs::new();
+            $(
+                args.set(stringify!($key), $value);
+            )*
+            $crate::localize_impl!($id, &args)
+        }
+    };
+}
+
+/// Wrapper type which must not be constructed directly, only via the [`fluent_id!`] macro.
+/// This ensures that we can extract the IDs from the source code via a proc macro when needed.
+pub struct FluentID(&'static str);
+
+impl FluentID {
+    pub const fn new(id: &'static str) -> Self {
+        Self(id)
+    }
+}

1c8d70a046277dce3e6143f148d23aaa93b5d2c9
Daniel Rainer <daniel.rainer@localhost>
2025-10-10 18:06:49 +0200

    fluent: add initial implementation
    Adds an implementation allowing to use Fluent for localizations in
    Rust. This implementation is not used yet.
    
    The goal of this code is to eventually replace gettext, at least for
    localizations in Rust, but possibly also for the ones in fish scripts.
    
    To make use of the added code, the `localize!` macro it introduces
    should be used. It takes a `FluentID` as its first argument. This ID is
    used in FTL files to identify messages. In Rust, it can be constructed
    using the new `fluent_id!` macro, which takes a string literal, or by
    directly passing a string literal to `localize!`.
    
    If arguments should be passed to Fluent, this can be done via additional
    arguments to `localize!`. Either, by passing `&FluentArgs` (a type from
    the `fluent` crate), or by specifying the arguments as comma-separated
    key-value pairs, where the key needs to be a string literal, and the
    value needs to be a valid argument to `FluentArgs.set` (also from the
    `fluent` crate). The following example demonstrates the syntax:
    `localize!("some-id", "string arg": "a string", "number": 42)`
    The result will be a `String`, formatted according to the rules in the
    relevant FTL file. On errors, this macro panics.
    To integrate with fish, we should add a macro which wraps `localize!`
    and does PUA-decoding and printing in addition to
    localization+formatting.
    
    For specifying a precedence of languages to use for localization, the
    `set_language_precedence` function can be used. Its argument should be a
    (possibly empty) list of language identifiers, such as `de` or `zh_CN`,
    with the preferred language coming first. By default, `en` is used as
    the only language, before this function is called.
    Treating `en` as a fallback language means that every message must have
    a translation in this language, which we enforce via the
    `fish-fluent-check` crate. Therefore, we ignore any language specified
    after `en` in the precedence list. `en` is always the last entry in the
    precedence list, even if it is not specified explicitly, which allows us
    to assume that there is always a valid fallback option.
    
    Translations are specified in the `fluent` directory. For each language,
    there is a file in Fluent's FTL format. The filename indicates the
    language, and has the suffix `.ftl`, e.g. `en.ftl`. At the moment, these
    contain no data, so we only have the empty file `en.ftl`.
    `rust-embed` is used to make these files available to the binary at
    runtime. Files will only be parsed if they are specified in the
    precedence list. (`en.ftl` is always parsed.)
    
    The `fish-fluent-check` crate contains a binary which can be used to
    check our FTL files. It uses the `fish-fluent-extract` crate, to extract
    Fluent IDs from the Rust sources.
    It panics if any of these properties are violated:
    - `en.ftl` does not contain every ID contained in the sources.
    - Any FTL file contains an ID not contained in the sources.
    - Any FTL file is not sorted by ID.
    These checks still need to be integrated into `check.sh` and CI.

---
+++ crates/fluent-extraction/src/lib.rs
@@ -0,0 +1,87 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use std::{ffi::OsString, fs::OpenOptions, io::Write};
+
+fn append_fluent_id_to_file(fluent_id: &TokenStream, file_path: &OsString) {
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(file_path)
+        .unwrap_or_else(|e| panic!("Could not open file {file_path:?}: {e}"));
+    let fluent_id = fluent_id.to_string();
+    if fluent_id.starts_with('-') {
+        panic!(
+            "Fluent ID may not start with a '-'. A leading '-' marks a term within an FTL file.\nAffected ID: {fluent_id}"
+        );
+    }
+    if fluent_id.contains('\n') {
+        panic!("Fluent IDs may not contain newlines. Newline found in '{fluent_id}'")
+    }
+    if fluent_id.contains('.') {
+        panic!(
+            "Fluent IDs may not contain periods. Period found in '{fluent_id}'.\nFluent attributes are unsupported."
+        )
+    }
+    if !fluent_id.starts_with('"') {
+        panic!("Fluent ID token stream does not start with double quote: {fluent_id}");
+    }
+    if !fluent_id.ends_with('"') {
+        panic!("Fluent ID token stream does not end with double quote: {fluent_id}");
+    }
+    if fluent_id.len() < 3 {
+        panic!("Fluent ID is empty: {fluent_id}");
+    }
+    let id_bytes = &fluent_id.as_bytes()[1..(fluent_id.len() - 1)];
+    file.write_all(id_bytes).unwrap();
+    file.write_all(b"\n").unwrap();
+}
+
+/// The `fluent_id` is passed through unmodified.
+/// If `FISH_FLUENT_ID_FILE` is defined in the environment,
+/// the Fluent ID is appended to the file at this path,
+/// so that it can be used to check whether all IDs have associated translations in the
+/// default/fallback language, and that no ID are specified in FTL files which are not used by the
+/// code.
+/// The `fluent_id` must be a string literal.
+///
+/// # Panics
+///
+/// This macro panics if the `FISH_FLUENT_ID_FILE` variable is set and `fluent_id` has an
+/// unexpected format.
+/// Note that for example `concat!(...)` cannot be passed to this macro, because expansion works
+/// outside in, meaning this macro would still see the `concat!` macro invocation, instead of a
+/// string literal.
+#[proc_macro]
+pub fn fluent_extract(fluent_id: TokenStream) -> TokenStream {
+    if let Some(file_path) = std::env::var_os("FISH_FLUENT_ID_FILE") {
+        let pm2_fluent_id = proc_macro2::TokenStream::from(fluent_id.clone());
+        let mut token_trees = pm2_fluent_id.into_iter();
+        let first_token = token_trees
+            .next()
+            .expect("fluent_extract got empty token stream. Expected one token.");
+        if token_trees.next().is_some() {
+            panic!(
+                "Invalid number of tokens passed to fluent_extract. Expected one token, but got more."
+            )
+        }
+        if let proc_macro2::TokenTree::Group(group) = first_token {
+            let mut group_tokens = group.stream().into_iter();
+            let first_group_token = group_tokens
+                .next()
+                .expect("fluent_extract expected one group token but got none.");
+            if group_tokens.next().is_some() {
+                panic!(
+                    "Invalid number of tokens in group passed to fluent_extract. Expected one token, but got more."
+                )
+            }
+            if let proc_macro2::TokenTree::Literal(_) = first_group_token {
+                append_fluent_id_to_file(&fluent_id, &file_path);
+            } else {
+                panic!("Expected literal in fluent_extract, but got: {first_group_token:?}");
+            }
+        } else {
+            panic!("Expected group in fluent_extract, but got: {first_token:?}");
+        }
+    }
+    fluent_id
+}

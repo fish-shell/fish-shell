f03113d048a8ceaf0e2ad3639789154f15ce4aca
Johannes Altmanninger <aclopte@gmail.com>
2025-10-12 06:40:48 +0200

    __fish_cache_put: fix for BusyBox stat
    On alpine, tests/checks/check-completions fails with
    
    	stat: can't read file system information for '%u:%g': No such file or directory

--- share/functions/__fish_cache_put.fish
+++ share/functions/__fish_cache_put.fish
@@ -5,7 +5,11 @@ function __fish_cache_put
     set -l dir (path dirname $cache_file)
     chown --reference=$dir $cache_file 2>/dev/null ||
         chown (
-            stat --format '%u:%g' $dir 2>/dev/null ||
-            stat -f '%u:%g' $dir
+            if stat --version 2>&1 | string match -q 'BusyBox*'
+                stat -c '%u:%g' $dir
+            else
+                stat --format '%u:%g' $dir 2>/dev/null ||
+                stat -f '%u:%g' $dir
+            end
         ) $cache_file
 end

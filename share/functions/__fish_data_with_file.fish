48bdf249648af1a624da2c467e5f7da481bd2a12
Johannes Altmanninger <aclopte@gmail.com>
2025-10-14 10:14:00 +0200

    __fish_data_with_file: better variable name

--- share/functions/__fish_data_with_file.fish
+++ share/functions/__fish_data_with_file.fish
@@ -1,10 +1,10 @@
 # localization: skip(private)
 function __fish_data_with_file
-    set -l file $argv[1]
+    set -l path $argv[1]
     set -l cmd $argv[2..]
     if set -q __fish_data_dir[1]
-        $cmd $__fish_data_dir/$file
+        $cmd $__fish_data_dir/$path
     else
-        status get-file $file | $cmd
+        status get-file $path | $cmd
     end
 end

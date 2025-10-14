ea69133e48b7bc7b906824c518cd81026264b5ed
Johannes Altmanninger <aclopte@gmail.com>
2025-10-14 08:46:39 +0200

    tests/fish_delta

--- tests/checks/fish_delta.fish
+++ tests/checks/fish_delta.fish
@@ -1,8 +1,21 @@
 # RUN: %fish %s
 
-fish_delta | grep -A1 '\bfunction __fish_print_help\b'
-# CHECK: -function __fish_print_help --description "Print help for the specified fish function or builtin"
-# CHECK: -    set -l item $argv[1]
+touch $__fish_config_dir/functions/delta-test-custom-function.fish
+for path in fish_greeting fish_job_summary
+    __fish_data_with_file functions/$path.fish \
+        cat >$__fish_config_dir/functions/$path.fish
+end
+sed -i 's/$/ # Modified/' $__fish_config_dir/functions/fish_greeting.fish
+
+fish_delta --no-diff --new |
+    string match -r '.*\b(?:delta-test|fish_greeting|fish_job_summary)\b.*'
+# CHECK: New: {{.*}}/fish/functions/delta-test-custom-function.fish
+# CHECK: Changed: {{.*}}/fish/functions/fish_greeting.fish
+# CHECK: Unmodified: {{.*}}/fish/functions/fish_job_summary.fish
+
+fish_delta | grep -A1 '\bfunction fish_greeting\b'
+# CHECK: -function fish_greeting
+# CHECK: -    if not set -q fish_greeting
 # CHECK: --
-# CHECK: +function __fish_print_help
-# CHECK: +    echo Documentation for $argv >&2
+# CHECK: +function fish_greeting # Modified
+# CHECK: +    if not set -q fish_greeting # Modified

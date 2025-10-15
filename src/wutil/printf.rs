78e8f87e54d9d82c08cccd5cafc95126afe07274
Daniel Rainer <daniel.rainer@localhost>
2025-10-16 01:48:18 +0200

    tests: remove duplicates
    These were made duplicates by removing length modifiers.
    
    Closes #11951

--- src/wutil/printf.rs
+++ src/wutil/printf.rs
@@ -55,7 +55,5 @@ mod tests {
     #[test]
     fn test_sprintf() {
         assert_eq!(sprintf!("Hello, %s!", "world"), "Hello, world!");
-        assert_eq!(sprintf!("Hello, %s!", "world"), "Hello, world!");
-        assert_eq!(sprintf!("Hello, %s!", "world"), "Hello, world!");
     }
 }

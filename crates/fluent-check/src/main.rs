d435844bc7179412d6239802d9e3407fac472ac7
Daniel Rainer <daniel.rainer@localhost>
2025-10-16 01:00:34 +0200

    fluent: add cargo test to check FTL files
    Runs the checks we have for FTL files as a cargo test. To avoid
    recompilation, the `FISH_FLUENT_ID_FILE` env var is used to communicate
    to the `fish-fluent-check` binary to use an existing file instead of
    compiling fish when the env var is set.
    
    This means that the test will run from `check.sh` and in CI. For the
    former, we can avoid recompilation of fish by making use of the new env
    var. In CI, we don't support that yet.

--- crates/fluent-check/src/main.rs
+++ crates/fluent-check/src/main.rs
@@ -45,23 +45,31 @@ fn resources_to_bundles(
 }
 
 fn extract_fluent_ids() -> HashSet<String> {
-    let mut tempfile = fish_tempfile::new_file().unwrap();
-    Command::new(env!("CARGO"))
-        .args([
-            "check",
-            "--features=fluent-extract",
-            "--workspace",
-            "--all-targets",
-        ])
-        .env("FISH_FLUENT_ID_FILE", tempfile.path().as_os_str())
-        .status()
-        .map_err(|e| format!("Failed to extract Fluent IDs: {e}"))
-        .unwrap();
-    let mut id_file_content = String::new();
-    tempfile
-        .get_mut()
-        .read_to_string(&mut id_file_content)
-        .unwrap();
+    let id_file_content = match std::env::var_os("FISH_FLUENT_ID_FILE") {
+        Some(file_path) => std::fs::read_to_string(&file_path).unwrap_or_else(|_| {
+            panic!("Failed to open file {file_path:?} (passed via FISH_FLUENT_ID_FILE)")
+        }),
+        None => {
+            let mut tempfile = fish_tempfile::new_file().unwrap();
+            Command::new(env!("CARGO"))
+                .args([
+                    "check",
+                    "--features=fluent-extract",
+                    "--workspace",
+                    "--all-targets",
+                ])
+                .env("FISH_FLUENT_ID_FILE", tempfile.path().as_os_str())
+                .status()
+                .map_err(|e| format!("Failed to extract Fluent IDs: {e}"))
+                .unwrap();
+            let mut file_content = String::new();
+            tempfile
+                .get_mut()
+                .read_to_string(&mut file_content)
+                .unwrap();
+            file_content
+        }
+    };
     HashSet::from_iter(id_file_content.lines().map(|line| line.to_string()))
 }
 
@@ -182,5 +190,15 @@ fn check_for_unsorted_ids(
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check() {
+        main();
     }
 }

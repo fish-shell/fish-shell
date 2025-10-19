9657e6f7aee50ae59b20018865cf72542fd787d0
kerty <g.kabakov@inbox.ru>
2025-10-19 17:09:00 +0300

    Fix not preserving empty last line in multiline prompts
    Reproduction:
    fish -C '
        function fish_prompt
            echo left-prompt\n
        end
        function fish_right_prompt
            echo right-prompt
        end
    '
    and pressing Enter would not preserve the line with right prompt.
    
    This occurred because Screen::cursor_is_wrapped_to_own_line assumed
    the last prompt line always had index 0. However, commit 606802daa (Make
    ScreenData track multiline prompt, 2025-10-15) changed this so the last
    prompt line's index is now `Screen.actual.visible_prompt_lines - 1`.
    
    Screen::cursor_is_wrapped_to_own_line also didn't account for situations
    where commandline indentation was 0.
    
    Fix Screen::cursor_is_wrapped_to_own_line and add tests for prompts with
    empty last lines.

---
+++ tests/checks/tmux-empty-prompt.fish
@@ -0,0 +1,80 @@
+#RUN: %fish %s
+#REQUIRES: command -v tmux
+
+isolated-tmux-start
+isolated-tmux send-keys '
+    function fish_prompt; end
+    function fish_right_prompt
+        set -q right_prompt
+        and echo right-prompt
+    end
+    set right_prompt 1
+    bind ctrl-g "set right_prompt 1" repaint
+    bind alt-g "set -e right_prompt" repaint
+' 
+tmux-sleep
+isolated-tmux send-keys M-g C-l Enter  
+tmux-sleep
+isolated-tmux send-keys C-g Enter
+tmux-sleep
+isolated-tmux capture-pane -p | string replace -r '$' '+'
+#CHECK: +
+#CHECK: right-prompt+
+#CHECK: right-prompt+
+#CHECK: +
+#CHECK: +
+#CHECK: +
+#CHECK: +
+#CHECK: +
+#CHECK: +
+#CHECK: +
+
+isolated-tmux send-keys M-g Tab Tab
+tmux-sleep
+isolated-tmux capture-pane -p | string replace -r '$' '+'
+#CHECK: +
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+
+isolated-tmux send-keys '
+    function fish_prompt
+        echo left-prompt\n
+    end
+'  
+tmux-sleep
+isolated-tmux send-keys C-l Enter  
+tmux-sleep
+isolated-tmux send-keys C-g Enter
+tmux-sleep
+isolated-tmux capture-pane -p | string replace -r '$' '+'
+#CHECK: left-prompt+
+#CHECK: +
+#CHECK: left-prompt+
+#CHECK: right-prompt+
+#CHECK: left-prompt+
+#CHECK: right-prompt+
+#CHECK: +
+#CHECK: +
+#CHECK: +
+#CHECK: +
+
+isolated-tmux send-keys M-g Tab Tab
+tmux-sleep
+isolated-tmux capture-pane -p | string replace -r '$' '+'
+#CHECK: left-prompt+
+#CHECK: +
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+
+#CHECK: {{.*}}+

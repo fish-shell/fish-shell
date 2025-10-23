27f49b9523ffa7d17c52b39fd5e14c277c7f628b
Johannes Altmanninger <aclopte@gmail.com>
2025-10-23 18:40:13 +0200

    __fish_macos_set_env: extract function to make it testable

---
+++ tests/checks/__fish_macos_set_env.fish
@@ -0,0 +1,16 @@
+# RUN: %fish %s
+
+mkdir etc
+{
+    echo /usr/share/man
+    echo /usr/local/share/man
+} >etc/manpaths
+mkdir etc/manpaths.d
+
+MANPATH=:/custom/man-path \
+    __fish_macos_set_env MANPATH etc/manpaths etc/manpaths.d
+string join \n entry=$MANPATH
+# CHECK: entry=/usr/share/man
+# CHECK: entry=/usr/local/share/man
+# CHECK: entry=
+# CHECK: entry=/Applications/Ghostty.app/Contents/Resources/ghostty/../man

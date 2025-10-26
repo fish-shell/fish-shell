d27537c4fc67e763979475b4fc958a6e1eae8acb
David Adam <zanchey@ucc.gu.uwa.edu.au>
2025-10-26 10:34:16 +0800

    CMake: use configured Rust_CARGO for tests, not the cargo on $PATH

--- cmake/Tests.cmake
+++ cmake/Tests.cmake
@@ -57,7 +57,7 @@ endif()
 add_custom_target(fish_run_tests
   # TODO: This should be replaced with a unified solution, possibly build_tools/check.sh.
   COMMAND ${CMAKE_SOURCE_DIR}/tests/test_driver.py ${max_concurrency_flag} ${CMAKE_CURRENT_BINARY_DIR}
-  COMMAND env ${VARS_FOR_CARGO} cargo test --no-default-features ${CARGO_FLAGS} --workspace --target-dir ${rust_target_dir} ${cargo_test_flags}
+  COMMAND env ${VARS_FOR_CARGO} ${Rust_CARGO} test --no-default-features ${CARGO_FLAGS} --workspace --target-dir ${rust_target_dir} ${cargo_test_flags}
   WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
   DEPENDS fish fish_indent fish_key_reader fish_test_helper
   USES_TERMINAL

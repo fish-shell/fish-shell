9b75b6ee88faf076a22ebe344917216227707d0e
Daniel Rainer <daniel.rainer@localhost>
2025-11-05 20:45:58 +0100

    l10n: move `po/` to `localization/po/`
    This is done in preparation for Fluent's FTL files, which will be placed
    in `localization/fluent/`. Having a shared parent reduces top-level
    clutter in the repo and makes it easier to find the localization files
    for translators, including realizing that both PO and FTL files exist.
    
    We keep PO and FTL files in separate directories because we need to
    rebuild on any changes in the PO directory (technically only when there
    are changes to `*.po` files, but due to technical limitations we can't
    reliably trigger rebuilds only if those changes but not other files in
    the same directory.) Changes to FTL files do not require rebuilds for
    dev builds, since for these `rust-embed` does not actually embed them
    into the binary but rather loads them from the file system at runtime.
    
    Closes #12193

--- build_tools/update_translations.fish
+++ build_tools/update_translations.fish
@@ -9,9 +9,10 @@
 # For developers:
 #   - Run with no args to update all PO files after making changes to Rust/fish sources.
 # For translators:
-#   - Specify the language you want to work on as an argument, which must be a file in the po/
-#     directory. You can specify a language which does not have translations yet by specifying the
-#     name of a file which does not yet exist. Make sure to follow the naming convention.
+#   - Specify the language you want to work on as an argument, which must be a file in the
+#     localization/po/ directory. You can specify a language which does not have translations
+#     yet by specifying the name of a file which does not yet exist.
+#     Make sure to follow the naming convention.
 # For testing:
 #   - Specify `--dry-run` to see if any updates to the PO files would by applied by this script.
 #     If this flag is specified, the script will exit with an error if there are outstanding
@@ -31,7 +32,7 @@
 set -gx LC_ALL C.UTF-8
 
 set -l build_tools (status dirname)
-set -l po_dir $build_tools/../po
+set -l po_dir $build_tools/../localization/po
 
 set -l extract
 
@@ -89,8 +90,9 @@ if set -l --query extract
 end
 
 if set -l --query _flag_dry_run
-    # On a dry run, we do not modify po/ but write to a temporary directory instead and check if
-    # there is a difference between po/ and the tmpdir after re-generating the PO files.
+    # On a dry run, we do not modify localization/po/ but write to a temporary directory instead
+    # and check if there is a difference between localization/po/ and the tmpdir after re-generating
+    # the PO files.
     set -g tmpdir (mktemp -d)
 
     # Ensure tmpdir has the same initial state as the po dir.
@@ -146,7 +148,7 @@ end
 if set -g --query tmpdir[1]
     diff -ur $po_dir $tmpdir
     or begin
-        echo ERROR: translations in ./po/ are stale. Try running build_tools/update_translations.fish
+        echo ERROR: translations in localization/po/ are stale. Try running build_tools/update_translations.fish
         cleanup_exit
     end
 end

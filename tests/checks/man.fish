8cc25fe7721e5509e28e03b23a00583871be4306
Johannes Altmanninger <aclopte@gmail.com>
2025-11-10 08:37:32 +0100

    tests/man: work around man idiosyncracy on macOS/FreeBSD
    This man implementation fails to remove control characters when stdout
    is not a TTY. Upstream tracking issue:
    https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=282414

--- tests/checks/man.fish
+++ tests/checks/man.fish
@@ -8,12 +8,14 @@ __fish_data_with_file functions/__fish_print_help.fish source
 # REQUIRES: test "$FISH_BUILD_DOCS" != "0"
 # REQUIRES: %fish -c 'status build-info' | grep '^Features:.*embed-data'
 
+set -l deroff col -b -p -x
+
 set -lx MANWIDTH 80
-man abbr | head -n4
+man abbr | $deroff | head -n4
 # CHECK: ABBR(1) {{ *}} fish-shell {{ *}} ABBR(1)
 # CHECK: NAME
 # CHECK:        abbr - manage fish abbreviations
-man : | head -n4
+man : | $deroff | head -n4
 # CHECK: TRUE(1) {{ *}} fish-shell {{ *}} TRUE(1)
 # CHECK: NAME
 # CHECK:        true - return a successful result

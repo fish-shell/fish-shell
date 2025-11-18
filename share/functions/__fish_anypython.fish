55b3b6af791b5208effb11ad112cbe61001df949
Johannes Altmanninger <aclopte@gmail.com>
2025-11-18 09:02:47 +0100

    __fish_anypython: remove hardcoded list of minor versions
    We always use python3 but don't use, say python3.999 until we update
    our hardcoded list.  This is inconsistent (we're careful about the
    latter but not the former).  Fix this by always picking the highest
    minor version (>= 3.5+) there is. This also reduces maintenance work.
    
    Note that NetBSD is the only OS we know about that doesn't provide
    the python3 symlink OOTB.  In future, the NetBSD package should
    add a patch to replace "python3" in __fish_anypython.fish
    with the correct path.  Downstream discussion is at
    https://mail-index.netbsd.org/pkgsrc-users/2025/11/17/msg042205.html

--- share/functions/__fish_anypython.fish
+++ share/functions/__fish_anypython.fish
@@ -1,19 +1,35 @@
 # localization: skip(private)
 function __fish_anypython
-    # Try python3 first, because that's usually faster and generally nicer.
-    # Do not consider the stub /usr/bin/python3 that comes installed on Darwin to be Python
-    # unless Xcode reports a real directory path.
-    for py in python3 python3.{14,13,12,11,10,9,8,7,6,5}
-        if set -l py_path (command -s $py)
-            if string match -q /usr/bin/python3 -- $py_path
-                and string match -q Darwin -- "$(uname)"
-                and type -q xcode-select
-                and not xcode-select --print-path &>/dev/null
-                continue
-            end
-            echo $py
-            return 0
+    if set -l path (command -s python3)
+        # Do not consider the stub /usr/bin/python3 that comes installed on Darwin to be Python
+        # unless Xcode reports a real directory path.
+        and not begin
+            string match -q Darwin -- "$(uname)"
+            and string match -q /usr/bin/python3 -- $path
+            and type -q xcode-select
+            and not xcode-select --print-path &>/dev/null
         end
+        echo $path
+        return 0
+    end
+    set -l best_path
+    set -l best_minor_version
+    set -l minor_version
+    for path in (path filter -fx -- $PATH/python3.*)
+        string match -rq -- '.*/python3\.(?<minor_version>\d+)$' $path
+        or continue
+        test $minor_version -lt 5 # Python 3.5+
+        and continue
+        and begin
+            not set -q best_path[1]
+            or test $minor_version -gt $best_minor_version
+        end
+        set best_path $path
+        set best_minor_version $minor_version
+    end
+    if set -q best_path[1]
+        echo $best_path
+        return 0
     end
     # We have no python.
     return 1

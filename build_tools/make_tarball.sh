548f37eabba156393775fc0f5f88954df6294e64
Johannes Altmanninger <aclopte@gmail.com>
2025-11-20 08:13:30 +0100

    Simplify make_tarball.sh
    Note that we don't need to set the mtime anymore -- it was added in
    5b5b53872c5 (tarball generation: include config.h.in, set mode and
    ownership, 2013-09-09) for autotools' multi-stage builds.

--- build_tools/make_tarball.sh
+++ build_tools/make_tarball.sh
@@ -6,56 +6,18 @@
 # Exit on error
 set -e
 
-# We will generate a tarball with a prefix "fish-VERSION"
-
-# We need GNU tar as that supports the --mtime and --transform options
-TAR=notfound
-for try in tar gtar gnutar; do
-    if $try -Pcf /dev/null --mtime now /dev/null >/dev/null 2>&1; then
-        TAR=$try
-        break
-    fi
-done
-
-if [ "$TAR" = "notfound" ]; then
-    echo 'No suitable tar (supporting --mtime) found as tar/gtar/gnutar in PATH'
-    exit 1
-fi
-
-# Get the current directory, which we'll use for symlinks
-wd="$PWD"
-
 # Get the version
 VERSION=$(build_tools/git_version_gen.sh --stdout 2>/dev/null)
 
-# The name of the prefix, which is the directory that you get when you untar
-prefix="fish-$VERSION"
+prefix=fish-$VERSION
+path=${FISH_ARTEFACT_PATH:-~/fish_built}/$prefix.tar.xz
 
-# The path where we will output the tar file
-# Defaults to ~/fish_built
-path=${FISH_ARTEFACT_PATH:-~/fish_built}/$prefix.tar
-
-# Clean up stuff we've written before
-rm -f "$path" "$path".xz
-
-# git starts the archive
-git archive --format=tar --prefix="$prefix"/ HEAD > "$path"
-
-# tarball out the documentation, generate a version file
-PREFIX_TMPDIR=$(mktemp -d)
-cd "$PREFIX_TMPDIR"
-echo "$VERSION" > version
-
-$TAR --append --file=$path --mtime=now --owner=0 --group=0 \
-    --mode=g+w,a+rX --transform "s/^/$prefix\//" \
-    version
-
-cd -
-rm -r "$PREFIX_TMPDIR"
-
-# xz it
-xz "$path"
+git archive \
+    --prefix="$prefix/" \
+    --add-virtual-file="$prefix/version:$VERSION" \
+    HEAD |
+    xz >"$path"
 
 # Output what we did, and the sha256 hash
-echo "Tarball written to $path".xz
-openssl dgst -sha256 "$path".xz
+echo "Tarball written to $path"
+openssl dgst -sha256 "$path"

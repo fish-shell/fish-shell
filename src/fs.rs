c0f91a50fab73ba836bbfdc5f5d04dfce80acf48
Daniel Rainer <daniel.rainer@localhost>
2025-11-23 01:10:31 +0100

    fix: avoid race conditions of `mkstemp`
    The `mkstemp` function opens files without setting `O_CLOEXEC`. We could
    manually set this using `fnctl` once the file is opened, but that has
    the issue of introducing race conditions. If fish `exec`s in another
    thread before the `fnctl` call completes, the file would be left open.
    
    One way of mitigating this is `mkostemp`, but that function is not
    available on all systems fish supports, so we can't rely on it.
    
    Instead, build our own tempfile creation logic which uses the `rand`
    crate for getting entropy and relies on Rust's stdlib for the rest.
    The stdlib functions we use set `O_CLOEXEC` by default.
    
    For directory creation we keep using `mkdtemp`, since there we don't
    open anything. We could replace this by extending our custom logic a
    bit, which would allow us to drop the `nix` dependency for our
    `tempfile` crate, but since the code is simpler as it is now and we need
    nix in fish's main crate, there is no need to modify the directory
    creation code.
    
    Part of #12030

--- src/fs.rs
+++ src/fs.rs
@@ -1,6 +1,6 @@
 use crate::{
     FLOG, FLOGF,
-    common::{bytes2wcstring, wcs2osstring, wcs2zstring},
+    common::{bytes2wcstring, wcs2bytes, wcs2osstring},
     fds::wopen_cloexec,
     path::{DirRemoteness, path_remoteness},
     wchar::prelude::*,
@@ -8,63 +8,44 @@ use crate::{
         FileId, INVALID_FILE_ID, file_id_for_file, file_id_for_path, wdirname, wrename, wunlink,
     },
 };
-use errno::errno;
+use fish_tempfile::random_filename;
 use libc::{LOCK_EX, LOCK_SH, c_int};
 use nix::{fcntl::OFlag, sys::stat::Mode};
 use std::{
-    ffi::CString,
+    ffi::OsString,
     fs::{File, OpenOptions},
     os::{
-        fd::{AsRawFd, FromRawFd},
-        unix::fs::MetadataExt,
+        fd::AsRawFd,
+        unix::{ffi::OsStringExt, fs::MetadataExt},
     },
+    path::PathBuf,
 };
 
-// Replacement for mkostemp(str, O_CLOEXEC)
-// This uses mkostemp if available,
-// otherwise it uses mkstemp followed by fcntl
-fn fish_mkstemp_cloexec(name_template: CString) -> std::io::Result<(File, CString)> {
-    let name = name_template.into_raw();
-    #[cfg(not(apple))]
-    let fd = {
-        use libc::O_CLOEXEC;
-        unsafe { libc::mkostemp(name, O_CLOEXEC) }
-    };
-    #[cfg(apple)]
-    let fd = {
-        use libc::{F_SETFD, FD_CLOEXEC};
-        let fd = unsafe { libc::mkstemp(name) };
-        if fd != -1 {
-            unsafe { libc::fcntl(fd, F_SETFD, FD_CLOEXEC) };
+/// Creates a temporary file in the same directory as as `original_path`, meaning `original_path`
+/// must be a valid file path. The filename will be created by appending random alphanumeric ASCII
+/// chars to the `original_filename`.
+fn create_temporary_file(original_path: &wstr) -> std::io::Result<(File, WString)> {
+    let original_path = PathBuf::from(OsString::from_vec(wcs2bytes(original_path)));
+    // original path must be a valid file path, so file_name should never return None.
+    let prefix = original_path.file_name().unwrap().to_owned();
+    let dir = original_path.parent().unwrap();
+    let (path, result) =
+        fish_tempfile::create_file_with_retry(|| dir.join(random_filename(prefix.clone())));
+    match result {
+        Ok(file) => Ok((file, bytes2wcstring(path.as_os_str().as_encoded_bytes()))),
+        Err(e) => {
+            FLOG!(
+                error,
+                wgettext_fmt!(
+                    "Unable to create temporary file '%s': %s",
+                    // TODO(MSRV>=1.87): use OsString::display()
+                    format!("{:?}", path),
+                    e
+                )
+            );
+            Err(e)
         }
-        fd
-    };
-    if fd == -1 {
-        Err(std::io::Error::from(errno()))
-    } else {
-        unsafe { Ok((File::from_raw_fd(fd), CString::from_raw(name))) }
     }
-}
-
-/// Creates a temporary file created according to the template and its name if successful.
-pub fn create_temporary_file(name_template: &wstr) -> std::io::Result<(File, WString)> {
-    let (fd, c_string_template) = loop {
-        match fish_mkstemp_cloexec(wcs2zstring(name_template)) {
-            Ok(tmp_file_data) => break tmp_file_data,
-            Err(e) => match e.kind() {
-                std::io::ErrorKind::Interrupted => {}
-                _ => {
-                    FLOG!(
-                        error,
-                        wgettext_fmt!("Unable to create temporary file '%s': %s", name_template, e)
-                    );
-
-                    return Err(e);
-                }
-            },
-        }
-    };
-    Ok((fd, bytes2wcstring(c_string_template.to_bytes())))
 }
 
 /// Use this struct for all accesses to file which need mutual exclusion.
@@ -484,9 +465,7 @@ where
         ))
     }
 
-    const TMP_FILE_SUFFIX: &wstr = L!(".XXXXXX");
-    let tmp_file_template = path.to_owned() + TMP_FILE_SUFFIX;
-    let (tmp_file, tmp_name) = create_temporary_file(&tmp_file_template)?;
+    let (tmp_file, tmp_name) = create_temporary_file(path)?;
     let result = try_rewriting(path, rewrite, &tmp_name, tmp_file);
     // Do not leave the tmpfile around.
     // Note that we do not unlink when renaming succeeded.

655b1aa7a19291d150aa21fbd301197d1da39adf
Johannes Altmanninger <aclopte@gmail.com>
2025-11-28 10:28:51 +0100

    fish_prompt: remove stray initialization of fish_color_status
    I can't reproduce the problem mentioned in 0420901cb28 (default prompt:
    Set fish_color_status if unset, 2021-04-11).

--- share/functions/fish_prompt.fish
+++ share/functions/fish_prompt.fish
@@ -5,8 +5,6 @@ function fish_prompt --description 'Write out the prompt'
     set -l last_pipestatus $pipestatus
     set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
     set -l normal (set_color normal)
-    set -q fish_color_status
-    or set -g fish_color_status red
 
     # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd

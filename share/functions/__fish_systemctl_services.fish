5d37698ef83bdc37c6815d163d05f08e0411e919
チセ <123655015+chise0713@users.noreply.github.com>
2025-12-13 19:25:57 +0800

    fix: `__fish_systemctl_services` mode handling
    Closes #12157

--- share/functions/__fish_systemctl_services.fish
+++ share/functions/__fish_systemctl_services.fish
@@ -7,14 +7,30 @@ function __fish_systemctl_services
     # We don't want to complete with ANSI color codes
     set -lu SYSTEMD_COLORS
 
+    # use the user specified mode, also exclude user defined alias
+    set -l tokens (commandline -xp)
+    # for determining whether user is using `journalctl`
+    set -l command $tokens[1]
+    set -l mode
+    for t in $tokens[2..]
+        switch $t
+            case --system --user
+                set mode $t
+            case --global
+                # `journalctl` does not have `global` option
+                if test "$command" != journalctl
+                    set mode $t
+                end
+            case --
+                break
+        end
+    end
+
     set -l common_args --full --no-legend --no-pager --plain --type=service
-    if __fish_contains_opt user
-        systemctl --user list-unit-files $common_args
-        systemctl --user list-units --state=loaded $common_args
-    else
-        # list-unit-files will also show disabled units
-        systemctl list-unit-files $common_args
-        # list-units will not show disabled units but will show instances (like wpa_supplicant@wlan0.service)
-        systemctl list-units --state=loaded $common_args
+    begin
+        # `list-unit-files` will also show disabled units
+        systemctl $mode list-unit-files $common_args
+        # `list-units`      will not show disabled units but will show instances (like wpa_supplicant@wlan0.service)
+        systemctl $mode list-units --state=loaded $common_args
     end 2>/dev/null | string split -f 1 ' '
 end

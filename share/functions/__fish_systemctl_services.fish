0306ec673f2a0db152ad17e4244ee06e647812ca
Johannes Altmanninger <aclopte@gmail.com>
2025-12-15 22:11:09 +0100

    __fish_systemctl_services: remove unused argument

--- share/functions/__fish_systemctl_services.fish
+++ share/functions/__fish_systemctl_services.fish
@@ -5,11 +5,11 @@ function __fish_systemctl_services
 
     if type -q systemctl
         if __fish_contains_opt user
-            systemctl --user list-unit-files --full --no-legend --no-pager --plain --type=service 2>/dev/null $argv | string split -f 1 ' '
+            systemctl --user list-unit-files --full --no-legend --no-pager --plain --type=service 2>/dev/null | string split -f 1 ' '
             systemctl --user list-units --state=loaded --full --no-legend --no-pager --plain --type=service 2>/dev/null | string split -f 1 ' '
         else
             # list-unit-files will also show disabled units
-            systemctl list-unit-files --full --no-legend --no-pager --plain --type=service 2>/dev/null $argv | string split -f 1 ' '
+            systemctl list-unit-files --full --no-legend --no-pager --plain --type=service 2>/dev/null | string split -f 1 ' '
             # list-units will not show disabled units but will show instances (like wpa_supplicant@wlan0.service)
             systemctl list-units --state=loaded --full --no-legend --no-pager --plain --type=service 2>/dev/null | string split -f 1 ' '
         end

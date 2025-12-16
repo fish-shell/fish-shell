d88c0674a3efe16985ec86e1b9d2a82f5f6589cc
Johannes Altmanninger <aclopte@gmail.com>
2025-12-16 05:52:40 +0100

    __fish_systemctl_services: early return

--- share/functions/__fish_systemctl_services.fish
+++ share/functions/__fish_systemctl_services.fish
@@ -1,17 +1,19 @@
 # localization: skip(private)
 function __fish_systemctl_services
+    if not type -q systemctl
+        return
+    end
+
     # We don't want to complete with ANSI color codes
     set -lu SYSTEMD_COLORS
 
-    if type -q systemctl
-        if __fish_contains_opt user
-            systemctl --user list-unit-files --full --no-legend --no-pager --plain --type=service 2>/dev/null | string split -f 1 ' '
-            systemctl --user list-units --state=loaded --full --no-legend --no-pager --plain --type=service 2>/dev/null | string split -f 1 ' '
-        else
-            # list-unit-files will also show disabled units
-            systemctl list-unit-files --full --no-legend --no-pager --plain --type=service 2>/dev/null | string split -f 1 ' '
-            # list-units will not show disabled units but will show instances (like wpa_supplicant@wlan0.service)
-            systemctl list-units --state=loaded --full --no-legend --no-pager --plain --type=service 2>/dev/null | string split -f 1 ' '
-        end
+    if __fish_contains_opt user
+        systemctl --user list-unit-files --full --no-legend --no-pager --plain --type=service 2>/dev/null | string split -f 1 ' '
+        systemctl --user list-units --state=loaded --full --no-legend --no-pager --plain --type=service 2>/dev/null | string split -f 1 ' '
+    else
+        # list-unit-files will also show disabled units
+        systemctl list-unit-files --full --no-legend --no-pager --plain --type=service 2>/dev/null | string split -f 1 ' '
+        # list-units will not show disabled units but will show instances (like wpa_supplicant@wlan0.service)
+        systemctl list-units --state=loaded --full --no-legend --no-pager --plain --type=service 2>/dev/null | string split -f 1 ' '
     end
 end

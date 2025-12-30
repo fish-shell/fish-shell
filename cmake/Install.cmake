fish-shell/fish-shell

commit d8f1a2a24f51010d2ee870395044fd1900689257

Move sample_prompts/themes to share/
They are used by "fish_config" CLI too, so no need to make them
private to webconfig.  Putting them at top-level seems simpler overall.

cmake/Install.cmake
@@ -95,8 +95,8 @@ fish_create_dirs(${rel_datadir}/fish ${rel_datadir}/fish/completions
                  ${rel_datadir}/fish/man/man1 ${rel_datadir}/fish/tools
                  ${rel_datadir}/fish/tools/web_config
                  ${rel_datadir}/fish/tools/web_config/js
-                 ${rel_datadir}/fish/tools/web_config/sample_prompts
-                 ${rel_datadir}/fish/tools/web_config/themes
+                 ${rel_datadir}/fish/prompts
+                 ${rel_datadir}/fish/themes
                  )
 
 configure_file(share/__fish_build_paths.fish.in share/__fish_build_paths.fish)

contrib/debian/copyright
@@ -17,11 +17,11 @@ Files: share/tools/web_config/js/alpine.js
 Copyright: 2019-2021 Caleb Porzio and contributors
 License: MIT
 
-Files: share/tools/web_config/themes/Dracula.theme
+Files: share/themes/Dracula.theme
 Copyright: 2018 Dracula Team
 License: MIT
 
-Files: share/tools/web_config/themes/Nord.theme
+Files: share/themes/Nord.theme
 Copyright: 2016-2024 Sven Greb
 License: MIT
 

share/functions/__fish_theme_cat.fish
@@ -3,7 +3,7 @@ function __fish_theme_cat -a theme_name
     set -l theme_path (__fish_theme_paths $theme_name)[1]
     if not set -q theme_path[1]
         echo >&2 "No such theme: $theme_name"
-        echo >&2 Searched (__fish_theme_dir) "and `status list-files tools/web_config/themes`"
+        echo >&2 Searched (__fish_theme_dir) "and `status list-files themes`"
         return 1
     end
     __fish_data_with_file $theme_path cat

share/functions/__fish_theme_paths.fish
@@ -1,5 +1,5 @@
 # localization: skip(private)
 function __fish_theme_paths
     __fish_config_files --user-dir=(__fish_theme_dir) \
-        tools/web_config/themes .theme $argv
+        themes .theme $argv
 end

share/functions/fish_config.fish
@@ -203,7 +203,7 @@ end
 
 function __fish_config_list_prompts
     set -lx dir
-    set -l prompt_paths (__fish_config_files tools/web_config/sample_prompts .fish $argv)
+    set -l prompt_paths (__fish_config_files prompts .fish $argv)
     if [ (count $argv) = 1 ] && set -q prompt_paths[2]
         echo >&2 "fish_config: internal error: multiple prompts matching '$argv' ??"
         set --erase prompt_paths[2..]

commit d4aa3bc71d66e42b443b0390c6f4e7acd43cd859

Fix installation of prompts and theme files after 4.3.0
Installation of these files was accidentally broken by d8f1a2a.

Fixes #12241

cmake/Install.cmake
@@ -131,6 +131,14 @@ install(DIRECTORY share/functions/
         DESTINATION ${rel_datadir}/fish/functions
         FILES_MATCHING PATTERN "*.fish")
 
+install(DIRECTORY share/prompts/
+        DESTINATION ${rel_datadir}/fish/prompts
+        FILES_MATCHING PATTERN "*.fish")
+
+install(DIRECTORY share/themes/
+        DESTINATION ${rel_datadir}/fish/themes
+        FILES_MATCHING PATTERN "*.theme")
+
 # CONDEMNED_PAGE is managed by the conditional above
 # Building the man pages is optional: if sphinx isn't installed, they're not built
 install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/user_doc/man/man1/
@@ -149,9 +157,7 @@ install(DIRECTORY share/tools/web_config
         PATTERN "*.css"
         PATTERN "*.html"
         PATTERN "*.py"
-        PATTERN "*.js"
-        PATTERN "*.theme"
-        PATTERN "*.fish")
+        PATTERN "*.js")
 
 # Building the man pages is optional: if Sphinx isn't installed, they're not built
 install(FILES ${MANUALS} DESTINATION ${mandir}/man1/ OPTIONAL)
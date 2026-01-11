564ef66665e52e1b331b7470db80206bf6566f8f
Daniel Rainer <daniel.rainer@localhost>
2026-01-11 19:02:56 +0100

    git_version_gen: remove obsolete code
    The `git_version_gen.sh` script is no longer used to write any files, so
    remove the logic for it.
    
    This code included handling for permission problems when running `git
    describe`. It was introduced by
    15f1b5f36873960d1feede7d3f545f326b0991f6, but unfortunately without
    mentioning which CVE caused the change in git. I found CVE-2022-24765,
    which was published not long before the commit was made, and its
    description looks like it's fitting. On a recent version of git
    (2.52.0), I had no problems running `make && sudo make install`, so it
    seems like this issue might no longer be relevant. One suboptimal thing
    to note is that `sudo make install` currently builds `fish_indent` even
    if it was built via `make` before, which is not great, but unrelated to
    these changes.
    
    Part of #12307

--- build_tools/git_version_gen.sh
+++ build_tools/git_version_gen.sh
@@ -9,62 +9,21 @@ set -e
 # Find the fish directory as two levels up from script directory.
 FISH_BASE_DIR="$( cd "$( dirname "$( dirname "$0" )" )" && pwd )"
 DEF_VER=unknown
-git_permission_failed=0
 
 # First see if there is a version file (included in release tarballs),
 # then try git-describe, then default.
 if test -f "$FISH_BASE_DIR"/version
 then
-	VN=$(cat "$FISH_BASE_DIR"/version) || VN="$DEF_VER"
+    VN=$(cat "$FISH_BASE_DIR"/version) || VN="$DEF_VER"
+elif VN=$(git -C "$FISH_BASE_DIR" describe --always --dirty 2>/dev/null); then
+   :
 else
-    if VN=$(git -C "$FISH_BASE_DIR" describe --always --dirty 2>/dev/null); then
-       :
-    else
-        if test $? = 128; then
-            # Current git versions return status 128
-            # when run in a repo owned by another user.
-            # Even for describe and everything.
-            # This occurs for `sudo make install`.
-            git_permission_failed=1
-        fi
-	    VN="$DEF_VER"
-    fi
+    VN="$DEF_VER"
 fi
 
 # If the first param is --stdout, then output to stdout and exit.
 if test "$1" = '--stdout'
 then
-	echo $VN
+    echo "$VN"
     exit 0
 fi
-
-# Set the output directory as either the first param or cwd.
-test -n "$1" && OUTPUT_DIR=$1/ || OUTPUT_DIR=
-FBVF="${OUTPUT_DIR}FISH-BUILD-VERSION-FILE"
-
-if test "$VN" = unknown && test -r "$FBVF" && test "$git_permission_failed" = 1
-then
-    # HACK: Git failed, so we keep the current version file.
-    # This helps in case you built fish as a normal user
-    # and then try to `sudo make install` it.
-    date +%s > "${OUTPUT_DIR}"fish-build-version-witness.txt
-    exit 0
-fi
-
-if test -r "$FBVF"
-then
-	VC=$(cat "$FBVF")
-else
-	VC="unset"
-fi
-
-# Maybe output the FBVF
-# It looks like "2.7.1-621-ga2f065e6"
-test "$VN" = "$VC" || {
-	echo >&2 "$VN"
-	echo "$VN" >"$FBVF"
-}
-
-# Output the fish-build-version-witness.txt
-# See https://cmake.org/cmake/help/v3.4/policy/CMP0058.html
-date +%s > "${OUTPUT_DIR}"fish-build-version-witness.txt

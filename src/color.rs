b14692cb95f52bb365ac442f83e1da87dd1cb5ec
Daniel Rainer <daniel.rainer@localhost>
2026-01-12 02:19:42 +0100

    color: be explicit about widestring usage
    This eliminates the dependency on the main library crate, preparing
    `src/color.rs` for extraction into a dedicated crate.
    
    Part of #12311

--- src/color.rs
+++ src/color.rs
@@ -1,7 +1,7 @@
 use std::cmp::Ordering;
 
-use crate::prelude::*;
 use fish_common::assert_sorted_by_name;
+use fish_widestring::{L, wstr};
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct Color24 {

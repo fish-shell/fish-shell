b363e4afe714a764211dbcc0cd1fa7a6a6f56af6
Johannes Altmanninger <aclopte@gmail.com>
2026-01-12 13:04:19 +0100

    tests/checks/version: support shallow clones

--- tests/checks/version.fish
+++ tests/checks/version.fish
@@ -4,13 +4,12 @@
 # 1.2.3
 # Git version:
 # 1.2.3-42-gdeadbeef
-# Git version when no tags are present. Distros should probably not package it like this,
-# that's why this test currently fails for such versions.
+# Git version when no tags are present. Distros should probably not package it like this.
 # 1.2.3-gdeadbeef
 
 $fish -v
-# CHECK: fish, version {{\d+\.\d+\.\d+(-\d+-g[0-9a-f]{7,}(-dirty)?)?$}}
+# CHECK: fish, version {{\d+\.\d+\.\d+(-\d+-g[0-9a-f]{7,}(-dirty)?|-g[0-9a-f]{7,})?$}}
 
 set -l workspace_root (status dirname)/../..
 $workspace_root/build_tools/git_version_gen.sh
-# CHECK: {{\d+\.\d+\.\d+(-\d+-g[0-9a-f]{7,}(-dirty)?)?$}}
+# CHECK: {{\d+\.\d+\.\d+(-\d+-g[0-9a-f]{7,}(-dirty)?|-g[0-9a-f]{7,})?$}}

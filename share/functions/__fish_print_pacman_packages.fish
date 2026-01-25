da692aaa5fbaad1c419ca4ab4c2c675eb5e1391f
Tiago de Paula <tiagodepalves@gmail.com>
2026-01-25 01:25:58 -0300

    completion/pacman: completions for `-S $repo/$package`

--- share/functions/__fish_print_pacman_packages.fish
+++ share/functions/__fish_print_pacman_packages.fish
@@ -3,14 +3,17 @@ function __fish_print_pacman_packages
     # Caches for 5 minutes
     type -q -f pacman || return 1
 
-    argparse i/installed -- $argv
+    argparse i/installed r/repo=+ -- $argv
     or return 1
 
-    if not set -q _flag_installed
-        __fish_cached -t 250 -- "pacman -Ssq | sed -e 's/\$/\\tPackage/'"
+    if set -q _flag_installed
+        pacman -Q | string replace ' ' \t
+        return 0
+    else if test (count $_flag_repo) -gt 0
+        pacman -Sl $_flag_repo | string replace --regex '^(\S+) (\S+) .*' '$1/$2\tPackage'
         return 0
     else
-        pacman -Q | string replace ' ' \t
+        __fish_cached -t 250 -- "pacman -Ssq | sed -e 's/\$/\\tPackage/'"
         return 0
     end
 end

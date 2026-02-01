fish-shell/fish-shell

commit 081c469f6f111e7392b18f47de584707316379dd

tarball: include .cargo/config.toml again
The problem worked around by commit e4674cd7b5f (.cargo/config.toml:
exclude from tarball, 2025-01-12) is as follows:
our OBS packages are built by doing something like

	tar xf $tarball && tar xf $vendor_tarball

except that the tool apparently break when a file is present in
both tarballs.

Our workaround is to not include .cargo/config.toml in the tarball.

The workaround seems too broad. It need not affect all tarballs
but only the ones used by OBS. We want to add xtask aliases to
.cargo/config.toml. They will be used by CMake targets (sphinx-doc),
so they should be available to tarball consumers.

Restrict the scope of the workaround: add back .cargo/config.toml
to the export, and allow opting in to this behavioer by passing a
negative pathspec

	build_tools/make_tarball.sh :/!.cargo/config.toml

build_tools/make_tarball.sh
@@ -3,6 +3,10 @@
 # Script to generate a tarball
 # Outputs to $FISH_ARTEFACT_PATH or ~/fish_built by default
 
+# Example usage:
+#
+#   build_tools/make_tarball.sh :/!.cargo/config.toml
+
 # Exit on error
 set -e
 
@@ -14,7 +18,7 @@ path=${FISH_ARTEFACT_PATH:-~/fish_built}/$prefix.tar.xz
 
 git archive \
     --prefix="$prefix/" \
-    HEAD |
+    HEAD "$@" |
     xz >"$path"
 
 # Output what we did, and the sha256 hash

.gitattributes
@@ -18,9 +18,6 @@
 /.github/* export-ignore
 /.builds export-ignore
 /.builds/* export-ignore
-# to make cargo vendor work correctly
-/.cargo/ export-ignore
-/.cargo/config.toml export-ignore
 
 # for linguist, which drives GitHub's language statistics
 alpine.js linguist-vendored

build_tools/make_vendor_tarball.sh
@@ -42,8 +42,14 @@ rm -f "$path" "$path".xz
 PREFIX_TMPDIR=$(mktemp -d)
 cd "$PREFIX_TMPDIR"
 
+# Add .cargo/config.toml. This means that the caller may need to remove that file from the tarball.
+# See e4674cd7b5f (.cargo/config.toml: exclude from tarball, 2025-01-12)
+
 mkdir .cargo
-cargo vendor --manifest-path "$wd/Cargo.toml" >> .cargo/config.toml
+{
+    cat "$wd"/.cargo/config.toml
+    cargo vendor --manifest-path "$wd/Cargo.toml"
+} > .cargo/config.toml
 
 tar cfvJ "$path".xz vendor .cargo
 

commit 73e3b47ebde706ef66b14ae73c6d8381d50f68e7

tarball: capture Git version
Commit 92dae88f620 (tarball: remove redundant "version" file,
2026-01-12) committed to using Cargo.toml as single source of truth
for our version string.

This works fine for tarballs built from a tag, but those built from
an arbitrary Git commit will show a misleading version ("4.3.3"
instead of something like "4.3.3-190-g460081725b5-dirty").

Fix this by copying the Git version to the tarball's Cargo.{toml,lock}.

It's not clear if we really need this (it's only for nightly builds)
so we might end up reverting this.

Ref: https://matrix.to/#/!YLTeaulxSDauOOxBoR:matrix.org/$BdRagDGCV-8yVjBs0i3QyWUdBK820vTmjuSBqgpsuJY

Note that OBS builds are probably still broken from 081c469f6f1
(tarball: include .cargo/config.toml again, 2026-01-13); see
https://github.com/fish-shell/fish-shell/pull/12292#discussion_r2694000477

build_tools/make_tarball.sh
@@ -16,10 +16,39 @@ VERSION=$(build_tools/git_version_gen.sh)
 prefix=fish-$VERSION
 path=${FISH_ARTEFACT_PATH:-~/fish_built}/$prefix.tar.xz
 
+tmpdir=$(mktemp -d)
+manifest=$tmpdir/Cargo.toml
+lockfile=$tmpdir/Cargo.lock
+
+sed "s/^version = \".*\"\$/version = \"$VERSION\"/g" Cargo.toml \
+ >"$manifest"
+awk -v version=$VERSION '
+    /^name = "fish"$/ { ok=1 }
+    ok == 1 && /^version = ".*"$/ {
+        ok = 2;
+        $0 = "version = \"" version "\"";
+    }
+    {print}
+	' \
+    Cargo.lock >"$lockfile"
+
+for ext in toml lock; do
+    if cmp Cargo.$ext "$tmpdir/Cargo.$ext" >/dev/null; then
+        echo >&2 "failed to update Cargo.$ext version?"
+        exit 1
+    fi
+done
+
 git archive \
     --prefix="$prefix/" \
+    --add-virtual-file="$prefix/Cargo.toml:$(cat "$manifest")" \
+    --add-virtual-file="$prefix/Cargo.lock:$(cat "$lockfile")" \
     HEAD "$@" |
     xz >"$path"
+
+rm "$manifest"
+rm "$lockfile"
+rmdir "$tmpdir"
 
 # Output what we did, and the sha256 hash
 echo "Tarball written to $path"
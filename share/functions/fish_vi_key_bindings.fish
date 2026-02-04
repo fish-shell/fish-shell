fish-shell/fish-shell

commit bbb2f0de8d58610e7f9d4f1b00903562753ec199

feat(vi-mode): make word movements vi-compliant
- The behavior of `{,d}{w,W}`, `{,d}{,g}{e,E}` bindings in vi-mode is
  now more compatible with vim, except that the underscore is not a
  keyword (which can be achieved by setting `set iskeyword-=_` in vim).

- Add commands `{forward,kill}-{word,bigword}-vi`,
  `{forward,backward,kill,backward-kill}-{word,bigword}-end` and
  `kill-{a,inner}-{word,bigword}` corresponding to above-mentioned
  bindings.

- Closes #10393.

Closes #12269

Co-authored-by: Johannes Altmanninger <aclopte@gmail.com>

share/functions/fish_vi_key_bindings.fish
@@ -14,7 +14,6 @@ function fish_vi_run_count --description 'Run a command $__fish_vi_count times'
     set -l count (__fish_vi_consume_count __fish_vi_count)
 
     for i in (seq $count)
-        # Check if the first argument is a defined shell function (like fish_vi_move_end_word)
         if functions -q -- $argv[1]
             $argv
         else
@@ -72,49 +71,74 @@ function fish_vi_exec_motion
                     commandline -f kill-whole-line yank
                 end
             case swap-case
-                for i in (seq $total)
-                    commandline -f beginning-of-line begin-selection down-line togglecase-selection end-selection
+                # Not implemented yet
+                return
+        end
+    else
+        set -l use_selection true
+        set -l swap_case_hack
+        switch $motion
+            case forward-word-vi forward-bigword-vi
+                if test $__fish_vi_operator = swap-case
+                    set swap_case_hack (string replace -r -- '^forward-((?:big)?word)-vi$' '$1' $motion)
+                else
+                    set use_selection false
+                    set motion (string replace -- forward kill $motion)
                 end
         end
-        commandline -f repaint-mode
-    else
+        if $use_selection
+            commandline -f begin-selection
+        else
+            commandline -f begin-undo-group
+        end
         switch $__fish_vi_operator
             case delete
-                commandline -f begin-selection
                 for i in (seq $total)
                     commandline -f $motion
                 end
-                commandline -f kill-selection end-selection repaint-mode
+                if $use_selection
+                    commandline -f kill-selection
+                end
             case change
-                commandline -f begin-selection
                 for i in (seq $total)
                     commandline -f $motion
                 end
-                commandline -f kill-selection end-selection repaint-mode
-                # Switch to insert mode
+                if $use_selection
+                    commandline -f kill-selection
+                end
                 set fish_bind_mode insert
             case yank
-                commandline -f begin-selection
                 for i in (seq $total)
                     commandline -f $motion
                 end
-                commandline -f kill-selection yank end-selection repaint-mode
+                if $use_selection
+                    commandline -f kill-selection
+                end
+                commandline -f yank
             case swap-case
-                commandline -f begin-selection
                 for i in (seq $total)
                     commandline -f $motion
                 end
-                commandline -f togglecase-selection end-selection repaint-mode
+                if set -q swap_case_hack[1]
+                    set -l word $swap_case_hack
+                    commandline -f \
+                        backward-$word \
+                        forward-$word-end \
+                        togglecase-selection \
+                        backward-$word \
+                        forward-$word-vi
+                else
+                    commandline -f togglecase-selection
+                end
+        end
+        if $use_selection
+            commandline -f end-selection
+        else
+            commandline -f end-undo-group
         end
     end
+    commandline -f repaint-mode
     set -g __fish_vi_operator
-end
-
-function fish_vi_move_end_word
-    set -l cmd $argv[1]
-    set fish_cursor_end_mode exclusive
-    commandline -f forward-single-char $cmd backward-char
-    set fish_cursor_end_mode inclusive
 end
 
 # TODO: Currently we do not support hexadecimal and octal values.
@@ -276,14 +300,14 @@ function fish_vi_key_bindings --description 'vi-like key bindings for fish'
 
     bind -s --preset -M default b 'fish_vi_run_count backward-word'
     bind -s --preset -M default B 'fish_vi_run_count backward-bigword'
-    bind -s --preset -M default g,e 'fish_vi_run_count backward-word'
-    bind -s --preset -M default g,E 'fish_vi_run_count backward-bigword'
+    bind -s --preset -M default g,e 'fish_vi_run_count backward-word-end'
+    bind -s --preset -M default g,E 'fish_vi_run_count backward-bigword-end'
 
-    bind -s --preset -M default w 'fish_vi_run_count forward-word forward-single-char'
-    bind -s --preset -M default W 'fish_vi_run_count forward-bigword forward-single-char'
+    bind -s --preset -M default w 'fish_vi_run_count forward-word-vi'
+    bind -s --preset -M default W 'fish_vi_run_count forward-bigword-vi'
 
-    bind -s --preset -M default e 'fish_vi_run_count fish_vi_move_end_word forward-word'
-    bind -s --preset -M default E 'fish_vi_run_count fish_vi_move_end_word forward-bigword'
+    bind -s --preset -M default e 'fish_vi_run_count forward-word-end'
+    bind -s --preset -M default E 'fish_vi_run_count forward-bigword-end'
 
     bind -s --preset -M default x 'fish_vi_run_count delete-char'
     bind -s --preset -M default X 'fish_vi_run_count backward-delete-char'
@@ -316,6 +340,9 @@ function fish_vi_key_bindings --description 'vi-like key bindings for fish'
     bind -s --preset [ history-token-search-backward
     bind -s --preset ] history-token-search-forward
     bind -s --preset -m insert / history-pager repaint-mode
+
+    __fish_per_os_bind --preset $argv ctrl-right forward-token forward-word-vi
+    # ctrl-left is same as emacs mode
 
     bind -s --preset -M insert ctrl-n accept-autosuggestion
 
@@ -355,12 +382,12 @@ function fish_vi_key_bindings --description 'vi-like key bindings for fish'
     bind -s --preset -M operator j 'fish_vi_exec_motion down-line'
     bind -s --preset -M operator b 'fish_vi_exec_motion backward-word'
     bind -s --preset -M operator B 'fish_vi_exec_motion backward-bigword'
-    bind -s --preset -M operator g,e 'fish_vi_exec_motion backward-word'
-    bind -s --preset -M operator g,E 'fish_vi_exec_motion backward-bigword'
-    bind -s --preset -M operator w 'fish_vi_exec_motion forward-word'
-    bind -s --preset -M operator W 'fish_vi_exec_motion forward-bigword'
-    bind -s --preset -M operator e 'fish_vi_exec_motion forward-single-char forward-word backward-char'
-    bind -s --preset -M operator E 'fish_vi_exec_motion forward-single-char forward-bigword backward-char'
+    bind -s --preset -M operator g,e 'fish_vi_exec_motion backward-word-end'
+    bind -s --preset -M operator g,E 'fish_vi_exec_motion backward-bigword-end'
+    bind -s --preset -M operator w 'fish_vi_exec_motion forward-word-vi'
+    bind -s --preset -M operator W 'fish_vi_exec_motion forward-bigword-vi'
+    bind -s --preset -M operator e 'fish_vi_exec_motion forward-word-end'
+    bind -s --preset -M operator E 'fish_vi_exec_motion forward-bigword-end'
 
     bind -s --preset -M operator 0 'fish_vi_exec_motion beginning-of-line'
     bind -s --preset -M operator \^ 'fish_vi_exec_motion beginning-of-line'
@@ -383,10 +410,10 @@ function fish_vi_key_bindings --description 'vi-like key bindings for fish'
     bind -s --preset d,\^ backward-kill-line
     bind -s --preset d,0 backward-kill-line
 
-    bind -s --preset d,i,w forward-single-char forward-single-char backward-word kill-word
-    bind -s --preset d,i,W forward-single-char forward-single-char backward-bigword kill-bigword
-    bind -s --preset d,a,w forward-single-char forward-single-char backward-word kill-word
-    bind -s --preset d,a,W forward-single-char forward-single-char backward-bigword kill-bigword
+    bind -s --preset d,i,w kill-inner-word
+    bind -s --preset d,i,W kill-inner-bigword
+    bind -s --preset d,a,w kill-a-word
+    bind -s --preset d,a,W kill-a-bigword
     bind -s --preset d,i,b jump-till-matching-bracket and jump-till-matching-bracket and begin-selection jump-till-matching-bracket kill-selection end-selection
     bind -s --preset d,a,b jump-to-matching-bracket and jump-to-matching-bracket and begin-selection jump-to-matching-bracket kill-selection end-selection
     bind -s --preset d,i backward-jump-till and repeat-jump-reverse and begin-selection repeat-jump kill-selection end-selection
@@ -401,10 +428,10 @@ function fish_vi_key_bindings --description 'vi-like key bindings for fish'
     bind -s --preset -m insert c,\^ backward-kill-line repaint-mode
     bind -s --preset -m insert c,0 backward-kill-line repaint-mode
 
-    bind -s --preset -m insert c,i,w forward-single-char forward-single-char backward-word kill-word repaint-mode
-    bind -s --preset -m insert c,i,W forward-single-char forward-single-char backward-bigword kill-bigword repaint-mode
-    bind -s --preset -m insert c,a,w forward-single-char forward-single-char backward-word kill-word repaint-mode
-    bind -s --preset -m insert c,a,W forward-single-char forward-single-char backward-bigword kill-bigword repaint-mode
+    bind -s --preset -m insert c,i,w kill-inner-word repaint-mode
+    bind -s --preset -m insert c,i,W kill-inner-bigword repaint-mode
+    bind -s --preset -m insert c,a,w kill-a-word repaint-mode
+    bind -s --preset -m insert c,a,W kill-a-bigword repaint-mode
     bind -s --preset -m insert c,i,b jump-till-matching-bracket and jump-till-matching-bracket and begin-selection jump-till-matching-bracket kill-selection end-selection
     bind -s --preset -m insert c,a,b jump-to-matching-bracket and jump-to-matching-bracket and begin-selection jump-to-matching-bracket kill-selection end-selection
     bind -s --preset -m insert c,i backward-jump-till and repeat-jump-reverse and begin-selection repeat-jump kill-selection end-selection repaint-mode
@@ -425,11 +452,10 @@ function fish_vi_key_bindings --description 'vi-like key bindings for fish'
     bind -s --preset y,\$ kill-line yank
     bind -s --preset y,\^ backward-kill-line yank
     bind -s --preset y,0 backward-kill-line yank
-
-    bind -s --preset y,i,w forward-single-char forward-single-char backward-word kill-word yank
-    bind -s --preset y,i,W forward-single-char forward-single-char backward-bigword kill-bigword yank
-    bind -s --preset y,a,w forward-single-char forward-single-char backward-word kill-word yank
-    bind -s --preset y,a,W forward-single-char forward-single-char backward-bigword kill-bigword yank
+    bind -s --preset y,i,w kill-inner-word yank
+    bind -s --preset y,i,W kill-inner-bigword yank
+    bind -s --preset y,a,w kill-a-word yank
+    bind -s --preset y,a,W kill-a-bigword yank
     bind -s --preset y,i,b jump-till-matching-bracket and jump-till-matching-bracket and begin-selection jump-till-matching-bracket kill-selection yank end-selection
     bind -s --preset y,a,b jump-to-matching-bracket and jump-to-matching-bracket and begin-selection jump-to-matching-bracket kill-selection yank end-selection
     bind -s --preset y,i backward-jump-till and repeat-jump-reverse and begin-selection repeat-jump kill-selection yank end-selection
@@ -500,12 +526,12 @@ function fish_vi_key_bindings --description 'vi-like key bindings for fish'
 
     bind -s --preset -M visual b backward-word
     bind -s --preset -M visual B backward-bigword
-    bind -s --preset -M visual g,e backward-word
-    bind -s --preset -M visual g,E backward-bigword
-    bind -s --preset -M visual w forward-word
-    bind -s --preset -M visual W forward-bigword
-    bind -s --preset -M visual e 'set fish_cursor_end_mode exclusive' forward-single-char forward-word backward-char 'set fish_cursor_end_mode inclusive'
-    bind -s --preset -M visual E 'set fish_cursor_end_mode exclusive' forward-single-char forward-bigword backward-char 'set fish_cursor_end_mode inclusive'
+    bind -s --preset -M visual g,e backward-word-end
+    bind -s --preset -M visual g,E backward-bigword-end
+    bind -s --preset -M visual w forward-word-vi
+    bind -s --preset -M visual W forward-bigword-vi
+    bind -s --preset -M visual e forward-word-end
+    bind -s --preset -M visual E forward-bigword-end
     bind -s --preset -M visual o swap-selection-start-stop repaint-mode
 
     bind -s --preset -M visual % jump-to-matching-bracket

src/input.rs
@@ -107,19 +107,23 @@ const INPUT_FUNCTION_METADATA: &[InputFunctionMetadata] = &[
     make_md(L!("accept-autosuggestion"), ReadlineCmd::AcceptAutosuggestion),
     make_md(L!("and"), ReadlineCmd::FuncAnd),
     make_md(L!("backward-bigword"), ReadlineCmd::BackwardBigword),
+    make_md(L!("backward-bigword-end"), ReadlineCmd::BackwardBigwordEnd),
     make_md(L!("backward-char"), ReadlineCmd::BackwardChar),
     make_md(L!("backward-char-passive"), ReadlineCmd::BackwardCharPassive),
     make_md(L!("backward-delete-char"), ReadlineCmd::BackwardDeleteChar),
     make_md(L!("backward-jump"), ReadlineCmd::BackwardJump),
     make_md(L!("backward-jump-till"), ReadlineCmd::BackwardJumpTill),
     make_md(L!("backward-kill-bigword"), ReadlineCmd::BackwardKillBigword),
+    make_md(L!("backward-kill-bigword-end"), ReadlineCmd::BackwardKillBigwordEnd),
     make_md(L!("backward-kill-line"), ReadlineCmd::BackwardKillLine),
     make_md(L!("backward-kill-path-component"), ReadlineCmd::BackwardKillPathComponent),
     make_md(L!("backward-kill-token"), ReadlineCmd::BackwardKillToken),
     make_md(L!("backward-kill-word"), ReadlineCmd::BackwardKillWord),
+    make_md(L!("backward-kill-word-end"), ReadlineCmd::BackwardKillWordEnd),
     make_md(L!("backward-path-component"), ReadlineCmd::BackwardPathComponent),
     make_md(L!("backward-token"), ReadlineCmd::BackwardToken),
     make_md(L!("backward-word"), ReadlineCmd::BackwardWord),
+    make_md(L!("backward-word-end"), ReadlineCmd::BackwardWordEnd),
     make_md(L!("begin-selection"), ReadlineCmd::BeginSelection),
     make_md(L!("begin-undo-group"), ReadlineCmd::BeginUndoGroup),
     make_md(L!("beginning-of-buffer"), ReadlineCmd::BeginningOfBuffer),
@@ -146,7 +150,9 @@ const INPUT_FUNCTION_METADATA: &[InputFunctionMetadata] = &[
     make_md(L!("exit"), ReadlineCmd::Exit),
     make_md(L!("expand-abbr"), ReadlineCmd::ExpandAbbr),
     make_md(L!("force-repaint"), ReadlineCmd::ForceRepaint),
-    make_md(L!("forward-bigword"), ReadlineCmd::ForwardBigword),
+    make_md(L!("forward-bigword"), ReadlineCmd::ForwardBigwordEmacs),
+    make_md(L!("forward-bigword-end"), ReadlineCmd::ForwardBigwordEnd),
+    make_md(L!("forward-bigword-vi"), ReadlineCmd::ForwardBigwordVi),
     make_md(L!("forward-char"), ReadlineCmd::ForwardChar),
     make_md(L!("forward-char-passive"), ReadlineCmd::ForwardCharPassive),
     make_md(L!("forward-jump"), ReadlineCmd::ForwardJump),
@@ -154,7 +160,9 @@ const INPUT_FUNCTION_METADATA: &[InputFunctionMetadata] = &[
     make_md(L!("forward-path-component"), ReadlineCmd::ForwardPathComponent),
     make_md(L!("forward-single-char"), ReadlineCmd::ForwardSingleChar),
     make_md(L!("forward-token"), ReadlineCmd::ForwardToken),
-    make_md(L!("forward-word"), ReadlineCmd::ForwardWord),
+    make_md(L!("forward-word"), ReadlineCmd::ForwardWordEmacs),
+    make_md(L!("forward-word-end"), ReadlineCmd::ForwardWordEnd),
+    make_md(L!("forward-word-vi"), ReadlineCmd::ForwardWordVi),
     make_md(L!("history-delete"), ReadlineCmd::HistoryDelete),
     make_md(L!("history-last-token-search-backward"), ReadlineCmd::HistoryLastTokenSearchBackward),
     make_md(L!("history-last-token-search-forward"), ReadlineCmd::HistoryLastTokenSearchForward),
@@ -171,15 +179,23 @@ const INPUT_FUNCTION_METADATA: &[InputFunctionMetadata] = &[
     make_md(L!("insert-line-under"), ReadlineCmd::InsertLineUnder),
     make_md(L!("jump-till-matching-bracket"), ReadlineCmd::JumpTillMatchingBracket),
     make_md(L!("jump-to-matching-bracket"), ReadlineCmd::JumpToMatchingBracket),
-    make_md(L!("kill-bigword"), ReadlineCmd::KillBigword),
+    make_md(L!("kill-a-bigword"), ReadlineCmd::KillABigWord),
+    make_md(L!("kill-a-word"), ReadlineCmd::KillAWord),
+    make_md(L!("kill-bigword"), ReadlineCmd::KillBigwordEmacs),
+    make_md(L!("kill-bigword-end"), ReadlineCmd::KillBigwordEnd),
+    make_md(L!("kill-bigword-vi"), ReadlineCmd::KillBigwordVi),
+    make_md(L!("kill-inner-bigword"), ReadlineCmd::KillInnerBigWord),
     make_md(L!("kill-inner-line"), ReadlineCmd::KillInnerLine),
+    make_md(L!("kill-inner-word"), ReadlineCmd::KillInnerWord),
     make_md(L!("kill-line"), ReadlineCmd::KillLine),
     make_md(L!("kill-path-component"), ReadlineCmd::KillPathComponent),
     make_md(L!("kill-selection"), ReadlineCmd::KillSelection),
     make_md(L!("kill-token"), ReadlineCmd::KillToken),
     make_md(L!("kill-whole-line"), ReadlineCmd::KillWholeLine),
-    make_md(L!("kill-word"), ReadlineCmd::KillWord),
-    make_md(L!("nextd-or-forward-word"), ReadlineCmd::NextdOrForwardWord),
+    make_md(L!("kill-word"), ReadlineCmd::KillWordEmacs),
+    make_md(L!("kill-word-end"), ReadlineCmd::KillWordEnd),
+    make_md(L!("kill-word-vi"), ReadlineCmd::KillWordVi),
+    make_md(L!("nextd-or-forward-word"), ReadlineCmd::NextdOrForwardWordEmacs),
     make_md(L!("or"), ReadlineCmd::FuncOr),
     make_md(L!("pager-toggle-search"), ReadlineCmd::PagerToggleSearch),
     make_md(L!("prevd-or-backward-word"), ReadlineCmd::PrevdOrBackwardWord),

src/input_common.rs
@@ -50,15 +50,21 @@ pub enum ReadlineCmd {
     BackwardCharPassive,
     ForwardSingleChar,
     ForwardCharPassive,
-    ForwardWord,
     BackwardWord,
-    ForwardBigword,
+    ForwardWordEmacs,
+    ForwardBigwordEmacs,
     BackwardBigword,
+    ForwardWordEnd,
+    BackwardWordEnd,
+    ForwardBigwordEnd,
+    BackwardBigwordEnd,
+    ForwardWordVi,
+    ForwardBigwordVi,
     ForwardPathComponent,
     ForwardToken,
     BackwardPathComponent,
     BackwardToken,
-    NextdOrForwardWord,
+    NextdOrForwardWordEmacs,
     PrevdOrBackwardWord,
     HistoryDelete,
     HistorySearchBackward,
@@ -81,13 +87,23 @@ pub enum ReadlineCmd {
     BackwardKillLine,
     KillWholeLine,
     KillInnerLine,
-    KillWord,
-    KillBigword,
+    KillWordEmacs,
+    KillBigwordEmacs,
+    KillWordVi,
+    KillBigwordVi,
+    KillWordEnd,
+    KillBigwordEnd,
+    KillInnerWord,
+    KillInnerBigWord,
+    KillAWord,
+    KillABigWord,
     KillPathComponent,
     KillToken,
     BackwardKillWord,
+    BackwardKillWordEnd,
     BackwardKillPathComponent,
     BackwardKillBigword,
+    BackwardKillBigwordEnd,
     BackwardKillToken,
     HistoryTokenSearchBackward,
     HistoryTokenSearchForward,

tests/pexpects/bind.py
@@ -325,6 +325,199 @@ expect_prompt(
     unmatched="Unexpected vi bind modes",
 )
 
+# Test word movements
+# Test 'w' with underscore - should not jump over single punctuation
+send("echo abc_def")
+send("\033")
+sleep(0.200)
+send("0wwD\r")  # From start, 'w' twice should stop at underscore, delete from there
+expect_prompt(
+    "\r\n.*abc\r\n", unmatched="vi mode 'w' should stop at single punctuation"
+)
+
+# Test 'w' with multiple spaces - should skip spaces and land at start of next word
+send("echo abc  def")
+send("\033")
+sleep(0.200)
+send("0wwwD\r")  # Skip 'echo', 'abc', 'def', then delete last char
+expect_prompt("\r\n.*abc de\r\n", unmatched="vi mode 'w' with multiple spaces")
+
+# Test 'w' with multiple punctuations - should stop at punctuation group
+send("echo abc...def")
+send("\033")
+sleep(0.200)
+send("0wwD\r")  # Skip 'echo', then 'w' should stop at first '.', delete to end
+expect_prompt("\r\n.*abc\r\n", unmatched="vi mode 'w' with multiple punctuations")
+
+# Test 'diw' when cursor is on space - should delete only spaces
+send("echo abc  def")
+send("\033")
+sleep(0.200)
+send("0wwhdiw\r")  # Move to 'def', back to space, delete inner word (spaces only)
+expect_prompt(
+    "\r\n.*abcdef\r\n", unmatched="vi mode 'diw' on space should delete spaces"
+)
+
+# Test 'daw' - should delete word and trim trailing space
+send("echo abc def ghi")
+send("\033")
+sleep(0.200)
+send("0wwdaw\r")  # Skip 'echo', move to 'def', delete word with space
+expect_prompt("\r\n.*abc ghi\r\n", unmatched="vi mode 'daw' should trim trailing space")
+
+# Test 'b' backward movement with punctuation - should stop at punctuation
+send("echo abc_def")
+send("\033")
+sleep(0.200)
+send("bD\r")  # From end, 'b' should stop at 'd', delete to end
+expect_prompt("\r\n.*abc_\r\n", unmatched="vi mode 'b' should stop at punctuation")
+
+# Test 'e' end-of-word movement
+send("echo abc_def")
+send("\033")
+sleep(0.200)
+send("0weD\r")  # From start, 'w' to 'abc', 'e' to end of 'abc', delete to end
+expect_prompt("\r\n.*ab\r\n", unmatched="vi mode 'e' should move to word end")
+
+# Test 'W' WORD movement - should skip punctuation within WORD
+send("echo abc-def ghi")
+send("\033")
+sleep(0.200)
+send("0wWD\r")  # From start, 'w' to 'abc', 'W' should skip 'abc-def', delete 'ghi'
+expect_prompt(
+    "\r\n.*abc-def\r\n",
+    unmatched="vi mode 'W' should treat punctuation as part of WORD",
+)
+
+# Test 'E' end-of-WORD movement
+send("echo abc-def ghi")
+send("\033")
+sleep(0.200)
+send("0wED\r")  # From start, 'w' to 'abc', 'E' to end of 'abc-def', delete to end
+expect_prompt("\r\n.*abc-de\r\n", unmatched="vi mode 'E' should move to WORD end")
+
+# Test 'B' backward WORD movement
+send("echo abc-def ghi")
+send("\033")
+sleep(0.200)
+send("BD\r")  # From end, 'B' backward to 'ghi', delete to end
+expect_prompt("\r\n.*abc-def\r\n", unmatched="vi mode 'B' backward WORD movement")
+
+# Test 'ge' backward to end of previous word
+send("echo abc def")
+send("\033")
+sleep(0.200)
+send("0wwgex\r")  # Move to 'def', 'ge' to 'c' of 'abc', delete char with 'x'
+expect_prompt(
+    "\r\n.*ab def\r\n", unmatched="vi mode 'ge' should move to previous word end"
+)
+
+# Test 'gE' backward to end of previous WORD
+send("echo abc-def ghi")
+send("\033")
+sleep(0.200)
+send(
+    "0WWgEx\r"
+)  # Use 'W' to move by WORDs: to 'abc-def', then 'ghi', then 'gE' back to 'f' of 'abc-def', delete char
+expect_prompt(
+    "\r\n.*abc-de ghi\r\n", unmatched="vi mode 'gE' should move to previous WORD end"
+)
+
+# Test 'diW' (delete inner WORD) with punctuation
+send("echo abc-def ghi")
+send("\033")
+sleep(0.200)
+send("0wldiW\r")  # Move to 'bc-def', delete inner WORD
+expect_prompt("\r\n.*ghi\r\n", unmatched="vi mode 'diW' should delete entire WORD")
+
+# Test 'daW' (delete a WORD) with punctuation
+send("echo abc-def ghi")
+send("\033")
+sleep(0.200)
+send("0wldaW\r")  # Move to 'bc-def', delete a WORD with space
+expect_prompt("\r\n.*ghi\r\n", unmatched="vi mode 'daW' should delete WORD and space")
+
+# Test Unicode character category separation
+# In vim, different unicode categories are separated into words
+send("echo abcあいう")
+send("\033")
+sleep(0.200)
+send("0wwD\r")  # Skip 'echo', then from 'a' of 'abc', 'w' should stop at 'あ'
+expect_prompt(
+    "\r\n.*abc\r\n", unmatched="vi mode 'w' should stop at Unicode category boundary"
+)
+
+# Test Unicode with multiple categories
+send("echo abcあいう甲乙")
+send("\033")
+sleep(0.200)
+send("0wwwD\r")  # Skip 'echo', 'abc', hiragana, then at kanji, delete to end
+expect_prompt(
+    "\r\n.*abcあいう\r\n", unmatched="vi mode 'w' should separate hiragana and kanji"
+)
+
+# Test 'cw' - change word, deletes to start of next word (like vim's 'dw')
+send("echo abc def")
+send("\033")
+sleep(0.200)
+send("0wcwXXX\r")  # Move to 'abc', 'cw' deletes 'abc ' (including space), type 'XXX'
+expect_prompt(
+    "\r\n.*XXXdef\r\n", unmatched="vi mode 'cw' should delete to start of next word"
+)
+
+# Test 'ce' - change to end of word (like vim's 'de')
+send("echo abc def")
+send("\033")
+sleep(0.200)
+send("0wceXXX\r")  # Move to 'abc', 'ce' deletes 'abc' (not the space), type 'XXX'
+expect_prompt(
+    "\r\n.*XXX def\r\n", unmatched="vi mode 'ce' should change to end of word"
+)
+
+# Test 'cW' - change WORD, deletes to start of next WORD (like vim's 'dW')
+send("echo abc-def ghi")
+send("\033")
+sleep(0.200)
+send(
+    "0wcWXXX\r"
+)  # Move to 'abc-def', 'cW' deletes 'abc-def ' (including space), type 'XXX'
+expect_prompt(
+    "\r\n.*XXXghi\r\n", unmatched="vi mode 'cW' should delete to start of next WORD"
+)
+
+# Test 'cE' - change to end of WORD (like vim's 'dE')
+send("echo abc-def ghi")
+send("\033")
+sleep(0.200)
+send(
+    "0wcEXXX\r"
+)  # Move to 'abc-def', 'cE' deletes 'abc-def' (not the space), type 'XXX'
+expect_prompt(
+    "\r\n.*XXX ghi\r\n", unmatched="vi mode 'cE' should change to end of WORD"
+)
+
+# Test running commands on empty line (should not crash)
+send("\033")
+sleep(0.200)
+send("dawdiwdwdedgedgE\r")  # run many commands
+expect_prompt()
+
+# Test accepting autosuggestions with w/W
+sendline("echo test-suggestion   test-suggestion")
+expect_prompt()
+send("echo te")
+sleep(0.100)
+send("\033")  # Enter normal mode
+sleep(0.200)
+send("w")  # forward-word-vi should accept 'st' from autosuggestion
+expect_str("echo test")
+send("w")  # forward-word-vi should accept '-'
+expect_str("echo test-")
+send("w")  # forward-word-vi should accept 'suggestion  ' from autosuggestion
+expect_str("echo test-suggestion   ")
+send("W\r")  # forward-word-vi should accept 'test-suggestion' from autosuggestion
+expect_prompt("test-suggestion   test-suggestion")
+
 # Switch back to regular (emacs mode) key bindings.
 sendline("set -g fish_key_bindings fish_default_key_bindings")
 expect_prompt()

commit 1356c3094384ff8c4d601411ef6a664d58fa185f

Fix vi key bindings: use -M insert instead of $argv for ctrl-right
Commit bbb2f0de8d added a ctrl-right binding to override the shared
binding with forward-word-vi for vi-compliance. However, it incorrectly
used $argv instead of -M insert, which caused the error:
  "bind: cannot parse key 'default'"

when calling fish_vi_key_bindings with a mode argument like:
  fish_vi_key_bindings "default"

The $argv variable contains the user's mode argument (e.g., "default"),
which was being passed to bind as a key name instead of a mode.

Fix by explicitly specifying -M insert to override the shared binding
for ctrl-right in insert mode only.

Fixes regression introduced in bbb2f0de8d.

Co-Authored-By: Claude Sonnet 4.5 <noreply@anthropic.com>

share/functions/fish_vi_key_bindings.fish
@@ -349,7 +349,7 @@ function fish_vi_key_bindings --description 'vi-like key bindings for fish'
     bind -s --preset ] history-token-search-forward
     bind -s --preset -m insert / history-pager repaint-mode
 
-    __fish_per_os_bind --preset $argv ctrl-right forward-token forward-word-vi
+    __fish_per_os_bind --preset -M insert ctrl-right forward-token forward-word-vi
     # ctrl-left is same as emacs mode
 
     bind -s --preset -M insert ctrl-n accept-autosuggestion
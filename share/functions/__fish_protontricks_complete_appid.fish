9235c5de6c8c9ed99a6c09d3e08e463d3706d73a
Janne Pulkkinen <janne.pulkkinen@protonmail.com>
2026-02-14 23:31:22 +0200

    completions/protontricks: use new flag for complete
    `protontricks -l` will launch a graphical prompt to choose Steam
    installation if multiple installations are found. `-L/--list-all`
    is a new flag introduced in 1.14.0 that retrieves all games without user
    interaction.
    
    Also silence stderr, since it can cause warning messages to be printed.
    
    Part of #12477

--- share/functions/__fish_protontricks_complete_appid.fish
+++ share/functions/__fish_protontricks_complete_appid.fish
@@ -1,6 +1,9 @@
 # localization: skip(private)
 function __fish_protontricks_complete_appid
-    protontricks -l |
+    # '-L' is a newer flag that lists all games without a graphical prompt
+    # if multiple Steam installations exist
+    set output "$(protontricks -L 2>/dev/null || protontricks -l 2>/dev/null)"
+    echo $output |
         string match --regex '.*\(\d+\)' |
         string replace --regex '(.*) \((\d+)\)' '$2\t$1'
 end

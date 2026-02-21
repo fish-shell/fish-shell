5b44c9668f4d00b2e969615efcde299038b94261
David Adam <zanchey@ucc.gu.uwa.edu.au>
2026-02-21 10:38:16 +0800

    add script to generate a dput.cf for Ubuntu PPA uploads

---
+++ build_tools/dput_cf_gen.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Script to generate the dput.cf for a set of Ubuntu series, prints the filename
+# Arguments are the PPA followed by the series names
+
+set -ex
+
+outfile=$(mktemp --tmpdir dput.XXXXX.cf)
+
+[ $# -lt 2 ] &&
+    echo "$0: at least two arguments (a PPA and at least one series) are required" >&2 &&
+    exit 1
+
+ppa=$1
+shift
+
+for series in "$@"; do
+    cat >> "$outfile" <<EOF
+[fish-$ppa-$series]
+fqdn = ppa.launchpad.net
+method = ftp
+login = anonymous
+incoming = ~fish-shell/$ppa/ubuntu/$series
+
+EOF
+done
+
+echo "$outfile"

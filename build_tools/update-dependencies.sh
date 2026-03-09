8f7007ffdc73b6f77b4c3d60db8ec9ad936620d0
Helen Chong <contact@helenchong.dev>
2026-03-10 05:55:22 +0800

    chore(update-dependencies.sh): pin Catppuccin theme repository commit for fetching

--- build_tools/update-dependencies.sh
+++ build_tools/update-dependencies.sh
@@ -11,7 +11,7 @@ command -v uv
 sort --version-sort </dev/null
 
 # TODO This is copied from .github/actions/install-sphinx/action.yml
-uv lock --check --exclude-newer="$(awk -F'"' <uv.lock '/^exclude-newer[[:space:]]*=/ {print $2}')"
+uv lock --check --exclude-newer="$(awk -F'"' '/^exclude-newer[[:space:]]*=/ {print $2}' <uv.lock)"
 
 update_gh_action() {
     repo=$1
@@ -45,14 +45,23 @@ from_gh() {
     path=$2
     destination=$3
     contents=$(curl -fsS https://raw.githubusercontent.com/"${repo}"/refs/heads/master/"${path}")
-    printf '%s\n' >"$destination" "$contents"
+    printf '%s\n' "$contents" >"$destination"
 }
 
 from_gh ridiculousfish/widecharwidth widechar_width.rs crates/widecharwidth/src/widechar_width.rs
 from_gh ridiculousfish/littlecheck littlecheck/littlecheck.py tests/littlecheck.py
-from_gh catppuccin/fish 'themes/Catppuccin Frappe.theme' share/themes/catppuccin-frappe.theme
-from_gh catppuccin/fish 'themes/Catppuccin Macchiato.theme' share/themes/catppuccin-macchiato.theme
-from_gh catppuccin/fish 'themes/Catppuccin Mocha.theme' share/themes/catppuccin-mocha.theme
+
+from_gh_ctp() {
+    ctp_flavor=$1
+    ctp_file_destination=$2
+    ctp_commit="bc201afe737fa0c8884ffcef206217f8aac88866"
+    ctp_contents=$(curl -fsS https://raw.githubusercontent.com/catppuccin/fish/"${ctp_commit}"/themes/"${ctp_flavor}".theme)
+    printf '%s\n' "$ctp_contents" >"$ctp_file_destination"
+}
+
+from_gh_ctp 'Catppuccin%20Frappe' share/themes/catppuccin-frappe.theme
+from_gh_ctp 'Catppuccin%20Macchiato' share/themes/catppuccin-macchiato.theme
+from_gh_ctp 'Catppuccin%20Mocha' share/themes/catppuccin-mocha.theme
 
 # Update Cargo.lock
 cargo update

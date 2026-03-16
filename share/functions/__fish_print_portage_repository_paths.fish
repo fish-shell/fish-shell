37c26a0b9255f0f8fdfb024e600ba952b47affa5
Steffen Winter <steffen.winter@proton.me>
2026-03-17 00:34:11 +0100

    functions: dynamically query gentoo system paths
    Use portageq to retrieve system paths instead of hardcoding them in.
    This helps especially in Gentoo Prefix, where the installation is not
    in / but rather offset inside a subdirectory (usually a users home
    directory).
    
    This only affects the "slow" path. When eix is installed it will be used
    instead. It already accounts for Prefix installations.

--- share/functions/__fish_print_portage_repository_paths.fish
+++ share/functions/__fish_print_portage_repository_paths.fish
@@ -1,17 +1,6 @@
 # localization: skip(private)
 function __fish_print_portage_repository_paths --description 'Print the paths of all configured repositories'
-    set -l a /etc/portage/repos.conf
-    set -l b
-    set -l c /usr/share/portage/config/repos.conf
-    test -d $a
-    and set b (path filter -f -- $a/**)
-    test -f $a
-    and set b $a
-    test -n "$b"
-    and string match -q "[gentoo]" -- (cat $b 2>/dev/null)
-    and set c $b
-    or set -a c $b
-    path is -r -- $c
-    or return
-    cat $c 2>/dev/null | string match -g -r '^location = (.*$)'
+    set -l eroot (portageq envvar EROOT)
+    set -l repos (portageq get_repos $eroot | string split ' ')
+    portageq get_repo_path $eroot $repos
 end

8cc05ef35d4112bfe8a4b619eb01965860b662b9
Steffen Winter <steffen.winter@proton.me>
2026-03-17 00:34:11 +0100

    functions: dynamically query gentoo system paths
    Use portageq to retrieve system paths instead of hardcoding them in.
    This helps especially in Gentoo Prefix, where the installation is not
    in / but rather offset inside a subdirectory (usually a users home
    directory).
    
    This only affects the "slow" path. When eix is installed it will be used
    instead. It already accounts for Prefix installations.

--- share/functions/__fish_print_portage_installed_pkgs.fish
+++ share/functions/__fish_print_portage_installed_pkgs.fish
@@ -3,6 +3,6 @@ function __fish_print_portage_installed_pkgs --description 'Print all installed 
     if type -f -q eix
         EIX_LIMIT=0 eix -I --only-names
     else
-        find /var/db/pkg -mindepth 2 -maxdepth 2 -type d -printf '%P\n' 2>/dev/null | string replace -r -- '-[0-9][0-9.]*.*$' ''
+        find (portageq envvar vdb_path) -mindepth 2 -maxdepth 2 -type d -printf '%P\n' 2>/dev/null | string replace -r -- '-[0-9][0-9.]*.*$' ''
     end
 end

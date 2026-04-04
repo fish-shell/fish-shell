9e25d0077cba18856f37dc620dd703f1792c17cc
Saúl Nogueras <susensio@gmail.com>
2026-04-04 15:04:31 +0200

    fish_commandline_append: don't add extra space character

--- share/functions/fish_commandline_append.fish
+++ share/functions/fish_commandline_append.fish
@@ -1,15 +1,21 @@
-function fish_commandline_append --description "Append the given string to the command-line, or remove the suffix if already there"
+function fish_commandline_append --description 'Append the given string to the command-line, or remove the suffix if already there'
     if not commandline | string length -q
         commandline -r $history[1]
     end
 
-    set -l escaped (string escape --style=regex -- $argv[1])
+    set -l suffix $argv[1]
+    set -l escaped (string escape --style=regex -- $suffix)
     set -l job (commandline -j | string collect)
+
+    if test (string sub --start -1 "$job") = " "
+        set suffix (string trim --left -- $suffix)
+    end
+
     if set job (string replace -r -- $escaped\$ "" $job)
         set -l cursor (commandline -C)
         commandline -rj -- $job
         commandline -C $cursor
     else
-        commandline -aj -- $argv[1]
+        commandline -aj -- $suffix
     end
 end

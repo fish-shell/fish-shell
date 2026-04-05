944ab91fabd64245207cc00dc52d10e8d1a78b9a
Nahor <nahor.j+github@gmail.com>
2026-04-04 18:11:29 -0700

    tests: various fixes for Cygwin itself and ACL mounts
    Most notably:
    - Unlike MSYS, Cygwin seems to always properly handle symlinks (at least
    in common scenarios)
    - With ACL, "x" permission also requires "r" do to anything, be it files
    or directories
    
    Closes #12642

--- tests/checks/sphinx-html.fish
+++ tests/checks/sphinx-html.fish
@@ -1,5 +1,8 @@
 #RUN: fish_indent=%fish_indent %fish %s
 #REQUIRES: command -v sphinx-build
+# Cygwin has some old version of python/sphinx/... triggering deprecation errors
+# (and MSYS doesn't have sphinx-build at all)
+#REQUIRES: %fish -c "not __fish_is_cygwin"
 
 set -l workspace_root (status dirname)/../..
 set -l build_script $workspace_root/tests/test_functions/sphinx-shared.sh

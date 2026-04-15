b0d30d608036901221828b0fb56a8ecc54b11644
Saúl Nogueras <susensio@gmail.com>
2026-04-15 08:46:16 +0200

    Handle escaped trailing whitespaces gracefully

--- share/functions/fish_commandline_append.fish
+++ share/functions/fish_commandline_append.fish
@@ -1,4 +1,4 @@
-function fish_commandline_append --description 'Append the given string to the command-line, or remove the suffix if already there'
+function fish_commandline_append --description "Append the given string to the command-line, or remove the suffix if already there"
     if not commandline | string length -q
         commandline -r $history[1]
     end
@@ -6,8 +6,12 @@ function fish_commandline_append --description 'Append the given string to the c
     set -l suffix $argv[1]
     set -l escaped (string escape --style=regex -- $suffix)
     set -l job (commandline -j | string collect)
+    set -l tokens (commandline -jx)
 
-    if test (string sub --start -1 "$job") = " "
+    # Avoid double spaces if trailing whitespace is a word separator
+    set -l last_raw_char (string sub --start -1 -- "$job")
+    set -l last_token_char (string sub --start -1 -- "$tokens[-1]")
+    if test $last_raw_char = " " && test $last_token_char != " "
         set suffix (string trim --left -- $suffix)
     end
 

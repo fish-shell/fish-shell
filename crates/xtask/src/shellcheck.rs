ca443e2e549c6ff1f20324313ceeb5128d5a8970
Daniel Rainer <daniel.rainer@localhost>
2026-04-20 18:54:17 +0200

    lint: add xtask for running ShellCheck
    ShellCheck does not have a built-in way of detecting which files it
    should check, so we use ripgrep's `ignore` library to find files not
    ignored by our gitignore rules, and then look for a non-fish shebang in
    the first line of the file. The resulting shell scripts are then passed
    to ShellCheck.
    
    Part of #12661

---
+++ crates/xtask/src/shellcheck.rs
@@ -0,0 +1,49 @@
+use fish_build_helper::workspace_root;
+use ignore::Walk;
+use pcre2::bytes::Regex;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::{Path, PathBuf},
+    process::Command,
+    sync::OnceLock,
+};
+
+pub fn shellcheck() {
+    let file_paths = files_to_check();
+    match Command::new("shellcheck")
+        .args(file_paths)
+        .current_dir(workspace_root())
+        .status()
+    {
+        Ok(status) => {
+            std::process::exit(status.code().unwrap_or(1));
+        }
+        Err(e) => {
+            eprintln!("Failed to run shellcheck: {e}");
+            std::process::exit(1);
+        }
+    }
+}
+
+fn is_shell_script<P: AsRef<Path>>(path: P) -> bool {
+    let file = File::open(&path).unwrap();
+    let mut first_line = String::new();
+    let Ok(_) = BufReader::new(file).read_line(&mut first_line) else {
+        return false;
+    };
+    static SHEBANG_REGEX: OnceLock<Regex> = OnceLock::new();
+    SHEBANG_REGEX
+        .get_or_init(|| Regex::new("^#!.*[^i]sh").unwrap())
+        .is_match(first_line.trim().as_bytes())
+        .unwrap()
+}
+
+fn files_to_check() -> Vec<PathBuf> {
+    Walk::new(workspace_root())
+        .map(|path| path.unwrap_or_else(|e| fail!("Error traversing workspace: {e}")))
+        .filter(|path| path.file_type().unwrap().is_file())
+        .map(|path| path.into_path())
+        .filter(|path| is_shell_script(path))
+        .collect()
+}

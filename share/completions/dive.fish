9956100b2826da3653f22de3eae8ee4f89e93710
Remo Senekowitsch <remo@buenzli.dev>
2026-05-07 10:49:24 +0200

    completions/dive: add image tags from podman

--- share/completions/dive.fish
+++ share/completions/dive.fish
@@ -4,4 +4,19 @@
 dive completion fish | source
 
 # Arguments
-complete -c dive -xa "(docker images --format '{{.Repository}}:{{.Tag}}' | command grep -v '<none>')"
+function __fish_docker_or_podman_image_tags
+    command -v docker >/dev/null
+    set --local docker_status $status
+    command -v podman >/dev/null
+    set --local podman_status $status
+
+    if test $docker_status -eq 0 && test $podman_status -eq 0
+        docker images --format '{{.Repository}}:{{.Tag}}' 2>/dev/null | command grep -v '<none>' | sed 's#^#docker://#'
+        podman images --format '{{.Repository}}:{{.Tag}}' 2>/dev/null | command grep -v '<none>' | sed 's#^#podman://#'
+    else if test $docker_status -eq 0
+        docker images --format '{{.Repository}}:{{.Tag}}' 2>/dev/null | command grep -v '<none>'
+    else if test $podman_status -eq 0
+        podman images --format '{{.Repository}}:{{.Tag}}' 2>/dev/null | command grep -v '<none>'
+    end
+end
+complete -c dive -xa "(__fish_docker_or_podman_image_tags)"

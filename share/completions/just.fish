fbf05f4128c08e5d88346d7fdd175cc848058d30
Remo Senekowitsch <remo@buenzli.dev>
2026-05-07 10:58:18 +0200

    completions/just: use upstream completions

--- share/completions/just.fish
+++ share/completions/just.fish
@@ -1,4 +1,1 @@
-function _justfile_targets
-    just -l | tail -n +2 | string trim -l | string replace -r '(\s*#\s*)' '\t' | string replace -r '(\s*[\*\+][^\s]*)' ''
-end
-complete -c just -f -a '(_justfile_targets)'
+just --completions fish | source

a32ce95846b9fad0f87c8c1a1de95dd20f8443f9
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2026-05-19 10:06:55 -0600

    fix: handle multiple arguments as error

--- share/functions/mkcd.fish
+++ share/functions/mkcd.fish
@@ -4,6 +4,10 @@
 #
 
 function mkcd --description "Create a directory and enter it"
+    if test (count $argv) -ne 1
+        echo "mkcd: expected 1 argument, got "(count $argv) >&2
+        return 1
+    end
     mkdir -p $argv
     and cd $argv
 end

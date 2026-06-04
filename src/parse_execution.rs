fd52c5dcdf22f87a1555bff5d297c6da9b3c2b26
Deepak kudi <deepakkudi23@adsl-172-10-9-116.dsl.sndg02.sbcglobal.net>
2026-06-04 09:08:55 +0530

    Propagate SIGINT from function processes

--- src/parse_execution.rs
+++ src/parse_execution.rs
@@ -130,6 +130,10 @@ impl ExecutionContext {
         &self.pstree
     }
 
+    pub fn cancel_signal(&self) -> Option<RawSignal> {
+        self.cancel_signal
+    }
+
     pub fn eval_node(
         &mut self,
         ctx: &mut OperationContext<'_>,

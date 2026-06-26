5fc7deab16bfd5c25ba68e5b3558182a97561893
Johannes Altmanninger <aclopte@gmail.com>
2026-06-26 14:53:22 +0200

    Fix crash on unexpected tcsetpgrp failure (Rust port regression)
    Fixes 77aeb6a2a88 (Port execution, 2023-10-08).
    Fixes #12841

--- tests/checks/tmux-job.fish
+++ tests/checks/tmux-job.fish
@@ -1,4 +1,4 @@
-#RUN: %fish %s
+#RUN: fish=%fish %fish %s
 #REQUIRES: command -v tmux
 
 isolated-tmux-start
@@ -18,7 +18,25 @@ isolated-tmux capture-pane -p
 # (I've seen this print " world", I guess it depends on tmux version and how big it thinks the terminal is)
 # CHECK: {{.*}} world
 
-isolated-tmux send-keys C-u C-l "sleep 3 | cat &" Enter "bg %1" Enter
+isolated-tmux send-keys C-u C-l "sleep 1 | cat &" Enter "bg %1" Enter
 tmux-sleep
 isolated-tmux capture-pane -p | string match '*to background*'
-# CHECK: Send job 1 'sleep 3 | cat &' to background
+# CHECK: Send job 1 'sleep 1 | cat &' to background
+
+sleep 1.5
+
+isolated-tmux send-keys C-l $fish -c '
+    status job-control full
+    for x in (seq 10)
+        sleep 0.0001 &
+        fg
+    end
+'
+tmux-sleep
+isolated-tmux capture-pane -p |
+    string match -rv '^fg: There are no suitable jobs' |
+    string match -rv '^killpg\(\d+, SIGCONT\): No such process$' |
+    string match -rv '^Send job \d+ \(sleep 0.0001 &\) to foreground$' |
+    string match -rv '^warning: Could not send job \d+ \(\'sleep 0.0001 &\'\) with pgid \d+ to foreground' |
+    string match -rv '^tcsetpgrp: No such process'
+# CHECK: prompt 4>

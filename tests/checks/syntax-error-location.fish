fish-shell/fish-shell

commit abaf54f0f8e2f331e4c3038c1cfb21cfbdcc7a04

tokenizer: support nested variable slices
Fixes #3339
Fixes #12894

Closes #12903

localization/po/de.po
@@ -1406,19 +1406,17 @@ msgstr ""
 msgid "Unable to read input file: %s"
 msgstr ""
 
-msgid "Unexpected ')' for unopened parenthesis"
+#, c-format
+msgid "Unexpected '%s' found, expecting '%s'"
 msgstr ""
 
-msgid "Unexpected ')' found, expecting '}'"
+msgid "Unexpected ')' for unopened parenthesis"
 msgstr ""
 
 msgid "Unexpected '[' at this location"
 msgstr ""
 
 msgid "Unexpected '}' for unopened brace"
-msgstr ""
-
-msgid "Unexpected '}' found, expecting ')'"
 msgstr ""
 
 #, c-format

localization/po/es.po
@@ -1406,20 +1406,18 @@ msgstr "No se puede ubicar el directorio %s."
 msgid "Unable to read input file: %s"
 msgstr "No se puede leer el archivo de entrada: %s"
 
+#, c-format
+msgid "Unexpected '%s' found, expecting '%s'"
+msgstr "Se encontró ‘%s’, pero se esperaba ‘%s’"
+
 msgid "Unexpected ')' for unopened parenthesis"
 msgstr "‘)’ inesperado sin un paréntesis de apertura"
-
-msgid "Unexpected ')' found, expecting '}'"
-msgstr "Se encontró ‘)’, pero se esperaba ‘}’"
 
 msgid "Unexpected '[' at this location"
 msgstr "‘[’ inesperado en esta ubicación"
 
 msgid "Unexpected '}' for unopened brace"
 msgstr "‘}’ inesperado sin una llave de apertura"
-
-msgid "Unexpected '}' found, expecting ')'"
-msgstr "Se encontró ‘}’, pero se esperaba ‘)’"
 
 #, c-format
 msgid "Unexpected argument -- '%s'"

localization/po/fr.po
@@ -1535,19 +1535,17 @@ msgstr "Impossible de localiser le dossier %s."
 msgid "Unable to read input file: %s"
 msgstr ""
 
-msgid "Unexpected ')' for unopened parenthesis"
+#, c-format
+msgid "Unexpected '%s' found, expecting '%s'"
 msgstr ""
 
-msgid "Unexpected ')' found, expecting '}'"
+msgid "Unexpected ')' for unopened parenthesis"
 msgstr ""
 
 msgid "Unexpected '[' at this location"
 msgstr ""
 
 msgid "Unexpected '}' for unopened brace"
-msgstr ""
-
-msgid "Unexpected '}' found, expecting ')'"
 msgstr ""
 
 #, c-format

localization/po/ja_JP.po
@@ -1405,20 +1405,18 @@ msgstr "%s ディレクトリを特定できません"
 msgid "Unable to read input file: %s"
 msgstr "入力ファイルを読み取れません: %s"
 
+#, c-format
+msgid "Unexpected '%s' found, expecting '%s'"
+msgstr "予期しない '%s' が見つかりました。 '%s' が必要です"
+
 msgid "Unexpected ')' for unopened parenthesis"
 msgstr "開かれていない括弧に対応する予期しない ')' があります"
-
-msgid "Unexpected ')' found, expecting '}'"
-msgstr "予期しない ')' が見つかりました。 '}' が必要です"
 
 msgid "Unexpected '[' at this location"
 msgstr "この場所に予期しない '[' があります"
 
 msgid "Unexpected '}' for unopened brace"
 msgstr "開かれていない中括弧に対応する予期しない '}' があります"
-
-msgid "Unexpected '}' found, expecting ')'"
-msgstr "予期しない '}' が見つかりました。 ')' が必要です"
 
 #, c-format
 msgid "Unexpected argument -- '%s'"

localization/po/pl.po
@@ -1402,19 +1402,17 @@ msgstr ""
 msgid "Unable to read input file: %s"
 msgstr ""
 
-msgid "Unexpected ')' for unopened parenthesis"
+#, c-format
+msgid "Unexpected '%s' found, expecting '%s'"
 msgstr ""
 
-msgid "Unexpected ')' found, expecting '}'"
+msgid "Unexpected ')' for unopened parenthesis"
 msgstr ""
 
 msgid "Unexpected '[' at this location"
 msgstr ""
 
 msgid "Unexpected '}' for unopened brace"
-msgstr ""
-
-msgid "Unexpected '}' found, expecting ')'"
 msgstr ""
 
 #, c-format

localization/po/pt_BR.po
@@ -1407,19 +1407,17 @@ msgstr ""
 msgid "Unable to read input file: %s"
 msgstr ""
 
-msgid "Unexpected ')' for unopened parenthesis"
+#, c-format
+msgid "Unexpected '%s' found, expecting '%s'"
 msgstr ""
 
-msgid "Unexpected ')' found, expecting '}'"
+msgid "Unexpected ')' for unopened parenthesis"
 msgstr ""
 
 msgid "Unexpected '[' at this location"
 msgstr ""
 
 msgid "Unexpected '}' for unopened brace"
-msgstr ""
-
-msgid "Unexpected '}' found, expecting ')'"
 msgstr ""
 
 #, c-format

localization/po/sv.po
@@ -1403,19 +1403,17 @@ msgstr ""
 msgid "Unable to read input file: %s"
 msgstr ""
 
-msgid "Unexpected ')' for unopened parenthesis"
+#, c-format
+msgid "Unexpected '%s' found, expecting '%s'"
 msgstr ""
 
-msgid "Unexpected ')' found, expecting '}'"
+msgid "Unexpected ')' for unopened parenthesis"
 msgstr ""
 
 msgid "Unexpected '[' at this location"
 msgstr ""
 
 msgid "Unexpected '}' for unopened brace"
-msgstr ""
-
-msgid "Unexpected '}' found, expecting ')'"
 msgstr ""
 
 #, c-format

localization/po/zh_CN.po
@@ -1427,20 +1427,18 @@ msgstr "无法找到目录 %s。"
 msgid "Unable to read input file: %s"
 msgstr "无法读取输入文件：%s"
 
+#, c-format
+msgid "Unexpected '%s' found, expecting '%s'"
+msgstr "意外找到 '%s'，期待 '%s'"
+
 msgid "Unexpected ')' for unopened parenthesis"
 msgstr "未闭合的括号前出现意外的 ')'"
-
-msgid "Unexpected ')' found, expecting '}'"
-msgstr "意外找到 ')'，期待 '}'"
 
 msgid "Unexpected '[' at this location"
 msgstr "此位置意外找到 '['"
 
 msgid "Unexpected '}' for unopened brace"
 msgstr "未闭合的大括号前出现意外的 '}'"
-
-msgid "Unexpected '}' found, expecting ')'"
-msgstr "意外找到 '}'，期待 ')'"
 
 #, c-format
 msgid "Unexpected argument -- '%s'"

commit e45e146ce036b7fbaf50ca2aa9231e0a84672bf0

tokenizer: fix crash caused by char/string mix-up
(introduced by abaf54f0f8)

localization/po/de.po
@@ -1407,7 +1407,7 @@ msgid "Unable to read input file: %s"
 msgstr ""
 
 #, c-format
-msgid "Unexpected '%s' found, expecting '%s'"
+msgid "Unexpected '%c' found, expecting '%c'"
 msgstr ""
 
 msgid "Unexpected ')' for unopened parenthesis"

localization/po/es.po
@@ -1407,7 +1407,7 @@ msgid "Unable to read input file: %s"
 msgstr "No se puede leer el archivo de entrada: %s"
 
 #, c-format
-msgid "Unexpected '%s' found, expecting '%s'"
+msgid "Unexpected '%c' found, expecting '%c'"
 msgstr ""
 
 msgid "Unexpected ')' for unopened parenthesis"

localization/po/fr.po
@@ -1536,7 +1536,7 @@ msgid "Unable to read input file: %s"
 msgstr ""
 
 #, c-format
-msgid "Unexpected '%s' found, expecting '%s'"
+msgid "Unexpected '%c' found, expecting '%c'"
 msgstr ""
 
 msgid "Unexpected ')' for unopened parenthesis"

localization/po/ja_JP.po
@@ -1406,7 +1406,7 @@ msgid "Unable to read input file: %s"
 msgstr "入力ファイルを読み取れません: %s"
 
 #, c-format
-msgid "Unexpected '%s' found, expecting '%s'"
+msgid "Unexpected '%c' found, expecting '%c'"
 msgstr ""
 
 msgid "Unexpected ')' for unopened parenthesis"

localization/po/pl.po
@@ -1403,7 +1403,7 @@ msgid "Unable to read input file: %s"
 msgstr ""
 
 #, c-format
-msgid "Unexpected '%s' found, expecting '%s'"
+msgid "Unexpected '%c' found, expecting '%c'"
 msgstr ""
 
 msgid "Unexpected ')' for unopened parenthesis"

localization/po/pt_BR.po
@@ -1408,7 +1408,7 @@ msgid "Unable to read input file: %s"
 msgstr ""
 
 #, c-format
-msgid "Unexpected '%s' found, expecting '%s'"
+msgid "Unexpected '%c' found, expecting '%c'"
 msgstr ""
 
 msgid "Unexpected ')' for unopened parenthesis"

localization/po/sv.po
@@ -1404,7 +1404,7 @@ msgid "Unable to read input file: %s"
 msgstr ""
 
 #, c-format
-msgid "Unexpected '%s' found, expecting '%s'"
+msgid "Unexpected '%c' found, expecting '%c'"
 msgstr ""
 
 msgid "Unexpected ')' for unopened parenthesis"

localization/po/zh_CN.po
@@ -1428,7 +1428,7 @@ msgid "Unable to read input file: %s"
 msgstr "无法读取输入文件：%s"
 
 #, c-format
-msgid "Unexpected '%s' found, expecting '%s'"
+msgid "Unexpected '%c' found, expecting '%c'"
 msgstr ""
 
 msgid "Unexpected ')' for unopened parenthesis"

localization/po/zh_TW.po
@@ -1402,7 +1402,7 @@ msgid "Unable to read input file: %s"
 msgstr "無法讀取輸入檔案：%s"
 
 #, c-format
-msgid "Unexpected '%s' found, expecting '%s'"
+msgid "Unexpected '%c' found, expecting '%c'"
 msgstr ""
 
 msgid "Unexpected ')' for unopened parenthesis"

src/tokenizer.rs
@@ -145,7 +145,7 @@ pub const TOK_CONTINUE_AFTER_ERROR: TokFlags = TokFlags(8);
 pub const TOK_ARGUMENT_LIST: TokFlags = TokFlags(16);
 
 localizable_consts!(
-    UNEXPECTED_CLOSING_CHAR "Unexpected '%s' found, expecting '%s'"
+    UNEXPECTED_CLOSING_CHAR "Unexpected '%c' found, expecting '%c'"
 );
 
 impl From<TokenizerError> for WString {

tests/checks/syntax-error-location.fish
@@ -138,3 +138,28 @@ $fish -c 'echo (echo *nosuchname*)'
 # CHECKERR: fish: Unmatched wildcard
 # CHECKERR: echo (echo *nosuchname*)
 # CHECKERR: ^~~~~~~~~~~~~~~~~~^
+
+$fish -c 'foo )'
+# CHECKERR: fish: Unexpected ')' for unopened parenthesis
+# CHECKERR: foo )
+# CHECKERR:     ^
+
+$fish -c 'foo (})'
+# CHECKERR: fish: Unexpected '}' found, expecting ')'
+# CHECKERR: foo (})
+# CHECKERR:      ^
+
+$fish -c 'foo {)}'
+# CHECKERR: fish: Unexpected ')' found, expecting '}'
+# CHECKERR: foo {)}
+# CHECKERR:      ^
+
+$fish -c 'foo $var[)]'
+# CHECKERR: fish: Unexpected ')' found, expecting ']'
+# CHECKERR: foo $var[)]
+# CHECKERR:          ^
+
+$fish -c 'foo $var[}]'
+# CHECKERR: fish: Unexpected '}' found, expecting ']'
+# CHECKERR: foo $var[}]
+# CHECKERR:          ^
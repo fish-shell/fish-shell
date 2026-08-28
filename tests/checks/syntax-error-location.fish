f64f8d41002db84f5bc420462f8066c5749ec9ea
Nahor <nahor.j+github@gmail.com>
2026-08-28 10:56:01 -0700

    tests/syntax-error-location: align carets in more error messages

--- tests/checks/syntax-error-location.fish
+++ tests/checks/syntax-error-location.fish
@@ -45,124 +45,124 @@ $fish -c 'echo "ok $(echo still ok)syntax error: \x"' 2>| string replace -r '.*'
 # CHECK: <                         ^~~~~~~~~~~~~~~~^>
 
 echo "function this_should_be_an_error" >$TMPDIR/this_should_be_an_error.fish
-$fish -c "set -g fish_function_path $(string escape $TMPDIR); this_should_be_an_error" 2>| string replace -r "^ {$(string length "$TMPDIR")}" "" >&2
+$fish -c "set -g fish_function_path $(string escape $TMPDIR); this_should_be_an_error" 2>| string replace -r "^ {$(string length "$TMPDIR")}" "      " >&2
 # CHECKERR: ~/temp/this_should_be_an_error.fish (line 1): Missing end to balance this function definition
-# CHECKERR: function this_should_be_an_error
+# CHECKERR: {{^}}function this_should_be_an_error
 # CHECKERR: {{^}}^~~~~~~^
 # CHECKERR: from sourcing file ~/temp/this_should_be_an_error.fish
 # CHECKERR: source: Error while reading file '{{.*}}/this_should_be_an_error.fish'
 # CHECKERR: fish: Unknown command: this_should_be_an_error
 # CHECKERR: fish:
-# CHECKERR: set -g fish_function_path {{.*}}; this_should_be_an_error
-# CHECKERR: {{^                            }}^~~~~~~~~~~~~~~~~~~~~~^
+# CHECKERR: {{^}}set -g fish_function_path {{.*}}; this_should_be_an_error
+# CHECKERR: {{^}}                                  ^~~~~~~~~~~~~~~~~~~~~~^
 
 $fish -c 'echo {$}'
 # CHECKERR: fish: Expected a variable name after this $.
-# CHECKERR: echo {$}
-# CHECKERR: {{^       }}^
+# CHECKERR: {{^}}echo {$}
+# CHECKERR: {{^}}       ^
 
 $fish -c 'echo {$,}'
 # CHECKERR: fish: Expected a variable name after this $.
-# CHECKERR: echo {$,}
-# CHECKERR: {{^       }}^
+# CHECKERR: {{^}}echo {$,}
+# CHECKERR: {{^}}       ^
 
 echo "bind -M" | $fish
 # CHECKERR: bind: -M: option requires an argument
 # CHECKERR: Standard input (line 1):
-# CHECKERR: bind -M
+# CHECKERR: {{^}}bind -M
 # CHECKERR: {{^}}^
 # CHECKERR: (Type 'help bind' for related documentation)
 
 $fish -c 'if -e; end'
 # CHECKERR: fish: Unknown command: -e
 # CHECKERR: fish:
-# CHECKERR: if -e; end
-# CHECKERR: {{^   }}^^
+# CHECKERR: {{^}}if -e; end
+# CHECKERR: {{^}}   ^^
 
 $fish -c 'begin --notanoption; end'
 # CHECKERR: fish: Unknown command: --notanoption
 # CHECKERR: fish:
-# CHECKERR: begin --notanoption; end
-# CHECKERR: {{^      }}^~~~~~~~~~~~^
+# CHECKERR: {{^}}begin --notanoption; end
+# CHECKERR: {{^}}      ^~~~~~~~~~~~^
 
 $fish -c 'begin --help'
 # CHECKERR: Documentation for begin
 
 $fish -c 'echo (for status in foo; end)'
 # CHECKERR: fish: for: status: cannot overwrite read-only variable
-# CHECKERR: for status in foo; end
-# CHECKERR: {{^    }}^~~~~^
+# CHECKERR: {{^}}for status in foo; end
+# CHECKERR: {{^}}    ^~~~~^
 # CHECKERR: in command substitution
 # CHECKERR: fish: Invalid arguments
-# CHECKERR: echo (for status in foo; end)
-# CHECKERR: {{^     }}^~~~~~~~~~~~~~~~~~~~~~~^
+# CHECKERR: {{^}}echo (for status in foo; end)
+# CHECKERR: {{^}}     ^~~~~~~~~~~~~~~~~~~~~~~^
 
 $fish -c 'echo (echo <&foo)'
 # CHECKERR: fish: Requested redirection to 'foo', which is not a valid file descriptor
-# CHECKERR: echo <&foo
-# CHECKERR: {{^     }}^~~~^
+# CHECKERR: {{^}}echo <&foo
+# CHECKERR: {{^}}     ^~~~^
 # CHECKERR: in command substitution
 # CHECKERR: fish: Invalid arguments
-# CHECKERR: echo (echo <&foo)
-# CHECKERR: {{^     }}^~~~~~~~~~~^
+# CHECKERR: {{^}}echo (echo <&foo)
+# CHECKERR: {{^}}     ^~~~~~~~~~~^
 
 
 $fish -c 'echo (time echo foo &)'
 # CHECKERR: fish: 'time' is not supported for background jobs. Consider using 'command time'.
-# CHECKERR: time echo foo &
+# CHECKERR: {{^}}time echo foo &
 # CHECKERR: {{^}}^~~~~~~~~~~~~~^
 # CHECKERR: in command substitution
 # CHECKERR: fish: Invalid arguments
-# CHECKERR: echo (time echo foo &)
-# CHECKERR: {{^     }}^~~~~~~~~~~~~~~~^
+# CHECKERR: {{^}}echo (time echo foo &)
+# CHECKERR: {{^}}     ^~~~~~~~~~~~~~~~^
 
 $fish -c 'time begin; end &'
 # CHECKERR: fish: 'time' is not supported for background jobs. Consider using 'command time'.
-# CHECKERR: time begin; end &
+# CHECKERR: {{^}}time begin; end &
 # CHECKERR: {{^}}^~~~~~~~~~~~~~~~^
 
 $fish -c 'echo (set -l foo 1 2 3; for $foo in foo; end)'
 # CHECKERR: fish: Unable to expand variable name ''
-# CHECKERR: set -l foo 1 2 3; for $foo in foo; end
-# CHECKERR: {{^                      }}^~~^
+# CHECKERR: {{^}}set -l foo 1 2 3; for $foo in foo; end
+# CHECKERR: {{^}}                      ^~~^
 # CHECKERR: in command substitution
 # CHECKERR: fish: Expansion error
-# CHECKERR: echo (set -l foo 1 2 3; for $foo in foo; end)
-# CHECKERR: {{^     }}^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^
+# CHECKERR: {{^}}echo (set -l foo 1 2 3; for $foo in foo; end)
+# CHECKERR: {{^}}     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^
 
 $fish -c 'echo (echo *nosuchname*)'
 # CHECKERR: fish: No matches for wildcard '*nosuchname*'. See `help language#wildcards-globbing`.
-# CHECKERR: echo *nosuchname*
-# CHECKERR: {{^     }}^~~~~~~~~~~^
+# CHECKERR: {{^}}echo *nosuchname*
+# CHECKERR: {{^}}     ^~~~~~~~~~~^
 # CHECKERR: in command substitution
 # CHECKERR: fish: Unmatched wildcard
-# CHECKERR: echo (echo *nosuchname*)
-# CHECKERR: {{^     }}^~~~~~~~~~~~~~~~~~^
+# CHECKERR: {{^}}echo (echo *nosuchname*)
+# CHECKERR: {{^}}     ^~~~~~~~~~~~~~~~~~^
 
 $fish -c 'foo )'
 # CHECKERR: fish: Unexpected ')' for unopened parenthesis
-# CHECKERR: foo )
-# CHECKERR: {{^    }}^
+# CHECKERR: {{^}}foo )
+# CHECKERR: {{^}}    ^
 
 $fish -c 'foo (})'
 # CHECKERR: fish: Unexpected '}' found, expecting ')'
-# CHECKERR: foo (})
-# CHECKERR: {{^     }}^
+# CHECKERR: {{^}}foo (})
+# CHECKERR: {{^}}     ^
 
 $fish -c 'foo {)}'
 # CHECKERR: fish: Unexpected ')' found, expecting '}'
-# CHECKERR: foo {)}
-# CHECKERR: {{^     }}^
+# CHECKERR: {{^}}foo {)}
+# CHECKERR: {{^}}     ^
 
 $fish -c 'foo $var[)]'
 # CHECKERR: fish: Unexpected ')' found, expecting ']'
-# CHECKERR: foo $var[)]
-# CHECKERR: {{^         }}^
+# CHECKERR: {{^}}foo $var[)]
+# CHECKERR: {{^}}         ^
 
 $fish -c 'foo $var[}]'
 # CHECKERR: fish: Unexpected '}' found, expecting ']'
-# CHECKERR: foo $var[}]
-# CHECKERR: {{^         }}^
+# CHECKERR: {{^}}foo $var[}]
+# CHECKERR: {{^}}         ^
 
 $fish -c 'echo foo"bar$(echo)$(echo)bur'
 # CHECKERR: fish: Unexpected end of string, quotes are not balanced

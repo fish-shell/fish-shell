830fe4b9332f5f2e8394f7b71058bc47a0609510
nekoside <dev@nekoside.com>
2026-09-10 11:09:47 +0800

    completion: fix typo for passwd

--- share/completions/passwd.fish
+++ share/completions/passwd.fish
@@ -22,7 +22,7 @@ if passwd --help >/dev/null 2>&1
     complete -c passwd -s S -l status -f -d "Display account status"
     complete -c passwd -s u -l unlock -f -d "Unlock password"
     complete -c passwd -s w -l warndays -x -d "Define warning period before mandatory password change"
-    complete -c passwd -s w -l warndays -x -d "Define maximum period of password validity"
+    complete -c passwd -s x -l maxdays -x -d "Define maximum period of password validity"
     complete -c passwd -n '__fish_not_contain_opt -s A all' -f -a '(__fish_complete_users)' -d "Account to be altered"
 else # Not Linux, so let's see what it is, with the ugly uname
     set -l os_type (uname)

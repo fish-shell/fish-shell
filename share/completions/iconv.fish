16a944aa5629e63cd342502704f67d851f467dde
Johannes Altmanninger <aclopte@gmail.com>
2026-09-13 16:57:09 +0000

    completions/iconv: fix help/version descriptions and add -l short option

--- share/completions/iconv.fish
+++ share/completions/iconv.fish
@@ -1,7 +1,7 @@
 complete -c iconv -s f -l from-code -d "Convert from specified encoding" -x -a "(__fish_print_encodings)"
 complete -c iconv -s t -l to-code -d "Convert to specified encoding" -x -a "(__fish_print_encodings)"
-complete -c iconv -l list -d "List known coded character sets"
+complete -c iconv -s l -l list -d "List known coded character sets"
 complete -c iconv -s o -l output -d "Output file" -r
 complete -c iconv -l verbose -d "Print progress information"
-complete -c iconv -l help -d "Display version and exit"
-complete -c iconv -l version -d "Display help and exit"
+complete -c iconv -l help -d "Display help and exit"
+complete -c iconv -l version -d "Display version and exit"

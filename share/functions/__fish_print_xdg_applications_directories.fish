f533b81c3fddd9f70100fd9c16c74629cd3253b6
Kitty Makin <autumnull@posteo.net>
2026-09-20 06:46:28 +0100

    Fix krita completions
    - Only apply image format suggestions to --new-image
    - Respect $XDG_DATA_HOME/XDG_DATA_DIRS
    - Combine shared logic into __fish_print_xdg_data_directories

--- share/functions/__fish_print_xdg_applications_directories.fish
+++ share/functions/__fish_print_xdg_applications_directories.fish
@@ -1,19 +1,6 @@
 # localization: skip(private)
 function __fish_print_xdg_applications_directories --description 'Print directories where desktop files are stored'
-    set -l data_home $XDG_DATA_HOME
-    if test -z "$data_home"
-        set data_home $HOME/.local/share/
-    end
-
-    set -l data_dirs $XDG_DATA_DIRS
-    if test -z "$data_dirs"
-        set data_dirs /usr/local/share/:/usr/share/
-    end
-
-    set data_dirs $data_home:$data_dirs
-
-    # We don't know if the dir ended in a "/" or not, but duplicate slashes are okay.
-    for path in (string split : $data_dirs)/applications
+    for path in (__fish_print_xdg_data_directories)/applications
         if test -d $path
             echo $path
         end

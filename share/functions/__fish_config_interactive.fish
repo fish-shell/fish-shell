950eb3fd8313e7ffb57bcd2b0dd54bcf67d7c312
Aram Drevekenin <aram@poor.dev>
2026-09-25 10:16:12 +0200

    __fish_config_interactive: extract regex building

--- share/functions/__fish_config_interactive.fish
+++ share/functions/__fish_config_interactive.fish
@@ -119,7 +119,12 @@ function __fish_config_interactive -d "Initializations that should be performed 
         # Konsole reflows since version 21.04. Konsole added XTVERSION
         # in v22.03.80~7.
         # TODO(term-workaround)
-        if string match -rq -- '^(?:VTE\b|Konsole |WezTerm )' (status terminal)
+        set -l re (string escape --style=regex -- \
+                'Konsole ' \
+                'VTE(' \
+                'WezTerm ' \
+            | string join '|')
+        if string match -rq -- "^(?:$re)" (status terminal)
             or begin
                 set -q KONSOLE_VERSION
                 and test "$KONSOLE_VERSION" -ge 210400 2>/dev/null
